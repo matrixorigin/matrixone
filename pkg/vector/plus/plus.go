1de3ad3a3ac0fa93ccf31f2c2b8a7a38de17c8ce
bRong Njam <longran1989@gmail.com>
2021-02-20 15:25:13 +0800

    Add AVX2 implementation of a + b

--- pkg/vector/plus/plus.go
+++ pkg/vector/plus/plus.go
@@ -1,5 +1,7 @@
 package plus
 
+import "matrixbase/pkg/internal/cpu"
+
 var (
 	i64PlusOne func(int64, []int64, []int64) []int64
 	i64Plus    func([]int64, []int64, []int64) []int64
@@ -9,12 +11,25 @@ var (
 )
 
 func init() {
-	i64Plus = i64PlusPure
-	i64PlusOne = i64PlusOnePure
+	if cpu.X86.HasAVX2 {
+		i64Plus = i64PlusAvx
+		i64PlusOne = i64PlusOneAvx
 
-	f64Plus = f64PlusPure
-	f64PlusOne = f64PlusOnePure
+		f64Plus = f64PlusAvx
+		f64PlusOne = f64PlusOneAvx
+	} else {
+		i64Plus = i64PlusPure
+		i64PlusOne = i64PlusOnePure
+
+		f64Plus = f64PlusPure
+		f64PlusOne = f64PlusOnePure
+	}
 }
+
+func iPlusAvx([]int64, []int64, []int64)
+func iPlusOneAvx(int64, []int64, []int64)
+func fPlusAvx([]float64, []float64, []float64)
+func fPlusOneAvx(float64, []float64, []float64)
 
 func I64Plus(xs, ys, rs []int64) []int64 {
 	return i64Plus(xs, ys, rs)
@@ -30,6 +45,42 @@ func F64Plus(xs, ys, rs []float64) []float64 {
 
 func F64PlusOne(x float64, ys, rs []float64) []float64 {
 	return f64PlusOnePure(x, ys, rs)
+}
+
+func i64PlusAvx(xs, ys, rs []int64) []int64 {
+	n := len(xs) / 4
+	iPlusAvx(xs[:n*4], ys[:n*4], rs[:n*4])
+	for i, j := n*4, len(xs); i < j; i++ {
+		rs[i] = xs[i] + ys[i]
+	}
+	return rs
+}
+
+func i64PlusOneAvx(x int64, ys, rs []int64) []int64 {
+	n := len(ys) / 4
+	iPlusOneAvx(x, ys[:n*4], rs[:n*4])
+	for i, j := n*4, len(ys); i < j; i++ {
+		rs[i] = x + ys[i]
+	}
+	return rs
+}
+
+func f64PlusAvx(xs, ys, rs []float64) []float64 {
+	n := len(xs) / 4
+	fPlusAvx(xs[:n*4], ys[:n*4], rs[:n*4])
+	for i, j := n*4, len(xs); i < j; i++ {
+		rs[i] = xs[i] + ys[i]
+	}
+	return rs
+}
+
+func f64PlusOneAvx(x float64, ys, rs []float64) []float64 {
+	n := len(ys) / 4
+	fPlusOneAvx(x, ys[:n*4], rs[:n*4])
+	for i, j := n*4, len(ys); i < j; i++ {
+		rs[i] = x + ys[i]
+	}
+	return rs
 }
 
 func i64PlusPure(xs, ys, rs []int64) []int64 {
