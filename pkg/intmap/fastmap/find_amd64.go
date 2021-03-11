70a809e85a589fd546ad774e49dc465ff5513398
nnsgmsone <nnsmgsone@outlook.com>
2021-03-11 14:29:03 +0800

    Add intmap

---
+++ pkg/intmap/fastmap/find_amd64.go
@@ -0,0 +1,26 @@
+package fastmap
+
+import "matrixbase/pkg/internal/cpu"
+
+func findAvx([]uint64, uint64) int
+
+var (
+	Find func([]uint64, uint64) int
+)
+
+func init() {
+	if cpu.X86.HasAVX2 {
+		Find = findAvx
+	} else {
+		Find = findPure
+	}
+}
+
+func findPure(xs []uint64, v uint64) int {
+	for i, x := range xs {
+		if x == v {
+			return i
+		}
+	}
+	return -1
+}
