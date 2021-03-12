a5baf70352ed7fc106adc1dc225daaf3bf620ecc
bRong Njam <longran1989@gmail.com>
2021-03-12 20:59:49 +0800

    Add avx2/avx512 implementation of LENGTH

--- pkg/vectorize/length/length.go
+++ pkg/vectorize/length/length.go
@@ -1,21 +1,51 @@
 package length
 
-import "matrixbase/pkg/container/vector"
+import (
+	"matrixbase/pkg/container/types"
+
+	"golang.org/x/sys/cpu"
+)
 
 var (
-	bytesLength func(*vector.Bytes, []int64) []int64
+	strLength func(*types.Bytes, []int64) []int64
 )
 
 func init() {
-	bytesLength = bytesLengthPure
+	if cpu.X86.HasAVX2 {
+		strLength = strLengthAvx512
+	} else if cpu.X86.HasAVX2 {
+		strLength = strLengthAvx2
+	} else {
+		strLength = strLengthPure
+	}
 }
 
-func BytesLength(xs *vector.Bytes, rs []int64) []int64 {
-	return bytesLength(xs, rs)
+func StrLength(xs *types.Bytes, rs []int64) []int64 {
+	return strLength(xs, rs)
 }
 
-func bytesLengthPure(xs *vector.Bytes, rs []int64) []int64 {
-	for i, n := range xs.Ns {
+func strLengthAvx2(xs *types.Bytes, rs []int64) []int64 {
+	lengths := xs.Lengths
+	n := len(lengths) / 4
+	strLengthAvx2Asm(lengths, rs)
+	for i, j := n*4, len(lengths); i < j; i++ {
+		rs[i] = int64(lengths[i])
+	}
+	return rs
+}
+
+func strLengthAvx512(xs *types.Bytes, rs []int64) []int64 {
+	lengths := xs.Lengths
+	n := len(lengths) / 8
+	strLengthAvx512Asm(lengths, rs)
+	for i, j := n*8, len(lengths); i < j; i++ {
+		rs[i] = int64(lengths[i])
+	}
+	return rs
+}
+
+func strLengthPure(xs *types.Bytes, rs []int64) []int64 {
+	for i, n := range xs.Lengths {
 		rs[i] = int64(n)
 	}
 	return rs
