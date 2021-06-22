5ecab8afc2b84690fdd066ff4eeae63aabb9a43a
XuPeng-SH <xupeng3112@163.com>
2021-06-22 23:14:34 +0800

    (aoe): add FilterCtx

--- pkg/vm/engine/aoe/storage/layout/table/index/types.go
+++ pkg/vm/engine/aoe/storage/layout/table/index/types.go
@@ -3,17 +3,66 @@ package index
 import (
 	buf "matrixone/pkg/vm/engine/aoe/storage/buffer"
 	"matrixone/pkg/vm/engine/aoe/storage/layout/base"
+
+	"github.com/pilosa/pilosa/roaring"
 )
 
-// TODO: Just for index framework implementation placeholder
+type OpType uint8
+
+const (
+	OpInv OpType = iota
+	OpEq
+	OpNe
+	OpLt
+	OpLe
+	OpGt
+	OpGe
+	OpIn
+	OpOut
+)
+
+type FilterCtx struct {
+	Op  OpType
+	Val interface{}
+
+	// Used for IN | NOT IN
+	ValSet map[interface{}]bool
+
+	ValMin interface{}
+	ValMax interface{}
+
+	BoolRes bool
+	BMRes   *roaring.Bitmap
+	Err     error
+}
+
+func NewFilterCtx(t OpType) *FilterCtx {
+	ctx := &FilterCtx{
+		Op:     t,
+		ValSet: make(map[interface{}]bool),
+	}
+	return ctx
+}
+
+func (ctx *FilterCtx) Reset() {
+	ctx.Op = OpInv
+	ctx.Val = nil
+	for k := range ctx.ValSet {
+		delete(ctx.ValSet, k)
+	}
+	ctx.ValMin = nil
+	ctx.ValMax = nil
+	ctx.BoolRes = false
+	ctx.BMRes = nil
+	ctx.Err = nil
+}
+
+func (ctx *FilterCtx) Eval(i Index) error {
+	return i.Eval(ctx)
+}
+
 type Index interface {
 	buf.IMemoryNode
 	Type() base.IndexType
-	Eq(interface{}) bool
-	Ne(interface{}) bool
-	Lt(interface{}) bool
-	Le(interface{}) bool
-	Gt(interface{}) bool
-	Ge(interface{}) bool
-	Btw(interface{}) bool
+	Eval(ctx *FilterCtx) error
 }
