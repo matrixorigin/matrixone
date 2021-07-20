71a86b400edec7ab50a1a1c4b413e6feaf779820
XuPeng-SH <xupeng3112@163.com>
2021-07-20 18:37:12 +0800

    (aoe): update nodeio 2

--- pkg/vm/engine/aoe/storage/buffer/node/nodeio.go
+++ pkg/vm/engine/aoe/storage/buffer/node/nodeio.go
@@ -16,12 +16,9 @@ func NewNodeIO(opts *e.Options, ctx context.Context) ioif.IO {
 		panic("logic error")
 	}
 
-	segmentFile := ctx.Value("segmentfile")
-	if segmentFile == nil {
-		id := handle.GetID()
-		filename := e.MakeFilename(dio.WRITER_FACTORY.Dirname, e.FTTransientNode, fmt.Sprintf("%d", id), false)
-		ctx = context.WithValue(ctx, "filename", filename)
-	}
+	id := handle.GetID()
+	filename := e.MakeFilename(dio.WRITER_FACTORY.Dirname, e.FTTransientNode, fmt.Sprintf("%d", id), false)
+	ctx = context.WithValue(ctx, "filename", filename)
 
 	iof := dio.NewIOFactory(dio.WRITER_FACTORY, dio.READER_FACTORY, dio.CLEANER_FACTORY)
 	nio := iof.MakeIO(NODE_WRITER, NODE_READER, NODE_CLEANER, ctx)
