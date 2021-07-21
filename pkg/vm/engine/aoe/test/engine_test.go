5b8fe038124c201aa9da1b914bd1ba52867bf78a
sukki37 <77312370+sukki37@users.noreply.github.com>
2021-07-21 13:31:53 +0800

    Fix aoe incompatible integration (#238)
    * Add untransfer
    
    * Modify segmentInfo
    
    * Add some testcase
    
    Co-authored-by: nnsgmsone <nnsmgsone@outlook.com>

--- pkg/vm/engine/aoe/test/engine_test.go
+++ pkg/vm/engine/aoe/test/engine_test.go
@@ -6,8 +6,15 @@ import (
 	catalog2 "matrixone/pkg/vm/engine/aoe/catalog"
 	"matrixone/pkg/vm/engine/aoe/dist/testutil"
 	"matrixone/pkg/vm/engine/aoe/engine"
+	md "matrixone/pkg/vm/engine/aoe/storage/metadata/v1"
 	"testing"
 	"time"
+)
+
+var (
+	testDBName = "db1"
+	testTableName = "t1"
+	colCnt = 4
 )
 
 func TestAOEEngine(t *testing.T) {
@@ -24,19 +31,33 @@ func TestAOEEngine(t *testing.T) {
 	catalog := catalog2.DefaultCatalog(c.Applications[0])
 	aoeEngine := engine.Mock(&catalog)
 
-	err = aoeEngine.Create("testdb", 0)
-
+	err = aoeEngine.Create(testDBName, 0)
 	require.NoError(t, err)
 
 	dbs := aoeEngine.Databases()
-
 	require.Equal(t, 1, len(dbs))
 
-	err = aoeEngine.Delete("testdb")
+	err = aoeEngine.Delete(testDBName)
 	require.NoError(t, err)
 
-	dbs1 := aoeEngine.Databases()
-	require.Equal(t, 0, len(dbs1))
+	dbs = aoeEngine.Databases()
+	require.Equal(t, 0, len(dbs))
+
+	_, err = aoeEngine.Database(testDBName)
+	require.NotNil(t, err)
+
+
+	err = aoeEngine.Create(testDBName, 0)
+	require.NoError(t, err)
+	db, err := aoeEngine.Database(testDBName)
+	require.NoError(t, err)
+
+	tbls := db.Relations()
+	require.Equal(t, 0, len(tbls))
+
+	mockTbl := md.MockTableInfo(colCnt)
+	mockTbl.Name = testTableName
+	//db.Create(testTableName, helper.Attribute(mockTbl), helper.PartitionDef(mockTbl), nil, "")
 
 }
 
---
+++ pkg/vm/engine/aoe/test/server_test.go
@@ -0,0 +1,98 @@
+package test
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	stdLog "log"
+	"matrixone/pkg/config"
+	"matrixone/pkg/server"
+	"matrixone/pkg/util/signal"
+	catalog2 "matrixone/pkg/vm/engine/aoe/catalog"
+	"matrixone/pkg/vm/engine/aoe/dist/testutil"
+	"matrixone/pkg/vm/engine/aoe/engine"
+	"matrixone/pkg/vm/metadata"
+	"matrixone/pkg/vm/mmu/host"
+	"os"
+	"testing"
+	"time"
+)
+
+var (
+	svr      server.Server
+	configPath = "./system_vars_config.toml"
+)
+
+func TestServer(t *testing.T) {
+
+	c, err := testutil.NewTestClusterStore(t)
+	require.NoError(t, err)
+	defer c.Stop()
+
+	time.Sleep(2 * time.Second)
+
+	require.NoError(t, err)
+	stdLog.Printf("app all started.")
+
+	catalog := catalog2.DefaultCatalog(c.Applications[0])
+	aoeEngine := engine.Mock(&catalog)
+
+	//before anything using the configuration
+	if err := config.GlobalSystemVariables.LoadInitialValues(); err != nil {
+		fmt.Printf("error:%v\n",err)
+		return
+	}
+
+	if err := config.LoadvarsConfigFromFile(configPath, &config.GlobalSystemVariables); err != nil {
+		fmt.Printf("error:%v\n",err)
+		return
+	}
+
+	fmt.Println("Shutdown The Server With Ctrl+C | Ctrl+\\.")
+
+	config.HostMmu = host.New(config.GlobalSystemVariables.GetHostMmuLimitation())
+
+	if ! config.GlobalSystemVariables.GetDumpEnv() {
+		fmt.Println("Using Dump Storage Engine and Cluster Nodes.")
+		//test storage engine
+		config.StorageEngine = aoeEngine
+
+		//test cluster nodes
+		config.ClusterNodes = metadata.Nodes{}
+	}else{
+		panic("The Official Storage Engine and Cluster Nodes are in the developing.")
+
+		//TODO:
+		config.StorageEngine = nil
+
+		config.ClusterNodes = nil
+	}
+
+	createServer()
+	registerSignalHandlers()
+	runServer()
+	cleanup()
+	os.Exit(0)
+
+}
+
+
+
+func createServer() {
+	address := fmt.Sprintf("%s:%d", config.GlobalSystemVariables.GetHost(), config.GlobalSystemVariables.GetPort())
+	svr = server.NewServer(address)
+}
+
+func runServer() {
+	svr.Loop()
+}
+
+func serverShutdown(isgraceful bool) {
+	svr.Quit()
+}
+
+func registerSignalHandlers() {
+	signal.SetupSignalHandler(serverShutdown)
+}
+
+func cleanup() {
+}
