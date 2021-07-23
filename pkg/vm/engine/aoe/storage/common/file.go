68dbc99e983ef6f27edba118b93dae6428d15add
XuPeng-SH <xupeng3112@163.com>
2021-07-23 21:41:32 +0800

    (aoe): update for compression 5

--- pkg/vm/engine/aoe/storage/common/file.go
+++ pkg/vm/engine/aoe/storage/common/file.go
@@ -24,3 +24,30 @@ type IVFile interface {
 	Stat() FileInfo
 	GetFileType() FileType
 }
+
+type baseFileInfo struct {
+	size int64
+}
+
+func (i *baseFileInfo) Name() string      { return "" }
+func (i *baseFileInfo) Size() int64       { return i.size }
+func (i *baseFileInfo) OriginSize() int64 { return i.size }
+func (i *baseFileInfo) CompressAlgo() int { return 0 }
+
+type baseMemFile struct {
+	stat baseFileInfo
+}
+
+func NewMemFile(size int64) IVFile {
+	return &baseMemFile{
+		stat: baseFileInfo{
+			size: size,
+		},
+	}
+}
+
+func (f *baseMemFile) Ref()                             {}
+func (f *baseMemFile) Unref()                           {}
+func (f *baseMemFile) Read(p []byte) (n int, err error) { return n, err }
+func (f *baseMemFile) Stat() FileInfo                   { return &f.stat }
+func (f *baseMemFile) GetFileType() FileType            { return MemFile }
