8ff707863b18b533f857953ce7a4975bea4e3572
bRong Njam <longran1989@gmail.com>
2021-08-03 11:00:12 +0800

    (aoe): Fix member field inconsistency

--- pkg/vm/engine/aoe/mergesort/int32s/types.go
+++ pkg/vm/engine/aoe/mergesort/int32s/types.go
@@ -1,22 +1,22 @@
 package int32s
 
 type sortElem struct {
-	elem int32
+	data int32
 	idx  uint32
 }
 
 type sortSlice []sortElem
 
-func (x sortSlice) Less(i, j int) bool { return x[i].elem < x[j].elem }
+func (x sortSlice) Less(i, j int) bool { return x[i].data < x[j].data }
 func (x sortSlice) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
 
 type heapElem struct {
-	elem int32
+	data int32
 	src  uint16
 	next uint32
 }
 
 type heapSlice []heapElem
 
-func (x heapSlice) Less(i, j int) bool { return x[i].elem < x[j].elem }
+func (x heapSlice) Less(i, j int) bool { return x[i].data < x[j].data }
 func (x heapSlice) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
