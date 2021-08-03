9df0c4db15397f27817dd84fe388a24f070847e2
bRong Njam <longran1989@gmail.com>
2021-08-03 19:11:53 +0800

    Fix bugs in purge go AND/OR implementation

--- pkg/vectorize/and/and.go
+++ pkg/vectorize/and/and.go
@@ -4,7 +4,7 @@ var (
 	SelAnd func([]int64, []int64, []int64) int64
 )
 
-func selAnd(xs, ys, rs []int64) []int64 {
+func selAnd(xs, ys, rs []int64) int64 {
 	cnt := 0
 	i, j, n, m := 0, 0, len(xs), len(ys)
 	for i < n && j < m {
@@ -14,11 +14,11 @@ func selAnd(xs, ys, rs []int64) []int64 {
 		case xs[i] < ys[j]:
 			i++
 		default:
-			xs[cnt] = xs[i]
+			rs[cnt] = xs[i]
 			i++
 			j++
 			cnt++
 		}
 	}
-	return xs[:cnt]
+	return int64(cnt)
 }
