4d8fa707b66467ea71e82d133ddf32e6093dd210
Horatius <longran1989@gmail.com>
2021-08-04 13:10:39 +0800

    Add build constraint to fastmap (#302)

--- pkg/intmap/fastmap/find_amd64.go
+++ pkg/intmap/fastmap/find_amd64.go
@@ -1,26 +1,15 @@
+// +build amd64
+
 package fastmap
 
 import "matrixone/pkg/internal/cpu"
 
-func findAvx([]uint64, uint64) int
-
-var (
-	Find func([]uint64, uint64) int
-)
+func findAvx2([]uint64, uint64) int
 
 func init() {
 	if cpu.X86.HasAVX2 {
-		Find = findAvx
+		Find = findAvx2
 	} else {
-		Find = findPure
+		Find = find
 	}
-}
-
-func findPure(xs []uint64, v uint64) int {
-	for i, x := range xs {
-		if x == v {
-			return i
-		}
-	}
-	return -1
 }
