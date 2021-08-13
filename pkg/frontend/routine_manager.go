a4246998e444a25a6d7976ecf22870c6a5a34753
daviszhen <60595215+daviszhen@users.noreply.github.com>
2021-08-13 15:50:09 +0800

    Remove catalog service from the epoch gc (#379)
    * Move catalog interface out of PDCallback
    
    * Remove catalog service from epoch-gc
    
    * Fix varchar -> MYSQL_TYPE_VARCHAR

--- pkg/frontend/routine_manager.go
+++ pkg/frontend/routine_manager.go
@@ -3,10 +3,10 @@ package frontend
 import (
 	"errors"
 	"fmt"
+	"github.com/fagongzi/goetty"
 	"matrixone/pkg/config"
 	"sync"
-
-	"github.com/fagongzi/goetty"
+	"time"
 )
 
 type RoutineManager struct {
@@ -49,6 +49,13 @@ func (rm *RoutineManager) Closed(rs goetty.IOSession) {
 }
 
 func (rm *RoutineManager) Handler(rs goetty.IOSession, msg interface{}, received uint64) error {
+	if rm.pu.SV.GetRejectWhenHeartbeatFromPDLeaderIsTimeout() {
+		if !rm.pdHook.CanAcceptSomething() {
+			fmt.Printf("The Heartbeat From PDLeader Is Timeout. The Server Go Offline.\n")
+			return errors.New("The Heartbeat From PDLeader Is Timeout. The Server Reject Connection.\n")
+		}
+	}
+
 	rm.rwlock.RLock()
 	routine, ok := rm.clients[rs]
 	rm.rwlock.RUnlock()
@@ -98,6 +105,7 @@ func (rm *RoutineManager) Handler(rs goetty.IOSession, msg interface{}, received
 	var resp *Response
 
 	req := routine.protocol.GetRequest(payload)
+	reqBegin := time.Now()
 	if resp, err = routine.executor.ExecRequest(req); err != nil {
 		fmt.Printf("routine execute request failed. error:%v \n", err)
 		return nil
@@ -108,6 +116,10 @@ func (rm *RoutineManager) Handler(rs goetty.IOSession, msg interface{}, received
 			fmt.Printf("routine send response failed %v. error:%v ", resp, err)
 			return nil
 		}
+	}
+
+	if rm.pu.SV.GetRecordTimeElapsedOfSqlRequest() {
+		fmt.Printf("connection id %d , the time of handling the request %s \n",rs.ID(),time.Since(reqBegin).String())
 	}
 
 	return nil
