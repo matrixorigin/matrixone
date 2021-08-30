cf27b6852a9ab5ced937b6a24863d4cbd6e0d19c
Matthew <58507761+lignay@users.noreply.github.com>
2021-08-30 16:08:49 +0800

    [#549 Disable race test for PR and add regular batch run] (#551)
    Co-authored-by: Yang Li <yangli@Yangs-MacBook-Pro.local>

--- optools/run_unit_test.sh
+++ optools/run_unit_test.sh
@@ -3,24 +3,22 @@
 ###################################################################
 # Title	 : run_unit_test.sh
 # Desc.  : Executing unit test cases.
-# Usage  : run_unit_test.sh VetTestReportName UnitTestReportName
 # Author : Matthew Li (lignay@me.com)
 ###################################################################
 
-if [[ $# != 2 ]]; then
-    echo "Usage: $0 VetTestReportName UnitTestReportName"
+function usage() {
+	echo "Usage:"
+    echo "./run_unit_test.sh VetTestReportName UnitTestReportName SkipTests"
+    echo ""
+    echo "Options:"
+    echo "    VetTestReportName  vet testing report name"
+    echo "    UnitTestReportName MatrixOne unit test report name"
+    echo "    SkipTests          Skipping test list."
+    echo ""
+    echo "Example:"
+    echo "    $0 vt_reports ut_reports race"
     exit 1
-fi
-
-VET_RESULT=$1
-UT_RESULT=$2
-
-BUILD_WKS="$(pwd)/../"
-UT_TIMEOUT=15
-UT_FILTER="/tmp/ut_filter"
-UT_COUNT="/tmp/ut_count"
-
-cd $BUILD_WKS
+}
 
 function msl() {
     str='*'
@@ -47,31 +45,59 @@ function run_tests(){
     [[ -f $UT_FILTER ]] && rm $UT_FILTER
     [[ -f $UT_COUNT ]] && rm $UT_COUNT
 
-    go test -v -race -timeout "${UT_TIMEOUT}m" -v $(go list ./... | egrep -v "frontend") | tee $UT_RESULT
+    if [[ $SKIP_TESTS == 'race' ]]; then
+		echo "Run UT without race check"
+        go test -v -timeout "${UT_TIMEOUT}m" -v $(go list ./... | egrep -v "frontend") | tee $UT_RESULT
+    else
+		echo "Run UT with race check"
+        go test -v -race -timeout "${UT_TIMEOUT}m" -v $(go list ./... | egrep -v "frontend") | tee $UT_RESULT
+    fi
     egrep -a '^=== RUN *Test[^\/]*$|^\-\-\- PASS: *Test|^\-\-\- FAIL: *Test'  $UT_RESULT > $UT_FILTER
 }
 
-run_vet
-run_tests
-total=$(cat "$UT_FILTER" | egrep '^=== RUN *Test' | wc -l | xargs)
-pass=$(cat "$UT_FILTER" | egrep "^\-\-\- PASS: *Test" | wc -l | xargs)
-fail=$(cat "$UT_FILTER" | egrep "^\-\-\- FAIL: *Test" | wc -l | xargs)
-unknown=$(( $total - $pass - $fail))
-cat << EOF > $UT_COUNT
+function ut_summary(){
+    local total=$(cat "$UT_FILTER" | egrep '^=== RUN *Test' | wc -l | xargs)
+    local pass=$(cat "$UT_FILTER" | egrep "^\-\-\- PASS: *Test" | wc -l | xargs)
+    local fail=$(cat "$UT_FILTER" | egrep "^\-\-\- FAIL: *Test" | wc -l | xargs)
+    local unknown=$(cat "$UT_FILTER" | sed '/^=== RUN/{x;p;x;}' | sed -n '/=== RUN/N;/--- /!p' | grep -v '^$' | wc -l | xargs)
+    cat << EOF > $UT_COUNT
 Total: $total; Passed: $pass; Failed: $fail; Unknown: $unknown
 
 FAILED CASES:
-$(egrep "^\-\-\- FAIL: *Test" $UT_FILTER)
-EOF
-msl
-cat $UT_COUNT
-msl
+$(cat "$UT_FILTER" | egrep "^\-\-\- FAIL: *Test")
 
-if (( $fail > 0 )) || (( $unknown > 0 )); then
-  echo "Unit Testing FAILED !!!"
-  exit 2
-else
-  echo "Unit Testing SUCCEEDED !!!"
+UNKNOWN CASES:
+$(cat "$UT_FILTER" | sed '/^=== RUN/{x;p;x;}' | sed -n '/=== RUN/N;/--- /!p' | grep -v '^$')
+EOF
+    msl
+    cat $UT_COUNT
+    msl
+
+    if (( $fail > 0 )) || (( $unknown > 0 )); then
+      echo "Unit Testing FAILED !!!"
+      exit 3
+    else
+      echo "Unit Testing SUCCEEDED !!!"
+    fi
+}
+
+if (( $# < 3 )); then
+    usage
 fi
+
+VET_RESULT=$1
+UT_RESULT=$2
+SKIP_TESTS=$3
+
+BUILD_WKS="$(pwd)/../"
+UT_TIMEOUT=15
+UT_FILTER="/tmp/ut_filter"
+UT_COUNT="/tmp/ut_count"
+
+cd $BUILD_WKS
+
+run_vet
+run_tests
+ut_summary || exit $?
 
 exit 0
