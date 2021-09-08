ad0e13aa9391321d8cca81738a81801b5ac0ae47
Matthew Li <lignay@me.com>
2021-09-08 10:13:41 +0800

    [devops] Enable unit test coverage

--- optools/run_ut.sh
+++ optools/run_ut.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #===============================================================================
 #
-#          FILE: run_bvt.sh
+#          FILE: run_ut.sh
 #   DESCRIPTION: 
 #        AUTHOR: Matthew Li, lignay@me.com
 #  ORGANIZATION: 
@@ -21,9 +21,9 @@ fi
 
 TEST_TYPE=$1
 if [[ $# == 2 ]]; then 
-	SKIP_TESTS=$2; 
+    SKIP_TESTS=$2; 
 else
-	SKIP_TESTS="";
+    SKIP_TESTS="";
 fi
 
 source $HOME/.bash_profile
@@ -33,10 +33,12 @@ BUILD_WKSP=$(dirname "$PWD") && cd $BUILD_WKSP
 
 UT_TIMEOUT=15
 LOG="$G_TS-$TEST_TYPE.log"
-SCA_REPORT="$G_WKSP/$G_TS-SCA-Report.txt"
-UT_REPORT="$G_WKSP/$G_TS-UT-Report.txt"
-UT_FILTER="$G_WKSP/$G_TS-UT-Filter"
-UT_COUNT="$G_WKSP/$G_TS-UT-Count"
+SCA_REPORT="$G_WKSP/$G_TS-SCA-Report.out"
+UT_REPORT="$G_WKSP/$G_TS-UT-Report.out"
+UT_FILTER="$G_WKSP/$G_TS-UT-Filter.out"
+UT_COUNT="$G_WKSP/$G_TS-UT-Count.out"
+CODE_COVERAGE="$G_WKSP/$G_TS-UT-Coverage.html"
+RAW_COVERAGE="coverage.out"
 
 if [[ -f $SCA_REPORT ]]; then rm $SCA_REPORT; fi
 if [[ -f $UT_REPORT ]]; then rm $UT_REPORT; fi
@@ -52,28 +54,51 @@ function logger(){
 }
 
 function run_vet(){
+    cd $BUILD_WKSP
+    horiz_rule
+    echo "#  BUILD WORKSPACE: $BUILD_WKSP"
+    echo "#  SCA REPORT:      $SCA_REPORT"
+    horiz_rule
+
     if [[ -f $SCA_REPORT ]]; then rm $SCA_REPORT; fi
-	logger "INF" "Test is in progress... "
-    go vet ./pkg/... 2>&1 | tee $SCA_REPORT
-	logger "INF" "Refer to $SCA_REPORT for details"
+    logger "INF" "Test is in progress... "
+    go vet -unsafeptr=false ./pkg/... 2>&1 | tee $SCA_REPORT
+    logger "INF" "Refer to $SCA_REPORT for details"
+
 }
 
 function run_tests(){
-	logger "INF" "Clean go test cache"
-    go clean -testcache
+    cd $BUILD_WKSP
+    horiz_rule
+    echo "#  BUILD WORKSPACE: $BUILD_WKSP"
+    echo "#  SKIPPED TEST:    $SKIP_TESTS"
+    echo "#  UT REPORT:       $UT_REPORT"
+    echo "#  COVERAGE REPORT: $CODE_COVERAGE"
+    echo "#  UT TIMEOUT:      $UT_TIMEOUT"
+    horiz_rule
 
+    logger "INF" "Clean go test cache"
+    go clean -testcache
+    
+    local test_scope=$(go list ./... | egrep -v "frontend")
     if [[ $SKIP_TESTS == 'race' ]]; then
         logger "INF" "Run UT without race check"
-        go test -v -timeout "${UT_TIMEOUT}m" -v $(go list ./... | egrep -v "frontend") | tee $UT_REPORT
+        go test -v -parallel 1 -timeout "${UT_TIMEOUT}m" -covermode=count -coverprofile=$RAW_COVERAGE $test_scope | tee $UT_REPORT
+        local html_coverage="coverage.html"
+        logger "INF" "Check on code coverage"
+        go tool cover -o $CODE_COVERAGE -html=$RAW_COVERAGE
+        cp -f $CODE_COVERAGE $html_coverage
     else
         logger "INF" "Run UT with race check"
-        go test -v -race -timeout "${UT_TIMEOUT}m" -v $(go list ./... | egrep -v "frontend") | tee $UT_REPORT
+        go test -v -parallel 1 -timeout "${UT_TIMEOUT}m" -race $test_scope | tee $UT_REPORT
     fi
     egrep -a '^=== RUN *Test[^\/]*$|^\-\-\- PASS: *Test|^\-\-\- FAIL: *Test'  $UT_REPORT > $UT_FILTER
-	logger "INF" "Refer to $UT_REPORT for details"
+    logger "INF" "Refer to $UT_REPORT for details"
+
 }
 
 function ut_summary(){
+    cd $BUILD_WKSP
     local total=$(cat "$UT_FILTER" | egrep '^=== RUN *Test' | wc -l | xargs)
     local pass=$(cat "$UT_FILTER" | egrep "^\-\-\- PASS: *Test" | wc -l | xargs)
     local fail=$(cat "$UT_FILTER" | egrep "^\-\-\- FAIL: *Test" | wc -l | xargs)
@@ -86,33 +111,30 @@ function ut_summary(){
 # 
 # UNKNOWN CASES:
 # $(cat "$UT_FILTER" | sed '/^=== RUN/{x;p;x;}' | sed -n '/=== RUN/N;/--- /!p' | grep -v '^$' | xargs)
+#
+#
+# Code Coverage
+$(go tool cover -func=$RAW_COVERAGE)
+#
 EOF
     horiz_rule
     cat $UT_COUNT
     horiz_rule
     if (( $fail > 0 )) || (( $unknown > 0 )); then
       logger "INF" "UNIT TESTING FAILED !!!"
-      exit 3
+      exit 1
     else
       logger "INF" "UNIT TESTING SUCCEEDED !!!"
     fi
 }
 
-function teardown(){
-	local aoe_test=$(find  pkg/vm/engine/aoe/test/* -type d -maxdepth 0)
-	for dir in ${aoe_test[@]}; do
-		logger "WRN" "Remove $dir"
-		rm -rf $dir
-	done
+function post_test(){
+    local aoe_test=$(find  pkg/vm/engine/aoe/test/* -type d -maxdepth 0)
+    for dir in ${aoe_test[@]}; do
+        logger "WRN" "Remove $dir"
+        rm -rf $dir
+    done
 }
-horiz_rule
-echo "#  BUILD WORKSPACE: $BUILD_WKSP"
-echo "#  UT REPORT:       $UT_REPORT"
-echo "#  SCA REPORT:      $SCA_REPORT"
-echo "#  SKIPPED TEST:    $SKIP_TESTS"
-echo "#  CONTAINER ID:    $G_CONT_ID"
-echo "#  UT TIMEOUT:      $UT_TIMEOUT"
-horiz_rule
 
 if [[ 'SCA' == $TEST_TYPE ]]; then
     horiz_rule
@@ -126,15 +148,14 @@ elif [[ 'UT' == $TEST_TYPE ]]; then
     run_tests
 
     horiz_rule
-    echo "# Teardown UT"
+    echo "# Post testing"
     horiz_rule
-	teardown    
+    post_test
 
-    ut_summary || exit $?
+    ut_summary
 else
     logger "ERR" "Wrong test type"
     exit 1
 fi
-
-
+    
 exit 0
