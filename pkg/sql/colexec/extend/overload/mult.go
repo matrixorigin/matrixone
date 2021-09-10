874909df26ae4139e8f6b8b3558ff7c58c227b95
XuPeng-SH <xupeng3112@163.com>
2021-09-10 16:29:35 +0800

    hot fix for missing package

--- pkg/sql/colexec/extend/overload/mult.go
+++ pkg/sql/colexec/extend/overload/mult.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.package overload
 
+package overload
+
 import (
 	"matrixone/pkg/container/nulls"
 	"matrixone/pkg/container/types"
