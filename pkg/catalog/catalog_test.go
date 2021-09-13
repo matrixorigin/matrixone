5f54b1e425b4a0a624afb5ebc803f16f02ae2dca
jiangxinmeng <jiangxinmeng@matrixorigin.cn>
2021-09-13 18:00:59 +0800

    replace TestCatalog with TestCatalogWithUtil

--- pkg/catalog/catalog_test.go
+++ pkg/catalog/catalog_test.go
@@ -7,22 +7,15 @@ import (
 	stdLog "log"
 	"time"
 
-	"matrixone/pkg/vm/driver"
-	aoeDriver "matrixone/pkg/vm/driver/aoe"
-	dConfig "matrixone/pkg/vm/driver/config"
-	aoeStorage "matrixone/pkg/vm/engine/aoe/storage"
 	"matrixone/pkg/vm/engine/aoe"
 	"matrixone/pkg/vm/driver/testutil"
 	"matrixone/pkg/vm/driver/config"
 	e "matrixone/pkg/vm/engine/aoe/storage"
 	aoe3 "matrixone/pkg/vm/driver/aoe"
+	md "matrixone/pkg/vm/engine/aoe/storage/metadata/v1"
 
-	cPebble "github.com/matrixorigin/matrixcube/storage/pebble"
-	"github.com/matrixorigin/matrixcube/vfs"
 	"github.com/matrixorigin/matrixcube/raftstore"
 
-	"github.com/BurntSushi/toml"
-	"github.com/cockroachdb/pebble"
 	"github.com/stretchr/testify/assert"
 	"github.com/fagongzi/log"
 )
@@ -34,55 +27,11 @@ const (
 	blockCnt           = blockCntPerSegment * segmentCnt
 	restart            = false
 )
-
-var targetDir="."
-var configDir="../../cmd/generate-config/system_vars_def.toml"
 var testDatabaceName="test_db"
-var testTable=aoe.TableInfo{Name: "test_tb"}
-func TestCatalog(t *testing.T){
-	metaStorage, _ := cPebble.NewStorage(targetDir+"/pebble/meta", &pebble.Options{
-		FS: vfs.NewPebbleFS(vfs.Default),
-	})
-	pebbleDataStorage, _ := cPebble.NewStorage(targetDir+"/pebble/data", &pebble.Options{
-		FS: vfs.NewPebbleFS(vfs.Default),
-	})
-	opt := aoeStorage.Options{}
-	toml.DecodeFile(configDir, &opt)
-	aoeDataStorage, _ := aoeDriver.NewStorageWithOptions(targetDir+"/aoe", &opt)
-	cfg := dConfig.Config{}
-	toml.DecodeFile(configDir, &cfg.CubeConfig)
-	a, _ := driver.NewCubeDriverWithOptions(metaStorage, pebbleDataStorage, aoeDataStorage, &cfg)
-	a.Start()
-	c := NewCatalog(a)
-	shardid,err:=c.getAvailableShard(0)
-	fmt.Print(shardid)
-	assert.NoError(t,err,"getAvailableShard Fail")
-	//test CreateDatabase
-	dbid,err:=c.CreateDatabase(0,testDatabaceName,0)
-	assert.NoError(t,err,"CreateDatabase Fail")
-	// _,err=c.CreateDatabase(0,testDatabaceName,0)
-	// assert.Equal(t,err,errors.New("db already exists"),"CreateExistingDatabase Fail")
-	//test ListDatabases
-	schemas,err:=c.ListDatabases()
-	assert.NoError(t,err,"ListDatabases Fail")
-	assert.Equal(t,schemas[0].Name,testDatabaceName,"ListDatabases: Wrong name")
-	assert.Equal(t,schemas[0].Id,dbid,"ListDatabases: Wrong id")
-	// fmt.Print(schema)
-	//test GetDatabase
-	schema,err:=c.GetDatabase(testDatabaceName)
-	assert.NoError(t,err,"GetDatabase Fail")
-	assert.Equal(t,schema.Id,dbid,"GetDatabase: Wrong id")
-	//test CreateTable
-	_,err=c.CreateTable(0,dbid,testTable)
-	assert.NoError(t,err,"CreateTable Fail")
-	//test GetDatabase
-	// schema,err:=c.DropTable(testDatabaceName)
-	// assert.NoError(t,err,"GetDatabase Fail")
-	// assert.Equal(t,schema.Id,dbid,"GetDatabase: Wrong id")
-	// fmt.Print(schema)
-	//test DropDatabase
-	err=c.DropDatabase(0,testDatabaceName)
-	assert.NoError(t,err,"DropDatabase Fail")
+var testTable *aoe.TableInfo
+func init() {
+	testTable = md.MockTableInfo(colCnt)
+	testTable.Id = 100
 }
 
 func TestCatalogWithUtil(t *testing.T){
@@ -128,7 +77,7 @@ func TestCatalogWithUtil(t *testing.T){
 	driver := c.CubeDrivers[0]
 	
 	ctlg := NewCatalog(driver)
-	
+
 	shardid,err:=ctlg.getAvailableShard(0)
 	fmt.Print(shardid)
 	assert.NoError(t,err,"getAvailableShard Fail")
@@ -148,14 +97,25 @@ func TestCatalogWithUtil(t *testing.T){
 	assert.NoError(t,err,"GetDatabase Fail")
 	assert.Equal(t,schema.Id,dbid,"GetDatabase: Wrong id")
 	//test CreateTable
-	_,err=ctlg.CreateTable(0,dbid,testTable)
+	createId,err:=ctlg.CreateTable(0,dbid,*testTable)
 	assert.NoError(t,err,"CreateTable Fail")
-	//test GetDatabase
-	// schema,err:=c.DropTable(testDatabaceName)
-	// assert.NoError(t,err,"GetDatabase Fail")
-	// assert.Equal(t,schema.Id,dbid,"GetDatabase: Wrong id")
+	//test ListTables
+	tables,err:=ctlg.ListTables(dbid)
+	assert.NoError(t,err,"ListTables Fail")
+	assert.Equal(t,len(tables),1,"ListTables: Wrong id")
+	assert.Equal(t,tables[0].Id,createId,"ListTables: Wrong id")
+	assert.Equal(t,tables[0].Name,"mocktbl","ListTables: Wrong id")
+	//test GetTable
+	table,err:=ctlg.GetTable(dbid,"mocktbl")
+	assert.NoError(t,err,"GetTable Fail")
+	assert.Equal(t,len(tables),1,"GetTable: Wrong id")
+	assert.Equal(t,table.Id,createId,"GetTable: Wrong id")
+	assert.Equal(t,table.Name,"mocktbl","GetTable: Wrong id")
+	//test DropTable
+	dropId,err:=ctlg.DropTable(0,dbid,"mocktbl")
+	assert.NoError(t,err,"DropTable Fail")
+	assert.Equal(t,createId,dropId,"DropTable: Wrong id")
 	// fmt.Print(schema)
-	//test DropDatabase
 	err=ctlg.DropDatabase(0,testDatabaceName)
 	assert.NoError(t,err,"DropDatabase Fail")
 }
