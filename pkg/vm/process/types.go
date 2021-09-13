7bca37ee80504081532be1410ef449de6e025ec3
sukki37 <cat0043@outlook.com>
2021-09-13 23:32:26 +0800

    add comments for pkg/vm/process/types.go

--- pkg/vm/process/types.go
+++ pkg/vm/process/types.go
@@ -36,39 +36,45 @@ type WaitRegister struct {
 	Ch chan interface{}
 }
 
-/*
-{
-	// normal
-	bat = proc.Reg.Ax // 获取上一个算子的结果
-	rbat = process(bat)
-	proc.Reg.Ax = rba // 把结果喂给下一个算子
-
-	// merge
-	bat = proc.Reg.Ax // 获取上一个算子的结果
-	proc.Reg.Ws[i].Ch <- bat
-}
-*/
+// Register used in execution pipeline and shared with all operators of the same pipeline.
 type Register struct {
-	Ss [][]int64       // 临时数组，存放行号列表，为了内存复用
-	Ax interface{}     // 存放上一个算子产生的结果
-	Ts []interface{}   // 缓存列数据/向量
-	Ws []*WaitRegister // merge算子
+	// Ss, temporarily stores the row number list in the execution of operators
+	// and it can be reused in the future execution.
+	Ss [][]int64
+	// Ax, stores the result of the previous operator.
+	Ax interface{}
+	// Ts, temporarily stores the column data in the execution of operators
+	// and it can be reused in the future execution to avoid mem alloc and type casting overhead.
+	Ts []interface{}
+	// Ws, receives result of multi previous operators from other pipelines
+	// e.g. merge operator.
+	Ws []*WaitRegister
 }
 
+//Limitation specifies the maximum resources that can be used in one query.
 type Limitation struct {
-	Size          int64 // memory threshold
-	BatchRows     int64 // max rows for batch
-	BatchSize     int64 // max size for batch
-	PartitionRows int64 // max rows for partition
+	// Size, memory threshold.
+	Size int64
+	// BatchRows, max rows for batch.
+	BatchRows int64
+	// BatchSize, max size for batch.
+	BatchSize int64
+	// PartitionRows, max rows for partition.
+	PartitionRows int64
 }
 
+// Process used in query execution,
+// one or more pipeline will be generated for one query,
+// and one pipeline has one process instance.
 type Process struct {
-	Id    string // query id
-	Reg   Register
-	Lim   Limitation // resource limits
-	Gm    *guest.Mmu
-	Mp    *mempool.Mempool // a pool for fast allocation and deallocation of objects.
+	// Id, query id.
+	Id  string
+	Reg Register
+	Lim Limitation
+	// Gm, records the resources used in current query.
+	Gm *guest.Mmu
+	// Mp, a pool for fast allocation and deallocation of objects.
+	Mp *mempool.Mempool
+	// Refer, traces the count of the reference of variables.
 	Refer map[string]uint64
 }
-
-//一个query多个pipeline， 一个pipeline只有一个proc
