a55f45a89061bd5bba9f5f30d4a80af76258333c
nnsgmsone <nnsmgsone@outlook.com>
2021-09-22 14:59:00 +0800

    Modify test logic to avoid a lot of loops

--- pkg/encoding/encoding_test.go
+++ pkg/encoding/encoding_test.go
@@ -22,8 +22,6 @@ import (
 	"testing"
 )
 
-
-
 func TestEncodeType(t *testing.T) {
 	typesConst := []int{0, 1, 2, 3, 5, 6, 7, 9, 10, 11, 12, 13, 15, 18, 20, 21, 32, 200, 201}
 	for _, typeId := range typesConst {
@@ -37,74 +35,56 @@ func TestEncodeType(t *testing.T) {
 }
 
 func TestEncodeInt8(t *testing.T) {
-	var num int8
-	for num = math.MinInt8; num != math.MaxInt8; num++ {
+	nums := []int8{math.MinInt8, math.MaxInt8, 0}
+	for _, num := range nums {
 		if DecodeInt8(EncodeInt8(num)) != num {
 			t.Fatalf("Int8 Encoding Error\n")
 		}
 	}
-	if DecodeInt8(EncodeInt8(math.MaxInt8)) != math.MaxInt8 {
-		t.Fatalf("Int8 Encoding Error\n")
-	}
 }
 
 func TestEncodeUint8(t *testing.T) {
-	var num uint8
-	for num = 0; num != math.MaxUint8; num++ {
+	nums := []uint8{math.MaxUint8, 0}
+	for _, num := range nums {
 		if DecodeUint8(EncodeUint8(num)) != num {
 			t.Fatalf("Uint8 Encoding Error\n")
 		}
 	}
-	if DecodeUint8(EncodeUint8(math.MaxUint8)) != math.MaxUint8 {
-		t.Fatalf("UInt8 Encoding Error\n")
-	}
 }
 
 func TestEncodeInt16(t *testing.T) {
-	var num int16
-	for num = math.MinInt16; num != math.MaxInt16; num++ {
+	nums := []int16{math.MinInt16, math.MaxInt16, 0}
+	for _, num := range nums {
 		if DecodeInt16(EncodeInt16(num)) != num {
 			t.Fatalf("Int16 Encoding Error\n")
 		}
 	}
-	if DecodeInt16(EncodeInt16(math.MaxInt16)) != math.MaxInt16 {
-		t.Fatalf("Int16 Encoding Error\n")
-	}
 }
 
 func TestEncodeUint16(t *testing.T) {
-	var num uint16
-	for num = 0; num != math.MaxUint16; num++ {
+	nums := []uint16{math.MaxUint16, 0}
+	for _, num := range nums {
 		if DecodeUint16(EncodeUint16(num)) != num {
 			t.Fatalf("Uint16 Encoding Error\n")
 		}
 	}
-	if DecodeUint16(EncodeUint16(num)) != num {
-		t.Fatalf("Uint16 Encoding Error\n")
-	}
 }
 
 func TestEncodeInt32(t *testing.T) {
-	var num int32
-	for num = math.MinInt32; num != math.MaxInt32; num++ {
+	nums := []int32{math.MinInt32, math.MaxInt32, 0}
+	for _, num := range nums {
 		if DecodeInt32(EncodeInt32(num)) != num {
 			t.Fatalf("Int32 Encoding Error\n")
 		}
 	}
-	if DecodeInt32(EncodeInt32(num)) != num {
-		t.Fatalf("Int32 Encoding Error\n")
-	}
 }
 
 func TestEncodeUint32(t *testing.T) {
-	var num uint32
-	for num = 0; num != math.MaxUint32; num++ {
+	nums := []uint32{math.MaxUint32, 0}
+	for _, num := range nums {
 		if DecodeUint32(EncodeUint32(num)) != num {
 			t.Fatalf("Uint32 Encoding Error\n")
 		}
-	}
-	if DecodeUint32(EncodeUint32(num)) != num {
-		t.Fatalf("Uint32 Encoding Error\n")
 	}
 }
 
@@ -127,7 +107,7 @@ func TestEncodeUint64(t *testing.T) {
 }
 
 func TestEncodeFloat32(t *testing.T) {
-	nums := []float32{math.MaxFloat32,math.SmallestNonzeroFloat32,-math.MaxFloat32,-math.SmallestNonzeroFloat32}
+	nums := []float32{math.MaxFloat32, math.SmallestNonzeroFloat32, -math.MaxFloat32, -math.SmallestNonzeroFloat32}
 	for _, num := range nums {
 		if DecodeFloat32(EncodeFloat32(num)) != num {
 			t.Fatalf("Float32 Encoding Error\n")
@@ -136,7 +116,7 @@ func TestEncodeFloat32(t *testing.T) {
 }
 
 func TestEncodeFloat64(t *testing.T) {
-	nums := []float64{math.MaxFloat64,math.SmallestNonzeroFloat64,-math.MaxFloat64,-math.SmallestNonzeroFloat64}
+	nums := []float64{math.MaxFloat64, math.SmallestNonzeroFloat64, -math.MaxFloat64, -math.SmallestNonzeroFloat64}
 	for _, num := range nums {
 		if DecodeFloat64(EncodeFloat64(num)) != num {
 			t.Fatalf("Float64 Encoding Error\n")
@@ -145,14 +125,11 @@ func TestEncodeFloat64(t *testing.T) {
 }
 
 func TestEncodeDate(t *testing.T) {
-	var num types.Date
-	for num = 0; num != math.MaxInt32; num++ {
+	nums := []types.Date{0, math.MaxInt32}
+	for _, num := range nums {
 		if DecodeDate(EncodeDate(num)) != num {
 			t.Fatalf("Date Encoding Error\n")
 		}
-	}
-	if DecodeDate(EncodeDate(num)) != num {
-		t.Fatalf("Date Encoding Error\n")
 	}
 }
 
