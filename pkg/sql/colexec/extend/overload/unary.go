b6a0d50cbaf793847864b2809b0ebaf3d6fcd1ad
chenmingsong <mschenjj@foxmail.com>
2021-10-21 11:06:28 +0800

    Rewrite /pkg/sql/colexec/extend/overload code with template.

--- pkg/sql/colexec/extend/overload/unary.go
+++ pkg/sql/colexec/extend/overload/unary.go
@@ -18,10 +18,20 @@ import (
 	"fmt"
 	"matrixone/pkg/container/types"
 	"matrixone/pkg/container/vector"
-	"matrixone/pkg/encoding"
-	"matrixone/pkg/vectorize/neg"
 	"matrixone/pkg/vm/process"
-	"matrixone/pkg/vm/register"
+)
+
+var (
+	// unaryOpsReturnType contains returnType of a unary expr, likes
+	// not (1 + 5), not bool, and so on
+	unaryOpsReturnType [][]types.T
+
+	// UnaryOps contains the unary operations indexed by operation type.
+	UnaryOps = map[int][]*UnaryOp{}
+
+	// variants only used to init and get items from unaryOpsReturnType
+	unaryOperators = []int{UnaryMinus, Not}
+	firstUnaryOp = unaryOperators[0]
 )
 
 func UnaryEval(op int, typ types.T, c bool, v *vector.Vector, p *process.Process) (*vector.Vector, error) {
@@ -39,145 +49,11 @@ func unaryCheck(op int, arg types.T, val types.T) bool {
 	return arg == val
 }
 
-var UnaryOps = map[int][]*UnaryOp{
-	UnaryMinus: {
-		&UnaryOp{
-			Typ:        types.T_int8,
-			ReturnType: types.T_int8,
-			Fn: func(v *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
-				if v.Ref == 1 || v.Ref == 0 {
-					v.Ref = 0
-					vs := v.Col.([]int8)
-					neg.Int8Neg(vs, vs)
-					return v, nil
-				}
-				vs := v.Col.([]int8)
-				vec, err := register.Get(proc, int64(len(vs)), v.Typ)
-				if err != nil {
-					return nil, err
-				}
-				rs := encoding.DecodeInt8Slice(vec.Data)
-				rs = rs[:len(vs)]
-				vec.Col = rs
-				vec.Nsp.Set(v.Nsp)
-				vec.SetCol(neg.Int8Neg(vs, rs))
-				return vec, nil
-			},
-		},
-		&UnaryOp{
-			Typ:        types.T_int16,
-			ReturnType: types.T_int16,
-			Fn: func(v *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
-				if v.Ref == 1 || v.Ref == 0 {
-					v.Ref = 0
-					vs := v.Col.([]int16)
-					neg.Int16Neg(vs, vs)
-					return v, nil
-				}
-				vs := v.Col.([]int16)
-				vec, err := register.Get(proc, int64(len(vs)*2), v.Typ)
-				if err != nil {
-					return nil, err
-				}
-				rs := encoding.DecodeInt16Slice(vec.Data)
-				rs = rs[:len(vs)]
-				vec.Col = rs
-				vec.Nsp.Set(v.Nsp)
-				vec.SetCol(neg.Int16Neg(vs, rs))
-				return vec, nil
-			},
-		},
-		&UnaryOp{
-			Typ:        types.T_int32,
-			ReturnType: types.T_int32,
-			Fn: func(v *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
-				if v.Ref == 1 || v.Ref == 0 {
-					v.Ref = 0
-					vs := v.Col.([]int32)
-					neg.Int32Neg(vs, vs)
-					return v, nil
-				}
-				vs := v.Col.([]int32)
-				vec, err := register.Get(proc, int64(len(vs)*4), v.Typ)
-				if err != nil {
-					return nil, err
-				}
-				rs := encoding.DecodeInt32Slice(vec.Data)
-				rs = rs[:len(vs)]
-				vec.Col = rs
-				vec.Nsp.Set(v.Nsp)
-				vec.SetCol(neg.Int32Neg(vs, rs))
-				return vec, nil
-			},
-		},
-		&UnaryOp{
-			Typ:        types.T_int64,
-			ReturnType: types.T_int64,
-			Fn: func(v *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
-				if v.Ref == 1 || v.Ref == 0 {
-					v.Ref = 0
-					vs := v.Col.([]int64)
-					neg.Int64Neg(vs, vs)
-					return v, nil
-				}
-				vs := v.Col.([]int64)
-				vec, err := register.Get(proc, int64(len(vs)*8), v.Typ)
-				if err != nil {
-					return nil, err
-				}
-				rs := encoding.DecodeInt64Slice(vec.Data)
-				rs = rs[:len(vs)]
-				vec.Col = rs
-				vec.Nsp.Set(v.Nsp)
-				vec.SetCol(neg.Int64Neg(vs, rs))
-				return vec, nil
-			},
-		},
-		&UnaryOp{
-			Typ:        types.T_float32,
-			ReturnType: types.T_float32,
-			Fn: func(v *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
-				if v.Ref == 1 || v.Ref == 0 {
-					v.Ref = 0
-					vs := v.Col.([]float32)
-					neg.Float32Neg(vs, vs)
-					return v, nil
-				}
-				vs := v.Col.([]float32)
-				vec, err := register.Get(proc, int64(len(vs)*4), v.Typ)
-				if err != nil {
-					return nil, err
-				}
-				rs := encoding.DecodeFloat32Slice(vec.Data)
-				rs = rs[:len(vs)]
-				vec.Col = rs
-				vec.Nsp.Set(v.Nsp)
-				vec.SetCol(neg.Float32Neg(vs, rs))
-				return vec, nil
-			},
-		},
-		&UnaryOp{
-			Typ:        types.T_float64,
-			ReturnType: types.T_float64,
-			Fn: func(v *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
-				if v.Ref == 1 || v.Ref == 0 {
-					v.Ref = 0
-					vs := v.Col.([]float64)
-					neg.Float64Neg(vs, vs)
-					return v, nil
-				}
-				vs := v.Col.([]float64)
-				vec, err := register.Get(proc, int64(len(vs)*8), v.Typ)
-				if err != nil {
-					return nil, err
-				}
-				rs := encoding.DecodeFloat64Slice(vec.Data)
-				rs = rs[:len(vs)]
-				vec.Col = rs
-				vec.Nsp.Set(v.Nsp)
-				vec.SetCol(neg.Float64Neg(vs, rs))
-				return vec, nil
-			},
-		},
-	},
+// GetUnaryOpReturnType returns the returnType of unary op and its arg types.
+func GetUnaryOpReturnType(op int, arg types.T) types.T {
+	t := unaryOpsReturnType[op-firstUnaryOp][arg]
+	if t == noRt { // just ignore and return any type to make error message normal.
+		t = arg
+	}
+	return t
 }
