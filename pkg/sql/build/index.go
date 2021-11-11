06fd0f70e1f01748f906194dd1f8e21cf81dbed9
jiangxinmeng <jiangxinmeng@sjtu.edu.cn>
2021-11-11 16:06:55 +0800

    Update index

--- pkg/sql/build/index.go
+++ pkg/sql/build/index.go
@@ -28,7 +28,12 @@ func (b *build) buildCreateIndex(stmt *tree.CreateIndex) (op.OP, error) {
 	if err != nil {
 		return nil, err
 	}
-	typ := 1
+	var typ tree.IndexType
+	if stmt.IndexOption != nil {
+		typ = stmt.IndexOption.IType
+	} else {
+		typ = tree.INDEX_TYPE_BTREE
+	}
 	def := engine.IndexTableDef{Typ: int(typ), ColNames: stmt.KeyParts[0].ColName.Parts[:1], Name: string(stmt.Name)}
 	defs = append(defs, &def)
 	return createIndex.New(stmt.IfNotExists, r, defs), nil
