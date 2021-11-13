41eb93921f39cb6429d8eb17167466869989a028
XuPeng-SH <xupeng3112@163.com>
2021-11-13 11:27:05 +0800

    update for snapshot test

--- pkg/vm/engine/aoe/storage/db/snapshot_test.go
+++ pkg/vm/engine/aoe/storage/db/snapshot_test.go
@@ -5,7 +5,9 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/aoe/storage/dbi"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/aoe/storage/metadata/v1"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/aoe/storage/mock"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/aoe/storage/testutils"
 	"github.com/stretchr/testify/assert"
 )
@@ -59,6 +61,293 @@ func TestSnapshot1(t *testing.T) {
 	idx, err := inst.CreateSnapshot(database.Name, getSnapshotPath(defaultSnapshotPath, t))
 	assert.Nil(t, err)
 	assert.Equal(t, idxGen.Get(), idx)
+
+	err = inst.ApplySnapshot(database.Name, getSnapshotPath(defaultSnapshotPath, t))
+	assert.Nil(t, err)
+
+	idx2 := inst.GetDBCheckpointId(database.Name)
+	assert.Equal(t, idx, idx2)
+	inst.Close()
+
+	inst2, _, _ := initTestDB3(t)
+	defer inst2.Close()
+	idx3 := inst2.GetDBCheckpointId(database.Name)
+	assert.Equal(t, idx, idx3)
+
+	names = inst2.TableNames(database.Name)
+	assert.Equal(t, len(schemas), len(names))
+
+	// t.Log(inst.Store.Catalog.PString(metadata.PPL0, 0))
+	t.Log(inst2.Store.Catalog.IndexWal.String())
+}
+
+func TestSnapshot2(t *testing.T) {
+	initTestEnv(t)
+	prepareSnapshotPath(defaultSnapshotPath, t)
+	inst, gen, database := initTestDB1(t)
+	shardId := database.GetShardId()
+	idxGen := gen.Shard(shardId)
+	schemas := make([]*metadata.Schema, 3)
+	for i, _ := range schemas {
+		schema := metadata.MockSchema(i + 1)
+		_, err := inst.CreateTable(database.Name, schema, idxGen.Next())
+		assert.Nil(t, err)
+		schemas[i] = schema
+	}
+
+	testutils.WaitExpect(200, func() bool {
+		return idxGen.Get() == database.GetCheckpointId()
+	})
+	assert.Equal(t, idxGen.Get(), database.GetCheckpointId())
+	ckId := idxGen.Get()
+
+	names := inst.TableNames(database.Name)
+	assert.Equal(t, len(schemas), len(names))
+
+	rows := inst.Store.Catalog.Cfg.BlockMaxRows / 10
+	ck0 := mock.MockBatch(schemas[0].Types(), rows)
+	appendCtx := dbi.AppendCtx{
+		OpIndex:   gen.Alloc(database.GetShardId()),
+		OpSize:    1,
+		TableName: schemas[0].Name,
+		DBName:    database.Name,
+		Data:      ck0,
+	}
+	err := inst.Append(appendCtx)
+	assert.Nil(t, err)
+
+	schema := metadata.MockSchema(2)
+	_, err = inst.CreateTable(database.Name, schema, idxGen.Next())
+
+	idx, err := inst.CreateSnapshot(database.Name, getSnapshotPath(defaultSnapshotPath, t))
+	assert.Nil(t, err)
+	assert.Equal(t, ckId, idx)
+
+	err = inst.ApplySnapshot(database.Name, getSnapshotPath(defaultSnapshotPath, t))
+	assert.Nil(t, err)
+
+	idx2 := inst.GetDBCheckpointId(database.Name)
+	assert.Equal(t, idx, idx2)
+	inst.Close()
+
+	inst2, _, _ := initTestDB3(t)
+	defer inst2.Close()
+	idx3 := inst2.GetDBCheckpointId(database.Name)
+	assert.Equal(t, idx, idx3)
+
+	names = inst2.TableNames(database.Name)
+	assert.Equal(t, len(schemas), len(names))
+
+	// t.Log(inst.Store.Catalog.PString(metadata.PPL0, 0))
+	t.Log(inst2.Store.Catalog.IndexWal.String())
+}
+
+func TestSnapshot3(t *testing.T) {
+	initTestEnv(t)
+	prepareSnapshotPath(defaultSnapshotPath, t)
+	inst, gen, database := initTestDB1(t)
+	shardId := database.GetShardId()
+	idxGen := gen.Shard(shardId)
+	schemas := make([]*metadata.Schema, 2)
+	for i, _ := range schemas {
+		schema := metadata.MockSchema(i + 1)
+		_, err := inst.CreateTable(database.Name, schema, idxGen.Next())
+		assert.Nil(t, err)
+		schemas[i] = schema
+	}
+
+	testutils.WaitExpect(200, func() bool {
+		return idxGen.Get() == database.GetCheckpointId()
+	})
+	assert.Equal(t, idxGen.Get(), database.GetCheckpointId())
+	ckId := idxGen.Get()
+
+	names := inst.TableNames(database.Name)
+	assert.Equal(t, len(schemas), len(names))
+
+	rows := inst.Store.Catalog.Cfg.BlockMaxRows / 10
+	ck0 := mock.MockBatch(schemas[0].Types(), rows)
+	appendCtx := dbi.AppendCtx{
+		OpIndex:   gen.Alloc(database.GetShardId()),
+		OpSize:    1,
+		TableName: schemas[0].Name,
+		DBName:    database.Name,
+		Data:      ck0,
+	}
+	err := inst.Append(appendCtx)
+	assert.Nil(t, err)
+
+	schema := metadata.MockSchema(2)
+	_, err = inst.CreateTable(database.Name, schema, idxGen.Next())
+
+	ck1 := mock.MockBatch(schemas[1].Types(), rows)
+	appendCtx.OpIndex = gen.Alloc(database.GetShardId())
+	appendCtx.TableName = schemas[1].Name
+	appendCtx.Data = ck1
+	err = inst.Append(appendCtx)
+	assert.Nil(t, err)
+
+	err = inst.Flush(database.Name, schemas[1].Name)
+	assert.Nil(t, err)
+	stats := inst.Store.Catalog.IndexWal.GetAllPendingEntries()
+	t.Log(stats[0].Count)
+	testutils.WaitExpect(200, func() bool {
+		return stats[0].Count == 2
+	})
+	assert.Equal(t, 2, stats[0].Count)
+
+	idx, err := inst.CreateSnapshot(database.Name, getSnapshotPath(defaultSnapshotPath, t))
+	assert.Nil(t, err)
+	assert.Equal(t, ckId, idx)
+
+	err = inst.ApplySnapshot(database.Name, getSnapshotPath(defaultSnapshotPath, t))
+	assert.Nil(t, err)
+
+	idx2 := inst.GetDBCheckpointId(database.Name)
+	assert.Equal(t, idx, idx2)
+	inst.Close()
+
+	inst2, _, _ := initTestDB3(t)
+	defer inst2.Close()
+	idx3 := inst2.GetDBCheckpointId(database.Name)
+	assert.Equal(t, idx, idx3)
+
+	names = inst2.TableNames(database.Name)
+	assert.Equal(t, len(schemas), len(names))
+
+	// t.Log(inst.Store.Catalog.PString(metadata.PPL0, 0))
+	t.Log(inst2.Store.Catalog.IndexWal.String())
+}
+
+func TestSnapshot4(t *testing.T) {
+	initTestEnv(t)
+	prepareSnapshotPath(defaultSnapshotPath, t)
+	inst, gen, database := initTestDB1(t)
+	shardId := database.GetShardId()
+	idxGen := gen.Shard(shardId)
+	schemas := make([]*metadata.Schema, 3)
+	for i, _ := range schemas {
+		schema := metadata.MockSchema(i + 1)
+		_, err := inst.CreateTable(database.Name, schema, idxGen.Next())
+		assert.Nil(t, err)
+		schemas[i] = schema
+	}
+
+	testutils.WaitExpect(200, func() bool {
+		return idxGen.Get() == database.GetCheckpointId()
+	})
+	assert.Equal(t, idxGen.Get(), database.GetCheckpointId())
+	ckId := idxGen.Get()
+
+	names := inst.TableNames(database.Name)
+	assert.Equal(t, len(schemas), len(names))
+
+	rows := inst.Store.Catalog.Cfg.BlockMaxRows / 10
+	ck0 := mock.MockBatch(schemas[0].Types(), rows)
+	appendCtx := dbi.AppendCtx{
+		OpIndex:   gen.Alloc(database.GetShardId()),
+		OpSize:    1,
+		TableName: schemas[0].Name,
+		DBName:    database.Name,
+		Data:      ck0,
+	}
+	err := inst.Append(appendCtx)
+	assert.Nil(t, err)
+
+	ck1 := mock.MockBatch(schemas[1].Types(), rows)
+	appendCtx.OpIndex = gen.Alloc(database.GetShardId())
+	appendCtx.TableName = schemas[1].Name
+	appendCtx.Data = ck1
+	err = inst.Append(appendCtx)
+	assert.Nil(t, err)
+
+	err = inst.Flush(database.Name, schemas[1].Name)
+	assert.Nil(t, err)
+	stats := inst.Store.Catalog.IndexWal.GetAllPendingEntries()
+	t.Log(stats[0].Count)
+	testutils.WaitExpect(200, func() bool {
+		return stats[0].Count == 2
+	})
+	assert.Equal(t, 2, stats[0].Count)
+
+	idx, err := inst.CreateSnapshot(database.Name, getSnapshotPath(defaultSnapshotPath, t))
+	assert.Nil(t, err)
+	assert.Equal(t, ckId, idx)
+
+	err = inst.ApplySnapshot(database.Name, getSnapshotPath(defaultSnapshotPath, t))
+	assert.Nil(t, err)
+
+	idx2 := inst.GetDBCheckpointId(database.Name)
+	assert.Equal(t, idx, idx2)
+	inst.Close()
+
+	inst2, _, _ := initTestDB3(t)
+	defer inst2.Close()
+	idx3 := inst2.GetDBCheckpointId(database.Name)
+	assert.Equal(t, idx, idx3)
+
+	names = inst2.TableNames(database.Name)
+	assert.Equal(t, len(schemas), len(names))
+
+	// t.Log(inst.Store.Catalog.PString(metadata.PPL0, 0))
+	t.Log(inst2.Store.Catalog.IndexWal.String())
+}
+
+func TestSnapshot5(t *testing.T) {
+	initTestEnv(t)
+	prepareSnapshotPath(defaultSnapshotPath, t)
+	inst, gen, database := initTestDB1(t)
+	shardId := database.GetShardId()
+	idxGen := gen.Shard(shardId)
+	schemas := make([]*metadata.Schema, 3)
+	for i, _ := range schemas {
+		schema := metadata.MockSchema(i + 1)
+		_, err := inst.CreateTable(database.Name, schema, idxGen.Next())
+		assert.Nil(t, err)
+		schemas[i] = schema
+	}
+
+	testutils.WaitExpect(200, func() bool {
+		return idxGen.Get() == database.GetCheckpointId()
+	})
+	assert.Equal(t, idxGen.Get(), database.GetCheckpointId())
+
+	names := inst.TableNames(database.Name)
+	assert.Equal(t, len(schemas), len(names))
+
+	rows := inst.Store.Catalog.Cfg.BlockMaxRows / 10
+	ck0 := mock.MockBatch(schemas[0].Types(), rows)
+	appendCtx := dbi.AppendCtx{
+		OpIndex:   gen.Alloc(database.GetShardId()),
+		OpSize:    1,
+		TableName: schemas[0].Name,
+		DBName:    database.Name,
+		Data:      ck0,
+	}
+	err := inst.Append(appendCtx)
+	assert.Nil(t, err)
+
+	ck1 := mock.MockBatch(schemas[1].Types(), rows)
+	appendCtx.OpIndex = gen.Alloc(database.GetShardId())
+	appendCtx.TableName = schemas[1].Name
+	appendCtx.Data = ck1
+	err = inst.Append(appendCtx)
+	assert.Nil(t, err)
+
+	err = inst.Flush(database.Name, schemas[0].Name)
+	assert.Nil(t, err)
+	testutils.WaitExpect(200, func() bool {
+		stats := inst.Store.Catalog.IndexWal.GetAllPendingEntries()
+		return stats[0].Count == 1
+	})
+	stats := inst.Store.Catalog.IndexWal.GetAllPendingEntries()
+	assert.Equal(t, 1, stats[0].Count)
+	ckId := inst.GetDBCheckpointId(database.Name)
+	assert.Equal(t, uint64(4), ckId)
+
+	idx, err := inst.CreateSnapshot(database.Name, getSnapshotPath(defaultSnapshotPath, t))
+	assert.Nil(t, err)
+	assert.Equal(t, ckId, idx)
 
 	err = inst.ApplySnapshot(database.Name, getSnapshotPath(defaultSnapshotPath, t))
 	assert.Nil(t, err)
