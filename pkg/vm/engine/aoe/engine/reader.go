3770df6f174f15d8f716a51372da73115a1a9fcc
GreatRiver <2552853833@qq.com>
2021-11-22 19:15:38 +0800

    Fix the failed to remove block bug of Reader

--- pkg/vm/engine/aoe/engine/reader.go
+++ pkg/vm/engine/aoe/engine/reader.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"matrixone/pkg/container/batch"
 	"matrixone/pkg/container/vector"
+	"matrixone/pkg/logutil"
 	"matrixone/pkg/vm/engine"
 	"unsafe"
 )
@@ -21,7 +22,7 @@ func (a aoeReader) NewSparseFilter() engine.SparseFilter {
 }
 
 func (a aoeReader) Read(refCount []uint64, attrs []string) (*batch.Batch, error) {
-	if len(a.blocks) == 0 {
+	if a.blocks == nil || len(*(a.blocks)) == 0 {
 		return nil, nil
 	}
 	compressedBytes := make([]*bytes.Buffer, len(refCount))
@@ -31,25 +32,29 @@ func (a aoeReader) Read(refCount []uint64, attrs []string) (*batch.Batch, error)
 		compressedBytes[i] = bytes.NewBuffer(make([]byte, 0, 8))
 		decompressedBytes[i] = bytes.NewBuffer(make([]byte, 0, 8))
 	}
-	a.blocks[0].Prefetch(attrs)
+	(*(a.blocks))[0].Prefetch(attrs)
 
-	bat, err := a.blocks[0].Read(refCount, attrs, compressedBytes, decompressedBytes)
+	bat, err := (*(a.blocks))[0].Read(refCount, attrs, compressedBytes, decompressedBytes)
 	if err != nil {
 		return nil, err
 	}
+	logutil.Infof("vecs len is %d", vector.Length(bat.Vecs[0]))
 
 	//zs, err := mheap.Alloc(a.mp, int64(vector.Length(bat.Vecs[0])))
 	zs := make([]*bytes.Buffer, int64(vector.Length(bat.Vecs[0])))
 	if err != nil {
 		return nil, err
 	}
-	
+
 	bat.Zs = unsafe.Slice((*int64)(unsafe.Pointer(&zs[0])), len(zs)/8)
 	for i := 0; i < len(bat.Zs); i++ {
 		bat.Zs[i] = 1
 	}
-	if len(a.blocks) > 1 {
-		a.blocks = append(a.blocks[:1], a.blocks[2:]...)
+	if len(*(a.blocks)) > 1 {
+		*(a.blocks) = append((*(a.blocks))[:1], (*(a.blocks))[2:]...)
+	} else if len(*(a.blocks)) == 1 {
+		*(a.blocks) = (*(a.blocks))[0:0]
 	}
+	logutil.Infof("bat is %v", bat)
 	return bat, nil
 }
