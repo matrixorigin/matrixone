ad9fae545fa605fc801d9d825d0e2a7f4cddac5c
jiangxinmeng1 <51114574+jiangxinmeng1@users.noreply.github.com>
2021-12-10 06:03:02 -0600

    Define and implement whole split process: add catalog listener (#1321)
    * catalog listener
    
    * add catalog/helper.go
    
    * add driver/helper.go
    
    * update getTablets
    
    * update OnPostSplit

--- cmd/db-server/main.go
+++ cmd/db-server/main.go
@@ -215,6 +215,8 @@ func main() {
 		os.Exit(DecodeAoeConfigExit)
 	}
 
+	catalogListener := catalog.NewCatalogListener()
+	opt.EventListener = catalogListener
 	aoeDataStorage, err = aoeDriver.NewStorageWithOptions(targetDir+"/aoe", &opt)
 	if err != nil {
 		logutil.Infof("Create aoe driver error, %v\n", err)
@@ -251,6 +253,7 @@ func main() {
 	}
 	c = catalog.NewCatalog(a)
 	config.ClusterCatalog = c
+	catalogListener.UpdateCatalog(c)
 	eng := aoeEngine.New(c)
 	pci.SetRemoveEpoch(removeEpoch)
 
