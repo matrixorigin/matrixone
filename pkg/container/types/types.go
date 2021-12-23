92cf6af7cfde2b671b9fbe4a39fd9cd3f3549fb3
chenmingsong <59043531+m-schen@users.noreply.github.com>
2021-12-23 14:39:40 +0800

    Fix vector's function for type Date, Datetime (#1503)

--- pkg/container/types/types.go
+++ pkg/container/types/types.go
@@ -126,9 +126,9 @@ func (t T) ToType() Type {
 		typ.Size = 1
 	case T_int16:
 		typ.Size = 2
-	case T_int32:
+	case T_int32, T_date:
 		typ.Size = 4
-	case T_int64:
+	case T_int64, T_datetime:
 		typ.Size = 8
 	case T_uint8:
 		typ.Size = 1
@@ -225,6 +225,10 @@ func (t T) OidString() string {
 		return "T_char"
 	case T_varchar:
 		return "T_varchar"
+	case T_date:
+		return "T_date"
+	case T_datetime:
+		return "T_datetime"
 	}
 	return "unknown_type"
 }
@@ -232,9 +236,9 @@ func (t T) OidString() string {
 // GoType returns go type string for T
 func (t T) GoType() string {
 	switch t {
-	case T_int64:
+	case T_int64, T_datetime:
 		return "int64"
-	case T_int32:
+	case T_int32, T_date:
 		return "int32"
 	case T_int16:
 		return "int16"
@@ -278,9 +282,9 @@ func (t T) TypeLen() int {
 		return 1
 	case T_int16:
 		return 2
-	case T_int32:
+	case T_int32, T_date:
 		return 4
-	case T_int64:
+	case T_int64, T_datetime:
 		return 8
 	case T_uint8:
 		return 1
