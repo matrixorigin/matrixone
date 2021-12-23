c024592232553b8637e83123beec0d43d88a83fc
daviszhen <60595215+daviszhen@users.noreply.github.com>
2021-12-23 19:08:17 +0800

    add ut

--- pkg/frontend/util_test.go
+++ pkg/frontend/util_test.go
@@ -15,8 +15,10 @@
 package frontend
 
 import (
+	cvey "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 func Test_PathExists(t *testing.T) {
@@ -39,4 +41,106 @@ func Test_PathExists(t *testing.T) {
 		require.True(t, exist == c.exist)
 		require.True(t, isfile == c.isfile)
 	}
+}
+
+func Test_closeFlag(t *testing.T) {
+	cvey.Convey("closeFlag",t, func() {
+		cf := &CloseFlag{}
+		cf.setClosed(0)
+		cvey.So(cf.IsOpened(),cvey.ShouldBeTrue)
+
+		cf.Open()
+		cvey.So(cf.IsOpened(),cvey.ShouldBeTrue)
+
+		cf.Close()
+		cvey.So(cf.IsClosed(),cvey.ShouldBeTrue)
+	})
+}
+
+func Test_MinMax(t *testing.T) {
+	cvey.Convey("min",t, func() {
+		cvey.So(Min(10,9),cvey.ShouldEqual,9)
+		cvey.So(Min(9,10),cvey.ShouldEqual,9)
+	})
+
+	cvey.Convey("minInt64",t, func() {
+		cvey.So(MinInt64(10,9),cvey.ShouldEqual,9)
+		cvey.So(MinInt64(9,10),cvey.ShouldEqual,9)
+	})
+
+	cvey.Convey("minUint64",t, func() {
+		cvey.So(MinUint64(10,9),cvey.ShouldEqual,9)
+		cvey.So(MinUint64(9,10),cvey.ShouldEqual,9)
+	})
+
+	cvey.Convey("max",t, func() {
+		cvey.So(Max(10,9),cvey.ShouldEqual,10)
+		cvey.So(Max(9,10),cvey.ShouldEqual,10)
+	})
+
+	cvey.Convey("maxInt64",t, func() {
+		cvey.So(MaxInt64(10,9),cvey.ShouldEqual,10)
+		cvey.So(MaxInt64(9,10),cvey.ShouldEqual,10)
+	})
+
+	cvey.Convey("maxUint64",t, func() {
+		cvey.So(MaxUint64(10,9),cvey.ShouldEqual,10)
+		cvey.So(MaxUint64(9,10),cvey.ShouldEqual,10)
+	})
+}
+
+func Test_uint64list(t *testing.T) {
+	cvey.Convey("uint64list",t, func() {
+		var l Uint64List = make(Uint64List,3)
+		cvey.So(l.Len(),cvey.ShouldEqual,3)
+		cvey.So(l.Less(0,1),cvey.ShouldBeFalse)
+		a,b := l[0],l[1]
+		l.Swap(0,1)
+		cvey.So(a == l[1] && b == l[0],cvey.ShouldBeTrue)
+	})
+}
+
+func Test_routineid(t *testing.T) {
+	cvey.Convey("rtid",t, func() {
+		x := GetRoutineId()
+		cvey.So(x,cvey.ShouldBeGreaterThanOrEqualTo,0)
+	})
+}
+
+func Test_debugcounter(t *testing.T) {
+	cvey.Convey("debugCounter",t, func() {
+		dc := NewDebugCounter(3)
+		dc.Set(0,1)
+		cvey.So(dc.Get(0),cvey.ShouldEqual,1)
+		dc.Add(0,1)
+		cvey.So(dc.Get(0),cvey.ShouldEqual,2)
+		cvey.So(dc.Len(),cvey.ShouldEqual,3)
+
+		go dc.DCRoutine()
+
+		time.Sleep(6 * time.Second)
+
+		dc.Cf.Close()
+	})
+}
+
+func Test_timeout(t *testing.T){
+	cvey.Convey("timeout",t, func() {
+		to := NewTimeout(5 * time.Second,true)
+		to.UpdateTime(time.Now())
+		cvey.So(to.isTimeout(),cvey.ShouldBeFalse)
+	})
+}
+
+func Test_substringFromBegin(t *testing.T) {
+	cvey.Convey("ssfb",t, func() {
+		cvey.So(SubStringFromBegin("abcdef",3),cvey.ShouldEqual,"abc...")
+	})
+}
+
+func Test_makedebuginfo(t *testing.T) {
+	cvey.Convey("makedebuginfo",t, func() {
+		MakeDebugInfo([]byte{0,1,2,3,4,5,6,7,8,9},
+			6,3)
+	})
 }
