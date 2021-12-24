f6e296b0a9d5cabdb0bd85ac288d134e83cdf458
jiangxinmeng <jiangxinmeng@sjtu.edu.cn>
2021-12-24 12:06:22 +0800

    update label in catalog

--- pkg/catalog/catalog.go
+++ pkg/catalog/catalog.go
@@ -47,7 +47,8 @@ const (
 	cPreSplitPrefix       = "PreSplit"
 	cSplitPrefix          = "Split"
 	cDeletedTablePrefix   = "DeletedTableQueue"
-	cRuleName             = "Table"
+	cRuleName             = "RuleTable"
+	cLabelName            = "LabelTable"
 	timeout               = 2000 * time.Millisecond
 	idPoolSize            = 20
 )
@@ -434,7 +435,7 @@ func (c *Catalog) CreateTable(epoch, dbId uint64, tbl aoe.TableInfo) (tid uint64
 		}
 		logutil.Infof("create tablet, tid %v, sid %v", tid, shardId)
 		tbl.Name = tableName
-		if err := c.Driver.AddLabelToShard(shardId, tbl.Name, strconv.Itoa(int(tid))); err != nil {
+		if err := c.Driver.AddLabelToShard(shardId, cLabelName, tbl.Name); err != nil {
 			logutil.Errorf("ErrAddLabelFailed, %v, %v, %v", shardId, tid, err)
 			return tid, ErrTabletCreateFailed
 		}
@@ -464,7 +465,7 @@ func (c *Catalog) CreateTable(epoch, dbId uint64, tbl aoe.TableInfo) (tid uint64
 	}
 
 	for {
-		err := c.Driver.AddSchedulingRule(cRuleName, tbl.Name)
+		err := c.Driver.AddSchedulingRule(cRuleName,cLabelName)
 		if err == nil {
 			break
 		}
