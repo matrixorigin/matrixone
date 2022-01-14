459513ff0cc49bf099228cbf01b4b3f60c2dc02f
broccoliSpicy <93440049+broccoliSpicy@users.noreply.github.com>
2022-01-14 16:05:40 +0800

    vectorized floor function_ (#1610)

---
+++ pkg/builtin/multi/floor.go
@@ -0,0 +1,368 @@
+// Copyright 2021 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package multi
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/matrixorigin/matrixone/pkg/builtin"
+	"github.com/matrixorigin/matrixone/pkg/container/nulls"
+	"github.com/matrixorigin/matrixone/pkg/container/types"
+	"github.com/matrixorigin/matrixone/pkg/container/vector"
+	"github.com/matrixorigin/matrixone/pkg/encoding"
+	"github.com/matrixorigin/matrixone/pkg/sql/colexec/extend"
+	"github.com/matrixorigin/matrixone/pkg/sql/colexec/extend/overload"
+	"github.com/matrixorigin/matrixone/pkg/vectorize/floor"
+	"github.com/matrixorigin/matrixone/pkg/vm/process"
+)
+
+func init() {
+	extend.FunctionRegistry["floor"] = builtin.Floor
+	for _, item := range argsAndRets {
+		overload.AppendFunctionRets(builtin.Floor, item.args, item.ret)
+	}
+	extend.MultiReturnTypes[builtin.Floor] = func(es []extend.Extend) types.T {
+		return getMultiReturnType(builtin.Floor, es)
+	}
+
+	extend.MultiStrings[builtin.Floor] = func(es []extend.Extend) string {
+		if len(es) > 1 {
+			return fmt.Sprintf("floor(%s, %s)", es[0], es[1])
+		} else {
+			return fmt.Sprintf("floor(%s)", es[0])
+		}
+	}
+	overload.OpTypes[builtin.Floor] = overload.Multi
+	overload.MultiOps[builtin.Floor] = []*overload.MultiOp{
+		{
+			Min:        1,
+			Max:        2,
+			Typ:        types.T_uint8,
+			ReturnType: types.T_uint8,
+			Fn: func(vecs []*vector.Vector, proc *process.Process, cs []bool) (*vector.Vector, error) {
+				digits := int64(0)
+				vs := vecs[0].Col.([]uint8)
+				if len(vecs) > 1 {
+					if !cs[1] && vecs[1].Typ.Oid != types.T_int64 {
+						return nil, errors.New("The second argument of the round function must be an int64 constant")
+					}
+					digits = vecs[1].Col.([]int64)[0]
+				}
+				if vecs[0].Ref == 1 || vecs[0].Ref == 0 {
+					vecs[0].Ref = 0
+					floor.FloorUint8(vs, vs, digits)
+					return vecs[0], nil
+				}
+				vec, err := process.Get(proc, int64(len(vs)), types.Type{Oid: types.T_uint8, Size: 1})
+				if err != nil {
+					return nil, err
+				}
+				rs := encoding.DecodeUint8Slice(vec.Data)
+				rs = rs[:len(vs)]
+				vec.Col = rs
+				nulls.Set(vec.Nsp, vecs[0].Nsp)
+				vector.SetCol(vec, floor.FloorUint8(vs, rs, digits))
+				return vec, nil
+			},
+		},
+		{
+			Min:        1,
+			Max:        2,
+			Typ:        types.T_uint16,
+			ReturnType: types.T_uint16,
+			Fn: func(vecs []*vector.Vector, proc *process.Process, cs []bool) (*vector.Vector, error) {
+				digits := int64(0)
+				vs := vecs[0].Col.([]uint16)
+				if len(vecs) > 1 {
+					if !cs[1] && vecs[1].Typ.Oid != types.T_int64 {
+						return nil, errors.New("The second argument of the round function must be an int64 constant")
+					}
+					digits = vecs[1].Col.([]int64)[0]
+				}
+				if vecs[0].Ref == 1 || vecs[0].Ref == 0 {
+					vecs[0].Ref = 0
+					floor.FloorUint16(vs, vs, digits)
+					return vecs[0], nil
+				}
+				vec, err := process.Get(proc, 2*int64(len(vs)), types.Type{Oid: types.T_uint16, Size: 2})
+				if err != nil {
+					return nil, err
+				}
+				rs := encoding.DecodeUint16Slice(vec.Data)
+				rs = rs[:len(vs)]
+				vec.Col = rs
+				nulls.Set(vec.Nsp, vecs[0].Nsp)
+				vector.SetCol(vec, floor.FloorUint16(vs, rs, digits))
+				return vec, nil
+			},
+		},
+		{
+			Min:        1,
+			Max:        2,
+			Typ:        types.T_uint32,
+			ReturnType: types.T_uint32,
+			Fn: func(vecs []*vector.Vector, proc *process.Process, cs []bool) (*vector.Vector, error) {
+				digits := int64(0)
+				vs := vecs[0].Col.([]uint32)
+				if len(vecs) > 1 {
+					if !cs[1] && vecs[1].Typ.Oid != types.T_int64 {
+						return nil, errors.New("The second argument of the round function must be an int64 constant")
+					}
+					digits = vecs[1].Col.([]int64)[0]
+				}
+				if vecs[0].Ref == 1 || vecs[0].Ref == 0 {
+					vecs[0].Ref = 0
+					floor.FloorUint32(vs, vs, digits)
+					return vecs[0], nil
+				}
+				vec, err := process.Get(proc, 4*int64(len(vs)), types.Type{Oid: types.T_uint32, Size: 4})
+				if err != nil {
+					return nil, err
+				}
+				rs := encoding.DecodeUint32Slice(vec.Data)
+				rs = rs[:len(vs)]
+				vec.Col = rs
+				nulls.Set(vec.Nsp, vecs[0].Nsp)
+				vector.SetCol(vec, floor.FloorUint32(vs, rs, digits))
+				return vec, nil
+			},
+		},
+
+		{
+			Min:        1,
+			Max:        2,
+			Typ:        types.T_uint64,
+			ReturnType: types.T_uint64,
+			Fn: func(vecs []*vector.Vector, proc *process.Process, cs []bool) (*vector.Vector, error) {
+				digits := int64(0)
+				vs := vecs[0].Col.([]uint64)
+				if len(vecs) > 1 {
+					if !cs[1] && vecs[1].Typ.Oid != types.T_int64 {
+						return nil, errors.New("The second argument of the round function must be an int64 constant")
+					}
+					digits = vecs[1].Col.([]int64)[0]
+				}
+				if vecs[0].Ref == 1 || vecs[0].Ref == 0 {
+					vecs[0].Ref = 0
+					floor.FloorUint64(vs, vs, digits)
+					return vecs[0], nil
+				}
+				vec, err := process.Get(proc, 8*int64(len(vs)), types.Type{Oid: types.T_uint64, Size: 8})
+				if err != nil {
+					return nil, err
+				}
+				rs := encoding.DecodeUint64Slice(vec.Data)
+				rs = rs[:len(vs)]
+				vec.Col = rs
+				nulls.Set(vec.Nsp, vecs[0].Nsp)
+				vector.SetCol(vec, floor.FloorUint64(vs, rs, digits))
+				return vec, nil
+			},
+		},
+
+		{
+			Min:        1,
+			Max:        2,
+			Typ:        types.T_int8,
+			ReturnType: types.T_int8,
+			Fn: func(vecs []*vector.Vector, proc *process.Process, cs []bool) (*vector.Vector, error) {
+				digits := int64(0)
+				vs := vecs[0].Col.([]int8)
+				if len(vecs) > 1 {
+					if !cs[1] && vecs[1].Typ.Oid != types.T_int64 {
+						return nil, errors.New("The second argument of the round function must be an int64 constant")
+					}
+					digits = vecs[1].Col.([]int64)[0]
+				}
+				if vecs[0].Ref == 1 || vecs[0].Ref == 0 {
+					vecs[0].Ref = 0
+					floor.FloorInt8(vs, vs, digits)
+					return vecs[0], nil
+				}
+				vec, err := process.Get(proc, int64(len(vs)), types.Type{Oid: types.T_int8, Size: 1})
+				if err != nil {
+					return nil, err
+				}
+				rs := encoding.DecodeInt8Slice(vec.Data)
+				rs = rs[:len(vs)]
+				vec.Col = rs
+				nulls.Set(vec.Nsp, vecs[0].Nsp)
+				vector.SetCol(vec, floor.FloorInt8(vs, rs, digits))
+				return vec, nil
+			},
+		},
+
+		{
+			Min:        1,
+			Max:        2,
+			Typ:        types.T_int16,
+			ReturnType: types.T_int16,
+			Fn: func(vecs []*vector.Vector, proc *process.Process, cs []bool) (*vector.Vector, error) {
+				digits := int64(0)
+				vs := vecs[0].Col.([]int16)
+				if len(vecs) > 1 {
+					if !cs[1] && vecs[1].Typ.Oid != types.T_int64 {
+						return nil, errors.New("The second argument of the round function must be an int64 constant")
+					}
+					digits = vecs[1].Col.([]int64)[0]
+				}
+				if vecs[0].Ref == 1 || vecs[0].Ref == 0 {
+					vecs[0].Ref = 0
+					floor.FloorInt16(vs, vs, digits)
+					return vecs[0], nil
+				}
+				vec, err := process.Get(proc, 2*int64(len(vs)), types.Type{Oid: types.T_int16, Size: 2})
+				if err != nil {
+					return nil, err
+				}
+				rs := encoding.DecodeInt16Slice(vec.Data)
+				rs = rs[:len(vs)]
+				vec.Col = rs
+				nulls.Set(vec.Nsp, vecs[0].Nsp)
+				vector.SetCol(vec, floor.FloorInt16(vs, rs, digits))
+				return vec, nil
+			},
+		},
+
+		{
+			Min:        1,
+			Max:        2,
+			Typ:        types.T_int32,
+			ReturnType: types.T_int32,
+			Fn: func(vecs []*vector.Vector, proc *process.Process, cs []bool) (*vector.Vector, error) {
+				digits := int64(0)
+				vs := vecs[0].Col.([]int32)
+				if len(vecs) > 1 {
+					if !cs[1] && vecs[1].Typ.Oid != types.T_int64 {
+						return nil, errors.New("The second argument of the round function must be an int64 constant")
+					}
+					digits = vecs[1].Col.([]int64)[0]
+				}
+				if vecs[0].Ref == 1 || vecs[0].Ref == 0 {
+					vecs[0].Ref = 0
+					floor.FloorInt32(vs, vs, digits)
+					return vecs[0], nil
+				}
+				vec, err := process.Get(proc, 4*int64(len(vs)), types.Type{Oid: types.T_int32, Size: 4})
+				if err != nil {
+					return nil, err
+				}
+				rs := encoding.DecodeInt32Slice(vec.Data)
+				rs = rs[:len(vs)]
+				vec.Col = rs
+				nulls.Set(vec.Nsp, vecs[0].Nsp)
+				vector.SetCol(vec, floor.FloorInt32(vs, rs, digits))
+				return vec, nil
+			},
+		},
+
+		{
+			Min:        1,
+			Max:        2,
+			Typ:        types.T_int64,
+			ReturnType: types.T_int64,
+			Fn: func(vecs []*vector.Vector, proc *process.Process, cs []bool) (*vector.Vector, error) {
+				digits := int64(0)
+				vs := vecs[0].Col.([]int64)
+				if len(vecs) > 1 {
+					if !cs[1] && vecs[1].Typ.Oid != types.T_int64 {
+						return nil, errors.New("The second argument of the round function must be an int64 constant")
+					}
+					digits = vecs[1].Col.([]int64)[0]
+				}
+				if vecs[0].Ref == 1 || vecs[0].Ref == 0 {
+					vecs[0].Ref = 0
+					floor.FloorInt64(vs, vs, digits)
+					return vecs[0], nil
+				}
+				vec, err := process.Get(proc, 8*int64(len(vs)), types.Type{Oid: types.T_int64, Size: 8})
+				if err != nil {
+					return nil, err
+				}
+				rs := encoding.DecodeInt64Slice(vec.Data)
+				rs = rs[:len(vs)]
+				vec.Col = rs
+				nulls.Set(vec.Nsp, vecs[0].Nsp)
+				vector.SetCol(vec, floor.FloorInt64(vs, rs, digits))
+				return vec, nil
+			},
+		},
+
+		{
+			Min:        1,
+			Max:        2,
+			Typ:        types.T_float32,
+			ReturnType: types.T_float32,
+			Fn: func(vecs []*vector.Vector, proc *process.Process, cs []bool) (*vector.Vector, error) {
+				digits := int64(0)
+				vs := vecs[0].Col.([]float32)
+				if len(vecs) > 1 {
+					if !cs[1] && vecs[1].Typ.Oid != types.T_int64 {
+						return nil, errors.New("The second argument of the round function must be an int64 constant")
+					}
+					digits = vecs[1].Col.([]int64)[0]
+				}
+				if vecs[0].Ref == 1 || vecs[0].Ref == 0 {
+					vecs[0].Ref = 0
+					floor.FloorFloat32(vs, vs, digits)
+					return vecs[0], nil
+				}
+				vec, err := process.Get(proc, 4*int64(len(vs)), types.Type{Oid: types.T_float32, Size: 4})
+				if err != nil {
+					return nil, err
+				}
+				rs := encoding.DecodeFloat32Slice(vec.Data)
+				rs = rs[:len(vs)]
+				vec.Col = rs
+				nulls.Set(vec.Nsp, vecs[0].Nsp)
+				vector.SetCol(vec, floor.FloorFloat32(vs, rs, digits))
+				return vec, nil
+			},
+		},
+
+		{
+			Min:        1,
+			Max:        2,
+			Typ:        types.T_float64,
+			ReturnType: types.T_float64,
+			Fn: func(vecs []*vector.Vector, proc *process.Process, cs []bool) (*vector.Vector, error) {
+				digits := int64(0)
+				vs := vecs[0].Col.([]float64)
+				if len(vecs) > 1 {
+					if !cs[1] && vecs[1].Typ.Oid != types.T_int64 {
+						return nil, errors.New("The second argument of the round function must be an int64 constant")
+					}
+					digits = vecs[1].Col.([]int64)[0]
+				}
+				if vecs[0].Ref == 1 || vecs[0].Ref == 0 {
+					vecs[0].Ref = 0
+					floor.FloorFloat64(vs, vs, digits)
+					return vecs[0], nil
+				}
+				vec, err := process.Get(proc, 8*int64(len(vs)), types.Type{Oid: types.T_float64, Size: 8})
+				if err != nil {
+					return nil, err
+				}
+				rs := encoding.DecodeFloat64Slice(vec.Data)
+				rs = rs[:len(vs)]
+				vec.Col = rs
+				nulls.Set(vec.Nsp, vecs[0].Nsp)
+				vector.SetCol(vec, floor.FloorFloat64(vs, rs, digits))
+				return vec, nil
+			},
+		},
+	}
+}
---
+++ pkg/vectorize/floor/floor.go
@@ -0,0 +1,322 @@
+// Copyright 2021 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package floor
+
+import (
+	"math"
+)
+
+var (
+	floorUint8   func([]uint8, []uint8, int64) []uint8
+	floorUint16  func([]uint16, []uint16, int64) []uint16
+	floorUint32  func([]uint32, []uint32, int64) []uint32
+	floorUint64  func([]uint64, []uint64, int64) []uint64
+	floorInt8    func([]int8, []int8, int64) []int8
+	floorInt16   func([]int16, []int16, int64) []int16
+	floorInt32   func([]int32, []int32, int64) []int32
+	floorInt64   func([]int64, []int64, int64) []int64
+	floorFloat32 func([]float32, []float32, int64) []float32
+	floorFloat64 func([]float64, []float64, int64) []float64
+)
+
+var maxUint8digits = digits(math.MaxUint8)
+var maxUint16digits = digits(math.MaxUint16)
+var maxUint32digits = digits(math.MaxUint32)
+var maxUint64digits = digits(math.MaxUint64) // 20
+var maxInt8digits = digits(math.MaxInt8)
+var maxInt16digits = digits(math.MaxInt16)
+var maxInt32digits = digits(math.MaxInt32)
+var maxInt64digits = digits(math.MaxInt64) // 19
+
+func digits(value uint64) int64 {
+	digits := int64(0)
+	for value > 0 {
+		value /= 10
+		digits++
+	}
+	return digits
+}
+
+// scaleTable is a lookup array for digits
+var scaleTable = [...]uint64{
+	1,
+	10,
+	100,
+	1000,
+	10000,
+	100000,
+	1000000,
+	10000000,
+	100000000,
+	1000000000,
+	10000000000,
+	100000000000,
+	1000000000000,
+	10000000000000,
+	100000000000000,
+	1000000000000000,
+	10000000000000000,
+	100000000000000000,
+	1000000000000000000,
+	10000000000000000000, // 1 followed by 19 zeros, maxUint64 number has 20 digits, so the max scale is 1 followed by 19 zeroes
+}
+
+func init() {
+	floorUint8 = floorUint8Pure
+	floorUint16 = floorUint16Pure
+	floorUint32 = floorUint32Pure
+	floorUint64 = floorUint64Pure
+	floorInt8 = floorInt8Pure
+	floorInt16 = floorInt16Pure
+	floorInt32 = floorInt32Pure
+	floorInt64 = floorInt64Pure
+	floorFloat32 = floorFloat32Pure
+	floorFloat64 = floorFloat64Pure
+}
+
+func FloorUint8(xs, rs []uint8, digits int64) []uint8 {
+	return floorUint8(xs, rs, digits)
+}
+
+func floorUint8Pure(xs, rs []uint8, digits int64) []uint8 {
+	// maximum uint8 number is 255, so we only need to worry about a few digit cases,
+	switch {
+	case digits >= 0:
+		return xs
+	case digits == -1 || digits == -2:
+		scale := uint8(scaleTable[-digits])
+		for i := range xs {
+			rs[i] = xs[i] / scale * scale
+		}
+	case digits <= -maxUint8digits:
+		for i := range xs {
+			rs[i] = 0
+		}
+	}
+	return rs
+}
+
+func FloorUint16(xs, rs []uint16, digits int64) []uint16 {
+	return floorUint16(xs, rs, digits)
+}
+
+func floorUint16Pure(xs, rs []uint16, digits int64) []uint16 {
+	switch {
+	case digits >= 0:
+		return xs
+	case digits > -maxUint16digits:
+		scale := uint16(scaleTable[-digits])
+		for i := range xs {
+			rs[i] = xs[i] / scale * scale
+		}
+	case digits <= -maxUint16digits:
+		for i := range xs {
+			rs[i] = 0
+		}
+	}
+	return rs
+}
+
+func FloorUint32(xs, rs []uint32, digits int64) []uint32 {
+	return floorUint32(xs, rs, digits)
+}
+
+func floorUint32Pure(xs, rs []uint32, digits int64) []uint32 {
+	switch {
+	case digits >= 0:
+		return xs
+	case digits > -maxUint32digits:
+		scale := uint32(scaleTable[-digits])
+		for i := range xs {
+			rs[i] = xs[i] / scale * scale
+		}
+	case digits <= maxUint32digits:
+		for i := range xs {
+			rs[i] = 0
+		}
+	}
+	return rs
+}
+
+func FloorUint64(xs, rs []uint64, digits int64) []uint64 {
+	return floorUint64(xs, rs, digits)
+}
+
+func floorUint64Pure(xs, rs []uint64, digits int64) []uint64 {
+	switch {
+	case digits >= 0:
+		return xs
+	case digits > -maxUint64digits:
+		scale := uint64(scaleTable[-digits])
+		for i := range xs {
+			rs[i] = xs[i] / scale * scale
+		}
+	case digits <= -maxUint64digits:
+		for i := range xs {
+			rs[i] = 0
+		}
+	}
+	return rs
+}
+
+func FloorInt8(xs, rs []int8, digits int64) []int8 {
+	return floorInt8(xs, rs, digits)
+}
+
+func floorInt8Pure(xs, rs []int8, digits int64) []int8 {
+	switch {
+	case digits >= 0:
+		return xs
+	case digits == -1 || digits == -2:
+		scale := int8(scaleTable[-digits])
+		for i := range xs {
+			value := xs[i]
+			if value < 0 {
+				value -= scale - 1
+			}
+			rs[i] = value / scale * scale
+		}
+	case digits <= -maxInt8digits:
+		for i := range xs {
+			rs[i] = 0
+		}
+	}
+	return rs
+}
+
+func FloorInt16(xs, rs []int16, digits int64) []int16 {
+	return floorInt16(xs, rs, digits)
+}
+
+func floorInt16Pure(xs, rs []int16, digits int64) []int16 {
+	switch {
+	case digits >= 0:
+		return xs
+	case digits > -maxInt16digits:
+		scale := int16(scaleTable[-digits])
+		for i := range xs {
+			value := xs[i]
+			if value < 0 {
+				value -= scale - 1
+			}
+			rs[i] = value / scale * scale
+		}
+	case digits <= -maxInt16digits:
+		for i := range xs {
+			rs[i] = 0
+		}
+	}
+	return rs
+}
+
+func FloorInt32(xs, rs []int32, digits int64) []int32 {
+	return floorInt32(xs, rs, digits)
+}
+
+func floorInt32Pure(xs, rs []int32, digits int64) []int32 {
+	switch {
+	case digits >= 0:
+		return xs
+	case digits > -maxInt32digits:
+		scale := int32(scaleTable[-digits])
+		for i := range xs {
+			value := xs[i]
+			if value < 0 {
+				value -= scale - 1
+			}
+			rs[i] = value / scale * scale
+		}
+	case digits <= -maxInt32digits:
+		for i := range xs {
+			rs[i] = 0
+		}
+	}
+	return rs
+}
+
+func FloorInt64(xs, rs []int64, digits int64) []int64 {
+	return floorInt64(xs, rs, digits)
+}
+
+func floorInt64Pure(xs, rs []int64, digits int64) []int64 {
+	switch {
+	case digits >= 0:
+		return xs
+	case digits > -maxInt64digits:
+		scale := int64(scaleTable[-digits])
+		for i := range xs {
+			value := xs[i]
+			if value < 0 {
+				value -= scale - 1
+			}
+			rs[i] = value / scale * scale
+		}
+	case digits <= -maxInt64digits:
+		for i := range xs {
+			rs[i] = 0
+		}
+	}
+	return rs
+}
+
+func FloorFloat32(xs, rs []float32, digits int64) []float32 {
+	return floorFloat32(xs, rs, digits)
+}
+
+func floorFloat32Pure(xs, rs []float32, digits int64) []float32 {
+	if digits == 0 {
+		for i := range xs {
+			rs[i] = float32(math.Floor(float64(xs[i])))
+		}
+	} else if digits > 0 {
+		scale := float32(scaleTable[digits])
+		for i := range xs {
+			value := xs[i] * scale
+			rs[i] = float32(math.Floor(float64(value))) / scale
+		}
+	} else {
+		scale := float32(scaleTable[-digits])
+		for i := range xs {
+			value := xs[i] / scale
+			rs[i] = float32(math.Floor(float64(value))) * scale
+		}
+	}
+	return rs
+}
+
+func FloorFloat64(xs, rs []float64, digits int64) []float64 {
+	return floorFloat64(xs, rs, digits)
+}
+
+func floorFloat64Pure(xs, rs []float64, digits int64) []float64 {
+	if digits == 0 {
+		for i := range xs {
+			rs[i] = math.Floor(xs[i])
+		}
+	} else if digits > 0 {
+		scale := float64(scaleTable[digits])
+		for i := range xs {
+			value := xs[i] * scale
+			rs[i] = math.Floor(value) / scale
+		}
+	} else {
+		scale := float64(scaleTable[-digits])
+		for i := range xs {
+			value := xs[i] / scale
+			rs[i] = math.Floor(value) * scale
+		}
+	}
+	return rs
+}
