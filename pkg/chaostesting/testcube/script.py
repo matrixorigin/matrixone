60e27ef21be058f4423e77afc570e2fc23b3d837
reus <reusee@gmail.com>
2022-01-18 16:58:31 +0800

    add script function comments

--- pkg/chaostesting/testcube/script.py
+++ pkg/chaostesting/testcube/script.py
@@ -1,17 +1,26 @@
+# number of parallel test cases running in `run` command
 parallel(2)
 
-# on MacOS, run `sudo ifconfig lo0 alias 192.168.168.168` to add an ip to the loopback interface
+# host address for cube to listen on
+# this setting overwrites the use_dummy_interface setting
 #listen_host("192.168.168.168")
 #listen_host("localhost")
 
+# port range for cube to use
 port_range(40000, 50000)
 
+# threshold for timeout reporting
 timeout_report_threshold(1)
 
+# timeout of single test case
 execute_timeout(15 * minute)
 
-enable_cpu_profile(True)
+# enable cpu profile for whole program
+enable_cpu_profile(False)
 
-http_server_addr("localhost:8889")
+# address for http server, mainly for net/http/pprof
+#http_server_addr("localhost:8889")
 
+# use dummy network interface to isolate, linux only
 use_dummy_interface(False)
+
