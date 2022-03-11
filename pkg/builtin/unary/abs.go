122737e9695096f65496eaacf296b9322cc8b77f
Otter <2311566266@qq.com>
2022-03-11 20:22:16 +0800

    Feature builtin-in function log (#1865)

--- pkg/builtin/unary/abs.go
+++ pkg/builtin/unary/abs.go
@@ -1,3 +1,17 @@
+// Copyright 2022 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package unary
 
 import (
@@ -13,7 +27,7 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/vm/process"
 )
 
-var argAndRets = []argsAndRet{
+var ArgAndRets = []argsAndRet{
 	{[]types.T{types.T_uint8}, types.T_uint8},
 	{[]types.T{types.T_uint16}, types.T_uint16},
 	{[]types.T{types.T_uint32}, types.T_uint32},
@@ -29,7 +43,7 @@ var argAndRets = []argsAndRet{
 func init() {
 	extend.FunctionRegistry["abs"] = builtin.Abs
 
-	for _, item := range argAndRets {
+	for _, item := range ArgAndRets {
 		// append function parameter types and return types
 		overload.AppendFunctionRets(builtin.Abs, item.args, item.ret)
 	}
