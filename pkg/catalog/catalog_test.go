c6a499561746d950bf0f6c54bfdce63b533bc6f0
jiangxinmeng <jiangxinmeng@sjtu.edu.cn>
2022-03-25 12:35:52 +0800

    update log level

--- pkg/catalog/catalog_test.go
+++ pkg/catalog/catalog_test.go
@@ -226,7 +226,7 @@ func TestCatalogWithUtil(t *testing.T) {
 				cfg.Worker.RaftEventWorkers = 8
 			}),
 			// raftstore.WithTestClusterNodeCount(1),
-			raftstore.WithTestClusterLogLevel(zapcore.WarnLevel),
+			raftstore.WithTestClusterLogLevel(zapcore.InfoLevel),
 			raftstore.WithTestClusterDataPath("./test")))
 
 	c.Start()
