f988fe7443dee873b43c64d69e44294fb797122e
noneback <chenlan_of_learn@126.com>
2022-03-26 23:00:30 +0800

    Add UTC_TIMESTAMP built-in func

--- pkg/vectorize/utc_timestamp/utc_timestamp.go
+++ pkg/vectorize/utc_timestamp/utc_timestamp.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Matrix Origin
+// Copyright 2022 Matrix Origin
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,24 +11,18 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+package utc_timestamp
 
-package builtin
+import "github.com/matrixorigin/matrixone/pkg/container/types"
 
-import (
-	"github.com/matrixorigin/matrixone/pkg/sql/colexec/extend/overload"
+var (
+	getUTCTimestamp func() types.Datetime
 )
 
-const (
-	Length = iota + overload.NE + 1
-	Year
-	Round
-	Floor
-	Abs
-	Log
-	Ln
-	Ceil
-	Exp
-	Power
-	Pi
-	UTCTimestamp
-)
+func init() {
+	getUTCTimestamp = GetUTCTimestamp
+}
+
+func GetUTCTimestamp() types.Datetime {
+	return types.Now().UTC()
+}
