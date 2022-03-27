22d2ac9142978603f41af030a34e824871e88519
ZtXavier <1491918111@qq.com>
2022-03-27 14:23:43 +0800

    go

---
+++ pkg/builtin/unary/cot.go
@@ -0,0 +1,221 @@
+package unary
+
+import (
+	"fmt"
+	"github.com/matrixorigin/matrixone/pkg/builtin"
+	"github.com/matrixorigin/matrixone/pkg/container/nulls"
+	"github.com/matrixorigin/matrixone/pkg/container/types"
+	"github.com/matrixorigin/matrixone/pkg/container/vector"
+	"github.com/matrixorigin/matrixone/pkg/encoding"
+	"github.com/matrixorigin/matrixone/pkg/sql/colexec/extend"
+	"github.com/matrixorigin/matrixone/pkg/sql/colexec/extend/overload"
+	"github.com/matrixorigin/matrixone/pkg/vectorize/cot"
+	"github.com/matrixorigin/matrixone/pkg/vm/process"
+)
+
+
+var argAndRets = []argsAndRet {
+	{[]types.T{types.T_uint8}, types.T_float64},
+	{[]types.T{types.T_uint16}, types.T_float64},
+	{[]types.T{types.T_uint32}, types.T_float64},
+	{[]types.T{types.T_uint64}, types.T_float64},
+
+	{[]types.T{types.T_int8}, types.T_float64},
+	{[]types.T{types.T_int16}, types.T_float64},
+	{[]types.T{types.T_int32}, types.T_float64},
+	{[]types.T{types.T_int64}, types.T_float64},
+
+	{[]types.T{types.T_float32}, types.T_float64},
+	{[]types.T{types.T_float64}, types.T_float64},
+}
+
+func init() {
+	extend.FunctionRegistry["cot"] = builtin.Cot	
+
+	for _, item := range argAndRets {
+		overload.AppendFunctionRets(builtin.Cot, item.args, item.ret)	
+	}
+	extend.UnaryReturnTypes[builtin.Cot] = func(extend extend.Extend) types.T {
+		return getUnaryReturnType(builtin.Cot, extend)	
+	}
+
+	extend.UnaryStrings[builtin.Cot] = func(e extend.Extend) string {	
+		return fmt.Sprintf("cot(%s)", e)
+	}
+	overload.OpTypes[builtin.Cot] = overload.Unary	
+
+	overload.UnaryOps[builtin.Cot] = []*overload.UnaryOp {
+		{
+			Typ: types.T_float32,
+			ReturnType: types.T_float64,
+			Fn: func(origVec *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
+				origVecCol := origVec.Col.([]float32)
+				resultVector, err := process.Get(proc, 8*int64(len(origVecCol)), types.Type{Oid: types.T_float64, Size: 8})
+				if err != nil {
+					return nil, err
+				}
+				results := encoding.DecodeFloat64Slice(resultVector.Data)
+				results = results[:len(origVecCol)]
+				resultVector.Col = results
+				nulls.Set(resultVector.Nsp, origVec.Nsp)
+				vector.SetCol(resultVector, cot.CotFloat32(origVecCol, results))
+				return resultVector, nil
+			},
+		},
+		{
+			Typ: types.T_float64,
+			ReturnType: types.T_float64,
+			Fn: func(origVec *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
+				origVecCol := origVec.Col.([]float64)
+				resultVector, err := process.Get(proc, 8*int64(len(origVecCol)), types.Type{Oid: types.T_float64, Size: 8})
+				if err != nil {
+					return nil, err
+				}
+				results := encoding.DecodeFloat64Slice(resultVector.Data)
+				results = results[:len(origVecCol)]
+				resultVector.Col = results
+				nulls.Set(resultVector.Nsp, origVec.Nsp)
+				vector.SetCol(resultVector, cot.CotFloat64(origVecCol, results))
+				return resultVector, nil
+			},
+		},
+
+		{
+			Typ: types.T_uint8,
+			ReturnType: types.T_float64,
+			Fn: func(origVec *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
+				origVecCol := origVec.Col.([]uint8)
+				resultVector, err := process.Get(proc, 8*int64(len(origVecCol)), types.Type{Oid: types.T_float64, Size: 8})
+				if err != nil {
+					return nil, err
+				}
+				results := encoding.DecodeFloat64Slice(resultVector.Data)
+				results = results[:len(origVecCol)]
+				resultVector.Col = results
+				nulls.Set(resultVector.Nsp, origVec.Nsp)
+				vector.SetCol(resultVector, cot.CotUint8(origVecCol, results))
+				return resultVector, nil
+			},
+		},
+		{
+			Typ: types.T_uint16,
+			ReturnType: types.T_float64,
+			Fn: func(origVec *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
+				origVecCol := origVec.Col.([]uint16)
+				resultVector, err := process.Get(proc, 8*int64(len(origVecCol)), types.Type{Oid: types.T_float64, Size: 8})
+				if err != nil {
+					return nil, err
+				}
+				results := encoding.DecodeFloat64Slice(resultVector.Data)
+				results = results[:len(origVecCol)]
+				resultVector.Col = results
+				nulls.Set(resultVector.Nsp, origVec.Nsp)
+				vector.SetCol(resultVector, cot.CotUint16(origVecCol, results))
+				return resultVector, nil
+			},
+		},
+		{
+			Typ: types.T_uint32,
+			ReturnType: types.T_float64,
+			Fn: func(origVec *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
+				origVecCol := origVec.Col.([]uint32)
+				resultVector, err := process.Get(proc, 8*int64(len(origVecCol)), types.Type{Oid: types.T_float64, Size: 8})
+				if err != nil {
+					return nil, err
+				}
+				results := encoding.DecodeFloat64Slice(resultVector.Data)
+				results = results[:len(origVecCol)]
+				resultVector.Col = results
+				nulls.Set(resultVector.Nsp, origVec.Nsp)
+				vector.SetCol(resultVector, cot.CotUint32(origVecCol, results))
+				return resultVector, nil
+			},
+		},
+		{
+			Typ: types.T_uint64,
+			ReturnType: types.T_float64,
+			Fn: func(origVec *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
+				origVecCol := origVec.Col.([]uint64)
+				resultVector, err := process.Get(proc, 8*int64(len(origVecCol)), types.Type{Oid: types.T_float64, Size: 8})
+				if err != nil {
+					return nil, err
+				}
+				results := encoding.DecodeFloat64Slice(resultVector.Data)
+				results = results[:len(origVecCol)]
+				resultVector.Col = results
+				nulls.Set(resultVector.Nsp, origVec.Nsp)
+				vector.SetCol(resultVector,cot.CotUint64(origVecCol, results))
+				return resultVector, nil
+			},
+		},
+
+		{
+			Typ: types.T_int8,
+			ReturnType: types.T_float64,
+			Fn: func(origVec *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
+				origVecCol := origVec.Col.([]int8)
+				resultVector, err := process.Get(proc, 8*int64(len(origVecCol)), types.Type{Oid: types.T_float64, Size: 8})
+				if err != nil {
+					return nil, err
+				}
+				results := encoding.DecodeFloat64Slice(resultVector.Data)
+				results = results[:len(origVecCol)]
+				resultVector.Col = results
+				nulls.Set(resultVector.Nsp, origVec.Nsp)
+				vector.SetCol(resultVector, cot.CotInt8(origVecCol, results))
+				return resultVector, nil
+			},
+		},
+		{
+			Typ: types.T_int16,
+			ReturnType: types.T_float64,
+			Fn: func(origVec *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
+				origVecCol := origVec.Col.([]int16)
+				resultVector, err := process.Get(proc, 8*int64(len(origVecCol)), types.Type{Oid: types.T_float64, Size: 8})
+				if err != nil {
+					return nil, err
+				}
+				results := encoding.DecodeFloat64Slice(resultVector.Data)
+				results = results[:len(origVecCol)]
+				resultVector.Col = results
+				nulls.Set(resultVector.Nsp, origVec.Nsp)
+				vector.SetCol(resultVector, cot.CotInt16(origVecCol, results))
+				return resultVector, nil
+			},
+		},
+		{
+			Typ: types.T_int32,
+			ReturnType: types.T_float64,
+			Fn: func(origVec *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
+				origVecCol := origVec.Col.([]int32)
+				resultVector, err := process.Get(proc, 8*int64(len(origVecCol)), types.Type{Oid: types.T_float64, Size: 8})
+				if err != nil {
+					return nil, err
+				}
+				results := encoding.DecodeFloat64Slice(resultVector.Data)
+				results = results[:len(origVecCol)]
+				resultVector.Col = results
+				nulls.Set(resultVector.Nsp, origVec.Nsp)
+				vector.SetCol(resultVector, cot.CotInt32(origVecCol, results))
+				return resultVector, nil
+			},
+		},
+		{
+			Typ: types.T_int64,
+			ReturnType: types.T_float64,
+			Fn: func(origVec *vector.Vector, proc *process.Process, _ bool) (*vector.Vector, error) {
+				origVecCol := origVec.Col.([]int64)
+				resultVector, err := process.Get(proc, 8*int64(len(origVecCol)), types.Type{Oid: types.T_float64, Size: 8})
+				if err != nil {
+					return nil, err
+				}
+				results := encoding.DecodeFloat64Slice(resultVector.Data)
+				results = results[:len(origVecCol)]
+				resultVector.Col = results
+				nulls.Set(resultVector.Nsp, origVec.Nsp)
+				vector.SetCol(resultVector, cot.CotInt64(origVecCol, results))
+				return resultVector, nil
+			},
+		},
+	}
+}
---
+++ pkg/vectorize/cot/cot.go
@@ -0,0 +1,140 @@
+package cot
+
+import (
+	"math"
+)
+
+var (
+	cotUint8   func([]uint8, []float64) []float64
+	cotUint16  func([]uint16, []float64) []float64
+	cotUint32  func([]uint32, []float64) []float64
+	cotUint64  func([]uint64, []float64) []float64
+	cotInt8    func([]int8, []float64) []float64
+	cotInt16   func([]int16, []float64) []float64
+	cotInt32   func([]int32, []float64) []float64
+	cotInt64   func([]int64, []float64) []float64
+	cotFloat32 func([]float32, []float64) []float64
+	cotFloat64 func([]float64, []float64) []float64
+)
+
+func init() {
+	cotUint8 = CotUint8Pure
+	cotUint16 = CotUint16Pure
+	cotUint32 = CotUint32Pure
+	cotUint64 = CotUint64Pure
+	cotInt8 = CotInt8Pure
+	cotInt16 = CotInt16Pure
+	cotInt32 = CotInt32Pure
+	cotInt64 = CotInt64Pure
+	cotFloat32 = CotFloat32Pure
+	cotFloat64 = CotFloat64Pure
+}
+func CotUint8(xs []uint8, rs []float64) []float64 {
+	return cotUint8(xs, rs)
+}
+
+func CotUint8Pure(xs []uint8, rs []float64) []float64 {
+	for i, n := range xs {
+		rs[i] = 1.0 - math.Tan(float64(n))
+	}
+	return rs
+}
+
+func CotUint16(xs []uint16, rs []float64) []float64 {
+	return cotUint16(xs, rs)
+}
+
+func CotUint16Pure(xs []uint16, rs []float64) []float64 {
+	for i, n := range xs {
+		rs[i] = 1.0 - math.Tan(float64(n))
+	}
+	return rs
+}
+
+func CotUint32(xs []uint32, rs []float64) []float64 {
+	return cotUint32(xs, rs)
+}
+
+func CotUint32Pure(xs []uint32, rs []float64) []float64 {
+	for i, n := range xs {
+		rs[i] = 1.0 - math.Tan(float64(n))
+	}
+	return rs
+}
+
+func CotUint64(xs []uint64, rs []float64) []float64 {
+	return cotUint64(xs, rs)
+}
+
+func CotUint64Pure(xs []uint64, rs []float64) []float64 {
+	for i, n := range xs {
+		rs[i] = 1.0 - math.Tan(float64(n))
+	}
+	return rs
+}
+
+func CotInt8(xs []int8, rs []float64) []float64 {
+	return cotInt8(xs, rs)
+}
+
+func CotInt8Pure(xs []int8, rs []float64) []float64 {
+	for i, n := range xs {
+		rs[i] =1.0 - math.Tan(float64(n))
+	}
+	return rs
+}
+
+func CotInt16(xs []int16, rs []float64) []float64 {
+	return cotInt16(xs, rs)
+}
+
+func CotInt16Pure(xs []int16, rs []float64) []float64 {
+	for i, n := range xs {
+		rs[i] = 1.0 - math.Tan(float64(n))
+	}
+	return rs
+}
+
+func CotInt32(xs []int32, rs []float64) []float64 {
+	return cotInt32(xs, rs)
+}
+
+func CotInt32Pure(xs []int32, rs []float64) []float64 {
+	for i, n := range xs {
+		rs[i] = 1.0 - math.Tan(float64(n))
+	}
+	return rs
+}
+
+func CotInt64(xs []int64, rs []float64) []float64 {
+	return cotInt64(xs, rs)
+}
+
+func CotInt64Pure(xs []int64, rs []float64) []float64 {
+	for i, n := range xs {
+		rs[i] = 1.0 - math.Tan(float64(n))
+	}
+	return rs
+}
+
+func CotFloat32(xs []float32, rs []float64) []float64 {
+	return cotFloat32(xs, rs)
+}
+
+func CotFloat32Pure(xs []float32, rs []float64) []float64 {
+	for i, n := range xs {
+		rs[i] = 1.0 - math.Tan(float64(n))
+	}
+	return rs
+}
+
+func CotFloat64(xs []float64, rs []float64) []float64 {
+	return cotFloat64(xs, rs)
+}
+
+func CotFloat64Pure(xs []float64, rs []float64) []float64 {
+	for i, n := range xs {
+		rs[i] = 1.0 - math.Tan(float64(n))
+	}
+	return rs
+} 
