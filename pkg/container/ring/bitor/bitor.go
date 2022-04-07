40e9b6e0a6ed42359e185de7d4a615196d3b2f31
NTH19 <74389817+NTH19@users.noreply.github.com>
2022-04-07 17:06:11 +0800

    Fix a bitor problem of bit_or  (#2173)

--- pkg/container/ring/bitor/bitor.go
+++ pkg/container/ring/bitor/bitor.go
@@ -133,9 +133,8 @@ func (r *BitOrRing) Grows(size int, m *mheap.Mheap) error {
 	}
 	r.Values = r.Values[:n+size]
 
-	// set all empty to ^uint64(0)
 	for i := 0; i < size; i++ {
-		r.Values[n+i] = ^uint64(0)
+		r.Values[n+i] = 0
 	}
 
 	for i := 0; i < size; i++ {
