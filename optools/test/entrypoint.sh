e7a4ad5021628c03682f958425baefe6cfff6cac
lei <wanglei4687@gmail.com>
2022-04-07 20:49:19 +0800

    add docker config

--- optools/test/entrypoint.sh
+++ optools/test/entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 sed -i "s/HOST/${HOST}/g" system_vars_config.toml
 sed -i "s/NAME/${NAME}/g" system_vars_config.toml
 
-/usr/local/bin/dumb-init ./mo-server ./system_vars_config.toml
+./mo-server ./system_vars_config.toml
