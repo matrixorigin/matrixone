b315cd57320a3e70969ea726e817edc3c720e6dd
NTH19 <74389817+NTH19@users.noreply.github.com>
2022-04-07 11:57:58 +0800

    Update bitor.go

--- pkg/container/ring/bitor/bitor.go
+++ pkg/container/ring/bitor/bitor.go
@@ -133,7 +133,7 @@ func (r *BitOrRing) Grows(size int, m *mheap.Mheap) error {
 	}
 	r.Values = r.Values[:n+size]
 
-	// set all empty to ^uint64(0)
+	// set all empty to 0
 	for i := 0; i < size; i++ {
 		r.Values[n+i] = 0
 	}
