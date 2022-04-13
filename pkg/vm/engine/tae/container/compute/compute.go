6d72435595501cce92d6d8ab9dcbe8ec0ee6560d
XuPeng-SH <xupeng3112@163.com>
2022-04-13 18:19:05 +0800

    (tae): small refactor 2

--- pkg/vm/engine/tae/container/compute/compute.go
+++ pkg/vm/engine/tae/container/compute/compute.go
@@ -1,9 +1,13 @@
 package compute
 
 import (
+	"github.com/RoaringBitmap/roaring"
+	"github.com/RoaringBitmap/roaring/roaring64"
 	gbat "github.com/matrixorigin/matrixone/pkg/container/batch"
+	"github.com/matrixorigin/matrixone/pkg/container/nulls"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	gvec "github.com/matrixorigin/matrixone/pkg/container/vector"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/common"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/container/batch"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/container/vector"
 )
@@ -326,4 +330,150 @@ func CopyToIBatch(data *gbat.Batch, capacity uint64) (bat batch.IBatch, err erro
 	}
 	bat, err = batch.NewBatch(attrs, vecs)
 	return
+}
+
+func ApplyDeleteToVector(vec *gvec.Vector, deletes *roaring.Bitmap) *gvec.Vector {
+	if deletes == nil || deletes.GetCardinality() == 0 {
+		return vec
+	}
+	col := vec.Col
+	deletesIterator := deletes.Iterator()
+	nsp := &nulls.Nulls{}
+	nsp.Np = &roaring64.Bitmap{}
+	var nspIterator roaring64.IntPeekable64
+	if vec.Nsp != nil && vec.Nsp.Np != nil {
+		nspIterator = vec.Nsp.Np.Iterator()
+	}
+	deleted := 0
+	switch vec.Typ.Oid {
+	case types.T_int8, types.T_int16, types.T_int32, types.T_int64, types.T_uint8, types.T_uint16, types.T_uint32, types.T_uint64,
+		types.T_decimal, types.T_float32, types.T_float64, types.T_date, types.T_datetime:
+		vec.Col = common.InplaceDeleteRows(vec.Col, deletesIterator)
+		deletesIterator = deletes.Iterator()
+		for deletesIterator.HasNext() {
+			row := deletesIterator.Next()
+			if nspIterator != nil {
+				var n uint64
+				if nspIterator.HasNext() {
+					for nspIterator.HasNext() {
+						n = nspIterator.PeekNext()
+						if uint32(n) < row {
+							nspIterator.Next()
+						} else {
+							if uint32(n) == row {
+								nspIterator.Next()
+							}
+							break
+						}
+						nsp.Np.Add(n - uint64(deleted))
+					}
+				}
+			}
+			deleted++
+		}
+		if nspIterator != nil {
+			for nspIterator.HasNext() {
+				n := nspIterator.Next()
+				nsp.Np.Add(n - uint64(deleted))
+			}
+		}
+	case types.T_char, types.T_varchar, types.T_json:
+		data := col.(*types.Bytes)
+		pre := -1
+		for deletesIterator.HasNext() {
+			row := deletesIterator.Next()
+			currRow := row - uint32(deleted)
+			if pre != -1 {
+				if int(currRow) == len(data.Lengths)-1 {
+					UpdateOffsets(data, pre-1, int(currRow))
+				} else {
+					UpdateOffsets(data, pre-1, int(currRow)+1)
+				}
+			}
+			if int(currRow) == len(data.Lengths)-1 {
+				data.Data = data.Data[:data.Offsets[currRow]]
+				data.Lengths = data.Lengths[:currRow]
+				data.Offsets = data.Offsets[:currRow]
+			} else {
+				data.Data = append(data.Data[:data.Offsets[currRow]], data.Data[data.Offsets[currRow+1]:]...)
+				data.Lengths = append(data.Lengths[:currRow], data.Lengths[currRow+1:]...)
+				data.Offsets = append(data.Offsets[:currRow], data.Offsets[currRow+1:]...)
+			}
+			if nspIterator != nil {
+				var n uint64
+				if nspIterator.HasNext() {
+					for nspIterator.HasNext() {
+						n = nspIterator.PeekNext()
+						if uint32(n) < row {
+							nspIterator.Next()
+						} else {
+							if uint32(n) == row {
+								nspIterator.Next()
+							}
+							break
+						}
+						nsp.Np.Add(n - uint64(deleted))
+					}
+				}
+			}
+			deleted++
+			pre = int(currRow)
+		}
+		if nspIterator != nil {
+			for nspIterator.HasNext() {
+				n := nspIterator.Next()
+				nsp.Np.Add(n - uint64(deleted))
+			}
+		}
+		if pre != -1 {
+			UpdateOffsets(data, pre-1, len(data.Lengths)-1)
+		}
+	}
+	vec.Nsp = nsp
+	return vec
+}
+
+func ApplyUpdateToVector(vec *gvec.Vector, mask *roaring.Bitmap, vals map[uint32]interface{}) *gvec.Vector {
+	if mask == nil || mask.GetCardinality() == 0 {
+		return vec
+	}
+	iterator := mask.Iterator()
+	col := vec.Col
+	switch vec.Typ.Oid {
+	case types.T_int8, types.T_int16, types.T_int32, types.T_int64, types.T_uint8, types.T_uint16, types.T_uint32, types.T_uint64,
+		types.T_decimal, types.T_float32, types.T_float64, types.T_date, types.T_datetime:
+		for iterator.HasNext() {
+			row := iterator.Next()
+			SetFixSizeTypeValue(vec, row, vals[row])
+			if vec.Nsp != nil && vec.Nsp.Np != nil {
+				if vec.Nsp.Np.Contains(uint64(row)) {
+					vec.Nsp.Np.Flip(uint64(row), uint64(row+1))
+				}
+			}
+		}
+	case types.T_char, types.T_varchar, types.T_json:
+		data := col.(*types.Bytes)
+		pre := -1
+		for iterator.HasNext() {
+			row := iterator.Next()
+			if pre != -1 {
+				UpdateOffsets(data, pre, int(row))
+			}
+			val := vals[row].([]byte)
+			suffix := data.Data[data.Offsets[row]+data.Lengths[row]:]
+			data.Lengths[row] = uint32(len(val))
+			val = append(val, suffix...)
+			data.Data = append(data.Data[:data.Offsets[row]], val...)
+			pre = int(row)
+			if vec.Nsp != nil && vec.Nsp.Np != nil {
+				if vec.Nsp.Np.Contains(uint64(row)) {
+					vec.Nsp.Np.Flip(uint64(row), uint64(row+1))
+				}
+			}
+		}
+		if pre != -1 {
+			UpdateOffsets(data, pre, len(data.Lengths)-1)
+		}
+	}
+	return vec
 }
