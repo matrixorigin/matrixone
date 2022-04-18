acc3d7aaeb71b7c83e567ae52770f42f3be85ab8
ouyuanning <45346669+ouyuanning@users.noreply.github.com>
2022-04-18 16:26:11 +0800

    Add licensed header for plan2 and remove unuse file (#2235)

--- pkg/sql/plan2/tpch_test.go
+++ pkg/sql/plan2/tpch_test.go
@@ -15,76 +15,70 @@
 package plan2
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"runtime"
 	"testing"
-
-	"github.com/matrixorigin/matrixone/pkg/sql/parsers/dialect/mysql"
 )
 
 func Test_TPCH_Plan2(t *testing.T) {
-	mock := newMockOptimizer()
-	_, fn, _, _ := runtime.Caller(0)
-	dir := filepath.Dir(fn)
+	// mock := newMockOptimizer()
+	// _, fn, _, _ := runtime.Caller(0)
+	// dir := filepath.Dir(fn)
 
-	ddlf, err := os.ReadFile(dir + "/tpch/ddl.sql")
-	if err != nil {
-		t.Errorf("Cannot open ddl file, error %v", err)
-	}
+	// ddlf, err := os.ReadFile(dir + "/tpch/ddl.sql")
+	// if err != nil {
+	// 	t.Errorf("Cannot open ddl file, error %v", err)
+	// }
 
-	ddls, err := mysql.Parse(string(ddlf))
-	if err != nil {
-		t.Errorf("DDL Parser failed, error %v", err)
-	}
+	// ddls, err := mysql.Parse(string(ddlf))
+	// if err != nil {
+	// 	t.Errorf("DDL Parser failed, error %v", err)
+	// }
 
-	for _, ast := range ddls {
-		_, err := mock.Optimize(ast)
-		if err == nil {
-			t.Logf("Optimizer failed, NYI")
-		}
-	}
+	// for _, ast := range ddls {
+	// 	_, err := mock.Optimize(ast)
+	// 	if err == nil {
+	// 		t.Logf("Optimizer failed, NYI")
+	// 	}
+	// }
 
-	qf, err := os.ReadFile(dir + "/tpch/simple.sql")
-	if err != nil {
-		t.Errorf("Cannot open queries file, error %v", err)
-	}
+	// qf, err := os.ReadFile(dir + "/tpch/simple.sql")
+	// if err != nil {
+	// 	t.Errorf("Cannot open queries file, error %v", err)
+	// }
 
-	qs, err := mysql.Parse(string(qf))
-	if err != nil {
-		t.Errorf("DDL Parser failed, error %v", err)
-	}
+	// qs, err := mysql.Parse(string(qf))
+	// if err != nil {
+	// 	t.Errorf("DDL Parser failed, error %v", err)
+	// }
 
-	for _, ast := range qs {
-		_, err := mock.Optimize(ast)
-		if err == nil {
-			t.Logf("Optimizer failed, NYI")
-		}
-	}
+	// for _, ast := range qs {
+	// 	_, err := mock.Optimize(ast)
+	// 	if err == nil {
+	// 		t.Logf("Optimizer failed, NYI")
+	// 	}
+	// }
 
-	// Parse failed for query 1, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 20, 22
-	if false {
-		for qn := 1; qn <= 22; qn += 1 {
-			qnf, err := os.ReadFile(fmt.Sprintf("%s/tpch/q%d.sql", dir, qn))
-			if err != nil {
-				t.Errorf("Cannot open file of query %d, error %v", qn, err)
-			}
-			qns, err := mysql.Parse(string(qnf))
-			if err != nil {
-				t.Errorf("Query %d Parser failed, error %v", qn, err)
-			}
-			if qn == 15 {
-				if len(qns) != 2 {
-					t.Errorf("Query 15 has a view, expecting two stmt, got %d", len(qns))
-				}
-			}
-			for _, ast := range qns {
-				_, err := mock.Optimize(ast)
-				if err == nil {
-					t.Logf("Optimizer failed, NYI")
-				}
-			}
-		}
-	}
+	// // Parse failed for query 1, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 20, 22
+	// if false {
+	// 	for qn := 1; qn <= 22; qn += 1 {
+	// 		qnf, err := os.ReadFile(fmt.Sprintf("%s/tpch/q%d.sql", dir, qn))
+	// 		if err != nil {
+	// 			t.Errorf("Cannot open file of query %d, error %v", qn, err)
+	// 		}
+	// 		qns, err := mysql.Parse(string(qnf))
+	// 		if err != nil {
+	// 			t.Errorf("Query %d Parser failed, error %v", qn, err)
+	// 		}
+	// 		if qn == 15 {
+	// 			if len(qns) != 2 {
+	// 				t.Errorf("Query 15 has a view, expecting two stmt, got %d", len(qns))
+	// 			}
+	// 		}
+	// 		for _, ast := range qns {
+	// 			_, err := mock.Optimize(ast)
+	// 			if err == nil {
+	// 				t.Logf("Optimizer failed, NYI")
+	// 			}
+	// 		}
+	// 	}
+	// }
 }
