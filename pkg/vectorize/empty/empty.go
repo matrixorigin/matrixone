a86a0d2303d01c069613231b87e7a552d6c599b9
e11jah <30852919+e11jah@users.noreply.github.com>
2022-04-29 15:38:54 +0800

    Add built-in function empty (#2276)

--- pkg/vectorize/empty/empty.go
+++ pkg/vectorize/empty/empty.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Matrix Origin
+// Copyright 2022 Matrix Origin
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,44 +11,25 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+package empty
 
-package builtin
+import "github.com/matrixorigin/matrixone/pkg/container/types"
 
-import "github.com/matrixorigin/matrixone/pkg/sql/colexec/extend/overload"
-
-const (
-	Length = iota + overload.NE + 1
-	Space
-	Reverse
-	Substring
-	Ltrim
-	Rtrim
-	StartsWith
-	Lpad
-	Rpad
- 	Empty
-	LengthUTF8 
-	Round
-	Floor
-	Abs
-	Log
-	Ln
-	Ceil
-	Exp
-	Power
-	Pi
-	Sin
-	Sinh
-	Cos
-	Acos
-	Tan
-	Atan
-	Cot
-	UTCTimestamp
-	DayOfYear
-	Month
-	Year
-	Weekday
-	EndsWith
-	Date
+var (
+	Empty func(*types.Bytes, []uint8) []uint8
 )
+
+func init() {
+	Empty = empty
+}
+
+func empty(xs *types.Bytes, rs []uint8) []uint8 {
+	for i := range xs.Offsets {
+		if xs.Lengths[i] == 0 {
+			rs[i] = 1
+		} else {
+			rs[i] = 0
+		}
+	}
+	return rs
+}
