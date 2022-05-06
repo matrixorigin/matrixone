deca21fb77e67ec3883b23076f2c91cbf6573077
lyfer233 <2217636939@qq.com>
2022-05-06 19:40:13 +0800

    bit_length function finished

--- pkg/vectorize/bit_length/bit_length.go
+++ pkg/vectorize/bit_length/bit_length.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Matrix Origin
+// Copyright 2022 Matrix Origin
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,44 +12,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package builtin
+package bit_length
 
-import "github.com/matrixorigin/matrixone/pkg/sql/colexec/extend/overload"
-
-const (
-	Length = iota + overload.NE + 1
-	Space
-	Reverse
-	Substring
-	Ltrim
-	Rtrim
-	StartsWith
-	Lpad
-	Rpad
-	Empty
-	LengthUTF8
-	Round
-	Floor
-	Abs
-	Log
-	Ln
-	Ceil
-	Exp
-	Power
-	Pi
-	Sin
-	Sinh
-	Cos
-	Acos
-	Tan
-	Atan
-	Cot
-	UTCTimestamp
-	DayOfYear
-	Month
-	Year
-	Weekday
-	EndsWith
-	Date
-	BitLength
+import (
+	"github.com/matrixorigin/matrixone/pkg/container/types"
 )
+
+var (
+	StrBitLength func(*types.Bytes, []int64) []int64
+)
+
+func init() {
+	StrBitLength = strBitLength
+}
+
+func strBitLength(xs *types.Bytes, rs []int64) []int64 {
+	for i, n := range xs.Lengths {
+		rs[i] = int64(n) * 8
+	}
+	return rs
+}
