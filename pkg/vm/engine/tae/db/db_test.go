ec491e49a00f974f3e4078fa7ad67179a8ff6854
XuPeng-SH <xupeng3112@163.com>
2022-05-13 01:35:16 +0800

    (tae): update ut

--- pkg/vm/engine/tae/db/db_test.go
+++ pkg/vm/engine/tae/db/db_test.go
@@ -653,11 +653,11 @@ func TestAutoCompactABlk1(t *testing.T) {
 		rel.Append(bat)
 		assert.Nil(t, txn.Commit())
 	}
+	err := tae.Catalog.Checkpoint(tae.Scheduler.GetSafeTS())
+	assert.Nil(t, err)
 	testutils.WaitExpect(1000, func() bool {
 		return tae.Scheduler.GetPenddingLSNCnt() == 0
 	})
-	err := tae.Catalog.Checkpoint(tae.Scheduler.GetSafeTS())
-	assert.Nil(t, err)
 	assert.Equal(t, uint64(0), tae.Scheduler.GetPenddingLSNCnt())
 	t.Log(tae.Catalog.SimplePPString(common.PPL1))
 	{
