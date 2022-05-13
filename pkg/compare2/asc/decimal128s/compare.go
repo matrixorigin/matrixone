a6a282d6f6f9abaa0efc9271f7f6a24aa261c00c
broccoliSpicy <broccoli.spicy@outlook.com>
2022-05-13 10:17:59 +0800

    add decimal sort and decimal compare2

--- pkg/compare2/asc/decimal128s/compare.go
+++ pkg/compare2/asc/decimal128s/compare.go
@@ -12,18 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package decimal64s
+package decimal128s
 
 import (
 	"github.com/matrixorigin/matrixone/pkg/container/nulls"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
-	"github.com/matrixorigin/matrixone/pkg/vm/process"
+	process "github.com/matrixorigin/matrixone/pkg/vm/process2"
 )
 
 func New() *compare {
 	return &compare{
-		xs: make([][]types.Decimal64, 2),
+		xs: make([][]types.Decimal128, 2),
 		ns: make([]*nulls.Nulls, 2),
 		vs: make([]*vector.Vector, 2),
 	}
@@ -36,12 +36,12 @@ func (c *compare) Vector() *vector.Vector {
 func (c *compare) Set(idx int, v *vector.Vector) {
 	c.vs[idx] = v
 	c.ns[idx] = v.Nsp
-	c.xs[idx] = v.Col.([]types.Decimal64)
+	c.xs[idx] = v.Col.([]types.Decimal128)
 }
 
 // Compare method for decimal needs to know the decimal's scale, so we need to fill in the c.vs field before using this function
 func (c *compare) Compare(veci, vecj int, vi, vj int64) int {
-	return int(types.CompareDecimal64Decimal64(c.xs[veci][vi], c.xs[vecj][vj], c.vs[0].Typ.Scale, c.vs[1].Typ.Scale))
+	return int(types.CompareDecimal128Decimal128(c.xs[veci][vi], c.xs[vecj][vj], c.vs[0].Typ.Scale, c.vs[1].Typ.Scale))
 }
 
 func (c *compare) Copy(vecSrc, vecDst int, src, dst int64, _ *process.Process) error {
--- pkg/compare2/desc/decimal64s/compare.go
+++ pkg/compare2/desc/decimal64s/compare.go
@@ -1,24 +1,10 @@
-// Copyright 2021 Matrix Origin
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package decimal64s
 
 import (
 	"github.com/matrixorigin/matrixone/pkg/container/nulls"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
-	"github.com/matrixorigin/matrixone/pkg/vm/process"
+	process "github.com/matrixorigin/matrixone/pkg/vm/process2"
 )
 
 func New() *compare {
@@ -41,7 +27,7 @@ func (c *compare) Set(idx int, v *vector.Vector) {
 
 // Compare method for decimal needs to know the decimal's scale, so we need to fill in the c.vs field before using this function
 func (c *compare) Compare(veci, vecj int, vi, vj int64) int {
-	return int(types.CompareDecimal64Decimal64(c.xs[veci][vi], c.xs[vecj][vj], c.vs[0].Typ.Scale, c.vs[1].Typ.Scale))
+	return -int(types.CompareDecimal64Decimal64(c.xs[veci][vi], c.xs[vecj][vj], c.vs[0].Typ.Scale, c.vs[1].Typ.Scale))
 }
 
 func (c *compare) Copy(vecSrc, vecDst int, src, dst int64, _ *process.Process) error {
