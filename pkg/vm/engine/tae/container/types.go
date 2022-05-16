6eab24fac8a070ecfd83cb44b233996f8e10487b
XuPeng-SH <xupeng3112@163.com>
2022-05-16 19:23:44 +0800

    (tae): update for container

--- pkg/vm/engine/tae/container/types.go
+++ pkg/vm/engine/tae/container/types.go
@@ -24,9 +24,9 @@ import (
 type Mask = uint64
 
 const (
-	ReadonlyMask Mask = 0x01000000
-	HasNullMask  Mask = 0x02000000
-	PosMask      Mask = 0x00FFFFFF
+	ReadonlyMask Mask = 0x10000000
+	HasNullMask  Mask = 0x20000000
+	PosMask      Mask = 0x0FFFFFFF
 )
 
 type VectorType uint8
