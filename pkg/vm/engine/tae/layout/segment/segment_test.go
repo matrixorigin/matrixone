def69e813028724269a62713c8b35b1e968d3c65
GreatRiver <2552853833@qq.com>
2022-05-18 18:49:47 +0800

    Add UT & Fix level0 offset (#2575)
    * Add UT & Fix level0 offset
    
    * Enable bitmap level1

--- pkg/vm/engine/tae/layout/segment/segment_test.go
+++ pkg/vm/engine/tae/layout/segment/segment_test.go
@@ -29,6 +29,78 @@ const (
 	ModuleName = "LAYOUT"
 )
 
+func mackData(size uint32) []byte {
+	var sbuffer bytes.Buffer
+	err := binary.Write(&sbuffer, binary.BigEndian, []byte(fmt.Sprintf("this is tests %d", size)))
+	if err != nil {
+		return nil
+	}
+	ibufLen := (size - (uint32(sbuffer.Len()) % size)) + uint32(sbuffer.Len())
+	if ibufLen > uint32(sbuffer.Len()) {
+		zero := make([]byte, ibufLen-uint32(sbuffer.Len()))
+		err = binary.Write(&sbuffer, binary.BigEndian, zero)
+		if err != nil {
+			return nil
+		}
+	}
+	return sbuffer.Bytes()
+}
+
+/*func debugBitmap(b *BitmapAllocator) (info string) {
+	if len(b.level1) < 20 {
+		// log allocator
+		return ""
+	}
+	info = fmt.Sprintf("level0-")
+	for i := 0; i < 200; i++ {
+		info = fmt.Sprintf("%v-%x\n", info, b.level0[i])
+	}
+	info = fmt.Sprintf("%v-level1-", info)
+	for i := 0; i < 5; i++ {
+		info = fmt.Sprintf("%v-%x\n", info, b.level1[i])
+	}
+	return info
+}*/
+
+func TestBitmapAllocator(t *testing.T) {
+	dir := testutils.InitTestEnv(ModuleName, t)
+	name := path.Join(dir, "init.seg")
+	seg := Segment{}
+	err := seg.Init(name)
+	assert.Nil(t, err)
+	seg.Mount()
+	file := seg.NewBlockFile("bitmap")
+	level0 := seg.allocator.(*BitmapAllocator).level0
+	level1 := seg.allocator.(*BitmapAllocator).level1
+	for i := 0; i < 20; i++ {
+		buffer1 := mackData(1048576)
+		assert.NotNil(t, buffer1)
+		err = file.segment.Append(file, buffer1)
+		assert.Nil(t, err)
+		buffer2 := mackData(4096)
+		assert.NotNil(t, buffer2)
+		err = file.segment.Append(file, buffer2)
+		assert.Nil(t, err)
+		buffer3 := mackData(5242880)
+		assert.NotNil(t, buffer3)
+		err = file.segment.Append(file, buffer3)
+		assert.Nil(t, err)
+	}
+	l0pos := uint32(file.snode.originSize) / seg.GetPageSize() / BITS_PER_UNIT
+	l1pos := l0pos / BITS_PER_UNITSET
+
+	assert.Equal(t, ALL_UNIT_CLEAR, int(level0[l0pos-1]))
+	ret := 0xFFFFFFFFFFF00000 - level0[l0pos]
+	assert.Equal(t, 0, int(ret))
+	ret = 0xF000000000000000 - level1[l1pos]
+	assert.Equal(t, 0, int(ret))
+
+	seg.allocator.Free(8192, 4096)
+	//ret = uint64(0x4) - level0[0]
+	assert.Equal(t, 4, int(level0[0]))
+	//fmt.Printf(debugBitmap(seg.allocator.(*BitmapAllocator)))
+}
+
 func TestSegment_Init(t *testing.T) {
 	dir := testutils.InitTestEnv(ModuleName, t)
 	name := path.Join(dir, "init.seg")
