80f548c308b65eb98afde8d9f768ca0254aabf47
broccoliSpicy <broccoli.spicy@outlook.com>
2022-05-26 18:51:53 +0800

    use mheap.Alloc instead of process.Get

--- pkg/sql/plan2/function/extend2/plus.go
+++ pkg/sql/plan2/function/extend2/plus.go
@@ -5,6 +5,7 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
 	"github.com/matrixorigin/matrixone/pkg/encoding"
 	"github.com/matrixorigin/matrixone/pkg/vectorize/add"
+	"github.com/matrixorigin/matrixone/pkg/vm/mheap"
 	"github.com/matrixorigin/matrixone/pkg/vm/process"
 	"golang.org/x/exp/constraints"
 )
@@ -19,39 +20,43 @@ func Plus[T constraints.Integer | constraints.Float](vectors []*vector.Vector, p
 	case left.IsConst && right.IsConst:
 		// in the case where the result is a const, I chose to return only one row containing the const,
 		// this may require corresponding changes in the frontend, is this okay?
-		resultVector, err := process.Get(proc, int64(resultElementSize), left.Typ)
-		if err != nil {
-			return nil, err
-		}
-		resultValues := encoding.DecodeFixedSlice[T](resultVector.Data, resultElementSize)
+		resultVector := vector.New(left.Typ)
+		resultValues := make([]T, 1)
 		nulls.Or(left.Nsp, right.Nsp, resultVector.Nsp)
 		vector.SetCol(resultVector, add.NumericAdd[T](leftValues, rightValues, resultValues)) // if our input contains null, this step may be redundant,
 		resultVector.IsConst = true
 		resultVector.Length = left.Length
 		return resultVector, nil
 	case left.IsConst && !right.IsConst:
-		resultVector, err := process.Get(proc, int64(resultElementSize*len(rightValues)), left.Typ)
+		data, err := mheap.Alloc(proc.Mp, int64(resultElementSize*len(rightValues)))
 		if err != nil {
 			return nil, err
 		}
+		resultVector := vector.New(left.Typ)
+		resultVector.Data = data
 		resultValues := encoding.DecodeFixedSlice[T](resultVector.Data, resultElementSize)
+		// resultValues := resultValues[:len(rightValues)] // I think this line of code is redundant
 		nulls.Or(left.Nsp, right.Nsp, resultVector.Nsp)
 		vector.SetCol(resultVector, add.NumericAddScalar[T](leftValues[0], rightValues, resultValues))
 		return resultVector, nil
 	case !left.IsConst && right.IsConst:
-		resultVector, err := process.Get(proc, int64(resultElementSize*len(leftValues)), left.Typ)
+		data, err := mheap.Alloc(proc.Mp, int64(resultElementSize*len(leftValues)))
 		if err != nil {
 			return nil, err
 		}
+		resultVector := vector.New(left.Typ)
+		resultVector.Data = data
 		resultValues := encoding.DecodeFixedSlice[T](resultVector.Data, resultElementSize)
 		nulls.Or(left.Nsp, right.Nsp, resultVector.Nsp)
 		vector.SetCol(resultVector, add.NumericAddScalar[T](rightValues[0], leftValues, resultValues))
 		return resultVector, nil
 	}
-	resultVector, err := process.Get(proc, int64(resultElementSize*len(leftValues)), left.Typ)
+	data, err := mheap.Alloc(proc.Mp, int64(resultElementSize*len(rightValues)))
 	if err != nil {
 		return nil, err
 	}
+	resultVector := vector.New(left.Typ)
+	resultVector.Data = data
 	resultValues := encoding.DecodeFixedSlice[T](resultVector.Data, resultElementSize)
 	nulls.Or(left.Nsp, right.Nsp, resultVector.Nsp)
 	vector.SetCol(resultVector, add.NumericAdd[T](leftValues, rightValues, resultValues))
