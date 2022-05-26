04806cd9377e0810226986c197add58f61005318
jiangxinmeng <jiangxinmeng@sjtu.edu.cn>
2022-05-26 18:26:11 +0800

    fix UpdateOffsets

--- pkg/vm/engine/tae/db/db_test.go
+++ pkg/vm/engine/tae/db/db_test.go
@@ -2259,7 +2259,7 @@ func TestChaos1(t *testing.T) {
 	blk := it.GetBlock()
 	view, err := blk.GetColumnDataById(int(schema.PrimaryKey), nil, nil)
 	assert.Equal(t, int(appendCnt), view.Length())
-	// view.ApplyDeletes()
+	view.ApplyDeletes()
 	t.Log(view.DeleteMask.String())
 	t.Log(view.String())
 }
