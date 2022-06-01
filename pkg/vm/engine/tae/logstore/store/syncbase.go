44364fa7ea0bc04eee210ba0d5354ab5dc8f13d2
jiangxinmeng <jiangxinmeng@sjtu.edu.cn>
2022-06-01 22:00:45 +0800

    use closeIntervals

--- pkg/vm/engine/tae/logstore/store/syncbase.go
+++ pkg/vm/engine/tae/logstore/store/syncbase.go
@@ -32,7 +32,7 @@ type syncBase struct {
 	syncing                      map[uint32]uint64
 	checkpointed, synced, ckpCnt *syncMap
 	uncommits                    map[uint32][]uint64
-	addrs                        map[uint32]map[int]common.ClosedInterval //group-version-glsn range
+	addrs                        map[uint32]map[int]common.ClosedIntervals //group-version-glsn range
 	addrmu                       sync.RWMutex
 }
 
@@ -130,7 +130,7 @@ func newSyncBase() *syncBase {
 		synced:        newSyncMap(),
 		ckpCnt:        newSyncMap(),
 		uncommits:     make(map[uint32][]uint64),
-		addrs:         make(map[uint32]map[int]common.ClosedInterval),
+		addrs:         make(map[uint32]map[int]common.ClosedIntervals),
 		addrmu:        sync.RWMutex{},
 	}
 }
@@ -153,7 +153,7 @@ func (base *syncBase) GetVersionByGLSN(groupId uint32, lsn uint64) (int, error) 
 		return 0, errors.New("group not existed")
 	}
 	for ver, interval := range versionsMap {
-		if interval.Contains(common.ClosedInterval{Start: lsn, End: lsn}) {
+		if interval.Contains(*common.NewClosedIntervalsByInt(lsn)) {
 			return ver, nil
 		}
 	}
@@ -216,13 +216,13 @@ func (base *syncBase) OnEntryReceived(v *entry.Info) error {
 	addr := v.Info.(*VFileAddress)
 	versionRanges, ok := base.addrs[addr.Group]
 	if !ok {
-		versionRanges = make(map[int]common.ClosedInterval)
+		versionRanges = make(map[int]common.ClosedIntervals)
 	}
 	interval, ok := versionRanges[addr.Version]
 	if !ok {
-		interval = common.ClosedInterval{}
+		interval = *common.NewClosedIntervals()
 	}
-	interval.TryMerge(common.ClosedInterval{Start: 0, End: addr.LSN})
+	interval.TryMerge(*common.NewClosedIntervalsByInt(addr.LSN))
 	versionRanges[addr.Version] = interval
 	base.addrs[addr.Group] = versionRanges
 	// fmt.Printf("versionsMap is %v\n", base.addrs)
