dfb5dfc02de331c3ac06ef51b67c75c35f224cb3
JackTan25 <tanboyu@whut.edu.cn>
2022-06-01 23:07:00 +0800

    finish cos,atan,cot migration, fix conflicts

--- pkg/sql/plan2/function/builtin/binary/findinset.go
+++ pkg/sql/plan2/function/builtin/binary/findinset.go
@@ -19,58 +19,58 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
 	"github.com/matrixorigin/matrixone/pkg/encoding"
-	"github.com/matrixorigin/matrixone/pkg/vectorize/power"
+	"github.com/matrixorigin/matrixone/pkg/vectorize/findinset"
 	"github.com/matrixorigin/matrixone/pkg/vm/process"
 )
 
-func Power(vectors []*vector.Vector, proc *process.Process) (*vector.Vector, error) {
+func FindInSet(vectors []*vector.Vector, proc *process.Process) (*vector.Vector, error) {
 	left, right := vectors[0], vectors[1]
-	leftValues, rightValues := left.Col.([]float64), right.Col.([]float64)
-	resultType := types.Type{Oid: types.T_float64, Size: 8}
+	leftValues, rightValues := left.Col.(*types.Bytes), right.Col.(*types.Bytes)
+	resultType := types.Type{Oid: types.T_uint64, Size: 8}
 	resultElementSize := int(resultType.Size)
 	switch {
-	case left.IsConst && right.IsConst:
+	case left.IsScalar() && right.IsScalar():
 		if left.ConstVectorIsNull() || right.ConstVectorIsNull() {
 			return proc.AllocScalarNullVector(resultType), nil
 		}
 		resultVector := vector.NewConst(resultType)
-		resultValues := make([]float64, 1)
-		vector.SetCol(resultVector, power.Power(leftValues, rightValues, resultValues)) // if our input contains null, this step may be redundant,
+		resultValues := make([]uint64, 1)
+		vector.SetCol(resultVector, findinset.FindInSetWithAllConst(leftValues, rightValues, resultValues))
 		return resultVector, nil
-	case left.IsConst && !right.IsConst:
+	case left.IsScalar() && !right.IsScalar():
 		if left.ConstVectorIsNull() {
 			return proc.AllocScalarNullVector(resultType), nil
 		}
-		resultVector, err := proc.AllocVector(resultType, int64(resultElementSize*len(rightValues)))
+		resultVector, err := proc.AllocVector(resultType, int64(resultElementSize*len(rightValues.Lengths)))
 		if err != nil {
 			return nil, err
 		}
-		resultValues := encoding.DecodeFloat64Slice(resultVector.Data)
-		resultValues = resultValues[:len(rightValues)]
+		resultValues := encoding.DecodeUint64Slice(resultVector.Data)
+		resultValues = resultValues[:len(rightValues.Lengths)]
 		nulls.Set(resultVector.Nsp, right.Nsp)
-		vector.SetCol(resultVector, power.PowerScalarLeftConst(leftValues[0], rightValues, resultValues))
+		vector.SetCol(resultVector, findinset.FindInSetWithLeftConst(leftValues, rightValues, resultValues))
 		return resultVector, nil
-	case !left.IsConst && right.IsConst:
+	case !left.IsScalar() && right.IsScalar():
 		if right.ConstVectorIsNull() {
 			return proc.AllocScalarNullVector(resultType), nil
 		}
-		resultVector, err := proc.AllocVector(resultType, int64(resultElementSize*len(leftValues)))
+		resultVector, err := proc.AllocVector(resultType, int64(resultElementSize*len(leftValues.Lengths)))
 		if err != nil {
 			return nil, err
 		}
-		resultValues := encoding.DecodeFloat64Slice(resultVector.Data)
-		resultValues = resultValues[:len(leftValues)]
+		resultValues := encoding.DecodeUint64Slice(resultVector.Data)
+		resultValues = resultValues[:len(leftValues.Lengths)]
 		nulls.Set(resultVector.Nsp, left.Nsp)
-		vector.SetCol(resultVector, power.PowerScalarRightConst(rightValues[0], leftValues, resultValues))
+		vector.SetCol(resultVector, findinset.FindInSetWithRightConst(leftValues, rightValues, resultValues))
 		return resultVector, nil
 	}
-	resultVector, err := proc.AllocVector(resultType, int64(resultElementSize*len(rightValues)))
+	resultVector, err := proc.AllocVector(resultType, int64(resultElementSize*len(rightValues.Lengths)))
 	if err != nil {
 		return nil, err
 	}
-	resultValues := encoding.DecodeFloat64Slice(resultVector.Data)
-	resultValues = resultValues[:len(leftValues)]
+	resultValues := encoding.DecodeUint64Slice(resultVector.Data)
+	resultValues = resultValues[:len(rightValues.Lengths)]
 	nulls.Or(left.Nsp, right.Nsp, resultVector.Nsp)
-	vector.SetCol(resultVector, power.Power(leftValues, rightValues, resultValues))
+	vector.SetCol(resultVector, findinset.FindInSet(leftValues, rightValues, resultValues))
 	return resultVector, nil
 }
--- pkg/sql/plan2/function/builtin/multi/pi.go
+++ pkg/sql/plan2/function/builtin/multi/pi.go
@@ -15,21 +15,17 @@
 package multi
 
 import (
-	"github.com/matrixorigin/matrixone/pkg/container/nulls"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
 	"github.com/matrixorigin/matrixone/pkg/vectorize/pi"
 	"github.com/matrixorigin/matrixone/pkg/vm/process"
 )
 
-func Pi(lv []*vector.Vector, proc *process.Process) (*vector.Vector, error) {
-	vec, err := proc.AllocVector(types.Type{Oid: types.T_float64, Size: 8}, 0)
-	if err != nil {
-		return nil, err
-	}
-	nulls.Set(vec.Nsp, new(nulls.Nulls))
-	result := make([]float64, 0)
-	result = append(result, pi.GetPi())
-	vector.SetCol(vec, result)
-	return vec, nil
+func Pi(_ []*vector.Vector, _ *process.Process) (*vector.Vector, error) {
+	resultType := types.Type{Oid: types.T_float64, Size: 8}
+	resultVector := vector.NewConst(resultType)
+	result := make([]float64, 1)
+	result[0] = pi.GetPi()
+	vector.SetCol(resultVector, result)
+	return resultVector, nil
 }
