91f552f7013c4bfc4666522f8a07e7f5f7b2487d
zhangxu <zhangxu19830126@gmail.com>
2022-06-09 12:52:26 +0800

    Add make pb to makefile

---
+++ proto/gen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Generate all matrixcube protobuf bindings.
+# Run from repository root.
+#
+set -e
+
+PB_DIR="$PWD/pkg/pb"
+PROTOC_DIR="$PWD/proto"
+
+mkdir -p $PB_DIR/plan
+protoc --proto_path=$PROTOC_DIR --go_out=paths=source_relative:$PB_DIR/plan  $PROTOC_DIR/plan.proto 
