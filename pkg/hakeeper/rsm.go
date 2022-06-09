6321c58f050ff8638f5aef5e864a7e885dadec1f
Lei Ni <nilei81@gmail.com>
2022-06-09 13:50:45 +0800

    hakeeper: minor update

--- pkg/hakeeper/rsm.go
+++ pkg/hakeeper/rsm.go
@@ -23,6 +23,7 @@ import (
 	sm "github.com/lni/dragonboat/v4/statemachine"
 
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
+	"github.com/matrixorigin/matrixone/pkg/pb/logservice"
 )
 
 var (
@@ -41,8 +42,10 @@ const (
 	// DefaultHAKeeperShardID is the shard ID assigned to the special HAKeeper
 	// shard.
 	DefaultHAKeeperShardID uint64 = 0
-	queryLogShardIDTag     uint16 = 0xAE01
-	createLogShardTag      uint16 = 0xAE02
+
+	createLogShardTag uint16 = 0xAE01
+	dnHeartbeatTag    uint16 = 0xAE02
+	logHeartbeatTag   uint16 = 0xAE03
 )
 
 type logShardIDQuery struct {
@@ -56,8 +59,11 @@ type logShardIDQueryResult struct {
 
 type stateMachine struct {
 	replicaID uint64
+	Tick      uint64
 	NextID    uint64
 	LogShards map[string]uint64
+	DNState   DNState
+	LogState  LogState
 }
 
 func parseCmdTag(cmd []byte) uint16 {
@@ -79,6 +85,25 @@ func isCreateLogShardCmd(cmd []byte) (string, bool) {
 	return isLogShardCmd(cmd, createLogShardTag)
 }
 
+func isDNHeartbeatCmd(cmd []byte) bool {
+	return isHeartbeatCmd(cmd, dnHeartbeatTag)
+}
+
+func isLogHeartbeatCmd(cmd []byte) bool {
+	return isHeartbeatCmd(cmd, logHeartbeatTag)
+}
+
+func isHeartbeatCmd(cmd []byte, tag uint16) bool {
+	if len(cmd) <= headerSize {
+		return false
+	}
+	return parseCmdTag(cmd) == tag
+}
+
+func getHeartbeatCmd(cmd []byte) []byte {
+	return cmd[headerSize:]
+}
+
 func isLogShardCmd(cmd []byte, tag uint16) (string, bool) {
 	if len(cmd) <= headerSize {
 		return "", false
@@ -96,7 +121,13 @@ func NewStateMachine(shardID uint64, replicaID uint64) sm.IStateMachine {
 	return &stateMachine{
 		replicaID: replicaID,
 		LogShards: make(map[string]uint64),
+		DNState:   NewDNState(),
+		LogState:  NewLogState(),
 	}
+}
+
+func (s *stateMachine) Close() error {
+	return nil
 }
 
 func (s *stateMachine) assignID() uint64 {
@@ -118,14 +149,34 @@ func (s *stateMachine) handleCreateLogShardCmd(cmd []byte) (sm.Result, error) {
 	return sm.Result{Value: s.NextID}, nil
 }
 
-func (s *stateMachine) Close() error {
-	return nil
+func (s *stateMachine) handleDNHeartbeat(cmd []byte) (sm.Result, error) {
+	data := getHeartbeatCmd(cmd)
+	var hb logservice.DNStoreHeartbeat
+	if err := hb.Unmarshal(data); err != nil {
+		panic(err)
+	}
+	s.DNState.Update(hb, s.Tick)
+	return sm.Result{}, nil
+}
+
+func (s *stateMachine) handleLogHeartbeat(cmd []byte) (sm.Result, error) {
+	data := getHeartbeatCmd(cmd)
+	var hb logservice.LogStoreHeartbeat
+	if err := hb.Unmarshal(data); err != nil {
+		panic(err)
+	}
+	s.LogState.Update(hb, s.Tick)
+	return sm.Result{}, nil
 }
 
 func (s *stateMachine) Update(e sm.Entry) (sm.Result, error) {
 	cmd := e.Cmd
 	if _, ok := isCreateLogShardCmd(cmd); ok {
 		return s.handleCreateLogShardCmd(cmd)
+	} else if isDNHeartbeatCmd(cmd) {
+		return s.handleDNHeartbeat(cmd)
+	} else if isLogHeartbeatCmd(cmd) {
+		return s.handleLogHeartbeat(cmd)
 	}
 	panic(moerr.NewError(moerr.INVALID_INPUT, "unexpected haKeeper cmd"))
 }
