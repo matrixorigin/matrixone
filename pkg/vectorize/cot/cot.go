223b24e85f2f23f6293937cfbbdd738a089e1ebc
JackTan25 <tanboyu@whut.edu.cn>
2022-06-10 16:54:38 +0800

    fix redundant computation of Tan

--- pkg/vectorize/cot/cot.go
+++ pkg/vectorize/cot/cot.go
@@ -118,12 +118,13 @@ func cotFloat64(xs []float64, rs []float64) []float64 {
 
 func Cot[T constraints.Integer | constraints.Float](inputValues []T, resultValues []float64) []float64 {
 	for i, n := range inputValues {
-		if math.Tan(float64(n)) < 1e-9 && math.Tan(float64(n)) >= 0 {
+		res := math.Tan(float64(n))
+		if res < 1e-9 && res >= 0 {
 			resultValues[i] = 16331239353195392.0000
-		} else if math.Tan(float64(n)) > -1e-9 && math.Tan(float64(n)) < 0 {
+		} else if res > -1e-9 && res < 0 {
 			resultValues[i] = -16331239353195392.0000
 		} else {
-			resultValues[i] = 1.0 / math.Tan(float64(n))
+			resultValues[i] = 1.0 / res
 		}
 	}
 	return resultValues
