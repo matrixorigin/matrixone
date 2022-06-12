2240ce3fee3ec4ee82ee973555c86fa3f0b51a5c
GreatRiver <2552853833@qq.com>
2022-06-12 18:51:55 +0800

    Support default value for tae (#2891)
    * Support default value for tae
    
    * Update SchemaToDefs
    
    * Add UT
    
    * Modify form of default value (#2)
    
    Co-authored-by: iamlinjunhong <49111204+iamlinjunhong@users.noreply.github.com>
    Co-authored-by: Jin Hai <haijin.chn@gmail.com>

--- pkg/frontend/insert.go
+++ pkg/frontend/insert.go
@@ -577,6 +577,7 @@ func buildInsertValues(stmt *tree.Insert, plan *InsertValues, eg engine.Engine, 
 		}
 		bat.Vecs = append(bat.Vecs, vec)
 	}
+	batch.Reorder(bat, orderAttr)
 	plan.dataBatch = bat
 	return nil
 }
@@ -587,23 +588,60 @@ func makeExprFromVal(typ types.Type, value interface{}, isNull bool) tree.Expr {
 		return tree.NewNumVal(constant.MakeUnknown(), "NULL", false)
 	}
 	switch typ.Oid {
-	case types.T_int8, types.T_int16, types.T_int32, types.T_int64:
+	case types.T_int8:
+		res := int64(value.(int8))
+		str := strconv.FormatInt(res, 10)
+		if res < 0 {
+			return tree.NewNumVal(constant.MakeUint64(uint64(-res)), str, true)
+		}
+		return tree.NewNumVal(constant.MakeInt64(res), str, false)
+	case types.T_int16:
+		res := int64(value.(int16))
+		str := strconv.FormatInt(res, 10)
+		if res < 0 {
+			return tree.NewNumVal(constant.MakeUint64(uint64(-res)), str, true)
+		}
+		return tree.NewNumVal(constant.MakeInt64(res), str, false)
+	case types.T_int32:
+		res := int64(value.(int32))
+		str := strconv.FormatInt(res, 10)
+		if res < 0 {
+			return tree.NewNumVal(constant.MakeUint64(uint64(-res)), str, true)
+		}
+		return tree.NewNumVal(constant.MakeInt64(res), str, false)
+	case types.T_int64:
 		res := value.(int64)
 		str := strconv.FormatInt(res, 10)
 		if res < 0 {
 			return tree.NewNumVal(constant.MakeUint64(uint64(-res)), str, true)
 		}
 		return tree.NewNumVal(constant.MakeInt64(res), str, false)
-	case types.T_uint8, types.T_uint16, types.T_uint32, types.T_uint64:
+	case types.T_uint8:
+		res := uint64(value.(uint8))
+		str := strconv.FormatUint(res, 10)
+		return tree.NewNumVal(constant.MakeUint64(res), str, false)
+	case types.T_uint16:
+		res := uint64(value.(uint16))
+		str := strconv.FormatUint(res, 10)
+		return tree.NewNumVal(constant.MakeUint64(res), str, false)
+	case types.T_uint32:
+		res := uint64(value.(uint32))
+		str := strconv.FormatUint(res, 10)
+		return tree.NewNumVal(constant.MakeUint64(res), str, false)
+	case types.T_uint64:
 		res := value.(uint64)
 		str := strconv.FormatUint(res, 10)
 		return tree.NewNumVal(constant.MakeUint64(res), str, false)
-	case types.T_float32, types.T_float64:
+	case types.T_float32:
+		res := float64(value.(float32))
+		str := strconv.FormatFloat(res, 'f', 10, 64)
+		return tree.NewNumVal(constant.MakeFloat64(res), str, res < 0)
+	case types.T_float64:
 		res := value.(float64)
 		str := strconv.FormatFloat(res, 'f', 10, 64)
 		return tree.NewNumVal(constant.MakeFloat64(res), str, res < 0)
 	case types.T_char, types.T_varchar:
-		res := value.(string)
+		res := string(value.([]byte)[:])
 		return tree.NewNumVal(constant.MakeString(res), res, false)
 	case types.T_date:
 		res := value.(types.Date).String()
