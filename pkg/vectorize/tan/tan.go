2ce930cad985e58b5493cfead0d8f0c278acbd17
JackTan25 <tanboyu@whut.edu.cn>
2022-06-12 14:50:22 +0800

    delete rebundant codes

--- pkg/vectorize/tan/tan.go
+++ pkg/vectorize/tan/tan.go
@@ -15,7 +15,6 @@
 package tan
 
 import (
-	"fmt"
 	"math"
 
 	"golang.org/x/exp/constraints"
@@ -119,7 +118,6 @@ func tanFloat64(xs []float64, rs []float64) []float64 {
 
 func Tan[T constraints.Integer | constraints.Float](inputValues []T, resultValues []float64) []float64 {
 	for i, n := range inputValues {
-		fmt.Println(n, float64(n), math.Tan(float64(n)))
 		resultValues[i] = math.Tan(float64(n))
 	}
 	return resultValues
