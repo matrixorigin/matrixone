484e0c36909e25ddd8854ffa63317c4f1150b05d
nnsgmsone <nnsmgsone@outlook.com>
2022-06-17 15:29:00 +0800

    Fix the bug of aggregate function

--- pkg/container/ring/starcount/count.go
+++ pkg/container/ring/starcount/count.go
@@ -99,6 +99,7 @@ func (r *CountRing) Grow(m *mheap.Mheap) error {
 		r.Vs = encoding.DecodeInt64Slice(data)
 	}
 	r.Vs = r.Vs[:n+1]
+	r.Da = r.Da[:(n+1)*8]
 	r.Vs[n] = 0
 	r.Ns = append(r.Ns, 0)
 	return nil
@@ -125,6 +126,7 @@ func (r *CountRing) Grows(size int, m *mheap.Mheap) error {
 		r.Vs = encoding.DecodeInt64Slice(data)
 	}
 	r.Vs = r.Vs[:n+size]
+	r.Da = r.Da[:(n+size)*8]
 	for i := 0; i < size; i++ {
 		r.Ns = append(r.Ns, 0)
 	}
