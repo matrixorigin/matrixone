b2ec040e5548557ba3bd9a8a6108bc271faa32c4
Wei Ziran <weiziran125@gmail.com>
2022-06-17 15:43:08 +0800

    modify copyright statement

--- pkg/hakeeper/operator/steps.go
+++ pkg/hakeeper/operator/steps.go
@@ -1,5 +1,4 @@
 // Copyright 2020 PingCAP, Inc.
-// Modifications copyright (C) 2021 MatrixOrigin.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +10,13 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// Portions of this file are additionally subject to the following
+// copyright.
+//
+// Copyright (C) 2021 MatrixOrigin.
+//
+// Modified the behavior and the interface of the step.
 
 package operator
 
