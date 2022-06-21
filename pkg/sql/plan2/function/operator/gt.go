d33d71a522c14e1862ee22542e17b5593ee18e77
jianwan0214 <jianwan0214@gmail.com>
2022-06-21 15:28:33 +0800

    fix col compare with null bug in where expression

--- pkg/sql/plan2/function/operator/gt.go
+++ pkg/sql/plan2/function/operator/gt.go
@@ -20,6 +20,7 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/container/nulls"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
+	encoding2 "github.com/matrixorigin/matrixone/pkg/encoding"
 	"github.com/matrixorigin/matrixone/pkg/vectorize/gt"
 	"github.com/matrixorigin/matrixone/pkg/vectorize/lt"
 	"github.com/matrixorigin/matrixone/pkg/vm/process"
@@ -195,7 +196,17 @@ func ColGtConst[T DataValue](lv, rv *vector.Vector, proc *process.Process) (*vec
 }
 
 func ColGtNull[T DataValue](lv, rv *vector.Vector, proc *process.Process) (*vector.Vector, error) {
-	return proc.AllocScalarNullVector(proc.GetBoolTyp(lv.Typ)), nil
+	n := GetRetColLen[T](lv)
+	vec, err := proc.AllocVector(proc.GetBoolTyp(lv.Typ), int64(n)*1)
+	if err != nil {
+		return nil, err
+	}
+	col := encoding2.DecodeFixedSlice[bool](vec.Data, 1)
+	vector.SetCol(vec, col)
+	for i := 0; i < n; i++ {
+		nulls.Add(vec.Nsp, uint64(i))
+	}
+	return vec, nil
 }
 
 func ConstGtCol[T DataValue](lv, rv *vector.Vector, proc *process.Process) (*vector.Vector, error) {
@@ -228,7 +239,7 @@ func ConstGtNull[T DataValue](lv, rv *vector.Vector, proc *process.Process) (*ve
 }
 
 func NullGtCol[T DataValue](lv, rv *vector.Vector, proc *process.Process) (*vector.Vector, error) {
-	return proc.AllocScalarNullVector(proc.GetBoolTyp(lv.Typ)), nil
+	return ColGtNull[T](rv, lv, proc)
 }
 
 func NullGtConst[T DataValue](lv, rv *vector.Vector, proc *process.Process) (*vector.Vector, error) {
