7ee13d540a628c4d30c2d6dcb43c23e6cbf4aa27
nnsgmsone <nnsmgsone@outlook.com>
2022-06-24 14:58:12 +0800

    Add cast table

--- pkg/sql/plan2/function/type_check.go
+++ pkg/sql/plan2/function/type_check.go
@@ -27,6 +27,9 @@ type binaryTargetTypes struct {
 	left, right types.T
 }
 
+// castable indicates whether a type can be automatically converted to another type
+var castable [][]bool
+
 // binaryTable is a cast rule table for some binary-operators' parameters
 // e.g. PLUS, MINUS and so on.
 // Format is `binaryTable[LeftInput][RightInput] = {LeftTarget, RightTarget}`
@@ -48,6 +51,114 @@ func init() {
 			convert: true,
 			left:    r[2],
 			right:   r[3],
+		}
+	}
+
+	all := []types.T{
+		types.T_any,
+		types.T_bool,
+		types.T_int8, types.T_int16, types.T_int32, types.T_int64,
+		types.T_uint8, types.T_uint16, types.T_uint32, types.T_uint64,
+		types.T_float32, types.T_float64,
+		types.T_date, types.T_datetime, types.T_timestamp,
+		types.T_char, types.T_varchar,
+		types.T_decimal64, types.T_decimal128,
+	}
+
+	numbers := []types.T{ // numbers without decimal
+		types.T_int8, types.T_int16, types.T_int32, types.T_int64,
+		types.T_uint8, types.T_uint16, types.T_uint32, types.T_uint64,
+	}
+
+	floats := []types.T{types.T_float32, types.T_float64}
+	strings := []types.T{types.T_char, types.T_varchar}
+
+	maxTypes := 255
+	castable = make([][]bool, maxTypes)
+	{ // bool
+		castable[types.T_bool] = make([]bool, maxTypes)
+		castable[types.T_bool][types.T_bool] = true
+		for _, typ := range strings {
+			castable[types.T_bool][typ] = true
+		}
+	}
+	{ // date
+		castable[types.T_date] = make([]bool, maxTypes)
+		castable[types.T_date][types.T_date] = true
+		castable[types.T_date][types.T_timestamp] = true
+		castable[types.T_date][types.T_datetime] = true
+		for _, typ := range strings {
+			castable[types.T_date][typ] = true
+		}
+	}
+	{ // datetime
+		castable[types.T_datetime] = make([]bool, maxTypes)
+		castable[types.T_datetime][types.T_datetime] = true
+		castable[types.T_datetime][types.T_date] = true
+		castable[types.T_datetime][types.T_timestamp] = true
+		for _, typ := range strings {
+			castable[types.T_datetime][typ] = true
+		}
+	}
+	{ //  float
+		for _, t := range floats {
+			castable[t] = make([]bool, maxTypes)
+			for _, typ := range floats {
+				castable[t][typ] = true
+			}
+			for _, typ := range numbers {
+				castable[t][typ] = true
+			}
+			for _, typ := range strings {
+				castable[t][typ] = true
+			}
+		}
+	}
+	{ //  number
+		for _, t := range numbers {
+			castable[t] = make([]bool, maxTypes)
+			castable[t][t] = true
+			for _, typ := range floats {
+				castable[t][typ] = true
+			}
+			for _, typ := range numbers {
+				castable[t][typ] = true
+			}
+			castable[t][types.T_timestamp] = true
+			castable[t][types.T_decimal64] = true
+			castable[t][types.T_decimal128] = true
+			for _, typ := range strings {
+				castable[t][typ] = true
+			}
+		}
+		castable[types.T_decimal64] = make([]bool, maxTypes)
+		castable[types.T_decimal64][types.T_decimal64] = true
+		castable[types.T_decimal64][types.T_timestamp] = true
+		for _, typ := range strings {
+			castable[types.T_decimal64][typ] = true
+		}
+		castable[types.T_decimal128] = make([]bool, maxTypes)
+		castable[types.T_decimal128][types.T_decimal128] = true
+		castable[types.T_decimal128][types.T_timestamp] = true
+		for _, typ := range strings {
+			castable[types.T_decimal128][typ] = true
+		}
+	}
+	{ // timestamp
+		castable[types.T_timestamp] = make([]bool, maxTypes)
+		castable[types.T_timestamp][types.T_timestamp] = true
+		castable[types.T_timestamp][types.T_date] = true
+		castable[types.T_timestamp][types.T_datetime] = true
+		for _, typ := range strings {
+			castable[types.T_timestamp][typ] = true
+		}
+	}
+	{ // string
+		for _, t := range strings {
+			castable[t] = make([]bool, maxTypes)
+			for _, typ := range all {
+				castable[t][typ] = true
+			}
 		}
 	}
 }
