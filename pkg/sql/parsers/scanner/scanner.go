d97dcc6b437b8c0817ade4b834cf055bab4b80e4
aubin <1030420200@qq.com>
2022-06-27 15:09:11 +0800

    Fix #3156 but need to solve #3408 to get right result

--- pkg/sql/parsers/scanner/scanner.go
+++ pkg/sql/parsers/scanner/scanner.go
@@ -510,6 +510,11 @@ func (s *Scanner) scanNumber() (int, string) {
 			s.skip(1)
 			s.scanMantissa(16)
 			goto exit
+		} else if s.cur() == 'b' || s.cur() == 'B' {
+			token = BIT_LITERAL
+			s.skip(1)
+			s.scanMantissa(2)
+			goto exit
 		}
 	}
 
@@ -593,7 +598,7 @@ func (s *Scanner) scanHex() (int, string) {
 	if len(hex)%2 != 0 {
 		return LEX_ERROR, hex
 	}
-	return HEX, hex
+	return HEXNUM, hex
 }
 
 func (s *Scanner) scanMantissa(base int) {
