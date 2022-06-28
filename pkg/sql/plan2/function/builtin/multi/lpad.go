a71ecc0ec6f29abed493a4029f96bd8195a776a8
daviszhen <daviszhen007@gmail.com>
2022-06-28 11:15:00 +0800

    update

--- pkg/sql/plan2/function/builtin/multi/lpad.go
+++ pkg/sql/plan2/function/builtin/multi/lpad.go
@@ -19,7 +19,6 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/container/nulls"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
-	"github.com/matrixorigin/matrixone/pkg/vectorize/lpad"
 	"github.com/matrixorigin/matrixone/pkg/vm/process"
 )
 
@@ -31,45 +30,86 @@ func Lpad(vecs []*vector.Vector, proc *process.Process) (*vector.Vector, error) 
 	}
 	vs := vector.MustBytesCols(vecs[0]) //Get the first arg
 	if !vecs[1].IsScalar() && vecs[1].Typ.Oid != types.T_int64 {
-		return nil, errors.New("The second argument of the lpad function must be an int64 constant")
+		return nil, errors.New("The second argument of the " +
+			"lpad function must be an int64 constant")
 	}
 	if !vecs[2].IsScalar() && vecs[2].Typ.Oid != types.T_varchar {
-		return nil, errors.New("The third argument of the lpad function must be an string constant")
+		return nil, errors.New("The third argument" +
+			" of the lpad function must be an string constant")
 	}
 
-	length := vector.MustTCols[int64](vecs[1])
+	if vecs[0].IsScalar() && vecs[1].IsScalar() && vecs[2].IsScalar() {
+
+		return nil, nil
+	}
+
+	//characters length not bytes length
+	lengths := vector.MustTCols[int64](vecs[1])
+
+	//pad string
 	padds := vector.MustBytesCols(vecs[2])
 
-	if vecs[0].IsScalar() {
-		if length[0] < 0 || length[0] > int64(UINT16_MAX) {
-			return proc.AllocScalarNullVector(types.Type{Oid: types.T_varchar, Size: 24}), nil
+	//TODO : consider constant input
+
+	results := &types.Bytes{}
+	resultNUll := new(nulls.Nulls)
+
+	//evaluate bytes space for every long
+	rowCount := vector.Length(vecs[0])
+	for i := 0; i < rowCount; i++ {
+		//1.count characters in source and pad
+		var source []byte
+		if vecs[0].IsScalar() {
+			source = vs.Get(int64(0))
 		} else {
-			resultVec := proc.AllocScalarVector(types.Type{Oid: types.T_varchar, Size: 24})
-			results := &types.Bytes{
-				Data:    make([]byte, length[0]),
-				Offsets: make([]uint32, 1),
-				Lengths: make([]uint32, 1),
-			}
-			nulls.Set(resultVec.Nsp, vecs[0].Nsp)
-			vector.SetCol(resultVec, lpad.Lpad(results, vs, uint32(length[0]), padds))
-			return resultVec, nil
+			source = vs.Get(int64(i))
 		}
-	} else {
-		if length[0] < 0 || length[0] > int64(UINT16_MAX) {
-			return proc.AllocScalarNullVector(types.Type{Oid: types.T_varchar, Size: 24}), nil
-		} else {
-			resultVec, err := proc.AllocVector(types.Type{Oid: types.T_varchar, Size: 24}, int64(len(vs.Lengths))*length[0])
-			if err != nil {
-				return nil, err
+		sourceLen := len(source)
+		sourceRune := []rune(string(source))
+		sourceCharCnt := int64(len(sourceRune))
+		pad := padds.Get(int64(i))
+		padLen := len(pad)
+		padRune := []rune(string(pad))
+		padCharCnt := int64(len(padRune))
+		targetLength := lengths[i]
+		if targetLength < 0 {
+			//NULL
+			nulls.Add(resultNUll, uint64(i))
+			results.AppendOnce([]byte{})
+		} else if targetLength == 0 {
+			//empty string
+			results.AppendOnce([]byte{})
+		} else if targetLength < sourceCharCnt {
+			//shorten source string
+			sourcePart := sourceRune[:targetLength]
+			results.AppendOnce([]byte(string(sourcePart)))
+		} else if targetLength >= sourceCharCnt {
+			//calculate the space count
+			r := targetLength - sourceCharCnt
+			//complete pad count
+			p := r / padCharCnt
+			//partial pad count
+			m := r % padCharCnt
+			padSpaceCount := p*int64(padLen) + int64(len(string(padRune[:m]))) + int64(sourceLen)
+			padSpace := make([]byte, padSpaceCount)
+			// fill p * padStr
+			for j := int64(0); j < p; j++ {
+				copy(padSpace[j*int64(padLen):], pad)
 			}
-			results := &types.Bytes{
-				Data:    resultVec.Data,
-				Offsets: make([]uint32, len(vs.Offsets)),
-				Lengths: make([]uint32, len(vs.Lengths)),
-			}
-			nulls.Set(resultVec.Nsp, vecs[0].Nsp)
-			vector.SetCol(resultVec, lpad.Lpad(results, vs, uint32(length[0]), padds))
-			return resultVec, nil
+			// fill partial padStr
+			copy(padSpace[p*int64(padLen):], []byte(string(padRune[:m])))
+			// fill source
+			copy(padSpace[padSpaceCount-int64(sourceLen):], source)
+			results.AppendOnce(padSpace)
 		}
 	}
+
+	resultVec, err := proc.AllocVector(types.T_varchar.ToType(), 1)
+	if err != nil {
+		return nil, err
+	}
+
+	resultVec.Nsp = resultNUll
+	vector.SetCol(resultVec, results)
+	return resultVec, nil
 }
