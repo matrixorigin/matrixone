21d646f7ee0d9f831158588c3ff3c1f7a37b3a2e
daviszhen <daviszhen007@gmail.com>
2022-06-28 18:55:27 +0800

    update lpad

--- pkg/sql/plan2/function/builtin/multi/lpad_test.go
+++ pkg/sql/plan2/function/builtin/multi/lpad_test.go
@@ -33,57 +33,135 @@ func TestLpadVarchar(t *testing.T) {
 	}{
 		{
 			name:      "TEST01",
-			vecs:      makeLpadVectors("hello", 1, "#"),
+			vecs:      makeLpadVectors("hello", 1, "#", []int{1, 1, 1}),
 			proc:      process.New(mheap.New(nil)),
 			wantBytes: []byte("h"),
 		},
 		{
 			name:      "TEST02",
-			vecs:      makeLpadVectors("hello", 10, "#"),
+			vecs:      makeLpadVectors("hello", 10, "#", []int{1, 1, 1}),
 			proc:      process.New(mheap.New(nil)),
 			wantBytes: []byte("#####hello"),
 		},
 		{
 			name:      "TEST03",
-			vecs:      makeLpadVectors("hello", 15, "#@&"),
+			vecs:      makeLpadVectors("hello", 15, "#@&", []int{1, 1, 1}),
 			proc:      process.New(mheap.New(nil)),
 			wantBytes: []byte("#@&#@&#@&#hello"),
 		},
 		{
 			name:      "TEST04",
-			vecs:      makeLpadVectors("12345678", 10, "abcdefgh"),
+			vecs:      makeLpadVectors("12345678", 10, "abcdefgh", []int{1, 1, 1}),
 			proc:      process.New(mheap.New(nil)),
 			wantBytes: []byte("ab12345678"),
 		},
 		{
 			name:      "TEST05",
-			vecs:      makeLpadVectors("hello", 0, "#@&"),
+			vecs:      makeLpadVectors("hello", 0, "#@&", []int{1, 1, 1}),
 			proc:      process.New(mheap.New(nil)),
 			wantBytes: []byte(""),
 		},
 		{
 			name:      "TEST06",
-			vecs:      makeLpadVectors("hello", -1, "#@&"),
+			vecs:      makeLpadVectors("hello", -1, "#@&", []int{1, 1, 1}),
 			proc:      process.New(mheap.New(nil)),
 			wantBytes: []byte(nil),
 		},
 		{
 			name:      "Tx",
-			vecs:      makeLpadVectors("hello", 1, ""),
+			vecs:      makeLpadVectors("hello", 1, "", []int{1, 1, 1}),
 			proc:      process.New(mheap.New(nil)),
 			wantBytes: []byte(""),
 		},
 		{
 			name:      "Tx2",
-			vecs:      makeLpadVectors("", 5, "x"),
+			vecs:      makeLpadVectors("", 5, "x", []int{1, 1, 1}),
 			proc:      process.New(mheap.New(nil)),
 			wantBytes: []byte("xxxxx"),
 		},
 		{
 			name:      "Tx3",
-			vecs:      makeLpadVectors("你好", 10, "再见"),
+			vecs:      makeLpadVectors("你好", 10, "再见", []int{1, 1, 1}),
 			proc:      process.New(mheap.New(nil)),
 			wantBytes: []byte("再见再见再见再见你好"),
+		},
+		{
+			name:      "tx4",
+			vecs:      makeLpadVectors("hello", -1, "#@&", []int{0, 0, 0}),
+			proc:      process.New(mheap.New(nil)),
+			wantBytes: []byte(nil),
+		},
+		{
+			name:      "tx5",
+			vecs:      makeLpadVectors("hello", -1, "#@&", []int{0, 0, 1}),
+			proc:      process.New(mheap.New(nil)),
+			wantBytes: []byte(nil),
+		},
+		{
+			name:      "tx6",
+			vecs:      makeLpadVectors("hello", -1, "#@&", []int{0, 1, 0}),
+			proc:      process.New(mheap.New(nil)),
+			wantBytes: []byte(nil),
+		},
+		{
+			name:      "tx6",
+			vecs:      makeLpadVectors("hello", -1, "#@&", []int{1, 0, 0}),
+			proc:      process.New(mheap.New(nil)),
+			wantBytes: []byte(nil),
+		},
+		{
+			name:      "tx6",
+			vecs:      makeLpadVectors("hello", -1, "#@&", []int{1, 1, 0}),
+			proc:      process.New(mheap.New(nil)),
+			wantBytes: []byte(nil),
+		},
+		{
+			name:      "tx6",
+			vecs:      makeLpadVectors("hello", -1, "#@&", []int{1, 0, 1}),
+			proc:      process.New(mheap.New(nil)),
+			wantBytes: []byte(nil),
+		},
+		{
+			name:      "tx6",
+			vecs:      makeLpadVectors("hello", -1, "#@&", []int{0, 1, 1}),
+			proc:      process.New(mheap.New(nil)),
+			wantBytes: []byte(nil),
+		},
+		{
+			name:      "tx6",
+			vecs:      makeLpadVectors("hello", -1, "#@&", []int{1, 1, 1}),
+			proc:      process.New(mheap.New(nil)),
+			wantBytes: []byte(nil),
+		},
+		{
+			name:      "tx6",
+			vecs:      makeLpadVectors("a你", 15, "见", []int{1, 1, 1}),
+			proc:      process.New(mheap.New(nil)),
+			wantBytes: []byte("见见见见见见见见见见见见见a你"),
+		},
+		{
+			name:      "tx6",
+			vecs:      makeLpadVectors("a你a", 15, "见a", []int{1, 1, 1}),
+			proc:      process.New(mheap.New(nil)),
+			wantBytes: []byte("见a见a见a见a见a见aa你a"),
+		},
+		{
+			name:      "tx6",
+			vecs:      makeLpadVectors("a你aa", 15, "见aa", []int{1, 1, 1}),
+			proc:      process.New(mheap.New(nil)),
+			wantBytes: []byte("见aa见aa见aa见aa你aa"),
+		},
+		{
+			name:      "tx6",
+			vecs:      makeLpadVectors("a你aaa", 15, "见aaa", []int{1, 1, 1}),
+			proc:      process.New(mheap.New(nil)),
+			wantBytes: []byte("见aaa见aaa见aa你aaa"),
+		},
+		{
+			name:      "tx6",
+			vecs:      makeLpadVectors("a你aaaa", 15, "见aaaa", []int{1, 1, 1}),
+			proc:      process.New(mheap.New(nil)),
+			wantBytes: []byte("见aaaa见aaaa你aaaa"),
 		},
 	}
 
@@ -105,9 +183,8 @@ func TestLpadVarchar(t *testing.T) {
 
 }
 
-func makeLpadVectors(src string, length int64, pad string) []*vector.Vector {
+func makeLpadVectors(src string, length int64, pad string, nils []int) []*vector.Vector {
 	vec := make([]*vector.Vector, 3)
-
 	srcBytes := &types.Bytes{
 		Data:    []byte(src),
 		Offsets: []uint32{0},
@@ -127,10 +204,6 @@ func makeLpadVectors(src string, length int64, pad string) []*vector.Vector {
 		Length:  1,
 	}
 
-	//if src == nil {
-	//	nulls.Add(vec[0].Nsp, 0)
-	//}
-
 	vec[1] = &vector.Vector{
 		Col:     []int64{length},
 		Nsp:     &nulls.Nulls{},
@@ -145,6 +218,11 @@ func makeLpadVectors(src string, length int64, pad string) []*vector.Vector {
 		Typ:     types.Type{Oid: types.T_varchar, Size: 24},
 		IsConst: true,
 		Length:  1,
+	}
+	for i, n := range nils {
+		if n == 0 {
+			nulls.Add(vec[i].Nsp, uint64(i))
+		}
 	}
 	return vec
 }
