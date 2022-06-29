4102be6a5cbb100ca13649fca06c2d634afc2d59
nnsgmsone <nnsmgsone@outlook.com>
2022-06-29 17:36:18 +0800

    Add fill default values for vector

--- pkg/sql/plan2/function/operators.go
+++ pkg/sql/plan2/function/operators.go
@@ -15,12 +15,13 @@
 package function
 
 import (
+	"math"
+
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
 	"github.com/matrixorigin/matrixone/pkg/pb/plan"
 	"github.com/matrixorigin/matrixone/pkg/sql/plan2/function/operator"
 	"github.com/matrixorigin/matrixone/pkg/vm/process"
-	"math"
 )
 
 func initOperators() {
