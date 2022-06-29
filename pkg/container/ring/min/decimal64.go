ae256aca04d17aaa7f8d49612d59dc3de67ebc60
nnsgmsone <nnsmgsone@outlook.com>
2022-06-29 21:06:34 +0800

    Handle all nulls in a vector (#3518)
    * Add fill default values for vector
    
    * add bvt case
    
    * add check of null vector
    
    * Fix the bug of min
    
    * remove some case

--- pkg/container/ring/min/decimal64.go
+++ pkg/container/ring/min/decimal64.go
@@ -146,46 +146,41 @@ func (r *Decimal64Ring) Grows(size int, m *mheap.Mheap) error {
 }
 
 func (r *Decimal64Ring) Fill(i int64, sel, z int64, vec *vector.Vector) {
+	if nulls.Contains(vec.Nsp, uint64(sel)) {
+		r.Ns[i] += z
+		return
+	}
 	if v := vec.Col.([]types.Decimal64)[sel]; r.Es[i] || v < r.Vs[i] {
 		r.Vs[i] = v
 		r.Es[i] = false
-	}
-	if nulls.Contains(vec.Nsp, uint64(sel)) {
-		r.Ns[i] += z
 	}
 }
 
 func (r *Decimal64Ring) BatchFill(start int64, os []uint8, vps []uint64, zs []int64, vec *vector.Vector) {
 	vs := vec.Col.([]types.Decimal64)
 	for i := range os {
+		if nulls.Contains(vec.Nsp, uint64(start)+uint64(i)) {
+			r.Ns[vps[i]-1] += zs[int64(i)+start]
+			continue
+		}
 		j := vps[i] - 1
 		if r.Es[j] || vs[int64(i)+start] < r.Vs[j] {
 			r.Vs[j] = vs[int64(i)+start]
 			r.Es[j] = false
 		}
 	}
-	if nulls.Any(vec.Nsp) {
-		for i := range os {
-			if nulls.Contains(vec.Nsp, uint64(start)+uint64(i)) {
-				r.Ns[vps[i]-1] += zs[int64(i)+start]
-			}
-		}
-	}
 }
 
 func (r *Decimal64Ring) BulkFill(i int64, zs []int64, vec *vector.Vector) {
 	vs := vec.Col.([]types.Decimal64)
-	for _, v := range vs {
+	for j, v := range vs {
+		if nulls.Contains(vec.Nsp, uint64(j)) {
+			r.Ns[i] += zs[j]
+			continue
+		}
 		if r.Es[i] || v < r.Vs[i] {
 			r.Vs[i] = v
 			r.Es[i] = false
-		}
-	}
-	if nulls.Any(vec.Nsp) {
-		for j := range vs {
-			if nulls.Contains(vec.Nsp, uint64(j)) {
-				r.Ns[i] += zs[j]
-			}
 		}
 	}
 }
