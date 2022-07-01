3baa026382fec99275e88e52065b61ea89761395
qingxinhome <758355272@qq.com>
2022-07-01 15:35:09 +0800

    Rebuild branch collation code

--- pkg/sql/compile2/compile.go
+++ pkg/sql/compile2/compile.go
@@ -295,12 +295,18 @@ func (c *Compile) compilePlanScope(n *plan.Node, ns []*plan.Node) ([]*Scope, err
 			return nil, err
 		}
 		src := &Source{
-			RelationName: n.TableDef.Name,
-			SchemaName:   n.ObjRef.SchemaName,
-			Attributes:   make([]string, len(n.TableDef.Cols)),
+			RelationName:   n.TableDef.Name,
+			SchemaName:     n.ObjRef.SchemaName,
+			Attributes:     make([]string, len(n.TableDef.Cols)),
+			AttributeTypes: make([]plan.ColDef, len(n.TableDef.Cols)),
 		}
 		for i, col := range n.TableDef.Cols {
-			src.Attributes[i] = col.Name
+			src.AttributeTypes[i] = *col
+			if col.IsPrune {
+				continue
+			} else {
+				src.Attributes[i] = col.Name
+			}
 		}
 		nodes := rel.Nodes(snap)
 		if len(nodes) == 0 {
