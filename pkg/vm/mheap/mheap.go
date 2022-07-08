e6e872b18d6753ff0c8f317c71985910744b6447
nnsgmsone <nnsmgsone@outlook.com>
2022-07-08 12:00:41 +0800

    Add new vector and batch (#3827)

--- pkg/vm/mheap/mheap.go
+++ pkg/vm/mheap/mheap.go
@@ -12,19 +12,19 @@ func New(gm *guest.Mmu) *Mheap {
 	}
 }
 
-func Size(m *Mheap) int64 {
+func (m *Mheap) Size() int64 {
 	return m.Gm.Size()
 }
 
-func HostSize(m *Mheap) int64 {
+func (m *Mheap) HostSize() int64 {
 	return m.Gm.HostSize()
 }
 
-func Free(m *Mheap, data []byte) {
+func (m *Mheap) Free(data []byte) {
 	m.Gm.Free(int64(cap(data)))
 }
 
-func Alloc(m *Mheap, size int64) ([]byte, error) {
+func (m *Mheap) Alloc(size int64) ([]byte, error) {
 	data := mempool.Alloc(m.Mp, int(size))
 	if err := m.Gm.Alloc(int64(cap(data))); err != nil {
 		return nil, err
@@ -32,8 +32,8 @@ func Alloc(m *Mheap, size int64) ([]byte, error) {
 	return data[:size], nil
 }
 
-func Grow(m *Mheap, old []byte, size int64) ([]byte, error) {
-	data, err := Alloc(m, mempool.Realloc(old, size))
+func (m *Mheap) Grow(old []byte, size int64) ([]byte, error) {
+	data, err := m.Alloc(mempool.Realloc(old, size))
 	if err != nil {
 		return nil, err
 	}
