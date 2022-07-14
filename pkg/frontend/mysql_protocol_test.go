39d20e78528198b9b86885c7288251f4afb195a9
daviszhen <daviszhen007@gmail.com>
2022-07-14 10:47:38 +0800

    fix static check

--- pkg/frontend/mysql_protocol_test.go
+++ pkg/frontend/mysql_protocol_test.go
@@ -2037,14 +2037,16 @@ func Test_handleHandshake_Recover(t *testing.T) {
 		var payload []byte
 		for i := 0; i < count; i++ {
 			f.Fuzz(&payload)
-			mp.handleHandshake(payload)
+			err := mp.handleHandshake(payload)
+			convey.So(err, convey.ShouldBeNil)
 			maxLen = Max(maxLen, len(payload))
 		}
 		maxLen = 0
 		var payload2 string
 		for i := 0; i < count; i++ {
 			f.Fuzz(&payload2)
-			mp.handleHandshake([]byte(payload2))
+			err := mp.handleHandshake([]byte(payload2))
+			convey.So(err, convey.ShouldBeNil)
 			maxLen = Max(maxLen, len(payload2))
 		}
 	})
