9b0ba4482a54ae596c02b5978e6887b36097c4d8
ouyuanning <52741323@qq.com>
2022-07-14 14:44:15 +0800

    typo

--- pkg/sql/plan/explain/init.go
+++ pkg/sql/plan/explain/init.go
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// explain explain statment
+// explain explain statement
 package explain
