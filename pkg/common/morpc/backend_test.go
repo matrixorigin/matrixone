818f6f366b69649d1c9b52c95f8be661cb4d7ee9
fagongzi <zhangxu19830126@gmail.com>
2022-07-20 14:01:22 +0800

    morpc: add auto close idle backend connections (#4116)

--- pkg/common/morpc/backend_test.go
+++ pkg/common/morpc/backend_test.go
@@ -37,7 +37,7 @@ var (
 
 func TestSend(t *testing.T) {
 	testBackendSend(t,
-		func(conn goetty.IOSession, msg interface{}, seq uint64) error {
+		func(conn goetty.IOSession, msg interface{}, _ uint64) error {
 			return conn.Write(msg, goetty.WriteOptions{Flush: true})
 		},
 		func(b *remoteBackend) {
@@ -281,9 +281,12 @@ func TestBusy(t *testing.T) {
 
 func TestDoneWithClosedStreamCannotPanic(t *testing.T) {
 	c := make(chan Message, 1)
-	s := newStream(c, func(m backendSendMessage) error {
-		return nil
-	}, func(s *stream) {})
+	s := newStream(c,
+		func(m backendSendMessage) error {
+			return nil
+		},
+		func(s *stream) {},
+		func() {})
 	s.init(1)
 	assert.NoError(t, s.Send(&testMessage{id: s.ID()}, SendOptions{}))
 	assert.NoError(t, s.Close())
@@ -294,14 +297,88 @@ func TestDoneWithClosedStreamCannotPanic(t *testing.T) {
 
 func TestGCStream(t *testing.T) {
 	c := make(chan Message, 1)
-	s := newStream(c, func(m backendSendMessage) error {
-		return nil
-	}, func(s *stream) {})
+	s := newStream(c,
+		func(m backendSendMessage) error {
+			return nil
+		},
+		func(s *stream) {},
+		func() {})
 	s.init(1)
 	s = nil
 	debug.FreeOSMemory()
 	_, ok := <-c
 	assert.False(t, ok)
+}
+
+func TestLastActiveWithNew(t *testing.T) {
+	testBackendSend(t,
+		func(conn goetty.IOSession, msg interface{}, seq uint64) error {
+			return nil
+		},
+		func(b *remoteBackend) {
+			assert.NotEqual(t, time.Time{}, b.LastActiveTime())
+		},
+		WithBackendConnectWhenCreate())
+}
+
+func TestLastActiveWithSend(t *testing.T) {
+	c := make(chan struct{})
+	testBackendSend(t,
+		func(conn goetty.IOSession, msg interface{}, _ uint64) error {
+			<-c
+			return conn.Write(msg, goetty.WriteOptions{Flush: true})
+		},
+		func(b *remoteBackend) {
+			t1 := b.LastActiveTime()
+
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+			defer cancel()
+			req := newTestMessage(1)
+			f, err := b.Send(ctx, req, SendOptions{})
+			assert.NoError(t, err)
+			defer f.Close()
+
+			t2 := b.LastActiveTime()
+			assert.NotEqual(t, t1, t2)
+			assert.True(t, t2.After(t1))
+			c <- struct{}{}
+
+			resp, err := f.Get()
+			assert.NoError(t, err)
+			assert.Equal(t, req, resp)
+
+			t3 := b.LastActiveTime()
+			assert.NotEqual(t, t2, t3)
+			assert.True(t, t3.After(t2))
+
+		},
+		WithBackendConnectWhenCreate())
+}
+
+func TestLastActiveWithStream(t *testing.T) {
+	testBackendSend(t,
+		func(conn goetty.IOSession, msg interface{}, seq uint64) error {
+			return conn.Write(msg, goetty.WriteOptions{Flush: true})
+		},
+		func(b *remoteBackend) {
+			t1 := b.LastActiveTime()
+
+			st, err := b.NewStream()
+			assert.NoError(t, err)
+			defer func() {
+				assert.NoError(t, st.Close())
+			}()
+
+			n := 1
+			for i := 0; i < n; i++ {
+				req := &testMessage{id: st.ID()}
+				assert.NoError(t, st.Send(req, SendOptions{}))
+				t2 := b.LastActiveTime()
+				assert.NotEqual(t, t1, t2)
+				assert.True(t, t2.After(t1))
+			}
+		},
+		WithBackendConnectWhenCreate())
 }
 
 func testBackendSend(t *testing.T,
@@ -358,24 +435,47 @@ func (bf *testBackendFactory) Create(backend string) (Backend, error) {
 }
 
 type testBackend struct {
-	id   int
-	busy bool
+	sync.RWMutex
+	id         int
+	busy       bool
+	activeTime time.Time
+	closed     bool
 }
 
 func (b *testBackend) Send(ctx context.Context, request Message, opts SendOptions) (*Future, error) {
+	b.active()
 	f := newFuture(nil)
 	f.init(request.GetID(), ctx)
 	return f, nil
 }
 
 func (b *testBackend) NewStream() (Stream, error) {
-	st := newStream(make(chan Message, 1), func(m backendSendMessage) error { return nil }, func(s *stream) {})
+	b.active()
+	st := newStream(make(chan Message, 1),
+		func(m backendSendMessage) error { return nil },
+		func(s *stream) {},
+		b.active)
 	st.init(1)
 	return st, nil
 }
 
-func (b *testBackend) Close()     {}
+func (b *testBackend) Close() {
+	b.Lock()
+	defer b.Unlock()
+	b.closed = true
+}
 func (b *testBackend) Busy() bool { return b.busy }
+func (b *testBackend) LastActiveTime() time.Time {
+	b.RLock()
+	defer b.RUnlock()
+	return b.activeTime
+}
+
+func (b *testBackend) active() {
+	b.Lock()
+	defer b.Unlock()
+	b.activeTime = time.Now()
+}
 
 type testMessage struct {
 	id      uint64
