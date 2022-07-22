cc598f608c21307dcd6a83dfacd4b72a9b81536e
ouyuanning <52741323@qq.com>
2022-07-22 12:22:08 +0800

    don't modify test file

--- pkg/sql/plan/build_test.go
+++ pkg/sql/plan/build_test.go
@@ -34,7 +34,7 @@ func TestSingleSQL(t *testing.T) {
 	// sql := "SELECT nation2.* FROM nation2 natural join region"
 	// sql := `select n_name, avg(N_REGIONKEY) t from NATION where n_name != 'a' group by n_name having avg(N_REGIONKEY) > 10 order by t limit 20`
 	// sql := `select date_add('1997-12-31 23:59:59',INTERVAL 100000 SECOND)`
-	sql := "select t1.a, (case t1.a when 0 then 0 else t1.b end) d from t1"
+	sql := "prepare stmt1 from 'select current_timestamp() + 1"
 	// sql := "explain a"
 	// sql := "select 18446744073709551500"
 	// stmts, err := mysql.Parse(sql)
