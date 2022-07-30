cb8be637a312e110f6f88950ccf62cdbe9aa62c8
GuokeCui <jingjinglake@gmail.com>
2022-07-30 10:45:00 +0800

    Make `TestCluster` runnable;

--- pkg/tests/service/service.go
+++ pkg/tests/service/service.go
@@ -92,6 +92,41 @@ type ClusterWaitState interface {
 	// WaitReplicaByNum(shardID uint64, batch int, timeout time.Duration)
 }
 
+// ----------------------------------------------------
+// The following are implements for interface `Cluster`.
+// ----------------------------------------------------
+
+// testCluster simulates a cluster with dn and log service.
+type testCluster struct {
+	t      *testing.T
+	opt    Options
+	logger *zap.Logger
+
+	dn struct {
+		sync.Mutex
+		cfgs []*dnservice.Config
+		opts []dnOptions
+		svcs []DNService
+	}
+
+	log struct {
+		once sync.Once
+
+		sync.Mutex
+		cfgs []logservice.Config
+		svcs []LogService
+	}
+
+	address serviceAddress
+
+	fileservices *fileServices
+
+	mu struct {
+		sync.Mutex
+		running bool
+	}
+}
+
 // NewCluster construct a cluster for integration test.
 func NewCluster(t *testing.T, opt Options) (Cluster, error) {
 	opt.validate()
@@ -104,139 +139,19 @@ func NewCluster(t *testing.T, opt Options) (Cluster, error) {
 		zap.String("tests", "service"),
 	)
 
+	// build addresses for all services
 	c.address = c.buildServiceAddress()
-	c.filesvcs = c.buildFileServices()
 
-	c.buildLogConfigs(c.address)
-	c.initLogServices()
+	// build FileService instances
+	c.fileservices = c.buildFileServices()
 
-	c.buildDnConfigs(c.address)
-	c.initDNServices(c.filesvcs)
+	// build log service configurations
+	c.log.cfgs = c.buildLogConfigs(c.address)
+
+	// build dn service configurations
+	c.dn.cfgs, c.dn.opts = c.buildDnConfigs(c.address)
 
 	return c, nil
-}
-
-// testCluster simulates a cluster with dn and log service.
-type testCluster struct {
-	t      *testing.T
-	opt    Options
-	once   sync.Once
-	logger *zap.Logger
-
-	dn struct {
-		sync.Mutex
-		cfgs []*dnservice.Config
-		opts []dnOptions
-		svcs []DNService
-	}
-
-	log struct {
-		sync.Mutex
-		cfgs []logservice.Config
-		svcs []LogService
-	}
-
-	address serviceAddress
-
-	filesvcs *fileServices
-
-	mu struct {
-		sync.Mutex
-		running bool
-	}
-}
-
-// buildServiceAddress builds addresses for all services.
-func (c *testCluster) buildServiceAddress() serviceAddress {
-	return newServiceAddress(
-		c.t,
-		c.opt.initial.logServiceNum,
-		c.opt.initial.dnServiceNum,
-		c.opt.hostAddr,
-	)
-}
-
-// buildFileServices builds all file services.
-func (c *testCluster) buildFileServices() *fileServices {
-	return newFileServices(c.t, c.opt.initial.dnServiceNum)
-}
-
-// buildDnConfigs builds configurations for all dn services.
-func (c *testCluster) buildDnConfigs(address serviceAddress) {
-	batch := c.opt.initial.dnServiceNum
-
-	cfgs := make([]*dnservice.Config, 0, batch)
-	opts := make([]dnOptions, 0, batch)
-	for i := 0; i < batch; i++ {
-		cfg, opt := buildDnConfig(i, c.opt, address)
-		cfgs = append(cfgs, cfg)
-		opts = append(opts, opt)
-	}
-
-	c.dn.cfgs = cfgs
-	c.dn.opts = opts
-}
-
-// buildLogConfigs builds configurations for all log services.
-func (c *testCluster) buildLogConfigs(address serviceAddress) {
-	batch := c.opt.initial.logServiceNum
-
-	cfgs := make([]logservice.Config, 0, batch)
-	for i := 0; i < batch; i++ {
-		cfg := buildLogConfig(i, c.opt, address)
-		cfgs = append(cfgs, cfg)
-	}
-
-	c.log.cfgs = cfgs
-}
-
-// initDNServices builds all dn services.
-func (c *testCluster) initDNServices(filesvcs *fileServices) {
-	batch := c.opt.initial.dnServiceNum
-
-	c.logger.Info("initialize dn services", zap.Int("batch", batch))
-
-	svcs := make([]DNService, 0, batch)
-	for i := 0; i < batch; i++ {
-		cfg := c.dn.cfgs[i]
-		opt := c.dn.opts[i]
-		fsFactory := func(name string) (fileservice.FileService, error) {
-			switch strings.ToUpper(name) {
-			case "LOCAL":
-				return filesvcs.getLocalFileService(i), nil
-			case "MEM":
-				return filesvcs.getS3FileService(), nil
-			default:
-				return nil, ErrInvalidFSName
-			}
-		}
-
-		ds, err := newDNService(cfg, fsFactory, opt)
-		require.NoError(c.t, err)
-
-		svcs = append(svcs, ds)
-
-		c.logger.Info("dn service initialized", zap.Int("index", i), zap.Any("config", cfg))
-	}
-	c.dn.svcs = svcs
-}
-
-// initLogServices builds all log services.
-func (c *testCluster) initLogServices() {
-	batch := c.opt.initial.logServiceNum
-
-	c.logger.Info("initialize log services", zap.Int("batch", batch))
-
-	svcs := make([]LogService, 0, batch)
-	for i := 0; i < batch; i++ {
-		cfg := c.log.cfgs[i]
-		ls, err := newLogService(cfg)
-		require.NoError(c.t, err)
-		svcs = append(svcs, ls)
-
-		c.logger.Info("log service initialized", zap.Int("index", i), zap.Any("config", cfg))
-	}
-	c.log.svcs = svcs
 }
 
 func (c *testCluster) Start() error {
@@ -247,28 +162,14 @@ func (c *testCluster) Start() error {
 		return nil
 	}
 
-	// start log service
-	for _, ls := range c.log.svcs {
-		if err := ls.Start(); err != nil {
-			return err
-		}
-	}
-
-	// start hakeeper replica
-	if err := c.startHAKeeperReplica(); err != nil {
+	// start log services first
+	if err := c.startLogServices(); err != nil {
 		return err
 	}
 
-	// initialize cluster information
-	if err := c.setInitialClusterInfo(); err != nil {
+	// start dn services
+	if err := c.startDNServices(); err != nil {
 		return err
-	}
-
-	// start dn service
-	for _, ds := range c.dn.svcs {
-		if err := ds.Start(); err != nil {
-			return err
-		}
 	}
 
 	c.mu.running = true
@@ -283,19 +184,14 @@ func (c *testCluster) Close() error {
 		return nil
 	}
 
-	for _, ds := range c.dn.svcs {
-		if err := ds.Close(); err != nil {
-			return err
-		}
+	// close all dn services first
+	if err := c.closeDNServices(); err != nil {
+		return err
 	}
 
-	c.logger.Info("close log services in the cluster")
-	for i, ls := range c.log.svcs {
-		c.logger.Info("close log service", zap.Int("index", i))
-		if err := ls.Close(); err != nil {
-			return err
-		}
-		c.logger.Info("log service closed", zap.Int("index", i))
+	// close all log services
+	if err := c.closeLogServices(); err != nil {
+		return err
 	}
 
 	c.mu.running = false
@@ -356,23 +252,14 @@ func (c *testCluster) WaitHAKeeperLeader(timeout time.Duration) LogService {
 	}
 }
 
-func (c *testCluster) getHAKeeperLeader() LogService {
-	var leader LogService
-	for _, svc := range c.selectHAkeeperServices() {
-		if svc.IsLeaderHakeeper() {
-			return leader
-		}
-	}
-	return nil
-}
-
 func (c *testCluster) WaitHAKeeperState(timeout time.Duration, expected logpb.HAKeeperState) {
 	timeoutCh := time.After(timeout)
 	for {
 		select {
 		case <-timeoutCh:
-			c.logger.Warn("timeout when waiting for hakeeper running")
-			assert.FailNow(c.t, "timeout when wait for hakeeper running")
+			msg := "timeout when waiting for hakeeper running"
+			c.logger.Warn(msg)
+			assert.FailNow(c.t, msg)
 		default:
 			time.Sleep(100 * time.Millisecond)
 
@@ -404,4 +291,183 @@ func (c *testCluster) ListLogServices() []string {
 		ids = append(ids, svc.ID())
 	}
 	return ids
+}
+
+// ------------------------------------------------------
+// The following are private utilities for `testCluster`.
+// ------------------------------------------------------
+
+// buildServiceAddress builds addresses for all services.
+func (c *testCluster) buildServiceAddress() serviceAddress {
+	return newServiceAddress(
+		c.t,
+		c.opt.initial.logServiceNum,
+		c.opt.initial.dnServiceNum,
+		c.opt.hostAddr,
+	)
+}
+
+// buildFileServices builds all file services.
+func (c *testCluster) buildFileServices() *fileServices {
+	return newFileServices(c.t, c.opt.initial.dnServiceNum)
+}
+
+// buildDnConfigs builds configurations for all dn services.
+func (c *testCluster) buildDnConfigs(address serviceAddress) ([]*dnservice.Config, []dnOptions) {
+	batch := c.opt.initial.dnServiceNum
+
+	cfgs := make([]*dnservice.Config, 0, batch)
+	opts := make([]dnOptions, 0, batch)
+	for i := 0; i < batch; i++ {
+		cfg, opt := buildDnConfig(i, c.opt, address)
+		cfgs = append(cfgs, cfg)
+		opts = append(opts, opt)
+	}
+	return cfgs, opts
+}
+
+// buildLogConfigs builds configurations for all log services.
+func (c *testCluster) buildLogConfigs(address serviceAddress) []logservice.Config {
+	batch := c.opt.initial.logServiceNum
+
+	cfgs := make([]logservice.Config, 0, batch)
+	for i := 0; i < batch; i++ {
+		cfg := buildLogConfig(i, c.opt, address)
+		cfgs = append(cfgs, cfg)
+	}
+	return cfgs
+}
+
+// initDNServices builds all dn services.
+//
+// Before initializing dn service, log service must be started already.
+func (c *testCluster) initDNServices(fileservices *fileServices) []DNService {
+	batch := c.opt.initial.dnServiceNum
+
+	c.logger.Info("initialize dn services", zap.Int("batch", batch))
+
+	svcs := make([]DNService, 0, batch)
+	for i := 0; i < batch; i++ {
+		cfg := c.dn.cfgs[i]
+		opt := c.dn.opts[i]
+		fsFactory := func(name string) (fileservice.FileService, error) {
+			switch strings.ToUpper(name) {
+			case "LOCAL":
+				return fileservices.getLocalFileService(i), nil
+			case "S3":
+				return fileservices.getS3FileService(), nil
+			default:
+				return nil, ErrInvalidFSName
+			}
+		}
+
+		ds, err := newDNService(cfg, fsFactory, opt)
+		require.NoError(c.t, err)
+
+		c.logger.Info("dn service initialized", zap.Int("index", i), zap.Any("config", cfg))
+
+		svcs = append(svcs, ds)
+	}
+
+	return svcs
+}
+
+// initLogServices builds all log services.
+func (c *testCluster) initLogServices() []LogService {
+	batch := c.opt.initial.logServiceNum
+
+	c.logger.Info("initialize log services", zap.Int("batch", batch))
+
+	svcs := make([]LogService, 0, batch)
+	for i := 0; i < batch; i++ {
+		cfg := c.log.cfgs[i]
+		ls, err := newLogService(cfg)
+		require.NoError(c.t, err)
+
+		c.logger.Info("log service initialized", zap.Int("index", i), zap.Any("config", cfg))
+
+		svcs = append(svcs, ls)
+	}
+	return svcs
+}
+
+// startDNServices initializes and starts all dn services.
+func (c *testCluster) startDNServices() error {
+	// initialize all dn services
+	c.dn.svcs = c.initDNServices(c.fileservices)
+
+	// start dn services
+	for _, ds := range c.dn.svcs {
+		if err := ds.Start(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// startLogServices initializes and starts all log services.
+func (c *testCluster) startLogServices() error {
+	// initialize all log service
+	c.log.svcs = c.initLogServices()
+
+	// start log services
+	for _, ls := range c.log.svcs {
+		if err := ls.Start(); err != nil {
+			return err
+		}
+	}
+
+	// start hakeeper replicas
+	if err := c.startHAKeeperReplica(); err != nil {
+		return err
+	}
+
+	// initialize cluster information
+	if err := c.setInitialClusterInfo(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// closeDNServices closes all dn services.
+func (c *testCluster) closeDNServices() error {
+	c.logger.Info("start to close dn services")
+
+	for i, ds := range c.dn.svcs {
+		c.logger.Info("close dn service", zap.Int("index", i))
+		if err := ds.Close(); err != nil {
+			return err
+		}
+		c.logger.Info("dn service closed", zap.Int("index", i))
+	}
+
+	return nil
+}
+
+// closeLogServices closes all log services.
+func (c *testCluster) closeLogServices() error {
+	c.logger.Info("start to close log services")
+
+	for i, ls := range c.log.svcs {
+		c.logger.Info("close log service", zap.Int("index", i))
+		if err := ls.Close(); err != nil {
+			return err
+		}
+		c.logger.Info("log service closed", zap.Int("index", i))
+	}
+
+	return nil
+}
+
+// getHAKeeperLeader gets log service which is hakeeper leader.
+func (c *testCluster) getHAKeeperLeader() LogService {
+	var leader LogService
+	for _, svc := range c.selectHAkeeperServices() {
+		if svc.IsLeaderHakeeper() {
+			return leader
+		}
+	}
+	return nil
 }
