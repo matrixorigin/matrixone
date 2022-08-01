6c7b27a31305f4722ec64e71fe6ab85420de9e46
Wei Ziran <weiziran125@gmail.com>
2022-08-01 14:22:27 +0800

    fix TestOpExpiredAndThenCompleted error

--- pkg/hakeeper/checkers/coordinator_test.go
+++ pkg/hakeeper/checkers/coordinator_test.go
@@ -17,6 +17,7 @@ package checkers
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/matrixorigin/matrixone/pkg/hakeeper"
 	"github.com/matrixorigin/matrixone/pkg/hakeeper/checkers/util"
@@ -387,27 +388,28 @@ func TestOpExpiredAndThenCompleted(t *testing.T) {
 	cluster := pb.ClusterInfo{LogShards: []metadata.LogShardRecord{{ShardID: 1, NumberOfReplicas: 3}}}
 	idAlloc := util.NewTestIDAllocator(2)
 	coordinator := NewCoordinator(hakeeper.Config{})
-	fn := func(time uint64) uint64 { return time * hakeeper.DefaultTickPerSecond * 60 }
+	fn := func(time uint64) uint64 { return time * hakeeper.DefaultTickPerSecond }
+	currentTick := fn(uint64(hakeeper.DefaultLogStoreTimeout / time.Second))
 
 	replicas := map[uint64]string{1: "a", 2: "b"}
 	logShardInfo := pb.LogShardInfo{ShardID: 1, Replicas: replicas, Epoch: 2, LeaderID: 1}
 	logState := pb.LogState{
 		Shards: map[uint64]pb.LogShardInfo{1: {ShardID: 1, Replicas: replicas, Epoch: 1, LeaderID: 1}},
 		Stores: map[string]pb.LogStoreInfo{
-			"a": {Tick: fn(12), Replicas: []pb.LogReplicaInfo{{LogShardInfo: logShardInfo, ReplicaID: 1}}},
-			"b": {Tick: fn(13), Replicas: []pb.LogReplicaInfo{{LogShardInfo: logShardInfo, ReplicaID: 2}}},
-			"c": {Tick: fn(14) * hakeeper.DefaultTickPerSecond * 60},
+			"a": {Tick: 0, Replicas: []pb.LogReplicaInfo{{LogShardInfo: logShardInfo, ReplicaID: 1}}},
+			"b": {Tick: 0, Replicas: []pb.LogReplicaInfo{{LogShardInfo: logShardInfo, ReplicaID: 2}}},
+			"c": {Tick: 1},
 		},
 	}
 
-	assert.NotNil(t, coordinator.Check(idAlloc, cluster, pb.DNState{}, logState, fn(15)))
-	assert.Nil(t, coordinator.Check(idAlloc, cluster, pb.DNState{}, logState, fn(16)))
+	assert.NotNil(t, coordinator.Check(idAlloc, cluster, pb.DNState{}, logState, currentTick))
+	assert.Nil(t, coordinator.Check(idAlloc, cluster, pb.DNState{}, logState, currentTick))
 
 	ops := coordinator.OperatorController.GetOperators(1)
 	assert.Equal(t, 1, len(ops))
 	ops[0].SetStatus(operator.EXPIRED)
 
-	assert.NotNil(t, coordinator.Check(idAlloc, cluster, pb.DNState{}, logState, fn(17)))
+	assert.NotNil(t, coordinator.Check(idAlloc, cluster, pb.DNState{}, logState, currentTick))
 	ops = coordinator.OperatorController.GetOperators(1)
 	assert.Equal(t, 1, len(ops))
 
@@ -416,11 +418,11 @@ func TestOpExpiredAndThenCompleted(t *testing.T) {
 	logState = pb.LogState{
 		Shards: map[uint64]pb.LogShardInfo{1: {ShardID: 1, Replicas: replicas, Epoch: 1, LeaderID: 1}},
 		Stores: map[string]pb.LogStoreInfo{
-			"a": {Tick: fn(16), Replicas: []pb.LogReplicaInfo{{LogShardInfo: logShardInfo, ReplicaID: 1}}},
-			"b": {Tick: fn(17), Replicas: []pb.LogReplicaInfo{{LogShardInfo: logShardInfo, ReplicaID: 2}}},
-			"c": {Tick: fn(14), Replicas: []pb.LogReplicaInfo{{LogShardInfo: logShardInfo, ReplicaID: 4}}},
+			"a": {Tick: 0, Replicas: []pb.LogReplicaInfo{{LogShardInfo: logShardInfo, ReplicaID: 1}}},
+			"b": {Tick: 0, Replicas: []pb.LogReplicaInfo{{LogShardInfo: logShardInfo, ReplicaID: 2}}},
+			"c": {Tick: 0, Replicas: []pb.LogReplicaInfo{{LogShardInfo: logShardInfo, ReplicaID: 4}}},
 		},
 	}
 
-	assert.Nil(t, coordinator.Check(idAlloc, cluster, pb.DNState{}, logState, fn(18)))
+	assert.Nil(t, coordinator.Check(idAlloc, cluster, pb.DNState{}, logState, currentTick))
 }
