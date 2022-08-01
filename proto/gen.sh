1065965be3f3cbcdbbe0f6c71fa100fe3d55a4eb
maomao <77312370+sukki37@users.noreply.github.com>
2022-08-01 10:58:06 +0800

    Update Golangci-lint & `make pb` (#4298)
    - Upgrade Golangci-lint version
    - Refactor golangci-lint configurations and use three linters(gosimple,stylecheck,staticcheck) of golangci-lint to replace staticcheck
    - Optimize proto/gen.sh, add steps to install protoc and gogoprotobuf

--- proto/gen.sh
+++ proto/gen.sh
@@ -3,15 +3,107 @@
 # Generate all matrixcube protobuf bindings.
 # Run from repository root.
 #
-set -e
+set -ex
+
+
+program_exists() {
+  if command -v "${1}" > /dev/null 2>&1; then
+    echo "ok"
+  else
+    echo "fail"
+  fi
+}
 
 VENDOR_DIR="$PWD/vendor"
 PB_DIR="$PWD/pkg/pb"
 PROTOC_DIR="$PWD/proto"
+PROTO_SYNTAX_VERSION='3'
+PROTOC_VERSION='21.1'
+GOGOPROTOBUF_VERSION='1.'
+
+
+res=$(program_exists goimports)
+echo "res: ${res}"
+if [ "${res}" == "ok" ];then
+  echo "goimports exits"
+else
+  echo "install goimports"
+  go install golang.org/x/tools/cmd/goimports@latest
+fi
+
+res=$(program_exists protoc)
+echo "res: ${res}"
+if [ "${res}" == "ok" ];then
+  echo "protoc exits"
+  version=$(protoc --version)
+  if [[ "${version}" == *"${PROTO_SYNTAX_VERSION}.${PROTOC_VERSION}"* ]];then
+    echo "protoc version matches ${PROTOC_VERSION}"
+  else
+    echo "protoc version does not match"
+  fi
+  if [ -f ${GOPATH}/bin/protoc ]; then
+    ${GOPATH}/bin/protoc --version
+  else
+    ln -s $(which protoc) ${GOPATH}/bin/protoc
+    ${GOPATH}/bin/protoc --version
+  fi
+
+else
+  echo "install protoc"
+  if [ "$(uname)" == "Darwin" ];then
+    OS='osx'
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ];then
+    OS='linux'
+  else
+    echo "unsupported os, failed"
+    exit 1
+  fi
+  if [[ "$(uname -m)" == *"x86"* ]];then
+    ARCH='x86'
+  elif [[ "$(uname -m)" == *"arm"* ]];then
+    ARCH='aarch'
+  elif [[ "$(uname -m)" == *"aarch"* ]];then
+    ARCH='aarch'
+  else
+    echo "unsupported arch, failed"
+    exit 1
+  fi
+  if [[ "$(getconf LONG_BIT)" == "64" ]];then
+    SYSTEM_BIT='64'
+  elif [[ "$(getconf LONG_BIT)" == "32" ]];then
+    SYSTEM_BIT='32'
+  else
+    echo "unsupported system bit, failed"
+    exit 1
+  fi
+  PROTOC_ZIP="protoc-${PROTOC_VERSION}-${OS}-${ARCH}_${SYSTEM_BIT}.zip"
+  curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/${PROTOC_ZIP}
+  unzip -o $PROTOC_ZIP -d "${GOPATH}/" bin/protoc
+  unzip -o $PROTOC_ZIP -d "${GOPATH}/" 'include/*'
+  ${GOPATH}/bin/protoc --version
+fi
+
+res=$(program_exists protoc-gen-gogofast)
+echo "res: ${res}"
+if [ "${res}" == "ok" ];then
+  echo "protoc-gen-gogofast exits"
+else
+  echo "install protoc-gen-gogofast"
+  if [ -f protobuf/ ];then rm -rf protobuf/;fi
+  git clone https://github.com/gogo/protobuf.git
+  cd protobuf
+  git checkout v1.3.2
+  cd protoc-gen-gogofast
+  go build -o $GOPATH/bin/protoc-gen-gogofast
+  cd ../..
+fi
+
+
 for file in `ls $PROTOC_DIR/*.proto`
 do
 	dir=$(basename $file .proto)
 	mkdir -p $PB_DIR/$dir
-	protoc -I=.:$PROTOC_DIR:$VENDOR_DIR --gogofast_out=paths=source_relative:./pkg/pb/$dir  $file
+	${GOPATH}/bin/protoc -I=.:$PROTOC_DIR:$VENDOR_DIR --gogofast_out=paths=source_relative:./pkg/pb/$dir  $file
     goimports -w $PB_DIR/$dir/*pb.go
 done
+
