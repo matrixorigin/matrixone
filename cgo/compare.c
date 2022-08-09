a3fde96a90e257de51a3b85a3c282332193a93d8
qingxinhome <758355272@qq.com>
2022-08-09 17:16:25 +0800

    modify c code

--- cgo/compare.c
+++ cgo/compare.c
@@ -83,10 +83,10 @@
  * bool compare operator
  */
 #define COMPARE_BOOL_EQ(TGT, A, B)                                     \
-    TGT = ((((A) != 0) && ((B) != 0)) || (((A) == 0) && ((B) == 0)))
+    TGT = (((A) && (B)) || (!(A) && !(B)))
 
 #define COMPARE_BOOL_NE(TGT, A, B)                                     \
-    TGT = ((((A) != 0) && ((B) == 0)) || (((A) == 0) && ((B) != 0)))
+    TGT = ((!(A) && (B)) || ((A) && !(B)))
 
 #define COMPARE_BOOL_LE(TGT, A, B)                                     \
     TGT = (!(A) || (B))
