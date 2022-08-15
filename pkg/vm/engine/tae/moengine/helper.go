710530f937dfa8748f318486ede15a9a0411c485
ou yuanning <45346669+ouyuanning@users.noreply.github.com>
2022-08-15 10:12:05 +0800

    Add view support (#4459)
    add view support in MO
    
    Approved by: @fengttt, @iamlinjunhong, @aunjgr, @daviszhen, @nnsgmsone, @XuPeng-SH, @reusee, @aressu1985

--- pkg/vm/engine/tae/moengine/helper.go
+++ pkg/vm/engine/tae/moengine/helper.go
@@ -16,8 +16,9 @@ package moengine
 
 import (
 	"fmt"
-	"github.com/matrixorigin/matrixone/pkg/pb/plan"
 	"strings"
+
+	"github.com/matrixorigin/matrixone/pkg/pb/plan"
 
 	"github.com/matrixorigin/matrixone/pkg/vm/engine"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/catalog"
@@ -28,6 +29,12 @@ func SchemaToDefs(schema *catalog.Schema) (defs []engine.TableDef, err error) {
 		commentDef := new(engine.CommentDef)
 		commentDef.Comment = schema.Comment
 		defs = append(defs, commentDef)
+	}
+
+	if schema.View != "" {
+		viewDef := new(engine.ViewDef)
+		viewDef.View = schema.View
+		defs = append(defs, viewDef)
 	}
 	for _, col := range schema.ColDefs {
 		if col.IsPhyAddr() {
@@ -90,12 +97,18 @@ func DefsToSchema(name string, defs []engine.TableDef) (schema *catalog.Schema, 
 					return
 				}
 			}
+
 		case *engine.PropertiesDef:
 			for _, property := range defVal.Properties {
-				if strings.ToLower(property.Key) == "comment" {
+				switch strings.ToLower(property.Key) {
+				case catalog.SystemRelAttr_Comment:
 					schema.Comment = property.Value
 				}
 			}
+
+		case *engine.ViewDef:
+			schema.View = defVal.View
+
 		default:
 			// We will not deal with other cases for the time being
 		}
