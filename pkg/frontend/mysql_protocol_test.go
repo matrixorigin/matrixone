bf50ccedb3311bf8dfe21f8e2806217c5a485240
ouyuanning <52741323@qq.com>
2022-08-23 15:38:28 +0800

    change ut

--- pkg/frontend/mysql_protocol_test.go
+++ pkg/frontend/mysql_protocol_test.go
@@ -304,7 +304,6 @@ func TestReadStringLenEnc(t *testing.T) {
 }
 
 func TestMysqlClientProtocol_TlsHandshake(t *testing.T) {
-	time.Sleep(time.Second * 1)
 	//before anything using the configuration
 	pu := config.NewParameterUnit(&config.FrontendParameters{}, nil, nil, nil, nil, nil)
 	_, err := toml.DecodeFile("test/system_vars_config.toml", pu.SV)
@@ -1222,7 +1221,6 @@ func (tRM *TestRoutineManager) resultsetHandler(rs goetty.IOSession, msg interfa
 }
 
 func TestMysqlResultSet(t *testing.T) {
-	time.Sleep(time.Second * 1)
 	//client connection method: mysql -h 127.0.0.1 -P 6001 -udump -p
 	//pwd: mysql-server-mysql-8.0.23/mysql-test
 	//with mysqltest: mysqltest --test-file=t/1st.test --result-file=r/1st.result --user=dump -p111 -P 6001 --host=127.0.0.1
@@ -1300,22 +1298,22 @@ func open_tls_db(t *testing.T, port int) *sql.DB {
 	if ok := rootCertPool.AppendCertsFromPEM(pem); !ok {
 		log.Fatal("Failed to append PEM.")
 	}
-	clientCert := make([]tls.Certificate, 0, 1)
-	certs, err := tls.LoadX509KeyPair("test/client-cert.pem", "test/client-key.pem")
-	if err != nil {
-		require.NoError(t, err)
-	}
-	clientCert = append(clientCert, certs)
+	// clientCert := make([]tls.Certificate, 0, 1)
+	// certs, err := tls.LoadX509KeyPair("test/client-cert.pem", "test/client-key.pem")
+	// if err != nil {
+	// 	require.NoError(t, err)
+	// }
+	// clientCert = append(clientCert, certs)
 	err = mysqlDriver.RegisterTLSConfig(tlsName, &tls.Config{
-		RootCAs:            rootCertPool,
-		Certificates:       clientCert,
+		// RootCAs:            rootCertPool,
+		// Certificates:       clientCert,
 		InsecureSkipVerify: true,
 	})
 	if err != nil {
 		require.NoError(t, err)
 	}
 
-	dsn := fmt.Sprintf("dump:111@tcp(127.0.0.1:%d)/?readTimeout=10s&timeout=10s&writeTimeout=10s&tls=%s", port, tlsName)
+	dsn := fmt.Sprintf("dump:111@tcp(127.0.0.1:%d)/?readTimeout=5s&timeout=5s&writeTimeout=5s&tls=%s", port, tlsName)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		require.NoError(t, err)
