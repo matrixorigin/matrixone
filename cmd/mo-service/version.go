96ebd35803e638bcd4d8e8e13983c75c8b33d93a
xzxiong <xzxiong@yeah.net>
2022-08-25 12:54:36 +0800

    revert version missing part

--- cmd/mo-service/version.go
+++ cmd/mo-service/version.go
@@ -37,7 +37,7 @@ func maybePrintVersion() {
 		return
 	}
 
-	fmt.Print("MatrixOne build info:")
+	fmt.Println("MatrixOne build info:")
 	fmt.Printf("  The golang version used to build this binary: %s\n", GoVersion)
 	fmt.Printf("  Git branch name: %s\n", BranchName)
 	fmt.Printf("  Git commit ID: %s\n", CommitID)
