41cb6fc8c4dff3ec348f13afd1f150e0b7272269
aubin <1030420200@qq.com>
2022-08-29 11:14:34 +0800

    log func base 1 should be error

--- pkg/sql/plan/function/builtin/unary/math.go
+++ pkg/sql/plan/function/builtin/unary/math.go
@@ -18,6 +18,7 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/container/nulls"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
+	"github.com/matrixorigin/matrixone/pkg/sql/errors"
 	"github.com/matrixorigin/matrixone/pkg/sql/plan/function/operator"
 	"github.com/matrixorigin/matrixone/pkg/vectorize/momath"
 	"github.com/matrixorigin/matrixone/pkg/vm/process"
@@ -89,15 +90,15 @@ func Log(vs []*vector.Vector, proc *process.Process) (*vector.Vector, error) {
 	if len(vs) == 1 {
 		return math1(vs, proc, momath.Ln)
 	}
+	vals := vs[0].Col.([]float64)
+	for i := range vals {
+		if vals[i] == float64(1) {
+			return nil, errors.New("", "Logarithm function base cannot be 1")
+		}
+	}
 	v1, err := math1([]*vector.Vector{vs[0]}, proc, momath.Ln)
 	if err != nil {
 		return nil, err
-	}
-	vals := v1.Col.([]float64)
-	for i := range vals {
-		if vals[i] == float64(0) {
-			nulls.Add(v1.Nsp, uint64(i))
-		}
 	}
 	v2, err := math1([]*vector.Vector{vs[1]}, proc, momath.Ln)
 	if err != nil {
