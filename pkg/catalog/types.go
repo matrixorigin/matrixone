6a0009e3375e2b129b05ba7fbfca6236f6536059
nnsgmsone <nnsmgsone@outlook.com>
2022-09-13 14:42:04 +0800

    Add catalog

--- pkg/catalog/types.go
+++ pkg/catalog/types.go
@@ -14,6 +14,11 @@
 
 package catalog
 
+import (
+	"github.com/matrixorigin/matrixone/pkg/container/types"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine"
+)
+
 const (
 	// default database name for catalog
 	MO_CATALOG  = "mo_catalog"
@@ -63,5 +68,77 @@ var (
 		"owner",
 		"account_id",
 	}
-	MoColumnsSchema = []string{}
+	MoColumnsSchema = []string{
+		"att_uniq_name",
+		"account_id",
+		"att_database_id",
+		"att_database",
+		"att_relname_id",
+		"att_relname",
+		"attname",
+		"atttyp",
+		"attnum",
+		"att_length",
+		"attnotnull",
+		"atthasdef",
+		"att_default",
+		"attisdropped",
+		"att_constraint_type",
+		"att_is_unsigned",
+		"att_is_auto_increment",
+		"att_comment",
+		"att_is_hidden",
+	}
+	MoDatabaseTypes = []types.Type{
+		types.New(types.T_int32, 0, 0, 0),     // dat_id
+		types.New(types.T_varchar, 100, 0, 0), // datname
+		types.New(types.T_varchar, 100, 0, 0), // dat_catalog_name
+		types.New(types.T_varchar, 100, 0, 0), // dat_createsql
+		types.New(types.T_int32, 0, 0, 0),     // owner
+		types.New(types.T_int32, 0, 0, 0),     // creator
+		types.New(types.T_timestamp, 0, 0, 0), // created_time
+		types.New(types.T_int32, 0, 0, 0),     // account_id
+	}
+	MoTablesTypes = []types.Type{
+		types.New(types.T_int32, 0, 0, 0),     // rel_id
+		types.New(types.T_varchar, 100, 0, 0), // relname
+		types.New(types.T_varchar, 100, 0, 0), // reldatabase
+		types.New(types.T_int32, 0, 0, 0),     // reldatabase_id
+		types.New(types.T_varchar, 100, 0, 0), // relpersistence
+		types.New(types.T_varchar, 100, 0, 0), // relkind
+		types.New(types.T_varchar, 100, 0, 0), // rel_comment
+		types.New(types.T_varchar, 100, 0, 0), // rel_createsql
+		types.New(types.T_timestamp, 0, 0, 0), // created_time
+		types.New(types.T_int32, 0, 0, 0),     // creator
+		types.New(types.T_int32, 0, 0, 0),     // owner
+		types.New(types.T_int32, 0, 0, 0),     // account_id
+	}
+	MoColumnsTypes = []types.Type{
+		types.New(types.T_varchar, 256, 0, 0),  // att_uniq_name
+		types.New(types.T_int32, 0, 0, 0),      // account_id
+		types.New(types.T_int32, 0, 0, 0),      // att_database_id
+		types.New(types.T_varchar, 256, 0, 0),  // att_database
+		types.New(types.T_int32, 0, 0, 0),      // att_relname_id
+		types.New(types.T_varchar, 256, 0, 0),  // att_relname
+		types.New(types.T_varchar, 256, 0, 0),  // attname
+		types.New(types.T_int32, 0, 0, 0),      // atttyp
+		types.New(types.T_int32, 0, 0, 0),      // attnum
+		types.New(types.T_int32, 0, 0, 0),      // att_length
+		types.New(types.T_int8, 0, 0, 0),       // attnotnull
+		types.New(types.T_int8, 0, 0, 0),       // atthasdef
+		types.New(types.T_varchar, 1024, 0, 0), // att_default
+		types.New(types.T_int8, 0, 0, 0),       // attisdropped
+		types.New(types.T_char, 1, 0, 0),       // att_constraint_type
+		types.New(types.T_int8, 0, 0, 0),       // att_is_unsigned
+		types.New(types.T_int8, 0, 0, 0),       // att_is_auto_increment
+		types.New(types.T_varchar, 1024, 0, 0), // att_comment
+		types.New(types.T_int8, 0, 0, 0),       // att_is_hidden
+
+	}
+	// used by memengine or tae
+	MoDatabaseTableDefs = []engine.TableDef{}
+	// used by memengine or tae
+	MoTablesTableDefs = []engine.TableDef{}
+	// used by memengine or tae
+	MoColumnsTableDefs = []engine.TableDef{}
 )
