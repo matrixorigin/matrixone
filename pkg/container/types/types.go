e0f858d5eb3a270a3a1d01e57343bf42dd25f1c4
yjw1268 <yjw981213@163.com>
2022-09-15 16:46:54 +0800

    Add some blob tests

--- pkg/container/types/types.go
+++ pkg/container/types/types.go
@@ -88,9 +88,9 @@ type Type struct {
 	// XXX Dummies.  T is uint8, make it 4 bytes aligned, otherwise, it may contain
 	// garbage data.  In theory these unused garbage should not be a problem, but
 	// it is.  Give it a name will zero fill it ...
-	dummy1 uint8
-	dummy2 uint8
-	dummy3 uint8
+	Charset uint8
+	dummy1  uint8
+	dummy2  uint8
 
 	// Width means max Display width for float and double, char and varchar
 	// todo: need to add new attribute DisplayWidth ?
@@ -203,6 +203,16 @@ func New(oid T, width, scale, precision int32) Type {
 		Scale:     scale,
 		Precision: precision,
 		Size:      int32(TypeSize(oid)),
+		Charset:   CharsetType(oid),
+	}
+}
+
+func CharsetType(oid T) uint8 {
+	switch oid {
+	case T_blob:
+		return 1
+	default:
+		return 0
 	}
 }
 
