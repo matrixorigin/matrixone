a32d3508587a764901edf63bcb1925fc9b6b30c9
nnsgmsone <nnsmgsone@outlook.com>
2022-09-19 16:49:11 +0800

    Add transaction interface

--- pkg/vm/engine/disttae/engine.go
+++ pkg/vm/engine/disttae/engine.go
@@ -38,7 +38,6 @@ func New(
 	if err != nil {
 		return nil
 	}
-	// engine cache
 	return &Engine{
 		m:                 m,
 		cli:               cli,
@@ -51,9 +50,9 @@ func New(
 var _ engine.Engine = new(Engine)
 
 func (e *Engine) Create(ctx context.Context, name string, op client.TxnOperator) error {
-	txn, err := e.getOrAddTransaction(op, e.getClusterDetails)
-	if err != nil {
-		return err
+	txn := e.getTransaction(op)
+	if txn == nil {
+		return moerr.New(moerr.ErrTxnClosed, "the transaction has been committed or aborted")
 	}
 	bat, err := genCreateDatabaseTuple(name, e.m)
 	if err != nil {
@@ -70,9 +69,9 @@ func (e *Engine) Create(ctx context.Context, name string, op client.TxnOperator)
 
 func (e *Engine) Database(ctx context.Context, name string,
 	op client.TxnOperator) (engine.Database, error) {
-	txn, err := e.getOrAddTransaction(op, e.getClusterDetails)
-	if err != nil {
-		return nil, err
+	txn := e.getTransaction(op)
+	if txn == nil {
+		return nil, moerr.New(moerr.ErrTxnClosed, "the transaction has been committed or aborted")
 	}
 	id, err := txn.getDatabaseId(ctx, name)
 	if err != nil {
@@ -86,17 +85,17 @@ func (e *Engine) Database(ctx context.Context, name string,
 }
 
 func (e *Engine) Databases(ctx context.Context, op client.TxnOperator) ([]string, error) {
-	txn, err := e.getOrAddTransaction(op, e.getClusterDetails)
-	if err != nil {
-		return nil, err
+	txn := e.getTransaction(op)
+	if txn == nil {
+		return nil, moerr.New(moerr.ErrTxnClosed, "the transaction has been committed or aborted")
 	}
 	return txn.getDatabaseList(ctx)
 }
 
 func (e *Engine) Delete(ctx context.Context, name string, op client.TxnOperator) error {
-	txn, err := e.getOrAddTransaction(op, e.getClusterDetails)
-	if err != nil {
-		return err
+	txn := e.getTransaction(op)
+	if txn == nil {
+		return moerr.New(moerr.ErrTxnClosed, "the transaction has been committed or aborted")
 	}
 	// non-io operations do not need to pass context
 	if err := txn.WriteBatch(DELETE, catalog.MO_CATALOG_ID, catalog.MO_DATABASE_ID,
@@ -112,7 +111,33 @@ func (e *Engine) hasConflict(txn *Transaction) bool {
 	return false
 }
 
-func (e *Engine) PreCommit(ctx context.Context, op client.TxnOperator) error {
+func (e *Engine) New(ctx context.Context, op client.TxnOperator) error {
+	cluster, err := e.getClusterDetails()
+	if err != nil {
+		return err
+	}
+	txn := &Transaction{
+		db:       e.db,
+		readOnly: false,
+		meta:     op.Txn(),
+		dnStores: cluster.DNStores,
+		fileMap:  make(map[string]uint64),
+	}
+	txn.writes = append(txn.writes, make([]Entry, 0, 1))
+	e.newTransaction(op, txn)
+	if len(txn.dnStores) > 0 {
+		// update catalog's cache
+		e.db.Update(ctx, txn.dnStores[:1], catalog.MO_CATALOG_ID, catalog.MO_DATABASE_ID,
+			txn.meta.SnapshotTS)
+		e.db.Update(ctx, txn.dnStores[:1], catalog.MO_CATALOG_ID, catalog.MO_TABLES_ID,
+			txn.meta.SnapshotTS)
+		e.db.Update(ctx, txn.dnStores[:1], catalog.MO_CATALOG_ID, catalog.MO_COLUMNS_ID,
+			txn.meta.SnapshotTS)
+	}
+	return nil
+}
+
+func (e *Engine) Commit(ctx context.Context, op client.TxnOperator) error {
 	txn := e.getTransaction(op)
 	if txn == nil {
 		return moerr.New(moerr.ErrTxnClosed, "the transaction has been committed or aborted")
@@ -156,28 +181,10 @@ func (e *Engine) Hints() (h engine.Hints) {
 	return
 }
 
-func (e *Engine) getOrAddTransaction(op client.TxnOperator,
-	getClusterDetails GetClusterDetailsFunc) (*Transaction, error) {
-	id := string(op.Txn().ID)
+func (e *Engine) newTransaction(op client.TxnOperator, txn *Transaction) {
 	e.Lock()
 	defer e.Unlock()
-	txn, ok := e.txns[id]
-	if !ok {
-		cluster, err := getClusterDetails()
-		if err != nil {
-			return nil, err
-		}
-		txn = &Transaction{
-			db:       e.db,
-			readOnly: false,
-			meta:     op.Txn(),
-			dnStores: cluster.DNStores,
-			fileMap:  make(map[string]uint64),
-		}
-		txn.writes = append(txn.writes, make([]Entry, 0, 1))
-		e.txns[id] = txn
-	}
-	return txn, nil
+	e.txns[string(op.Txn().ID)] = txn
 }
 
 func (e *Engine) getTransaction(op client.TxnOperator) *Transaction {
