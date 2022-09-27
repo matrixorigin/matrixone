8491eef04d6b38c7d706f7e789c576e6b63bcbfd
JacksonXie <xzxiong@yeah.net>
2022-09-27 10:23:50 +0800

    log real exec plan under condition exec duration >= cfg.LongQueryTime (#5260)
    - log real exec plan after sql done.
    - log exec plan under condition `exec duration >= cfg.LongQueryTime`
    
    Approved by: @daviszhen, @zhangxu19830126, @fengttt

--- pkg/util/export/batch_processor_test.go
+++ pkg/util/export/batch_processor_test.go
@@ -212,6 +212,7 @@ func Test_newBufferHolder(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			buf := newBufferHolder(tt.args.name, tt.args.impl, tt.args.signal)
+			buf.Start()
 			for _, v := range tt.args.elems {
 				buf.Add(v)
 			}
