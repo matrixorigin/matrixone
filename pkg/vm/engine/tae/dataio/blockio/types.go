9edea25ed67855ed2c4eccf3b5314a8a2e3fea5b
aptend <aptend@hotmail.com>
2022-10-07 17:29:56 +0800

    add buffer node for delta

--- pkg/vm/engine/tae/dataio/blockio/types.go
+++ pkg/vm/engine/tae/dataio/blockio/types.go
@@ -16,10 +16,10 @@ package blockio
 
 import (
 	"fmt"
-	"github.com/matrixorigin/matrixone/pkg/objectio"
 	"strconv"
 	"strings"
 
+	"github.com/matrixorigin/matrixone/pkg/objectio"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/common"
 )
 
@@ -113,6 +113,26 @@ func EncodeBlkDeltaLoc(id *common.ID, extent objectio.Extent) string {
 		extent.OriginSize(),
 	)
 	return deltaLoc
+}
+
+func DecodeDeltaLoc(metaLoc string) (string, objectio.Extent) {
+	info := strings.Split(metaLoc, ":")
+	name := info[0]
+	location := strings.Split(info[1], "_")
+	offset, err := strconv.ParseUint(location[0], 10, 32)
+	if err != nil {
+		panic(any(err))
+	}
+	size, err := strconv.ParseUint(location[1], 10, 32)
+	if err != nil {
+		panic(any(err))
+	}
+	osize, err := strconv.ParseUint(location[2], 10, 32)
+	if err != nil {
+		panic(any(err))
+	}
+	extent := objectio.NewExtent(uint32(offset), uint32(size), uint32(osize))
+	return name, extent
 }
 
 func DecodeMetaLoc(metaLoc string) (string, objectio.Extent, uint32) {
