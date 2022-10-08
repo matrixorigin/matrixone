77ac85d49a41cc89f517b808fe39ef3bbaea6d57
ou yuanning <45346669+ouyuanning@users.noreply.github.com>
2022-10-08 12:22:40 +0800

    add some util function for CN Workspace (#5317)
    add some util function for CN Workspace:
    needRead:   check if a block needed to read by a expression
    writeBlock:  write a batch data to s3
    readBlock: read s3 file's data to a batch
    
    and some struct and function is temporary, just for test
    
    Approved by: @nnsgmsone, @daviszhen, @reusee, @fengttt, @aunjgr

--- pkg/vm/engine/disttae/txn.go
+++ pkg/vm/engine/disttae/txn.go
@@ -16,11 +16,15 @@ package disttae
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/matrixorigin/matrixone/pkg/catalog"
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 	"github.com/matrixorigin/matrixone/pkg/container/batch"
+	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
+	"github.com/matrixorigin/matrixone/pkg/fileservice"
+	"github.com/matrixorigin/matrixone/pkg/objectio"
 	"github.com/matrixorigin/matrixone/pkg/pb/plan"
 	"github.com/matrixorigin/matrixone/pkg/sql/colexec"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine"
@@ -28,12 +32,12 @@ import (
 )
 
 func (txn *Transaction) getTableList(ctx context.Context, databaseId uint64) ([]string, error) {
-	rows, err := txn.getRows(ctx, catalog.MO_CATALOG_ID, catalog.MO_TABLES_ID, txn.dnStores[:1],
-		[]string{
-			catalog.MoTablesSchema[catalog.MO_TABLES_REL_NAME_IDX],
-			catalog.MoTablesSchema[catalog.MO_TABLES_RELDATABASE_ID_IDX],
-			catalog.MoTablesSchema[catalog.MO_TABLES_ACCOUNT_ID_IDX],
-		},
+	columns := []string{
+		catalog.MoTablesSchema[catalog.MO_TABLES_REL_NAME_IDX],
+		catalog.MoTablesSchema[catalog.MO_TABLES_RELDATABASE_ID_IDX],
+		catalog.MoTablesSchema[catalog.MO_TABLES_ACCOUNT_ID_IDX],
+	}
+	rows, err := txn.getRows(ctx, catalog.MO_CATALOG_ID, catalog.MO_TABLES_ID, txn.dnStores[:1], columns,
 		genTableListExpr(getAccountId(ctx), databaseId))
 	if err != nil {
 		return nil, err
@@ -48,8 +52,9 @@ func (txn *Transaction) getTableList(ctx context.Context, databaseId uint64) ([]
 func (txn *Transaction) getTableInfo(ctx context.Context, databaseId uint64,
 	name string) (uint64, []engine.TableDef, error) {
 	accountId := getAccountId(ctx)
+	columns := catalog.MoTablesSchema
 	row, err := txn.getRow(ctx, catalog.MO_CATALOG_ID, catalog.MO_TABLES_ID,
-		txn.dnStores[:1], catalog.MoTablesSchema,
+		txn.dnStores[:1], columns,
 		genTableInfoExpr(accountId, databaseId, name))
 	if err != nil {
 		return 0, nil, err
@@ -73,13 +78,15 @@ func (txn *Transaction) getTableInfo(ctx context.Context, databaseId uint64,
 func (txn *Transaction) getTableId(ctx context.Context, databaseId uint64,
 	name string) (uint64, error) {
 	accountId := getAccountId(ctx)
+	columns := []string{
+		catalog.MoTablesSchema[catalog.MO_TABLES_REL_ID_IDX],
+		catalog.MoTablesSchema[catalog.MO_TABLES_REL_NAME_IDX],
+		catalog.MoTablesSchema[catalog.MO_TABLES_RELDATABASE_ID_IDX],
+		catalog.MoTablesSchema[catalog.MO_TABLES_ACCOUNT_ID_IDX],
+	}
+
 	row, err := txn.getRow(ctx, catalog.MO_CATALOG_ID, catalog.MO_TABLES_ID,
-		txn.dnStores[:1], []string{
-			catalog.MoTablesSchema[catalog.MO_TABLES_REL_ID_IDX],
-			catalog.MoTablesSchema[catalog.MO_TABLES_REL_NAME_IDX],
-			catalog.MoTablesSchema[catalog.MO_TABLES_RELDATABASE_ID_IDX],
-			catalog.MoTablesSchema[catalog.MO_TABLES_ACCOUNT_ID_IDX],
-		},
+		txn.dnStores[:1], columns,
 		genTableIdExpr(accountId, databaseId, name))
 	if err != nil {
 		return 0, err
@@ -88,11 +95,13 @@ func (txn *Transaction) getTableId(ctx context.Context, databaseId uint64,
 }
 
 func (txn *Transaction) getDatabaseList(ctx context.Context) ([]string, error) {
+	columns := []string{
+		catalog.MoDatabaseSchema[catalog.MO_DATABASE_DAT_NAME_IDX],
+		catalog.MoColumnsSchema[catalog.MO_DATABASE_ACCOUNT_ID_IDX],
+	}
+
 	rows, err := txn.getRows(ctx, catalog.MO_CATALOG_ID, catalog.MO_DATABASE_ID,
-		txn.dnStores[:1], []string{
-			catalog.MoDatabaseSchema[catalog.MO_DATABASE_DAT_NAME_IDX],
-			catalog.MoColumnsSchema[catalog.MO_DATABASE_ACCOUNT_ID_IDX],
-		},
+		txn.dnStores[:1], columns,
 		genDatabaseListExpr(getAccountId(ctx)))
 	if err != nil {
 		return nil, err
@@ -106,12 +115,12 @@ func (txn *Transaction) getDatabaseList(ctx context.Context) ([]string, error) {
 
 func (txn *Transaction) getDatabaseId(ctx context.Context, name string) (uint64, error) {
 	accountId := getAccountId(ctx)
+	columns := []string{catalog.MoDatabaseSchema[catalog.MO_DATABASE_DAT_ID_IDX],
+		catalog.MoColumnsSchema[catalog.MO_DATABASE_DAT_NAME_IDX],
+		catalog.MoColumnsSchema[catalog.MO_DATABASE_ACCOUNT_ID_IDX],
+	}
 	row, err := txn.getRow(ctx, catalog.MO_CATALOG_ID, catalog.MO_DATABASE_ID, txn.dnStores[:1],
-		[]string{catalog.MoDatabaseSchema[catalog.MO_DATABASE_DAT_ID_IDX],
-			catalog.MoColumnsSchema[catalog.MO_DATABASE_DAT_NAME_IDX],
-			catalog.MoColumnsSchema[catalog.MO_DATABASE_ACCOUNT_ID_IDX],
-		},
-		genDatabaseIdExpr(accountId, name))
+		columns, genDatabaseIdExpr(accountId, name))
 	if err != nil {
 		return 0, err
 	}
@@ -279,9 +288,9 @@ func (txn *Transaction) readTable(ctx context.Context, databaseId uint64, tableI
 			bats = append(bats, bat)
 		}
 	}
-	proc := process.New(context.Background(), txn.m, nil, nil, nil)
+
 	for i, bat := range bats {
-		vec, err := colexec.EvalExpr(bat, proc, expr)
+		vec, err := colexec.EvalExpr(bat, txn.proc, expr)
 		if err != nil {
 			return nil, err
 		}
@@ -291,16 +300,16 @@ func (txn *Transaction) readTable(ctx context.Context, databaseId uint64, tableI
 				bat.Shrink(nil)
 			}
 		} else {
-			sels := txn.m.GetSels()
+			sels := txn.proc.GetMheap().GetSels()
 			for i, b := range bs {
 				if b {
 					sels = append(sels, int64(i))
 				}
 			}
 			bat.Shrink(sels)
-			txn.m.PutSels(sels)
+			txn.proc.GetMheap().PutSels(sels)
 		}
-		vec.Free(txn.m)
+		vec.Free(txn.proc.GetMheap())
 		bats[i] = bat
 	}
 	return bats, nil
@@ -331,9 +340,39 @@ func (h *transactionHeap) Pop() any {
 }
 
 // needRead determine if a block needs to be read
-func needRead(expr *plan.Expr, blkInfo BlockMeta) bool {
-	//TODO
-	return false
+func needRead(expr *plan.Expr, blkInfo BlockMeta, tableDef *plan.TableDef, proc *process.Process) bool {
+	var err error
+	columns := getColumnsByExpr(expr)
+
+	// if expr match no columns, just eval expr
+	if len(columns) == 0 {
+		bat := batch.NewWithSize(0)
+		ifNeed, err := evalFilterExpr(expr, bat, proc)
+		if err != nil {
+			return true
+		}
+		return ifNeed
+	}
+
+	// get min max data from Meta
+	datas, dataTypes, err := getZonemapDataFromMeta(columns, blkInfo, tableDef)
+	if err != nil {
+		return true
+	}
+
+	// use all min/max data to build []vectors.
+	buildVectors := buildVectorsByData(datas, dataTypes, proc.GetMheap())
+	bat := batch.NewWithSize(len(columns))
+	bat.Zs = make([]int64, buildVectors[0].Length())
+	bat.Vecs = buildVectors
+
+	ifNeed, err := evalFilterExpr(expr, bat, proc)
+	if err != nil {
+		return true
+	}
+
+	return ifNeed
+
 }
 
 // needSyncDnStores determine the dn store need to sync
@@ -361,16 +400,90 @@ func blockUnmarshal(data []byte) BlockMeta {
 	return BlockMeta{}
 }
 
-/*
 // write a block to s3
-func blockWrite(ctx context.Context, blkInfo BlockMeta, bat *batch.Batch) error {
-	//TODO
-	return nil
+func blockWrite(ctx context.Context, blkInfo BlockMeta, bat *batch.Batch, fs fileservice.FileService) ([]objectio.BlockObject, error) {
+	// 1. check columns length, check types
+	if len(blkInfo.columns) != len(bat.Vecs) {
+		return nil, moerr.NewInternalError(fmt.Sprintf("write block error: need %v columns, get %v columns", len(blkInfo.columns), len(bat.Vecs)))
+	}
+	for i, vec := range bat.Vecs {
+		if blkInfo.columns[i].typ != uint8(vec.Typ.Oid) {
+			return nil, moerr.NewInternalError(fmt.Sprintf("write block error: column[%v]'s type is not match", i))
+		}
+	}
+
+	// 2. write bat
+	s3FileName := getNameFromMeta(blkInfo)
+	writer, err := objectio.NewObjectWriter(s3FileName, fs)
+	if err != nil {
+		return nil, err
+	}
+	fd, err := writer.Write(bat)
+	if err != nil {
+		return nil, err
+	}
+
+	// 3. write index (index and zonemap)
+	for i, vec := range bat.Vecs {
+		bloomFilter, zoneMap, err := getIndexDataFromVec(uint16(i), vec)
+		if err != nil {
+			return nil, err
+		}
+		if bloomFilter != nil {
+			err = writer.WriteIndex(fd, bloomFilter)
+			if err != nil {
+				return nil, err
+			}
+		}
+		if zoneMap != nil {
+			err = writer.WriteIndex(fd, zoneMap)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	// 4. get return
+	return writer.WriteEnd()
 }
-*/
 
 // read a block from s3
-func blockRead(ctx context.Context, columns []string, blkInfo BlockMeta) (*batch.Batch, error) {
-	//TODO
-	return nil, nil
+func blockRead(ctx context.Context, columns []string, blkInfo BlockMeta, fs fileservice.FileService, tableDef *plan.TableDef) (*batch.Batch, error) {
+	// 1. get extent from meta
+	extent := getExtentFromMeta(blkInfo)
+
+	// 2. get idxs
+	columnLength := len(columns)
+	idxs := make([]uint16, columnLength)
+	columnTypes := make([]types.Type, columnLength)
+	for i, column := range columns {
+		idxs[i] = uint16(tableDef.Name2ColIndex[column])
+		columnTypes[i] = types.T(blkInfo.columns[idxs[i]].typ).ToType()
+	}
+
+	// 2. read data
+	s3FileName := getNameFromMeta(blkInfo)
+	reader, err := objectio.NewObjectReader(s3FileName, fs)
+	if err != nil {
+		return nil, err
+	}
+	ioVec, err := reader.Read(extent, idxs)
+	if err != nil {
+		return nil, err
+	}
+
+	// 3. fill Batch
+	bat := batch.NewWithSize(columnLength)
+	bat.Attrs = columns
+	for i, entry := range ioVec.Entries {
+		vec := vector.New(columnTypes[i])
+		err := vec.Read(entry.Data)
+		if err != nil {
+			return nil, err
+		}
+		bat.Vecs[i] = vec
+	}
+	bat.Zs = make([]int64, int64(bat.Vecs[0].Length()))
+
+	return bat, nil
 }
