cc61533ee59cdd80ed69f56eb4e22ecdb042a97c
aptend <aptend@hotmail.com>
2022-10-17 09:02:51 +0800

    use real option and remove dead comment

--- pkg/vm/engine/tae/logtail/handle.go
+++ pkg/vm/engine/tae/logtail/handle.go
@@ -382,19 +382,6 @@ func (b *TableLogtailRespBuilder) visitBlkMeta(e *catalog.BlockEntry) (skipData 
 }
 
 func (b *TableLogtailRespBuilder) appendBlkMeta(e *catalog.BlockEntry, metaNode *catalog.MetadataMVCCNode) {
-	// var dstBatch *containers.Batch
-	// if !metaNode.HasDropCommitted() {
-	// 	dstBatch = b.blkMetaInsBatch
-	// 	dstBatch.GetVectorByName(blkMetaAttrBlockID).Append(e.ID)
-	// 	dstBatch.GetVectorByName(blkMetaAttrEntryState).Append(e.IsAppendable())
-	// 	dstBatch.GetVectorByName(blkMetaAttrCreateAt).Append(metaNode.CreatedAt)
-	// 	dstBatch.GetVectorByName(blkMetaAttrDeleteAt).Append(metaNode.DeletedAt)
-	// 	dstBatch.GetVectorByName(blkMetaAttrMetaLoc).Append([]byte(metaNode.MetaLoc))
-	// 	dstBatch.GetVectorByName(blkMetaAttrDeltaLoc).Append([]byte(metaNode.DeltaLoc))
-	// } else {
-	// 	dstBatch = b.blkMetaDelBatch
-	// }
-
 	// for now, we treat all mvcc nodes as update operation. Only Insert
 	dstBatch := b.blkMetaInsBatch
 
