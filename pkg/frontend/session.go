591b17be323a2fd3d5b008d9f5841510c9d64695
GreatRiver <2552853833@qq.com>
2022-10-21 11:33:20 +0800

    Fix "Execution of sql error, all subsequent sql execution fails"

--- pkg/frontend/session.go
+++ pkg/frontend/session.go
@@ -1152,6 +1152,7 @@ func (th *TxnHandler) CommitTxn() error {
 	defer cancel()
 	txnOp := th.GetTxnOperator()
 	if err := storage.Commit(ctx, txnOp); err != nil {
+		th.SetInvalid()
 		return err
 	}
 	err := txnOp.Commit(ctx)
@@ -1175,6 +1176,7 @@ func (th *TxnHandler) RollbackTxn() error {
 	defer cancel()
 	txnOp := th.GetTxnOperator()
 	if err := storage.Rollback(ctx, txnOp); err != nil {
+		th.SetInvalid()
 		return err
 	}
 	err := txnOp.Rollback(ctx)
