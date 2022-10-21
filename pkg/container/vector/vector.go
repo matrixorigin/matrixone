716a38ea4420884fb99869257436ce939e221a6f
ou yuanning <45346669+ouyuanning@users.noreply.github.com>
2022-10-21 14:54:42 +0800

    fix bug: insert table with default value have no vector.data (#5810)
    when insert into table with column's default value.  we use NewConstNull,  that method will not set vector.data, then will panic in disttae
    
    Approved by: @fengttt, @nnsgmsone

--- pkg/container/vector/vector.go
+++ pkg/container/vector/vector.go
@@ -17,9 +17,10 @@ package vector
 import (
 	"bytes"
 	"fmt"
-	"github.com/matrixorigin/matrixone/pkg/common/bitmap"
 	"reflect"
 	"unsafe"
+
+	"github.com/matrixorigin/matrixone/pkg/common/bitmap"
 
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 	"github.com/matrixorigin/matrixone/pkg/common/mpool"
@@ -515,6 +516,15 @@ func NewWithNspSize(typ types.Type, n int64) *Vector {
 		Typ:      typ,
 		original: false,
 	}
+}
+
+func NewConstNullWithData(typ types.Type, length int, mp *mpool.MPool) *Vector {
+	v := New(typ)
+	v.isConst = true
+	val := GetInitConstVal(typ)
+	v.Append(val, true, mp)
+	v.length = length
+	return v
 }
 
 func NewConst(typ types.Type, length int) *Vector {
@@ -1495,5 +1505,57 @@ func (v *Vector) String() string {
 
 	default:
 		panic("vec to string unknown types.")
+	}
+}
+
+func GetInitConstVal(typ types.Type) any {
+	switch typ.Oid {
+	case types.T_bool:
+		return false
+	case types.T_int8:
+		return int8(0)
+	case types.T_int16:
+		return int16(0)
+	case types.T_int32:
+		return int32(0)
+	case types.T_int64:
+		return int64(0)
+	case types.T_uint8:
+		return uint8(0)
+	case types.T_uint16:
+		return uint16(0)
+	case types.T_uint32:
+		return uint32(0)
+	case types.T_uint64:
+		return uint64(0)
+	case types.T_float32:
+		return float32(0)
+	case types.T_float64:
+		return float64(0)
+	case types.T_date:
+		return types.Date(0)
+	case types.T_datetime:
+		return types.Datetime(0)
+	case types.T_timestamp:
+		return types.Timestamp(0)
+	case types.T_decimal64:
+		return types.Decimal64{}
+	case types.T_decimal128:
+		return types.Decimal128{}
+	case types.T_uuid:
+		var emptyUuid [16]byte
+		return emptyUuid[:]
+	case types.T_TS:
+		var emptyTs [types.TxnTsSize]byte
+		return emptyTs[:]
+	case types.T_Rowid:
+		var emptyRowid [types.RowidSize]byte
+		return emptyRowid[:]
+	case types.T_char, types.T_varchar, types.T_blob, types.T_json, types.T_text:
+		var emptyVarlena [types.VarlenaSize]byte
+		return emptyVarlena[:]
+	default:
+		//T_any T_star T_tuple T_interval
+		return int64(0)
 	}
 }
