67f0e981f4abb29c05bba17ca4048ff3ed482734
tianyahui-python <1348221747@qq.com>
2022-10-24 17:12:33 +0800

    check bug

--- test/cases/load_data/import_data.sql
+++ test/cases/load_data/import_data.sql
@@ -108,9 +108,7 @@ import data infile '$resources/into_outfile_2/outfile_float_2.csv' into table t3
 select * from t3;
 delete from t3
 
--- @bvt:issue#5112
 import data infile '$resources/load_data/float_3.csv' into table t3;
--- @bvt:issue
 
 drop table t3;
 
