5361f06b88023959780589e62a08371ce993f652
mooleetzi <13208308200@163.com>
2022-10-28 19:54:06 +0800

    add convert test

--- cmd/mo-dump/main_test.go
+++ cmd/mo-dump/main_test.go
@@ -13,8 +13,52 @@
 // limitations under the License.
 package main
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
-func TestMoDump(t *testing.T) {
-	main()
+func TestConvertValue(t *testing.T) {
+	kase := []struct {
+		val string
+		typ string
+	}{
+		{"1", "int"},
+		{"1", "tinyint"},
+		{"1", "smallint"},
+		{"1", "bigint"},
+		{"1", "unsigned bigint"},
+		{"1", "unsigned int"},
+		{"1", "unsigned tinyint"},
+		{"1", "unsigned smallint"},
+		{"1.1", "float"},
+		{"1.1", "double"},
+		{"1.1", "decimal"},
+		{"asa", "varchar"},
+		{"asa", "char"},
+		{"asa", "text"},
+		{"asa", "blob"},
+		{"asa", "uuid"},
+		{"asa", "json"},
+		{"2021-01-01", "date"},
+		{"2021-01-01 00:00:00", "datetime"},
+		{"2021-01-01 00:00:00", "timestamp"},
+	}
+	for _, v := range kase {
+		s := convertValue(makeValue(v.val), v.typ)
+		switch v.typ {
+		case "int", "tinyint", "smallint", "bigint", "unsigned bigint", "unsigned int", "unsigned tinyint", "unsigned smallint", "float", "double":
+			require.Equal(t, v.val, s)
+		default:
+
+			require.Equal(t, fmt.Sprintf("'%v'", v.val), s)
+		}
+	}
+}
+
+func makeValue(val string) interface{} {
+	tmp := []byte(val)
+	var ret interface{} = tmp
+	return &ret
 }
