9a457bc5e8f956377739d67f9703828bd8b7e4f4
nnsgmsone <nnsmgsone@outlook.com>
2022-10-29 12:16:46 +0800

    Fix the bug of  using wrong timestamp to remove row according to block id (#6081)

--- pkg/vm/engine/disttae/types.go
+++ pkg/vm/engine/disttae/types.go
@@ -85,8 +85,8 @@ type MVCC interface {
 		blocks []BlockMeta, entries []Entry) ([]BlockMeta, map[uint64][]int)
 	// If blocks is empty, it means no merge operation with the files on s3 is required.
 	NewReader(ctx context.Context, readerNumber int, index memtable.Tuple, defs []engine.TableDef,
-		tableDef *plan.TableDef, blks []ModifyBlockMeta, ts timestamp.Timestamp,
-		fs fileservice.FileService, entries []Entry) ([]engine.Reader, error)
+		tableDef *plan.TableDef, skipBlocks map[uint64]uint8, blks []ModifyBlockMeta,
+		ts timestamp.Timestamp, fs fileservice.FileService, entries []Entry) ([]engine.Reader, error)
 }
 
 type Engine struct {
@@ -222,6 +222,9 @@ type table struct {
 	partition  string
 	relKind    string
 	createSql  string
+
+	// use for skip rows
+	skipBlocks map[uint64]uint8
 }
 
 type column struct {
