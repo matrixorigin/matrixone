79bec9dea4ddeb59d2a299a8bc7517a7faa14698
w-zr <weiziran125@gmail.com>
2022-11-04 15:46:39 +0800

    add rpc-client tags & fix goroutine leaks in cn

--- pkg/logservice/service.go
+++ pkg/logservice/service.go
@@ -431,5 +431,7 @@ func (s *Service) getBackendOptions() []morpc.BackendOption {
 
 // NB: leave an empty method for future extension.
 func (s *Service) getClientOptions() []morpc.ClientOption {
-	return nil
+	return []morpc.ClientOption{
+		morpc.WithClientTag("log-heartbeat"),
+	}
 }
