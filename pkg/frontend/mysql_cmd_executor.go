3009d22d5507273b3251c3d15478f87251cf127f
nitao <badboynt@126.com>
2022-11-04 20:04:47 +0800

    [Feature Request]: Problem with sqlalchemy #2174 (#6278)
    select version() now return 8.0.30-MatrixOne-v0.6.0

--- pkg/frontend/mysql_cmd_executor.go
+++ pkg/frontend/mysql_cmd_executor.go
@@ -2392,7 +2392,7 @@ func (mce *MysqlCmdExecutor) doComQuery(requestCtx context.Context, sql string) 
 		Host:          pu.SV.Host,
 		ConnectionID:  uint64(proto.ConnectionID()),
 		Database:      ses.GetDatabaseName(),
-		Version:       serverVersion.Load().(string),
+		Version:       "8.0.30-MatrixOne-v" + serverVersion.Load().(string),
 		TimeZone:      ses.GetTimeZone(),
 		StorageEngine: pu.StorageEngine,
 	}
