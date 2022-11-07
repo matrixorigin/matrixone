8a04b64edf30785923f3ace1e27d42436f40df4a
xzxiong <xzxiong@yeah.net>
2022-11-08 00:03:09 +0800

    fix bvt

--- test/distributed/cases/log/column_span.sql
+++ test/distributed/cases/log/column_span.sql
@@ -1,1 +1,1 @@
-select span_id, statement_id, parent_span_id, node_uuid, node_type, span_name, start_time, end_time, duration, `resource` from system.span_info limit 0;
+select trace_id, span_id, parent_span_id, node_uuid, node_type, span_name, start_time, end_time, duration, `resource` from system.span_info limit 0;
