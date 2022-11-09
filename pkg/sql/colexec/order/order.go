dcdfb3510e1b17b60bc2326f7a8d61f866b2be38
maomao <77312370+sukki37@users.noreply.github.com>
2022-11-09 11:47:32 +0800

    Revert "add operator clean method and rewrite pipeline clean up process. (#6378)" (#6426)
    This reverts commit fba7f9ba21ac4241a982a52779b68a8944317085.

--- pkg/sql/colexec/order/order.go
+++ pkg/sql/colexec/order/order.go
@@ -41,7 +41,7 @@ func String(arg any, buf *bytes.Buffer) {
 
 func Prepare(_ *process.Process, arg any) error {
 	ap := arg.(*Argument)
-	ap.ctr = new(container)
+	ap.ctr = new(Container)
 	{
 		ap.ctr.desc = make([]bool, len(ap.Fs))
 		ap.ctr.nullsLast = make([]bool, len(ap.Fs))
@@ -64,25 +64,18 @@ func Call(idx int, proc *process.Process, arg any) (bool, error) {
 	anal := proc.GetAnalyze(idx)
 	anal.Start()
 	defer anal.Stop()
-
-	bat := proc.InputBatch()
-	ap := arg.(*Argument)
+	bat := proc.Reg.InputBatch
 	if bat == nil {
-		ap.Free(proc, false)
 		return true, nil
 	}
 	if bat.Length() == 0 {
 		return false, nil
 	}
-	end, err := ap.ctr.process(ap, bat, proc)
-	if err != nil {
-		ap.Free(proc, true)
-		return false, err
-	}
-	return end, nil
+	ap := arg.(*Argument)
+	return ap.ctr.process(ap, bat, proc)
 }
 
-func (ctr *container) process(ap *Argument, bat *batch.Batch, proc *process.Process) (bool, error) {
+func (ctr *Container) process(ap *Argument, bat *batch.Batch, proc *process.Process) (bool, error) {
 	for i := 0; i < bat.VectorCount(); i++ {
 		vec := bat.GetVector(int32(i))
 		if vec.IsOriginal() {
@@ -97,6 +90,11 @@ func (ctr *container) process(ap *Argument, bat *batch.Batch, proc *process.Proc
 	for i, f := range ap.Fs {
 		vec, err := colexec.EvalExpr(bat, proc, f.Expr)
 		if err != nil {
+			for j := 0; j < i; j++ {
+				if ctr.vecs[j].needFree {
+					vector.Clean(ctr.vecs[j].vec, proc.Mp())
+				}
+			}
 			return false, err
 		}
 		ctr.vecs[i].vec = vec
@@ -108,15 +106,20 @@ func (ctr *container) process(ap *Argument, bat *batch.Batch, proc *process.Proc
 			}
 		}
 	}
-	defer ctr.cleanEvalVectors(proc.Mp())
+	defer func() {
+		for i := range ctr.vecs {
+			if ctr.vecs[i].needFree {
+				vector.Clean(ctr.vecs[i].vec, proc.Mp())
+			}
+		}
+	}()
 	ovec := ctr.vecs[0].vec
 	var strCol []string
-
-	sels := make([]int64, len(bat.Zs))
-	for i := 0; i < len(bat.Zs); i++ {
+	n := len(bat.Zs)
+	sels := make([]int64, n)
+	for i := range sels {
 		sels[i] = int64(i)
 	}
-
 	nullCnt := nulls.Length(ovec.Nsp)
 	// skip sort for all nulls
 	if nullCnt < ovec.Length() {
