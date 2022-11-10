5c3d92e2343086e85de12456bee36f3784641500
nnsgmsone <nnsmgsone@outlook.com>
2022-11-10 12:23:58 +0800

    Dealing with two transboundary bug (#6461)
    Approved by: @yingfeng

--- pkg/vm/engine/disttae/table.go
+++ pkg/vm/engine/disttae/table.go
@@ -345,10 +345,7 @@ func (tbl *table) NewReader(ctx context.Context, num int, expr *plan.Expr, range
 		}
 		return rds, nil
 	}
-	step := len(ranges) / num
-	if step < 1 {
-		step = 1
-	}
+	step := (len(ranges) / num) + 1
 	for i := 0; i < num; i++ {
 		if i == num-1 {
 			rds[i] = &blockReader{
