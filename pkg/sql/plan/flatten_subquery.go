297949c4ff744ba26acde842a25b7b4a8ddf539c
nitao <badboynt@126.com>
2022-11-12 01:03:48 +0800

    [Bug]: subquery with count got wrong result #6494 (#6496)
    * rewrite subquery
    
    * use deepcopyexpr
    
    * add bvt
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: fengttt <fengttt@gmail.com>

--- pkg/sql/plan/flatten_subquery.go
+++ pkg/sql/plan/flatten_subquery.go
@@ -18,6 +18,7 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/pb/plan"
+	"github.com/matrixorigin/matrixone/pkg/sql/plan/function"
 )
 
 var (
@@ -80,9 +81,12 @@ func (builder *QueryBuilder) flattenSubquery(nodeID int32, subquery *plan.Subque
 
 	switch subquery.Typ {
 	case plan.SubqueryRef_SCALAR:
+		var rewrite bool
 		// Uncorrelated subquery
 		if len(joinPreds) == 0 {
 			joinPreds = append(joinPreds, constTrue)
+		} else if builder.findAggrCount(subCtx.aggregates) {
+			rewrite = true
 		}
 
 		joinType := plan.Node_SINGLE
@@ -105,7 +109,7 @@ func (builder *QueryBuilder) flattenSubquery(nodeID int32, subquery *plan.Subque
 			}, ctx)
 		}
 
-		return nodeID, &plan.Expr{
+		retExpr := &plan.Expr{
 			Typ: subCtx.results[0].Typ,
 			Expr: &plan.Expr_Col{
 				Col: &plan.ColRef{
@@ -113,7 +117,37 @@ func (builder *QueryBuilder) flattenSubquery(nodeID int32, subquery *plan.Subque
 					ColPos: 0,
 				},
 			},
-		}, nil
+		}
+		if rewrite {
+			argsType := make([]types.Type, 1)
+			argsType[0] = makeTypeByPlan2Expr(retExpr)
+			funcID, returnType, _, _ := function.GetFunctionByName("isnull", argsType)
+			isNullExpr := &Expr{
+				Expr: &plan.Expr_F{
+					F: &plan.Function{
+						Func: getFunctionObjRef(funcID, "isnull"),
+						Args: []*Expr{retExpr},
+					},
+				},
+				Typ: makePlan2Type(&returnType),
+			}
+			zeroExpr := makePlan2Int64ConstExprWithType(0)
+			argsType = make([]types.Type, 3)
+			argsType[0] = makeTypeByPlan2Expr(isNullExpr)
+			argsType[1] = makeTypeByPlan2Expr(zeroExpr)
+			argsType[2] = makeTypeByPlan2Expr(retExpr)
+			funcID, returnType, _, _ = function.GetFunctionByName("case", argsType)
+			retExpr = &Expr{
+				Expr: &plan.Expr_F{
+					F: &plan.Function{
+						Func: getFunctionObjRef(funcID, "case"),
+						Args: []*Expr{isNullExpr, zeroExpr, DeepCopyExpr(retExpr)},
+					},
+				},
+				Typ: makePlan2Type(&returnType),
+			}
+		}
+		return nodeID, retExpr, nil
 
 	case plan.SubqueryRef_EXISTS:
 		// Uncorrelated subquery
@@ -347,6 +381,18 @@ func (builder *QueryBuilder) generateComparison(op string, child *plan.Expr, ctx
 			},
 		})
 	}
+}
+
+func (builder *QueryBuilder) findAggrCount(aggrs []*plan.Expr) bool {
+	for _, aggr := range aggrs {
+		switch exprImpl := aggr.Expr.(type) {
+		case *plan.Expr_F:
+			if exprImpl.F.Func.ObjName == "count" || exprImpl.F.Func.ObjName == "starcount" {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func (builder *QueryBuilder) findNonEqPred(preds []*plan.Expr) bool {
