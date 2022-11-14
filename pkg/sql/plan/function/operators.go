83ca9cefd4b17b1f35cfdd09254c68c2b4e3d500
nnsgmsone <nnsmgsone@outlook.com>
2022-11-14 14:20:57 +0800

    Fix the problem of wrong flag usage

--- pkg/sql/plan/function/operators.go
+++ pkg/sql/plan/function/operators.go
@@ -1845,7 +1845,7 @@ var operators = map[int]Functions{
 
 	LIKE: {
 		Id:     LIKE,
-		Flag:   plan.Function_MONOTONICAL,
+		Flag:   plan.Function_STRICT,
 		Layout: BINARY_LOGICAL_OPERATOR,
 		Overloads: []Function{
 			{
