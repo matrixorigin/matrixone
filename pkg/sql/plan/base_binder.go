5c8f4b8c7ed8f501854977f53e8eaeef2bcc5f9c
JackTan25 <jacktby@gmail.com>
2022-11-17 16:27:26 +0800

    fix compare and null as

--- pkg/sql/plan/base_binder.go
+++ pkg/sql/plan/base_binder.go
@@ -1167,6 +1167,9 @@ func (b *baseBinder) bindNumVal(astExpr *tree.NumVal, typ *Type) (*Expr, error) 
 	case tree.P_char:
 		expr := makePlan2StringConstExprWithType(astExpr.String())
 		return expr, nil
+	case tree.P_nulltext:
+		expr := MakePlan2NullTextConstExprWithType(astExpr.String())
+		return expr, nil
 	default:
 		return nil, moerr.NewInvalidInput("unsupport value '%s'", astExpr.String())
 	}
