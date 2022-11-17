ad111b15911f9acf7a3c47170a47e27ea0422d5a
daviszhen <daviszhen007@gmail.com>
2022-11-17 19:38:36 +0800

    clean account

--- pkg/frontend/session.go
+++ pkg/frontend/session.go
@@ -1262,11 +1262,6 @@ func (th *TxnHandler) NewTxn() error {
 		panic("context should not be nil")
 	}
 	storage := th.GetStorage()
-	ctx, cancel := context.WithTimeout(
-		ctx,
-		storage.Hints().CommitOrRollbackTimeout,
-	)
-	defer cancel()
 	err = storage.New(ctx, th.GetTxnOperator())
 	return err
 }
