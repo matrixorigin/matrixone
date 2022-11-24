cf1eda2760879b7c570ea2704b2b741bdcc819c7
YANGGMM <18916107305@163.com>
2022-11-24 16:47:29 +0800

    fix return vector

--- pkg/sql/plan/function/builtin/multi/field.go
+++ pkg/sql/plan/function/builtin/multi/field.go
@@ -45,7 +45,7 @@ func FieldString(vs []*vector.Vector, proc *process.Process) (*vector.Vector, er
 
 		//if first vector is null, the return value is 0
 		if firstVector.IsScalarNull() {
-			return resultVector, nil
+			return vector.NewConstFixed(returnType, vecLen*returnType.Oid.FixedLength(), uint64(0), proc.Mp()), err
 		}
 
 		//detect index
@@ -160,7 +160,7 @@ func FieldNumber[T number](vs []*vector.Vector, proc *process.Process) (*vector.
 
 		//if first vector is null, the return value is 0
 		if firstVector.IsScalarNull() {
-			return resultVector, nil
+			return vector.NewConstFixed(returnType, vecLen*returnType.Oid.FixedLength(), uint64(0), proc.Mp()), err
 		}
 
 		//detect index
