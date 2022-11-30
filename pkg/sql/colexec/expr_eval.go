aee729ffd1a4c2816255a9f43f45041dd0313b2a
YANGGMM <18916107305@163.com>
2022-11-30 19:01:31 +0800

    make fmt

--- pkg/sql/colexec/expr_eval.go
+++ pkg/sql/colexec/expr_eval.go
@@ -101,7 +101,7 @@ func getConstVec(proc *process.Process, expr *plan.Expr, length int) (*vector.Ve
 			vec = vector.NewConstString(constSType, length, sval, proc.Mp())
 		case *plan.Const_Defaultval:
 			defaultVal := t.C.GetDefaultval()
-			vec = vector.NewConstFixed(constBType, length, defaultVal, proc.Mp())	
+			vec = vector.NewConstFixed(constBType, length, defaultVal, proc.Mp())
 		default:
 			return nil, moerr.NewNYI(fmt.Sprintf("const expression %v", t.C.GetValue()))
 		}
