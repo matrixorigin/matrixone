6a00f9351d2901834c5409b0f15da3356afd1914
ouyuanning <52741323@qq.com>
2022-11-30 15:58:04 +0800

    fix typo

--- pkg/container/batch/batch.go
+++ pkg/container/batch/batch.go
@@ -129,7 +129,7 @@ func (bat *Batch) UnmarshalBinary(data []byte) error {
 		return err
 	}
 	bat.Cnt = 1
-	bat.Zs = rbat.Zs // if you drop rbat.Zs is ok, if you need return rbat,  you must deepcoy Zs.
+	bat.Zs = rbat.Zs // if you drop rbat.Zs is ok, if you need return rbat,  you must deepcopy Zs.
 	bat.Vecs = rbat.Vecs
 	bat.Attrs = rbat.Attrs
 	bat.Aggs = make([]agg.Agg[any], len(rbat.AggInfos))
