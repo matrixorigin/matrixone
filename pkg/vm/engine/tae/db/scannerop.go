13f152d96f70611b04bdcc3b8cbd93dbfe8b6e8b
aptend <aptend@hotmail.com>
2022-12-07 11:57:23 +0800

    better merge log

--- pkg/vm/engine/tae/db/scannerop.go
+++ pkg/vm/engine/tae/db/scannerop.go
@@ -69,10 +69,10 @@ func (processor *calibrationOp) onPostSegment(segmentEntry *catalog.SegmentEntry
 		segmentData := segmentEntry.GetSegmentData()
 		taskFactory, taskType, scopes, err := segmentData.BuildCompactionTaskFactory()
 		if err != nil || taskFactory == nil {
-			logutil.Warnf("%s: %v", segmentData.MutationInfo(), err)
+			logutil.Warnf("[Mergeblocks] Schedule %s: %v", segmentData.MutationInfo(), err)
 		} else {
 			_, err = processor.db.Scheduler.ScheduleMultiScopedTxnTask(nil, taskType, scopes, taskFactory)
-			logutil.Debugf("[Mergeblocks] | %s | Scheduled | State=%v | Scopes=%s", segmentEntry.String(), err, common.IDArraryString(scopes))
+			logutil.Infof("[Mergeblocks] | %s | Scheduled | State=%v | Scopes=%s", segmentEntry.String(), err, common.BlockIDArraryString(scopes))
 		}
 	}
 	processor.blkCntOfSegment = 0
