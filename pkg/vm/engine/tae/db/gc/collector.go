ff0c2328ca4e5b7990cc00d19f26a4c98d40052b
GreatRiver <2552853833@qq.com>
2022-12-07 19:57:24 +0800

    Add GC with CKP

---
+++ pkg/vm/engine/tae/db/gc/collector.go
@@ -0,0 +1,53 @@
+package gc
+
+import (
+	pkgcatalog "github.com/matrixorigin/matrixone/pkg/catalog"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/catalog"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/common"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/dataio/blockio"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/logtail"
+)
+
+type Collector struct {
+	table *GcTable
+}
+
+func NewCollector(table *GcTable) *Collector {
+	return &Collector{
+		table: table,
+	}
+}
+
+func (c *Collector) UpdateTable(data *logtail.CheckpointData) {
+	ins, insTxn, del, delTxn := data.GetBlkBatchs()
+	for i := 0; i < ins.Length(); i++ {
+		dbid := insTxn.GetVectorByName(catalog.SnapshotAttr_DBID).Get(i).(uint64)
+		tid := insTxn.GetVectorByName(catalog.SnapshotAttr_TID).Get(i).(uint64)
+		sid := insTxn.GetVectorByName(catalog.SnapshotAttr_SegID).Get(i).(uint64)
+		blkID := ins.GetVectorByName(pkgcatalog.BlockMeta_ID).Get(i).(uint64)
+		metaLoc := string(ins.GetVectorByName(pkgcatalog.BlockMeta_MetaLoc).Get(i).([]byte))
+		id := common.ID{
+			SegmentID: sid,
+			TableID:   tid,
+			BlockID:   blkID,
+			PartID:    uint32(dbid),
+		}
+		name, _, _ := blockio.DecodeMetaLoc(metaLoc)
+		c.table.AddBlock(id, name)
+	}
+	for i := 0; i < del.Length(); i++ {
+		dbid := delTxn.GetVectorByName(catalog.SnapshotAttr_DBID).Get(i).(uint64)
+		tid := delTxn.GetVectorByName(catalog.SnapshotAttr_TID).Get(i).(uint64)
+		sid := delTxn.GetVectorByName(catalog.SnapshotAttr_SegID).Get(i).(uint64)
+		blkID := del.GetVectorByName(catalog.AttrRowID).Get(i).(uint64)
+		metaLoc := string(delTxn.GetVectorByName(pkgcatalog.BlockMeta_MetaLoc).Get(i).([]byte))
+		id := common.ID{
+			SegmentID: sid,
+			TableID:   tid,
+			BlockID:   blkID,
+			PartID:    uint32(dbid),
+		}
+		name, _, _ := blockio.DecodeMetaLoc(metaLoc)
+		c.table.DeleteBlock(id, name)
+	}
+}
---
+++ pkg/vm/engine/tae/db/gc/table.go
@@ -0,0 +1,70 @@
+package gc
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/common"
+)
+
+type GcTable struct {
+	table  map[string][]common.ID
+	delete map[string][]common.ID
+}
+
+func NewGcTable() *GcTable {
+	table := &GcTable{
+		table:  make(map[string][]common.ID),
+		delete: make(map[string][]common.ID),
+	}
+	return table
+}
+
+func (t *GcTable) AddBlock(id common.ID, name string) {
+	blockList := t.table[name]
+	if blockList != nil {
+		blockList = make([]common.ID, 0)
+	}
+
+	blockList = append(blockList, id)
+	t.table[name] = blockList
+}
+
+func (t *GcTable) DeleteBlock(id common.ID, name string) {
+	blockList := t.delete[name]
+	if blockList != nil {
+		blockList = make([]common.ID, 0)
+	}
+
+	blockList = append(blockList, id)
+	t.table[name] = blockList
+}
+
+func (t *GcTable) String() string {
+	if len(t.table) == 0 {
+		return ""
+	}
+	var w bytes.Buffer
+	_, _ = w.WriteString("table:[")
+	for name, ids := range t.table {
+		_, _ = w.WriteString(fmt.Sprintf(" %v", name))
+		_, _ = w.WriteString("block:[")
+		for _, id := range ids {
+			_, _ = w.WriteString(fmt.Sprintf(" %v", id.String()))
+		}
+		_, _ = w.WriteString("]\n")
+	}
+	_, _ = w.WriteString("]\n")
+	if len(t.delete) != 0 {
+		_, _ = w.WriteString("delete:[")
+		for name, ids := range t.delete {
+			_, _ = w.WriteString(fmt.Sprintf(" %v", name))
+			_, _ = w.WriteString("block:[")
+			for _, id := range ids {
+				_, _ = w.WriteString(fmt.Sprintf(" %v", id.String()))
+			}
+			_, _ = w.WriteString("]\n")
+		}
+		_, _ = w.WriteString("]\n")
+	}
+	return w.String()
+}
