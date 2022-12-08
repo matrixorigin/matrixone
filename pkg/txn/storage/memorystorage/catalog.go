57c9f98b66014eff2135be171ae57828ed2b9d75
aubin <1030420200@qq.com>
2022-12-08 15:06:54 +0800

    use constraint instead of index in table def

--- pkg/txn/storage/memorystorage/catalog.go
+++ pkg/txn/storage/memorystorage/catalog.go
@@ -103,17 +103,15 @@ func (d *DatabaseRow) AttrByName(handler *MemHandler, tx *Transaction, name stri
 }
 
 type RelationRow struct {
-	ID                ID
-	DatabaseID        ID
-	Name              []byte
-	Type              memoryengine.RelationType
-	Comments          []byte
-	Properties        map[string]string
-	PartitionDef      []byte
-	ViewDef           []byte
-	UniqueIndexDef    []byte
-	SecondaryIndexDef []byte
-	Constraint        []byte
+	ID           ID
+	DatabaseID   ID
+	Name         []byte
+	Type         memoryengine.RelationType
+	Comments     []byte
+	Properties   map[string]string
+	PartitionDef []byte
+	ViewDef      []byte
+	Constraint   []byte
 }
 
 func (r *RelationRow) Key() ID {
