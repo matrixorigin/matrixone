b350ff1d7fbe44b8f5e02539781f0fe1d93a2dd9
xzxiong <xzxiong@yeah.net>
2022-12-13 15:56:54 +0800

    fix HavingBinder

--- pkg/sql/plan/having_binder.go
+++ pkg/sql/plan/having_binder.go
@@ -63,7 +63,7 @@ func (b *HavingBinder) BindExpr(astExpr tree.Expr, depth int32, isRoot bool) (*p
 				},
 			}, nil
 		} else {
-			return nil, moerr.NewInvalidInputNoCtx("nestted aggregate function")
+			return nil, moerr.NewInvalidInput(b.sysCtx, "nestted aggregate function")
 		}
 	}
 
@@ -78,18 +78,18 @@ func (b *HavingBinder) BindColRef(astExpr *tree.UnresolvedName, depth int32, isR
 		}
 
 		if _, ok := expr.Expr.(*plan.Expr_Corr); ok {
-			return nil, moerr.NewNYINoCtx("correlated columns in aggregate function")
+			return nil, moerr.NewNYI(b.sysCtx, "correlated columns in aggregate function")
 		}
 
 		return expr, nil
 	} else {
-		return nil, moerr.NewSyntaxErrorNoCtx("column %q must appear in the GROUP BY clause or be used in an aggregate function", tree.String(astExpr, dialect.MYSQL))
+		return nil, moerr.NewSyntaxError(b.sysCtx, "column %q must appear in the GROUP BY clause or be used in an aggregate function", tree.String(astExpr, dialect.MYSQL))
 	}
 }
 
 func (b *HavingBinder) BindAggFunc(funcName string, astExpr *tree.FuncExpr, depth int32, isRoot bool) (*plan.Expr, error) {
 	if b.insideAgg {
-		return nil, moerr.NewSyntaxErrorNoCtx("aggregate function %s calls cannot be nested", funcName)
+		return nil, moerr.NewSyntaxError(b.sysCtx, "aggregate function %s calls cannot be nested", funcName)
 	}
 
 	b.insideAgg = true
@@ -120,9 +120,9 @@ func (b *HavingBinder) BindAggFunc(funcName string, astExpr *tree.FuncExpr, dept
 
 func (b *HavingBinder) BindWinFunc(funcName string, astExpr *tree.FuncExpr, depth int32, isRoot bool) (*plan.Expr, error) {
 	if b.insideAgg {
-		return nil, moerr.NewSyntaxErrorNoCtx("aggregate function calls cannot contain window function calls")
+		return nil, moerr.NewSyntaxError(b.sysCtx, "aggregate function calls cannot contain window function calls")
 	} else {
-		return nil, moerr.NewSyntaxErrorNoCtx("window %s functions not allowed in having clause", funcName)
+		return nil, moerr.NewSyntaxError(b.sysCtx, "window %s functions not allowed in having clause", funcName)
 	}
 }
 
