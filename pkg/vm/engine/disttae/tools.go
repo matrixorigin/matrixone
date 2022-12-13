e9c47e34b6d613896611dbdca17966563f99613e
badboynt1 <badboynt@126.com>
2022-12-13 16:07:05 +0800

    use a map to search blocklist, instead of exhausitive search

--- pkg/vm/engine/disttae/tools.go
+++ pkg/vm/engine/disttae/tools.go
@@ -1027,11 +1027,10 @@ func genBlockMetas(
 	return metas, nil
 }
 
-func inBlockList(blk BlockMeta, blks []BlockMeta) bool {
-	for i := range blks {
-		if blk.Eq(blks[i]) {
-			return true
-		}
+func inBlockMap(blk BlockMeta, blockMap map[uint64]bool) bool {
+	_, ok := blockMap[blk.Info.BlockID]
+	if ok {
+		return true
 	}
 	return false
 }
@@ -1039,8 +1038,15 @@ func inBlockList(blk BlockMeta, blks []BlockMeta) bool {
 func genModifedBlocks(ctx context.Context, deletes map[uint64][]int, orgs, modfs []BlockMeta,
 	expr *plan.Expr, tableDef *plan.TableDef, proc *process.Process) []ModifyBlockMeta {
 	blks := make([]ModifyBlockMeta, 0, len(orgs)-len(modfs))
+
+	lenblks := len(modfs)
+	blockMap := make(map[uint64]bool, lenblks)
+	for i := 0; i < lenblks; i++ {
+		blockMap[modfs[i].Info.BlockID] = true
+	}
+
 	for i, blk := range orgs {
-		if !inBlockList(blk, modfs) {
+		if !inBlockMap(blk, blockMap) {
 			if needRead(ctx, expr, blk, tableDef, proc) {
 				blks = append(blks, ModifyBlockMeta{
 					meta:    orgs[i],
