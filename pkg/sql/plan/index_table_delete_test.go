dd3a3b28549d20460ea8fc15ea9fcd73dfd6315e
qingxinhome <758355272@qq.com>
2022-12-20 09:45:20 +0800

    Add Unit Tests for Index Table Updates

--- pkg/sql/plan/index_table_delete_test.go
+++ pkg/sql/plan/index_table_delete_test.go
@@ -20,13 +20,6 @@ import (
 
 // only use in developing
 func TestSingleSQLQuery(t *testing.T) {
-	//sql := "delete emp,dept from emp ,dept where emp.deptno = dept.deptno and emp.deptno = 10"
-	//sql := "delete from dept where dname = 'RESEARCH'"
-	//sql := "delete from emp where deptno = 20 order by sal limit 2"
-	//sql := "delete from employees where empno > 7800 order by empno limit 2"
-	//sql := "DELETE FROM employees, dept USING employees INNER JOIN dept WHERE employees.deptno = dept.deptno"
-	//sql := "delete emp from emp left join dept on emp.deptno = dept.deptno where dept.deptno = 20"
-	//sql := "delete emp from emp left join dept on emp.deptno = dept.deptno where emp.sal > 2000"
 	//sql := "update emp set comm = 1200 where deptno = 10"
 	//sql := "update dept set dname = 'XXX' where deptno = 10"
 	//sql := "update emp set sal = sal + 500, comm = 1200 where deptno = 10"
@@ -37,7 +30,20 @@ func TestSingleSQLQuery(t *testing.T) {
 
 	//sql := "update employees set ename = 'XXX', sal = sal + 2000 where empno = 7654"
 	//sql := "update employees set ename = 'XXX' where empno = 7654"
-	sql := "update employees set ename = 'XXX', sal = sal + 2000 where deptno = 10"
+	//sql := "update employees set ename = 'XXX', sal = sal + 2000 where deptno = 10"
+
+	//sql := "update emp,dept set emp.sal = 4000, dept.loc = 'XXX' where emp.deptno = 20"
+	//sql := "update emp t1,dept t2 set t1.sal = 4000, t2.loc = 'XXX' where t1.deptno = 20"
+	//sql := "update emp t1,dept t2 set t1.ename = 'MEIXI', t2.loc = 'XXX' where t1.deptno = 20"
+	//sql := "update emp t1, dept t2 set t1.ename = 'MEIXI', t1.empno = 1234, t2.loc = 'XXX' where t1.deptno = 20"
+	//sql := "update emp,dept set emp.sal = 4000 where emp.deptno = 20"
+
+	//sql := "update emp left join dept on emp.deptno = dept.deptno set emp.sal = 5000, dept.loc = 'YYY'"
+	//sql := "update emp left join dept on emp.deptno = dept.deptno set emp.ename = 'MEIXI', dept.loc = 'YYY'"
+	//sql := "update emp t1 left join dept t2 on t1.deptno = t2.deptno set t1.ename = 'MEIXI', t2.loc = 'YYY'"
+	//sql := "update emp left join dept on emp.deptno = dept.deptno set emp.ename = 'MEIXI', dept.loc = 'YYY' where dept.deptno = 10"
+
+	sql := "update emp, (select deptno from dept) as tx set emp.sal = 3333 where tx.deptno = emp.empno"
 
 	mock := NewMockOptimizer()
 	logicPlan, err := runOneStmt(mock, t, sql)
@@ -153,6 +159,42 @@ func TestSingleTableWithAliasUpdate(t *testing.T) {
 		"update emp t1 set empno = empno + 500, ename = 'LINJUNHONG' where deptno = 10",
 		"update emp t1 set t1.empno = empno + 500, ename = 'LINJUNHONG' where t1.deptno = 10",
 		"update emp t1 set t1.empno = t1.empno + 500, ename = 'LINJUNHONG' where t1.deptno = 10",
+	}
+
+	runTestShouldPass(mock, t, sqls, false, false)
+}
+
+func TestMultiTableUpdate(t *testing.T) {
+	mock := NewMockOptimizer()
+
+	sqls := []string{
+		//1.-----------------ok
+		"update emp,dept set emp.sal = 4000, dept.loc = 'XXX' where emp.deptno = 20",
+		"update emp t1,dept t2 set t1.sal = 4000, t2.loc = 'XXX' where t1.deptno = 20",
+		"update emp t1,dept t2 set t1.ename = 'MEIXI', t2.loc = 'XXX' where t1.deptno = 20",
+		"update emp t1, dept t2 set t1.ename = 'MEIXI', t1.empno = 1234, t2.loc = 'XXX' where t1.deptno = 20",
+		"update emp,dept set emp.sal = 4000 where emp.deptno = 20",
+
+		//2.---------------- cross join-ok
+		"update emp t1, dept t2 set t1.sal = 5000, t2.loc = 'YYY' where t1.deptno = t2.deptno",
+		"update emp t1, dept t2 set t1.empno = 5000, t1.ename = 'CLUO', t2.deptno = 50 where t1.deptno = t2.deptno",
+		"update emp t1, dept t2 set empno = 5000, loc = 'YYY' where t1.deptno = t2.deptno",
+		"update emp t1, dept t2 set t1.sal = 5000, loc = 'YYY' where t1.deptno = t2.deptno",
+
+		//3.--------------- inner join----ok
+		"UPDATE emp t1 inner join dept t2 on t1.deptno = t2.deptno set t1.sal = t1.sal + 500",
+		"UPDATE emp t1 inner join dept t2 on t1.deptno = t2.deptno set t1.sal = t1.sal + 500, ename = 'CLUO'",
+		"UPDATE emp t1 inner join dept t2 on t1.deptno = t2.deptno set t1.sal = t1.sal + 500 where t2.dname = 'RESEARCH'",
+		"UPDATE emp t1 inner join dept t2 on t1.deptno = t2.deptno set t1.empno = t2.deptno + 500 where t2.dname = 'RESEARCH'",
+
+		//4.--------------- left join
+		"update emp left join dept on emp.deptno = dept.deptno set emp.sal = 5000, dept.loc = 'YYY'",
+		"update emp left join dept on emp.deptno = dept.deptno set emp.ename = 'MEIXI', dept.loc = 'YYY'",
+		"update emp t1 left join dept t2 on t1.deptno = t2.deptno set t1.ename = 'MEIXI', t2.loc = 'YYY'",
+		"update emp left join dept on emp.deptno = dept.deptno set emp.ename = 'MEIXI', dept.loc = 'YYY' where dept.deptno = 10",
+
+		//5. ----------------sub query
+		"update emp, (select deptno from dept) as tx set emp.sal = 3333 where tx.deptno = emp.empno",
 	}
 
 	runTestShouldPass(mock, t, sqls, false, false)
