1717381fb4c564464ccf62fa6e65ccf1b3958b80
daviszhen <daviszhen007@gmail.com>
2022-12-27 12:57:47 +0800

    fix deepcopy.go

--- pkg/sql/plan/deepcopy.go
+++ pkg/sql/plan/deepcopy.go
@@ -56,6 +56,7 @@ func DeepCopyNode(node *plan.Node) *plan.Node {
 		UpdateCtxs:      make([]*plan.UpdateCtx, len(node.UpdateCtxs)),
 		TableDefVec:     make([]*plan.TableDef, len(node.TableDefVec)),
 		TblFuncExprList: make([]*plan.Expr, len(node.TblFuncExprList)),
+		ClusterTable:    DeepCopyClusterTable(node.GetClusterTable()),
 	}
 
 	copy(newNode.Children, node.Children)
@@ -430,6 +431,7 @@ func DeepCopyInsertValues(insert *plan.InsertValues) *plan.InsertValues {
 		Columns:       make([]*plan.Column, len(insert.Columns)),
 		OrderAttrs:    make([]string, len(insert.OrderAttrs)),
 		CompositePkey: DeepCopyColDef(insert.CompositePkey),
+		ClusterTable:  DeepCopyClusterTable(insert.GetClusterTable()),
 	}
 
 	for idx, col := range insert.ExplicitCols {
@@ -534,9 +536,10 @@ func DeepCopyDataDefinition(old *plan.DataDefinition) *plan.DataDefinition {
 	case *plan.DataDefinition_DropTable:
 		newDf.Definition = &plan.DataDefinition_DropTable{
 			DropTable: &plan.DropTable{
-				IfExists: df.DropTable.IfExists,
-				Database: df.DropTable.Database,
-				Table:    df.DropTable.Table,
+				IfExists:     df.DropTable.IfExists,
+				Database:     df.DropTable.Database,
+				Table:        df.DropTable.Table,
+				ClusterTable: DeepCopyClusterTable(df.DropTable.GetClusterTable()),
 			},
 		}
 
@@ -573,7 +576,8 @@ func DeepCopyDataDefinition(old *plan.DataDefinition) *plan.DataDefinition {
 	case *plan.DataDefinition_TruncateTable:
 		newDf.Definition = &plan.DataDefinition_TruncateTable{
 			TruncateTable: &plan.TruncateTable{
-				Table: df.TruncateTable.Table,
+				Table:        df.TruncateTable.Table,
+				ClusterTable: DeepCopyClusterTable(df.TruncateTable.GetClusterTable()),
 			},
 		}
 
@@ -745,4 +749,19 @@ func DeepCopyExpr(expr *Expr) *Expr {
 	}
 
 	return newExpr
+}
+
+func DeepCopyClusterTable(cluster *plan.ClusterTable) *plan.ClusterTable {
+	if cluster == nil {
+		return nil
+	}
+
+	accountIds := make([]uint32, len(cluster.GetAccountIDs()))
+	copy(accountIds, cluster.GetAccountIDs())
+	newClusterTable := &plan.ClusterTable{
+		IsClusterTable:         cluster.GetIsClusterTable(),
+		AccountIDs:             accountIds,
+		ColumnIndexOfAccountId: cluster.GetColumnIndexOfAccountId(),
+	}
+	return newClusterTable
 }
