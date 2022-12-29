5efa7da07193e82b74d2cfa4ebebb9ffe2d94c7d
xzxiong <xzxiong@yeah.net>
2022-12-30 00:08:12 +0800

    trsf span

--- pkg/util/export/env/cast_span.go
+++ pkg/util/export/env/cast_span.go
@@ -16,6 +16,9 @@ package env
 
 import (
 	"context"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
 	"github.com/matrixorigin/matrixone/pkg/util/export/table"
 	v11 "go.opentelemetry.io/proto/otlp/common/v1"
 	v1 "go.opentelemetry.io/proto/otlp/resource/v1"
@@ -34,9 +37,11 @@ type Span struct {
 	StartTime    time.Time
 	EndTime      time.Time
 	Duration     uint64
+	Status       *tracepb.Status
 	Resource     *v1.Resource
-	Links        []tracepb.Span_Link
 	Attributes   []*v11.KeyValue
+	Links        []*tracepb.Span_Link
+	Events       []*tracepb.Span_Event
 }
 
 var spanPool = &sync.Pool{New: func() any {
@@ -60,6 +65,16 @@ func (s *Span) CsvFields(ctx context.Context, row *table.Row) []string {
 	row.SetColumnVal(SpansTraceIDCol, s.TraceId)
 	row.SetColumnVal(SpansSpanIDCol, s.SpanId)
 	row.SetColumnVal(SpansParentTraceIDCol, s.ParentSpanId)
+	row.SetColumnVal(SpansSpanKindCol, s.Kind)
+	row.SetColumnVal(SpansSpanNameCol, s.Name)
+	row.SetColumnVal(SpansStartTimeCol, Time2DatetimeString(s.StartTime))
+	row.SetColumnVal(SpansEndTimeCol, Time2DatetimeString(s.EndTime))
+	row.SetColumnVal(SpansDurationCol, fmt.Sprintf("%d", s.Duration))
+	row.SetColumnVal(SpansStatusCol, trsfStatus(s.Status))
+	row.SetColumnVal(SpansResourceCol, trsfResource(s.Resource))
+	row.SetColumnVal(SpansAttributesCol, trsfAttributes(s.Attributes))
+	row.SetColumnVal(SpansLinksCol, trsfSpanLink(s.Links))
+	row.SetColumnVal(SpansEventsCol, trsfEvents(s.Events))
 
 	return row.ToStrings()
 }
@@ -83,4 +98,105 @@ func (s *Span) Free() {
 	s.Links = nil
 	s.Attributes = nil
 	spanPool.Put(s)
+}
+
+func trsfResource(r *v1.Resource) string {
+	return trsfAttributes(r.Attributes)
+}
+
+func trsfAttributes(kvs []*v11.KeyValue) string {
+
+	obj := TransferAttributes(kvs)
+	bytes, err := json.Marshal(&obj)
+	if err != nil {
+		return "{}"
+	}
+	return string(bytes[:])
+}
+
+func trsfStatus(s *tracepb.Status) string {
+	if bytes, err := json.Marshal(s); err != nil {
+		return "{}"
+	} else {
+		return string(bytes[:])
+	}
+	return
+}
+
+func trsfSpanLink(links []*tracepb.Span_Link) string {
+	if bytes, err := json.Marshal(&links); err != nil {
+		return "[]"
+	} else {
+		return string(bytes[:])
+	}
+}
+
+func trsfEvents(events []*tracepb.Span_Event) string {
+	if bytes, err := json.Marshal(&events); err != nil {
+		return "[]"
+	} else {
+		return string(bytes[:])
+	}
+}
+
+func TransferAttributes(attrs []*v11.KeyValue) map[string]any {
+
+	m := make(map[string]any, len(attrs))
+	for _, kv := range attrs {
+
+		key := kv.GetKey()
+		val := kv.GetValue()
+		switch val.Value.(type) {
+		case *v11.AnyValue_StringValue:
+			m[key] = val.GetStringValue()
+		case *v11.AnyValue_BoolValue:
+			m[key] = val.GetBoolValue()
+		case *v11.AnyValue_IntValue:
+			m[key] = val.GetIntValue()
+		case *v11.AnyValue_DoubleValue:
+			m[key] = val.GetDoubleValue()
+		case *v11.AnyValue_BytesValue:
+			m[key] = hex.EncodeToString(val.GetBytesValue())
+		case *v11.AnyValue_ArrayValue:
+			m[key] = TransferArrayValue(val.GetArrayValue())
+		case *v11.AnyValue_KvlistValue:
+			m[key] = TransferAttributes(val.GetKvlistValue().GetValues())
+		default:
+			m[key] = val
+		}
+	}
+	return m
+}
+
+func TransferArrayValue(vals *v11.ArrayValue) []any {
+	if len(vals.Values) == 0 {
+		return nil
+	}
+
+	val := vals.Values[0]
+	arr := make([]any, 0, len(vals.Values))
+
+	switch val.Value.(type) {
+	case *v11.AnyValue_StringValue:
+		for _, v := range vals.Values {
+			arr = append(arr, v.GetStringValue())
+		}
+	case *v11.AnyValue_BoolValue:
+		for _, v := range vals.Values {
+			arr = append(arr, v.GetBoolValue())
+		}
+	case *v11.AnyValue_IntValue:
+		for _, v := range vals.Values {
+			arr = append(arr, v.GetIntValue())
+		}
+	case *v11.AnyValue_DoubleValue:
+		for _, v := range vals.Values {
+			arr = append(arr, v.GetDoubleValue())
+		}
+	default:
+		for _, v := range vals.Values {
+			arr = append(arr, v.GetValue())
+		}
+	}
+	return arr
 }
