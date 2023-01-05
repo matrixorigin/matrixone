f435b7ef5def4f52f0a88fcd8ca4131ed59109a2
qingxinhome <758355272@qq.com>
2023-01-05 18:01:40 +0800

    add bvt test cases

--- test/distributed/cases/ddl/create_index.sql
+++ test/distributed/cases/ddl/create_index.sql
@@ -5,7 +5,22 @@ insert into t1 values(2,"Bob", 25);
 insert into t1 values(3,"Carol", 23);
 insert into t1 values(4,"Dora", 29);
 create unique index idx on t1(name);
+select * from t1;
 drop table t1;
+
+create table t2 (
+col1 bigint primary key,
+col2 varchar(25),
+col3 float,
+col4 varchar(50)
+);
+create unique index idx on t2(col2) comment 'create varchar index';
+insert into t2 values(1,"Abby", 24,'zbcvdf');
+insert into t2 values(2,"Bob", 25,'zbcvdf');
+insert into t2 values(3,"Carol", 23,'zbcvdf');
+insert into t2 values(4,"Dora", 29,'zbcvdf');
+select * from t2;
+drop table t2;
 
 create table t3 (
 col1 bigint primary key,
@@ -13,5 +28,10 @@ col2 varchar(25),
 col3 float,
 col4 varchar(50)
 );
-create unique index idx on t3(col2) comment 'create varchar index';
+create unique index idx on t3(col2,col3);
+insert into t3 values(1,"Abby", 24,'zbcvdf');
+insert into t3 values(2,"Bob", 25,'zbcvdf');
+insert into t3 values(3,"Carol", 23,'zbcvdf');
+insert into t3 values(4,"Dora", 29,'zbcvdf');
+select * from t3;
 drop table t3;
