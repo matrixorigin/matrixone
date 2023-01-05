6ff952f834e443f68fe4c476ddb3b7b85d4735f6
qingxinhome <758355272@qq.com>
2023-01-05 18:07:11 +0800

    add bvt test cases

--- test/distributed/cases/ddl/create_index.sql
+++ test/distributed/cases/ddl/create_index.sql
@@ -34,4 +34,5 @@ insert into t3 values(2,"Bob", 25,'zbcvdf');
 insert into t3 values(3,"Carol", 23,'zbcvdf');
 insert into t3 values(4,"Dora", 29,'zbcvdf');
 select * from t3;
+insert into t3 values(4,"Dora", 29,'zbcvdf');
 drop table t3;
