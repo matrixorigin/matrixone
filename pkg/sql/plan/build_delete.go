daf8cc107a73f7e11cdad8414d4a962795aaa9bb
ouyuanning <52741323@qq.com>
2023-01-06 11:00:29 +0800

    rewrite build single table delete sql

--- pkg/sql/plan/build_delete.go
+++ pkg/sql/plan/build_delete.go
@@ -22,12 +22,14 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 	"github.com/matrixorigin/matrixone/pkg/pb/plan"
 	"github.com/matrixorigin/matrixone/pkg/sql/parsers/dialect"
+	"github.com/matrixorigin/matrixone/pkg/sql/parsers/dialect/mysql"
 	"github.com/matrixorigin/matrixone/pkg/sql/parsers/tree"
 	"github.com/matrixorigin/matrixone/pkg/sql/util"
 )
 
 func buildDelete(stmt *tree.Delete, ctx CompilerContext) (*Plan, error) {
 	if len(stmt.Tables) == 1 && stmt.TableRefs == nil {
+		buildSingleTableDelete2(ctx, stmt)
 		return buildSingleTableDelete(ctx, stmt)
 	}
 	return buildMultTableDelete(ctx, stmt)
@@ -68,18 +70,6 @@ func getTableInfoForUpdate(tableExprs tree.TableExprs, ctx CompilerContext) (*ta
 			return nil, moerr.NewInternalError(ctx.GetContext(), "only the sys account can delete the cluster table %s", tableDef.GetName())
 		}
 
-		// var uniqueIdxDef []*UniqueIndexDef
-		// for _, def := range tableDef.Defs {
-		// 	if idxDef, ok := def.Def.(*plan.TableDef_DefType_UIdx); ok {
-		// 		uniqueIdxDef = append(uniqueIdxDef, idxDef.UIdx)
-		// 	}
-		// }
-
-		// var refTables [][2]string
-		// for _ = range tableDef.RefChildTbls {
-		// 	refTables = append(refTables, [2]string{"dbName", "tblName"})
-		// }
-
 		tblInfo.dbNames[idx] = dbName
 		tblInfo.tableNames[idx] = tblName
 		tblInfo.tableDefs[idx] = tableDef
@@ -91,7 +81,7 @@ func getTableInfoForUpdate(tableExprs tree.TableExprs, ctx CompilerContext) (*ta
 // delete from a1, a2 using t1 as a1 inner join t2 as a2 where a1.id = a2.id
 // select a1.row_id, a2.row_id from t1 as a1 inner join t2 as a2 where a1.id = a2.id
 // select _t.* from (select a1.row_id, a2.row_id from t1 as a1 inner join t2 as a2 where a1.id = a2.id) _t
-func deleteToSelect(node *tree.Delete, dialectType dialect.DialectType, tbls []string) string {
+func deleteToSelect(node *tree.Delete, dialectType dialect.DialectType, info *tableInfo, rewriteInfo *rewriteSelectInfo) string {
 	ctx := tree.NewFmtCtx(dialectType)
 
 	if node.With != nil {
@@ -99,11 +89,16 @@ func deleteToSelect(node *tree.Delete, dialectType dialect.DialectType, tbls []s
 		ctx.WriteByte(' ')
 	}
 	ctx.WriteString("select ")
-
 	prefix := ""
-	for idx := range node.Tables {
+	var column string
+	returnAll := rewriteInfo.projects != ""
+	for _, tbl := range info.tableNames {
 		ctx.WriteString(prefix)
-		column := fmt.Sprintf("%s.%s", tbls[idx], catalog.Row_ID)
+		if returnAll {
+			column = fmt.Sprintf("%s.*", tbl) //有bug，名称重复问题
+		} else {
+			column = fmt.Sprintf("%s.%s", tbl, catalog.Row_ID)
+		}
 		ctx.WriteString(column)
 		prefix = ", "
 	}
@@ -117,6 +112,14 @@ func deleteToSelect(node *tree.Delete, dialectType dialect.DialectType, tbls []s
 	if node.TableRefs != nil {
 		ctx.WriteString(" from ")
 		node.TableRefs.Format(ctx)
+	} else {
+		ctx.WriteString(" from ")
+		prefix := ""
+		for _, tbl := range info.tableNames {
+			ctx.WriteString(prefix)
+			ctx.WriteString(tbl)
+			prefix = ", "
+		}
 	}
 
 	if node.Where != nil {
@@ -134,34 +137,189 @@ func deleteToSelect(node *tree.Delete, dialectType dialect.DialectType, tbls []s
 	return ctx.String()
 }
 
-// // link ref: https://dev.mysql.com/doc/refman/8.0/en/delete.html
-// func buildSingleTableDelete2(ctx CompilerContext, stmt *tree.Delete) (*Plan, error) {
-// 	tblExpr := stmt.Tables[0].(*tree.AliasedTableExpr)
-// 	alias := tblExpr.As
-// 	dbName := string(tblExpr.Expr.(*tree.TableName).SchemaName)
-// 	if dbName == "" {
-// 		dbName = ctx.DefaultDatabase()
-// 	}
-// 	tblName := string(tblExpr.Expr.(*tree.TableName).ObjectName)
+const derivedTableName = "_t_"
 
-// 	objRef, tableDef := ctx.Resolve(dbName, tblName)
-// 	if tableDef == nil {
-// 		return nil, moerr.NewInvalidInput(ctx.GetContext(), "delete has no table def")
-// 	}
-// 	if tableDef.TableType == catalog.SystemExternalRel {
-// 		return nil, moerr.NewInvalidInput(ctx.GetContext(), "cannot delete from external table")
-// 	} else if tableDef.TableType == catalog.SystemViewRel {
-// 		return nil, moerr.NewInvalidInput(ctx.GetContext(), "cannot delete from view")
-// 	}
-// 	if util.TableIsClusterTable(tableDef.GetTableType()) && ctx.GetAccountId() != catalog.System_Account {
-// 		return nil, moerr.NewInternalError(ctx.GetContext(), "only the sys account can delete the cluster table %s", tableDef.GetName())
-// 	}
+type rewriteSelectInfo struct {
+	projects              string
+	leftJoins             string
+	idx                   int
+	onDeleteIdx           []int
+	onDeleteIdxTblName    [][2]string
+	onDeleteRestrict      []int
+	onDeleteRestrictTblId []uint64
+	onDeleteSet           []int
+	onDeleteSetTblId      []uint64
+	onDeleteCascade       []int
+	onDeleteCascadeTblId  []uint64
+}
 
-// 	// delete from t1 where a > 10 limit 10;  -> select row_id from t1 where a > 10 limit 10;
-// 	// delete from t1 a where a.a > 10 limit 10;  -> select a.row_id from t1 a where a.a > 10 limit 10;
+func rewriteDeleteSelect(ctx CompilerContext, tableDef *TableDef, info *rewriteSelectInfo) error {
 
-// 	return nil, nil
-// }
+	// rewrite index
+	for _, def := range tableDef.Defs {
+		if idxDef, ok := def.Def.(*plan.TableDef_DefType_UIdx); ok {
+			for idx, tblName := range idxDef.UIdx.TableNames {
+				if len(idxDef.UIdx.Fields[idx].Parts) == 1 {
+					orginIndexColumnName := idxDef.UIdx.Fields[idx].Parts[0]
+					info.projects += ", `" + tblName + "`." + catalog.Row_ID
+					leftJoinStr := fmt.Sprintf(" left join `%s` on `%s`.%s = `%s`.%s", tblName, tblName, catalog.IndexTableIndexColName, derivedTableName, orginIndexColumnName)
+					info.leftJoins += leftJoinStr
+				} else {
+					orginIndexColumnNames := ""
+					prefix := ""
+					for _, column := range idxDef.UIdx.Fields[idx].Parts {
+						orginIndexColumnNames += fmt.Sprintf(" `%s`.%s%s", derivedTableName, column, prefix)
+						prefix = ","
+					}
+					info.projects += ", `" + tblName + "`." + catalog.Row_ID
+					leftJoinStr := fmt.Sprintf(" left join `%s` on `%s`.%s = serial(%s)", tblName, tblName, catalog.IndexTableIndexColName, orginIndexColumnNames)
+					info.leftJoins += leftJoinStr
+				}
+
+				info.onDeleteIdxTblName = append(info.onDeleteIdxTblName, [2]string{ctx.DefaultDatabase(), tblName})
+				info.onDeleteIdx = append(info.onDeleteIdx, info.idx)
+				info.idx = info.idx + 1
+			}
+		}
+	}
+
+	// rewrite refChild
+	id2name := make(map[uint64]string)
+	for _, col := range tableDef.Cols {
+		id2name[col.ColId] = col.Name
+	}
+
+	for _, tableId := range tableDef.RefChildTbls {
+		_, childTableDef := ctx.ResolveById(tableId) //opt: actionRef是否也记录到RefChildTbls里？
+		for _, fk := range childTableDef.Fkeys {
+			if fk.ForeignTbl == tableDef.TblId {
+				leftJoinStr := ""
+				prefix := fmt.Sprintf(" left join %s on", childTableDef.Name)
+				for i, colId := range fk.Cols {
+					childColumnName := ""
+					originColumnName := ""
+					for _, col := range childTableDef.Cols {
+						if col.ColId == colId {
+							childColumnName = col.Name
+							originColumnName = id2name[fk.ForeignCols[i]]
+							break
+						}
+					}
+					leftJoinStr = fmt.Sprintf("%s %s.%s = %s.%s", prefix, childTableDef.Name, childColumnName, derivedTableName, originColumnName)
+					prefix = ", and"
+				}
+
+				switch fk.OnDelete {
+				case plan.ForeignKeyDef_CASCADE:
+					info.projects += ", " + childTableDef.Name + "." + catalog.Row_ID
+					info.leftJoins += leftJoinStr
+					info.onDeleteCascade = append(info.onDeleteIdx, info.idx)
+					info.idx = info.idx + 1
+					info.onDeleteCascadeTblId = append(info.onDeleteCascadeTblId, childTableDef.TblId)
+
+					err := rewriteDeleteSelect(ctx, childTableDef, info)
+					if err != nil {
+						return err
+					}
+
+				case plan.ForeignKeyDef_NO_ACTION, plan.ForeignKeyDef_RESTRICT:
+					info.projects += ", " + childTableDef.Name + "." + catalog.Row_ID
+					info.leftJoins += leftJoinStr
+					info.onDeleteRestrict = append(info.onDeleteIdx, info.idx)
+					info.idx = info.idx + 1
+					info.onDeleteRestrictTblId = append(info.onDeleteRestrictTblId, childTableDef.TblId)
+
+				case plan.ForeignKeyDef_SET_NULL, plan.ForeignKeyDef_SET_DEFAULT:
+					idMap := make(map[uint64]struct{})
+					for _, colId := range fk.Cols {
+						idMap[colId] = struct{}{}
+					}
+					info.projects += ", " + childTableDef.Name + "." + catalog.Row_ID
+					info.onDeleteSet = append(info.onDeleteIdx, info.idx)
+					info.idx = info.idx + 1
+					for _, col := range childTableDef.Cols {
+						if col.Name == catalog.Row_ID {
+							continue
+						}
+						if _, ok := idMap[col.ColId]; ok {
+							if fk.OnDelete == plan.ForeignKeyDef_SET_NULL {
+								info.projects += ", null"
+							} else {
+								info.projects += ", " + col.Default.OriginString
+							}
+						} else {
+							info.projects += ", " + childTableDef.Name + "." + col.Name
+						}
+						info.onDeleteSet = append(info.onDeleteIdx, info.idx)
+						info.idx = info.idx + 1
+					}
+					info.leftJoins += leftJoinStr
+					info.onDeleteSetTblId = append(info.onDeleteSetTblId, childTableDef.TblId)
+
+					err := rewriteDeleteSelect(ctx, childTableDef, info)
+					if err != nil {
+						return err
+					}
+
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// link ref: https://dev.mysql.com/doc/refman/8.0/en/delete.html
+func buildSingleTableDelete2(ctx CompilerContext, stmt *tree.Delete) (*Plan, error) {
+	tblInfo, err := getTableInfoForUpdate(stmt.Tables, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// delete from t1 where a > 10 limit 10;  -> select row_id from t1 where a > 10 limit 10;
+	// delete from t1 a where a.a > 10 limit 10;  -> select a.row_id from t1 a where a.a > 10 limit 10;
+	rewriteInfo := &rewriteSelectInfo{
+		idx: len(tblInfo.tableNames),
+	}
+	for _, tableDef := range tblInfo.tableDefs {
+		err := rewriteDeleteSelect(ctx, tableDef, rewriteInfo)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	sql := deleteToSelect(stmt, dialect.MYSQL, tblInfo, rewriteInfo)
+	if rewriteInfo.projects != "" {
+		project := "select"
+		for range tblInfo.tableNames {
+			project = project + " " + derivedTableName + "." + catalog.Row_ID //这里有bug，多表的问题
+		}
+		sql = fmt.Sprintf("%s %s from (%s) %s %s", project, rewriteInfo.projects, sql, derivedTableName, rewriteInfo.leftJoins)
+	}
+
+	stmts, err := mysql.Parse(ctx.GetContext(), sql)
+	if err != nil {
+		return nil, err
+	}
+	if len(stmts) > 1 {
+		return nil, moerr.NewInvalidInput(ctx.GetContext(), "cannot prepare multi statements")
+	}
+	builder := NewQueryBuilder(plan.Query_SELECT, ctx)
+	bindCtx := NewBindContext(builder, nil)
+	rootId, err := builder.buildSelect(stmts[0].(*tree.Select), bindCtx, false)
+	builder.qry.Steps = append(builder.qry.Steps, rootId)
+	if err != nil {
+		return nil, err
+	}
+	query, err := builder.createQuery()
+	if err != nil {
+		return nil, err
+	}
+	return &Plan{
+		Plan: &plan.Plan_Query{
+			Query: query,
+		},
+	}, err
+}
 
 // When the original table contains an index table of multi table deletion statement, build a multi table join query
 // execution plan to query the rowid of the original table and the index table respectively
