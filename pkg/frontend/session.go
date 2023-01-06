b6bf1b9690d6d3d707fb266633e7bda7f31c3de0
taofengliu <taofengliu@outlook.com>
2023-01-06 15:14:42 +0800

    resolve conflicts

--- pkg/frontend/session.go
+++ pkg/frontend/session.go
@@ -243,8 +243,8 @@ func NewSession(proto Protocol, mp *mpool.MPool, pu *config.ParameterUnit, gSysV
 			msgs:   make([]string, 0, MoDefaultErrorCount),
 			maxCnt: MoDefaultErrorCount,
 		},
-		cache:    &privilegeCache{},
-		blockIdx: 0,
+		cache:     &privilegeCache{},
+		blockIdx:  0,
 		planCache: newPlanCache(100),
 	}
 	if flag {
@@ -2087,6 +2087,61 @@ func (tcc *TxnCompilerContext) GetProcess() *process.Process {
 	tcc.mu.Lock()
 	defer tcc.mu.Unlock()
 	return tcc.proc
+}
+
+func (tcc *TxnCompilerContext) GetQueryResultMeta(uuid string) ([]*plan.ColDef, string, error) {
+	proc := tcc.proc
+	// get file size
+	fs := objectio.NewObjectFS(proc.FileService, catalog.QueryResultMetaDir)
+	dirs, err := fs.ListDir(catalog.QueryResultMetaDir)
+	if err != nil {
+		return nil, "", err
+	}
+	var size int64 = -1
+	name := catalog.BuildQueryResultMetaName(proc.SessionInfo.Account, uuid)
+	for _, d := range dirs {
+		if d.Name == name {
+			size = d.Size
+		}
+	}
+	if size == -1 {
+		return nil, "", moerr.NewQueryIdNotFound(proc.Ctx, uuid)
+	}
+	// read meta's meta
+	path := catalog.BuildQueryResultMetaPath(proc.SessionInfo.Account, uuid)
+	reader, err := objectio.NewObjectReader(path, proc.FileService)
+	if err != nil {
+		return nil, "", err
+	}
+	bs, err := reader.ReadAllMeta(proc.Ctx, size, proc.Mp())
+	if err != nil {
+		return nil, "", err
+	}
+	idxs := make([]uint16, 2)
+	idxs[0] = catalog.COLUMNS_IDX
+	idxs[1] = catalog.RESULT_PATH_IDX
+	// read meta's data
+	iov, err := reader.Read(proc.Ctx, bs[0].GetExtent(), idxs, proc.Mp())
+	if err != nil {
+		return nil, "", err
+	}
+	// cols
+	vec := vector.New(catalog.MetaColTypes[catalog.COLUMNS_IDX])
+	if err = vec.Read(iov.Entries[0].Object.([]byte)); err != nil {
+		return nil, "", err
+	}
+	def := vector.MustStrCols(vec)[0]
+	r := &plan.ResultColDef{}
+	if err = r.Unmarshal([]byte(def)); err != nil {
+		return nil, "", err
+	}
+	// paths
+	vec = vector.New(catalog.MetaColTypes[catalog.RESULT_PATH_IDX])
+	if err = vec.Read(iov.Entries[1].Object.([]byte)); err != nil {
+		return nil, "", err
+	}
+	str := vector.MustStrCols(vec)[0]
+	return r.ResultCols, str, nil
 }
 
 func (tcc *TxnCompilerContext) SetProcess(proc *process.Process) {
