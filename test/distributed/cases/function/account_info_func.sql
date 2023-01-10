1284d6a0bed5a85fd9a477b229e4f08033b28604
taofengliu <taofengliu@outlook.com>
2023-01-10 14:07:07 +0800

    tests

---
+++ test/distributed/cases/function/account_info_func.sql
@@ -0,0 +1,24 @@
+select current_account_name();
+select current_account_id();
+select current_user_name();
+select current_user_id();
+select current_role_name();
+select current_role_id();
+select * from current_account() as t; 
+
+drop account if exists abc;
+create account abc ADMIN_NAME 'admin' IDENTIFIED BY '123456';
+-- @session:id=1&user=abc:admin&password=123456
+drop role if exists test_role;
+create role test_role;
+grant test_role to admin;
+set role test_role;
+
+select current_account_name();
+select current_account_id();
+select current_user_name();
+select current_user_id();
+select current_role_name();
+select current_role_id();
+select * from current_account() as t;
+-- @session
