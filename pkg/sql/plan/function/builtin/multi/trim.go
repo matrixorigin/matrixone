662184ff00aa6c2128f3a724aa7f8bb83c6ce8af
mooleetzi <13208308200@163.com>
2023-01-20 09:14:48 +0800

    add ut

--- pkg/sql/plan/function/builtin/multi/trim.go
+++ pkg/sql/plan/function/builtin/multi/trim.go
@@ -1,3 +1,17 @@
+// Copyright 2022 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package multi
 
 import (
@@ -9,10 +23,16 @@ import (
 )
 
 func trimBoth(src, cuts string) string {
+	if len(cuts) == 0 {
+		return src
+	}
 	return trimLeading(trimTrailing(src, cuts), cuts)
 }
 
 func trimLeading(src, cuts string) string {
+	if len(cuts) == 0 {
+		return src
+	}
 	for strings.HasPrefix(src, cuts) {
 		src = src[len(cuts):]
 	}
@@ -20,6 +40,9 @@ func trimLeading(src, cuts string) string {
 }
 
 func trimTrailing(src, cuts string) string {
+	if len(cuts) == 0 {
+		return src
+	}
 	for strings.HasSuffix(src, cuts) {
 		src = src[:len(src)-len(cuts)]
 	}
@@ -36,7 +59,7 @@ func Trim(parameters []*vector.Vector, result vector.FunctionResultWrapper, proc
 	case "trailing":
 		return trim(parameters[1:], result, length, trimTrailing)
 	default:
-		return moerr.NewNotSupported(proc.Ctx, "trim", tp)
+		return moerr.NewNotSupported(proc.Ctx, "trim type %s", tp)
 	}
 }
 
