994862ad85e3e83649aef93e5d3dab448b52c124
arjunsk <admin@arjunsk.com>
2023-01-24 19:39:51 -0600

    Working Append and Length

--- pkg/vm/engine/tae/containers/cn_tae_vector_adapter.go
+++ pkg/vm/engine/tae/containers/cn_tae_vector_adapter.go
@@ -15,11 +15,13 @@ import (
 type CnTaeVector[T any] struct {
 	downstreamVector *cnVector.Vector
 	mpool            *mpool.MPool
+	nullable         bool
 }
 
 func NewTaeVector[T any](typ types.Type, nullable bool, opts ...Options) *CnTaeVector[T] {
 	vec := CnTaeVector[T]{
 		downstreamVector: cnVector.New(typ),
+		nullable:         nullable,
 	}
 
 	// mpool
@@ -31,14 +33,13 @@ func NewTaeVector[T any](typ types.Type, nullable bool, opts ...Options) *CnTaeV
 	if alloc == nil {
 		alloc = common.DefaultAllocator
 	}
-
 	vec.mpool = alloc
 
 	return &vec
 }
 
 func (vec CnTaeVector[T]) Get(i int) any {
-	return vec.downstreamVector.GetBytes(int64(i))
+	return cnVector.GetColumn[T](vec.downstreamVector)[i]
 }
 
 func (vec CnTaeVector[T]) Length() int {
@@ -53,38 +54,87 @@ func (vec CnTaeVector[T]) HasNull() bool {
 	return vec.downstreamVector.GetNulls().Any()
 }
 
-func (vec CnTaeVector[T]) String() string {
-	return vec.downstreamVector.String()
-}
-
 func (vec CnTaeVector[T]) Append(v any) {
-	vec.downstreamVector.Append(v, false, mpool.MustNewZero())
+	vec.tryCOW()
+
+	_, isNull := v.(types.Null)
+	if isNull {
+		_ = vec.downstreamVector.Append(types.DefaultVal[T](), true, vec.mpool)
+
+	} else {
+		_ = vec.downstreamVector.Append(v, false, vec.mpool)
+	}
+
 }
 
 func (vec CnTaeVector[T]) AppendMany(vs ...any) {
+	//TODO: Understand what Copy on Write is doing to roStorage and how is it helping?
+	vec.tryCOW()
 	for _, v := range vs {
 		vec.Append(v)
 	}
 }
 
-// --------------- Yet to Implement
-
-func (vec CnTaeVector[T]) IsView() bool {
-	//TODO implement me
-	panic("implement me")
+func (vec CnTaeVector[T]) Nullable() bool {
+	return vec.nullable
 }
 
-func (vec CnTaeVector[T]) Nullable() bool {
-	//TODO implement me
+func (vec CnTaeVector[T]) IsView() bool {
+	// TODO: What is the use of vector view? Can't find any usage.
+	return false
+}
+
+func (vec CnTaeVector[T]) GetAllocator() *mpool.MPool {
+	return vec.mpool
+}
+
+func (vec CnTaeVector[T]) GetView() VectorView {
+	// TODO: As of now, can't find any usage. Do we need it?
 	panic("implement me")
 }
 
 func (vec CnTaeVector[T]) IsNull(i int) bool {
-	//TODO implement me
-	panic("implement me")
+	return vec.downstreamVector.GetNulls() != nil && vec.downstreamVector.GetNulls().Contains(uint64(i))
 }
 
 func (vec CnTaeVector[T]) NullMask() *roaring64.Bitmap {
+	// TODO: Need to modify this
+
+	result := roaring64.New()
+
+	itr := vec.downstreamVector.GetNulls().Np.Iterator()
+	for itr.HasNext() {
+		result.Add(itr.Next())
+	}
+
+	return result
+
+}
+
+func (vec CnTaeVector[T]) GetType() types.Type {
+	return vec.downstreamVector.GetType()
+}
+
+// Issue
+
+func (vec CnTaeVector[T]) String() string {
+	return ""
+	//TODO: some issue
+	//return vec.downstreamVector.String()
+}
+
+func (vec CnTaeVector[T]) Extend(src Vector) {
+	srcVec, _ := src.(CnTaeVector[T])
+	//TODO: Issue
+	err := cnVector.Copy(vec.downstreamVector, srcVec.downstreamVector, 0, 0, vec.mpool)
+	if err != nil {
+		return
+	}
+}
+
+// --------------- Yet to Implement
+
+func (vec CnTaeVector[T]) Update(i int, v any) {
 	//TODO implement me
 	panic("implement me")
 }
@@ -124,14 +174,6 @@ func (vec CnTaeVector[T]) Allocated() int {
 	panic("implement me")
 }
 
-func (vec CnTaeVector[T]) GetAllocator() *mpool.MPool {
-	panic("implement me")
-}
-
-func (vec CnTaeVector[T]) GetType() types.Type {
-	return vec.downstreamVector.GetType()
-}
-
 func (vec CnTaeVector[T]) PPString(num int) string {
 	//TODO implement me
 	panic("implement me")
@@ -157,16 +199,6 @@ func (vec CnTaeVector[T]) ResetWithData(bs *Bytes, nulls *roaring64.Bitmap) {
 	panic("implement me")
 }
 
-func (vec CnTaeVector[T]) GetView() VectorView {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (vec CnTaeVector[T]) Update(i int, v any) {
-	//TODO implement me
-	panic("implement me")
-}
-
 func (vec CnTaeVector[T]) Delete(i int) {
 	panic("implement me")
 }
@@ -177,11 +209,6 @@ func (vec CnTaeVector[T]) Compact(bitmap *roaring.Bitmap) {
 }
 
 func (vec CnTaeVector[T]) AppendNoNulls(s any) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (vec CnTaeVector[T]) Extend(o Vector) {
 	//TODO implement me
 	panic("implement me")
 }
@@ -219,4 +246,8 @@ func (vec CnTaeVector[T]) ReadFrom(r io.Reader) (int64, error) {
 func (vec CnTaeVector[T]) ReadFromFile(file common.IVFile, buffer *bytes.Buffer) error {
 	//TODO implement me
 	panic("implement me")
+}
+
+func (vec CnTaeVector[T]) tryCOW() {
+
 }
--- pkg/vm/engine/tae/containers/cn_tae_vector_adapter_test.go
+++ pkg/vm/engine/tae/containers/cn_tae_vector_adapter_test.go
@@ -10,10 +10,16 @@ func TestAppend(t *testing.T) {
 
 	opt := withAllocator(Options{})
 	vec := MakeVector(types.T_int32.ToType(), false, opt)
-
 	vec.Append(int32(12))
 	vec.Append(int32(32))
-	assert.Equal(t, 2, vec.Length())
+	assert.False(t, vec.Nullable())
+	vec.AppendMany(int32(1), int32(100))
+	assert.Equal(t, 4, vec.Length())
+	assert.Equal(t, int32(12), vec.Get(0).(int32))
+	assert.Equal(t, int32(32), vec.Get(1).(int32))
+	assert.Equal(t, int32(1), vec.Get(2).(int32))
+	assert.Equal(t, int32(100), vec.Get(3).(int32))
 
 	vec.Close()
+
 }
