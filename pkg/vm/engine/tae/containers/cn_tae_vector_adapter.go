cd02d2244b29a615bef74e768f3b04c55cc36d0d
arjunsk <admin@arjunsk.com>
2023-01-27 23:13:23 -0600

    Updated Adapter to support first testcase.

--- pkg/vm/engine/tae/containers/cn_tae_vector_adapter.go
+++ pkg/vm/engine/tae/containers/cn_tae_vector_adapter.go
@@ -2,12 +2,16 @@ package containers
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/RoaringBitmap/roaring"
 	"github.com/RoaringBitmap/roaring/roaring64"
+	"github.com/matrixorigin/matrixone/pkg/common/bitmap"
 	"github.com/matrixorigin/matrixone/pkg/common/mpool"
+	cnNulls "github.com/matrixorigin/matrixone/pkg/container/nulls"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	cnVector "github.com/matrixorigin/matrixone/pkg/container/vector"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/common"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/stl"
 	"io"
 	"unsafe"
 )
@@ -55,7 +59,7 @@ func (vec CnTaeVector[T]) HasNull() bool {
 }
 
 func (vec CnTaeVector[T]) Append(v any) {
-	vec.tryCOW()
+	//TODO: is it required --- vec.tryCOW()
 
 	_, isNull := v.(types.Null)
 	if isNull {
@@ -68,8 +72,8 @@ func (vec CnTaeVector[T]) Append(v any) {
 }
 
 func (vec CnTaeVector[T]) AppendMany(vs ...any) {
-	//TODO: Understand what Copy on Write is doing to roStorage and how is it helping?
-	vec.tryCOW()
+
+	//TODO: Understand what Copy on Write is doing to roStorage and how is it helping? vec.tryCOW()
 	for _, v := range vs {
 		vec.Append(v)
 	}
@@ -100,51 +104,136 @@ func (vec CnTaeVector[T]) IsNull(i int) bool {
 func (vec CnTaeVector[T]) NullMask() *roaring64.Bitmap {
 	// TODO: Need to modify this
 
-	result := roaring64.New()
+	Nsp := vec.downstreamVector.GetNulls().Np
+	return BitmapToRoaring64Bitmap(Nsp)
+}
 
-	itr := vec.downstreamVector.GetNulls().Np.Iterator()
+func BitmapToRoaring64Bitmap(input *bitmap.Bitmap) *roaring64.Bitmap {
+	result := roaring64.New()
+	itr := input.Iterator()
 	for itr.HasNext() {
 		result.Add(itr.Next())
 	}
-
 	return result
+}
 
+func Roaring64ToBitmap(input *roaring64.Bitmap) *bitmap.Bitmap {
+	result := bitmap.New(int(input.GetCardinality()))
+	itr := input.Iterator()
+	for itr.HasNext() {
+		result.Add(itr.Next())
+	}
+	return result
 }
 
 func (vec CnTaeVector[T]) GetType() types.Type {
 	return vec.downstreamVector.GetType()
 }
 
-// Issue
+func (vec CnTaeVector[T]) Allocated() int {
+	return vec.downstreamVector.Size()
+}
 
 func (vec CnTaeVector[T]) String() string {
-	return ""
-	//TODO: some issue
-	//return vec.downstreamVector.String()
+	s := ""
+	end := 100
+	if vec.Length() < end {
+		end = vec.Length()
+	}
+
+	data := "Vals=["
+	for i := 0; i < end; i++ {
+		data = fmt.Sprintf("%s %v", data, vec.Get(i))
+	}
+	if vec.Length() > end {
+		s = fmt.Sprintf("%s %s...]", s, data)
+	} else {
+		s = fmt.Sprintf("%s %s]", s, data)
+	}
+
+	return s
 }
 
 func (vec CnTaeVector[T]) Extend(src Vector) {
-	srcVec, _ := src.(CnTaeVector[T])
-	//TODO: Issue
-	err := cnVector.Copy(vec.downstreamVector, srcVec.downstreamVector, 0, 0, vec.mpool)
-	if err != nil {
+	vec.ExtendWithOffset(src, 0, src.Length())
+}
+
+// Issues
+
+func (vec CnTaeVector[T]) ExtendWithOffset(src Vector, srcOff, srcLen int) {
+
+	if srcLen <= 0 {
 		return
 	}
+
+	if vec.downstreamVector.GetType().IsVarlen() {
+		bs := src.Bytes()
+		for i := srcOff; i < srcOff+srcLen; i++ {
+			vec.Append(any(bs.GetVarValueAt(i)).(T))
+		}
+		return
+	}
+
+	slice := unsafe.Slice((*T)(src.SlicePtr()), srcOff+srcLen)
+	for i := srcOff; i < srcOff+srcLen; i++ {
+		vec.Append(any(slice[i]).(T))
+	}
+}
+
+func (vec CnTaeVector[T]) Bytes() *Bytes {
+	bs := stl.NewBytesWithTypeSize(-types.VarlenaSize)
+	bs.Header = cnVector.GetColumn[types.Varlena](vec.downstreamVector)
+	bs.Storage = vec.downstreamVector.GetArea()
+
+	return bs
+}
+
+func (vec CnTaeVector[T]) Data() []byte {
+	panic("unimpl")
+	//TODO: wrong impl
+	length := vec.downstreamVector.Length()
+	var buffer []byte
+	for i := 0; i < length; i++ {
+		buffer = append(buffer, vec.downstreamVector.GetBytes(int64(i))...)
+	}
+
+	return buffer
+}
+
+func (vec CnTaeVector[T]) ResetWithData(bs *Bytes, nulls *roaring64.Bitmap) {
+	panic("Unimpl")
+
+	if vec.Nullable() {
+		vec.downstreamVector.Nsp = &cnNulls.Nulls{Np: Roaring64ToBitmap(nulls)}
+	}
+	vec.downstreamVector.Col = bs.Header
+	vec.downstreamVector.Read(bs.Storage)
 }
 
 // --------------- Yet to Implement
 
+func (vec CnTaeVector[T]) Delete(i int) {
+	panic("implement me")
+
+	//vec.downstreamVector.Col = append(vec.slice[:i], vec.slice[i+1:]...)
+	//size := len(vec.buf) - stl.Sizeof[T]()
+	//vec.buf = vec.buf[:size]
+	//
+	//nulls := impl.derived.nulls
+	//max := nulls.Maximum()
+	//if max < uint64(i) {
+	//	impl.vecBase.Delete(i)
+	//	return
+	//} else if max == uint64(i) {
+	//	nulls.Remove(uint64(i))
+	//	impl.vecBase.Delete(i)
+	//	return
+	//}
+	//
+	//return
+}
+
 func (vec CnTaeVector[T]) Update(i int, v any) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (vec CnTaeVector[T]) Data() []byte {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (vec CnTaeVector[T]) Bytes() *Bytes {
 	//TODO implement me
 	panic("implement me")
 }
@@ -156,7 +245,7 @@ func (vec CnTaeVector[T]) Slice() any {
 
 func (vec CnTaeVector[T]) SlicePtr() unsafe.Pointer {
 	//TODO implement me
-	panic("implement me")
+	return cnVector.GetPtrAt(vec.downstreamVector, 0)
 }
 
 func (vec CnTaeVector[T]) DataWindow(offset, length int) []byte {
@@ -165,11 +254,6 @@ func (vec CnTaeVector[T]) DataWindow(offset, length int) []byte {
 }
 
 func (vec CnTaeVector[T]) Capacity() int {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (vec CnTaeVector[T]) Allocated() int {
 	//TODO implement me
 	panic("implement me")
 }
@@ -194,26 +278,12 @@ func (vec CnTaeVector[T]) Reset() {
 	panic("implement me")
 }
 
-func (vec CnTaeVector[T]) ResetWithData(bs *Bytes, nulls *roaring64.Bitmap) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (vec CnTaeVector[T]) Delete(i int) {
-	panic("implement me")
-}
-
 func (vec CnTaeVector[T]) Compact(bitmap *roaring.Bitmap) {
 	//TODO implement me
 	panic("implement me")
 }
 
 func (vec CnTaeVector[T]) AppendNoNulls(s any) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (vec CnTaeVector[T]) ExtendWithOffset(src Vector, srcOff, srcLen int) {
 	//TODO implement me
 	panic("implement me")
 }
@@ -246,8 +316,4 @@ func (vec CnTaeVector[T]) ReadFrom(r io.Reader) (int64, error) {
 func (vec CnTaeVector[T]) ReadFromFile(file common.IVFile, buffer *bytes.Buffer) error {
 	//TODO implement me
 	panic("implement me")
-}
-
-func (vec CnTaeVector[T]) tryCOW() {
-
 }
