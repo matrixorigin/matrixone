d208b934af05af7518df3debf6d010466ef3dace
JackTan25 <jacktby@gmail.com>
2023-02-02 15:10:30 +0800

    add IsRemote Judge

--- pkg/sql/colexec/insert/insert.go
+++ pkg/sql/colexec/insert/insert.go
@@ -674,7 +674,7 @@ func writeBatch(ctx context.Context,
 		// remember that, pkIdx can hold cpkeys, or single col pk, or clusterBy key
 		// but it can't hold them above towo of them , that means it can only hold \
 		// one of them at most.
-		if len(n.container.pkIndex) == 0 {
+		if n.IsRemote && len(n.container.pkIndex) == 0 {
 			for i := oldLen; i < newLen; i++ {
 				n.container.pkIndex = append(n.container.pkIndex, i)
 			}
