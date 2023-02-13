71ec6945b299d547ee6f5f54dc658577a2e590fd
GuokeCui <jingjinglake@gmail.com>
2023-02-13 18:33:20 +0800

    Update segmentation logic according to PR #7931 (#8000)
    Enlarge reserved size for morpc internal header in order to make chunks pass.
    
    Approved by: @XuPeng-SH

--- pkg/vm/engine/tae/logtail/service/service_test.go
+++ pkg/vm/engine/tae/logtail/service/service_test.go
@@ -54,6 +54,7 @@ func TestService(t *testing.T) {
 		WithServerCollectInterval(500*time.Millisecond),
 		WithServerSendTimeout(5*time.Second),
 		WithServerEnableChecksum(true),
+		WithServerMaxMessageSize(32+reservedMorpcHeaderSize+7),
 		WithServerPayloadCopyBufferSize(16*mpool.KB),
 		WithServerMaxLogtailFetchFailure(5),
 	)
