52604a5f5c8970b78c27a9ecfc311c2bf5837199
GuokeCui <jingjinglake@gmail.com>
2023-02-16 17:26:34 +0800

    FIX: transfer timeout instead under asynchronous condition;

---
+++ pkg/vm/engine/tae/logtail/service/utils_test.go
@@ -0,0 +1,40 @@
+// Copyright 2021 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package service
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestContextTimeout(t *testing.T) {
+	backoff := time.Second
+	{
+		timeout := ContextTimeout(context.Background(), backoff)
+		t.Log("timeout:", timeout.Seconds())
+		require.Equal(t, backoff, timeout)
+	}
+
+	{
+		ctx, cancel := context.WithTimeout(context.Background(), 2*backoff)
+		defer cancel()
+		timeout := ContextTimeout(ctx, backoff)
+		t.Log("timeout:", timeout.Seconds())
+		require.NotEqual(t, backoff, timeout)
+	}
+}
