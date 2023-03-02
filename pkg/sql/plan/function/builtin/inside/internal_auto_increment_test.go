5995471052281c6afc6df06866603767e01b53dc
qingxinhome <758355272@qq.com>
2023-03-02 21:41:59 +0800

    add license header

--- pkg/sql/plan/function/builtin/inside/internal_auto_increment_test.go
+++ pkg/sql/plan/function/builtin/inside/internal_auto_increment_test.go
@@ -1,7 +1,26 @@
+// Copyright 2021 - 2022 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package inside
 
 import (
 	"context"
+	"reflect"
+	"sync"
+	"testing"
+	"time"
+
 	"github.com/golang/mock/gomock"
 	"github.com/matrixorigin/matrixone/pkg/compress"
 	"github.com/matrixorigin/matrixone/pkg/container/batch"
@@ -13,10 +32,6 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/testutil"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine"
 	"github.com/stretchr/testify/require"
-	"reflect"
-	"sync"
-	"testing"
-	"time"
 )
 
 func TestInternalAutoIncrement(t *testing.T) {
