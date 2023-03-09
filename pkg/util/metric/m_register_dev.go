4bb012f06caa38847cb56bf724cc83c506c7d4b0
arjunsk <arjunsk@users.noreply.github.com>
2023-03-09 13:39:42 -0600

    Fixed Duplicated Collector Registration Error

--- pkg/util/metric/m_register_dev.go
+++ pkg/util/metric/m_register_dev.go
@@ -1,13 +1,20 @@
 package metric
 
-import prom "github.com/prometheus/client_golang/prometheus"
+import (
+	"github.com/matrixorigin/matrixone/pkg/logutil"
+	prom "github.com/prometheus/client_golang/prometheus"
+)
 
 var (
 	DefaultDevMetricRegistry = prom.NewRegistry()
 )
 
 func RegisterDevMetric(c prom.Collector) {
-	DefaultDevMetricRegistry.MustRegister(c)
+	if err := DefaultDevMetricRegistry.Register(c); err != nil {
+		// err is either registering a collector more than once or metrics have duplicate description.
+		// in any case, we respect the existing collectors in the prom registry
+		logutil.Debugf("[Metric] register to prom register: %v", err)
+	}
 }
 
 var FullyQualifiedName = func(name string) string {
