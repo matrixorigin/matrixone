314d8ea3bfe88c07b1dc255fb94c98214f6020ac
arjunsk <arjunsk@users.noreply.github.com>
2023-03-09 14:12:10 -0600

    Added Comments for GlobalDevMetricRegistry

--- pkg/util/metric/m_register_dev.go
+++ pkg/util/metric/m_register_dev.go
@@ -9,6 +9,7 @@ var (
 	DefaultDevMetricRegistry = prom.NewRegistry()
 )
 
+// RegisterDevMetric We register collector to global dev metric registry. Similar to https://pkg.go.dev/github.com/prometheus/client_golang/prometheus#Register
 func RegisterDevMetric(c prom.Collector) {
 	if err := DefaultDevMetricRegistry.Register(c); err != nil {
 		// err is either registering a collector more than once or metrics have duplicate description.
