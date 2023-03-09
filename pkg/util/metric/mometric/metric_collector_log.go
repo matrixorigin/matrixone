68a3ebf06220b4aad7d6c9d5d8e381a92b34609a
arjunsk <arjunsk@users.noreply.github.com>
2023-03-09 14:52:41 -0600

    init Metric Collector Log.

--- pkg/util/metric/mometric/metric_collector_log.go
+++ pkg/util/metric/mometric/metric_collector_log.go
@@ -2,6 +2,7 @@ package mometric
 
 import (
 	"context"
+	"github.com/matrixorigin/matrixone/pkg/logutil"
 	pb "github.com/matrixorigin/matrixone/pkg/pb/metric"
 	bp "github.com/matrixorigin/matrixone/pkg/util/batchpipe"
 )
@@ -16,7 +17,18 @@ var _ MetricCollector = new(metricLogCollector)
 var _ bp.PipeImpl[*pb.MetricFamily, string] = new(metricLogCollector)
 
 func newMetricLogCollector(opts ...collectorOpt) MetricCollector {
-	return &metricLogCollector{}
+	initOpts := defaultCollectorOpts()
+	for _, o := range opts {
+		o.ApplyTo(&initOpts)
+	}
+	c := &metricLogCollector{
+		opts: initOpts,
+	}
+	pipeOpts := []bp.BaseBatchPipeOpt{bp.PipeWithBatchWorkerNum(c.opts.sqlWorkerNum), bp.PipeWithBufferWorkerNum(1)}
+	base := bp.NewBaseBatchPipe[*pb.MetricFamily, string](c, pipeOpts...)
+	c.BaseBatchPipe = base
+	return c
+
 }
 
 func (m *metricLogCollector) SendMetrics(ctx context.Context, mfs []*pb.MetricFamily) error {
@@ -28,12 +40,18 @@ func (m *metricLogCollector) SendMetrics(ctx context.Context, mfs []*pb.MetricFa
 	return nil
 }
 
-func (m *metricLogCollector) NewItemBuffer(name string) bp.ItemBuffer[*pb.MetricFamily, string] {
-	//TODO implement me
-	panic("implement me")
+func (m *metricLogCollector) NewItemBuffer(_ string) bp.ItemBuffer[*pb.MetricFamily, string] {
+	//TODO: Check if I can reuse mfset by modifying some parts of it.
+	// I believe mfset is used only metricCollector (deprecated) and not in metricFsCollector.
+	return &mfset{
+		Reminder:        bp.NewConstantClock(m.opts.flushInterval),
+		metricThreshold: m.opts.metricThreshold,
+		sampleThreshold: m.opts.sampleThreshold,
+	}
 }
 
 func (m *metricLogCollector) NewItemBatchHandler(ctx context.Context) func(batch string) {
-	//TODO implement me
-	panic("implement me")
+	return func(batch string) {
+		logutil.Info(batch)
+	}
 }
