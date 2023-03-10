66420999b8e39c75d5fa33c2f10978847a75a67b
ou yuanning <45346669+ouyuanning@users.noreply.github.com>
2023-03-10 14:57:47 +0800

    more support in on duplicate key clause (#8276)
    support unique key/ multi primary key/multi unique key   for on duplicate key clause
    
    Approved by: @aressu1985, @m-schen, @nnsgmsone, @aunjgr

--- pkg/sql/compile/operator.go
+++ pkg/sql/compile/operator.go
@@ -511,6 +511,8 @@ func constructOnduplicateKey(n *plan.Node, eg engine.Engine, proc *process.Proce
 		OnDuplicateExpr: oldCtx.OnDuplicateExpr,
 		Source:          originRel,
 		UniqueSource:    indexRels,
+
+		IdxIdx: oldCtx.IdxIdx,
 	}, nil
 }
 
