ec37676da55bcb5da52b6b7e10c74e0937688f36
zhangxu <zhangxu19830126@gmail.com>
2023-03-16 17:36:43 +0800

    fix tests

--- pkg/lockservice/service_remote_test.go
+++ pkg/lockservice/service_remote_test.go
@@ -152,7 +152,7 @@ func TestLockBlockedOnRemote(t *testing.T) {
 				close(c)
 			}()
 			select {
-			case <-time.After(time.Second):
+			case <-time.After(time.Second * 10):
 				break
 			case <-c:
 				assert.Fail(t, "can not get remote lock")
