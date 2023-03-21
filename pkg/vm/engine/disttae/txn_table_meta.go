fa66a05288ea18433c361841eff173de598dab40
nnsgmsone <nnsmgsone@outlook.com>
2023-03-21 10:04:15 +0800

    Fix the delete problem of workspace (#8534)
    Approved by: @m-schen

--- pkg/vm/engine/disttae/txn_table_meta.go
+++ pkg/vm/engine/disttae/txn_table_meta.go
@@ -17,38 +17,11 @@ package disttae
 import (
 	"context"
 
-	"github.com/matrixorigin/matrixone/pkg/catalog"
 	"github.com/matrixorigin/matrixone/pkg/pb/plan"
-	"github.com/matrixorigin/matrixone/pkg/vm/engine"
 )
 
 func (tbl *txnTable) updateMeta(ctx context.Context, expr *plan.Expr) error {
-
-	primaryKeys := make([]*engine.Attribute, 0, 1)
-	if tbl.primaryIdx >= 0 {
-		for _, def := range tbl.defs {
-			attr, ok := def.(*engine.AttributeDef)
-			if !ok {
-				continue
-			}
-			if !attr.Attr.Primary {
-				continue
-			}
-			primaryKeys = append(primaryKeys, &attr.Attr)
-		}
-	}
-
-	dnList := needSyncDnStores(ctx, expr, tbl.tableDef, primaryKeys,
-		tbl.db.txn.dnStores, tbl.db.txn.proc)
-	switch tbl.tableId {
-	case catalog.MO_DATABASE_ID,
-		catalog.MO_TABLES_ID,
-		catalog.MO_COLUMNS_ID:
-		tbl.dnList = []int{0}
-	default:
-		tbl.dnList = dnList
-	}
-
+	tbl.dnList = []int{0}
 	_, created := tbl.db.txn.createMap.Load(genTableKey(ctx, tbl.tableName, tbl.db.databaseId))
 	if !created && !tbl.updated {
 		if tbl.db.txn.engine.UsePushModelOrNot() {
@@ -65,7 +38,6 @@ func (tbl *txnTable) updateMeta(ctx context.Context, expr *plan.Expr) error {
 				return err
 			}
 		}
-
 		columnLength := len(tbl.tableDef.Cols) - 1 //we use this data to fetch zonemap, but row_id has no zonemap
 		meta, err := tbl.db.txn.getTableMeta(ctx, tbl.db.databaseId, tbl.tableId, true, columnLength, false)
 		if err != nil {
@@ -74,6 +46,5 @@ func (tbl *txnTable) updateMeta(ctx context.Context, expr *plan.Expr) error {
 		tbl.meta = meta
 		tbl.updated = true
 	}
-
 	return nil
 }
