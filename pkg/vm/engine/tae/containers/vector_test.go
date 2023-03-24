247a03f8eca27a5894298323b08bd93b7b0e3859
arjunsk <arjunsk@users.noreply.github.com>
2023-03-23 19:42:30 -0500

    Removed unwanted alloc from vector

--- pkg/vm/engine/tae/containers/vector_test.go
+++ pkg/vm/engine/tae/containers/vector_test.go
@@ -31,22 +31,6 @@ func withAllocator(opt Options) Options {
 	return opt
 }
 
-// func checkFullyEqualVector(t *testing.T, v1, v2 Vector) {
-// 	checkEqualVector(t, v1, v2)
-// 	assert.Equal(t, v1.Capacity(), v2.Capacity())
-// 	assert.Equal(t, v1.Allocated(), v2.Allocated())
-// }
-
-// func checkEqualVector(t *testing.T, v1, v2 Vector) {
-// 	assert.Equal(t, v1.GetType(), v2.GetType())
-// 	assert.Equal(t, v1.Length(), v2.Length())
-// 	assert.Equal(t, v1.HasNull(), v2.HasNull())
-// 	assert.Equal(t, v1.Nullable(), v2.Nullable())
-// 	for i := 0; i < v1.Length(); i++ {
-// 		assert.Equal(t, v1.Get(i), v2.Get(i))
-// 	}
-// }
-
 func TestVectorShallowForeach(t *testing.T) {
 	defer testutils.AfterTest(t)()
 	opt := withAllocator(Options{})
