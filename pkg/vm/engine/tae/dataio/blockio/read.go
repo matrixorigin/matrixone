4322ac6b9603f76b0dac339feb3d61b0e209e8bd
GreatRiver <2552853833@qq.com>
2023-03-25 21:21:48 +0800

    Workaround for fix # 8684 (#8686)
    Approved by: @XuPeng-SH

--- pkg/vm/engine/tae/dataio/blockio/read.go
+++ pkg/vm/engine/tae/dataio/blockio/read.go
@@ -17,6 +17,7 @@ package blockio
 import (
 	"context"
 	"github.com/matrixorigin/matrixone/pkg/container/nulls"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/containers"
 	"time"
 
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
@@ -82,11 +83,14 @@ func BlockReadInner(
 	}
 	// remove rows from columns
 	for i, col := range columnBatch.Vecs {
-		if col.GetType().Oid != types.T_Rowid {
-			columnBatch.Vecs[i], err = col.Dup(pool)
-		}
+		// Fixme: Due to # 8684, we are not able to use mpool yet
+		columnBatch.Vecs[i] = containers.CloneMOVec(col)
 		if err != nil {
 			return nil, err
+		}
+		if col.GetType().Oid == types.T_Rowid {
+			// rowid need free
+			col.Free(pool)
 		}
 		if len(deleteRows) > 0 {
 			columnBatch.Vecs[i].Shrink(deleteRows, true)
