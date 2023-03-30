e4c2838309b56bdef9a19e577713002a31c5dee7
iceTTTT <74845916+iceTTTT@users.noreply.github.com>
2023-03-30 20:24:04 +0800

    Remove bvt tag. (#8781)
    Approved by: @heni02

--- test/distributed/cases/sequence/create_sequence.sql
+++ test/distributed/cases/sequence/create_sequence.sql
@@ -159,11 +159,11 @@ drop sequence seq_15;
 drop sequence seq_non;
 
 --prepare
--- @bvt:issue#8724
+create sequence seq_15;
 create sequence seq_16 increment 10 start with 20 no cycle;
 truncate table seq_table_01;
 prepare stmt1 from 'insert into seq_table_01 values(?)';
-set @a_var = nextval('seq15');
+set @a_var = nextval('seq_15');
 execute stmt1 using @a_var;
 select * from seq_table_01;
 execute stmt1 using @a_var;
@@ -171,6 +171,7 @@ select * from seq_table_01;
 execute stmt1 using @a_var;
 select * from seq_table_01;
 drop sequence seq_16;
+drop sequence seq_15;
 -- @bvt:issue
 --lastval and setval
 create sequence seq_17 increment 10 start with 20 no cycle;
