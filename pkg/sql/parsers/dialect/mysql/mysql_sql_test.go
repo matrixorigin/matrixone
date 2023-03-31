473673ad80e8f8fc862062e5ef350d75d8b9f5c0
iceTTTT <74845916+iceTTTT@users.noreply.github.com>
2023-03-31 19:05:28 +0800

    Fix binary bug (#8790)
    Approved by: @ouyuanning, @heni02, @iamlinjunhong, @aunjgr

--- pkg/sql/parsers/dialect/mysql/mysql_sql_test.go
+++ pkg/sql/parsers/dialect/mysql/mysql_sql_test.go
@@ -2095,6 +2095,10 @@ var (
 			output: "call test1(@session, @increment)",
 		},
 		{
+			input:  "select cast(123 as binary)",
+			output: "select cast(123 as binary)",
+		},
+		{
 			input:  "select BINARY 124",
 			output: "select binary(124)",
 		},
