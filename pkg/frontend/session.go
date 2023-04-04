8c06208f7410aee34e381b6900b1a81d08971619
wjc <329512443@qq.com>
2023-04-04 10:46:26 +0800

    Remove seq init of bgsession

--- pkg/frontend/session.go
+++ pkg/frontend/session.go
@@ -322,6 +322,10 @@ func NewSession(proto Protocol, mp *mpool.MPool, pu *config.ParameterUnit, gSysV
 		ses.userDefinedVars = make(map[string]interface{})
 		ses.prepareStmts = make(map[string]*PrepareStmt)
 		ses.statsCache = plan2.NewStatsCache()
+		// For seq init values.
+		ses.seqCurValues = make(map[uint64]string)
+		ses.seqLastValue = ""
+		ses.sqlHelper = &SqlHelper{ses: ses}
 	}
 	ses.flag = flag
 	ses.uuid, _ = uuid.NewUUID()
@@ -329,12 +333,6 @@ func NewSession(proto Protocol, mp *mpool.MPool, pu *config.ParameterUnit, gSysV
 	ses.GetTxnCompileCtx().SetSession(ses)
 	ses.GetTxnHandler().SetSession(ses)
 	ses.SetAutoIncrCacheManager(aicm)
-
-	// For seq init values.
-	ses.seqCurValues = make(map[uint64]string)
-	ses.seqLastValue = ""
-
-	ses.sqlHelper = &SqlHelper{ses: ses}
 
 	var err error
 	if ses.mp == nil {
