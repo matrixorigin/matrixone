60cc92b3e476e51e6acab91379eff4f88330e57a
badboynt1 <badboynt@126.com>
2023-04-11 16:48:01 +0800

    fix

--- pkg/sql/plan/agg_pushdown_pullup.go
+++ pkg/sql/plan/agg_pushdown_pullup.go
@@ -222,7 +222,6 @@ func addAnyValue(expr *plan.Expr, agg *plan.Node, builder *QueryBuilder) {
 			break
 		}
 	}
-	
 	if idx == -1 {
 		idx = len(agg.AggList)
 		anyValueExpr, _ := bindFuncExprImplByPlanExpr(builder.compCtx.GetContext(), "any_value", []*plan.Expr{DeepCopyExpr(expr)})
