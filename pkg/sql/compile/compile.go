ef408958e27a7a7609c8e3a2a4f49051bf94807d
triump2020 <triump2020@outlook.com>
2023-04-13 15:01:31 +0800

    remove the comments

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -1908,8 +1908,6 @@ func (c *Compile) generateNodes(n *plan.Node) (engine.Nodes, error) {
 	for i := 0; i < len(ranges); i += step {
 		j := i / step
 		if i+step >= len(ranges) {
-			//FIXME::just for debug
-			//if c.cnList[j].Addr == c.addr {
 			if isSameCN(c.cnList[j].Addr, c.addr) {
 				if len(nodes) == 0 {
 					nodes = append(nodes, engine.Node{
@@ -1929,8 +1927,6 @@ func (c *Compile) generateNodes(n *plan.Node) (engine.Nodes, error) {
 				})
 			}
 		} else {
-			//FIXME::just for debug
-			//if c.cnList[j].Addr == c.addr {
 			if isSameCN(c.cnList[j].Addr, c.addr) {
 				if len(nodes) == 0 {
 					nodes = append(nodes, engine.Node{
