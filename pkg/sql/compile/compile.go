eb3ae3823e749d219f2e5367f30d61f38b02fffc
Boyu Tan <60096118+JackTan25@users.noreply.github.com>
2023-04-18 17:35:56 +0800

    Cn liveness check (#9035)
    Approved by: @nnsgmsone

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -18,10 +18,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net"
 	"runtime"
 	"strings"
 	"sync"
 	"sync/atomic"
+
+	"github.com/matrixorigin/matrixone/pkg/cnservice/cnclient"
 
 	"github.com/matrixorigin/matrixone/pkg/catalog"
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
@@ -398,7 +401,30 @@ func (c *Compile) compileAttachedScope(ctx context.Context, attachedPlan *plan.P
 
 func (c *Compile) compileQuery(ctx context.Context, qry *plan.Query) ([]*Scope, error) {
 	var err error
+	client := cnclient.GetRPCClient()
 	c.cnList, err = c.e.Nodes()
+	if client != nil {
+		for i := 0; i < len(c.cnList); i++ {
+			addrs := strings.Split(c.cnList[i].Addr, ":")
+			if len(addrs) != 2 {
+				logutil.Warnf("compileScope received a malformed cn address '%s', expected 'ip:port'", c.cnList[i].Addr)
+			}
+			// InValid Addr, this should be docker addr,
+			// "cn-0","cn-1", just skip it
+			if address := net.ParseIP(addrs[0]); address == nil {
+				continue
+			}
+			if isSameCN(c.addr, c.cnList[i].Addr) {
+				continue
+			}
+			err := client.Ping(ctx, c.cnList[i].Addr)
+			// ping failed
+			if err != nil {
+				c.cnList = append(c.cnList[:i], c.cnList[i+1:]...)
+				i--
+			}
+		}
+	}
 	if err != nil {
 		return nil, err
 	}
