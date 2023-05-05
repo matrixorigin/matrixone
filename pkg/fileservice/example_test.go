60c8ebde0042106aae1572079fe9898986230506
arjunsk <arjunsk@users.noreply.github.com>
2023-05-04 21:23:44 -0500

    Updated E2E tests

--- pkg/fileservice/example_test.go
+++ pkg/fileservice/example_test.go
@@ -14,111 +14,57 @@
 
 package fileservice
 
-//
-//func TestCacheWithRCExample(t *testing.T) {
-//	dir := t.TempDir()
-//	fs, err := NewLocalFS(
-//		"rc",
-//		dir,
-//		CacheConfig{
-//			MemoryCapacity: 32 << 20,
-//		},
-//		nil,
-//	)
-//	assert.Nil(t, err)
-//
-//	ctx := context.Background()
-//
-//	// write
-//	err = fs.Write(ctx, IOVector{
-//		FilePath: "foo",
-//		Entries: []IOEntry{
-//			{
-//				Data: []byte("42"),
-//				Size: 2,
-//			},
-//		},
-//	})
-//	assert.Nil(t, err)
-//
-//	// read
-//	vec := &IOVector{
-//		FilePath: "foo",
-//		Entries: []IOEntry{
-//			{
-//				Offset: 0,
-//				Size:   2,
-//				ToObjectBytes: func(_ io.Reader, data []byte) (any, int64, error) {
-//					i, err := strconv.Atoi(string(data))
-//					assert.Nil(t, err)
-//					return objcache.NewRCValue(i), 8, nil
-//				},
-//			},
-//		},
-//	}
-//	err = fs.Read(ctx, vec)
-//	assert.Nil(t, err)
-//
-//	value := vec.Entries[0].ObjectBytes.(*objcache.RCValue[int])
-//	assert.Equal(t, 42, value.Value)
-//
-//	value.IncRef()       // pin, cache will not evict this item
-//	defer value.DecRef() // unpin
-//
-//}
-//
-//func TestCacheWithReleasableExample(t *testing.T) {
-//	dir := t.TempDir()
-//	fs, err := NewLocalFS(
-//		"rc",
-//		dir,
-//		CacheConfig{
-//			MemoryCapacity: 32 << 20,
-//		},
-//		nil,
-//	)
-//	assert.Nil(t, err)
-//
-//	ctx := context.Background()
-//	pool := NewPool(64, func() []byte {
-//		return make([]byte, 1024)
-//	}, nil, nil)
-//
-//	// write
-//	err = fs.Write(ctx, IOVector{
-//		FilePath: "foo",
-//		Entries: []IOEntry{
-//			{
-//				Data: []byte("42"),
-//				Size: 2,
-//			},
-//		},
-//	})
-//	assert.Nil(t, err)
-//
-//	// read
-//	vec := &IOVector{
-//		FilePath: "foo",
-//		Entries: []IOEntry{
-//			{
-//				Offset: 0,
-//				Size:   2,
-//				ToObjectBytes: func(_ io.Reader, data []byte) (any, int64, error) {
-//					// allocs from pool
-//					copied, put := pool.Get()
-//					copied = copied[:copy(copied, data)]
-//					return objcache.NewReleasableValue(copied, func() {
-//						// return to pool when evict
-//						put()
-//					}), int64(len(copied)), nil
-//				},
-//			},
-//		},
-//	}
-//	err = fs.Read(ctx, vec)
-//	assert.Nil(t, err)
-//
-//	value := vec.Entries[0].ObjectBytes.(objcache.ReleasableValue[[]byte])
-//	assert.Equal(t, []byte("42"), value.Value)
-//
-//}
+import (
+	"context"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCacheExample(t *testing.T) {
+	dir := t.TempDir()
+	fs, err := NewLocalFS(
+		"rc",
+		dir,
+		CacheConfig{
+			MemoryCapacity: 32 << 20,
+		},
+		nil,
+	)
+	assert.Nil(t, err)
+
+	ctx := context.Background()
+
+	// write
+	err = fs.Write(ctx, IOVector{
+		FilePath: "foo",
+		Entries: []IOEntry{
+			{
+				Data: []byte("42"),
+				Size: 2,
+			},
+		},
+	})
+	assert.Nil(t, err)
+
+	// read
+	vec := &IOVector{
+		FilePath: "foo",
+		Entries: []IOEntry{
+			{
+				Offset: 0,
+				Size:   2,
+				ToObjectBytes: func(_ io.Reader, data []byte) ([]byte, int64, error) {
+					return []byte("42"), 8, nil
+				},
+			},
+		},
+	}
+	err = fs.Read(ctx, vec)
+	assert.Nil(t, err)
+
+	value := vec.Entries[0].ObjectBytes
+	assert.Equal(t, []byte("42"), value)
+
+}
