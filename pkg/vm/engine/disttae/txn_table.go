f3bb48e0f991ffb2cd51c60d9bc09915fc63aace
XuPeng-SH <xupeng3112@163.com>
2023-05-05 12:29:02 +0800

    [disttae]: remove some useless and time-consuming code (#9253)
    Approved by: @nnsgmsone

--- pkg/vm/engine/disttae/txn_table.go
+++ pkg/vm/engine/disttae/txn_table.go
@@ -103,11 +103,6 @@ func (tbl *txnTable) Rows(ctx context.Context) (rows int64, err error) {
 			if _, ok := deletes[entry.RowID]; ok {
 				continue
 			}
-			if tbl.skipBlocks != nil {
-				if _, ok := tbl.skipBlocks[entry.BlockID]; ok {
-					continue
-				}
-			}
 			rows++
 		}
 		iter.Close()
@@ -252,7 +247,6 @@ func (tbl *txnTable) Ranges(ctx context.Context, expr *plan.Expr) (ranges [][]by
 
 	ranges = make([][]byte, 0, 1)
 	ranges = append(ranges, []byte{})
-	tbl.skipBlocks = make(map[types.Blockid]uint8)
 	if len(tbl.blockInfos) == 0 {
 		return
 	}
@@ -310,7 +304,6 @@ func (tbl *txnTable) Ranges(ctx context.Context, expr *plan.Expr) (ranges [][]by
 		}
 		var meta objectio.ObjectMeta
 		for _, blk := range blks {
-			tbl.skipBlocks[blk.BlockID] = 0
 			ok := true
 			if exprMono {
 				location := blk.MetaLocation()
@@ -944,7 +937,6 @@ func (tbl *txnTable) newReader(
 		typsMap:         mp,
 		inserts:         inserts,
 		deletes:         deletes,
-		skipBlocks:      tbl.skipBlocks,
 		iter:            iter,
 		colIdxMp:        colIdxMp,
 		extendId2s3File: make(map[string]int),
