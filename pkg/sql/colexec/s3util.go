bfb29e419935799f455c11fddc7e7c7ac8613924
gaviny <gyue@outlook.com>
2023-05-11 23:44:38 -0700

    try : adjust TagS3Size 2m

--- pkg/sql/colexec/s3util.go
+++ pkg/sql/colexec/s3util.go
@@ -74,7 +74,7 @@ const (
 	// trigger write s3
 	WriteS3Threshold uint64 = 64 * mpool.MB
 
-	TagS3Size uint64 = 4 * mpool.MB
+	TagS3Size uint64 = 2 * mpool.MB
 )
 
 func (w *S3Writer) Free(proc *process.Process) {
