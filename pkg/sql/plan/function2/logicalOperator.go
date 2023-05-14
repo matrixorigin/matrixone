b932490f9a5f72941f536d71e3546dfd507bc0c1
chenmingsong <mschenjj@foxmail.com>
2023-05-14 18:40:34 +0800

    optimize logical operator

--- pkg/sql/plan/function2/logicalOperator.go
+++ pkg/sql/plan/function2/logicalOperator.go
@@ -24,6 +24,62 @@ func andFn(parameters []*vector.Vector, result vector.FunctionResultWrapper, _ *
 	p2 := vector.GenerateFunctionFixedTypeParameter[bool](parameters[1])
 	rs := vector.MustFunctionResult[bool](result)
 
+	if parameters[0].IsConst() {
+		v1, null1 := p1.GetValue(0)
+		if null1 {
+			for i := uint64(0); i < uint64(length); i++ {
+				v2, null2 := p2.GetValue(i)
+				if err := rs.Append(v2, v2 || null2); err != nil {
+					return err
+				}
+			}
+		} else {
+			if v1 {
+				for i := uint64(0); i < uint64(length); i++ {
+					v2, null2 := p2.GetValue(i)
+					if err := rs.Append(v2, null2); err != nil {
+						return err
+					}
+				}
+			} else {
+				for i := uint64(0); i < uint64(length); i++ {
+					if err := rs.Append(false, false); err != nil {
+						return err
+					}
+				}
+			}
+		}
+		return nil
+	}
+
+	if parameters[1].IsConst() {
+		v2, null2 := p2.GetValue(0)
+		if null2 {
+			for i := uint64(0); i < uint64(length); i++ {
+				v1, null1 := p1.GetValue(i)
+				if err := rs.Append(v1, v1 || null1); err != nil {
+					return err
+				}
+			}
+		} else {
+			if v2 {
+				for i := uint64(0); i < uint64(length); i++ {
+					v1, null1 := p1.GetValue(i)
+					if err := rs.Append(v1, null1); err != nil {
+						return err
+					}
+				}
+			} else {
+				for i := uint64(0); i < uint64(length); i++ {
+					if err := rs.Append(false, false); err != nil {
+						return err
+					}
+				}
+			}
+		}
+		return nil
+	}
+
 	for i := uint64(0); i < uint64(length); i++ {
 		v1, null1 := p1.GetValue(i)
 		v2, null2 := p2.GetValue(i)
@@ -51,6 +107,86 @@ func orFn(parameters []*vector.Vector, result vector.FunctionResultWrapper, _ *p
 	p2 := vector.GenerateFunctionFixedTypeParameter[bool](parameters[1])
 	rs := vector.MustFunctionResult[bool](result)
 
+	if parameters[0].IsConst() {
+		v1, null1 := p1.GetValue(0)
+		if v1 {
+			for i := uint64(0); i < uint64(length); i++ {
+				if err := rs.Append(true, false); err != nil {
+					return err
+				}
+			}
+		} else {
+			if null1 {
+				for i := uint64(0); i < uint64(length); i++ {
+					v2, _ := p2.GetValue(i)
+					if v2 {
+						if err := rs.Append(true, false); err != nil {
+							return err
+						}
+					} else {
+						if err := rs.Append(false, true); err != nil {
+							return err
+						}
+					}
+				}
+			} else {
+				for i := uint64(0); i < uint64(length); i++ {
+					v2, null2 := p2.GetValue(i)
+					if v2 {
+						if err := rs.Append(true, false); err != nil {
+							return err
+						}
+					} else {
+						if err := rs.Append(false, null2); err != nil {
+							return err
+						}
+					}
+				}
+			}
+		}
+		return nil
+	}
+
+	if parameters[1].IsConst() {
+		v2, null2 := p2.GetValue(0)
+		if v2 {
+			for i := uint64(0); i < uint64(length); i++ {
+				if err := rs.Append(true, false); err != nil {
+					return err
+				}
+			}
+		} else {
+			if null2 {
+				for i := uint64(0); i < uint64(length); i++ {
+					v1, _ := p1.GetValue(i)
+					if v1 {
+						if err := rs.Append(true, false); err != nil {
+							return err
+						}
+					} else {
+						if err := rs.Append(false, true); err != nil {
+							return err
+						}
+					}
+				}
+			} else {
+				for i := uint64(0); i < uint64(length); i++ {
+					v1, null1 := p1.GetValue(i)
+					if v1 {
+						if err := rs.Append(true, false); err != nil {
+							return err
+						}
+					} else {
+						if err := rs.Append(false, null1); err != nil {
+							return err
+						}
+					}
+				}
+			}
+		}
+		return nil
+	}
+
 	for i := uint64(0); i < uint64(length); i++ {
 		v1, null1 := p1.GetValue(i)
 		v2, null2 := p2.GetValue(i)
@@ -72,6 +208,44 @@ func xorFn(parameters []*vector.Vector, result vector.FunctionResultWrapper, _ *
 	p1 := vector.GenerateFunctionFixedTypeParameter[bool](parameters[0])
 	p2 := vector.GenerateFunctionFixedTypeParameter[bool](parameters[1])
 	rs := vector.MustFunctionResult[bool](result)
+
+	if parameters[0].IsConst() {
+		v1, null1 := p1.GetValue(0)
+		if null1 {
+			for i := uint64(0); i < uint64(length); i++ {
+				if err := rs.Append(false, true); err != nil {
+					return err
+				}
+			}
+		} else {
+			for i := uint64(0); i < uint64(length); i++ {
+				v2, null2 := p2.GetValue(i)
+				if err := rs.Append(v1 != v2, null2); err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	}
+
+	if parameters[1].IsConst() {
+		v2, null2 := p2.GetValue(0)
+		if null2 {
+			for i := uint64(0); i < uint64(length); i++ {
+				if err := rs.Append(false, true); err != nil {
+					return err
+				}
+			}
+		} else {
+			for i := uint64(0); i < uint64(length); i++ {
+				v1, null1 := p1.GetValue(i)
+				if err := rs.Append(v2 != v1, null1); err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	}
 
 	for i := uint64(0); i < uint64(length); i++ {
 		v1, null1 := p1.GetValue(i)
