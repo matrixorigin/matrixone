40236e641e6b1c57c5d3ff96ed2dd5dc08289ac4
jianwan0214 <jianwan0214@gmail.com>
2023-05-17 21:40:06 +0800

    fix sql_source_type bvt error

--- test/distributed/cases/sql_source_type/sql_source_type.sql
+++ test/distributed/cases/sql_source_type/sql_source_type.sql
@@ -1,14 +1,15 @@
 -- prepare
 create account if not exists `sql_source_type` ADMIN_NAME 'admin' IDENTIFIED BY '123456';
-
+-- @session:id=1&user=sql_source_type:admin:accountadmin&password=123456
+create database if not exists ssb;
+use ssb;
 /* cloud_user */drop table if exists __mo_t1;
 /* cloud_nonuser */ create table __mo_t1(a int);
 insert into __mo_t1 values(1);
 select * from __mo_t1;
 /* cloud_nonuser */ use system;/* cloud_user */show tables;
--- @session:id=1&user=sql_source_type:admin:accountadmin&password=123456
-select sleep(15);
 -- @session
-select statement, sql_source_type from system.statement_info where user="dump" order by request_at desc limit 4;
+select sleep(15);
+select statement, sql_source_type from system.statement_info where user="admin" order by request_at desc limit 4;
 
 drop account if exists sql_source_type;
