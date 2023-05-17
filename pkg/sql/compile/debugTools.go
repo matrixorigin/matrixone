723075e150071c55156e232f35dcfd2e7f84b1ae
nitao <badboynt@126.com>
2023-05-17 19:51:25 +0800

    fix a bug that shuffle group output wrong results on multi cn occasionally (#9508)
    shuffle group must make sure that all data sent to a fixed reg on the same CN.
    
    Approved by: @m-schen, @dongdongyang33, @nnsgmsone

--- pkg/sql/compile/debugTools.go
+++ pkg/sql/compile/debugTools.go
@@ -201,7 +201,7 @@ func debugShowScopes(ss []*Scope, gap int, rmp map[*process.WaitRegister]int) st
 					}
 				}
 				switch arg.FuncId {
-				case dispatch.ShuffleToAllFunc, dispatch.ShuffleToAllLocalFunc:
+				case dispatch.ShuffleToAllFunc:
 					str += fmt.Sprintf(" shuffle to all of MergeReceiver [%s].", chs)
 				case dispatch.SendToAllFunc, dispatch.SendToAllLocalFunc:
 					str += fmt.Sprintf(" to all of MergeReceiver [%s].", chs)
