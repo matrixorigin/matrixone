8734fcb8d9a87555608f3e9e563981b36839516e
xzxiong <xzxiong@yeah.net>
2023-05-18 11:16:47 +0800

    fix case sql_source_type

--- test/distributed/cases/sql_source_type/sql_source_type.sql
+++ test/distributed/cases/sql_source_type/sql_source_type.sql
@@ -1,14 +1,20 @@
 -- prepare
 create account if not exists `sql_source_type` ADMIN_NAME 'admin' IDENTIFIED BY '123456';
 
+-- testcase
+-- @session:id=1&user=sql_source_type:admin:accountadmin&password=123456
+create database if not exists ssb;
+use ssb;
 /* cloud_user */drop table if exists __mo_t1;
 /* cloud_nonuser */ create table __mo_t1(a int);
 insert into __mo_t1 values(1);
 select * from __mo_t1;
 /* cloud_nonuser */ use system;/* cloud_user */show tables;
--- @session:id=1&user=sql_source_type:admin:accountadmin&password=123456
-select sleep(16);
 -- @session
-select statement, sql_source_type from system.statement_info where user="dump" and statement not like '%mo_ctl%' order by request_at desc limit 4;
 
+-- result check
+select sleep(16);
+select statement, sql_source_type from system.statement_info where account="sql_source_type" order by request_at desc limit 4;
+
+-- cleanup
 drop account if exists sql_source_type;
