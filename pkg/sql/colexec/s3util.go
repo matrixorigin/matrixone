7d77cb16e7f411ecf3a0bb455647be69062d7ee9
gaviny <gyue@outlook.com>
2023-05-19 17:50:56 -0700

    change s3 threshold only for mo_logger - try 256k

--- pkg/sql/colexec/s3util.go
+++ pkg/sql/colexec/s3util.go
@@ -76,7 +76,7 @@ const (
 	WriteS3Threshold uint64 = 64 * mpool.MB
 
 	TagS3Size            uint64 = 10 * mpool.MB
-	TagS3SizeForMOLogger        = 512 * mpool.KB
+	TagS3SizeForMOLogger        = 256 * mpool.KB
 )
 
 func (w *S3Writer) Free(proc *process.Process) {
