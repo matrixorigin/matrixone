c4d9978af6411aabfcaf4d3e74c9ad9c4cbe6302
aptend <aptend@hotmail.com>
2023-05-21 14:20:20 +0800

    remove log

--- pkg/vm/engine/tae/tables/txnentries/mergeblocks.go
+++ pkg/vm/engine/tae/tables/txnentries/mergeblocks.go
@@ -19,7 +19,6 @@ import (
 	"time"
 
 	"github.com/RoaringBitmap/roaring"
-	"github.com/matrixorigin/matrixone/pkg/logutil"
 	"github.com/matrixorigin/matrixone/pkg/objectio"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/catalog"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/common"
@@ -251,12 +250,6 @@ func (entry *mergeBlocksEntry) PrepareCommit() (err error) {
 			return err
 		}
 		blks[i] = blk
-	}
-
-	for i, dels := range entry.deletes {
-		if dels != nil {
-			logutil.Infof("%d deletes=%v", i, dels.ToArray())
-		}
 	}
 
 	skippedCnt := 0
