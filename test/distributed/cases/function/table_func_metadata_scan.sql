3560a8fb8c39b04c0b973d58e005a858d6dd5388
Wu Qinxuan <wuqinxuan33@gmail.com>
2023-05-23 14:45:07 +0800

    add metadata scan bvt case
    Signed-off-by: Wu Qinxuan <wuqinxuan33@gmail.com>

--- test/distributed/cases/function/table_func_metadata_scan.sql
+++ test/distributed/cases/function/table_func_metadata_scan.sql
@@ -1,3 +1,4 @@
+select * from metadata_scan('table_func_metadata_scan.no_exist_table', '*') g;
 drop table if exists t;
 create table t(a int, b varchar);
 insert into t values(1, null);
@@ -18,6 +19,7 @@ select count(*) from t;
 -- @sleep:10
 select count(*) from metadata_scan('table_func_metadata_scan.t', '*') g;
 select count(*) from metadata_scan('table_func_metadata_scan.t', 'a') g;
+select count(*) from metadata_scan('table_func_metadata_scan.t', 'c') g;
 select column_name, rows_count, null_count, compress_size, origin_size from metadata_scan('table_func_metadata_scan.t', 'a') g;
 select column_name, rows_count, null_count, compress_size, origin_size from metadata_scan('table_func_metadata_scan.t', '*') g;
 select sum(compress_size) from metadata_scan('table_func_metadata_scan.t', '*') g;
