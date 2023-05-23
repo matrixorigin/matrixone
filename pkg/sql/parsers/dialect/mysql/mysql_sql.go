a917ed577fcfd5f23ecbd302b308497e3fb76222
jiangxinmeng <jiangxinmeng@sjtu.edu.cn>
2023-05-24 05:28:28 +0800

    fix merge

--- pkg/sql/parsers/dialect/mysql/mysql_sql.go
+++ pkg/sql/parsers/dialect/mysql/mysql_sql.go
@@ -10268,7 +10268,7 @@ yydefault:
 //line mysql_sql.y:2615
 		{
 			assignments := []*tree.VarAssignmentExpr{
-				{
+				&tree.VarAssignmentExpr{
 					System: true,
 					Global: true,
 					Name:   yyDollar[6].str,
