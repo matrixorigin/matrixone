90c907c7791ae8030ce863d8e12dc434d28f22c5
gaviny <gyue@outlook.com>
2023-05-24 10:58:02 -0700

    try s3 threshold 16m and debug log level, add a lock
    Do not check the txn timestamp
    
    S3 write threshold to 1m

--- pkg/sql/colexec/s3util.go
+++ pkg/sql/colexec/s3util.go
@@ -75,7 +75,7 @@ type S3Writer struct {
 const (
 	// WriteS3Threshold when batches'  size of table reaches this, we will
 	// trigger write s3
-	WriteS3Threshold uint64 = 16 * mpool.MB
+	WriteS3Threshold uint64 = 1 * mpool.MB
 
 	TagS3Size            uint64 = 10 * mpool.MB
 	TagS3SizeForMOLogger        = 256 * mpool.KB
