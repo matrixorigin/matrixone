3dc847ab93dd8a8c2b5e8e8032945e3d162420de
gaviny <gyue@outlook.com>
2023-05-25 12:28:49 -0700

    adjust tests in txnmgr.go = fix bug

--- pkg/vm/engine/tae/txn/txnbase/txnmgr.go
+++ pkg/vm/engine/tae/txn/txnbase/txnmgr.go
@@ -295,7 +295,7 @@ func (mgr *TxnManager) onPrePrepare(op *OpTxn) {
 	mgr.CommitListener.OnBeginPrePrepare(op.Txn)
 	defer mgr.CommitListener.OnEndPrePrepare(op.Txn)
 	// If txn is trying committing, call txn.PrePrepare()
-	now := time.Now()
+	//now := time.Now()
 	op.Txn.SetError(op.Txn.PrePrepare())
 	common.DoIfDebugEnabled(func() {
 		logutil.Debug("gavin error")
