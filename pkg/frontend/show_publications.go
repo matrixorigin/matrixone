8090b818bcd962852bf7de116e92c998e6ebaef7
daviszhen <daviszhen007@gmail.com>
2023-05-26 19:01:16 +0800

    update

--- pkg/frontend/show_publications.go
+++ pkg/frontend/show_publications.go
@@ -150,7 +150,7 @@ func doShowCreatePublications(ctx context.Context, ses *Session, scp *tree.ShowC
 	ses.SetMysqlResultSet(rs)
 	err = bh.Exec(ctx, "commit;")
 	if err != nil {
-		goto handleFailed
+		return err
 	}
 	return nil
 handleFailed:
--- pkg/frontend/show_subsriptions.go
+++ pkg/frontend/show_subsriptions.go
@@ -107,7 +107,7 @@ func doShowSubscriptions(ctx context.Context, ses *Session, sp *tree.ShowSubscri
 	ses.SetMysqlResultSet(rs)
 	err = bh.Exec(ctx, "commit;")
 	if err != nil {
-		goto handleFailed
+		return err
 	}
 	return nil
 handleFailed:
