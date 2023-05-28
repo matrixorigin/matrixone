cec8ffcbcc450e2ca7837032e95a5b0f0e633e17
gaviny <gyue@outlook.com>
2023-05-27 17:51:15 -0700

    totally disable the collect

--- pkg/util/export/batch_processor.go
+++ pkg/util/export/batch_processor.go
@@ -318,8 +318,8 @@ func (c *MOCollector) Collect(ctx context.Context, item batchpipe.HasName) error
 	case <-c.stopCh:
 		ctx = errutil.ContextWithNoReport(ctx, true)
 		return moerr.NewInternalError(ctx, "MOCollector stopped")
-	case c.awakeCollect <- item:
-		return nil
+	//case c.awakeCollect <- item:
+	//	return nil
 	default:
 		return nil
 	}
