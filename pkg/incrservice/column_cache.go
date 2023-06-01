c3df6de77d0e9f05450a1698c922013a61154e1f
zhangxu <zhangxu19830126@gmail.com>
2023-06-01 21:11:39 +0800

    add comments

--- pkg/incrservice/column_cache.go
+++ pkg/incrservice/column_cache.go
@@ -387,6 +387,9 @@ func (col *columnCache) waitPrevAllocatingLocked(ctx context.Context) error {
 			return nil
 		}
 		c := col.allocatingC
+		// we must unlock here, becase we may wait for a long time. And Lock will added
+		// before return, because the caller holds the lock and call this method and use
+		// defer to unlock.
 		col.Unlock()
 		select {
 		case <-ctx.Done():
