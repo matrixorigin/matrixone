03ca309fc8a54e80fe25c4c7a42eea273a756aa8
gaviny <gyue@outlook.com>
2023-06-02 10:07:13 -0700

    change log level

--- pkg/sql/compile/scope.go
+++ pkg/sql/compile/scope.go
@@ -16,8 +16,9 @@ package compile
 
 import (
 	"context"
-	"github.com/matrixorigin/matrixone/pkg/vm/engine/disttae"
 	"hash/crc32"
+
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/disttae"
 
 	"github.com/matrixorigin/matrixone/pkg/catalog"
 	"github.com/matrixorigin/matrixone/pkg/cnservice/cnclient"
@@ -181,8 +182,7 @@ func (s *Scope) RemoteRun(c *Compile) error {
 
 // ParallelRun try to execute the scope in parallel way.
 func (s *Scope) ParallelRun(c *Compile, remote bool) error {
-	logutil.Infof("---->ParallelRun---> %s \n", DebugShowScopes([]*Scope{s}))
-	//fmt.Printf("---->ParallelRun---> %s \n", DebugShowScopes([]*Scope{s}))
+	logutil.Debugf("---->ParallelRun---> %s \n", DebugShowScopes([]*Scope{s}))
 
 	var rds []engine.Reader
 
