81e61b7e5dc3811db11af8b45d4ecbd9300dd77c
reus <reusee@gmail.com>
2023-06-04 23:32:28 +0800

    logtailreplay: add exact, prefix, min-max primary key iter

--- pkg/vm/engine/disttae/txn_table.go
+++ pkg/vm/engine/disttae/txn_table.go
@@ -1366,7 +1366,7 @@ func (tbl *txnTable) newReader(
 	if len(encodedPrimaryKey) > 0 {
 		iter = state.NewPrimaryKeyIter(
 			types.TimestampToTS(ts),
-			encodedPrimaryKey,
+			logtailreplay.Exact(encodedPrimaryKey),
 		)
 	} else {
 		iter = state.NewRowsIter(
