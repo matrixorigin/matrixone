39fde981911ed8cbce5f2045972e7298d2accbb5
maomao <77312370+sukki37@users.noreply.github.com>
2023-06-07 21:36:34 +0800

    Add restart test job in CI (#9903)
    Add restart test job in CI
    
    Approved by: @guguducken, @aressu1985

--- test/distributed/cases/query_result/query_result.sql
+++ test/distributed/cases/query_result/query_result.sql
@@ -9,7 +9,9 @@ select count(*) from meta_scan(last_query_id()) as u;
 set global save_query_result = off;
 
 select * from tt;
+-- @bvt:issue#9886
 select * from result_scan(last_query_id()) as u;
+-- @bvt:issue
 set global save_query_result = on;
 drop table if exists t2;
 create table t2 (a int, b int, c int);
