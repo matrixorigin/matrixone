67184ac45cfd815ebad3eb8150302a20d04f79ba
w-zr <weiziran125@gmail.com>
2023-06-09 17:39:33 +0800

    print hakeeper state when bootstrap

--- pkg/hakeeper/rsm.go
+++ pkg/hakeeper/rsm.go
@@ -26,8 +26,10 @@ import (
 	"github.com/lni/dragonboat/v4/logger"
 	sm "github.com/lni/dragonboat/v4/statemachine"
 	"github.com/mohae/deepcopy"
+	"go.uber.org/zap"
 
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
+	"github.com/matrixorigin/matrixone/pkg/common/runtime"
 	pb "github.com/matrixorigin/matrixone/pkg/pb/logservice"
 	"github.com/matrixorigin/matrixone/pkg/pb/metadata"
 )
@@ -386,6 +388,8 @@ func (s *stateMachine) handleSetStateCmd(cmd []byte) sm.Result {
 	case pb.HAKeeperBootstrapCommandsReceived:
 		if state == pb.HAKeeperBootstrapFailed || state == pb.HAKeeperRunning {
 			s.state.State = state
+			runtime.DefaultRuntime().Logger().Info("HAKeeper is ready",
+				zap.String("state", s.state.String()))
 			return sm.Result{}
 		}
 		return re()
