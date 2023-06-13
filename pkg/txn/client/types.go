e2d7f660070392ee02cbf900a5b7873cec04b6a2
zhangxu <zhangxu19830126@gmail.com>
2023-06-13 12:00:17 +0800

    fix logtail related bugs

--- pkg/txn/client/types.go
+++ pkg/txn/client/types.go
@@ -162,14 +162,21 @@ func SetupRuntimeTxnOptions(
 // In the Push mode of LogTail's Event, the DN pushes the logtail to the subscribed
 // CN once a transaction has been Committed. So there is a actual wait (last push commit
 // ts >= start ts). This is unfriendly to TP, so we can lose some freshness and use the
-// latest commit ts received from the current DN pushg as the start ts of the transaction,
+// latest commit ts received from the current DN push as the start ts of the transaction,
 // which eliminates this physical wait.
 type TimestampWaiter interface {
+	// Epoch returns the epoch of current cn apply dn's logtail. Epoch will changed when
+	// cn and dn's logtail connection is broken, and UpdateEpoch will be called.
+	Epoch() uint64
+	// UpdateEpoch update the epoch of current cn apply dn's logtail.
+	UpdateEpoch(epoch uint64)
 	// GetTimestamp get the latest commit ts as snapshot ts of the new txn. It will keep
 	// blocking if latest commit timestamp received from DN is less than the given value.
-	GetTimestamp(context.Context, timestamp.Timestamp) (timestamp.Timestamp, error)
-	// NotifyLatestCommitTS notify the latest timestamp that received from DN
-	NotifyLatestCommitTS(timestamp.Timestamp)
+	GetTimestamp(context.Context, timestamp.Timestamp) (uint64, timestamp.Timestamp, error)
+	// NotifyLatestCommitTS notify the latest timestamp that received from DN. A applied logtail
+	// commit ts is corresponds to an epoch. Whenever the connection of logtail of cn and dn is
+	// reset, the epoch will be reset and all the ts of the old epoch should be invalidated.
+	NotifyLatestCommitTS(appliedTS timestamp.Timestamp)
 	// Close close the timestamp waiter
 	Close()
 }
