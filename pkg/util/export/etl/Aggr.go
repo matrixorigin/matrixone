e0e42015c48c01224f765c6ec31450707dbf7793
gaviny <gyue@outlook.com>
2023-06-13 21:41:27 -0700

    add filter func

--- pkg/util/export/etl/Aggr.go
+++ pkg/util/export/etl/Aggr.go
@@ -10,34 +10,42 @@ type Item interface {
 
 type Aggregator struct {
 	Grouped     map[interface{}]Item
+	Ungrouped   []Item
 	WindowSize  time.Duration
 	NewItemFunc func() Item
 	UpdateFunc  func(existing, new Item)
+	FilterFunc  func(i Item) bool
 }
 
-func NewAggregator(windowSize time.Duration, newItemFunc func() Item, updateFunc func(existing, new Item)) *Aggregator {
+func NewAggregator(windowSize time.Duration, newItemFunc func() Item, updateFunc func(existing, new Item), filterFunc func(i Item) bool) *Aggregator {
 	return &Aggregator{
 		Grouped:     make(map[interface{}]Item),
+		Ungrouped:   []Item{},
 		WindowSize:  windowSize,
 		NewItemFunc: newItemFunc,
 		UpdateFunc:  updateFunc,
+		FilterFunc:  filterFunc,
 	}
 }
 
 func (a *Aggregator) AddItem(i Item) {
-	group, exists := a.Grouped[i.Key(a.WindowSize)]
-	if !exists {
-		group = a.NewItemFunc()
-		a.Grouped[i.Key(a.WindowSize)] = group
-	}
+	if a.FilterFunc(i) {
+		group, exists := a.Grouped[i.Key(a.WindowSize)]
+		if !exists {
+			group = a.NewItemFunc()
+			a.Grouped[i.Key(a.WindowSize)] = group
+		}
 
-	a.UpdateFunc(group, i)
+		a.UpdateFunc(group, i)
+	} else {
+		a.Ungrouped = append(a.Ungrouped, i)
+	}
 }
 
-func (a *Aggregator) GetResults() []Item {
+func (a *Aggregator) GetResults() ([]Item, []Item) {
 	var results []Item
 	for _, group := range a.Grouped {
 		results = append(results, group)
 	}
-	return results
+	return results, a.Ungrouped
 }
