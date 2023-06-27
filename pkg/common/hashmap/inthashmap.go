1be9d1eaca605c1a8d786fc42066161c86d920ff
nnsgmsone <nnsmgsone@outlook.com>
2023-06-27 10:29:47 +0800

    Fix the bug of const fill of hashmap

--- pkg/common/hashmap/inthashmap.go
+++ pkg/common/hashmap/inthashmap.go
@@ -123,10 +123,18 @@ func fillKeys[T types.FixedSizeT](m *IntHashMap, vec *vector.Vector, size uint32
 	} else if vec.IsConst() {
 		ptr := (*T)(vector.GetPtrAt(vec, 0))
 		// The old code was too stupid and would lead to out-of-bounds writing
-		for i := 0; i < n; i++ {
-			*(*T)(unsafe.Add(unsafe.Pointer(&keys[i]), keyOffs[i])) = *ptr
+		if m.hasNull {
+			for i := 0; i < n; i++ {
+				*(*T)(unsafe.Add(unsafe.Pointer(&keys[i]), keyOffs[i])) = *ptr
+			}
+			uint32AddScalar(1+size, keyOffs[:n], keyOffs[:n])
+		} else {
+			for i := 0; i < n; i++ {
+				*(*int8)(unsafe.Add(unsafe.Pointer(&keys[i]), keyOffs[i])) = 0
+				*(*T)(unsafe.Add(unsafe.Pointer(&keys[i]), keyOffs[i]+1)) = *ptr
+			}
+			uint32AddScalar(size, keyOffs[:n], keyOffs[:n])
 		}
-		uint32AddScalar(size, keyOffs[:n], keyOffs[:n])
 	} else if !vec.GetNulls().Any() {
 		if m.hasNull {
 			for i := 0; i < n; i++ {
