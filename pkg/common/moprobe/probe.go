379be53b2505bcb532f7162e850cb52d34fb5939
zhangxu <zhangxu19830126@gmail.com>
2023-07-06 21:18:20 +0800

    fix

--- pkg/common/moprobe/probe.go
+++ pkg/common/moprobe/probe.go
@@ -73,6 +73,7 @@ var regionTypeStr = [RegionTypeMax]string{
 	"partiton state del",
 	"partiton state metainsert",
 	"subsciption pull logtail",
+	"rpc stream receive",
 }
 
 func WithRegion(ctx context.Context, rt RegionType, fn func()) {
