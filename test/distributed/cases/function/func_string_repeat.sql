fbb92ac897b2884c1be22e97cd0509edb7007a5e
chenmingsong <mschenjj@foxmail.com>
2023-07-08 15:53:07 +0800

    op case

--- test/distributed/cases/function/func_string_repeat.sql
+++ test/distributed/cases/function/func_string_repeat.sql
@@ -1,3 +1,5 @@
-SELECT repeat('abc', -1), repeat('abc', 0), repeat('abc', 1), repeat('abc', 2), repeat('abc', 3);
+SELECT repeat('abc', -1);
+SELECT repeat('abc', 0);
+SELECT repeat('abc', 1), repeat('abc', 2), repeat('abc', 3);
 SELECT repeat('abc', null);
 select repeat('abc', 100000000000);
