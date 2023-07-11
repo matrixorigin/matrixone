73c65ebfc2cfb52df9abcd43fc8fc3ae14d05470
YANGGMM <18916107305@163.com>
2023-07-11 11:23:05 +0800

    fix bvt test

--- test/distributed/cases/database/config.sql
+++ test/distributed/cases/database/config.sql
@@ -31,22 +31,3 @@ drop database test;
 drop database test1;
 -- @session
 drop account abc;
-
-
-create account default_1 ADMIN_NAME admin IDENTIFIED BY '111111';
--- @session:id=3&user= &password=111111
-create role role1;
-grant create database on account * to role1;
-create user user1 identified by '123456' default role role1;
-create role role2;
-grant create database on account * to role2;
-create user user2 identified by '123456' default role role2;
--- @session
--- @session:id=4&user=default_1:user1:role1&password=123456
-create database db1;
-alter database db1 set mysql_compatibility_mode = '0.7.0';
--- @session
--- @session:id=5&user=default_1:user2:role2&password=123456
-alter database db1 set mysql_compatibility_mode = '0.8.0';
--- @session
-drop account default_1;
