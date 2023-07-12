e9427a483fbd57afb77e58d30992a9c4efacc067
YANGGMM <18916107305@163.com>
2023-07-12 15:15:58 +0800

    fix conflict

--- test/distributed/cases/database/config.sql
+++ test/distributed/cases/database/config.sql
@@ -63,22 +63,3 @@ SELECT @@GLOBAL.sql_mode;
 SELECT @@GLOBAL.sql_mode;
 set global sql_mode = "ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION,NO_ZERO_DATE,NO_ZERO_IN_DATE,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES";
 drop account abc;
-
-
-create account default_1 ADMIN_NAME admin IDENTIFIED BY '111111';
--- @session:id=3&user=default_1:admin&password=111111
-create role role1;
-grant create database on account * to role1;
-create user user1 identified by '123456' default role role1;
-create role role2;
-grant create database on account * to role2;
-create user user2 identified by '123456' default role role2;
--- @session
--- @session:id=4&user=default_1:user1:role1&password=123456
-create database db1;
-alter database db1 set mysql_compatibility_mode = '0.7.0';
--- @session
--- @session:id=5&user=default_1:user2:role2&password=123456
-alter database db1 set mysql_compatibility_mode = '0.8.0';
--- @session
-drop account default_1;
