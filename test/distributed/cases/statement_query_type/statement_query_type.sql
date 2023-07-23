2e54fc5ef8efbaea9cf07ef2096aded86edc0fcd
gaviny <gyue@outlook.com>
2023-07-22 22:08:39 -0700

    fix bvt

--- test/distributed/cases/statement_query_type/statement_query_type.sql
+++ test/distributed/cases/statement_query_type/statement_query_type.sql
@@ -161,7 +161,7 @@ select sleep(1);
 -- RESULT CHECK: part 1
 select sleep(15);
 -- @bvt:issue#10793
-select statement,query_type,sql_source_type from  system.statement_info where account="bvt_query_type" and sql_source_type="external_sql" and status != "Running" and statement not like '%mo_ctl%' and aggr_count <1 order by request_at desc limit 97;
+select statement,query_type,sql_source_type from  system.statement_info where account="bvt_query_type" and sql_source_type="external_sql" and status != "Running" and statement not like '%mo_ctl%' and aggr_count <1 order by request_at desc limit 96;
 -- @bvt:issue
 
 -- CASE: part 2
