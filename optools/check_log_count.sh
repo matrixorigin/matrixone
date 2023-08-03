81bf2d14421e540070e3db3efa702c745aa238ea
xzxiong <xzxiong@yeah.net>
2023-08-03 17:19:04 +0800

    add one more arg: metric_interval

--- optools/check_log_count.sh
+++ optools/check_log_count.sh
@@ -33,8 +33,10 @@ echo_proxy() {
     echo "[`date '+%F %T'`] $@"
 }
 
-show_output_filename() {
-    echo_proxy "out_log_count: $out_log_count"
+show_env() {
+    echo_proxy "arg count_threshold : $count_threshold"
+    echo_proxy "arg metric_interval : $metric_interval"
+    echo_proxy "out_log_count file  : $out_log_count"
 }
 
 get_log_count() {
@@ -44,16 +46,16 @@ get_log_count() {
     # mo_log_message_count    2023-08-03 15:08:08.591955  37  7c4dccb4-4d3c-41f8-b482-5251dc7a41bf    ALL sys info
     #
     ### calculation result example
-    # collecttime	cnt_per_minute	node	role	level
+    # collecttime	cnt_per_second	node	role	level
     # 2023-08-03 14:37:24.977181	35.78	7c4dccb4-4d3c-41f8-b482-5251dc7a41bf	ALL	error
     # 2023-08-03 14:37:24.977181	31.00	7c4dccb4-4d3c-41f8-b482-5251dc7a41bf	ALL	info
     # 2023-08-03 14:38:24.987134	21.02	7c4dccb4-4d3c-41f8-b482-5251dc7a41bf	ALL	error
     #
     local sql=`cat << EOF
 select * from
-(select collecttime, cast( value / $metric_interval as DECIMAL(38,2)) as cnt_per_minute, node, role, type as level from system_metrics.metric
+(select collecttime, cast( value / $metric_interval as DECIMAL(38,2)) as cnt_per_second, node, role, type as level from system_metrics.metric
  where metric_name = 'mo_log_message_count') a
- where a.cnt_per_minute > $count_threshold
+ where a.cnt_per_second > $count_threshold
  order by collecttime
 EOF`
     echo_proxy "Query: $sql"
@@ -63,11 +65,11 @@ EOF`
 check_log_count() {
     local rows=`wc -l $out_log_count | awk '{print $1}'`
     if [ "$rows" == "0" ]; then
-        echo_proxy "All log messages spitting out in threshold(val: $count_threshold) per minute"
+        echo_proxy "All log messages spitting out in threshold(val: $count_threshold) per second"
         return 0
     fi
 
-    echo_proxy "log messages per minute threshold(val: $count_threshold)"
+    echo_proxy "log messages per second threshold(val: $count_threshold)"
     echo_proxy "each rows show last $metric_interval secs status"
     echo_proxy
     cat $out_log_count
@@ -77,29 +79,35 @@ check_log_count() {
 
 usage() {
     cat << EOF
-Usage: $0 [cnt_threshold]
+Usage: $0 [cnt_threshold [metric_interval]]
 like:  $0
   or   $0 1000
+  or   $0 1000 60
 
 options
-    cnt_threshold   - int, log messages per minute threshold
+    cnt_threshold   - int, log messages per second threshold
                       default: $count_threshold
+    metric_interval - int, metric collected interval
+                      default: $metric_interval
 EOF
 }
 
 ## main
 ################
 
-if [ $# -gt 0 ]; then
+if [ $# -eq 1 ]; then
     arg=$1
     if [ "$arg" == "-h" -o "$arg" == "--help" ]; then
         usage
         exit 1
     fi
     count_threshold=$arg
+elif [ $# -eq 2 ]; then
+    count_threshold=$1
+    metric_interval=$2
 fi
 
-show_output_filename
+show_env
 get_log_count
 check_log_count
 ret=$?
