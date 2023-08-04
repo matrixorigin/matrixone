9902b0daf605c6b7351c20d1fc0a56bd5e60a4e1
GreatRiver <2552853833@qq.com>
2023-08-05 00:17:03 +0800

    Update code

--- pkg/vm/engine/tae/logtail/utils.go
+++ pkg/vm/engine/tae/logtail/utils.go
@@ -611,7 +611,7 @@ func (data *CNCheckpointData) ReadFromData(
 			if err != nil {
 				return
 			}
-			logutil.Infof("load block %v: %d-%d to %d", block.GetLocation().String(), block.GetStartOffset(), block.GetEndOffset(), bat.Vecs[0].Length())
+			//logutil.Infof("load block %v: %d-%d to %d", block.GetLocation().String(), block.GetStartOffset(), block.GetEndOffset(), bat.Vecs[0].Length())
 			if block.GetEndOffset() == 0 {
 				continue
 			}
@@ -920,7 +920,6 @@ func (data *CheckpointData) WriteTo(
 			}
 			Endoffset := offset + bat.Length()
 			blockLoc := BuildBlockLoaction(block.GetID(), uint64(offset), uint64(Endoffset))
-			logutil.Infof("blockLoc is %d-%d", blockLoc.GetStartOffset(), blockLoc.GetEndOffset())
 			blockIndexs[i] = append(blockIndexs[i], &blockLoc)
 			offset += bat.Length()
 		}
@@ -978,8 +977,8 @@ func (data *CheckpointData) WriteTo(
 				if table.Start >= block.GetEndOffset() {
 					continue
 				}
-				blockLoc1 := objectio.BuildLocation(name, blks[block.GetID()].GetExtent(), 0, block.GetID())
-				logutil.Infof("write block %v to %d-%d, table is %d-%d", blockLoc1.String(), block.GetStartOffset(), block.GetEndOffset(), table.Start, table.End)
+				//blockLoc1 := objectio.BuildLocation(name, blks[block.GetID()].GetExtent(), 0, block.GetID())
+				//logutil.Infof("write block %v to %d-%d, table is %d-%d", blockLoc1.String(), block.GetStartOffset(), block.GetEndOffset(), table.Start, table.End)
 				if table.Uint64Contains(block.GetStartOffset(), block.GetEndOffset()) {
 					blockLoc := BuildBlockLoactionWithLocation(
 						name, blks[block.GetID()].GetExtent(), 0, block.GetID(),
