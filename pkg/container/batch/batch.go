75b756f0fb21549f792bcd9559a03b05fe23b525
qingxinhome <758355272@qq.com>
2023-08-05 09:45:48 +0800

    delete unused code

--- pkg/container/batch/batch.go
+++ pkg/container/batch/batch.go
@@ -18,10 +18,8 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"sync/atomic"
-	"time"
-
 	"github.com/matrixorigin/matrixone/pkg/sql/colexec/agg"
+	"sync/atomic"
 
 	"github.com/matrixorigin/matrixone/pkg/common/hashmap"
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
@@ -344,84 +342,4 @@ func (bat *Batch) DupJmAuxData() (ret *hashmap.JoinMap) {
 		bat.AuxData = nil
 	}
 	return
-}
-
-// PrintBatch Convert batch to a two-dimensional table string
-// Warning: This method is only used for debugging code and is not suitable for printing big batch data
-func (bat *Batch) PrintBatch() string {
-	stringBuffer := bytes.NewBufferString("")
-	if bat.RowCount() > 0 {
-		for i := 0; i < bat.RowCount(); i++ {
-			isFirst := true
-			stringBuffer.WriteString(fmt.Sprintf("row %v : ", i) + "(")
-			for _, vec := range bat.Vecs {
-				if isFirst {
-					isFirst = false
-				} else {
-					stringBuffer.WriteString(", ")
-				}
-				if vec.GetNulls().Contains(uint64(i)) {
-					stringBuffer.WriteString("null")
-				} else if vec.IsConstNull() {
-					stringBuffer.WriteString("null")
-				} else {
-					switch vec.GetType().Oid {
-					case types.T_bool:
-						stringBuffer.WriteString(fmt.Sprintf("%v", vector.MustFixedCol[bool](vec)[i]))
-					case types.T_int8:
-						stringBuffer.WriteString(fmt.Sprintf("%v", vector.MustFixedCol[int8](vec)[i]))
-					case types.T_int16:
-						stringBuffer.WriteString(fmt.Sprintf("%v", vector.MustFixedCol[int16](vec)[i]))
-					case types.T_int32:
-						stringBuffer.WriteString(fmt.Sprintf("%v", vector.MustFixedCol[int32](vec)[i]))
-					case types.T_int64:
-						stringBuffer.WriteString(fmt.Sprintf("%v", vector.MustFixedCol[int64](vec)[i]))
-					case types.T_uint8:
-						stringBuffer.WriteString(fmt.Sprintf("%v", vector.MustFixedCol[uint8](vec)[i]))
-					case types.T_uint16:
-						stringBuffer.WriteString(fmt.Sprintf("%v", vector.MustFixedCol[uint16](vec)[i]))
-					case types.T_uint32:
-						stringBuffer.WriteString(fmt.Sprintf("%v", vector.MustFixedCol[uint32](vec)[i]))
-					case types.T_uint64:
-						stringBuffer.WriteString(fmt.Sprintf("%v", vector.MustFixedCol[uint64](vec)[i]))
-					case types.T_float32:
-						stringBuffer.WriteString(fmt.Sprintf("%v", vector.MustFixedCol[float32](vec)[i]))
-					case types.T_float64:
-						stringBuffer.WriteString(fmt.Sprintf("%v", vector.MustFixedCol[float64](vec)[i]))
-					case types.T_char, types.T_varchar, types.T_binary, types.T_varbinary, types.T_text, types.T_blob:
-						stringBuffer.WriteString(fmt.Sprintf("%v", vec.GetStringAt(i)))
-					case types.T_decimal64:
-						val := vector.GetFixedAt[types.Decimal64](vec, i)
-						stringBuffer.WriteString(val.Format(vec.GetType().Scale))
-					case types.T_decimal128:
-						val := vector.GetFixedAt[types.Decimal128](vec, i)
-						stringBuffer.WriteString(val.Format(vec.GetType().Scale))
-					case types.T_json:
-						val := vec.GetBytesAt(i)
-						byteJson := types.DecodeJson(val)
-						stringBuffer.WriteString(byteJson.String())
-					case types.T_uuid:
-						val := vector.MustFixedCol[types.Uuid](vec)[i]
-						stringBuffer.WriteString(val.ToString())
-					case types.T_date:
-						val := vector.MustFixedCol[types.Date](vec)[i]
-						stringBuffer.WriteString(val.String())
-					case types.T_time:
-						val := vector.MustFixedCol[types.Time](vec)[i]
-						stringBuffer.WriteString(val.String())
-					case types.T_datetime:
-						val := vector.MustFixedCol[types.Datetime](vec)[i]
-						stringBuffer.WriteString(val.String())
-					case types.T_timestamp:
-						val := vector.MustFixedCol[types.Timestamp](vec)[i]
-						stringBuffer.WriteString(val.String2(time.Local, vec.GetType().Scale))
-					default:
-						stringBuffer.WriteString("UNSUPPORT TYPE")
-					}
-				}
-			}
-			stringBuffer.WriteString("), ")
-		}
-	}
-	return stringBuffer.String()
 }
