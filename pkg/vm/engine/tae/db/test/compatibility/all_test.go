2cf721a2b9092d78d55e1942a75b013c03e7012e
XuPeng-SH <xupeng3112@163.com>
2023-08-07 23:18:02 +0800

    update

--- pkg/vm/engine/tae/db/test/compatibility/all_test.go
+++ pkg/vm/engine/tae/db/test/compatibility/all_test.go
@@ -21,8 +21,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-type PrepareStatementType int
-
 func runPrepareCase(prepareCase PrepareCase, t *testing.T) {
 	prepareCase.prepareFn(
 		prepareCase, t,
