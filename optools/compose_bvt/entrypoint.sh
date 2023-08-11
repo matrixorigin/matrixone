580dc1000781a9e2661c4e896ddc747e813b2520
brown <1173781832@qq.com>
2023-08-11 13:46:26 +0800

    Update entrypoint.sh

--- optools/compose_bvt/entrypoint.sh
+++ optools/compose_bvt/entrypoint.sh
@@ -18,13 +18,13 @@ function run_bvt() {
     i=0
     while [ $(mysql -h cn0 -P 6001 -u dump -p111 --execute 'create database if not exists compose_test;use compose_test; create table if not exists compose_test_table(col1 int auto_increment primary key);show tables;' 2>&1 | tee /dev/stderr | grep 'compose_test_table' | wc -l) -lt 1 ]; do
       echo "wait mo init finished...$i"
-        if [ $i -ge 300 ]; then
-          echo "wait for $i seconds, mo init not finish, so exit 1"
-          docker ps
-          exit 1;
-        fi
-        i=$(($i+1))
-        sleep 1
+      if [ $i -ge 300 ]; then
+        echo "wait for $i seconds, mo init not finish, so exit 1"
+        docker ps
+        exit 1;
+      fi
+      i=$(($i+1))
+      sleep 1
     done
     cd /mo-tester && ./run.sh -n -g -p /matrixone/test/distributed/cases/ -s /test/distributed/resources 2>&1
 }
