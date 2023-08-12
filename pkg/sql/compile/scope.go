9f0369f762dfffb86afa714b292b2d8a248b6502
nitao <badboynt@126.com>
2023-08-12 09:00:20 +0800

    fix

--- pkg/sql/compile/scope.go
+++ pkg/sql/compile/scope.go
@@ -479,7 +479,7 @@ func (s *Scope) JoinRun(c *Compile) error {
 			s.PreScopes = append(s.PreScopes, probeScope)
 		}
 		// this is for shuffle join
-		s.Proc.Reg.MergeReceivers[0].Ch = make(chan *batch.Batch, 8)
+		s.Proc.Reg.MergeReceivers[0].Ch = make(chan *batch.Batch, 16)
 		return s.MergeRun(c)
 	}
 
