cc9f6163339de1163c225629df7d2002e8604c13
daviszhen <daviszhen007@gmail.com>
2023-08-15 21:28:38 +0800

    add backup

---
+++ pkg/backup/backup.go
@@ -0,0 +1,164 @@
+package backup
+
+import (
+    "context"
+    "encoding/csv"
+    "github.com/google/uuid"
+    "github.com/matrixorigin/matrixone/pkg/fileservice"
+    "github.com/matrixorigin/matrixone/pkg/logutil"
+    "github.com/matrixorigin/matrixone/pkg/sql/parsers/tree"
+    "golang.org/x/exp/rand"
+    "os"
+    "path"
+    "strings"
+)
+
+// Backup
+// Note: ctx needs to support cancel. The user can cancel the backup task by canceling the ctx.
+func Backup(ctx context.Context, bs *tree.BackupStart, cfg *Config) error {
+    var err error
+    var s3Conf *s3Config
+    cfg.Metas = NewMetas()
+
+    // step 1 : setup fileservice
+    //1.1 setup ETL fileservice for general usage
+    if !bs.IsS3 {
+        cfg.GeneralDir, _, err = setupFilesystem(ctx, bs.Dir, true)
+        if err != nil {
+            return err
+        }
+        //for tae hakeeper
+        cfg.TaeDir, _, err = setupFilesystem(ctx, bs.Dir, false)
+        if err != nil {
+            return err
+        }
+        cfg.TaeDir = fileservice.SubPath(cfg.TaeDir, taeDir)
+    } else {
+        s3Conf, err = getS3Config(ctx, bs.Option)
+        if err != nil {
+            return err
+        }
+        cfg.GeneralDir, _, err = setupS3(ctx, s3Conf, true)
+        if err != nil {
+            return err
+        }
+        cfg.TaeDir, _, err = setupS3(ctx, s3Conf, false)
+        if err != nil {
+            return err
+        }
+        cfg.TaeDir = fileservice.SubPath(cfg.TaeDir, taeDir)
+    }
+    
+    // step 2 : backup mo
+    if err = backupBuildInfo(ctx, cfg); err != nil {
+        return err
+    }
+
+    if err = backupConfigs(ctx, cfg); err != nil {
+        return err
+    }
+
+    if err = backupTae(ctx, cfg); err != nil {
+        return err
+    }
+
+    if err = backupHakeeper(ctx, cfg); err != nil {
+        return err
+    }
+
+    if err = saveMetas(ctx,cfg) ; err != nil{
+        return err
+    }
+
+    return err
+}
+
+// saveBuildInfo saves backupVersion, build info.
+func backupBuildInfo(ctx context.Context, cfg *Config) error {
+    cfg.Metas.AppendVersion(Version)
+    cfg.Metas.AppendBuildinfo(buildInfo())
+    return nil
+}
+
+// saveConfigs saves cluster config or service config
+func backupConfigs(ctx context.Context, cfg *Config) error {
+    var err error
+    // save cluster config files
+    for typ, files := range launchConfigPaths {
+        for _, f := range files {
+            err = backupConfigFile(ctx, typ,f, cfg)
+            if err != nil {
+                return err
+            }
+        }
+    }
+
+    return err
+}
+
+func backupTae(ctx context.Context, config *Config) error {
+    return saveDumpFiles(ctx, config.TaeDir,1,1)
+}
+
+func backupHakeeper(ctx context.Context, config *Config) error {
+    fs := fileservice.SubPath(config.GeneralDir, hakeeperDir)
+    return saveDumpHakeeper(ctx,fs,1)
+}
+
+func saveDumpFiles(ctx context.Context, fs fileservice.FileService,size, count int) error{
+    var err error
+    data := make([]byte,size * mb)
+    for i := 0; i < count; i++ {
+        _,_ = rand.Read(data)
+        uid,_ := uuid.NewUUID()
+        err =  writeFile(ctx,fs,uid.String(),data)
+        if err != nil{
+            return err
+        }
+    }
+    return err
+}
+
+func saveDumpHakeeper(ctx context.Context, fs fileservice.FileService,size int) error{
+    data := make([]byte,size * mb)
+    _,_ = rand.Read(data)
+    return writeFile(ctx,fs,HakeeperFile,data)
+}
+
+func backupConfigFile(ctx context.Context,typ, configPath string, cfg *Config) error {
+    data, err := os.ReadFile(configPath)
+    if err != nil {
+        logutil.Errorf("read file %s failed, err: %v", configPath, err)
+        //!!!neglect the error
+        return nil
+    }
+    uid, _ := uuid.NewUUID()
+    _, file := path.Split(configPath)
+    newfile := file + "_" + uid.String()
+    cfg.Metas.AppendLaunchconfig(typ,newfile)
+    filename := configDir + "/" + newfile
+    return writeFile(ctx, cfg.GeneralDir, filename, data)
+}
+
+func saveMetas(ctx context.Context, cfg *Config) error{
+    lines := cfg.Metas.CsvString()
+    metas, err := ToCsvLine2(lines)
+    if err != nil{
+        return err
+    }
+    return writeFile(ctx, cfg.GeneralDir, moMeta, []byte(metas))
+}
+
+func ToCsvLine2(s [][]string) (string, error) {
+    ss := strings.Builder{}
+    writer := csv.NewWriter(&ss)
+    for _, t := range s {
+        err := writer.Write(t)
+        if err != nil {
+            return "", err
+        }
+    }
+
+    writer.Flush()
+    return ss.String(), nil
+}
---
+++ pkg/backup/fs.go
@@ -0,0 +1,129 @@
+package backup
+
+import (
+    "context"
+    "encoding/csv"
+    "fmt"
+    "github.com/matrixorigin/matrixone/pkg/fileservice"
+    "github.com/matrixorigin/matrixone/pkg/logutil"
+    "strconv"
+    "strings"
+)
+
+// setupFilesystem returns a FileService for ETL which the reader outside the matrixone
+// can read the content. a FileService for Backup which only the matrixone
+// can read the content.
+func setupFilesystem(ctx context.Context, path string, forETL bool) (res fileservice.FileService, readPath string, err error) {
+    return setupFileservice(ctx, &pathConfig{
+        isS3:             false,
+        forETL:           forETL,
+        filesystemConfig: filesystemConfig{path: path},
+    })
+}
+
+// setupS3 returns a FileService for ETL which the reader outside the matrixone
+// can read the content.a FileService for Backup which only the matrixone
+// can read the content.
+func setupS3(ctx context.Context, s3 *s3Config, forETL bool) (res fileservice.FileService, readPath string, err error) {
+    return setupFileservice(ctx, &pathConfig{
+        isS3:     true,
+        forETL:   forETL,
+        s3Config: *s3,
+    })
+}
+
+func setupFileservice(ctx context.Context, conf *pathConfig) (res fileservice.FileService, readPath string, err error) {
+    var s3opts string
+    if conf.isS3 {
+        s3opts, err = makeS3Opts(&conf.s3Config)
+        if err != nil {
+            return nil, "", err
+        }
+        if conf.forETL {
+            s3path := fileservice.JoinPath(s3opts, etlFSDir(conf.filepath))
+            //TODO:remove debug
+            logutil.Debugf("==>s3path: %s", s3path)
+            res, readPath, err = fileservice.GetForETL(nil, s3path)
+            if err != nil{
+                return nil, "", err
+            }
+        } else {
+            s3path := fileservice.JoinPath(s3opts, conf.filepath)
+            res, err = fileservice.GetForBackup(s3path)
+            if err != nil{
+                return nil, "", err
+            }
+        }
+        res = fileservice.SubPath(res, conf.filepath)
+    } else {
+        if conf.forETL {
+            res, readPath, err = fileservice.GetForETL(nil, etlFSDir(conf.path))
+            if err != nil {
+                return nil, "", err
+            }
+        } else {
+            res, err = fileservice.GetForBackup(conf.path)
+            if err != nil {
+                return nil, "", err
+            }
+        }
+    }
+
+    return res, readPath, err
+}
+
+func makeS3Opts(s3 *s3Config) (string, error) {
+    var err error
+    buf := new(strings.Builder)
+    w := csv.NewWriter(buf)
+    opts := []string{
+        "s3-opts",
+        "endpoint=" + s3.endpoint,
+        "region=" + s3.region,
+        "key=" + s3.accessKeyId,
+        "secret=" + s3.secretAccessKey,
+        "bucket=" + s3.bucket,
+        "role-arn=" + s3.roleArn,
+        "is-minio=" + strconv.FormatBool(s3.isMinio),
+        //"external-id="              /*+ param.S3Param.ExternalId*/,
+    }
+    if err = w.Write(opts); err != nil {
+        return "", err
+    }
+    w.Flush()
+    return buf.String(), nil
+}
+
+func etlFSDir(filepath string) string {
+    return filepath + "/_"
+}
+
+func directoryIsEmpty(ctx context.Context, fs fileservice.FileService, dir string) (bool, error) {
+    var err error
+    var dirEntries []fileservice.DirEntry
+    dirEntries, err = fs.List(ctx, "")
+    if err != nil {
+        return false, err
+    }
+    for i, entry := range dirEntries {
+        logutil.Debugf("%d: %s", i, dirEntryToString(entry))
+    }
+    return len(dirEntries) == 0, err
+}
+
+func writeFile(ctx context.Context, fs fileservice.FileService, path string, data []byte) error {
+    return fs.Write(ctx, fileservice.IOVector{
+        FilePath: path,
+        Entries: []fileservice.IOEntry{
+            {
+                Offset: 0,
+                Size:   int64(len(data)),
+                Data:   data,
+            },
+        },
+    })
+}
+
+func dirEntryToString(dir fileservice.DirEntry) string {
+    return fmt.Sprintf("Name %s Dir %v Size %d", dir.Name, dir.IsDir, dir.Size)
+}
