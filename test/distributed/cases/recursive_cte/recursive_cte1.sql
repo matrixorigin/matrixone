9e36a8297b1d3e56da217f0bbb27ea5b9e09aecd
heni02 <heni_2010@126.com>
2023-08-22 14:17:18 +0800

    recursive cte cases

--- test/distributed/cases/recursive_cte/recursive_cte1.sql
+++ test/distributed/cases/recursive_cte/recursive_cte1.sql
@@ -1,7 +1,7 @@
-CREATE TABLE emp(id INT PRIMARY KEY NOT NULL,name VARCHAR(100) NOT NULL,manager_id INT NULL,INDEX (manager_id),FOREIGN KEY (manager_id) REFERENCES employees_mgr(id));
-INSERT INTO emp VALUES(333, "总经理", NULL), (198, "副总1", 333), (692, "副总2", 333),(29, "主任1", 198),(4610, "职员1", 29),(72, "职员2", 29),(123, "主任2", 692);
 create table employees_mgr(id int primary key not null,name varchar(25));
 insert into employees_mgr values(333,'ami'),(198,'lucky'),(29,'jack'),(692,'sammi');
+CREATE TABLE emp(id INT PRIMARY KEY NOT NULL,name VARCHAR(100) NOT NULL,manager_id INT NULL,INDEX (manager_id),FOREIGN KEY (manager_id) REFERENCES employees_mgr(id));
+INSERT INTO emp VALUES(333, "总经理", NULL), (198, "副总1", 333), (692, "副总2", 333),(29, "主任1", 198),(4610, "职员1", 29),(72, "职员2", 29),(123, "主任2", 692);
 create table product (id int primary key,p_id int,p_name varchar(25),price decimal(10,3));
 insert into product values (3,2,"bed",3560.98),(2,null,"chair",1599.00),(4,1,"desk",2999.99),(5,3,"door",8123.09),(6,3,"mirrors",698.00),(7,4,"tv",5678);
 
@@ -13,8 +13,9 @@ with non_cte_4 as (select count(id) as emp_num,manager_id from emp group by mana
 with non_cte_5(manager_id,job_name,employee_id)as (select a.manager_id,name,id from emp a where exists (select id from employees_mgr b where a.manager_id=b.id))select * from non_cte_5;
 with non_cte_6(manager_id,job_name,employee_id) as (select a.manager_id,name,id from emp a where exists (select id from employees_mgr b where a.manager_id=b.id))select manager_id,job_name,count(employee_id) as emp_num from non_cte_6 group by manager_id,job_name order by manager_id,job_name;
 with non_cte_7(manager_id,emp_num)as (select manager_id ,count(id) from emp where manager_id is not null group by manager_id) select avg(emp_num) as "average emp per manager" from non_cte_7;
+with recursive non_cte_10(id, productID,js,price,old_price) AS (select p.id,p.p_id,'20' as js,p.price+3.65,ceil(p.price) from product p where p.p_id < 100)select id, productID,js,price,old_price from cte_ab_10 order by old_price;
 
--- drop table ,truncate table
+-- abnormal test: drop table ,truncate table
 truncate table non_cte_6;
 drop table non_cte_7;
 
@@ -27,11 +28,28 @@ with non_cte_8(manager_id,name) as
          m.name
     FROM employees_mgr m
     WHERE m.name != "lucky")
-SELECT *
+SELECT a.manager_id,a.name
 FROM non_cte_8 a
 JOIN non_cte_9 b
     ON a.manager_id = b.id
-ORDER BY  a.manager_id
+ORDER BY  a.manager_id;
+
+with non_cte_8(manager_id,name) as
+    (SELECT a.manager_id,
+        a.name AS job_name
+    FROM emp a), non_cte_9(id,name) as
+    (SELECT m.id,
+         m.name
+    FROM employees_mgr m
+    WHERE m.name != "lucky"), non_cte_11(id,p_name) as
+    (
+    select p.id,p.p_name from product p
+    )
+select ncte.manager_id,ncte.name from (SELECT a.manager_id,a.name
+FROM non_cte_8 a
+JOIN non_cte_9 b
+    ON a.manager_id = b.id) as ncte
+union all select c.id,c.p_name from non_cte_11 c;
 
 --expression_name: key word,number
 with date as(select manager_id,id,name from emp  order by manager_id )select * from date;
@@ -40,26 +58,24 @@ with 111 as(select manager_id,id,name from emp  order by manager_id )select * fr
 -- recursive cte
 CREATE TABLE MyEmployees
 (
-EmployeeID SMALLINT NOT NULL,
-FirstName NVARCHAR(30) NOT NULL,
-LastName NVARCHAR(40) NOT NULL,
-Title NVARCHAR(50) NOT NULL,
+EmployeeID SMALLINT PRIMARY KEY NOT NULL,
+FirstName VARCHAR(30) NOT NULL,
+LastName VARCHAR(40) NOT NULL,
+Title VARCHAR(50) NOT NULL,
 DeptID SMALLINT NOT NULL,
-ManagerID SMALLINT NULL,
-CONSTRAINT PK_EmployeeID PRIMARY KEY CLUSTERED (EmployeeID ASC),
-CONSTRAINT FK_MyEmployees_ManagerID_EmployeeID FOREIGN KEY (ManagerID) REFERENCES MyEmployees (EmployeeID)
+ManagerID SMALLINT NULL
 );
 
 INSERT INTO MyEmployees VALUES
-(1, N'Ken', N'Sánchez', N'Chief Executive Officer',16, NULL)
-,(273, N'Brian', N'Welcker', N'Vice President of Sales', 3, 1)
-,(274, N'Stephen', N'Jiang', N'North American Sales Manager', 3, 273)
-,(275, N'Michael', N'Blythe', N'Sales Representative', 3, 274)
-,(276, N'Linda', N'Mitchell', N'Sales Representative', 3, 274)
-,(285, N'Syed', N'Abbas', N'Pacific Sales Manager', 3, 273)
-,(286, N'Lynn', N'Tsoflias', N'Sales Representative', 3, 285)
-,(16, N'David', N'Bradley', N'Marketing Manager', 4, 273)
-,(23, N'Mary', N'Gibson', N'Marketing Specialist', 4, 16)
+(1, 'Ken', 'Sánchez', 'Chief Executive Officer',16, NULL)
+,(273, 'Brian', 'Welcker', 'Vice President of Sales', 3, 1)
+,(274, 'Stephen', 'Jiang', 'North American Sales Manager', 3, 273)
+,(275, 'Michael', 'Blythe', 'Sales Representative', 3, 274)
+,(276, 'Linda', 'Mitchell', 'Sales Representative', 3, 274)
+,(285, 'Syed', 'Abbas', 'Pacific Sales Manager', 3, 273)
+,(286, 'Lynn', 'Tsoflias', 'Sales Representative', 3, 285)
+,(16, 'David', 'Bradley', 'Marketing Manager', 4, 273)
+,(23, 'Mary', 'Gibson', 'Marketing Specialist', 4, 16);
 
 WITH RECURSIVE DirectReports(ManagerID, EmployeeID, Title, EmployeeLevel) AS
 (
@@ -94,6 +110,24 @@ AS (SELECT concat(e.FirstName," ",e.LastName) as name,
     )
 SELECT EmployeeID, Name, Title, EmployeeLevel
 FROM DirectReports order by EmployeeID;
+
+-- recursive cte :concat function
+with recursive cte_ab_11(id, productID,price) AS
+    (SELECT p.id,
+         p.p_id,
+         p.price
+    FROM product p
+    WHERE p.p_id is null
+    UNION all
+    SELECT pr.id,
+         pr.p_id,
+         pr.price*12.01
+    FROM product pr
+    JOIN (select * from cte_ab_11 c where c.price between 20 and 1000)
+        ON pr.id = c.productID
+    GROUP BY  c.id, c.productID )
+SELECT *
+FROM cte_ab_11;
 
 -- multi recursive cte
 WITH RECURSIVE DirectReports(ManagerID, EmployeeID, Title, EmployeeLevel) AS
@@ -131,6 +165,7 @@ insert into cte_insert_table  WITH RECURSIVE DirectReports(ManagerID, EmployeeID
 SELECT ManagerID, EmployeeID, Title, EmployeeLevel
 FROM DirectReports
 ORDER BY ManagerID;
+select * from cte_insert_table;
 
 -- recursive cte: update cte
 update cte_insert_table set c3= (WITH RECURSIVE DirectReports(ManagerID, EmployeeID, Title, EmployeeLevel) AS
@@ -145,6 +180,7 @@ update cte_insert_table set c3= (WITH RECURSIVE DirectReports(ManagerID, Employe
         ON e.ManagerID = d.EmployeeID
 )
 select title from  DirectReports where ManagerID=16) where c2=274;
+select * from cte_insert_table;
 
 -- recursive cte: delete
 delete from  cte_insert_table where  c3 = (WITH RECURSIVE DirectReports(ManagerID, EmployeeID, Title, EmployeeLevel) AS
@@ -159,6 +195,7 @@ delete from  cte_insert_table where  c3 = (WITH RECURSIVE DirectReports(ManagerI
         ON e.ManagerID = d.EmployeeID
 )
 select Title from  DirectReports where ManagerID=16);
+select * from cte_insert_table;
 
 -- recursive cte: create view as
 create view cte_view as(
@@ -181,14 +218,9 @@ SELECT EmployeeID, Name, Title, EmployeeLevel
 FROM DirectReports order by EmployeeID);
 select * from cte_view order by EmployeeLevel;
 
--- recursive cte abnormal: multi select cte?
-WITH RECURSIVE test(id, name, path)
-AS
-(SELECT id, name, CAST(id AS CHAR(200))
-FROM emp WHERE manager_id IS NULL
-UNION ALL
-SELECT e.id, e.name, CONCAT(ep.path, ',', e.id)
-FROM test AS ep JOIN emp AS e  ON ep.id = e.manager_id)SELECT * FROM test ORDER BY path;
+-- recursive cte
+with recursive  cte_ab_15(productID,price) as(select p_id,price from product  union all select c.productID,p.price from product p join cte_ab_15 c on p.p_id = c.productID)select * from cte_ab_15;
+
 
 -- recursive cte abnormal:  recursive_query include sum/avg/count/max/min,group by,having,order by
 with recursive cte_ab_1(id, productID,price) as (SELECT p.id, p.p_id, p.price FROM product p where p.p_id is null UNION all SELECT p.id, p.p_id, avg(p.price) FROM product p JOIN cte_ab_1 c  ON p.id = c.productID  GROUP BY c.id, c.productID )SELECT * FROM cte_ab_1;
@@ -205,7 +237,7 @@ with recursive cte_ab_9(id,manager_id,name,levels) as(select p.id,p.manager_id,p
 
 -- abnormal: SELECT list and column names list have different column counts
 with recursive cte_ab_10(id,manager_id,name,levels) as(select p.id,p.manager_id,p.name from emp p where manager_id is null union all select p.id,p.manager_id,p.name from emp p join cte_ab_10 c on p.manager_id= c.id ) select * from cte_ab_10 order by id;
-with non_cte_8(manager_id,name) as(SELECT a.manager_id, a.id,a.name AS job_name FROM emp a), non_cte_9(id,name) as(SELECT m.id, m.name FROM employees_mgr m WHERE m.name != "lucky") select * FROM non_cte_8 a JOIN non_cte_9 b ON a.manager_id = b.id ORDER BY  a.manager_id
+with non_cte_8(manager_id,name) as(SELECT a.manager_id, a.id,a.name AS job_name FROM emp a), non_cte_9(id,name) as(SELECT m.id, m.name FROM employees_mgr m WHERE m.name != "lucky") select * FROM non_cte_8 a JOIN non_cte_9 b ON a.manager_id = b.id ORDER BY  a.manager_id;
 
 -- recursive cte abnormal: with as update
 WITH RECURSIVE DirectReports(ManagerID, EmployeeID, Title, EmployeeLevel) AS
