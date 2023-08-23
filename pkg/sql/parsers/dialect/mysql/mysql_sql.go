9df1f226880a7bca9cd72e9327e8f764ccd35fb1
reus <reusee@gmail.com>
2023-08-23 15:28:53 +0800

    make pb

--- pkg/sql/parsers/dialect/mysql/mysql_sql.go
+++ pkg/sql/parsers/dialect/mysql/mysql_sql.go
@@ -11301,7 +11301,7 @@ yydefault:
 //line mysql_sql.y:2976
 		{
 			assignments := []*tree.VarAssignmentExpr{
-				&tree.VarAssignmentExpr{
+				{
 					System: true,
 					Global: true,
 					Name:   yyDollar[6].str,
