b545a3b86a62769e8517268171e9e42d023f2c03
YANGGMM <18916107305@163.com>
2023-08-23 12:57:20 +0800

    fix scanner for '+' condition (#11389)
    fix scanner for '+' condition
    
    Approved by: @iamlinjunhong

--- pkg/sql/parsers/dialect/mysql/mysql_sql_test.go
+++ pkg/sql/parsers/dialect/mysql/mysql_sql_test.go
@@ -2452,7 +2452,7 @@ var (
 			output: "set global GTID_PURGED = 65c4c218-d343-11eb-8106-525400f4f901:1-769275",
 		}, {
 			input:  " /*!40103 SET TIME_ZONE='+00:00' */",
-			output: "set time_zone = 0:00",
+			output: "set time_zone = +00:00",
 		}, {
 			input:  "/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */",
 			output: "set OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT",
