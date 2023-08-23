01cc7fdcab037609428bc3a9f809a8c9182b6b50
jiangxinmeng1 <51114574+jiangxinmeng1@users.noreply.github.com>
2023-08-23 12:21:18 +0800

    fix compability problems (#11405)
    Approved by: @XuPeng-SH

--- pkg/vm/engine/tae/db/testutil/engine.go
+++ pkg/vm/engine/tae/db/testutil/engine.go
@@ -473,8 +473,8 @@ func isBatchEqual(ctx context.Context, t *testing.T, bat1, bat2 *containers.Batc
 	for i := 0; i < getBatchLength(bat1); i++ {
 		for j, vec1 := range bat1.Vecs {
 			vec2 := bat2.Vecs[j]
-			// for commitTS and rowid in checkpoint
 			if vec1.Length() == 0 || vec2.Length() == 0 {
+				// for commitTS and rowid in checkpoint
 				// logutil.Warnf("empty vec attr %v", bat1.Attrs[j])
 				continue
 			}
@@ -487,7 +487,7 @@ func isBatchEqual(ctx context.Context, t *testing.T, bat1, bat2 *containers.Batc
 				continue
 			}
 			// t.Logf("attr %v, row %d", bat1.Attrs[j], i)
-			require.Equal(t, vec1.Get(i), vec2.Get(i))
+			require.Equal(t, vec1.Get(i), vec2.Get(i), "name is \"%v\"", bat1.Attrs[j])
 		}
 	}
 }
