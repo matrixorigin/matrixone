1503fa1829be6af28a976d9ebc5ca6577c9fe58b
maomao <77312370+sukki37@users.noreply.github.com>
2023-08-25 01:29:15 +0800

    build on ouyuanning:change-def-tx-mode (#11454)
    change default transaction mode from Optimistic to Pessimistic

--- pkg/dnservice/cfg_test.go
+++ pkg/dnservice/cfg_test.go
@@ -33,5 +33,5 @@ func TestValidate(t *testing.T) {
 	assert.Equal(t, defaultHeatbeatInterval, c.HAKeeper.HeatbeatInterval.Duration)
 	assert.Equal(t, defaultHeatbeatTimeout, c.HAKeeper.HeatbeatTimeout.Duration)
 	assert.Equal(t, defaultConnectTimeout, c.LogService.ConnectTimeout.Duration)
-	assert.Equal(t, "false", c.Txn.IncrementalDedup)
+	assert.Equal(t, "true", c.Txn.IncrementalDedup)
 }
