e60d1b1039d6a5fa83f80d1a6018c5fb206ce3d9
ou yuanning <45346669+ouyuanning@users.noreply.github.com>
2023-08-28 09:21:19 +0800

    fix bug: get latest schema when retry to run SQL (#11418)
    1、when insert table with fake pk. we add lock node now.
    2、fix bug: when retry to run SQL after lock free.   we can get the latest schema now.
    
    Approved by: @triump2020, @badboynt1, @nnsgmsone, @aunjgr, @heni02

--- pkg/vm/engine/disttae/cache/catalog.go
+++ pkg/vm/engine/disttae/cache/catalog.go
@@ -16,6 +16,7 @@ package cache
 
 import (
 	"fmt"
+	"math"
 	"sort"
 
 	"github.com/matrixorigin/matrixone/pkg/catalog"
@@ -301,6 +302,26 @@ func (cc *CatalogCache) DeleteTable(bat *batch.Batch) {
 				Ts:         timestamps[i].ToTimestamp(),
 			}
 			cc.tables.addTableItem(newItem)
+
+			key := TableKey{
+				AccountId:  item.AccountId,
+				DatabaseId: item.DatabaseId,
+				Name:       item.Name,
+			}
+
+			oldVersion := cc.tables.tableGuard.getSchemaVersion(key)
+
+			if oldVersion != nil && oldVersion.TableId != item.Id {
+				// drop old table for alter table stmt
+				oldVersion.Version = math.MaxUint32
+				cc.tables.tableGuard.setSchemaVersion(key, oldVersion)
+			} else {
+				// normal drop table stmt
+				cc.tables.tableGuard.setSchemaVersion(key, &TableVersion{
+					Version: math.MaxUint32,
+					Ts:      &item.Ts,
+				})
+			}
 		}
 	}
 }
@@ -362,7 +383,8 @@ func (cc *CatalogCache) InsertTable(bat *batch.Batch) {
 		item.ClusterByIdx = -1
 		copy(item.Rowid[:], rowids[i][:])
 		// invalid old name table
-		if exist, ok := cc.tables.rowidIndex.Get(&TableItem{Rowid: rowids[i]}); ok && exist.Name != item.Name {
+		exist, ok := cc.tables.rowidIndex.Get(&TableItem{Rowid: rowids[i]})
+		if ok && exist.Name != item.Name {
 			logutil.Infof("rename invalidate %d-%s,v%d@%s", exist.Id, exist.Name, exist.Version, item.Ts.String())
 			newItem := &TableItem{
 				deleted:    true,
@@ -375,7 +397,30 @@ func (cc *CatalogCache) InsertTable(bat *batch.Batch) {
 				Ts:         item.Ts,
 			}
 			cc.tables.addTableItem(newItem)
+
+			key := TableKey{
+				AccountId:  account,
+				DatabaseId: item.DatabaseId,
+				Name:       exist.Name,
+			}
+			cc.tables.tableGuard.setSchemaVersion(key, &TableVersion{
+				Version: math.MaxUint32,
+				Ts:      &item.Ts,
+				TableId: item.Id,
+			})
 		}
+
+		key := TableKey{
+			AccountId:  account,
+			DatabaseId: item.DatabaseId,
+			Name:       item.Name,
+		}
+
+		cc.tables.tableGuard.setSchemaVersion(key, &TableVersion{
+			Version: item.Version,
+			Ts:      &item.Ts,
+			TableId: item.Id,
+		})
 		cc.tables.addTableItem(item)
 		cc.tables.rowidIndex.Set(item)
 	}
@@ -585,21 +630,12 @@ func getTableDef(name string, defs []engine.TableDef) *plan.TableDef {
 	}
 }
 
-// GetDeletedTableIndex returns the max index of deleted tables slice.
-func (cc *CatalogCache) GetDeletedTableIndex() int {
-	return cc.tables.tableGuard.getDeletedTableIndex()
+// GetSchemaVersion returns the version of table
+func (cc *CatalogCache) GetSchemaVersion(name TableKey) *TableVersion {
+	return cc.tables.tableGuard.getSchemaVersion(name)
 }
 
-// GetDeletedTables returns the deleted tables in [cachedIndex+1:] whose timestamp is less than ts.
-func (cc *CatalogCache) GetDeletedTables(cachedIndex int, ts timestamp.Timestamp) []*TableItem {
-	return cc.tables.tableGuard.getDeletedTables(cachedIndex, ts)
-}
-
-// addTableItem inserts a new table item. If it is a deleted one, also push the
-// item into tableCache.tableGuard.mu.deletedTables.
+// addTableItem inserts a new table item.
 func (c *tableCache) addTableItem(item *TableItem) {
 	c.data.Set(item)
-	if item != nil && item.deleted {
-		c.tableGuard.pushDeletedTable(item)
-	}
 }
