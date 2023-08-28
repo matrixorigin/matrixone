18157547ee30b52624196ae6979516896b47f84a
YANGGMM <18916107305@163.com>
2023-08-28 14:50:10 +0800

    fix ut test

--- pkg/frontend/authenticate_test.go
+++ pkg/frontend/authenticate_test.go
@@ -9981,6 +9981,7 @@ func TestDoCheckFilePath(t *testing.T) {
 		ses.SetTenantInfo(tenant)
 
 		cs := &tree.Select{}
+		ses.InitExportConfig(cs.Ep)
 
 		//no result set
 		bh.sql2result["begin;"] = nil
@@ -10026,6 +10027,7 @@ func TestDoCheckFilePath(t *testing.T) {
 				FilePath: "/mnt/disk1/t1.csv",
 			},
 		}
+		ses.InitExportConfig(cs.Ep)
 
 		//no result set
 		bh.sql2result["begin;"] = nil
@@ -10075,6 +10077,7 @@ func TestDoCheckFilePath(t *testing.T) {
 				FilePath: "/mnt/disk1/t1.csv",
 			},
 		}
+		ses.InitExportConfig(cs.Ep)
 
 		//no result set
 		bh.sql2result["begin;"] = nil
@@ -10126,6 +10129,7 @@ func TestDoCheckFilePath(t *testing.T) {
 				FilePath: "stage1:/t1.csv",
 			},
 		}
+		ses.InitExportConfig(cs.Ep)
 
 		//no result set
 		bh.sql2result["begin;"] = nil
@@ -10175,6 +10179,7 @@ func TestDoCheckFilePath(t *testing.T) {
 				FilePath: "stage1:/t1.csv",
 			},
 		}
+		ses.InitExportConfig(cs.Ep)
 
 		//no result set
 		bh.sql2result["begin;"] = nil
@@ -10226,6 +10231,7 @@ func TestDoCheckFilePath(t *testing.T) {
 				FilePath: "stage1:/t1.csv",
 			},
 		}
+		ses.InitExportConfig(cs.Ep)
 
 		//no result set
 		bh.sql2result["begin;"] = nil
