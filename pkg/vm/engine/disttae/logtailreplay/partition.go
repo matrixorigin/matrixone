9d6d1cf300a8cd6b45ede0b4da9ed4bbf24892d7
chenmingsong <mschenjj@foxmail.com>
2023-08-30 15:21:27 +0800

    fix a bug will cause deadlock

--- pkg/vm/engine/disttae/logtailreplay/partition.go
+++ pkg/vm/engine/disttae/logtailreplay/partition.go
@@ -15,103 +15,107 @@
 package logtailreplay
 
 import (
-	"bytes"
-	"context"
-	"sync/atomic"
+    "bytes"
+    "context"
+    "sync/atomic"
 
-	"github.com/matrixorigin/matrixone/pkg/container/types"
-	"github.com/matrixorigin/matrixone/pkg/pb/timestamp"
+    "github.com/matrixorigin/matrixone/pkg/container/types"
+    "github.com/matrixorigin/matrixone/pkg/pb/timestamp"
 )
 
 // a partition corresponds to a dn
 type Partition struct {
-	lock  chan struct{}
-	state atomic.Pointer[PartitionState]
-	TS    timestamp.Timestamp // last updated timestamp
+    lock  chan struct{}
+    state atomic.Pointer[PartitionState]
+    TS    timestamp.Timestamp // last updated timestamp
 
-	// assuming checkpoints will be consumed once
-	checkpointConsumed atomic.Bool
+    // assuming checkpoints will be consumed once
+    checkpointConsumed atomic.Bool
 }
 
 func NewPartition() *Partition {
-	lock := make(chan struct{}, 1)
-	lock <- struct{}{}
-	ret := &Partition{
-		lock: lock,
-	}
-	ret.state.Store(NewPartitionState(false))
-	return ret
+    lock := make(chan struct{}, 1)
+    lock <- struct{}{}
+    ret := &Partition{
+        lock: lock,
+    }
+    ret.state.Store(NewPartitionState(false))
+    return ret
 }
 
 type RowID types.Rowid
 
 func (r RowID) Less(than RowID) bool {
-	return bytes.Compare(r[:], than[:]) < 0
+    return bytes.Compare(r[:], than[:]) < 0
 }
 
 func (p *Partition) Snapshot() *PartitionState {
-	return p.state.Load()
+    return p.state.Load()
 }
 
 func (*Partition) CheckPoint(ctx context.Context, ts timestamp.Timestamp) error {
-	panic("unimplemented")
+    panic("unimplemented")
 }
 
 func (p *Partition) MutateState() (*PartitionState, func()) {
-	curState := p.state.Load()
-	state := curState.Copy()
-	return state, func() {
-		if !p.state.CompareAndSwap(curState, state) {
-			panic("concurrent mutation")
-		}
-	}
+    curState := p.state.Load()
+    state := curState.Copy()
+    return state, func() {
+        if !p.state.CompareAndSwap(curState, state) {
+            panic("concurrent mutation")
+        }
+    }
 }
 
-func (p *Partition) Lock() <-chan struct{} {
-	return p.lock
+func (p *Partition) Lock(ctx context.Context) error {
+    select {
+    case <-p.lock:
+        return nil
+    case <-ctx.Done():
+        return ctx.Err()
+    }
 }
 
 func (p *Partition) Unlock() {
-	p.lock <- struct{}{}
+    p.lock <- struct{}{}
 }
 
 func (p *Partition) ConsumeCheckpoints(
-	ctx context.Context,
-	fn func(
-		checkpoint string,
-		state *PartitionState,
-	) error,
+    ctx context.Context,
+    fn func(
+    checkpoint string,
+    state *PartitionState,
+) error,
 ) (
-	err error,
+    err error,
 ) {
 
-	if p.checkpointConsumed.Load() {
-		return nil
-	}
+    if p.checkpointConsumed.Load() {
+        return nil
+    }
 
-	select {
-	case <-p.Lock():
-		defer p.Unlock()
-	case <-ctx.Done():
-		return ctx.Err()
-	}
+    lockErr := p.Lock(ctx)
+    if lockErr != nil {
+        return lockErr
+    }
+    defer p.Unlock()
 
-	curState := p.state.Load()
-	if len(curState.checkpoints) == 0 {
-		return nil
-	}
+    curState := p.state.Load()
+    if len(curState.checkpoints) == 0 {
+        return nil
+    }
 
-	state := curState.Copy()
+    state := curState.Copy()
 
-	if err := state.consumeCheckpoints(fn); err != nil {
-		return err
-	}
+    if err := state.consumeCheckpoints(fn); err != nil {
+        return err
+    }
 
-	if !p.state.CompareAndSwap(curState, state) {
-		panic("concurrent mutation")
-	}
+    if !p.state.CompareAndSwap(curState, state) {
+        panic("concurrent mutation")
+    }
 
-	p.checkpointConsumed.Store(true)
+    p.checkpointConsumed.Store(true)
 
-	return
+    return
 }
