b0e899ff462ea92e1b70a8a81ac753904149bd19
Ariznawl <ariznawl@163.com>
2023-08-30 11:08:24 +0800

    add tag

--- test/distributed/cases/pessimistic_transaction/transaction_enhance.sql
+++ test/distributed/cases/pessimistic_transaction/transaction_enhance.sql
@@ -419,6 +419,7 @@ show create table atomic_table_12_5;
 
 
 -- alter table rename primary key column
+-- @bvt:issue#11213
 drop table if exists alter01;
 create table alter01(col1 int primary key,col2 varchar(25));
 insert into alter01 values (3,"a"),(4,"b"),(5,"c");
@@ -432,6 +433,7 @@ select * from alter01;
 -- @session
 insert into alter01 values (6,"h");
 select * from alter01;
+-- @bvt:issue
 
 ----------------------------------------------------------
 -- alter table add primary key column
