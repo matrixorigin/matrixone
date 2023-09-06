94945b4a7cd999b31260b22426e2aaa3536302f1
maomao <77312370+sukki37@users.noreply.github.com>
2023-09-06 22:59:41 +0800

    Revert "split large batch in inner join (#11673)"
    This reverts commit 74bda48af43399cfc3390e6dda8edc6170828dea.

--- pkg/sql/colexec/join/types.go
+++ pkg/sql/colexec/join/types.go
@@ -29,7 +29,6 @@ const (
 	Build = iota
 	Probe
 	End
-	Find
 )
 
 type evalVector struct {
@@ -58,11 +57,6 @@ type container struct {
 	vecs  []*vector.Vector
 
 	mp *hashmap.JoinMap
-
-	itr  hashmap.Iterator
-	i    int
-	cb   *batch.Batch
-	bats []*batch.Batch
 }
 
 type Argument struct {
@@ -109,14 +103,6 @@ func (ctr *container) cleanBatch(mp *mpool.MPool) {
 	if ctr.joinBat2 != nil {
 		ctr.joinBat2.Clean(mp)
 		ctr.joinBat2 = nil
-	}
-	if len(ctr.bats) > 0 {
-		for i := range ctr.bats {
-			ctr.bats[i].Clean(mp)
-		}
-	}
-	if ctr.cb != nil {
-		ctr.cb.Clean(mp)
 	}
 }
 
