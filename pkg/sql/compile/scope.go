7d11f95b89eaa9ccfadf18fee2b7acc89d5509e3
chenmingsong <mschenjj@foxmail.com>
2023-09-08 15:42:33 +0800

    change all Run method of scope to ignore the error if ctx is done

--- pkg/sql/compile/scope.go
+++ pkg/sql/compile/scope.go
@@ -81,17 +81,18 @@ func (s *Scope) Run(c *Compile) (err error) {
 	} else {
 		p = pipeline.New(s.DataSource.Attributes, s.Instructions, s.Reg)
 		if s.DataSource.Bat != nil {
-			if _, err = p.ConstRun(s.DataSource.Bat, s.Proc); err != nil {
-				return err
-			}
+			_, err = p.ConstRun(s.DataSource.Bat, s.Proc)
 		} else {
-			if _, err = p.Run(s.DataSource.R, s.Proc); err != nil {
-				return err
-			}
+			_, err = p.Run(s.DataSource.R, s.Proc)
 		}
 	}
 
-	return nil
+	select {
+	case <-s.Proc.Ctx.Done():
+		err = nil
+	default:
+	}
+	return err
 }
 
 func (s *Scope) SetContextRecursively(ctx context.Context) {
@@ -115,20 +116,16 @@ func (s *Scope) MergeRun(c *Compile) error {
 			switch scope.Magic {
 			case Normal:
 				errChan <- scope.Run(c)
-				wg.Done()
 			case Merge, MergeInsert:
 				errChan <- scope.MergeRun(c)
-				wg.Done()
 			case Remote:
 				errChan <- scope.RemoteRun(c)
-				wg.Done()
 			case Parallel:
 				errChan <- scope.ParallelRun(c, scope.IsRemote)
-				wg.Done()
 			case Pushdown:
 				errChan <- scope.PushdownRun()
-				wg.Done()
 			}
+			wg.Done()
 		})
 	}
 	defer wg.Wait()
@@ -141,14 +138,19 @@ func (s *Scope) MergeRun(c *Compile) error {
 	}
 	p := pipeline.NewMerge(s.Instructions, s.Reg)
 	if _, err := p.MergeRun(s.Proc); err != nil {
-		p.Cleanup(s.Proc, true)
-		return err
+		select {
+		case <-s.Proc.Ctx.Done():
+		default:
+			p.Cleanup(s.Proc, true)
+			return err
+		}
 	}
 	p.Cleanup(s.Proc, false)
 
-	// check sub-goroutine's error
-	if errReceiveChan == nil {
-		// check sub-goroutine's error
+	// receive and check error from pre-scopes and remote scopes.
+	preScopeCount := len(s.PreScopes)
+	remoteScopeCount := len(s.RemoteReceivRegInfos)
+	if remoteScopeCount == 0 {
 		for i := 0; i < len(s.PreScopes); i++ {
 			if err := <-errChan; err != nil {
 				return err
@@ -157,23 +159,22 @@ func (s *Scope) MergeRun(c *Compile) error {
 		return nil
 	}
 
-	slen := len(s.PreScopes)
-	rlen := len(s.RemoteReceivRegInfos)
 	for {
 		select {
 		case err := <-errChan:
 			if err != nil {
 				return err
 			}
-			slen--
+			preScopeCount--
+
 		case err := <-errReceiveChan:
 			if err != nil {
 				return err
 			}
-			rlen--
+			remoteScopeCount--
 		}
 
-		if slen == 0 && rlen == 0 {
+		if preScopeCount == 0 && remoteScopeCount == 0 {
 			return nil
 		}
 	}
