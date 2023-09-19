6659c2190c548d75f2e9b784c11bc6fd9c5e20cf
daviszhen <daviszhen007@gmail.com>
2023-09-19 21:09:05 +0800

    add mo_locks

--- pkg/cnservice/upgrader/newAddTable.go
+++ pkg/cnservice/upgrader/newAddTable.go
@@ -177,5 +177,23 @@ var MoSessionsView = &table.Table{
 	CreateTableSql: "drop view `mo_catalog`.`mo_sessions`;",
 }
 
-var needUpgradNewView = []*table.Table{PARTITIONSView, STATISTICSView, MoSessionsView}
+var MoLocksView = &table.Table{
+	Account:  table.AccountAll,
+	Database: catalog.MO_CATALOG,
+	Table:    "mo_locks",
+	Columns: []table.Column{
+		table.StringColumn("txn_id", "the txn id which holds the lock"),
+		table.StringColumn("table_id", "the table that the lock is on"),
+		table.StringColumn("lock_key", "point or range"),
+		table.UInt64Column("lock_content", "the content the clock is on"),
+		table.StringColumn("lock_mode", "shared or exclusive"),
+		table.StringColumn("lock_status", "acquired or wait"),
+		table.StringColumn("lock_wait", "the txn that waits on the lock"),
+	},
+	CreateViewSql: "CREATE VIEW IF NOT EXISTS `mo_catalog`.`mo_locks` AS SELECT * FROM mo_locks() AS mo_locks_tmp;",
+	//actually drop view here
+	CreateTableSql: "drop view `mo_catalog`.`mo_locks`;",
+}
+
+var needUpgradNewView = []*table.Table{PARTITIONSView, STATISTICSView, MoSessionsView, MoLocksView}
 var registeredViews = []*table.Table{processlistView}
