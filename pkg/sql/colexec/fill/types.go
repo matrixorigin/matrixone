64f86b3bf9150e7cb0d97dda9b810b524d370ed7
iamlinjunhong <1030420200@qq.com>
2023-09-19 15:15:31 +0800

    time window and fill query

---
+++ pkg/sql/colexec/fill/types.go
@@ -0,0 +1,94 @@
+// Copyright 2021 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fill
+
+import (
+	"github.com/matrixorigin/matrixone/pkg/common/mpool"
+	"github.com/matrixorigin/matrixone/pkg/container/batch"
+	"github.com/matrixorigin/matrixone/pkg/container/vector"
+	"github.com/matrixorigin/matrixone/pkg/pb/plan"
+	"github.com/matrixorigin/matrixone/pkg/sql/colexec"
+	"github.com/matrixorigin/matrixone/pkg/vm/process"
+)
+
+const (
+	receiveBat    = 0
+	withoutNewBat = 1
+	findNull      = 2
+	findValue     = 3
+	fillValue     = 4
+
+	findNullPre = 5
+)
+
+type container struct {
+	colexec.ReceiverOperator
+
+	// value
+	valVecs []*vector.Vector
+
+	// prev
+	prevVecs []*vector.Vector
+
+	// next
+	bats      []*batch.Batch
+	preIdx    int
+	preRow    int
+	curIdx    int
+	curRow    int
+	status    int
+	subStatus int
+	colIdx    int
+
+	// linear
+	nullIdx int
+	nullRow int
+	exes    []colexec.ExpressionExecutor
+
+	process func(ctr *container, ap *Argument, proc *process.Process, anal process.Analyze) (process.ExecStatus, error)
+}
+
+type Argument struct {
+	ctr *container
+
+	ColLen   int
+	FillType plan.Node_FillType
+	FillVal  []*plan.Expr
+	AggIds   []int32
+}
+
+func (arg *Argument) Free(proc *process.Process, pipelineFailed bool, err error) {
+	ctr := arg.ctr
+	if ctr != nil {
+		ctr.cleanBatch(proc.Mp())
+		ctr.cleanExes()
+	}
+}
+
+func (ctr *container) cleanBatch(mp *mpool.MPool) {
+	for _, b := range ctr.bats {
+		if b != nil {
+			b.Clean(mp)
+		}
+	}
+}
+
+func (ctr *container) cleanExes() {
+	for i := range ctr.exes {
+		if ctr.exes[i] != nil {
+			ctr.exes[i].Free()
+		}
+	}
+}
---
+++ pkg/sql/colexec/timewin/types.go
@@ -0,0 +1,154 @@
+// Copyright 2021 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package timewin
+
+import (
+	"github.com/matrixorigin/matrixone/pkg/common/mpool"
+	"github.com/matrixorigin/matrixone/pkg/container/batch"
+	"github.com/matrixorigin/matrixone/pkg/container/types"
+	"github.com/matrixorigin/matrixone/pkg/container/vector"
+	"github.com/matrixorigin/matrixone/pkg/pb/plan"
+	"github.com/matrixorigin/matrixone/pkg/sql/colexec"
+	"github.com/matrixorigin/matrixone/pkg/sql/colexec/agg"
+	"github.com/matrixorigin/matrixone/pkg/vm/process"
+)
+
+const (
+	initTag     = 0
+	evalTag     = 1
+	nextTag     = 2
+	dataTag     = 3
+	endTag      = 4
+	evalLastCur = 5
+	evalLastPre = 6
+	resultTag   = 7
+)
+
+type preType int
+
+const (
+	withoutPre preType = iota
+	hasPre
+)
+
+type curType int
+
+const (
+	withoutGrow curType = iota
+	hasGrow
+)
+
+type container struct {
+	colexec.ReceiverOperator
+
+	rbat   *batch.Batch
+	colCnt int
+
+	bats []*batch.Batch
+
+	aggExe []colexec.ExpressionExecutor
+	aggVec [][]*vector.Vector
+
+	tsExe colexec.ExpressionExecutor
+	tsVec []*vector.Vector
+
+	tsOid types.T
+	tsTyp *types.Type
+
+	status int32
+
+	start     int64
+	end       int64
+	nextStart int64
+
+	pre    preType
+	preRow int
+	preIdx int
+
+	cur    curType
+	curRow int
+	curIdx int
+
+	group int
+	aggs  []agg.Agg[any]
+
+	wstart []int64
+	wend   []int64
+
+	calRes func(ctr *container, ap *Argument, proc *process.Process) (err error)
+	eval   func(ctr *container, ap *Argument, proc *process.Process) (err error)
+}
+
+type Argument struct {
+	ctr *container
+
+	Types []types.Type
+	Aggs  []agg.Aggregate
+
+	Interval *Interval
+	Sliding  *Interval
+	Ts       *plan.Expr
+
+	WStart bool
+	WEnd   bool
+}
+
+type Interval struct {
+	Typ types.IntervalType
+	Val int64
+}
+
+func (arg *Argument) Free(proc *process.Process, pipelineFailed bool, err error) {
+	ctr := arg.ctr
+	if ctr != nil {
+		ctr.cleanBatch(proc.Mp())
+		ctr.cleanTsVector()
+		ctr.cleanAggVector()
+		ctr.cleanWin()
+	}
+}
+
+func (ctr *container) cleanBatch(mp *mpool.MPool) {
+	if ctr.rbat != nil {
+		ctr.rbat.Clean(mp)
+	}
+	for _, b := range ctr.bats {
+		if b != nil {
+			b.Clean(mp)
+		}
+	}
+}
+
+func (ctr *container) cleanTsVector() {
+	if ctr.tsExe != nil {
+		ctr.tsExe.Free()
+	}
+	ctr.tsVec = nil
+}
+
+func (ctr *container) cleanAggVector() {
+	for i := range ctr.aggExe {
+		if ctr.aggExe[i] != nil {
+			ctr.aggExe[i].Free()
+		}
+	}
+	ctr.aggVec = nil
+}
+
+func (ctr *container) cleanWin() {
+	ctr.wstart = nil
+	ctr.wend = nil
+	ctr.aggs = nil
+}
