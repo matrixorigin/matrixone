69a9a5aa6967a724efd40fa8c7df9476ad263617
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-09-25 10:43:11 -0700

    Alter done.

--- pkg/sql/plan/alter_util.go
+++ pkg/sql/plan/alter_util.go
@@ -32,7 +32,9 @@ func checkDropColumnWithPrimaryKey(colName string, pkey *plan.PrimaryKeyDef, ctx
 
 func checkDropColumnWithIndex(colName string, indexes []*plan.IndexDef, ctx CompilerContext) error {
 	for _, indexInfo := range indexes {
-		if indexInfo.Unique {
+		// check both Unique and Secondary Index
+		//TODO: need to verify if this works or not.
+		if indexInfo.TableExist {
 			for _, column := range indexInfo.Parts {
 				if column == colName {
 					return moerr.NewInvalidInput(ctx.GetContext(), "can't drop column %s with unique index covered now", colName)
