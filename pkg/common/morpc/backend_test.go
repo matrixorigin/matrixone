1a02bac5975c67a6c94b8f294acee5ddf3da117c
fagongzi <zhangxu19830126@gmail.com>
2023-10-07 12:05:59 +0800

    fix heartbeat context deadline not set (#11968)
    ## What type of PR is this?
    
    - [ ] API-change
    - [x] BUG
    - [ ] Improvement
    - [ ] Documentation
    - [ ] Feature
    - [ ] Test and CI
    - [ ] Code Refactoring
    
    ## Which issue(s) this PR fixes:
    
    issue #11964 #11957
    
    ## What this PR does / why we need it:
    Fix context deadline not set  when send heartbeat message
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pkg/common/morpc/backend_test.go
+++ pkg/common/morpc/backend_test.go
@@ -58,6 +58,44 @@ func TestSend(t *testing.T) {
 	)
 }
 
+func TestReadTimeoutWithNormalMessageMissed(t *testing.T) {
+	testBackendSend(t,
+		func(conn goetty.IOSession, msg interface{}, _ uint64) error {
+			request := msg.(RPCMessage)
+			if request.internal {
+				if m, ok := request.Message.(*flagOnlyMessage); ok {
+					switch m.flag {
+					case flagPing:
+						return conn.Write(RPCMessage{
+							Ctx:      request.Ctx,
+							internal: true,
+							Message: &flagOnlyMessage{
+								flag: flagPong,
+								id:   m.id,
+							},
+						}, goetty.WriteOptions{Flush: true})
+					default:
+						panic(fmt.Sprintf("invalid internal message, flag %d", m.flag))
+					}
+				}
+			}
+			// no response
+			return nil
+		},
+		func(b *remoteBackend) {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+			req := newTestMessage(1)
+			f, err := b.Send(ctx, req)
+			assert.NoError(t, err)
+			defer f.Close()
+			_, err = f.Get()
+			assert.Equal(t, ctx.Err(), err)
+		},
+		WithBackendReadTimeout(time.Millisecond*200),
+	)
+}
+
 func TestReadTimeout(t *testing.T) {
 	testBackendSend(t,
 		func(conn goetty.IOSession, msg interface{}, _ uint64) error {
@@ -71,9 +109,8 @@ func TestReadTimeout(t *testing.T) {
 			f, err := b.Send(ctx, req)
 			assert.NoError(t, err)
 			defer f.Close()
-
 			_, err = f.Get()
-			assert.Error(t, err)
+			assert.Equal(t, backendClosed, err)
 		},
 		WithBackendReadTimeout(time.Millisecond*200),
 	)
