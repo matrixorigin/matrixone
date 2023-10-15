eb849f6844be87bd3def3a3ccc23d0291b7e68fb
triump2020 <triump2020@outlook.com>
2023-10-15 23:34:52 +0800

    fix datarace for txn.meta.snapshotts

--- pkg/sql/colexec/lockop/lock_op_test.go
+++ pkg/sql/colexec/lockop/lock_op_test.go
@@ -260,7 +260,6 @@ func TestCallLockOpWithHasPrevCommitLessMe(t *testing.T) {
 				defer arg2.rt.parker.FreeMem()
 
 				proc.TxnOperator.TxnRef().SnapshotTS = timestamp.Timestamp{PhysicalTime: math.MaxInt64}
-
 				_, err = Call(0, proc, arg2, false, false)
 				assert.NoError(t, err)
 
