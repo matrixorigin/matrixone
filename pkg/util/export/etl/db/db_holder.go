b7b782161cf4211f0c7b3f3f6d379ef34aa8e3e4
gaviny <gyue@outlook.com>
2023-10-17 20:36:02 -0700

    add stats to the record

--- pkg/util/export/etl/db/db_holder.go
+++ pkg/util/export/etl/db/db_holder.go
@@ -237,9 +237,9 @@ func bulkInsert(ctx context.Context, sqlDb *sql.DB, records [][]string, tbl *tab
 	// append the stats to the CSV
 	csvDataSize := len(csvWriter.GetContent())
 
-	numColumns := len(records[0]) // Assuming all records have the same number of columns
-	additionalLineSlice := make([]string, numColumns)
-	additionalLine := fmt.Sprintf("%d r %d b", len(records), csvDataSize)
+	additionalLineSlice := make([]string, len(records[0]))
+	copy(additionalLineSlice, records[0])
+	additionalLine := fmt.Sprintf("s: %d r %d b", len(records), csvDataSize)
 	additionalLineSlice[0] = additionalLine
 
 	if err := csvWriter.WriteStrings(additionalLineSlice); err != nil {
