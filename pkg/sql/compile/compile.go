ba9735ae80e191e53ed38fc1d89802aabcd8d06d
qingxinhome <758355272@qq.com>
2023-10-19 10:33:45 +0800

    modify comment

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -886,6 +886,10 @@ func (c *Compile) compilePlanScope(ctx context.Context, step int32, curNodeIdx i
 		}
 		return c.compileSort(n, c.compileProjection(n, c.compileRestrict(node, ss))), nil
 	case plan.Node_TABLE_SCAN:
+		if n.TableDef.Name == "employees" {
+			fmt.Println("---------------------------------")
+		}
+
 		ss, err := c.compileTableScan(n)
 		if err != nil {
 			return nil, err
