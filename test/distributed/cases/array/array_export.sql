6ef642f3099f1fe62e840768584d1fc1ef3b42c7
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-10-20 14:23:41 -0700

    Fix a bug in BVT

--- test/distributed/cases/array/array_export.sql
+++ test/distributed/cases/array/array_export.sql
@@ -5,17 +5,16 @@ use vecdb2;
 drop table if exists vec_table;
 create table vec_table(a int, b vecf32(3), c vecf64(3));
 
--- read old file
-load data infile '$resources/array/array_out.csv' into table vec_table ignore 1 lines;
+-- read from an already exported file
+load data infile '$resources/array/import/array_out.csv' into table vec_table ignore 1 lines;
 select * from vec_table;
 
--- write new file
--- NOTE: This does not work, if array_out.csv file already present in the resource folder.
-select * from vec_table into outfile '$resources/array/array_out.csv';
+-- write a new export file
+select * from vec_table into outfile '$resources/array/export/array_out.csv';
 delete from vec_table;
 
--- read new file
-load data infile '$resources/array/array_out.csv' into table vec_table ignore 1 lines;
+-- read from the newly exported file
+load data infile '$resources/array/export/array_out.csv' into table vec_table ignore 1 lines;
 select * from vec_table;
 
 -- post
