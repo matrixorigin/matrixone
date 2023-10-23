633fda5ba00bf0fa6597e22bc00d7c48d4cdb505
YANGGMM <18916107305@163.com>
2023-10-23 15:15:56 +0800

    fix

--- pkg/sql/compile/ddl.go
+++ pkg/sql/compile/ddl.go
@@ -18,7 +18,6 @@ import (
 	"context"
 	"fmt"
 	"math"
-	"strconv"
 	"strings"
 
 	"github.com/matrixorigin/matrixone/pkg/catalog"
@@ -2193,10 +2192,10 @@ func makeAlterSequenceParam[T constraints.Integer](ctx context.Context, stmt *tr
 	var minValue, maxValue, startNum, lastNum T
 	var incrNum int64
 	var cycle bool
-	var err error
 
+	var preincrNum int64
 	if incr, ok := result[4].(int64); ok {
-		incrNum = incr
+		preincrNum = incr
 	}
 
 	// if alter increment value
@@ -2206,6 +2205,8 @@ func makeAlterSequenceParam[T constraints.Integer](ctx context.Context, stmt *tr
 			return 0, 0, 0, 0, 0, false, moerr.NewInvalidInput(ctx, "incr value's data type is int64")
 		}
 		incrNum = getValue[int64](stmt.IncrementBy.Minus, stmt.IncrementBy.Num)
+	} else {
+		incrNum = preincrNum
 	}
 	if incrNum == 0 {
 		return 0, 0, 0, 0, 0, false, moerr.NewInvalidInput(ctx, "Incr value for sequence must not be 0")
@@ -2227,16 +2228,10 @@ func makeAlterSequenceParam[T constraints.Integer](ctx context.Context, stmt *tr
 		maxValue = getInterfaceValue[T](preMaxValue)
 	}
 
+	preLastSeq := result[0]
+	preLastSeqNum := getInterfaceValue[T](preLastSeq)
 	// if alter startWith value of sequence
-	var preStartWith int64
-	if len(curval) == 0 {
-		preStartWith = getInterfaceValue[int64](result[3])
-	} else {
-		preStartWith, err = strconv.ParseInt(curval, 10, 64)
-		if err != nil {
-			return 0, 0, 0, 0, 0, false, moerr.NewInvalidInput(ctx, "Alter sequence currval parse err")
-		}
-	}
+	preStartWith := preLastSeqNum
 	if stmt.StartWith != nil {
 		startNum = getValue[T](stmt.StartWith.Minus, stmt.StartWith.Num)
 		if startNum < T(preStartWith) {
@@ -2245,7 +2240,21 @@ func makeAlterSequenceParam[T constraints.Integer](ctx context.Context, stmt *tr
 	} else {
 		startNum = getInterfaceValue[T](preStartWith)
 	}
-	lastNum = startNum + T(incrNum)
+	if len(curval) != 0 {
+		lastNum = preLastSeqNum + T(incrNum)
+		if lastNum < startNum+T(incrNum) {
+			lastNum = startNum + T(incrNum)
+		}
+	} else {
+		if stmt.IncrementBy != nil {
+			lastNum = preLastSeqNum + T(incrNum) - T(preincrNum)
+			if lastNum < startNum+T(incrNum)-T(preincrNum) {
+				lastNum = startNum + T(incrNum) - T(preincrNum)
+			}
+		} else {
+			lastNum = preLastSeqNum
+		}
+	}
 
 	// if alter cycle state of sequence
 	preCycle := result[5]
@@ -2262,11 +2271,11 @@ func makeAlterSequenceParam[T constraints.Integer](ctx context.Context, stmt *tr
 
 // Checkout values.
 func valueCheckOut[T constraints.Integer](maxValue, minValue, startNum T, ctx context.Context) error {
-	if maxValue < minValue {
-		return moerr.NewInvalidInput(ctx, "Max value of sequence must be bigger than min value of it")
+	if maxValue <= minValue {
+		return moerr.NewInvalidInput(ctx, "MAXVALUE (%d) of sequence must be bigger than MINVALUE (%d) of it", maxValue, minValue)
 	}
 	if startNum < minValue || startNum > maxValue {
-		return moerr.NewInvalidInput(ctx, "Start value for sequence must between minvalue and maxvalue")
+		return moerr.NewInvalidInput(ctx, "STARTVALUE (%d) for sequence must between MINVALUE (%d) and MAXVALUE (%d)", startNum, minValue, maxValue)
 	}
 	return nil
 }
