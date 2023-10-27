4305daaccf367552983ab74afd3f0272e1fcd7a4
triump2020 <triump2020@outlook.com>
2023-10-27 23:36:13 +0800

    fix merge main

--- pkg/sql/parsers/dialect/mysql/mysql_sql.go
+++ pkg/sql/parsers/dialect/mysql/mysql_sql.go
@@ -11624,7 +11624,7 @@ yydefault:
 //line mysql_sql.y:3025
 		{
 			assignments := []*tree.VarAssignmentExpr{
-				&tree.VarAssignmentExpr{
+				{
 					System: true,
 					Global: true,
 					Name:   yyDollar[6].str,
