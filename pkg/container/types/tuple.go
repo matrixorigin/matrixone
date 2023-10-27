135e7094dadbb1f46f20254c89e3bbf53d0341f7
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-10-27 01:33:41 -0700

    Fix UnmarshalBinary issue

--- pkg/container/types/tuple.go
+++ pkg/container/types/tuple.go
@@ -718,10 +718,6 @@ func decodeTuple(b []byte) (Tuple, int, error) {
 			el, off = decodeBytes(b[i+1:])
 			off += 1
 		default:
-			//ERROR:
-			//panic(fmt.Sprintf("unknown typecode %v %v %v %02x", b[i] == enumCode, b[i], enumCode, b[i]))
-			// ERROR 20101 (HY000): internal error: panic unknown typecode false 128 80 80:
-			// ERROR 20101 (HY000): internal error: panic unknown typecode false 240 80 f0:
 			return nil, i, moerr.NewInternalErrorNoCtx("unable to decode tuple element with unknown typecode %02x", b[i])
 		}
 		t = append(t, el)
