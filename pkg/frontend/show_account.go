7bdd53f5ff211258f59324aa207bbe73f757e2af
gouhongshen <gouhongshen@hotmail.com>
2023-10-31 21:46:34 +0800

    [fix]: fix overwritten ses.proc

--- pkg/frontend/show_account.go
+++ pkg/frontend/show_account.go
@@ -28,6 +28,7 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/pb/plan"
 	"github.com/matrixorigin/matrixone/pkg/sql/parsers/tree"
 	ctl2 "github.com/matrixorigin/matrixone/pkg/sql/plan/function/ctl"
+	"github.com/matrixorigin/matrixone/pkg/txn/client"
 	v2 "github.com/matrixorigin/matrixone/pkg/util/metric/v2"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/db"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/logtail"
@@ -127,7 +128,7 @@ func getSqlForTableStats(accountId int32) string {
 	return fmt.Sprintf(getTableStatsFormatV2, catalog.SystemPartitionRel, accountId)
 }
 
-func requestStorageUsage(ctx context.Context, ses *Session) (resp interface{}, err error) {
+func requestStorageUsage(ses *Session) (resp interface{}, err error) {
 	whichTN := func(string) ([]uint64, error) { return nil, nil }
 	payload := func(tnShardID uint64, parameter string, proc *process.Process) ([]byte, error) { return nil, nil }
 	responseUnmarshaler := func(payload []byte) (interface{}, error) {
@@ -138,12 +139,22 @@ func requestStorageUsage(ctx context.Context, ses *Session) (resp interface{}, e
 		return usage, nil
 	}
 
-	if ses.proc.Ctx, ses.proc.TxnOperator, err = ses.txnHandler.GetTxn(); err != nil {
+	var ctx context.Context
+	var txnOperator client.TxnOperator
+	if ctx, txnOperator, err = ses.txnHandler.GetTxn(); err != nil {
 		return nil, err
 	}
 
+	// create a new proc for `handler`
+	proc := process.New(ctx, ses.proc.GetMPool(),
+		ses.proc.TxnClient, txnOperator,
+		ses.proc.FileService, ses.proc.LockService,
+		ses.proc.QueryService, ses.proc.Hakeeper,
+		ses.proc.UdfService, ses.proc.Aicm,
+	)
+
 	handler := ctl2.GetTNHandlerFunc(ctl.CmdMethod_StorageUsage, whichTN, payload, responseUnmarshaler)
-	result, err := handler(ses.proc, "DN", "", ctl2.MoCtlTNCmdSender)
+	result, err := handler(proc, "DN", "", ctl2.MoCtlTNCmdSender)
 	if moerr.IsMoErrCode(err, moerr.ErrNotSupported) {
 		return nil, moerr.NewNotSupportedNoCtx("current tn version not supported `show accounts`")
 	} else if err != nil {
@@ -195,7 +206,7 @@ func handleStorageUsageResponse(ctx context.Context, fs fileservice.FileService,
 // by handling checkpoint
 func getAllAccountsStorageUsage(ctx context.Context, ses *Session) (map[int32]uint64, error) {
 	// step 1: pulling the newest ckp locations and block entries from tn
-	response, err := requestStorageUsage(ctx, ses)
+	response, err := requestStorageUsage(ses)
 	if err != nil {
 		return nil, err
 	}
