01664cdb9a31391b7c4fdda54a8d58b3bc5cccb0
chenmingsong <mschenjj@foxmail.com>
2023-11-10 15:34:08 +0800

    support parser for sample function

--- pkg/sql/parsers/dialect/mysql/keywords.go
+++ pkg/sql/parsers/dialect/mysql/keywords.go
@@ -611,5 +611,7 @@ func init() {
 		"backup":                     BACKUP,
 		"filesystem":                 FILESYSTEM,
 		"handler":                    HANDLER,
+		"sample":					  SAMPLE,
+		"percent":					  PERCENT,
 	}
 }
