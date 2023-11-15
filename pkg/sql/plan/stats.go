8d9ee6cddc6540555ad36317374625732e4de492
noorall <863485501@qq.com>
2023-11-15 19:49:50 +0800

    [mod] standardize the syntax of code

--- pkg/sql/plan/stats.go
+++ pkg/sql/plan/stats.go
@@ -770,10 +770,7 @@ func needStats(tableDef *TableDef) bool {
 	case "sys_async_task", "sys_cron_task":
 		return false
 	}
-	if strings.HasPrefix(tableDef.Name, "mo_") {
-		return false
-	}
-	return true
+	return !strings.HasPrefix(tableDef.Name, "mo_")
 }
 
 func DefaultHugeStats() *plan.Stats {
