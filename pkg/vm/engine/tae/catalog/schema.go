27885f04f52b8aafd087556e8b10fdce9b40684b
aptend <49832303+aptend@users.noreply.github.com>
2023-11-17 23:59:40 +0800

    sync persist mo-ctl policy 1.0 (#12804)

--- pkg/vm/engine/tae/catalog/schema.go
+++ pkg/vm/engine/tae/catalog/schema.go
@@ -162,6 +162,12 @@ func (s *Schema) Clone() *Schema {
 // recommended to close schema first and then apply alter table.
 func (s *Schema) ApplyAlterTable(req *apipb.AlterTableReq) error {
 	switch req.Kind {
+	case apipb.AlterKind_UpdatePolicy:
+		p := req.GetUpdatePolicy()
+		s.Extra.MaxRowsMergedObj = p.GetMaxObjOnerun()
+		s.Extra.MinRowsQuailifed = p.GetMinRowsQuailifed()
+		s.Extra.MaxObjOnerun = p.GetMaxObjOnerun()
+		s.Extra.Hints = p.GetHints()
 	case apipb.AlterKind_UpdateConstraint:
 		s.Constraint = req.GetUpdateCstr().GetConstraints()
 	case apipb.AlterKind_UpdateComment:
--- pkg/vm/engine/tae/db/scannerop.go
+++ pkg/vm/engine/tae/db/scannerop.go
@@ -24,6 +24,7 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/util/trace/impl/motrace"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/catalog"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/db/merge"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/iface/txnif"
 )
 
 type ScannerOp interface {
@@ -158,12 +159,17 @@ func (s *MergeTaskBuilder) ManuallyMerge(entry *catalog.TableEntry, segs []*cata
 	return s.executor.ManuallyExecute(entry, segs)
 }
 
-func (s *MergeTaskBuilder) ConfigPolicy(id uint64, c any) {
-	s.objPolicy.Config(id, c)
+func (s *MergeTaskBuilder) ConfigPolicy(tbl *catalog.TableEntry, c any) {
+	f := func() txnif.AsyncTxn {
+		txn, _ := s.db.StartTxn(nil)
+		return txn
+	}
+
+	s.objPolicy.SetConfig(tbl, f, c)
 }
 
-func (s *MergeTaskBuilder) GetPolicy(id uint64) any {
-	return s.objPolicy.GetConfig(id)
+func (s *MergeTaskBuilder) GetPolicy(tbl *catalog.TableEntry) any {
+	return s.objPolicy.GetConfig(tbl)
 }
 
 func (s *MergeTaskBuilder) trySchedMergeTask() {
@@ -183,7 +189,7 @@ func (s *MergeTaskBuilder) resetForTable(entry *catalog.TableEntry) {
 		s.name = entry.GetLastestSchema().Name
 	}
 	s.segmentHelper.reset()
-	s.objPolicy.ResetForTable(entry.ID, entry)
+	s.objPolicy.ResetForTable(entry)
 }
 
 func (s *MergeTaskBuilder) PreExecute() error {
@@ -197,6 +203,9 @@ func (s *MergeTaskBuilder) PostExecute() error {
 	return nil
 }
 func (s *MergeTaskBuilder) onDataBase(dbEntry *catalog.DBEntry) (err error) {
+	if merge.StopMerge.Load() {
+		return moerr.GetOkStopCurrRecur()
+	}
 	if s.executor.MemAvailBytes() < 100*1024*1024 {
 		return moerr.GetOkStopCurrRecur()
 	}
