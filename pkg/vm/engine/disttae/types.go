1acee8a6e84c1d47e4e0af1cb39fe120110e8984
nitao <badboynt@126.com>
2023-11-27 10:57:25 +0800

    tune ap cache policy (#12822)
    tune down prefetch parameter to reduce prefetch read
    for small scan and large scan, only prefetch file
    
    Approved by: @LeftHandCold, @XuPeng-SH

--- pkg/vm/engine/disttae/types.go
+++ pkg/vm/engine/disttae/types.go
@@ -47,8 +47,10 @@ import (
 )
 
 const (
-	PREFETCH_THRESHOLD = 512
-	PREFETCH_ROUNDS    = 32
+	PREFETCH_THRESHOLD  = 256
+	PREFETCH_ROUNDS     = 24
+	SMALLSCAN_THRESHOLD = 100
+	LARGESCAN_THRESHOLD = 1500
 )
 
 const (
@@ -57,6 +59,12 @@ const (
 	COMPACTION_CN
 	UPDATE
 	ALTER
+)
+
+const (
+	SMALL = iota
+	NORMAL
+	LARGE
 )
 
 const (
@@ -580,6 +588,7 @@ type blockReader struct {
 	steps       []int
 	currentStep int
 
+	scanType int
 	// block list to scan
 	blks []*catalog.BlockInfo
 	//buffer for block's deletes
