59741bbfe22f22bf4f2d8e4ad00449dcf513be71
Ziran Wei <weiziran125@gmail.com>
2023-11-28 21:44:14 +0800

    add comment

--- pkg/sql/plan/function/ctl/cmd_rpc_version.go
+++ pkg/sql/plan/function/ctl/cmd_rpc_version.go
@@ -17,9 +17,12 @@ package ctl
 import (
 	"context"
 	"encoding/json"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/matrixorigin/matrixone/pkg/clusterservice"
+	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 	"github.com/matrixorigin/matrixone/pkg/pb/metadata"
 	querypb "github.com/matrixorigin/matrixone/pkg/pb/query"
 	"github.com/matrixorigin/matrixone/pkg/vm/process"
@@ -72,6 +75,27 @@ func handleGetProtocolVersion(proc *process.Process,
 	}, nil
 }
 
+// handleSetProtocolVersion sets the version of mo components' protocol versions
+//
+// the cmd format for CN service:
+//
+//	mo_ctl("cn", "SetProtocolVersion" "uuids of cn:protocol version")
+//
+// examples as below:
+//
+//	mo_ctl("cn", "SetProtocolVersion", "cn_uuid1:1")
+//	mo_ctl("cn", "SetProtocolVersion", "cn_uuid1,cn_uuid2,...:2")
+//
+// the cmd format for TN service:
+//
+//	mo_ctl("dn", "SetProtocolVersion", "protocol version")
+//
+// (because there only exist one dn service, so we don't need to specify the uuid,
+//
+//	but, the uuid will be ignored and will not check its validation even though it is specified.)
+//
+// examples as below:
+// mo_ctl("dn", "SetProtocolVersion", "1")
 func handleSetProtocolVersion(proc *process.Process,
 	service serviceType,
 	parameter string,
@@ -120,4 +144,23 @@ func handleSetProtocolVersion(proc *process.Process,
 		Method: SetProtocolVersionMethod,
 		Data:   string(bytes),
 	}, nil
+}
+
+func checkProtocolParameter(param string) ([]string, int64, error) {
+	param = strings.ToLower(param)
+	// [uuids]:version
+	args := strings.Split(param, ":")
+	version, err := strconv.ParseInt(args[len(args)-1], 10, 64)
+	if err != nil {
+		return nil, 0, moerr.NewInternalErrorNoCtx("cmd invalid, expected version number")
+	}
+
+	if len(args) == 2 {
+		arg := args[0]
+		targets := strings.Split(arg, ",")
+		return targets, version, nil
+
+	}
+
+	return nil, version, nil
 }
