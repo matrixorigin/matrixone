4ebcd980d78db790244b8809a65737cdb2a64f3a
w-zr <weiziran125@gmail.com>
2023-12-01 18:13:36 +0800

    add method versin check for cache service

--- pkg/cacheservice/client/cache_client.go
+++ pkg/cacheservice/client/cache_client.go
@@ -19,8 +19,13 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 	"github.com/matrixorigin/matrixone/pkg/common/morpc"
 	"github.com/matrixorigin/matrixone/pkg/common/runtime"
+	"github.com/matrixorigin/matrixone/pkg/defines"
 	"github.com/matrixorigin/matrixone/pkg/pb/cache"
 )
+
+var methodVersion = map[cache.CmdMethod]int64{
+	cache.CmdMethod_RemoteRead: defines.MORPCVersion1,
+}
 
 type CacheClient interface {
 	// SendMessage send message to a cache server.
@@ -83,6 +88,9 @@ func (c *cacheClient) SendMessage(
 ) (*cache.Response, error) {
 	if address == "" {
 		return nil, moerr.NewInternalError(ctx, "invalid CN query address %s", address)
+	}
+	if err := runtime.CheckMethodVersion(ctx, methodVersion, req); err != nil {
+		return nil, err
 	}
 	f, err := c.client.Send(ctx, address, req)
 	if err != nil {
