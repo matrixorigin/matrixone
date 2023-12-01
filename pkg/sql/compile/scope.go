fcebc0b481a7a1f941fdd08c3441ca8bbfdc5af7
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-11-30 22:01:16 -0800

    Revert "Commenting err = nil"
    This reverts commit 8e1a9fb74510f619a7db999dfe6e352ee6d5f9c1.

--- pkg/sql/compile/scope.go
+++ pkg/sql/compile/scope.go
@@ -89,12 +89,11 @@ func (s *Scope) Run(c *Compile) (err error) {
 		}
 	}
 
-	// TODO: This is a temporary solution to debug the panic suppression in pipeline.
-	//select {
-	//case <-s.Proc.Ctx.Done():
-	//	err = nil
-	//default:
-	//}
+	select {
+	case <-s.Proc.Ctx.Done():
+		err = nil
+	default:
+	}
 	return err
 }
 
