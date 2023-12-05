e33230d72f0cb661faa22dd01e7ea9edb43c9490
nojomohr <122540749+nojomohr@users.noreply.github.com>
2023-12-04 23:03:33 -0500

    Update alter.go

--- pkg/sql/parsers/tree/alter.go
+++ pkg/sql/parsers/tree/alter.go
@@ -18,6 +18,16 @@ import (
 	"fmt"
 )
 
+const  (
+	AlterTableAddPartition AlterTableOptionType = iota + 1000
+)
+
+func (node *AlterTableAddPartitionClause) Format(ctx *FmtCtx) {
+	ctx.WriteString("add partition ")
+	node.PartitionDef.Format(ctx)
+}
+
+
 type AlterUser struct {
 	statementImpl
 	IfExists bool
@@ -214,6 +224,35 @@ type AlterTableAddPartitionClause struct {
 	PartitionDef *PartitionDefinition
 }
 
+type PartitionDefinition struct {
+	NodeFormatter
+}
+
+func (node *PartitionDefinition) Format(ctx *FmtCtx) {
+	ctx.WriteString("partition ")
+	if node.Name != "" {
+		ctx.FormatNode(&node.Name)
+	}
+	if len(node.Values) > 0 {
+		ctx.WriteString(" values (")
+		for i, value := range node.Values {
+			if i > 0 {
+				ctx.WriteString(", ")
+			}
+			ctx.FormatNode(value)
+		}
+		ctx.WriteString(")")
+	}
+	if len(node.StorageOptions) > 0 {
+		ctx.WriteString(" storage (")
+		for key, value := range node.StorageOptions {
+			ctx.FormatNode(key)
+			ctx.WriteString(" = ")
+			ctx.FormatNode(value)
+		}
+		ctx.WriteString(")")
+	}
+}
 // second issue for 6b end
 
 type AlterOptionAlterIndex struct {
