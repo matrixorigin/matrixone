cb4ef30a286c16bc68d48d6f1a6ceec5c6ec2bb0
nnsgmsone <nnsmgsone@outlook.com>
2023-12-06 10:58:54 +0800

    Solving the problem of concurrently reading channels

--- pkg/sql/compile/scopeRemoteRun.go
+++ pkg/sql/compile/scopeRemoteRun.go
@@ -125,6 +125,7 @@ func CnServerMessageHandler(
 				zap.String("error", err.Error()))
 			err = errors.Join(err, cs.Close())
 		}
+		cs.SafeClose(ctx)
 	}()
 
 	msg, ok := message.(*pipeline.Message)
--- pkg/vm/process/process.go
+++ pkg/vm/process/process.go
@@ -122,12 +122,6 @@ func NewFromProc(p *Process, ctx context.Context, regNumber int) *Process {
 }
 
 func (wreg *WaitRegister) CleanChannel(m *mpool.MPool) {
-	for len(wreg.Ch) > 0 {
-		bat := <-wreg.Ch
-		if bat != nil {
-			bat.Clean(m)
-		}
-	}
 }
 
 func (wreg *WaitRegister) MarshalBinary() ([]byte, error) {
