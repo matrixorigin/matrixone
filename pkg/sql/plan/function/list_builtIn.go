7532bd7dc9e5bf9b90abbe95a7dd02d0cc5ef208
qingxinhome <758355272@qq.com>
2023-12-08 18:20:35 +0800

    BI Tool Special Dialect Equivalent Rewriting

--- pkg/sql/plan/function/list_builtIn.go
+++ pkg/sql/plan/function/list_builtIn.go
@@ -787,6 +787,48 @@ var supportedStringBuiltIns = []FuncNew{
 		},
 	},
 
+	// function `lower`, `to_lower`
+	{
+		functionId: LOWER,
+		class:      plan.Function_STRICT,
+		layout:     STANDARD_FUNCTION,
+		checkFn:    fixedTypeMatch,
+
+		Overloads: []overload{
+			{
+				overloadId: 0,
+				args:       []types.T{types.T_varchar},
+				retType: func(parameters []types.Type) types.Type {
+					return parameters[0]
+				},
+				newOp: func() executeLogicOfOverload {
+					return builtInToLower
+				},
+			},
+		},
+	},
+
+	// function `upper`, `to_upper`
+	{
+		functionId: UPPER,
+		class:      plan.Function_STRICT,
+		layout:     STANDARD_FUNCTION,
+		checkFn:    fixedTypeMatch,
+
+		Overloads: []overload{
+			{
+				overloadId: 0,
+				args:       []types.T{types.T_varchar},
+				retType: func(parameters []types.Type) types.Type {
+					return parameters[0]
+				},
+				newOp: func() executeLogicOfOverload {
+					return builtInToUpper
+				},
+			},
+		},
+	},
+
 	// function `lpad`
 	{
 		functionId: LPAD,
