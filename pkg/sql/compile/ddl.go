118e5776c53ae2d034352dd2fd330634dbdf050a
Leiyang <30954408+noorall@users.noreply.github.com>
2023-12-10 22:01:32 +0800

    [perf] improve delete from table without where & limit (#13120)
    When a table does not have a foreign key. So for delete from table, it can be optimized to truncate.
    
    Approved by: @m-schen, @nnsgmsone, @badboynt1, @aunjgr, @ouyuanning

--- pkg/sql/compile/ddl.go
+++ pkg/sql/compile/ddl.go
@@ -1264,6 +1264,8 @@ func (s *Scope) TruncateTable(c *Compile) error {
 	dbName := tqry.GetDatabase()
 	tblName := tqry.GetTable()
 	oldId := tqry.GetTableId()
+	keepAutoIncrement := false
+	affectedRows := int64(0)
 
 	dbSource, err = c.e.Database(c.ctx, dbName, c.proc.TxnOperator)
 	if err != nil {
@@ -1300,6 +1302,14 @@ func (s *Scope) TruncateTable(c *Compile) error {
 			}
 			err = e
 		}
+		if err != nil {
+			return err
+		}
+	}
+
+	if tqry.IsDelete {
+		keepAutoIncrement = true
+		affectedRows, err = rel.Rows(c.ctx)
 		if err != nil {
 			return err
 		}
@@ -1388,7 +1398,7 @@ func (s *Scope) TruncateTable(c *Compile) error {
 		c.ctx,
 		oldId,
 		newId,
-		false,
+		keepAutoIncrement,
 		c.proc.TxnOperator)
 	if err != nil {
 		return err
@@ -1400,6 +1410,7 @@ func (s *Scope) TruncateTable(c *Compile) error {
 	if err != nil {
 		return err
 	}
+	c.addAffectedRows(uint64(affectedRows))
 	return nil
 }
 
