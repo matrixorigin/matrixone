724c356663d577673580398581cc6b03c8aaf981
noorall <863485501@qq.com>
2023-12-19 20:00:27 +0800

    [fix] double free for AnalInfos

--- pkg/sql/compile/scopeRemoteRun.go
+++ pkg/sql/compile/scopeRemoteRun.go
@@ -22,6 +22,7 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 	"github.com/matrixorigin/matrixone/pkg/common/morpc"
 	"github.com/matrixorigin/matrixone/pkg/common/mpool"
+	"github.com/matrixorigin/matrixone/pkg/common/reuse"
 	"github.com/matrixorigin/matrixone/pkg/container/batch"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
@@ -177,6 +178,8 @@ func cnMessageHandle(receiver *messageReceiverOnServer) error {
 		// decode and rewrite the scope.
 		s, err := decodeScope(receiver.scopeData, c.proc, true, c.e)
 		defer func() {
+			c.proc.AnalInfos = nil
+			c.anal.analInfos = nil
 			c.Release()
 			s.release()
 		}()
@@ -197,6 +200,15 @@ func cnMessageHandle(receiver *messageReceiverOnServer) error {
 			c.proc.AnalInfos[c.anal.curr].S3IOOutputCount += c.counterSet.FileService.S3.DeleteMulti.Load()
 
 			receiver.finalAnalysisInfo = c.proc.AnalInfos
+		} else {
+			// there are 3 situations to release analyzeInfo
+			// 1 is free analyzeInfo of Local CN when release analyze
+			// 2 is free analyzeInfo of remote CN before transfer back
+			// 3 is free analyzeInfo of remote CN when errors happen before transfer back
+			// this is situation 3
+			for i := range c.proc.AnalInfos {
+				reuse.Free[process.AnalyzeInfo](c.proc.AnalInfos[i], nil)
+			}
 		}
 		c.proc.FreeVectors()
 		c.proc.CleanValueScanBatchs()
@@ -1655,7 +1667,7 @@ func convertToPlanAnalyzeInfo(info *process.AnalyzeInfo) *plan.AnalyzeInfo {
 	// 2 is free analyzeInfo of remote CN before transfer back
 	// 3 is free analyzeInfo of remote CN when errors happen before transfer back
 	// this is situation 2
-	// reuse.Free[process.AnalyzeInfo](info, nil)
+	reuse.Free[process.AnalyzeInfo](info, nil)
 	return a
 }
 
