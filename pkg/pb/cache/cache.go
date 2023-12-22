a26ba142d1f77f935a35d8d5e09ba24d5ed0cddb
LiuBo <g.user.lb@gmail.com>
2023-12-22 10:55:35 +0800

    [enhancement] cache: release the ioVec when response is reset.

--- pkg/pb/cache/cache.go
+++ pkg/pb/cache/cache.go
@@ -18,9 +18,20 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 )
+
+type CacheResponse struct {
+	Response
+	ReleaseFunc func()
+}
+
+func (r *CacheResponse) Reset() {
+	if r.ReleaseFunc != nil {
+		r.ReleaseFunc()
+	}
+	r.Response.Reset()
+}
 
 // SetID implements the morpc.Message interface.
 func (m *Request) SetID(id uint64) {
