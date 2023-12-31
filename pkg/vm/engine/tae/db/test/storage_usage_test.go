5830d18bdd09715c00504dcdb585577a2c038a30
gouhongshen <gouhongshen@hotmail.com>
2023-12-31 11:15:24 +0800

    sync_to_1.1: adding lock to cn storage usage cache to avoid data race  (#13928)
    1. add sync.lock to avoid data race when cache.IsExpired().
    2. split the show accounts duration metric to different phases.
    
    Approved by: @XuPeng-SH, @zhangxu19830126, @qingxinhome, @sukki37

--- pkg/vm/engine/tae/db/test/storage_usage_test.go
+++ pkg/vm/engine/tae/db/test/storage_usage_test.go
@@ -39,6 +39,9 @@ import (
 func Test_StorageUsageCache(t *testing.T) {
 	// new cache with no option
 	cache := logtail.NewStorageUsageCache(logtail.WithLazyThreshold(1))
+	cache.Lock()
+	defer cache.Unlock()
+
 	require.True(t, cache.IsExpired())
 
 	allocator := atomic.Uint64{}
@@ -646,5 +649,4 @@ func Test_UpdateDataFromOldVersion(t *testing.T) {
 			require.Equal(t, uint64(0), size)
 		}
 	}
-
 }
