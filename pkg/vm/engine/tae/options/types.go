f7a0f5b41f89c027d1df892bf1caf5c5bb263c5d
jiangxinmeng1 <51114574+jiangxinmeng1@users.noreply.github.com>
2024-01-10 18:51:06 +0800

    update default global version interval (#14097)
    Set `DefaultGlobalVersionInterval` = 1h
    
    Approved by: @XuPeng-SH

--- pkg/vm/engine/tae/options/types.go
+++ pkg/vm/engine/tae/options/types.go
@@ -38,7 +38,7 @@ const (
 	DefaultCheckpointMinCount           = int64(100)
 	DefaultCheckpointIncremetalInterval = time.Minute
 	DefaultCheckpointGlobalMinCount     = 10
-	DefaultGlobalVersionInterval        = time.Minute * 5
+	DefaultGlobalVersionInterval        = time.Hour
 	DefaultGCCheckpointInterval         = time.Minute
 
 	DefaultScanGCInterval = time.Minute * 30
