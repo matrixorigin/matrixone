09f5b5e0d013d28b502ddcfdd0aa8810603b9bbb
zhangxu <zhangxu19830126@gmail.com>
2024-01-15 15:10:17 +0800

    fix lock table map

--- pkg/lockservice/service_remote.go
+++ pkg/lockservice/service_remote.go
@@ -45,7 +45,7 @@ func (s *service) initRemote() {
 		rpcClient,
 		s.cfg.KeepBindDuration.Duration,
 		s.cfg.KeepRemoteLockDuration.Duration,
-		&s.tables)
+		s.tables)
 	s.initRemoteHandler()
 	if err := s.remote.server.Start(); err != nil {
 		panic(err)
