0395299b8782883b1f1e9d3a0b04694df8020e83
badboynt1 <badboynt@126.com>
2024-01-18 10:57:38 +0800

    fix bvt

--- pkg/sql/colexec/loopanti/join.go
+++ pkg/sql/colexec/loopanti/join.go
@@ -109,7 +109,7 @@ func (ctr *container) build(ap *Argument, proc *process.Process, anal process.An
 		if bat == nil {
 			break
 		}
-		ctr.bat, err = ctr.bat.Append(proc.Ctx, proc.Mp(), bat)
+		ctr.bat, _, err = proc.AppendBatchFromOffset(ctr.bat, bat, 0)
 		if err != nil {
 			return err
 		}
