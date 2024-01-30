959a08c2ed6e3df63ad6c0c05b114f725a4e1be6
Leiyang <30954408+noorall@users.noreply.github.com>
2024-01-30 21:59:18 +0800

    [fix] load data stmt affect rows count calculation incorrectly (#14364)
    Fix load data stmt affect rows count calculation incorrectly.
    
    Approved by: @m-schen, @nnsgmsone

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -1429,7 +1429,8 @@ func (c *Compile) compilePlanScope(ctx context.Context, step int32, curNodeIdx i
 					Op: vm.MergeBlock,
 					Arg: mergeblock.NewArgument().
 						WithTbl(insertArg.InsertCtx.Rel).
-						WithPartitionSources(insertArg.InsertCtx.PartitionSources),
+						WithPartitionSources(insertArg.InsertCtx.PartitionSources).
+						WithAddAffectedRows(insertArg.InsertCtx.AddAffectedRows),
 				})
 				ss = []*Scope{rs}
 				insertArg.Release()
@@ -1497,7 +1498,8 @@ func (c *Compile) compilePlanScope(ctx context.Context, step int32, curNodeIdx i
 					Op: vm.MergeBlock,
 					Arg: mergeblock.NewArgument().
 						WithTbl(insertArg.InsertCtx.Rel).
-						WithPartitionSources(insertArg.InsertCtx.PartitionSources),
+						WithPartitionSources(insertArg.InsertCtx.PartitionSources).
+						WithAddAffectedRows(insertArg.InsertCtx.AddAffectedRows),
 				})
 				ss = []*Scope{rs}
 				insertArg.Release()
