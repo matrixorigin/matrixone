a6a9e597a734ec8616ea8c4b75047d1c5292cb13
XuPeng-SH <xupeng3112@163.com>
2024-02-02 00:04:09 +0800

    update

--- pkg/vm/engine/disttae/util.go
+++ pkg/vm/engine/disttae/util.go
@@ -1247,7 +1247,7 @@ func UnfoldBlkInfoFromObjStats(stats *objectio.ObjectStats) (blks []objectio.Blo
 func ForeachBlkInObjStatsList(
 	next bool,
 	dataMeta objectio.ObjectDataMeta,
-	onBlock func(blk *objectio.BlockInfo, blkMeta objectio.BlockObject) bool,
+	onBlock func(blk objectio.BlockInfo, blkMeta objectio.BlockObject) bool,
 	objects ...objectio.ObjectStats,
 ) {
 	stop := false
@@ -1307,7 +1307,7 @@ func (i *StatsBlkIter) Next() bool {
 	return i.cur < int(i.blkCnt)
 }
 
-func (i *StatsBlkIter) Entry() *objectio.BlockInfo {
+func (i *StatsBlkIter) Entry() objectio.BlockInfo {
 	if i.cur == -1 {
 		i.cur = 0
 	}
@@ -1322,7 +1322,7 @@ func (i *StatsBlkIter) Entry() *objectio.BlockInfo {
 	}
 
 	loc := objectio.BuildLocation(i.name, i.extent, i.curBlkRows, uint16(i.cur))
-	blk := &objectio.BlockInfo{
+	blk := objectio.BlockInfo{
 		BlockID:   *objectio.BuildObjectBlockid(i.name, uint16(i.cur)),
 		SegmentID: i.name.SegmentId(),
 		MetaLoc:   objectio.ObjectLocation(loc),
