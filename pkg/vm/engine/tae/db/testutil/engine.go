86d7a2716e791529e4c7a18529a988a6a43ab75d
GreatRiver <2552853833@qq.com>
2024-02-22 14:55:17 +0800

    UT pass

--- pkg/vm/engine/tae/db/testutil/engine.go
+++ pkg/vm/engine/tae/db/testutil/engine.go
@@ -94,7 +94,7 @@ func (e *TestEngine) Restart(ctx context.Context) {
 	var err error
 	e.DB, err = db.Open(ctx, e.Dir, e.Opts)
 	// only ut executes this checker
-	e.DB.DiskCleaner.AddChecker(
+	e.DB.DiskCleaner.GetCleaner().AddChecker(
 		func(item any) bool {
 			min := e.DB.TxnMgr.MinTSForTest()
 			ckp := item.(*checkpoint.CheckpointEntry)
@@ -350,7 +350,7 @@ func InitTestDBWithDir(
 ) *db.DB {
 	db, _ := db.Open(ctx, dir, opts)
 	// only ut executes this checker
-	db.DiskCleaner.AddChecker(
+	db.DiskCleaner.GetCleaner().AddChecker(
 		func(item any) bool {
 			min := db.TxnMgr.MinTSForTest()
 			ckp := item.(*checkpoint.CheckpointEntry)
@@ -369,7 +369,7 @@ func InitTestDB(
 	dir := testutils.InitTestEnv(moduleName, t)
 	db, _ := db.Open(ctx, dir, opts)
 	// only ut executes this checker
-	db.DiskCleaner.AddChecker(
+	db.DiskCleaner.GetCleaner().AddChecker(
 		func(item any) bool {
 			min := db.TxnMgr.MinTSForTest()
 			ckp := item.(*checkpoint.CheckpointEntry)
