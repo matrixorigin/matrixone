4eebc8eb5925ed38339234a4e2a3362c635dc077
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2024-02-22 02:48:42 -0800

    Revert "SEMI join between centroids and entries."
    This reverts commit c9ff77f0e8bd29937ecb071b0d33ea2bb43036be.

--- pkg/sql/plan/apply_indices_orderby.go
+++ pkg/sql/plan/apply_indices_orderby.go
@@ -391,7 +391,16 @@ func makeEntriesCrossJoinMetaOnCurrVersion(builder *QueryBuilder, bindCtx *BindC
 		OnList:   []*Expr{joinCond},
 	}, bindCtx)
 
-	return joinMetaAndEntriesId, nil
+	// 3. Project version, centroid_id_fk, origin_pk, meta.value
+	idxTags["entries.project"] = builder.genNewTag()
+	projectCols := builder.appendNode(&plan.Node{
+		NodeType:    plan.Node_PROJECT,
+		Children:    []int32{joinMetaAndEntriesId},
+		ProjectList: []*Expr{scanCols[0], scanCols[1], scanCols[2]},
+		BindingTags: []int32{idxTags["entries.project"]},
+	}, bindCtx)
+
+	return projectCols, nil
 }
 
 func makeEntriesCrossJoinCentroidsOnCentroidId(builder *QueryBuilder, bindCtx *BindContext, idxTableDefs []*TableDef, idxTags map[string]int32, entriesForCurrVersion int32, centroidsForCurrVersion int32) int32 {
@@ -400,7 +409,7 @@ func makeEntriesCrossJoinCentroidsOnCentroidId(builder *QueryBuilder, bindCtx *B
 			Typ: DeepCopyType(idxTableDefs[2].Cols[1].Typ),
 			Expr: &plan.Expr_Col{
 				Col: &plan.ColRef{
-					RelPos: idxTags["entries.scan"],
+					RelPos: idxTags["entries.project"],
 					ColPos: 1, // entries.__mo_index_centroid_fk_id
 				},
 			},
@@ -419,7 +428,7 @@ func makeEntriesCrossJoinCentroidsOnCentroidId(builder *QueryBuilder, bindCtx *B
 	// 1. Create JOIN entries and centroids on centroid_id_fk == centroid_id
 	joinEntriesAndCentroids := builder.appendNode(&plan.Node{
 		NodeType: plan.Node_JOIN,
-		JoinType: plan.Node_SEMI,
+		JoinType: plan.Node_INNER,
 		Children: []int32{entriesForCurrVersion, centroidsForCurrVersion},
 		OnList:   []*Expr{entriesCentroidIdEqCentroidId},
 	}, bindCtx)
@@ -435,7 +444,7 @@ func makeTblCrossJoinEntriesCentroidOnPK(builder *QueryBuilder, bindCtx *BindCon
 			Typ: DeepCopyType(idxTableDefs[2].Cols[2].Typ),
 			Expr: &plan.Expr_Col{
 				Col: &plan.ColRef{
-					RelPos: idxTags["entries.scan"],
+					RelPos: idxTags["entries.project"],
 					ColPos: 2, // entries.origin_pk
 				},
 			},
