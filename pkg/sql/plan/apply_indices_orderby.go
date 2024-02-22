0106b4ba8918f4fd1f2212431281c1bacf74a56e
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2024-02-22 00:21:10 -0800

    Remove Project and use l2Distance directly in Sort Key.

--- pkg/sql/plan/apply_indices_orderby.go
+++ pkg/sql/plan/apply_indices_orderby.go
@@ -279,7 +279,9 @@ func makeCentroidsSingleJoinMetaOnCurrVersionOrderByL2DistNormalizeL2(builder *Q
 		OnList:   []*Expr{joinCond},
 	}, bindCtx)
 
-	// 3. Project version, centroid_id, centroid, l2_distance(literal, normalize_l2(col))
+	// 3. Sort by l2_distance(normalize_l2(col), literal) limit @probe_limit
+
+	// 3.a.1
 	centroidsCol := &plan.Expr{
 		Typ: DeepCopyType(indexTableDefs[1].Cols[2].Typ),
 		Expr: &plan.Expr_Col{
@@ -297,17 +299,8 @@ func makeCentroidsSingleJoinMetaOnCurrVersionOrderByL2DistNormalizeL2(builder *Q
 		centroidsCol,   // centroid
 		normalizeL2Lit, // normalize_l2(literal)
 	})
-	idxTags["centroids.project"] = builder.genNewTag()
-	projectCols := builder.appendNode(&plan.Node{
-		NodeType:    plan.Node_PROJECT,
-		Children:    []int32{joinMetaAndCentroidsId},
-		ProjectList: []*Expr{scanCols[0], scanCols[1], scanCols[2], l2DistanceLitNormalizeL2Col},
-		BindingTags: []int32{idxTags["centroids.project"]},
-	}, bindCtx)
 
-	// 4. Sort by l2_distance(normalize_l2(col), literal) limit @probe_limit
-
-	// 4.1 @probe_limit is a system variable
+	// 3.b.1 @probe_limit is a system variable
 	probeLimitValueExpr := &plan.Expr{
 		Typ: makePlan2Type(&textType), // T_text
 		Expr: &plan.Expr_V{
@@ -319,7 +312,7 @@ func makeCentroidsSingleJoinMetaOnCurrVersionOrderByL2DistNormalizeL2(builder *Q
 		},
 	}
 
-	//4.2 ISNULL(@var)
+	//3.b.2 ISNULL(@var)
 	arg0, err := BindFuncExprImplByPlanExpr(builder.GetContext(), "isnull", []*plan.Expr{
 		probeLimitValueExpr,
 	})
@@ -327,10 +320,10 @@ func makeCentroidsSingleJoinMetaOnCurrVersionOrderByL2DistNormalizeL2(builder *Q
 		return -1, err
 	}
 
-	// 4.3 CAST( 1 AS BIGINT)
+	// 3.b.3 CAST( 1 AS BIGINT)
 	arg1 := makePlan2Int64ConstExprWithType(1)
 
-	// 4.4 CAST(@var AS BIGINT)
+	// 3.b.4 CAST(@var AS BIGINT)
 	targetType := types.T_int64.ToType()
 	planTargetType := makePlan2Type(&targetType)
 	arg2, err := appendCastBeforeExpr(builder.GetContext(), probeLimitValueExpr, planTargetType)
@@ -349,19 +342,11 @@ func makeCentroidsSingleJoinMetaOnCurrVersionOrderByL2DistNormalizeL2(builder *Q
 
 	sortCentroidsByL2DistanceId := builder.appendNode(&plan.Node{
 		NodeType: plan.Node_SORT,
-		Children: []int32{projectCols},
+		Children: []int32{joinMetaAndCentroidsId},
 		Limit:    ifNullLimitExpr,
 		OrderBy: []*OrderBySpec{
 			{
-				Expr: &plan.Expr{
-					Typ: makePlan2Type(&float64Type),
-					Expr: &plan.Expr_Col{
-						Col: &plan.ColRef{
-							RelPos: idxTags["centroids.project"],
-							ColPos: 3,
-						},
-					},
-				},
+				Expr: l2DistanceLitNormalizeL2Col,
 				Flag: sortDirection,
 			},
 		},
@@ -434,7 +419,7 @@ func makeEntriesCrossJoinCentroidsOnCentroidId(builder *QueryBuilder, bindCtx *B
 			Typ: DeepCopyType(idxTableDefs[1].Cols[1].Typ),
 			Expr: &plan.Expr_Col{
 				Col: &plan.ColRef{
-					RelPos: idxTags["centroids.project"],
+					RelPos: idxTags["centroids.scan"],
 					ColPos: 1, // centroids.__mo_index_centroid_id
 				},
 			},
