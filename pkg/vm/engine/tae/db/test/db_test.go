1c3c2d8053eb12b19aa266ec613a881ece31fca7
aptend <aptend@hotmail.com>
2024-02-26 18:14:41 +0800

    fix ut

--- pkg/vm/engine/tae/db/test/db_test.go
+++ pkg/vm/engine/tae/db/test/db_test.go
@@ -3767,7 +3767,7 @@ func TestDelete3(t *testing.T) {
 	// this task won't affect logic of TestAppend2, it just prints logs about dirty count
 	forest := logtail.NewDirtyCollector(tae.LogtailMgr, opts.Clock, tae.Catalog, new(catalog.LoopProcessor))
 	hb := ops.NewHeartBeaterWithFunc(5*time.Millisecond, func() {
-		forest.Run()
+		forest.Run(0)
 		t.Log(forest.String())
 	}, nil)
 	hb.Start()
@@ -4847,7 +4847,7 @@ func TestWatchDirty(t *testing.T) {
 			t.Errorf("timeout to wait zero")
 			return
 		default:
-			watcher.Run()
+			watcher.Run(0)
 			time.Sleep(5 * time.Millisecond)
 			_, _, blkCnt := watcher.DirtyCount()
 			// find block zero
@@ -4902,7 +4902,7 @@ func TestDirtyWatchRace(t *testing.T) {
 		go func(i int) {
 			for j := 0; j < 300; j++ {
 				time.Sleep(5 * time.Millisecond)
-				watcher.Run()
+				watcher.Run(0)
 				// tbl, obj, blk := watcher.DirtyCount()
 				// t.Logf("t%d: tbl %d, obj %d, blk %d", i, tbl, obj, blk)
 				_, _, _ = watcher.DirtyCount()
