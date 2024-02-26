28beab9a0a043238ea75dcee9e1cc8d6e1d1d82f
badboynt1 <badboynt@126.com>
2024-02-26 13:43:22 +0800

    fix

--- pkg/sql/plan/shuffle.go
+++ pkg/sql/plan/shuffle.go
@@ -280,13 +280,14 @@ func determinShuffleType(col *plan.ColRef, n *plan.Node, builder *QueryBuilder) 
 		return
 	}
 	if shouldUseHashShuffle(s.ShuffleRangeMap[colName]) {
+		logutil.Infof("colname %v uniform %v overlap %v", colName, s.ShuffleRangeMap[colName].Uniform, s.ShuffleRangeMap[colName].Overlap)
 		return
 	}
 	n.Stats.HashmapStats.ShuffleType = plan.ShuffleType_Range
 	n.Stats.HashmapStats.ShuffleColMin = int64(s.MinValMap[colName])
 	n.Stats.HashmapStats.ShuffleColMax = int64(s.MaxValMap[colName])
 	n.Stats.HashmapStats.Ranges = shouldUseShuffleRanges(s.ShuffleRangeMap[colName])
-	logutil.Infof("colname %v uniform %v overlap %v", colName, s.ShuffleRangeMap[colName].Uniform, s.ShuffleRangeMap[colName].Overlap)
+
 	n.Stats.HashmapStats.Nullcnt = int64(s.NullCntMap[colName])
 }
 
