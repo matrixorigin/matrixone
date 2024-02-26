e83d2d5ae7b1fbcbf9ff332bae049bf9123fe937
Jackson <xzxiong@yeah.net>
2024-02-26 19:04:11 +0800

    add bvt case

--- test/distributed/cases/log/column_metric.sql
+++ test/distributed/cases/log/column_metric.sql
@@ -1,1 +1,3 @@
 select metric_name, collecttime, value, `node`, role, account, type from system_metrics.metric limit 0;
+-- issue https://github.com/matrixorigin/MO-Cloud/issues/2569
+select count(1) cnt, collecttime, `node`, role, account, type from system_metrics.metric group by collecttime, `node`, role, account, type having cnt > 1;
