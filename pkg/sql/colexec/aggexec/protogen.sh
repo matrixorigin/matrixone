52a6859b8563d6db65527a868374e2d201f36973
chenmingsong <mschenjj@foxmail.com>
2024-02-28 11:50:33 +0800

    add proto file for agg

---
+++ pkg/sql/colexec/aggexec/protogen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+WORKSPACE=$(dirname $(go env GOMOD))
+VENDOR_DIR="$WORKSPACE/vendor"
+PROTOC_DIR="$WORKSPACE/proto"
+
+# https://github.com/gogo/protobuf.git@v1.3.2
+${GOPATH}/bin/protoc -I=.:$PROTOC_DIR:$VENDOR_DIR --gogofast_out=paths=source_relative:. serialize.proto
+goimports -w *.pb.go
