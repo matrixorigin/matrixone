8db11a402757763c63d22ba4c43afc59fc154aee
qingxinhome <758355272@qq.com>
2024-03-01 17:47:29 +0800

    Resolve package cycle dependency issues

--- pkg/bootstrap/service.go
+++ pkg/bootstrap/service.go
@@ -16,19 +16,15 @@ package bootstrap
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 	"github.com/matrixorigin/matrixone/pkg/fileservice"
 	"github.com/matrixorigin/matrixone/pkg/frontend"
-	"github.com/matrixorigin/matrixone/pkg/pb/task"
-	"github.com/matrixorigin/matrixone/pkg/util/export"
+	"github.com/matrixorigin/matrixone/pkg/predefine"
 	"github.com/matrixorigin/matrixone/pkg/util/file"
 	"github.com/matrixorigin/matrixone/pkg/util/metric/mometric"
 	"github.com/matrixorigin/matrixone/pkg/util/sysview"
 	"github.com/matrixorigin/matrixone/pkg/util/trace/impl/motrace"
-	"github.com/robfig/cron/v3"
-
 	//"github.com/matrixorigin/matrixone/pkg/frontend"
 	"github.com/matrixorigin/matrixone/pkg/logutil"
 	"strings"
@@ -163,7 +159,7 @@ func init() {
 	initSQLs = append(initSQLs, step2InitSQLs...)
 
 	// generate system cron tasks sql
-	sql, err := genInitCronTaskSQL()
+	sql, err := predefine.GenInitCronTaskSQL()
 	if err != nil {
 		panic(err)
 	}
@@ -359,89 +355,89 @@ func initPreprocessSQL(ctx context.Context, txn executor.TxnExecutor) error {
 	return nil
 }
 
-// genInitCronTaskSQL Generate `insert` statement for creating system cron tasks, which works on the `mo_task`.`sys_cron_task` table.
-func genInitCronTaskSQL() (string, error) {
-	cronParser := cron.NewParser(
-		cron.Second |
-			cron.Minute |
-			cron.Hour |
-			cron.Dom |
-			cron.Month |
-			cron.Dow |
-			cron.Descriptor)
-
-	createCronTask := func(value task.TaskMetadata, cronExpr string) (*task.CronTask, error) {
-		sche, err := cronParser.Parse(cronExpr)
-		if err != nil {
-			return nil, err
-		}
-
-		now := time.Now().UnixMilli()
-		next := sche.Next(time.UnixMilli(now))
-
-		return &task.CronTask{
-			Metadata:     value,
-			CronExpr:     cronExpr,
-			NextTime:     next.UnixMilli(),
-			TriggerTimes: 0,
-			CreateAt:     now,
-			UpdateAt:     now,
-		}, nil
-	}
-
-	cronTasks := make([]*task.CronTask, 0, 2)
-	task1, err := createCronTask(export.MergeTaskMetadata(task.TaskCode_MetricLogMerge), export.MergeTaskCronExprEvery05Min)
-	if err != nil {
-		return "", err
-	}
-	cronTasks = append(cronTasks, task1)
-
-	task2, err := createCronTask(mometric.TaskMetadata(mometric.StorageUsageCronTask, task.TaskCode_MetricStorageUsage), mometric.StorageUsageTaskCronExpr)
-	if err != nil {
-		return "", err
-	}
-	cronTasks = append(cronTasks, task2)
-
-	sql := fmt.Sprintf(`insert into %s.sys_cron_task (
-                           task_metadata_id,
-						   task_metadata_executor,
-                           task_metadata_context,
-                           task_metadata_option,
-                           cron_expr,
-                           next_time,
-                           trigger_times,
-                           create_at,
-                           update_at
-                    ) values `, catalog.MOTaskDB)
-
-	for i, t := range cronTasks {
-		j, err := json.Marshal(t.Metadata.Options)
-		if err != nil {
-			return "", err
-		}
-		if i == 0 {
-			sql += fmt.Sprintf("('%s' ,%d ,'%s' ,'%s' ,'%s' ,%d ,%d ,%d ,%d)",
-				t.Metadata.ID,
-				t.Metadata.Executor,
-				t.Metadata.Context,
-				string(j),
-				t.CronExpr,
-				t.NextTime,
-				t.TriggerTimes,
-				t.CreateAt,
-				t.UpdateAt)
-		} else {
-			sql += fmt.Sprintf(",('%s' ,%d ,'%s' ,'%s' ,'%s' ,%d ,%d ,%d ,%d)",
-				t.Metadata.ID,
-				t.Metadata.Executor,
-				t.Metadata.Context,
-				string(j),
-				t.CronExpr,
-				t.NextTime,
-				t.TriggerTimes,
-				t.CreateAt,
-				t.UpdateAt)
-		}
-	}
-	return sql, nil
-}
+//// genInitCronTaskSQL Generate `insert` statement for creating system cron tasks, which works on the `mo_task`.`sys_cron_task` table.
+//func genInitCronTaskSQL() (string, error) {
+//	cronParser := cron.NewParser(
+//		cron.Second |
+//			cron.Minute |
+//			cron.Hour |
+//			cron.Dom |
+//			cron.Month |
+//			cron.Dow |
+//			cron.Descriptor)
+//
+//	createCronTask := func(value task.TaskMetadata, cronExpr string) (*task.CronTask, error) {
+//		sche, err := cronParser.Parse(cronExpr)
+//		if err != nil {
+//			return nil, err
+//		}
+//
+//		now := time.Now().UnixMilli()
+//		next := sche.Next(time.UnixMilli(now))
+//
+//		return &task.CronTask{
+//			Metadata:     value,
+//			CronExpr:     cronExpr,
+//			NextTime:     next.UnixMilli(),
+//			TriggerTimes: 0,
+//			CreateAt:     now,
+//			UpdateAt:     now,
+//		}, nil
+//	}
+//
+//	cronTasks := make([]*task.CronTask, 0, 2)
+//	task1, err := createCronTask(export.MergeTaskMetadata(task.TaskCode_MetricLogMerge), export.MergeTaskCronExprEvery05Min)
+//	if err != nil {
+//		return "", err
+//	}
+//	cronTasks = append(cronTasks, task1)
+//
+//	task2, err := createCronTask(mometric.TaskMetadata(mometric.StorageUsageCronTask, task.TaskCode_MetricStorageUsage), mometric.StorageUsageTaskCronExpr)
+//	if err != nil {
+//		return "", err
+//	}
+//	cronTasks = append(cronTasks, task2)
+//
+//	sql := fmt.Sprintf(`insert into %s.sys_cron_task (
+//                           task_metadata_id,
+//						   task_metadata_executor,
+//                           task_metadata_context,
+//                           task_metadata_option,
+//                           cron_expr,
+//                           next_time,
+//                           trigger_times,
+//                           create_at,
+//                           update_at
+//                    ) values `, catalog.MOTaskDB)
+//
+//	for i, t := range cronTasks {
+//		j, err := json.Marshal(t.Metadata.Options)
+//		if err != nil {
+//			return "", err
+//		}
+//		if i == 0 {
+//			sql += fmt.Sprintf("('%s' ,%d ,'%s' ,'%s' ,'%s' ,%d ,%d ,%d ,%d)",
+//				t.Metadata.ID,
+//				t.Metadata.Executor,
+//				t.Metadata.Context,
+//				string(j),
+//				t.CronExpr,
+//				t.NextTime,
+//				t.TriggerTimes,
+//				t.CreateAt,
+//				t.UpdateAt)
+//		} else {
+//			sql += fmt.Sprintf(",('%s' ,%d ,'%s' ,'%s' ,'%s' ,%d ,%d ,%d ,%d)",
+//				t.Metadata.ID,
+//				t.Metadata.Executor,
+//				t.Metadata.Context,
+//				string(j),
+//				t.CronExpr,
+//				t.NextTime,
+//				t.TriggerTimes,
+//				t.CreateAt,
+//				t.UpdateAt)
+//		}
+//	}
+//	return sql, nil
+//}
