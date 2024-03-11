5a1c20ddff5183ff5ff06d9ded888b587c356644
bRong Njam <longran1989@gmail.com>
2024-03-11 17:08:28 +0800

    filter by prefix_between/prefix_in in Reader

--- pkg/container/vector/search.go
+++ pkg/container/vector/search.go
@@ -161,31 +161,89 @@ func FixedSizedBinarySearchOffsetByValFactory[T any](vals []T, cmp func(T, T) in
 	}
 }
 
-func CollectOffsetsByOnePrefixFactory(prefix []byte) func(*Vector) []int32 {
-	return func(vec *Vector) []int32 {
-		n1 := vec.Length()
-		if n1 == 0 {
+func CollectOffsetsByPrefixEqFactory(val []byte) func(*Vector) []int32 {
+	return func(lvec *Vector) []int32 {
+		lvlen := lvec.Length()
+		if lvlen == 0 {
 			return nil
 		}
-		start, _ := sort.Find(n1, func(i int) int {
-			return bytes.Compare(prefix, vec.GetBytesAt(i))
+		lcol, larea := MustVarlenaRawData(lvec)
+		start, _ := sort.Find(lvlen, func(i int) int {
+			return bytes.Compare(val, lcol[i].GetByteSlice(larea))
 		})
-		if start == n1 {
-			return nil
+		end := start
+		for end < lvlen && bytes.HasPrefix(lcol[end].GetByteSlice(larea), val) {
+			end++
 		}
-		if !bytes.HasPrefix(vec.GetBytesAt(start), prefix) {
+		if start == end {
 			return nil
-		}
-		var end int
-		for end = start + 1; end < n1; end++ {
-			if !bytes.HasPrefix(vec.GetBytesAt(end), prefix) {
-				break
-			}
 		}
 		sels := make([]int32, end-start)
 		for i := start; i < end; i++ {
 			sels[i-start] = int32(i)
 		}
+		return sels
+	}
+}
+
+func CollectOffsetsByPrefixBetweenFactory(lval, rval []byte) func(*Vector) []int32 {
+	return func(lvec *Vector) []int32 {
+		lvlen := lvec.Length()
+		if lvlen == 0 {
+			return nil
+		}
+		lcol, larea := MustVarlenaRawData(lvec)
+		start := sort.Search(lvlen, func(i int) bool {
+			return bytes.Compare(lcol[i].GetByteSlice(larea), lval) >= 0
+		})
+		if start == lvlen {
+			return nil
+		}
+		end := sort.Search(lvlen, func(i int) bool {
+			return types.PrefixCompare(lcol[i].GetByteSlice(larea), rval) > 0
+		})
+		if start == end {
+			return nil
+		}
+		sels := make([]int32, end-start)
+		for i := start; i < end; i++ {
+			sels[i-start] = int32(i)
+		}
+		return sels
+	}
+}
+
+func CollectOffsetsByPrefixInFactory(rvec *Vector) func(*Vector) []int32 {
+	return func(lvec *Vector) []int32 {
+		lvlen := lvec.Length()
+		if lvlen == 0 {
+			return nil
+		}
+
+		lcol, larea := MustVarlenaRawData(lvec)
+		rcol, rarea := MustVarlenaRawData(rvec)
+
+		rval := rcol[0].GetByteSlice(rarea)
+		rpos := 0
+		rvlen := rvec.Length()
+
+		sels := make([]int32, 0, rvlen)
+		for i := 0; i < lvlen; i++ {
+			lval := lcol[i].GetByteSlice(larea)
+			for types.PrefixCompare(lval, rval) > 0 {
+				rpos++
+				if rpos == rvlen {
+					return sels
+				}
+
+				rval = rcol[rpos].GetByteSlice(rarea)
+			}
+
+			if bytes.HasPrefix(lval, rval) {
+				sels = append(sels, int32(i))
+			}
+		}
+
 		return sels
 	}
 }
