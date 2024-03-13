f3c29f0141ad00969dc364fc65fa1c51f9199a9a
Jackson <xzxiong@yeah.net>
2024-03-13 20:22:25 +0800

    CU calculation & metric improvement in 1.1 (#14948)
    cherry-pick #14871, #14927, #14945
    changes
    1. support calculate cu while collect statement_info
    2. add new metric sql_statement_cu, group by sql_source_type
    - add new table `system_metrics.sql_statement_cu` for this metric
    3. add new buildin function: `mo_cu()` and `mo_cu_v1()`
    4. add new configuration `OBCUConfig` to set price config.
    
    Approved by: @daviszhen, @aptend, @nnsgmsone, @ouyuanning, @m-schen, @badboynt1, @heni02, @aunjgr, @zhangxu19830126, @sukki37

--- pkg/util/trace/impl/motrace/report_statement.go
+++ pkg/util/trace/impl/motrace/report_statement.go
@@ -31,6 +31,8 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/logutil"
 	db_holder "github.com/matrixorigin/matrixone/pkg/util/export/etl/db"
 	"github.com/matrixorigin/matrixone/pkg/util/export/table"
+	"github.com/matrixorigin/matrixone/pkg/util/metric"
+	v2 "github.com/matrixorigin/matrixone/pkg/util/metric/v2"
 	"github.com/matrixorigin/matrixone/pkg/util/trace/impl/motrace/statistic"
 
 	"github.com/google/uuid"
@@ -162,6 +164,11 @@ func StatementInfoFilter(i Item) bool {
 		return false
 	}
 
+	// for #14926
+	if statementInfo.statsArray.GetCU() < 0 {
+		return false
+	}
+
 	// Check SqlSourceType
 	switch statementInfo.SqlSourceType {
 	case constant.InternalSql, constant.ExternSql, constant.CloudNoUserSql:
@@ -239,6 +246,7 @@ type StatementInfo struct {
 	// keep []byte as elem
 	jsonByte   []byte
 	statsArray statistic.StatsArray
+	stated     bool
 
 	// skipTxnOnce, readonly, for flow control
 	// see more on NeedSkipTxn() and SkipTxnId()
@@ -263,6 +271,7 @@ var stmtPool = sync.Pool{
 func NewStatementInfo() *StatementInfo {
 	s := stmtPool.Get().(*StatementInfo)
 	s.statsArray.Reset()
+	s.stated = false
 	return s
 }
 
@@ -277,6 +286,10 @@ func (s *StatementInfo) Key(duration time.Duration) interface{} {
 
 func (s *StatementInfo) GetName() string {
 	return SingleStatementTable.GetName()
+}
+
+func (s *StatementInfo) IsMoLogger() bool {
+	return s.Account == "sys" && s.User == db_holder.MOLoggerUser
 }
 
 // deltaContentLength approximate value that may gen as table record
@@ -358,6 +371,7 @@ func (s *StatementInfo) free() {
 	// clean []byte
 	s.jsonByte = nil
 	s.statsArray.Reset()
+	s.stated = false
 	// clean skipTxn ctrl
 	s.skipTxnOnce = false
 	s.skipTxnID = nil
@@ -405,7 +419,8 @@ func (s *StatementInfo) FillRow(ctx context.Context, row *table.Row) {
 		float64Val := calculateAggrMemoryBytes(s.AggrMemoryTime, float64(s.Duration))
 		s.statsArray.WithMemorySize(float64Val)
 	}
-	stats := s.ExecPlan2Stats(ctx)
+	// stats := s.ExecPlan2Stats(ctx) // deprecated
+	stats := s.GetStatsArrayBytes()
 	if GetTracerProvider().disableSqlWriter {
 		// Be careful, this two string is unsafe, will be free after Free
 		row.SetColumnVal(execPlanCol, table.StringField(util.UnsafeBytesToString(execPlan)))
@@ -470,16 +485,12 @@ endL:
 
 // ExecPlan2Stats return Stats Serialized int array str
 // and set RowsRead, BytesScan from ExecPlan
-func (s *StatementInfo) ExecPlan2Stats(ctx context.Context) []byte {
+// and CalculateCU
+func (s *StatementInfo) ExecPlan2Stats(ctx context.Context) error {
 	var stats Statistic
 	var statsArray statistic.StatsArray
 
-	if s.ExecPlan == nil {
-		if s.statsArray.GetVersion() == 0 {
-			s.statsArray.Init()
-		}
-		return s.statsArray.ToJsonString()
-	} else {
+	if s.ExecPlan != nil && !s.stated {
 		statsArray, stats = s.ExecPlan.Stats(ctx)
 		if s.statsArray.GetTimeConsumed() > 0 {
 			logutil.GetSkip1Logger().Error("statsArray.GetTimeConsumed() > 0",
@@ -490,8 +501,15 @@ func (s *StatementInfo) ExecPlan2Stats(ctx context.Context) []byte {
 		s.statsArray.WithConnType(s.ConnType)
 		s.RowsRead = stats.RowsRead
 		s.BytesScan = stats.BytesScan
-		return s.statsArray.ToJsonString()
+		s.stated = true
 	}
+	cu := CalculateCU(s.statsArray, int64(s.Duration))
+	s.statsArray.WithCU(cu)
+	return nil
+}
+
+func (s *StatementInfo) GetStatsArrayBytes() []byte {
+	return s.statsArray.ToJsonString()
 }
 
 // SetSkipTxn set skip txn flag, cooperate with SetSkipTxnId()
@@ -579,6 +597,13 @@ func EndStatement(ctx context.Context, err error, sentRows int64, outBytes int64
 		}
 		outBytes += TcpIpv4HeaderSize * outPacket
 		s.statsArray.InitIfEmpty().WithOutTrafficBytes(float64(outBytes)).WithOutPacketCount(float64(outPacket))
+		s.ExecPlan2Stats(ctx)
+		if s.statsArray.GetCU() < 0 {
+			logutil.Warnf("negative cu: %f, %s", s.statsArray.GetCU(), uuid.UUID(s.StatementID).String())
+			v2.GetTraceNegativeCUCounter("cu").Inc()
+		} else {
+			metric.StatementCUCounter(s.Account, s.SqlSourceType).Add(s.statsArray.GetCU())
+		}
 		s.Status = StatementStatusSuccess
 		if err != nil {
 			s.Error = err
@@ -626,9 +651,9 @@ var ReportStatement = func(ctx context.Context, s *StatementInfo) error {
 		return nil
 	}
 	// Filter out the MO_LOGGER SQL statements
-	if s.User == db_holder.MOLoggerUser {
-		goto DiscardAndFreeL
-	}
+	//if s.User == db_holder.MOLoggerUser {
+	//	goto DiscardAndFreeL
+	//}
 
 	// Filter out the statement is empty
 	if s.Statement == "" {
@@ -649,7 +674,7 @@ var ReportStatement = func(ctx context.Context, s *StatementInfo) error {
 	}
 
 	// logging the statement that should not be here anymore
-	if s.exported || s.reported || s.User == db_holder.MOLoggerUser || s.Statement == "" {
+	if s.exported || s.reported || s.Statement == "" {
 		logutil.Error("StatementInfo should not be here anymore", zap.String("StatementInfo", s.Statement), zap.String("statement_id", uuid.UUID(s.StatementID).String()), zap.String("user", s.User), zap.Bool("exported", s.exported), zap.Bool("reported", s.reported))
 	}
 
