46253110512f0a742eb929ff2f7685b8841012fe
GreatRiver <2552853833@qq.com>
2024-03-18 16:52:20 +0800

    Update code

--- pkg/vm/engine/tae/db/gc/table.go
+++ pkg/vm/engine/tae/db/gc/table.go
@@ -349,8 +349,7 @@ func (t *GCTable) ReadTable(ctx context.Context, name string, size int64, fs *ob
 	if err != nil {
 		return err
 	}
-		logutil.Infof("rebuild table v2 %v", len(bats[CreateBlock].Vecs))
-	if len(bats[CreateBlock].Vecs) == 7 {
+	if len(bats) == 1 {
 		t.rebuildTableV2(bats)
 		return nil
 	}
