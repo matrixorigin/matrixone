610b4d788a54ec58e48f4d037ade74a84123b336
nnsgmsone <nnsmgsone@outlook.com>
2024-03-20 14:14:45 +0800

    Revert "Ignore warning of unsafe"
    This reverts commit 511ad76b0a04408e5e38cbea5f3e3ee416b59b32.

--- pkg/common/malloc/malloc.go
+++ pkg/common/malloc/malloc.go
@@ -15,19 +15,37 @@
 package malloc
 
 import (
-	"github.com/nnsgmsone/mmap"
+	"syscall"
+	"unsafe"
 )
+
+var fd = -1
+
+//go:linkname throw runtime.throw
+func throw(s string)
 
 // New allocates a slice of size n. The returned slice is from manually managed
 // memory and MUST be released by calling Free. Failure to do so will result in
 // a memory leak.
 func Alloc(n int) []byte {
-	return mmap.Alloc(n)
+	if n == 0 {
+		return make([]byte, 0)
+	}
+	size := rollup(n)
+	r0, _, e1 := syscall.Syscall6(syscall.SYS_MMAP, 0, uintptr(size), uintptr(syscall.PROT_READ|syscall.PROT_WRITE),
+		uintptr(syscall.MAP_ANON|syscall.MAP_PRIVATE), uintptr(fd), uintptr(0))
+	if e1 != 0 {
+		throw("out of memory")
+	}
+	//lint:ignore unsafe.Pointer: possible misuse of unsafe.Pointer
+	return unsafe.Slice((*byte)(unsafe.Pointer(r0)), n)
 }
 
 func Free(b []byte) {
-	if cap(b) == 0 {
-		return
-	}
-	mmap.Free(b)
+	size := int64(rollup(cap(b)))
+	syscall.Syscall(syscall.SYS_MUNMAP, uintptr(unsafe.Pointer(&b[0])), uintptr(size), 0)
+}
+
+func rollup(n int) int {
+	return (n + 4095) & (^4095)
 }
