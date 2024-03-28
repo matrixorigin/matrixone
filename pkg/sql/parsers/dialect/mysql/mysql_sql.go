63080f72854c84f38f26b9ee21c703272ce0b7eb
YANGGMM <18916107305@163.com>
2024-03-28 13:37:47 +0800

    make fmt

--- pkg/sql/parsers/dialect/mysql/mysql_sql.go
+++ pkg/sql/parsers/dialect/mysql/mysql_sql.go
@@ -12945,7 +12945,7 @@ yydefault:
 //line mysql_sql.y:3291
 		{
 			assignments := []*tree.VarAssignmentExpr{
-				&tree.VarAssignmentExpr{
+				{
 					System: true,
 					Global: true,
 					Name:   yyDollar[6].str,
@@ -13254,7 +13254,7 @@ yydefault:
 		{
 			s := &tree.ShowGrants{}
 			roles := []*tree.Role{
-				&tree.Role{UserName: yyDollar[5].cstrUnion().Compare()},
+				{UserName: yyDollar[5].cstrUnion().Compare()},
 			}
 			s.Roles = roles
 			s.ShowGrantType = tree.GrantForRole
