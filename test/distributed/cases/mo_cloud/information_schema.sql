aa8651f943803c311bdab56fe7587ec057f987b4
Ariznawlll <ariznawl@163.com>
2024-03-29 11:08:35 +0800

    add collate utf_8 case(1.1-dev) (#15223)

--- test/distributed/cases/mo_cloud/information_schema.sql
+++ test/distributed/cases/mo_cloud/information_schema.sql
@@ -116,3 +116,4 @@ order by
     table_name,
     grantee,
     privilege_type;
+select table_name from information_schema.tables where table_schema collate utf8_general_ci = 'information_schema' and table_name collate utf8_general_ci = 'parameters';
