5e0d41a8e2d373419d71b665f82bad8590d8affb
YANGGMM <18916107305@163.com>
2024-04-01 11:47:19 +0800

    add bvt test

--- test/distributed/cases/database/create_table_like.sql
+++ test/distributed/cases/database/create_table_like.sql
@@ -15,3 +15,13 @@ create table new_test like test1.test;
 show create table new_test;
 drop database test1;
 drop database test2;
+
+create database test1;
+use test1;
+create table test(a int);
+insert into test values(1);
+create table test like test;
+create view view1 as select * from test;
+show create view view1;
+create table new_test like view1;
+drop database test1;
