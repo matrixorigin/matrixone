922e9fe2c3fd7c0f2dafa77aee670fc7ddc14c65
gouhongshen <gouhongshen@hotmail.com>
2024-04-01 11:04:45 +0800

    remove debug log

--- pkg/vm/engine/tae/tables/mnode.go
+++ pkg/vm/engine/tae/tables/mnode.go
@@ -475,10 +475,6 @@ func (node *memoryNode) checkConflictAandVisibility(
 			node.object.RLock()
 		}
 	} else {
-		if n == nil || txn == nil {
-			x := 0
-			x++
-		}
 		needWait, txn := n.NeedWaitCommitting(txn.GetStartTS())
 		if needWait {
 			node.object.RUnlock()
