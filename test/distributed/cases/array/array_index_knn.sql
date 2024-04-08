1d794c9c52df215fce78a5224a4dd0bf35fff054
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2024-04-08 12:09:16 -0700

    bvt

--- test/distributed/cases/array/array_index_knn.sql
+++ test/distributed/cases/array/array_index_knn.sql
@@ -18,7 +18,7 @@ insert into t1 values(9, "[3,3,3,0]", "9");
 select a, b from t1 order by l2_distance(normalize_l2(b), normalize_l2("[1,1,1,0]")) limit 3;
 
 create index idx1 using ivfflat on t1(b) lists=3 op_type "vector_l2_ops";
-select a, b from t1 order by l2_distance(b, "[1,1,1,0]") limit 3;
+select a, b from t1 order by l2_distance(b, "[1,1,1,0]") limit 4;
 
 insert into t1 values(10, "[4,4,4,0]", "10");
 select a, b from t1 order by l2_distance(b, "[1,1,1,0]") limit 4;
@@ -73,6 +73,39 @@ select a, b from t2 order by l2_distance(b, "[1,1,0,0]") limit 4;
 select a, b from t2 order by l2_distance(b, "[1,1,1,0]") limit 4;
 select a, b from t2 order by l2_distance(b, "[1,1,1,1]") limit 4;
 
+-- 3.a KNN with no PK and embedding alone
+drop table if exists t3;
+create table t3(a int,b vecf32(4), c varchar(3));
+insert into t3 values(1, "[1,0,0,0]" , "1");
+insert into t3 values(2, "[2,0,0,0]", "2");
+insert into t3 values(3, "[3,0,0,0]", "3");
+insert into t3 values(4, "[1,1,0,0]", "4");
+insert into t3 values(5, "[2,2,0,0]", "5");
+insert into t3 values(6, "[3,3,0,0]", "6");
+insert into t3 values(7, "[1,1,1,0]", "7");
+insert into t3 values(8, "[2,2,2,0]", "8");
+insert into t3 values(9, "[3,3,3,0]", "9");
+select a, b from t3 order by l2_distance(normalize_l2(b), normalize_l2("[1,1,1,0]")) limit 3;
+
+create index idx3 using ivfflat on t3(b) lists=3 op_type "vector_l2_ops";
+select a, b from t3 order by l2_distance(b, "[1,1,1,0]") limit 4;
+
+insert into t3 values(10, "[4,4,4,0]", "10");
+select a, b from t3 order by l2_distance(b, "[1,1,1,0]") limit 4;
+
+
+-- 3.b KNN with no PK and embedding and non-indexed columns
+select a,b,c from t3 order by l2_distance(b, "[1,1,1,0]") limit 4;
+
+insert into t3 values(11, "[1,1,1,1]", "11");
+insert into t3 values(12, "[2,2,2,2]", "12");
+insert into t3 values(13, "[3,3,3,3]", "13");
+
+alter table t3 alter reindex idx3 ivfflat lists=4;
+select a, b from t3 order by l2_distance(b, "[1,0,0,0]") limit 4;
+select a, b from t3 order by l2_distance(b, "[1,1,0,0]") limit 4;
+select a, b from t3 order by l2_distance(b, "[1,1,1,0]") limit 4;
+select a, b from t3 order by l2_distance(b, "[1,1,1,1]") limit 4;
 
 -- post
 drop database vecdb3;
