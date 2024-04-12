f9fd18b912003760af396a5f6b16a3bb14730fbb
Jackson <xzxiong@yeah.net>
2024-04-12 21:52:12 +0800

    remove logutil.ConnectionIdField(ses.GetConnectionID())

--- pkg/frontend/util.go
+++ pkg/frontend/util.go
@@ -485,10 +485,10 @@ func logStatementStringStatus(ctx context.Context, ses FeSession, stmtStr string
 	str := SubStringFromBegin(stmtStr, int(getGlobalPu().SV.LengthOfQueryPrinted))
 	outBytes, outPacket := ses.GetMysqlProtocol().CalculateOutTrafficBytes(true)
 	if status == success {
-		ses.Debug(ctx, "query trace status", logutil.ConnectionIdField(ses.GetConnectionID()), logutil.StatementField(str), logutil.StatusField(status.String()))
+		ses.Debug(ctx, "query trace status", logutil.StatementField(str), logutil.StatusField(status.String()))
 		err = nil // make sure: it is nil for EndStatement
 	} else {
-		ses.Error(ctx, "query trace status", logutil.ConnectionIdField(ses.GetConnectionID()), logutil.StatementField(str), logutil.StatusField(status.String()), logutil.ErrorField(err))
+		ses.Error(ctx, "query trace status", logutil.StatementField(str), logutil.StatusField(status.String()), logutil.ErrorField(err))
 	}
 
 	// pls make sure: NO ONE use the ses.tStmt after EndStatement
