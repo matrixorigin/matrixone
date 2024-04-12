9c9034660be00e6f98649a8d8ebf2be2b2390d77
YANGGMM <18916107305@163.com>
2024-04-12 17:37:20 +0800

    fix ut

--- pkg/frontend/authenticate_test.go
+++ pkg/frontend/authenticate_test.go
@@ -11830,7 +11830,7 @@ func TestDoResolveSnapshotTsWithSnapShotName(t *testing.T) {
 		bh.sql2result["rollback;"] = nil
 
 		sql, _ := getSqlForGetSnapshotTsWithSnapshotName(ctx, "test_sp")
-		mrs := newMrsForPasswordOfUser([][]interface{}{{"2024-01-01 00:00:00"}})
+		mrs := newMrsForPasswordOfUser([][]interface{}{{10}})
 		bh.sql2result[sql] = mrs
 
 		_, err := doResolveSnapshotTsWithSnapShotName(ctx, ses, "test_sp")
