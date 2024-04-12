1558e4cac2f522b5ac1b59b72cd8011c2a5e4d9a
YANGGMM <18916107305@163.com>
2024-04-12 10:53:49 +0800

    fix bvt

--- test/distributed/cases/zz_accesscontrol/alter_account.sql
+++ test/distributed/cases/zz_accesscontrol/alter_account.sql
@@ -133,35 +133,6 @@ select account_name,status from mo_catalog.mo_account where account_name='testsu
 drop account testsuspend;
 select account_name,status from mo_catalog.mo_account where account_name='testsuspend';
 
---prepared
-drop account if exists test;
-create account test ADMIN_NAME 'admin' IDENTIFIED BY '111';
-PREPARE alter_ac_1 FROM alter account ? admin_name= ? IDENTIFIED BY ?;
-set @a_var = 'test';
-set @b_var = 'admin';
-set @c_var = '222';
-EXECUTE alter_ac_1 USING @a_var, @b_var, @c_var;
-DEALLOCATE PREPARE alter_ac_1;
--- @session:id=2&user=test:admin&password=222
-select 1;
--- @session
-PREPARE alter_ac_2 FROM "alter account ? admin_name 'admin' IDENTIFIED BY ?";
-set @a_var = 'test';
-set @c_var = '333';
-EXECUTE alter_ac_2 USING @a_var, @c_var;
-DEALLOCATE PREPARE alter_ac_2;
--- @session:id=2&user=test:admin&password=333
-select 1;
--- @session
-PREPARE alter_ac_3 FROM "alter account test admin_name 'admin' IDENTIFIED BY ?";
-set @c_var = '444';
-EXECUTE alter_ac_3 USING @c_var;
-DEALLOCATE PREPARE alter_ac_3;
--- @session:id=2&user=test:admin&password=444
-select 1;
--- @session
-drop account if exists test;
-
 --Illegal syntax
 create account if not exists test ADMIN_NAME 'adminuser' IDENTIFIED BY '123456' comment 'account comment';
 alter account test admin_name='adminuser'  IDENTIFIED BY '123456' comment 'new comment ' ;
