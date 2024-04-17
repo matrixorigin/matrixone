5db3bed2fbee26dd5ccedd2cc6abdd3a887eee90
GreatRiver <2552853833@qq.com>
2024-04-17 19:42:26 +0800

    Fix ListSnapshotCheckpoint

--- pkg/vm/engine/tae/db/checkpoint/snapshot.go
+++ pkg/vm/engine/tae/db/checkpoint/snapshot.go
@@ -89,7 +89,10 @@ func ListSnapshotCheckpoint(
 		}
 		bat.AddVector(colNames[i], vec)
 	}
-	entries, maxGlobalEnd := replayCheckpointEntries(bat, 3)
+	entries, maxGlobalEnd := replayCheckpointEntries(bat, 9)
+	sort.Slice(entries, func(i, j int) bool {
+		return entries[i].end.Less(&entries[j].end)
+	})
 	for i := range entries {
 		if entries[i].end.Equal(&maxGlobalEnd) && entries[i].entryType == ET_Global {
 			return entries[i:], nil
