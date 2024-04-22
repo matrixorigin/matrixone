08641dc937b287613e6c33c8852145211c68ddf5
maomao <77312370+sukki37@users.noreply.github.com>
2024-04-22 10:38:19 +0800

    Revert "[Fix] : replace point locks with table lock in some cases (#15578)"
    This reverts commit 57def8e01aad11c56b91943ea8a2210a4ff67da9.

--- pkg/sql/plan/build_dml_util.go
+++ pkg/sql/plan/build_dml_util.go
@@ -33,12 +33,13 @@ import (
 	"golang.org/x/exp/slices"
 )
 
+var CNPrimaryCheck = false
+
 var dmlPlanCtxPool = sync.Pool{
 	New: func() any {
 		return &dmlPlanCtx{}
 	},
 }
-
 var deleteNodeInfoPool = sync.Pool{
 	New: func() any {
 		return &deleteNodeInfo{}
@@ -1123,7 +1124,6 @@ func buildDeletePlans(ctx CompilerContext, builder *QueryBuilder, bindCtx *BindC
 						upPlanCtx.insertColPos = insertColPos
 						upPlanCtx.isFkRecursionCall = true
 						upPlanCtx.updatePkCol = updatePk
-						upPlanCtx.lockTable = ifNeedLockWholeTable(builder, lastNodeId)
 
 						err = buildUpdatePlans(ctx, builder, bindCtx, upPlanCtx, false)
 						putDmlPlanCtx(upPlanCtx)
@@ -1169,7 +1169,6 @@ func buildDeletePlans(ctx CompilerContext, builder *QueryBuilder, bindCtx *BindC
 								upPlanCtx.allDelTableIDs = map[uint64]struct{}{}
 								upPlanCtx.isFkRecursionCall = true
 								upPlanCtx.updatePkCol = updatePk
-								upPlanCtx.lockTable = ifNeedLockWholeTable(builder, lastNodeId)
 
 								err = buildUpdatePlans(ctx, builder, bindCtx, upPlanCtx, false)
 								putDmlPlanCtx(upPlanCtx)
@@ -1200,7 +1199,6 @@ func buildDeletePlans(ctx CompilerContext, builder *QueryBuilder, bindCtx *BindC
 								upPlanCtx.sourceStep = newSourceStep
 								upPlanCtx.beginIdx = 0
 								upPlanCtx.allDelTableIDs = allDelTableIDs
-								upPlanCtx.lockTable = ifNeedLockWholeTable(builder, lastNodeId)
 
 								err := buildDeletePlans(ctx, builder, bindCtx, upPlanCtx)
 								putDmlPlanCtx(upPlanCtx)
@@ -2343,13 +2341,12 @@ func appendPreInsertNode(builder *QueryBuilder, bindCtx *BindContext,
 	}
 
 	if !isUpdate {
-		ifLockTable := ifNeedLockWholeTable(builder, lastNodeId)
 		if lockNodeId, ok := appendLockNode(
 			builder,
 			bindCtx,
 			lastNodeId,
 			tableDef,
-			ifLockTable,
+			false,
 			false,
 			partitionIdx,
 			partTableIds,
@@ -2434,13 +2431,12 @@ func appendPreInsertSkMasterPlan(builder *QueryBuilder,
 	}
 
 	// 3. add lock
-	ifLockTable := ifNeedLockWholeTable(builder, lastNodeId)
 	if lockNodeId, ok := appendLockNode(
 		builder,
 		bindCtx,
 		lastNodeId,
 		indexTableDef,
-		ifLockTable,
+		false,
 		false,
 		-1,
 		nil,
@@ -2687,13 +2683,12 @@ func appendPreInsertSkVectorPlan(
 
 	lastNodeId = projectId
 
-	ifLockTable := ifNeedLockWholeTable(builder, lastNodeId)
 	if lockNodeId, ok := appendLockNode(
 		builder,
 		bindCtx,
 		lastNodeId,
 		indexTableDefs[2],
-		ifLockTable,
+		false,
 		false,
 		-1,
 		nil,
@@ -2896,13 +2891,12 @@ func appendPreInsertUkPlan(
 	}
 	lastNodeId = builder.appendNode(preInsertUkNode, bindCtx)
 
-	ifLockTable := ifNeedLockWholeTable(builder, lastNodeId)
 	if lockNodeId, ok := appendLockNode(
 		builder,
 		bindCtx,
 		lastNodeId,
 		uniqueTableDef,
-		ifLockTable,
+		false,
 		false,
 		-1,
 		nil,
@@ -3544,7 +3538,7 @@ func makePreUpdateDeletePlan(
 		PrimaryColIdxInBat: int32(pkPos),
 		PrimaryColTyp:      pkTyp,
 		RefreshTsIdxInBat:  -1,
-		LockTable:          delCtx.lockTable,
+		LockTable:          false,
 	}
 	if delCtx.tableDef.Partition != nil {
 		lockTarget.IsPartitionTable = true
@@ -3633,7 +3627,7 @@ func makePreUpdateDeletePlan(
 			PrimaryColIdxInBat: newPkPos,
 			PrimaryColTyp:      pkTyp,
 			RefreshTsIdxInBat:  -1, //unsupport now
-			LockTable:          delCtx.lockTable,
+			LockTable:          false,
 		}
 		if delCtx.tableDef.Partition != nil {
 			lockTarget.IsPartitionTable = true
@@ -3711,6 +3705,10 @@ func appendLockNode(
 	}
 	pkPos, pkTyp := getPkPos(tableDef, false)
 	if pkPos == -1 {
+		return -1, false
+	}
+
+	if builder.qry.LoadTag && !lockTable {
 		return -1, false
 	}
 
