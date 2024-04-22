8d2d502c095badcf0d9aa9ee3551f0669607176e
badboynt1 <badboynt@126.com>
2024-04-22 12:01:33 +0800

    fix

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -3618,6 +3618,9 @@ func (c *Compile) determinExpandRanges(n *plan.Node, rel engine.Relation) bool {
 	if n.TableDef.Partition != nil {
 		return true
 	}
+	if len(n.RuntimeFilterProbeList) == 0 {
+		return true
+	}
 	if n.Stats.BlockNum > plan2.BlockNumForceOneCN && len(c.cnList) > 1 {
 		return true
 	}
