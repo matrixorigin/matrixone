ceae3f84ae1a379d3cf4433cfe0e50ddaa438f3c
YANGGMM <18916107305@163.com>
2024-04-23 12:04:14 +0800

    add bvt test

--- test/distributed/cases/snapshot/snapshot_restore_database_level.sql
+++ test/distributed/cases/snapshot/snapshot_restore_database_level.sql
@@ -320,4 +320,147 @@ select count(*) from snapshot_read.v2;
 drop snapshot sp_01;
 drop account test_account;
 
+
+-- sys account restore normal account to new account drop table
+create account test_account admin_name = 'test_user' identified by '111';
+
+-- @session:id=8&user=test_account:test_user&password=111
+create database if not exists snapshot_read;
+use snapshot_read;
+create table test_snapshot_read (a int);
+insert into test_snapshot_read (a) values(1), (2), (3), (4), (5),(6), (7), (8), (9), (10), (11), (12),(13), (14), (15), (16), (17), (18), (19), (20),(21), (22), (23), (24), (25), (26), (27), (28), (29), (30),(31), (32), (33), (34), (35), (36), (37), (38), (39), (40),(41), (42), (43), (44), (45), (46), (47), (48), (49), (50),(51), (52), (53), (54), (55), (56), (57), (58), (59), (60),(61), (62), (63), (64), (65), (66), (67), (68), (69), (70),(71), (72), (73), (74), (75), (76), (77), (78), (79), (80), (81), (82), (83), (84), (85), (86), (87), (88), (89), (90),(91), (92), (93), (94), (95), (96), (97), (98), (99), (100);
+select count(*) from snapshot_read.test_snapshot_read;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO users (username, email, password) VALUES ('john_doe', 'john@example.com', 'securepassword123');
+INSERT INTO users (username, email, password) VALUES ('jane_smith', 'jane.smith@example.com', 'password123'),('alice_jones', 'alice.jones@gmail.com', 'ilovecats'),('bob_brown', 'bob.brown@yahoo.com', 'mysecretpassword'),('charlie_lee', 'charlie.lee@protonmail.ch', 'secure123'),('diana_wilson', 'diana.wilson@outlook.com', 'D1anaPass');
+INSERT INTO users (username, email, password) VALUES ('emily_adams', 'emily.adams@icloud.com', 'Em1Ly123'), ('francis_nguyen', 'francis.nguyen@domain.com', 'fNguyenPass'), ('grace_parker', 'grace.parker@server.com', 'G1race123'), ('henry_miller', 'henry.miller@company.org', 'hMillerSecret'), ('isabella_grant', 'isabella.grant@university.edu', 'iGrantPass');
+
+Create view v1 as select * from snapshot_read.users;
+select count(*) from snapshot_read.users;
+select count(*) from snapshot_read.v1;
+
+CREATE TABLE students (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL
+);
+
+INSERT INTO students (name, age) VALUES ('Alice', 20), ('Bob', 21), ('Charlie', 22), ('Diana', 23), ('Emily', 24);
+INSERT INTO students (name, age) VALUES ('Francis', 25), ('Grace', 26), ('Henry', 27), ('Isabella', 28), ('Jack', 29);
+INSERT INTO students (name, age) VALUES ('Katherine', 30), ('Liam', 31), ('Mia', 32), ('Noah', 33), ('Olivia', 34);
+INSERT INTO students (name, age) VALUES ('Penelope', 35), ('Quinn', 36), ('Ryan', 37), ('Sophia', 38), ('Thomas', 39);
+INSERT INTO students (name, age) VALUES ('Ursula', 40), ('Victor', 41), ('Wendy', 42), ('Xander', 43), ('Yvonne', 44);
+INSERT INTO students (name, age) VALUES ('Zachary', 45), ('Ava', 46), ('Benjamin', 47), ('Charlotte', 48), ('Daniel', 49);
+INSERT INTO students (name, age) VALUES ('Ella', 50), ('Finn', 51), ('Gabriella', 52), ('Henry', 53), ('Isabella', 54);
+INSERT INTO students (name, age) VALUES ('Jack', 55), ('Katherine', 56), ('Liam', 57), ('Mia', 58), ('Noah', 59);
+INSERT INTO students (name, age) VALUES ('Olivia', 60), ('Penelope', 61), ('Quinn', 62), ('Ryan', 63), ('Sophia', 64);
+INSERT INTO students (name, age) VALUES ('Thomas', 65), ('Ursula', 66), ('Victor', 67), ('Wendy', 68), ('Xander', 69);
+
+Create view v2 as select * from snapshot_read.students;
+select count(*) from snapshot_read.students;
+select count(*) from snapshot_read.v2;
+-- @session
+
+create snapshot sp_01 for account 'test_account';
+
+-- @session:id=9&user=test_account:test_user&password=111
+drop table students;
+drop table users;
+drop view v1;
+drop view v2;
+-- @session
+
+
+create account test_account_2 admin_name = 'test_user_2' identified by '111';
+restore account 'test_account' database 'snapshot_read' from snapshot 'sp_01' to NEWACCOUNT 'test_account_2';
+
+-- @session:id=10&user=test_account_2:test_user_2&password=111
+select count(*) from snapshot_read.students;
+select count(*) from snapshot_read.users;
+select count(*) from snapshot_read.v1;
+select count(*) from snapshot_read.v2;
+-- @session
+
+drop snapshot sp_01;
+drop account test_account;
+drop account test_account_2;
+
+
+
+
+-- sys account restore normal account to new account drop database
+create account test_account admin_name = 'test_user' identified by '111';
+
+-- @session:id=11&user=test_account:test_user&password=111
+create database if not exists snapshot_read;
+use snapshot_read;
+create table test_snapshot_read (a int);
+insert into test_snapshot_read (a) values(1), (2), (3), (4), (5),(6), (7), (8), (9), (10), (11), (12),(13), (14), (15), (16), (17), (18), (19), (20),(21), (22), (23), (24), (25), (26), (27), (28), (29), (30),(31), (32), (33), (34), (35), (36), (37), (38), (39), (40),(41), (42), (43), (44), (45), (46), (47), (48), (49), (50),(51), (52), (53), (54), (55), (56), (57), (58), (59), (60),(61), (62), (63), (64), (65), (66), (67), (68), (69), (70),(71), (72), (73), (74), (75), (76), (77), (78), (79), (80), (81), (82), (83), (84), (85), (86), (87), (88), (89), (90),(91), (92), (93), (94), (95), (96), (97), (98), (99), (100);
+select count(*) from snapshot_read.test_snapshot_read;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO users (username, email, password) VALUES ('john_doe', 'john@example.com', 'securepassword123');
+INSERT INTO users (username, email, password) VALUES ('jane_smith', 'jane.smith@example.com', 'password123'),('alice_jones', 'alice.jones@gmail.com', 'ilovecats'),('bob_brown', 'bob.brown@yahoo.com', 'mysecretpassword'),('charlie_lee', 'charlie.lee@protonmail.ch', 'secure123'),('diana_wilson', 'diana.wilson@outlook.com', 'D1anaPass');
+INSERT INTO users (username, email, password) VALUES ('emily_adams', 'emily.adams@icloud.com', 'Em1Ly123'), ('francis_nguyen', 'francis.nguyen@domain.com', 'fNguyenPass'), ('grace_parker', 'grace.parker@server.com', 'G1race123'), ('henry_miller', 'henry.miller@company.org', 'hMillerSecret'), ('isabella_grant', 'isabella.grant@university.edu', 'iGrantPass');
+
+Create view v1 as select * from snapshot_read.users;
+select count(*) from snapshot_read.users;
+select count(*) from snapshot_read.v1;
+
+CREATE TABLE students (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL
+);
+
+INSERT INTO students (name, age) VALUES ('Alice', 20), ('Bob', 21), ('Charlie', 22), ('Diana', 23), ('Emily', 24);
+INSERT INTO students (name, age) VALUES ('Francis', 25), ('Grace', 26), ('Henry', 27), ('Isabella', 28), ('Jack', 29);
+INSERT INTO students (name, age) VALUES ('Katherine', 30), ('Liam', 31), ('Mia', 32), ('Noah', 33), ('Olivia', 34);
+INSERT INTO students (name, age) VALUES ('Penelope', 35), ('Quinn', 36), ('Ryan', 37), ('Sophia', 38), ('Thomas', 39);
+INSERT INTO students (name, age) VALUES ('Ursula', 40), ('Victor', 41), ('Wendy', 42), ('Xander', 43), ('Yvonne', 44);
+INSERT INTO students (name, age) VALUES ('Zachary', 45), ('Ava', 46), ('Benjamin', 47), ('Charlotte', 48), ('Daniel', 49);
+INSERT INTO students (name, age) VALUES ('Ella', 50), ('Finn', 51), ('Gabriella', 52), ('Henry', 53), ('Isabella', 54);
+INSERT INTO students (name, age) VALUES ('Jack', 55), ('Katherine', 56), ('Liam', 57), ('Mia', 58), ('Noah', 59);
+INSERT INTO students (name, age) VALUES ('Olivia', 60), ('Penelope', 61), ('Quinn', 62), ('Ryan', 63), ('Sophia', 64);
+INSERT INTO students (name, age) VALUES ('Thomas', 65), ('Ursula', 66), ('Victor', 67), ('Wendy', 68), ('Xander', 69);
+
+Create view v2 as select * from snapshot_read.students;
+select count(*) from snapshot_read.students;
+select count(*) from snapshot_read.v2;
+-- @session
+
+create snapshot sp_01 for account 'test_account';
+
+-- @session:id=12&user=test_account:test_user&password=111
+drop database snapshot_read;
+-- @session
+
+
+create account test_account_2 admin_name = 'test_user_2' identified by '111';
+restore account 'test_account' database 'snapshot_read' from snapshot 'sp_01' to NEWACCOUNT 'test_account_2';
+
+-- @session:id=13&user=test_account_2:test_user_2&password=111
+select count(*) from snapshot_read.students;
+select count(*) from snapshot_read.users;
+select count(*) from snapshot_read.v1;
+select count(*) from snapshot_read.v2;
+-- @session
+
+drop snapshot sp_01;
+drop account test_account;
+drop account test_account_2;
+
 -- @bvt:issue
