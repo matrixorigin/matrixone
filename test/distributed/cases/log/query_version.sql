75eef7d392e7ed0d73fb4f6a577a109b3ca7f060
Jensen <jensenojs@qq.com>
2024-04-24 23:26:50 +0800

    add bvt tag

--- test/distributed/cases/log/query_version.sql
+++ test/distributed/cases/log/query_version.sql
@@ -11,4 +11,6 @@ select sleep(15) as s;
 
 select count(1) as cnt, statement_id, statement, status from system.statement_info group by statement_id, statement, status having count(1) > 1;
 
+-- @bvt:issue#15705
 select `database`, `aggr_count` from system.statement_info where statement LIKE '%select * from user limit 0%' order by request_at desc limit 1;
+-- @bvt:issue
