2164d3004b1636492a996ad780624916783bf145
nitao <badboynt@126.com>
2024-04-24 11:28:23 +0800

    when read blocks less than 200, force scan on one CN (#15680)
    when read blocks less than 200, force scan on one CN
    
    Approved by: @nnsgmsone, @sukki37

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -3893,7 +3893,7 @@ func (c *Compile) generateNodes(n *plan.Node) (engine.Nodes, []any, []types.T, e
 	}
 	// for multi cn in launch mode, put all payloads in current CN
 	// maybe delete this in the future
-	if isLaunchMode(c.cnList) {
+	if isLaunchMode(c.cnList) || len(ranges) < plan2.BlockNumForceOneCN {
 		return putBlocksInCurrentCN(c, ranges, rel, n), partialResults, partialResultTypes, nil
 	}
 	// disttae engine
@@ -4023,7 +4023,7 @@ func shuffleBlocksToMultiCN(c *Compile, ranges [][]byte, rel engine.Relation, n 
 		}
 	}
 	if minWorkLoad*2 < maxWorkLoad {
-		logstring := fmt.Sprintf("read table %v ,workload among %v nodes not balanced, max %v, min %v,", n.TableDef.Name, len(newNodes), maxWorkLoad, minWorkLoad)
+		logstring := fmt.Sprintf("read table %v ,workload %v blocks among %v nodes not balanced, max %v, min %v,", n.TableDef.Name, len(ranges), len(newNodes), maxWorkLoad, minWorkLoad)
 		logstring = logstring + " cnlist: "
 		for i := range c.cnList {
 			logstring = logstring + c.cnList[i].Addr + " "
