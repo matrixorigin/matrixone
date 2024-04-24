7534d85dfb7591ad2d369de2d61241ce02f6b55c
fagongzi <zhangxu19830126@gmail.com>
2024-04-24 15:37:32 +0800

    fix unlock orphan txn (#15671)
    When we find an orphaned transaction, we go to TN to mark the current transaction as cannot commit txn and release the lock on the orphaned transaction when we get a response from DN. However, this is not safe, because the commit request of the orphaned transaction may have reached the DN before that time, causing the lock to be released incorrectly.
    
    Approved by: @iamlinjunhong

--- pkg/lockservice/service.go
+++ pkg/lockservice/service.go
@@ -573,7 +573,7 @@ type mapBasedTxnHolder struct {
 	serviceID string
 	fsp       *fixedSlicePool
 	valid     func(sid string) (bool, error)
-	notify    func([]pb.OrphanTxn) error
+	notify    func([]pb.OrphanTxn) ([][]byte, error)
 	mu        struct {
 		sync.RWMutex
 		// remoteServices known remote service
@@ -589,7 +589,7 @@ func newMapBasedTxnHandler(
 	serviceID string,
 	fsp *fixedSlicePool,
 	valid func(sid string) (bool, error),
-	notify func([]pb.OrphanTxn) error,
+	notify func([]pb.OrphanTxn) ([][]byte, error),
 ) activeTxnHolder {
 	h := &mapBasedTxnHolder{}
 	h.fsp = fsp
@@ -752,10 +752,30 @@ func (h *mapBasedTxnHolder) getTimeoutRemoveTxn(
 	h.mu.Unlock()
 
 	if len(cannotCommit) > 0 {
-		if err := h.notify(cannotCommit); err == nil {
+		// found txn1 cannot commit, but txn1 is still running in other cn.
+		// There are 2 possible timings here:
+		// 1. txn1's commit request arrive TN before cannot commit request
+		// 2. txn1's commit request arrive TN after cannot commit request
+		//
+		// In case1: we cannot make txn1 as timeout txn.
+		// In case2: txn1'commit request will failed, and we can make txn1 as
+		//           timeout txn.
+		if committing, err := h.notify(cannotCommit); err == nil {
 			for sid, idx := range cannotCommitServices {
-				needRemoved = append(needRemoved, cannotCommit[idx].Txn...)
-				timeoutServices[sid] = struct{}{}
+				if len(committing) == 0 {
+					needRemoved = append(needRemoved, cannotCommit[idx].Txn...)
+					timeoutServices[sid] = struct{}{}
+				} else {
+					m := make(map[string]struct{}, len(committing))
+					for _, v := range committing {
+						m[util.UnsafeBytesToString(v)] = struct{}{}
+					}
+					for _, v := range cannotCommit[idx].Txn {
+						if _, ok := m[util.UnsafeBytesToString(v)]; !ok {
+							needRemoved = append(needRemoved, v)
+						}
+					}
+				}
 			}
 		}
 	}
