8bf5e977d9383f090e6936e3f94e5aad6d6599a4
Jensen <jensenojs@qq.com>
2024-04-25 08:35:38 +0800

    Revert "[Fix] : replace point locks with table lock in some cases (#15578)"
    This reverts commit 57def8e01aad11c56b91943ea8a2210a4ff67da9.

--- pkg/config/configuration.go
+++ pkg/config/configuration.go
@@ -165,7 +165,8 @@ var (
 	// default lower_case_table_names
 	defaultLowerCaseTableNames = "1"
 
-	CNPrimaryCheck = false
+	// default sql_mode
+	dafaultSqlMode = "ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION,NO_ZERO_DATE,NO_ZERO_IN_DATE,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES"
 )
 
 // FrontendParameters of the frontend
