079432cf8146553592cc7b1e1efe07e431dbfbb6
badboynt1 <badboynt@126.com>
2024-04-26 11:27:37 +0800

    fix

--- pkg/sql/plan/stats.go
+++ pkg/sql/plan/stats.go
@@ -1022,7 +1022,7 @@ func calcScanStats(node *plan.Node, builder *QueryBuilder) *plan.Stats {
 	for i := range node.FilterList {
 		node.FilterList[i].Selectivity = estimateExprSelectivity(node.FilterList[i], builder)
 		currentBlockSel := estimateFilterBlockSelectivity(builder.GetContext(), node.FilterList[i], node.TableDef, s)
-		if currentBlockSel < 1 {
+		if currentBlockSel < 1 || strings.HasPrefix(node.TableDef.Name, catalog.IndexTableNamePrefix) {
 			copyOfExpr := DeepCopyExpr(node.FilterList[i])
 			copyOfExpr.Selectivity = currentBlockSel
 			blockExprList = append(blockExprList, copyOfExpr)
