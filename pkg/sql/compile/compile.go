b7be423ee6acaeedf037849804d186aaa4bc917a
noorall <863485501@qq.com>
2024-04-28 17:44:09 +0800

    [fix] error caused by parallel slicing in load data

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -1967,10 +1967,11 @@ func (c *Compile) compileExternScan(ctx context.Context, n *plan.Node) ([]*Scope
 
 	t = time.Now()
 	var fileOffset [][]int64
+	cols := n.TableDef.Cols
 	for i := 0; i < len(fileList); i++ {
 		param.Filepath = fileList[i]
 		if param.Parallel {
-			arr, err := external.ReadFileOffset(param, mcpu, fileSize[i])
+			arr, err := external.ReadFileOffset(param, mcpu, fileSize[i], cols)
 			fileOffset = append(fileOffset, arr)
 			if err != nil {
 				return nil, err
