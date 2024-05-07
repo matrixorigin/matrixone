fc48df699bdc03ff3a220dba2566c54776d6f5bb
YANGGMM <18916107305@163.com>
2024-05-07 14:06:10 +0800

    fix

--- pkg/frontend/show_subscriptions_test.go
+++ pkg/frontend/show_subscriptions_test.go
@@ -14,161 +14,149 @@
 
 package frontend
 
-import (
-	"context"
-	"strings"
+// type mockedBackgroundHandler struct {
+// 	currentSql            string
+// 	accountIdNamesBatches []*batch.Batch
+// 	pubBatches            map[int32][]*batch.Batch
+// 	subBatches            []*batch.Batch
+// 	accountId             int32
+// 	sql2result            map[string]ExecResult
+// }
 
-	"github.com/matrixorigin/matrixone/pkg/common/mpool"
-	"github.com/matrixorigin/matrixone/pkg/container/batch"
-	"github.com/matrixorigin/matrixone/pkg/container/types"
-	"github.com/matrixorigin/matrixone/pkg/container/vector"
-	"github.com/matrixorigin/matrixone/pkg/defines"
-	"github.com/matrixorigin/matrixone/pkg/sql/parsers/tree"
-)
+// func (m *mockedBackgroundHandler) Close() {}
 
-type mockedBackgroundHandler struct {
-	currentSql            string
-	accountIdNamesBatches []*batch.Batch
-	pubBatches            map[int32][]*batch.Batch
-	subBatches            []*batch.Batch
-	accountId             int32
-	sql2result            map[string]ExecResult
-}
+// func (m *mockedBackgroundHandler) Exec(ctx context.Context, s string) error {
+// 	m.currentSql = s
+// 	m.accountId = int32(ctx.Value(defines.TenantIDKey{}).(uint32))
+// 	return nil
+// }
 
-func (m *mockedBackgroundHandler) Close() {}
+// func (m *mockedBackgroundHandler) ExecStmt(ctx context.Context, statement tree.Statement) error {
+// 	return nil
+// }
 
-func (m *mockedBackgroundHandler) Exec(ctx context.Context, s string) error {
-	m.currentSql = s
-	m.accountId = int32(ctx.Value(defines.TenantIDKey{}).(uint32))
-	return nil
-}
+// func (m *mockedBackgroundHandler) GetExecResultSet() []interface{} {
+// 	return nil
+// }
 
-func (m *mockedBackgroundHandler) ExecStmt(ctx context.Context, statement tree.Statement) error {
-	return nil
-}
+// func (m *mockedBackgroundHandler) ClearExecResultSet() {}
 
-func (m *mockedBackgroundHandler) GetExecResultSet() []interface{} {
-	return nil
-}
+// func (m *mockedBackgroundHandler) GetExecResultBatches() []*batch.Batch {
+// 	if m.currentSql == getAccountIdNamesSql {
+// 		return m.accountIdNamesBatches
+// 	} else if strings.HasPrefix(m.currentSql, getPubsSql) {
+// 		return m.pubBatches[m.accountId]
+// 	} else {
+// 		return m.subBatches
+// 	}
+// }
 
-func (m *mockedBackgroundHandler) ClearExecResultSet() {}
+// func (m *mockedBackgroundHandler) ClearExecResultBatches() {}
 
-func (m *mockedBackgroundHandler) GetExecResultBatches() []*batch.Batch {
-	if m.currentSql == getAccountIdNamesSql {
-		return m.accountIdNamesBatches
-	} else if strings.HasPrefix(m.currentSql, getPubsSql) {
-		return m.pubBatches[m.accountId]
-	} else {
-		return m.subBatches
-	}
-}
+// func (m *mockedBackgroundHandler) init(mp *mpool.MPool) {
+// 	m.pubBatches = make(map[int32][]*batch.Batch)
+// 	m.sql2result = make(map[string]ExecResult)
 
-func (m *mockedBackgroundHandler) ClearExecResultBatches() {}
+// 	var b *batch.Batch
+// 	v1 := vector.NewVec(types.T_int32.ToType())
+// 	_ = vector.AppendFixedList(v1, []int32{0, 1}, nil, mp)
+// 	v2 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v2, []string{"sys", "account1"}, nil, mp)
+// 	b = &batch.Batch{Vecs: []*vector.Vector{v1, v2}}
+// 	b.SetRowCount(2)
+// 	m.accountIdNamesBatches = []*batch.Batch{b}
 
-func (m *mockedBackgroundHandler) init(mp *mpool.MPool) {
-	m.pubBatches = make(map[int32][]*batch.Batch)
-	m.sql2result = make(map[string]ExecResult)
+// 	v3 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v3, []string{"pub1", "pub2"}, nil, mp)
+// 	v4 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v4, []string{"db1", "db2"}, nil, mp)
+// 	v12 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v12, []string{"all", "all"}, nil, mp)
+// 	v5 := vector.NewVec(types.T_timestamp.ToType())
+// 	_ = vector.AppendFixedList(v5, []types.Timestamp{1234567, 2234567}, nil, mp)
+// 	b = &batch.Batch{Vecs: []*vector.Vector{v3, v4, v12, v5}}
+// 	b.SetRowCount(2)
+// 	m.pubBatches[0] = []*batch.Batch{b}
 
-	var b *batch.Batch
-	v1 := vector.NewVec(types.T_int32.ToType())
-	_ = vector.AppendFixedList(v1, []int32{0, 1}, nil, mp)
-	v2 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v2, []string{"sys", "account1"}, nil, mp)
-	b = &batch.Batch{Vecs: []*vector.Vector{v1, v2}}
-	b.SetRowCount(2)
-	m.accountIdNamesBatches = []*batch.Batch{b}
+// 	v6 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v6, []string{"pub3", "pub4"}, nil, mp)
+// 	v7 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v7, []string{"db3", "db4"}, nil, mp)
+// 	v13 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v13, []string{"account2", "all"}, nil, mp)
+// 	v8 := vector.NewVec(types.T_timestamp.ToType())
+// 	_ = vector.AppendFixedList(v8, []types.Timestamp{3234567, 4234567}, nil, mp)
+// 	b = &batch.Batch{Vecs: []*vector.Vector{v6, v7, v13, v8}}
+// 	b.SetRowCount(2)
+// 	m.pubBatches[1] = []*batch.Batch{b}
 
-	v3 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v3, []string{"pub1", "pub2"}, nil, mp)
-	v4 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v4, []string{"db1", "db2"}, nil, mp)
-	v12 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v12, []string{"all", "all"}, nil, mp)
-	v5 := vector.NewVec(types.T_timestamp.ToType())
-	_ = vector.AppendFixedList(v5, []types.Timestamp{1234567, 2234567}, nil, mp)
-	b = &batch.Batch{Vecs: []*vector.Vector{v3, v4, v12, v5}}
-	b.SetRowCount(2)
-	m.pubBatches[0] = []*batch.Batch{b}
+// 	v9 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v9, []string{"sub1", "sub4"}, nil, mp)
+// 	v10 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v10, []string{
+// 		"create database sub1 from sys publication pub1",
+// 		"create database sub4 from account1 publication pub4",
+// 	}, nil, mp)
+// 	v11 := vector.NewVec(types.T_timestamp.ToType())
+// 	_ = vector.AppendFixedList(v11, []types.Timestamp{1234568, 4234568}, nil, mp)
+// 	b = &batch.Batch{Vecs: []*vector.Vector{v9, v10, v11}}
+// 	b.SetRowCount(2)
+// 	m.subBatches = []*batch.Batch{b}
+// }
 
-	v6 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v6, []string{"pub3", "pub4"}, nil, mp)
-	v7 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v7, []string{"db3", "db4"}, nil, mp)
-	v13 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v13, []string{"account2", "all"}, nil, mp)
-	v8 := vector.NewVec(types.T_timestamp.ToType())
-	_ = vector.AppendFixedList(v8, []types.Timestamp{3234567, 4234567}, nil, mp)
-	b = &batch.Batch{Vecs: []*vector.Vector{v6, v7, v13, v8}}
-	b.SetRowCount(2)
-	m.pubBatches[1] = []*batch.Batch{b}
+// func (m *mockedBackgroundHandler) initLike(mp *mpool.MPool) {
+// 	m.pubBatches = make(map[int32][]*batch.Batch)
 
-	v9 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v9, []string{"sub1", "sub4"}, nil, mp)
-	v10 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v10, []string{
-		"create database sub1 from sys publication pub1",
-		"create database sub4 from account1 publication pub4",
-	}, nil, mp)
-	v11 := vector.NewVec(types.T_timestamp.ToType())
-	_ = vector.AppendFixedList(v11, []types.Timestamp{1234568, 4234568}, nil, mp)
-	b = &batch.Batch{Vecs: []*vector.Vector{v9, v10, v11}}
-	b.SetRowCount(2)
-	m.subBatches = []*batch.Batch{b}
-}
+// 	var b *batch.Batch
+// 	v1 := vector.NewVec(types.T_int32.ToType())
+// 	_ = vector.AppendFixedList(v1, []int32{0, 1}, nil, mp)
+// 	v2 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v2, []string{"sys", "account1"}, nil, mp)
+// 	b = &batch.Batch{Vecs: []*vector.Vector{v1, v2}}
+// 	b.SetRowCount(2)
+// 	m.accountIdNamesBatches = []*batch.Batch{b}
 
-func (m *mockedBackgroundHandler) initLike(mp *mpool.MPool) {
-	m.pubBatches = make(map[int32][]*batch.Batch)
+// 	v3 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v3, []string{"pub1"}, nil, mp)
+// 	v4 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v4, []string{"db1"}, nil, mp)
+// 	v12 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v12, []string{"all"}, nil, mp)
+// 	v5 := vector.NewVec(types.T_timestamp.ToType())
+// 	_ = vector.AppendFixedList(v5, []types.Timestamp{1234567}, nil, mp)
+// 	b = &batch.Batch{Vecs: []*vector.Vector{v3, v4, v12, v5}}
+// 	b.SetRowCount(1)
+// 	m.pubBatches[0] = []*batch.Batch{b}
 
-	var b *batch.Batch
-	v1 := vector.NewVec(types.T_int32.ToType())
-	_ = vector.AppendFixedList(v1, []int32{0, 1}, nil, mp)
-	v2 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v2, []string{"sys", "account1"}, nil, mp)
-	b = &batch.Batch{Vecs: []*vector.Vector{v1, v2}}
-	b.SetRowCount(2)
-	m.accountIdNamesBatches = []*batch.Batch{b}
+// 	v6 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v6, []string{}, nil, mp)
+// 	v7 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v7, []string{}, nil, mp)
+// 	v13 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v13, []string{}, nil, mp)
+// 	v8 := vector.NewVec(types.T_timestamp.ToType())
+// 	_ = vector.AppendFixedList(v8, []types.Timestamp{}, nil, mp)
+// 	b = &batch.Batch{Vecs: []*vector.Vector{v6, v7, v13, v8}}
+// 	b.SetRowCount(0)
+// 	m.pubBatches[1] = []*batch.Batch{b}
 
-	v3 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v3, []string{"pub1"}, nil, mp)
-	v4 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v4, []string{"db1"}, nil, mp)
-	v12 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v12, []string{"all"}, nil, mp)
-	v5 := vector.NewVec(types.T_timestamp.ToType())
-	_ = vector.AppendFixedList(v5, []types.Timestamp{1234567}, nil, mp)
-	b = &batch.Batch{Vecs: []*vector.Vector{v3, v4, v12, v5}}
-	b.SetRowCount(1)
-	m.pubBatches[0] = []*batch.Batch{b}
+// 	v9 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v9, []string{"sub1", "sub4"}, nil, mp)
+// 	v10 := vector.NewVec(types.T_varchar.ToType())
+// 	_ = vector.AppendStringList(v10, []string{
+// 		"create database sub1 from sys publication pub1",
+// 		"create database sub4 from account1 publication pub4",
+// 	}, nil, mp)
+// 	v11 := vector.NewVec(types.T_timestamp.ToType())
+// 	_ = vector.AppendFixedList(v11, []types.Timestamp{1234568, 4234568}, nil, mp)
+// 	b = &batch.Batch{Vecs: []*vector.Vector{v9, v10, v11}}
+// 	b.SetRowCount(2)
+// 	m.subBatches = []*batch.Batch{b}
+// }
 
-	v6 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v6, []string{}, nil, mp)
-	v7 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v7, []string{}, nil, mp)
-	v13 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v13, []string{}, nil, mp)
-	v8 := vector.NewVec(types.T_timestamp.ToType())
-	_ = vector.AppendFixedList(v8, []types.Timestamp{}, nil, mp)
-	b = &batch.Batch{Vecs: []*vector.Vector{v6, v7, v13, v8}}
-	b.SetRowCount(0)
-	m.pubBatches[1] = []*batch.Batch{b}
+// func (m *mockedBackgroundHandler) Clear() {}
 
-	v9 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v9, []string{"sub1", "sub4"}, nil, mp)
-	v10 := vector.NewVec(types.T_varchar.ToType())
-	_ = vector.AppendStringList(v10, []string{
-		"create database sub1 from sys publication pub1",
-		"create database sub4 from account1 publication pub4",
-	}, nil, mp)
-	v11 := vector.NewVec(types.T_timestamp.ToType())
-	_ = vector.AppendFixedList(v11, []types.Timestamp{1234568, 4234568}, nil, mp)
-	b = &batch.Batch{Vecs: []*vector.Vector{v9, v10, v11}}
-	b.SetRowCount(2)
-	m.subBatches = []*batch.Batch{b}
-}
-
-func (m *mockedBackgroundHandler) Clear() {}
-
-var _ BackgroundExec = &mockedBackgroundHandler{}
+// var _ BackgroundExec = &mockedBackgroundHandler{}
 
 // var bh = &mockedBackgroundHandler{}
 
