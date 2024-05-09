74926e64d673eb5d0c5c3ce178d048e2b8b575b1
triump2020 <triump2020@outlook.com>
2024-05-09 21:03:30 +0800

    add log

--- pkg/vm/engine/disttae/db.go
+++ pkg/vm/engine/disttae/db.go
@@ -410,8 +410,8 @@ func (e *Engine) getOrCreateSnapPart(
 	}
 	if !ok {
 		logutil.Infof("xxxx getOrCreateSnapPart, "+
-			"dbName:%s, tbName:%s, dbid:%d, tid:%d, ts:%s, partition is null",
-			dbName, tblName, databaseId, tableId, ts.ToTimestamp().DebugString())
+			"dbName:%s, tbName:%s, dbid:%d, tid:%d, ts:%s, partition is %p",
+			dbName, tblName, databaseId, tableId, ts.ToTimestamp().DebugString(), partition)
 	} else {
 		pstart, pend := partition.GetDuration()
 		logutil.Infof("xxxx getOrCreateSnapPart, "+
@@ -513,6 +513,9 @@ func (e *Engine) getOrCreateLatestPart(databaseId, tableId uint64) *logtailrepla
 }
 
 func (e *Engine) lazyLoadLatestCkp(ctx context.Context, tbl *txnTable) (*logtailreplay.Partition, error) {
+	logutil.Infof("xxxx lazyLoadLatestCkp, txn:%s, dbName:%s, tbName:%s, dbid:%d, tid:%d",
+		tbl.db.op.Txn().DebugString(),
+		tbl.db.databaseName, tbl.tableName, tbl.db.databaseId, tbl.tableId)
 	part := e.getOrCreateLatestPart(tbl.db.databaseId, tbl.tableId)
 	cache := e.getLatestCatalogCache()
 
