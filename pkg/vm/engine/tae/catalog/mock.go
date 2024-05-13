4e8496144a0abc3ec3ec0dd718c1ce32a58eae38
aptend <aptend@hotmail.com>
2024-05-13 18:09:21 +0800

    refactor

--- pkg/vm/engine/tae/catalog/mock.go
+++ pkg/vm/engine/tae/catalog/mock.go
@@ -15,6 +15,7 @@
 package catalog
 
 import (
+	"context"
 	"sync"
 
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
@@ -229,7 +230,7 @@ func (txn *mockTxn) CreateDatabase(name, createSql, datTyp string) (handle.Datab
 	return h, nil
 }
 
-func (txn *mockTxn) CreateDatabaseWithID(name, createSql, datTyp string, id uint64) (handle.Database, error) {
+func (txn *mockTxn) CreateDatabaseWithID(ctx context.Context, name, createSql, datTyp string, id uint64) (handle.Database, error) {
 	entry, err := txn.catalog.CreateDBEntryWithID(name, createSql, datTyp, id, txn)
 	if err != nil {
 		return nil, err
