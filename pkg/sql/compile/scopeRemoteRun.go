0aad025f70c288e936394d2dcd4f3b178a72a849
badboynt1 <badboynt@126.com>
2024-05-14 16:22:09 +0800

    fix

--- pkg/sql/compile/scopeRemoteRun.go
+++ pkg/sql/compile/scopeRemoteRun.go
@@ -725,8 +725,6 @@ func convertToPipelineInstruction(opr *vm.Instruction, ctx *scopeContext, ctxId 
 			AffectedRows: t.AffectedRows(),
 			RemoteDelete: t.RemoteDelete,
 			SegmentMap:   t.SegmentMap,
-			IBucket:      t.IBucket,
-			NBucket:      t.Nbucket,
 			// deleteCtx
 			RowIdIdx:              int32(t.DeleteCtx.RowIdIdx),
 			PartitionTableIds:     t.DeleteCtx.PartitionTableIDs,
@@ -1087,8 +1085,6 @@ func convertToVmInstruction(opr *pipeline.Instruction, ctx *scopeContext, eng en
 		arg := deletion.NewArgument()
 		arg.RemoteDelete = t.RemoteDelete
 		arg.SegmentMap = t.SegmentMap
-		arg.IBucket = t.IBucket
-		arg.Nbucket = t.NBucket
 		arg.DeleteCtx = &deletion.DeleteCtx{
 			CanTruncate:           t.CanTruncate,
 			RowIdIdx:              int(t.RowIdIdx),
