e73837fa3e878b4b5b93aea301ed7901f64b29c8
Kai Cao <ck89119@users.noreply.github.com>
2024-05-14 18:17:38 +0800

    [Feature] support view on snapshot (#16073)
    - support snapshot read of view
    - collect dependent views when build create-view sql
    - restore views in order of dependency graph topological sorting
    
    Approved by: @daviszhen, @aunjgr, @aressu1985, @reusee, @iamlinjunhong, @sukki37

--- pkg/frontend/types.go
+++ pkg/frontend/types.go
@@ -434,6 +434,8 @@ func (ses *feSessionImpl) Close() {
 	}
 	if ses.txnCompileCtx != nil {
 		ses.txnCompileCtx.execCtx = nil
+		ses.txnCompileCtx.snapshot = nil
+		ses.txnCompileCtx.views = nil
 		ses.txnCompileCtx = nil
 	}
 	ses.sql = ""
