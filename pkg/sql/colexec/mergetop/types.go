b8a7733fb62f1683e006458012824405c65a7db2
ouyuanning <52741323@qq.com>
2024-05-16 08:56:44 +0800

    add reset interface

--- pkg/sql/colexec/mergetop/types.go
+++ pkg/sql/colexec/mergetop/types.go
@@ -92,6 +92,10 @@ func (arg *Argument) Release() {
 	}
 }
 
+func (arg *Argument) Reset(proc *process.Process, pipelineFailed bool, err error) {
+	arg.Free(proc, pipelineFailed, err)
+}
+
 func (arg *Argument) Free(proc *process.Process, pipelineFailed bool, err error) {
 	ctr := arg.ctr
 	if ctr != nil {
