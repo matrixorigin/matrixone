8c28223466ef5cc5119e62bf04c88307b5ae3e05
badboynt1 <badboynt@126.com>
2024-05-17 09:24:39 +0800

    Revert "Revert "[Bug]: pipeline client max connections is too large (#15713)" (#16045)"
    This reverts commit 591e5e5acf7dacdec11d89dac7bd4fa19255ed39.

--- pkg/sql/compile/scopeRemoteRunTypes.go
+++ pkg/sql/compile/scopeRemoteRunTypes.go
@@ -256,7 +256,7 @@ func (sender *messageSenderOnClient) close() {
 		sender.ctxCancel()
 	}
 	// XXX not a good way to deal it if close failed.
-	_ = sender.streamSender.Close(true)
+	_ = sender.streamSender.Close(false)
 }
 
 // messageReceiverOnServer is a structure
