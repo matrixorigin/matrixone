d10fe2cb970c606e28f4b969b461b9e58416d1b7
davis zhen <daviszhen007@gmail.com>
2024-05-18 14:44:46 +0800

    add debug info & profile interval (#16222)
    增加事务泄露调试信息。
    
    1,增加fe的footprint。
    
    每经过fe的一个函数。
    
    进入时，enterFPrint。增加enter计数器。
    退出时，exitFPrint。增加exit计数器。
    
    2，在fe的主流程和各个入口，设置了footprint
    
    方便在事务泄露时，打印出来sql经过的路径。
    
    3，在调用计算引擎执行前一刻。
    
    将fe的footprints 记录到txnOperator里面。
    
    4,txnOperator 增加设置footprints的接口。
    
    `	SetFootPrints(prints [][2]uint32)`
    
    5,在事务检测到异常行为时打印出fe经历过的footprints
    
    案例：
    ```
    {"level":"INFO","time":"2024/05/17 18:53:27.576243 +0800","caller":"blockio/pipeline.go:496","msg":"SelectivityStats: BLK[0/0=0.0000] COL[174/200=0.8700] RDF[2/2=1.0000,36/60=0.6000]RDD[0s/0s/0s/0]"}
    2024/05/17 18:53:27.598920 +0800 ERROR cn-service found leak txn {"uuid": "", "txn-id": "018f862ed5d07862b65e9740b32c18bf", "create-at": "2024/05/17 18:52:47.440563 +0800", "options": "Features:1 CN:\"7c4dccb4-4d3c-41f8-b482-5251dc7a41bf\" SessionID:\"018f862e-c56e-7f09-9e7f-3a03ac160a68\" ConnectionID:9566 UserName:\"dump\" counter:\"commit: enter:0, exit:0 rollback: enter:0, exit:0 runSql: enter:0, exit:0 footPrints: [0: enter:1 exit:0] [1: enter:1 exit:0] [2: enter:1 exit:0] [3: enter:1 exit:0] [4: enter:1 exit:0] [5: enter:1 exit:0] [77: enter:1 exit:1] \" sessionInfo:\"connectionId 9566|127.0.0.1:39526|{account sys:dump:moadmin -- 0:1:0}|goRoutineId 6757|018f862e-c56e-7f09-9e7f-3a03ac160a68\" byBegin:true autocommit:true ", "profile": "ETL:/profile/routine_018f862f-72ae-7bef-9348-01cbf638a594"}
    {"level":"INFO","time":"2024/05/17 18:53:27.647454 +0800","name":"log-service","caller":"taskservice/task_service_cron.go:44","msg":"cron task scheduler started or is stopping","uuid":"7c4dccb4-4d3c-41f8-b482-5251dc7a41bf"}
    
    {"level":"INFO","time":"2024/05/17 18:47:43.143714 +0800","caller":"blockio/pipeline.go:496","msg":"SelectivityStats: BLK[0/0=0.0000] COL[216/249=0.8675] RDF[2/2=1.0000,152/198=0.7677]RDD[0s/0s/0s/0]"}
    2024/05/17 18:47:43.228472 +0800 ERROR cn-service found leak txn {"uuid": "", "txn-id": "018f86282dba7b998e0c968cc8e2b98b", "create-at": "2024/05/17 18:45:31.194768 +0800", "options": "Features:1 CN:\"7c4dccb4-4d3c-41f8-b482-5251dc7a41bf\" SessionID:\"018f8622-502a-72c7-b64c-5ec24cda80db\" ConnectionID:9196 UserName:\"dump\" counter:\"commit: enter:0, exit:0 rollback: enter:0, exit:0 runSql: enter:1, exit:1 footPrints: [0: enter:1 exit:0] [1: enter:1 exit:0] [2: enter:1 exit:0] [3: enter:1 exit:0] [4: enter:1 exit:0] [5: enter:1 exit:0] [6: enter:1 exit:0] [7: enter:1 exit:1] [8: enter:1 exit:1] [62: enter:1 exit:1] [63: enter:1 exit:1] [64: enter:1 exit:1] [65: enter:1 exit:1] [77: enter:1 exit:1] [99: enter:1 exit:1] \" sessionInfo:\"connectionId 9196|127.0.0.1:34292|{account sys:dump:moadmin -- 0:1:0}|goRoutineId 8939|018f8622-502a-72c7-b64c-5ec24cda80db\" ", "profile": "ETL:/profile/routine_018f862a-317c-7613-89ff-226aaa3e738f"}
    
    
    ```
    
    
    定时profile
    
    mo-service启动时增加参数。 -profile-interval "10s"
    会在mo-data/etl/profile中每隔10s保存heap和goroutine。
    
    例如：
    
    ```
    ./mo-service -profile-interval "10s" -debug-http 127.0.0.1:6060 -launch etc/launch/launch.toml > log.txt
    ```
    
    启动成功后会有下面的效果
    
    ```
    xxxx@xxxx:~/Documents/temp/matrixone-temp/mo-data/etl/profile$ ls
    goroutine_018f8673-5a12-7b89-abcb-c940f7211201  goroutine_018f8673-cf43-7935-b749-415362b71e55  heap_018f8673-5a07-7ea4-a487-7eb235a1cba9  heap_018f8673-cf37-75de-872b-5613a8af0495
    goroutine_018f8673-8125-71b2-871f-fffe37abd752  goroutine_018f8673-f656-7ae5-b7bf-798036aac749  heap_018f8673-8118-70e0-bbf2-ddca5cf820f8  heap_018f8673-f647-7e2c-9608-55e33429153d
    goroutine_018f8673-a834-7eea-893c-5b5aedc13282  goroutine_018f8674-1d64-7d7b-b2b0-f7d37f9888f4  heap_018f8673-a827-754d-bcae-32cfbe9fa68a  heap_018f8674-1d58-72d1-ae78-43e84a0043f6
    
    ```
    
    Approved by: @reusee, @zhangxu19830126, @qingxinhome, @XuPeng-SH

--- pkg/frontend/self_handle.go
+++ pkg/frontend/self_handle.go
@@ -19,14 +19,19 @@ import (
 )
 
 func execInFrontend(ses *Session, execCtx *ExecCtx) (err error) {
+	ses.EnterFPrint(9)
+	defer ses.ExitFPrint(9)
 	//check transaction states
 	switch st := execCtx.stmt.(type) {
 	case *tree.BeginTransaction:
+		ses.EnterFPrint(10)
+		defer ses.ExitFPrint(10)
 		RecordStatementTxnID(execCtx.reqCtx, ses)
 	case *tree.CommitTransaction:
 	case *tree.RollbackTransaction:
 	case *tree.SetRole:
-
+		ses.EnterFPrint(11)
+		defer ses.ExitFPrint(11)
 		ses.InvalidatePrivilegeCache()
 		//switch role
 		err = handleSwitchRole(ses, execCtx, st)
@@ -34,7 +39,8 @@ func execInFrontend(ses *Session, execCtx *ExecCtx) (err error) {
 			return
 		}
 	case *tree.Use:
-
+		ses.EnterFPrint(12)
+		defer ses.ExitFPrint(12)
 		var v interface{}
 		v, err = ses.GetGlobalVar(execCtx.reqCtx, "lower_case_table_names")
 		if err != nil {
@@ -58,7 +64,8 @@ func execInFrontend(ses *Session, execCtx *ExecCtx) (err error) {
 			return
 		}
 	case *tree.PrepareStmt:
-
+		ses.EnterFPrint(13)
+		defer ses.ExitFPrint(13)
 		execCtx.prepareStmt, err = handlePrepareStmt(ses, execCtx, st)
 		if err != nil {
 			return
@@ -69,6 +76,8 @@ func execInFrontend(ses *Session, execCtx *ExecCtx) (err error) {
 			return
 		}
 	case *tree.PrepareString:
+		ses.EnterFPrint(14)
+		defer ses.ExitFPrint(14)
 		execCtx.prepareStmt, err = handlePrepareString(ses, execCtx, st)
 		if err != nil {
 			return
@@ -79,141 +88,166 @@ func execInFrontend(ses *Session, execCtx *ExecCtx) (err error) {
 			return
 		}
 	case *tree.CreateConnector:
-
+		ses.EnterFPrint(15)
+		defer ses.ExitFPrint(15)
 		err = handleCreateConnector(execCtx.reqCtx, ses, st)
 		if err != nil {
 			return
 		}
 	case *tree.PauseDaemonTask:
-
+		ses.EnterFPrint(16)
+		defer ses.ExitFPrint(16)
 		err = handlePauseDaemonTask(execCtx.reqCtx, ses, st)
 		if err != nil {
 			return
 		}
 	case *tree.CancelDaemonTask:
-
+		ses.EnterFPrint(17)
+		defer ses.ExitFPrint(17)
 		err = handleCancelDaemonTask(execCtx.reqCtx, ses, st.TaskID)
 		if err != nil {
 			return
 		}
 	case *tree.ResumeDaemonTask:
-
+		ses.EnterFPrint(18)
+		defer ses.ExitFPrint(18)
 		err = handleResumeDaemonTask(execCtx.reqCtx, ses, st)
 		if err != nil {
 			return
 		}
 	case *tree.DropConnector:
-
+		ses.EnterFPrint(19)
+		defer ses.ExitFPrint(19)
 		err = handleDropConnector(execCtx.reqCtx, ses, st)
 		if err != nil {
 			return
 		}
 	case *tree.ShowConnectors:
-
+		ses.EnterFPrint(20)
+		defer ses.ExitFPrint(20)
 		if err = handleShowConnectors(execCtx.reqCtx, ses); err != nil {
 			return
 		}
 	case *tree.Deallocate:
-
+		ses.EnterFPrint(21)
+		defer ses.ExitFPrint(21)
 		err = handleDeallocate(ses, execCtx, st)
 		if err != nil {
 			return
 		}
 	case *tree.Reset:
-
+		ses.EnterFPrint(22)
+		defer ses.ExitFPrint(22)
 		err = handleReset(ses, execCtx, st)
 		if err != nil {
 			return
 		}
 	case *tree.SetVar:
-
+		ses.EnterFPrint(23)
+		defer ses.ExitFPrint(23)
 		err = handleSetVar(ses, execCtx, st, execCtx.sqlOfStmt)
 		if err != nil {
 			return
 		}
 	case *tree.ShowVariables:
-
+		ses.EnterFPrint(24)
+		defer ses.ExitFPrint(24)
 		err = handleShowVariables(ses, execCtx, st)
 		if err != nil {
 			return
 		}
 	case *tree.ShowErrors, *tree.ShowWarnings:
-
+		ses.EnterFPrint(25)
+		defer ses.ExitFPrint(25)
 		err = handleShowErrors(ses)
 		if err != nil {
 			return
 		}
 	case *tree.AnalyzeStmt:
-
+		ses.EnterFPrint(26)
+		defer ses.ExitFPrint(26)
 		if err = handleAnalyzeStmt(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.ExplainStmt:
-
+		ses.EnterFPrint(27)
+		defer ses.ExitFPrint(27)
 		if err = handleExplainStmt(ses, execCtx, st); err != nil {
 			return
 		}
 	case *InternalCmdFieldList:
-
+		ses.EnterFPrint(28)
+		defer ses.ExitFPrint(28)
 		if err = handleCmdFieldList(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.CreatePublication:
-
+		ses.EnterFPrint(29)
+		defer ses.ExitFPrint(29)
 		if err = handleCreatePublication(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.AlterPublication:
-
+		ses.EnterFPrint(30)
+		defer ses.ExitFPrint(30)
 		if err = handleAlterPublication(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.DropPublication:
-
+		ses.EnterFPrint(31)
+		defer ses.ExitFPrint(31)
 		if err = handleDropPublication(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.ShowSubscriptions:
-
+		ses.EnterFPrint(32)
+		defer ses.ExitFPrint(32)
 		if err = handleShowSubscriptions(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.CreateStage:
-
+		ses.EnterFPrint(33)
+		defer ses.ExitFPrint(33)
 		if err = handleCreateStage(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.DropStage:
-
+		ses.EnterFPrint(34)
+		defer ses.ExitFPrint(34)
 		if err = handleDropStage(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.AlterStage:
-
+		ses.EnterFPrint(35)
+		defer ses.ExitFPrint(35)
 		if err = handleAlterStage(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.CreateAccount:
-
+		ses.EnterFPrint(36)
+		defer ses.ExitFPrint(36)
 		ses.InvalidatePrivilegeCache()
 		if err = handleCreateAccount(ses, execCtx, st, execCtx.proc); err != nil {
 			return
 		}
 	case *tree.DropAccount:
-
+		ses.EnterFPrint(37)
+		defer ses.ExitFPrint(37)
 		ses.InvalidatePrivilegeCache()
 		if err = handleDropAccount(ses, execCtx, st, execCtx.proc); err != nil {
 			return
 		}
 	case *tree.AlterAccount:
 		ses.InvalidatePrivilegeCache()
-
+		ses.EnterFPrint(38)
+		defer ses.ExitFPrint(38)
 		if err = handleAlterAccount(ses, execCtx, st, execCtx.proc); err != nil {
 			return
 		}
 	case *tree.AlterDataBaseConfig:
 		ses.InvalidatePrivilegeCache()
-
+		ses.EnterFPrint(39)
+		defer ses.ExitFPrint(39)
 		if st.IsAccountLevel {
 			if err = handleAlterAccountConfig(ses, execCtx, st); err != nil {
 				return
@@ -224,37 +258,43 @@ func execInFrontend(ses *Session, execCtx *ExecCtx) (err error) {
 			}
 		}
 	case *tree.CreateUser:
-
+		ses.EnterFPrint(40)
+		defer ses.ExitFPrint(40)
 		ses.InvalidatePrivilegeCache()
 		if err = handleCreateUser(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.DropUser:
-
+		ses.EnterFPrint(41)
+		defer ses.ExitFPrint(41)
 		ses.InvalidatePrivilegeCache()
 		if err = handleDropUser(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.AlterUser: //TODO
-
+		ses.EnterFPrint(42)
+		defer ses.ExitFPrint(42)
 		ses.InvalidatePrivilegeCache()
 		if err = handleAlterUser(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.CreateRole:
-
+		ses.EnterFPrint(43)
+		defer ses.ExitFPrint(43)
 		ses.InvalidatePrivilegeCache()
 		if err = handleCreateRole(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.DropRole:
-
+		ses.EnterFPrint(44)
+		defer ses.ExitFPrint(44)
 		ses.InvalidatePrivilegeCache()
 		if err = handleDropRole(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.CreateFunction:
-
+		ses.EnterFPrint(45)
+		defer ses.ExitFPrint(45)
 		if err = st.Valid(); err != nil {
 			return err
 		}
@@ -262,27 +302,32 @@ func execInFrontend(ses *Session, execCtx *ExecCtx) (err error) {
 			return
 		}
 	case *tree.DropFunction:
-
+		ses.EnterFPrint(46)
+		defer ses.ExitFPrint(46)
 		if err = handleDropFunction(ses, execCtx, st, execCtx.proc); err != nil {
 			return
 		}
 	case *tree.CreateProcedure:
-
+		ses.EnterFPrint(47)
+		defer ses.ExitFPrint(47)
 		if err = handleCreateProcedure(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.DropProcedure:
-
+		ses.EnterFPrint(48)
+		defer ses.ExitFPrint(48)
 		if err = handleDropProcedure(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.CallStmt:
-
+		ses.EnterFPrint(49)
+		defer ses.ExitFPrint(49)
 		if err = handleCallProcedure(ses, execCtx, st, execCtx.proc); err != nil {
 			return
 		}
 	case *tree.Grant:
-
+		ses.EnterFPrint(50)
+		defer ses.ExitFPrint(50)
 		ses.InvalidatePrivilegeCache()
 		switch st.Typ {
 		case tree.GrantTypeRole:
@@ -295,7 +340,8 @@ func execInFrontend(ses *Session, execCtx *ExecCtx) (err error) {
 			}
 		}
 	case *tree.Revoke:
-
+		ses.EnterFPrint(51)
+		defer ses.ExitFPrint(51)
 		ses.InvalidatePrivilegeCache()
 		switch st.Typ {
 		case tree.RevokeTypeRole:
@@ -308,34 +354,41 @@ func execInFrontend(ses *Session, execCtx *ExecCtx) (err error) {
 			}
 		}
 	case *tree.Kill:
-
+		ses.EnterFPrint(52)
+		defer ses.ExitFPrint(52)
 		ses.InvalidatePrivilegeCache()
 		if err = handleKill(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.ShowAccounts:
-
+		ses.EnterFPrint(53)
+		defer ses.ExitFPrint(53)
 		if err = handleShowAccounts(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.ShowCollation:
-
+		ses.EnterFPrint(54)
+		defer ses.ExitFPrint(54)
 		if err = handleShowCollation(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.ShowBackendServers:
+		ses.EnterFPrint(55)
+		defer ses.ExitFPrint(55)
 		if err = handleShowBackendServers(ses, execCtx); err != nil {
 			return
 		}
 	case *tree.SetTransaction:
-
+		ses.EnterFPrint(56)
+		defer ses.ExitFPrint(56)
 		//TODO: handle set transaction
 	case *tree.LockTableStmt:
 
 	case *tree.UnLockTableStmt:
 
 	case *tree.BackupStart:
-
+		ses.EnterFPrint(57)
+		defer ses.ExitFPrint(57)
 		if err = handleStartBackup(ses, execCtx, st); err != nil {
 			return
 		}
@@ -345,21 +398,29 @@ func execInFrontend(ses *Session, execCtx *ExecCtx) (err error) {
 			return
 		}
 	case *tree.CreateSnapShot:
+		ses.EnterFPrint(58)
+		defer ses.ExitFPrint(58)
 		//TODO: invalidate privilege cache
 		if err = handleCreateSnapshot(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.DropSnapShot:
+		ses.EnterFPrint(59)
+		defer ses.ExitFPrint(59)
 		//TODO: invalidate privilege cache
 		if err = handleDropSnapshot(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.RestoreSnapShot:
+		ses.EnterFPrint(60)
+		defer ses.ExitFPrint(60)
 		//TODO: invalidate privilege cache
 		if err = handleRestoreSnapshot(ses, execCtx, st); err != nil {
 			return
 		}
 	case *tree.UpgradeStatement:
+		ses.EnterFPrint(61)
+		defer ses.ExitFPrint(61)
 		//TODO: invalidate privilege cache
 		if err = handleExecUpgrade(ses, execCtx, st); err != nil {
 			return
