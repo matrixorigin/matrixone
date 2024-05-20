3ed11577ad8387e417a76be453e689d52f65981e
qingxinhome <758355272@qq.com>
2024-05-20 17:10:36 +0800

    delete unused code

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -3736,9 +3736,6 @@ func (c *Compile) fillAnalyzeInfo() {
 }
 
 func (c *Compile) determinExpandRanges(n *plan.Node) bool {
-	//if n.TableDef.Partition != nil {
-	//	return true
-	//}
 	if len(n.RuntimeFilterProbeList) == 0 {
 		return true
 	}
