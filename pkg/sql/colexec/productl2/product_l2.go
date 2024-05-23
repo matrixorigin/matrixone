0676b580718a8cc7ea75f10c90c449b6b268ebde
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2024-05-23 12:07:26 -0700

    Use L2DistanceSq instead of L2Distance during IndexScan

--- pkg/sql/colexec/productl2/product_l2.go
+++ pkg/sql/colexec/productl2/product_l2.go
@@ -190,7 +190,7 @@ func (ctr *container) probe(ap *Argument, proc *process.Process, anal process.An
 
 			for i = 0; i < buildCount; i++ {
 				clusterEmbeddingF32 = types.BytesToArray[float32](ctr.bat.Vecs[centroidColPos].GetBytesAt(i))
-				dist, _ := moarray.L2Distance[float32](clusterEmbeddingF32, tblEmbeddingF32)
+				dist, _ := moarray.L2DistanceSq[float32](clusterEmbeddingF32, tblEmbeddingF32)
 				if dist < leastDistance {
 					leastDistance = dist
 					leastClusterIndex = i
@@ -213,7 +213,7 @@ func (ctr *container) probe(ap *Argument, proc *process.Process, anal process.An
 
 			for i = 0; i < buildCount; i++ {
 				clusterEmbeddingF64 = types.BytesToArray[float64](ctr.bat.Vecs[centroidColPos].GetBytesAt(i))
-				dist, _ := moarray.L2Distance[float64](clusterEmbeddingF64, tblEmbeddingF64)
+				dist, _ := moarray.L2DistanceSq[float64](clusterEmbeddingF64, tblEmbeddingF64)
 				if dist < leastDistance {
 					leastDistance = dist
 					leastClusterIndex = i
