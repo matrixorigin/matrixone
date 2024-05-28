98eadeda102b4f407c78332f2cc9bc64895e3e0c
gouhongshen <gouhongshen@hotmail.com>
2024-05-28 21:08:20 +0800

    adding bvt case for fake_pk_col unsorted search

---
+++ test/distributed/cases/disttae_filters/reader_filters/block_reader_filter.sql
@@ -0,0 +1,13 @@
+drop database if exists testdb;
+create database testdb;
+use testdb;
+
+-- test fake pk col linear search filter
+create table t1(a int, b int, index(b));
+insert into t1 select *, * from generate_series(1, 8192000)g;
+select mo_ctl("dn", "flush", "testdb.t1");
+select a from t1 where b = 1;
+select a from t1 where b between 1 and 3;
+select a from t1 where b in (1,2,3);
+drop table t1;
+drop database testdb;
