17dfc88768b77fa2a6e8b57b2b8201ca5dd85f00
gouhongshen <gouhongshen@hotmail.com>
2024-05-28 22:49:50 +0800

    fix bvt

--- test/distributed/cases/disttae_filters/reader_filters/block_reader_filter.sql
+++ test/distributed/cases/disttae_filters/reader_filters/block_reader_filter.sql
@@ -5,6 +5,7 @@ use testdb;
 -- test fake pk col linear search filter
 create table t1(a int, b int, index(b));
 insert into t1 select *, * from generate_series(1, 8192000)g;
+-- @ignore:0
 select mo_ctl("dn", "flush", "testdb.t1");
 select a from t1 where b = 1;
 select a from t1 where b between 1 and 3;
