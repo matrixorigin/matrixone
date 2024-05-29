72699fd7221de27ca6c766734cc304c551f5238f
Jackson <xzxiong@yeah.net>
2024-05-29 20:48:47 +0800

    base: purge_log call objprune if target_date + 24h > now

--- pkg/bootstrap/versions/v1_2_1/cluster_upgrade_list.go
+++ pkg/bootstrap/versions/v1_2_1/cluster_upgrade_list.go
@@ -16,6 +16,7 @@ package v1_2_1
 
 import (
 	"fmt"
+
 	"github.com/matrixorigin/matrixone/pkg/bootstrap/versions"
 	"github.com/matrixorigin/matrixone/pkg/catalog"
 	"github.com/matrixorigin/matrixone/pkg/util/executor"
@@ -23,6 +24,9 @@ import (
 
 var clusterUpgEntries = []versions.UpgradeEntry{
 	upg_system_logInfo,
+	upg_system_rawlog_comment,
+	upg_system_statementInto_comment,
+	upg_systemMetric_metric_comment,
 }
 
 // viewSystemLogInfoDDL113 = "CREATE VIEW IF NOT EXISTS `system`.`log_info` as select `trace_id`, `span_id`, `span_kind`, `node_uuid`, `node_type`, `timestamp`, `logger_name`, `level`, `caller`, `message`, `extra`, `stack` from `system`.`rawlog` where `raw_item` = \"log_info\""
@@ -45,4 +49,65 @@ var upg_system_logInfo = versions.UpgradeEntry{
 		return false, nil
 	},
 	PreSql: fmt.Sprintf("DROP VIEW IF EXISTS %s.%s;", catalog.MO_SYSTEM, "log_info"),
+}
+
+const (
+	systemRawlogComment121        = `read merge data from log, error, span[mo_no_del_hint]`
+	systemStatementInfoComment121 = `record each statement and stats info[mo_no_del_hint]`
+	systemMetricComment121        = `metric data[mo_no_del_hint]`
+)
+
+func getDDLAlterComment(db, tbl, comment string) string {
+	return fmt.Sprintf("ALTER TABLE %s.%s COMMENT %q", db, tbl, comment)
+}
+
+var upg_system_rawlog_comment = versions.UpgradeEntry{
+	Schema:    catalog.MO_SYSTEM,
+	TableName: catalog.MO_RAWLOG,
+	UpgType:   versions.MODIFY_TABLE_COMMENT,
+	UpgSql:    getDDLAlterComment(catalog.MO_SYSTEM, catalog.MO_RAWLOG, systemRawlogComment121),
+	CheckFunc: func(txn executor.TxnExecutor, accountId uint32) (bool, error) {
+		exists, comment, err := versions.CheckTableComment(txn, accountId, catalog.MO_SYSTEM, catalog.MO_RAWLOG)
+		if err != nil {
+			return false, err
+		}
+		if exists && comment == systemRawlogComment121 {
+			return true, nil
+		}
+		return false, nil
+	},
+}
+
+var upg_system_statementInto_comment = versions.UpgradeEntry{
+	Schema:    catalog.MO_SYSTEM,
+	TableName: catalog.MO_STATEMENT,
+	UpgType:   versions.MODIFY_TABLE_COMMENT,
+	UpgSql:    getDDLAlterComment(catalog.MO_SYSTEM, catalog.MO_STATEMENT, systemStatementInfoComment121),
+	CheckFunc: func(txn executor.TxnExecutor, accountId uint32) (bool, error) {
+		exists, comment, err := versions.CheckTableComment(txn, accountId, catalog.MO_SYSTEM, catalog.MO_STATEMENT)
+		if err != nil {
+			return false, err
+		}
+		if exists && comment == systemStatementInfoComment121 {
+			return true, nil
+		}
+		return false, nil
+	},
+}
+
+var upg_systemMetric_metric_comment = versions.UpgradeEntry{
+	Schema:    catalog.MO_SYSTEM_METRICS,
+	TableName: catalog.MO_METRIC,
+	UpgType:   versions.MODIFY_TABLE_COMMENT,
+	UpgSql:    getDDLAlterComment(catalog.MO_SYSTEM_METRICS, catalog.MO_METRIC, systemMetricComment121),
+	CheckFunc: func(txn executor.TxnExecutor, accountId uint32) (bool, error) {
+		exists, comment, err := versions.CheckTableComment(txn, accountId, catalog.MO_SYSTEM_METRICS, catalog.MO_METRIC)
+		if err != nil {
+			return false, err
+		}
+		if exists && comment == systemMetricComment121 {
+			return true, nil
+		}
+		return false, nil
+	},
 }
