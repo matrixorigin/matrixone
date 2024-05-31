ab7e784ba77f7f04a6e0f72c3f8e7ca421b2688a
gouhongshen <gouhongshen@hotmail.com>
2024-05-31 18:04:17 +0800

    sort import

--- pkg/vm/engine/disttae/util.go
+++ pkg/vm/engine/disttae/util.go
@@ -23,6 +23,7 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/catalog"
 	"github.com/matrixorigin/matrixone/pkg/clusterservice"
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
+	"github.com/matrixorigin/matrixone/pkg/common/util"
 	"github.com/matrixorigin/matrixone/pkg/container/batch"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
