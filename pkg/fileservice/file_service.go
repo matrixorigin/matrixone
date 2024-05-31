21ecd841916f15b4ca3e27343ec9695bb1dcc88c
reus <reusee@gmail.com>
2024-05-31 10:38:41 +0800

    fileservice: add IOVector.prepareData

--- pkg/fileservice/file_service.go
+++ pkg/fileservice/file_service.go
@@ -105,7 +105,8 @@ type IOEntry struct {
 
 	// raw content
 	// when reading, if len(Data) < Size, a new Size-lengthed byte slice will be allocated
-	Data []byte
+	Data        []byte
+	releaseData func()
 
 	// when reading, if Writer is not nil, write data to it instead of setting Data field
 	WriterForRead io.Writer
@@ -138,8 +139,6 @@ type IOEntry struct {
 	fromCache IOVectorCache
 
 	allocator CacheDataAllocator
-
-	releaseFuncs []func()
 }
 
 func (i IOEntry) String() string {
