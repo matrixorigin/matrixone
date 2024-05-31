57ac19c9f16e3aa76a47356f64b5948b2be1cd04
reusee <reusee@gmail.com>
2024-05-31 13:45:46 +0800

    fileservice: add IOVector.prepareData (#16532)
    deallocate when error
    
    Approved by: @XuPeng-SH

--- pkg/fileservice/local_fs.go
+++ pkg/fileservice/local_fs.go
@@ -29,7 +29,6 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
-	"unsafe"
 
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 	"github.com/matrixorigin/matrixone/pkg/fileservice/memorycache"
@@ -600,13 +599,8 @@ func (l *LocalFS) read(ctx context.Context, vector *IOVector, bytesCounter *atom
 				entry.Size = int64(len(data))
 
 			} else {
-				if int64(len(entry.Data)) < entry.Size {
-					ptr, dec := getMallocAllocator().Allocate(uint64(entry.Size))
-					entry.Data = unsafe.Slice((*byte)(ptr), entry.Size)
-					entry.releaseFuncs = append(entry.releaseFuncs, func() {
-						dec.Deallocate(ptr)
-					})
-				}
+				finally := entry.prepareData()
+				defer finally(&err)
 				var n int
 				n, err = io.ReadFull(r, entry.Data)
 				if err != nil {
