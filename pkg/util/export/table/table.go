a8bbd052876b8723f35caee7b710b0e218622200
Jackson <xzxiong@yeah.net>
2024-05-31 17:51:12 +0800

    fix ut: external no comment

--- pkg/util/export/table/table.go
+++ pkg/util/export/table/table.go
@@ -446,7 +446,7 @@ func (tbl *Table) ToCreateSql(ctx context.Context, ifNotExists bool) string {
 	}
 	sb.WriteString("\n)")
 
-	if len(tbl.Comment) > 0 {
+	if len(tbl.Comment) > 0 && tbl.Engine != ExternalTableEngine {
 		sb.WriteString(fmt.Sprintf(" COMMENT %q ", tbl.Comment))
 	}
 	// cluster by
