2c2f4a34a4874b86904553be4172116af9e220bf
badboynt1 <badboynt@126.com>
2024-06-04 15:21:24 +0800

    fix

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -1230,7 +1230,10 @@ func (c *Compile) compilePlanScope(ctx context.Context, step int32, curNodeIdx i
 		defer groupInfo.Release()
 		anyDistinctAgg := groupInfo.AnyDistinctAgg()
 
-		if !anyDistinctAgg && n.Stats.HashmapStats != nil && n.Stats.HashmapStats.Shuffle {
+		if c.execType == plan2.ExecTypeTP {
+			ss = c.compileSort(n, c.compileProjection(n, c.compileRestrict(n, c.compileTPGroup(n, ss, ns))))
+			return ss, nil
+		} else if !anyDistinctAgg && n.Stats.HashmapStats != nil && n.Stats.HashmapStats.Shuffle {
 			ss = c.compileSort(n, c.compileShuffleGroup(n, ss, ns))
 			return ss, nil
 		} else {
@@ -3002,6 +3005,17 @@ func (c *Compile) compileSample(n *plan.Node, ss []*Scope) []*Scope {
 	return []*Scope{rs}
 }
 
+func (c *Compile) compileTPGroup(n *plan.Node, ss []*Scope, ns []*plan.Node) []*Scope {
+	ss[0].appendInstruction(vm.Instruction{
+		Op:      vm.Group,
+		Idx:     c.anal.curr,
+		IsFirst: c.anal.isFirst,
+		Arg:     constructGroup(c.ctx, n, ns[n.Children[0]], true, 0, c.proc),
+	})
+	c.anal.isFirst = false
+	return ss
+}
+
 func (c *Compile) compileMergeGroup(n *plan.Node, ss []*Scope, ns []*plan.Node, hasDistinct bool) []*Scope {
 	currentFirstFlag := c.anal.isFirst
 
@@ -3905,7 +3919,7 @@ func (c *Compile) generateNodes(n *plan.Node) (engine.Nodes, []any, []types.T, e
 		return nodes, nil, nil, nil
 	}
 
-	if len(n.AggList) > 0 && ranges.Len() > 1 {
+	if len(n.AggList) > 0 && ranges.Len() > 1 && c.execType != plan2.ExecTypeTP {
 		newranges := make([]byte, 0, ranges.Size())
 		newranges = append(newranges, ranges.GetBytes(0)...)
 		partialResults = make([]any, 0, len(n.AggList))
