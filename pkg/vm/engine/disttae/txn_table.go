0322d5eac5ddb4c88e51a13743741c9ec5272c7b
Wei Ziran <weiziran125@gmail.com>
2024-06-04 15:39:29 +0800

    ignore delete booking if do not need transfer

--- pkg/vm/engine/disttae/txn_table.go
+++ pkg/vm/engine/disttae/txn_table.go
@@ -2600,7 +2600,7 @@ func (tbl *txnTable) MergeObjects(ctx context.Context, objstats []objectio.Objec
 		return nil, err
 	}
 
-	if taskHost.DoTransfer() {
+	if !taskHost.DoTransfer() {
 		return taskHost.commitEntry, nil
 	}
 
