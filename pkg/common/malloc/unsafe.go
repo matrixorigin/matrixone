d5d7e5ab74e87176d6364829d24bf3d36ed1d832
reus <reusee@gmail.com>
2024-06-05 14:26:41 +0800

    malloc: port new allocator to 1.2

--- pkg/common/malloc/unsafe.go
+++ pkg/common/malloc/unsafe.go
@@ -1,4 +1,4 @@
-// Copyright 2022 Matrix Origin
+// Copyright 2024 Matrix Origin
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,31 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package memorycache
+package malloc
 
 import (
-	"sync/atomic"
-	"testing"
-
-	"github.com/matrixorigin/matrixone/pkg/common/malloc"
-	"github.com/stretchr/testify/require"
+	_ "unsafe"
 )
 
-func TestRCBytes(t *testing.T) {
-	allocator := malloc.GetDefault(nil)
+//go:linkname runtime_procPin runtime.procPin
+func runtime_procPin() int
 
-	var size atomic.Int64
-	r := RCBytes{
-		d:    newData(allocator, 1, &size),
-		size: &size,
-	}
-	// test Bytes
-	r.Bytes()[0] = 1
-	require.Equal(t, r.Bytes()[0], byte(1))
-	// test Slice
-	r = r.Slice(0).(RCBytes)
-	require.Equal(t, 0, len(r.Bytes()))
-	// test release
-	r.Release()
-	require.Equal(t, int64(0), size.Load())
-}
+//go:linkname runtime_procUnpin runtime.procUnpin
+func runtime_procUnpin() int
+
+//go:linkname fastrand runtime.fastrand
+func fastrand() uint32
