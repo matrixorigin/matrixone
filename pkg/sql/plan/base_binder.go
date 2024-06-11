7d3ba46cbd1c28db4b591763d1a1191d8d2fb71b
Cao Kai <ck89119@gmail.com>
2024-06-11 17:50:30 +0800

    remove keep_user_target_list_in_result && make case insensitive of sys db and table names

--- pkg/sql/plan/base_binder.go
+++ pkg/sql/plan/base_binder.go
@@ -1141,7 +1141,7 @@ func bindFuncExprImplUdf(b *baseBinder, name string, udf *function.Udf, args []t
 
 		if !strings.Contains(sql, "select") {
 			sql = "select " + sql
-			substmts, err := parsers.Parse(b.GetContext(), dialect.MYSQL, sql, 1, 0)
+			substmts, err := parsers.Parse(b.GetContext(), dialect.MYSQL, sql, 1)
 			if err != nil {
 				return nil, err
 			}
@@ -1150,7 +1150,7 @@ func bindFuncExprImplUdf(b *baseBinder, name string, udf *function.Udf, args []t
 				return nil, err
 			}
 		} else {
-			substmts, err := parsers.Parse(b.GetContext(), dialect.MYSQL, sql, 1, 0)
+			substmts, err := parsers.Parse(b.GetContext(), dialect.MYSQL, sql, 1)
 			if err != nil {
 				return nil, err
 			}
