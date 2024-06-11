f756ac8cf42dc5ccd1eb2afc0d89838a0e7162db
Kai Cao <ck89119@users.noreply.github.com>
2024-06-11 22:28:35 +0800

    Refactor system variable lower_case_table_names (#16627)
    - system variable lower_case_table_names **only** affects db name, table name and table alias now, which affect all identifiers before
    - all identifiers (except db name, table name and table alias) are **case insensitive**
    - show user origin input in `show xxx` statements (e.g. `show create table, show columns ...`) and other similar statements
    
    Approved by: @aunjgr, @daviszhen, @iamlinjunhong, @heni02

--- pkg/frontend/variables.go
+++ pkg/frontend/variables.go
@@ -960,7 +960,7 @@ func (m *GlobalSysVarsMgr) Get(accountId uint32, ses *Session, ctx context.Conte
 	m.Lock()
 	defer m.Unlock()
 	// get from gSysVarsDefs && table if absent
-	if _, ok := m.accountsGlobalSysVarsMap[accountId]; !ok {
+	if m.accountsGlobalSysVarsMap[accountId] == nil {
 		sysVars, err := ses.getGlobalSysVars(ctx)
 		if err != nil {
 			return nil, err
@@ -3511,6 +3511,14 @@ var gSysVarsDefs = map[string]SystemVariable{
 		Dynamic:           true,
 		SetVarHintApplies: false,
 		Type:              InitSystemVariableBoolType("experimental_ivf_index"),
+		Default:           int64(0),
+	},
+	"refresh_global_sys_vars_mgr": {
+		Name:              "refresh_global_sys_vars_mgr",
+		Scope:             ScopeBoth,
+		Dynamic:           true,
+		SetVarHintApplies: false,
+		Type:              InitSystemVariableBoolType("refresh_global_sys_vars_mgr"),
 		Default:           int64(0),
 	},
 }
