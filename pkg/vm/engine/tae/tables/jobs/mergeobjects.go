bd62d41f36b5564efd2a7ee09d0402e13f9b41f1
Wei Ziran <weiziran125@gmail.com>
2024-06-12 00:41:04 +0800

    Do not apply delete when aobject merge on 1.2 dev (#16789)
    do not apply delete when flush
    
    Approved by: @XuPeng-SH, @sukki37

--- pkg/vm/engine/tae/tables/jobs/mergeobjects.go
+++ pkg/vm/engine/tae/tables/jobs/mergeobjects.go
@@ -174,64 +174,6 @@ func (task *mergeObjectsTask) GetMPool() *mpool.MPool {
 
 func (task *mergeObjectsTask) HostHintName() string { return "DN" }
 
-func (task *mergeObjectsTask) PrepareData(ctx context.Context) ([]*batch.Batch, []*nulls.Nulls, func(), error) {
-	var err error
-	views := make([]*containers.BlockView, task.totalMergedBlkCnt)
-	releaseF := func() {
-		for _, view := range views {
-			if view != nil {
-				view.Close()
-			}
-		}
-	}
-	defer func() {
-		if err != nil {
-			releaseF()
-		}
-	}()
-	schema := task.rel.Schema().(*catalog.Schema)
-	idxs := make([]int, 0, len(schema.ColDefs)-1)
-	attrs := make([]string, 0, len(schema.ColDefs)-1)
-	for _, def := range schema.ColDefs {
-		if def.IsPhyAddr() {
-			continue
-		}
-		idxs = append(idxs, def.Idx)
-		attrs = append(attrs, def.Name)
-	}
-	for i, obj := range task.mergedObjsHandle {
-
-		maxBlockOffset := task.totalMergedBlkCnt
-		if i != len(task.mergedObjs)-1 {
-			maxBlockOffset = task.mergedBlkCnt[i+1]
-		}
-		minBlockOffset := task.mergedBlkCnt[i]
-
-		for j := 0; j < maxBlockOffset-minBlockOffset; j++ {
-			if views[minBlockOffset+j], err = obj.GetColumnDataByIds(ctx, uint16(j), idxs, common.MergeAllocator); err != nil {
-				return nil, nil, nil, err
-			}
-		}
-	}
-
-	batches := make([]*batch.Batch, 0, task.totalMergedBlkCnt)
-	dels := make([]*nulls.Nulls, 0, task.totalMergedBlkCnt)
-	for _, view := range views {
-		batch := batch.New(true, attrs)
-		if len(attrs) != len(view.Columns) {
-			panic(fmt.Sprintf("mismatch %v, %v, %v", attrs, len(attrs), len(view.Columns)))
-		}
-		for i, col := range view.Columns {
-			batch.Vecs[i] = col.GetData().GetDownstreamVector()
-		}
-		batch.SetRowCount(view.Columns[0].Length())
-		batches = append(batches, batch)
-		dels = append(dels, view.DeleteMask)
-	}
-
-	return batches, dels, releaseF, nil
-}
-
 func (task *mergeObjectsTask) LoadNextBatch(ctx context.Context, objIdx uint32) (*batch.Batch, *nulls.Nulls, func(), error) {
 	if objIdx >= uint32(len(task.mergedObjs)) {
 		panic("invalid objIdx")
@@ -336,7 +278,7 @@ func (task *mergeObjectsTask) Execute(ctx context.Context) (err error) {
 		sortkeyPos = schema.GetSingleSortKeyIdx()
 	}
 	phaseDesc = "1-DoMergeAndWrite"
-	if err = mergesort.DoMergeAndWrite(ctx, sortkeyPos, int(schema.BlockMaxRows), task); err != nil {
+	if err = mergesort.DoMergeAndWrite(ctx, sortkeyPos, task); err != nil {
 		return err
 	}
 
