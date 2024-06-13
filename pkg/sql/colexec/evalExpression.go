53def1874f2cb7b857859dd37a1920b8acb01fb5
zengyan1 <171838137@qq.com>
2024-06-13 23:32:28 +0800

    debug

--- pkg/sql/colexec/evalExpression.go
+++ pkg/sql/colexec/evalExpression.go
@@ -537,7 +537,7 @@ func (expr *FunctionExpressionExecutor) EvalCase(proc *process.Process, batches 
 			return err
 		}
 		if i != len(expr.parameterExecutor)-1 {
-			for j := 0; j < RowCount; j++ {
+			for j := 0; j < rowCount; j++ {
 				b, null := vector.GenerateFunctionFixedTypeParameter[bool](expr.parameterResults[i]).GetValue(uint64(j))
 				if !null && b {
 					expr.selectList1[j] = false
