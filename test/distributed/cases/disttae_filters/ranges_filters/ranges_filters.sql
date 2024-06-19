cb18604a8bdc6634419eb64f2409315f3c94ccb8
gouhongshen <gouhongshen@hotmail.com>
2024-06-19 19:02:13 +0800

    clean code

--- test/distributed/cases/disttae_filters/ranges_filters/ranges_filters.sql
+++ test/distributed/cases/disttae_filters/ranges_filters/ranges_filters.sql
@@ -10,5 +10,5 @@ drop table hhh;
 create table t2(a int primary key);
 insert into t2 select * from generate_series(1,8192000)g;
 select a from t2 where a in (1,2) or a in (10000,10001) or a in (20000,20001) or a in (40000,40001);
-drop table t2
+drop table t2;
 drop database testdb;
