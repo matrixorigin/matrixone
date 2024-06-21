a10a89c2782ac1668556f3770c3d165707519c35
GreatRiver <2552853833@qq.com>
2024-06-21 15:25:39 +0800

    Add handleDiskCleaner for mo_ctl

--- pkg/sql/parsers/dialect/mysql/mysql_sql.go
+++ pkg/sql/parsers/dialect/mysql/mysql_sql.go
@@ -13382,7 +13382,7 @@ yydefault:
 //line mysql_sql.y:3454
 		{
 			assignments := []*tree.VarAssignmentExpr{
-				&tree.VarAssignmentExpr{
+				{
 					System: true,
 					Global: true,
 					Name:   yyDollar[6].str,
@@ -13691,7 +13691,7 @@ yydefault:
 		{
 			s := &tree.ShowGrants{}
 			roles := []*tree.Role{
-				&tree.Role{UserName: yyDollar[5].cstrUnion().Compare()},
+				{UserName: yyDollar[5].cstrUnion().Compare()},
 			}
 			s.Roles = roles
 			s.ShowGrantType = tree.GrantForRole
