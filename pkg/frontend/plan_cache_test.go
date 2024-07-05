d1bebd3d47f2b4e0a41b8b33c78b45e7374838b8
CJKkkk-315 <1121033787@qq.com>
2024-07-05 12:32:27 +0800

    fix ut

--- pkg/frontend/plan_cache_test.go
+++ pkg/frontend/plan_cache_test.go
@@ -15,21 +15,29 @@
 package frontend
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func hashString(sql string) string {
+	hash := sha256.New()
+	hash.Write([]byte(sql))
+	return hex.EncodeToString(hash.Sum(nil))
+}
 
 func Test_BasicGet(t *testing.T) {
 	pc := newPlanCache(5)
 
 	pc.cache("abc", nil, nil)
 	require.True(t, pc.isCached("abc"))
-	require.Equal(t, pc.get("abc").sql, "abc")
+	require.Equal(t, pc.get("abc").sql, hashString("abc"))
 
 	pc.cache("abcd", nil, nil)
 	require.True(t, pc.isCached("abcd"))
-	require.Equal(t, pc.get("abcd").sql, "abcd")
+	require.Equal(t, pc.get("abcd").sql, hashString("abcd"))
 
 	require.False(t, pc.isCached("abcde"))
 }
@@ -48,7 +56,7 @@ func Test_LRU(t *testing.T) {
 	require.True(t, pc.isCached("4"))
 	require.False(t, pc.isCached("1"))
 
-	require.Equal(t, pc.get("2").sql, "2")
+	require.Equal(t, pc.get("2").sql, hashString("2"))
 	pc.cache("5", nil, nil)
 	require.True(t, pc.isCached("5"))
 	require.True(t, pc.isCached("4"))
