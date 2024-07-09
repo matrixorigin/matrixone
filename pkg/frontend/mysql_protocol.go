44ad34e6206f1fb712991e7aefab2da22ed0b793
CJKkkk-315 <1121033787@qq.com>
2024-07-09 17:19:50 +0800

    revert prepare paramType

--- pkg/frontend/mysql_protocol.go
+++ pkg/frontend/mysql_protocol.go
@@ -787,7 +787,6 @@ func (mp *MysqlProtocolImpl) ParseExecuteData(ctx context.Context, proc *process
 
 	if numParams > 0 {
 		var nullBitmaps []byte
-		var paramTypes []byte
 		nullBitmapLen := (numParams + 7) >> 3
 		nullBitmaps, pos, ok = mp.readCountOfBytes(data, pos, nullBitmapLen)
 		if !ok {
@@ -800,9 +799,8 @@ func (mp *MysqlProtocolImpl) ParseExecuteData(ctx context.Context, proc *process
 
 			// Just the first StmtExecute packet contain parameters type,
 			// we need save it for further use.
-			paramTypes, pos, ok = mp.readCountOfBytes(data, pos, numParams<<1)
-			stmt.ParamTypes = make([]byte, len(paramTypes))
-			copy(stmt.ParamTypes, paramTypes)
+			stmt.ParamTypes, pos, ok = mp.readCountOfBytes(data, pos, numParams<<1)
+
 			if !ok {
 				return moerr.NewInvalidInput(ctx, "mysql protocol error, malformed packet")
 			}
