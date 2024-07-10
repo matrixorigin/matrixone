0e90a97aa0706be986053c2a2e5a8e9f909bb5ce
GreatRiver <2552853833@qq.com>
2024-07-10 21:13:15 +0800

    Fix GC MaxConsumed For Backup (#17464)
    During backup, if GC does not consume the most recent global checkpoint, the restored cluster will not be able to find the checkpoint to be used for GC and will not work. This is because backup only backs up data after the most recent global checkpoint.
    So raise the GC water level
    
    Approved by: @XuPeng-SH, @daviszhen, @sukki37

--- pkg/vm/engine/tae/blockio/types.go
+++ pkg/vm/engine/tae/blockio/types.go
@@ -62,6 +62,14 @@ func EncodeGCMetadataFileName(dir, prefix string, start, end types.TS) string {
 	return fmt.Sprintf("%s/%s_%s_%s.%s", dir, prefix, start.ToString(), end.ToString(), GCFullExt)
 }
 
+func UpdateGCMetadataFileName(name string, start, end types.TS) string {
+	fileName := strings.Split(name, ".")
+	info := strings.Split(fileName[0], "_")
+	prefix := info[0]
+	ext := fileName[1]
+	return fmt.Sprintf("%s_%s_%s.%s", prefix, start.ToString(), end.ToString(), ext)
+}
+
 func DecodeCheckpointMetadataFileName(name string) (start, end types.TS) {
 	fileName := strings.Split(name, ".")
 	info := strings.Split(fileName[0], "_")
