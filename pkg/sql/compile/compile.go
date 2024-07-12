be57973c0a7d2c00601dc78d8d978d5f41394089
ouyuanning <52741323@qq.com>
2024-07-13 07:01:37 +0800

    bug fix

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -1804,12 +1804,12 @@ func (c *Compile) compileLockOp(n *plan.Node, ss []*Scope) ([]*Scope, error) {
 		if len(n.LockTargets) > 1 || n.LockTargets[0].LockTable || !n.LockTargets[0].LockTableAtTheEnd || n.LockTargets[0].LockRows == nil {
 			for i := range ss {
 				lockOpArg := constructLockOp(n, c, block)
-				ss[i].appendOperator(lockOpArg)
+				ss[i].setRootOperator(lockOpArg)
 			}
 		} else {
 			lockOpArg := constructLockOp(n, c, block)
 			ss = []*Scope{c.newMergeScope(ss)}
-			ss[0].appendOperator(lockOpArg)
+			ss[0].setRootOperator(lockOpArg)
 		}
 	}
 	ss = c.compileProjection(n, ss)
