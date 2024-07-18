29740f93baeda8b1d64f8122f1f59edddb1f25cb
reus <reusee@gmail.com>
2024-07-18 09:58:53 +0800

    fileservice: make allocators randomly freezable

--- pkg/fileservice/malloc.go
+++ pkg/fileservice/malloc.go
@@ -21,6 +21,22 @@ import (
 	metric "github.com/matrixorigin/matrixone/pkg/util/metric/v2"
 )
 
+const (
+	// 1/freezeFraction of allocations may be frozen to detect wrong mutation such as mutating the memory cache objects
+	freezeFraction = 100
+)
+
+func decorateAllocator(allocator malloc.Allocator) malloc.Allocator {
+	// freeze randomly to detect wrong mutation
+	// this makes the allocator randomly freezable, whether to freeze is decided by the callers of Allocate method
+	allocator = malloc.NewRandomAllocator(
+		allocator,
+		malloc.NewReadOnlyAllocator(allocator),
+		freezeFraction,
+	)
+	return allocator
+}
+
 var getMemoryCacheAllocator = func() func() malloc.Allocator {
 	var allocator malloc.Allocator
 	var initOnce sync.Once
@@ -35,8 +51,8 @@ var getMemoryCacheAllocator = func() func() malloc.Allocator {
 				metric.MallocCounterMemoryCacheAllocateObjects,
 				metric.MallocGaugeMemoryCacheInuseObjects,
 			)
-			// freezable
-			allocator = malloc.NewReadOnlyAllocator(allocator)
+			// decorate
+			allocator = decorateAllocator(allocator)
 		})
 		return allocator
 	}
@@ -56,8 +72,8 @@ var getBytesAllocator = func() func() malloc.Allocator {
 				metric.MallocCounterBytesAllocateObjects,
 				metric.MallocGaugeBytesInuseObjects,
 			)
-			// freezable
-			allocator = malloc.NewReadOnlyAllocator(allocator)
+			// decorate
+			allocator = decorateAllocator(allocator)
 		})
 		return allocator
 	}
@@ -68,13 +84,17 @@ var getIOAllocator = func() func() malloc.Allocator {
 	var initOnce sync.Once
 	return func() malloc.Allocator {
 		initOnce.Do(func() {
+			allocator = malloc.GetDefault(nil)
+			// with metrics
 			allocator = malloc.NewMetricsAllocator(
-				malloc.GetDefault(nil),
+				allocator,
 				metric.MallocCounterIOAllocateBytes,
 				metric.MallocGaugeIOInuseBytes,
 				metric.MallocCounterIOAllocateObjects,
 				metric.MallocGaugeIOInuseObjects,
 			)
+			// decorate
+			allocator = decorateAllocator(allocator)
 		})
 		return allocator
 	}
