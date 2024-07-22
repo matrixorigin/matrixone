b4fe043b628bb6c39073ca10ec216dad295be48e
iamlinjunhong <1030420200@qq.com>
2024-07-22 18:49:02 +0800

    txn table delegate

--- pkg/common/morpc/buffer_pool.go
+++ pkg/common/morpc/buffer_pool.go
@@ -17,6 +17,11 @@ package morpc
 import (
 	"github.com/fagongzi/goetty/v2/buf"
 	"github.com/matrixorigin/matrixone/pkg/common/reuse"
+	"github.com/matrixorigin/matrixone/pkg/objectio"
+	"github.com/matrixorigin/matrixone/pkg/pb/api"
+	"github.com/matrixorigin/matrixone/pkg/pb/plan"
+	pb "github.com/matrixorigin/matrixone/pkg/pb/statsinfo"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine"
 )
 
 type Buffer struct {
@@ -46,6 +51,88 @@ func (b *Buffer) EncodeUint64(
 	b.buf.Grow(8)
 	idx := b.buf.GetWriteIndex()
 	if err := b.buf.WriteUint64(v); err != nil {
+		panic(err)
+	}
+	return b.buf.RawSlice(idx, b.buf.GetWriteIndex())
+}
+
+func (b *Buffer) EncodeUint16(
+	v uint16,
+) []byte {
+	b.buf.Grow(2)
+	idx := b.buf.GetWriteIndex()
+	if err := b.buf.WriteUint16(v); err != nil {
+		panic(err)
+	}
+	return b.buf.RawSlice(idx, b.buf.GetWriteIndex())
+}
+
+func (b *Buffer) EncodeStatsInfo(
+	v *pb.StatsInfo,
+) []byte {
+	bys, err := v.Marshal()
+	if err != nil {
+		panic(err)
+	}
+	b.buf.Grow(len(bys))
+	idx := b.buf.GetWriteIndex()
+	if _, err = b.buf.Write(bys); err != nil {
+		panic(err)
+	}
+	return b.buf.RawSlice(idx, b.buf.GetWriteIndex())
+}
+
+func (b *Buffer) EncodeInt(
+	v int,
+) []byte {
+	b.buf.Grow(4)
+	idx := b.buf.GetWriteIndex()
+	if err := b.buf.WriteInt(v); err != nil {
+		panic(err)
+	}
+	return b.buf.RawSlice(idx, b.buf.GetWriteIndex())
+}
+
+func (b *Buffer) EncodeColumMetadataScanInfo(
+	infos []*plan.MetadataScanInfo,
+) []byte {
+	v := &plan.MetadataScanInfos{
+		Infos: infos,
+	}
+	bys, err := v.Marshal()
+	if err != nil {
+		panic(err)
+	}
+	b.buf.Grow(len(bys))
+	idx := b.buf.GetWriteIndex()
+	if _, err = b.buf.Write(bys); err != nil {
+		panic(err)
+	}
+	return b.buf.RawSlice(idx, b.buf.GetWriteIndex())
+}
+
+func (b *Buffer) EncodeMergeCommitEntry(
+	v *api.MergeCommitEntry,
+) []byte {
+	bys, err := v.Marshal()
+	if err != nil {
+		panic(err)
+	}
+	b.buf.Grow(len(bys))
+	idx := b.buf.GetWriteIndex()
+	if _, err = b.buf.Write(bys); err != nil {
+		panic(err)
+	}
+	return b.buf.RawSlice(idx, b.buf.GetWriteIndex())
+}
+
+func (b *Buffer) EncodeRanges(
+	v engine.Ranges,
+) []byte {
+	bys := []byte(*v.(*objectio.BlockInfoSlice))
+	b.buf.Grow(len(bys))
+	idx := b.buf.GetWriteIndex()
+	if _, err := b.buf.Write(bys); err != nil {
 		panic(err)
 	}
 	return b.buf.RawSlice(idx, b.buf.GetWriteIndex())
--- pkg/vm/engine/disttae/txn_table_sharding_handle.go
+++ pkg/vm/engine/disttae/txn_table_sharding_handle.go
@@ -16,8 +16,10 @@ package disttae
 
 import (
 	"context"
-
 	"github.com/matrixorigin/matrixone/pkg/common/morpc"
+	"github.com/matrixorigin/matrixone/pkg/container/types"
+	"github.com/matrixorigin/matrixone/pkg/container/vector"
+	"github.com/matrixorigin/matrixone/pkg/objectio"
 	"github.com/matrixorigin/matrixone/pkg/pb/shard"
 	"github.com/matrixorigin/matrixone/pkg/pb/timestamp"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine"
@@ -75,6 +77,237 @@ func HandleShardingReadSize(
 		return nil, err
 	}
 	return buffer.EncodeUint64(rows), nil
+}
+
+// HandleShardingReadStatus handles sharding read status
+func HandleShardingReadStatus(
+	ctx context.Context,
+	shard shard.TableShard,
+	engine engine.Engine,
+	param shard.ReadParam,
+	ts timestamp.Timestamp,
+	buffer *morpc.Buffer,
+) ([]byte, error) {
+	tbl, err := getTxnTable(
+		ctx,
+		param,
+		engine,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	info, err := tbl.Stats(
+		ctx,
+		param.StatsParam.Sync,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return buffer.EncodeStatsInfo(info), nil
+}
+
+// HandleShardingReadApproxObjectsNum handles sharding read ApproxObjectsNum
+func HandleShardingReadApproxObjectsNum(
+	ctx context.Context,
+	shard shard.TableShard,
+	engine engine.Engine,
+	param shard.ReadParam,
+	ts timestamp.Timestamp,
+	buffer *morpc.Buffer,
+) ([]byte, error) {
+	tbl, err := getTxnTable(
+		ctx,
+		param,
+		engine,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	num := tbl.ApproxObjectsNum(
+		ctx,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return buffer.EncodeInt(num), nil
+}
+
+// HandleShardingReadRanges handles sharding read Ranges
+func HandleShardingReadRanges(
+	ctx context.Context,
+	shard shard.TableShard,
+	engine engine.Engine,
+	param shard.ReadParam,
+	ts timestamp.Timestamp,
+	buffer *morpc.Buffer,
+) ([]byte, error) {
+	tbl, err := getTxnTable(
+		ctx,
+		param,
+		engine,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	ranges, err := tbl.Ranges(
+		ctx,
+		param.RangesParam.Exprs,
+		int(param.RangesParam.TxnOffset),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return buffer.EncodeRanges(ranges), nil
+}
+
+// HandleShardingReadGetColumMetadataScanInfo handles sharding read GetColumMetadataScanInfo
+func HandleShardingReadGetColumMetadataScanInfo(
+	ctx context.Context,
+	shard shard.TableShard,
+	engine engine.Engine,
+	param shard.ReadParam,
+	ts timestamp.Timestamp,
+	buffer *morpc.Buffer,
+) ([]byte, error) {
+	tbl, err := getTxnTable(
+		ctx,
+		param,
+		engine,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	infos, err := tbl.GetColumMetadataScanInfo(
+		ctx,
+		param.GetColumMetadataScanInfoParam.ColumnName,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return buffer.EncodeColumMetadataScanInfo(infos), nil
+}
+
+// HandleShardingReadReader handles sharding read Reader
+func HandleShardingReadReader(
+	ctx context.Context,
+	shard shard.TableShard,
+	engine engine.Engine,
+	param shard.ReadParam,
+	ts timestamp.Timestamp,
+	buffer *morpc.Buffer,
+) ([]byte, error) {
+	tbl, err := getTxnTable(
+		ctx,
+		param,
+		engine,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = tbl.NewReader(
+		ctx,
+		int(param.ReaderParam.Num),
+		param.ReaderParam.Expr,
+		param.ReaderParam.Ranges,
+		param.ReaderParam.OrderedScan,
+		int(param.ReaderParam.TxnOffset),
+	)
+	if err != nil {
+		return nil, err
+	}
+	// TODO:
+	return nil, nil
+}
+
+// HandleShardingReadPrimaryKeysMayBeModified handles sharding read PrimaryKeysMayBeModified
+func HandleShardingReadPrimaryKeysMayBeModified(
+	ctx context.Context,
+	shard shard.TableShard,
+	engine engine.Engine,
+	param shard.ReadParam,
+	ts timestamp.Timestamp,
+	buffer *morpc.Buffer,
+) ([]byte, error) {
+	tbl, err := getTxnTable(
+		ctx,
+		param,
+		engine,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	var from, to types.TS
+	err = from.Unmarshal(param.PrimaryKeysMayBeModifiedParam.From)
+	if err != nil {
+		return nil, err
+	}
+
+	err = to.Unmarshal(param.PrimaryKeysMayBeModifiedParam.To)
+	if err != nil {
+		return nil, err
+	}
+
+	keyVector := vector.NewVecFromReuse()
+	err = keyVector.UnmarshalBinary(param.PrimaryKeysMayBeModifiedParam.KeyVector)
+	if err != nil {
+		return nil, err
+	}
+
+	modify, err := tbl.PrimaryKeysMayBeModified(
+		ctx,
+		from,
+		to,
+		keyVector,
+	)
+	if err != nil {
+		return nil, err
+	}
+	var r uint16
+	if modify {
+		r = 1
+	}
+	return buffer.EncodeUint16(r), nil
+}
+
+// HandleShardingReadMergeObjects handles sharding read MergeObjects
+func HandleShardingReadMergeObjects(
+	ctx context.Context,
+	shard shard.TableShard,
+	engine engine.Engine,
+	param shard.ReadParam,
+	ts timestamp.Timestamp,
+	buffer *morpc.Buffer,
+) ([]byte, error) {
+	tbl, err := getTxnTable(
+		ctx,
+		param,
+		engine,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	objstats := make([]objectio.ObjectStats, len(param.MergeObjectsParam.Objstats))
+	for i, o := range param.MergeObjectsParam.Objstats {
+		objstats[i].UnMarshal(o)
+	}
+
+	entry, err := tbl.MergeObjects(
+		ctx,
+		objstats,
+		param.MergeObjectsParam.PolicyName,
+		param.MergeObjectsParam.TargetObjSize,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return buffer.EncodeMergeCommitEntry(entry), nil
 }
 
 func getTxnTable(
