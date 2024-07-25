01a472d2fed0f9636ef95ef5d099442bbc58bdbc
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2024-07-24 22:42:24 -0700

    BVT Fix

--- test/distributed/cases/function/func_load_file_datalink.sql
+++ test/distributed/cases/function/func_load_file_datalink.sql
@@ -5,5 +5,4 @@ create table t1(a int, b datalink);
 insert into t1 values(1, "wrong datalink url");
 insert into t1 values(1, 'file://$resources/file_test/normal.txt?offset=0&size=3');
 insert into t1 values(2, 'file://$resources/file_test/normal.txt');
-select * from t1;
 select a, load_file(b) from t1;
