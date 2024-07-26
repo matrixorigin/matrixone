f87c0c0ad0544c131e8b5aa06439f76baffdc703
LiuBo <g.user.lb@gmail.com>
2024-07-26 22:49:48 +0800

    [proxy] enhancement: don't change txn status if it is error packet (#17741)
    don't change txn status if it is error packet
    
    Approved by: @zhangxu19830126

--- pkg/proxy/tunnel.go
+++ pkg/proxy/tunnel.go
@@ -576,7 +576,10 @@ func (p *pipe) kickoff(ctx context.Context, peer *pipe) (e error) {
 				rotated = false
 			}
 
-			p.mu.inTxn = checkTxnStatus(buf)
+			inTxn, ok := checkTxnStatus(buf)
+			if ok {
+				p.mu.inTxn = inTxn
+			}
 			if !p.mu.inTxn && p.tun.transferIntent.Load() && !rotated {
 				peer.wg.Add(1)
 				p.transferred = true
@@ -741,7 +744,11 @@ func handleEOFPacket(msg []byte) bool {
 	return txnStatus(binary.LittleEndian.Uint16(msg[7:]))
 }
 
-func checkTxnStatus(msg []byte) bool {
+// the first return value is the txn status, and the second return value
+// indicates if we can get the txn status from the packet. If it is a ERROR
+// packet, the second return value is false.
+func checkTxnStatus(msg []byte) (bool, bool) {
+	ok := true
 	inTxn := true
 	// For the server->client pipe, we get the transaction status from the
 	// OK and EOF packet, which is used in connection transfer. If the session
@@ -750,6 +757,8 @@ func checkTxnStatus(msg []byte) bool {
 		inTxn = handleOKPacket(msg)
 	} else if isEOFPacket(msg) {
 		inTxn = handleEOFPacket(msg)
+	} else if isErrPacket(msg) {
+		ok = false
 	}
-	return inTxn
+	return inTxn, ok
 }
