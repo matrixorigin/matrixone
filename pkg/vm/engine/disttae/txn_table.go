28cebbdfae9dd65ac2a0d600f8542c830f8a879c
qingxinhome <758355272@qq.com>
2024-07-30 15:18:08 +0800

    add lock for data race issue

--- pkg/vm/engine/disttae/txn_table.go
+++ pkg/vm/engine/disttae/txn_table.go
@@ -702,10 +702,10 @@ var slowPathCounter atomic.Int64
 func (tbl *txnTable) rangesOnePart(
 	ctx context.Context,
 	state *logtailreplay.PartitionState, // snapshot state of this transaction
-	tableDef *plan.TableDef,             // table definition (schema)
-	exprs []*plan.Expr,                  // filter expression
-	outBlocks *objectio.BlockInfoSlice,  // output marshaled block list after filtering
-	proc *process.Process,               // process of this transaction
+	tableDef *plan.TableDef, // table definition (schema)
+	exprs []*plan.Expr, // filter expression
+	outBlocks *objectio.BlockInfoSlice, // output marshaled block list after filtering
+	proc *process.Process, // process of this transaction
 	txnOffset int,
 ) (err error) {
 	var done bool
@@ -1187,7 +1187,6 @@ func (tbl *txnTable) GetTableDef(ctx context.Context) *plan.TableDef {
 		tbl.tableDef = &plan.TableDef{
 			TblId:         tbl.tableId,
 			Name:          tbl.tableName,
-			DbName:        tbl.db.databaseName,
 			Cols:          cols,
 			Name2ColIndex: name2index,
 			Defs:          defs,
@@ -1203,6 +1202,9 @@ func (tbl *txnTable) GetTableDef(ctx context.Context) *plan.TableDef {
 			Version:       tbl.version,
 		}
 	}
+	tbl.Lock()
+	defer tbl.Unlock()
+	tbl.tableDef.DbName = tbl.db.databaseName
 	return tbl.tableDef
 }
 
