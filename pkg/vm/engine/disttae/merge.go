e1233daa0b0cd82d07f8e93f949c5ac056efaf83
GreatRiver <2552853833@qq.com>
2024-07-30 20:42:38 +0800

    update code

--- pkg/vm/engine/disttae/merge.go
+++ pkg/vm/engine/disttae/merge.go
@@ -28,7 +28,6 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
 	"github.com/matrixorigin/matrixone/pkg/fileservice"
-	"github.com/matrixorigin/matrixone/pkg/logutil"
 	"github.com/matrixorigin/matrixone/pkg/objectio"
 	"github.com/matrixorigin/matrixone/pkg/pb/api"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/disttae/logtailreplay"
