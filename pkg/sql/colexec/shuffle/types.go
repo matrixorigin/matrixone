7386d2927a8cf79ec30b937b23898104b2b53f4c
nitao <badboynt@126.com>
2024-08-09 21:59:39 +0800

    fix shuffle operator (#17985)
    ## What type of PR is this?
    
    - [ ] API-change
    - [ ] BUG
    - [ ] Improvement
    - [ ] Documentation
    - [ ] Feature
    - [ ] Test and CI
    - [ ] Code Refactoring
    
    ## Which issue(s) this PR fixes:
    
    issue #
    
    ## What this PR does / why we need it:
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pkg/sql/colexec/shuffle/types.go
+++ pkg/sql/colexec/shuffle/types.go
@@ -26,7 +26,7 @@ import (
 var _ vm.Operator = new(Shuffle)
 
 type Shuffle struct {
-	ctr                *container
+	ctr                container
 	ShuffleColIdx      int32
 	ShuffleType        int32
 	AliveRegCnt        int32
@@ -74,31 +74,40 @@ type container struct {
 	ending               bool
 	sels                 [][]int64
 	shufflePool          []*batch.Batch
-	sendPool             []*batch.Batch
-	lastSentBatch        *batch.Batch
+	sendPool             []int //pointers to shufflePool
+	lastSentBatchIdx     int
 	runtimeFilterHandled bool
 }
 
 func (shuffle *Shuffle) Reset(proc *process.Process, pipelineFailed bool, err error) {
-	shuffle.Free(proc, pipelineFailed, err)
+	if shuffle.RuntimeFilterSpec != nil {
+		shuffle.ctr.runtimeFilterHandled = false
+	}
+	for i := range shuffle.ctr.shufflePool {
+		if shuffle.ctr.shufflePool[i] != nil {
+			shuffle.ctr.shufflePool[i].CleanOnlyData()
+		}
+	}
+	if shuffle.ctr.sels != nil {
+		shuffle.ctr.sels = shuffle.ctr.sels[:0]
+	}
+	if shuffle.ctr.sendPool != nil {
+		shuffle.ctr.sendPool = shuffle.ctr.sendPool[:0]
+	}
+	shuffle.ctr.ending = false
 }
 
 func (shuffle *Shuffle) Free(proc *process.Process, pipelineFailed bool, err error) {
 	if shuffle.RuntimeFilterSpec != nil {
 		shuffle.ctr.runtimeFilterHandled = false
 	}
-	// can't free this
-	/*if arg.msgReceiver != nil {
-		arg.msgReceiver.Free()
-	}*/
-	if shuffle.ctr != nil {
-		for i := range shuffle.ctr.shufflePool {
-			if shuffle.ctr.shufflePool[i] != nil {
-				shuffle.ctr.shufflePool[i].Clean(proc.Mp())
-				shuffle.ctr.shufflePool[i] = nil
-			}
+	for i := range shuffle.ctr.shufflePool {
+		if shuffle.ctr.shufflePool[i] != nil {
+			shuffle.ctr.shufflePool[i].Clean(proc.Mp())
+			shuffle.ctr.shufflePool[i] = nil
 		}
-		shuffle.ctr.sels = nil
-		shuffle.ctr = nil
 	}
+	shuffle.ctr.sels = nil
+	shuffle.ctr.ending = false
+	shuffle.ctr.sendPool = nil
 }
