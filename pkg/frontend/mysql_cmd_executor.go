b9ab23d56fce0c5e508a806b7920d8999eee5e66
davis zhen <daviszhen007@gmail.com>
2024-08-12 00:06:41 +0800

    fix txn opt (#17982)
    问题：一个请求中，有多个语句。当第一个语句为begin时，下一条语句会提交事务。
    原因：begin语句时，设置的option，执行完后没有立即清理。导致下一条语句执行时，创建了新事务。
    修改：option每次复位。
    
    Approved by: @qingxinhome, @sukki37

--- pkg/frontend/mysql_cmd_executor.go
+++ pkg/frontend/mysql_cmd_executor.go
@@ -34,9 +34,10 @@ import (
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/fagongzi/goetty/v2"
 	"github.com/google/uuid"
-	"github.com/matrixorigin/matrixone/pkg/catalog"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+
+	"github.com/matrixorigin/matrixone/pkg/catalog"
 
 	"github.com/matrixorigin/matrixone/pkg/clusterservice"
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
@@ -2291,6 +2292,7 @@ func executeStmtWithWorkspace(ses FeSession,
 	//1. start txn
 	//special BEGIN,COMMIT,ROLLBACK
 	beginStmt := false
+	execCtx.txnOpt.Close()
 	switch execCtx.stmt.(type) {
 	case *tree.BeginTransaction:
 		execCtx.txnOpt.byBegin = true
@@ -2806,6 +2808,7 @@ func doComQuery(ses *Session, execCtx *ExecCtx, input *UserInput) (retErr error)
 		if ses.proc != nil {
 			ses.proc.UnixTime = proc.UnixTime
 		}
+		execCtx.txnOpt.Close()
 		execCtx.stmt = stmt
 		execCtx.isLastStmt = i >= len(cws)-1
 		execCtx.tenant = tenant
