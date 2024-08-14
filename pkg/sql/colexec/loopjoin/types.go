a50bacac2c0e36947786eb2c715b8bdd85d87cee
zengyan1 <93656539+zengyan1@users.noreply.github.com>
2024-08-14 11:55:07 +0800

    Refactor LoopJoin (#18079)
    - [ ] API-change
    - [ ] BUG
    - [ ] Improvement
    - [ ] Documentation
    - [ ] Feature
    - [ ] Test and CI
    - [ ] Code Refactoring
    
    issue #

--- pkg/sql/colexec/loopjoin/types.go
+++ pkg/sql/colexec/loopjoin/types.go
@@ -39,14 +39,13 @@ type container struct {
 	probeIdx int
 	bat      *batch.Batch
 	rbat     *batch.Batch
-	inBat    *batch.Batch
 	joinBat  *batch.Batch
 	expr     colexec.ExpressionExecutor
 	cfs      []func(*vector.Vector, *vector.Vector, int64, int) error
 }
 
 type LoopJoin struct {
-	ctr        *container
+	ctr        container
 	Cond       *plan.Expr
 	Result     []colexec.ResultPos
 	Typs       []types.Type
@@ -88,16 +87,29 @@ func (loopJoin *LoopJoin) Release() {
 }
 
 func (loopJoin *LoopJoin) Reset(proc *process.Process, pipelineFailed bool, err error) {
-	loopJoin.Free(proc, pipelineFailed, err)
+	ctr := &loopJoin.ctr
+
+	ctr.resetExprExecutor()
+	ctr.state = Build
+
+	if ctr.bat != nil {
+		ctr.bat.Clean(proc.Mp())
+		ctr.bat = nil
+	}
+
+	if loopJoin.ProjectList != nil {
+		anal := proc.GetAnalyze(loopJoin.GetIdx(), loopJoin.GetParallelIdx(), loopJoin.GetParallelMajor())
+		anal.Alloc(loopJoin.ProjectAllocSize)
+		loopJoin.ResetProjection(proc)
+	}
 }
 
 func (loopJoin *LoopJoin) Free(proc *process.Process, pipelineFailed bool, err error) {
-	ctr := loopJoin.ctr
-	if ctr != nil {
-		ctr.cleanBatch(proc.Mp())
-		ctr.cleanExprExecutor()
-		loopJoin.ctr = nil
-	}
+	ctr := &loopJoin.ctr
+
+	ctr.cleanBatch(proc.Mp())
+	ctr.cleanExprExecutor()
+
 	if loopJoin.ProjectList != nil {
 		anal := proc.GetAnalyze(loopJoin.GetIdx(), loopJoin.GetParallelIdx(), loopJoin.GetParallelMajor())
 		anal.Alloc(loopJoin.ProjectAllocSize)
@@ -117,6 +129,12 @@ func (ctr *container) cleanBatch(mp *mpool.MPool) {
 	if ctr.joinBat != nil {
 		ctr.joinBat.Clean(mp)
 		ctr.joinBat = nil
+	}
+}
+
+func (ctr *container) resetExprExecutor() {
+	if ctr.expr != nil {
+		ctr.expr.ResetForNextQuery()
 	}
 }
 
