e3131448422b93f0f10171b5b3d65e4af50b2ebc
Ariznawl@163.com <ariznawl@163.com>
2024-08-14 18:36:41 +0800

    add case

--- test/distributed/cases/publication_subscription/pub_sub3.sql
+++ test/distributed/cases/publication_subscription/pub_sub3.sql
@@ -1,5 +1,9 @@
 drop account if exists acc01;
 create account acc01 admin_name = 'test_account' identified by '111';
+drop account if exists acc02;
+create account acc02 admin_name = 'test_account' identified by '111';
+drop account if exists acc03;
+create account acc03 admin_name = 'test_account' identified by '111';
 -- publication name test
 drop database if exists db01;
 create database db01;
@@ -47,4 +51,50 @@ drop database sub02;
 drop database sub03;
 -- @session
 
+
+
+
+-- test mo-database
+-- @session:id=1&user=acc01:test_account&password=111
+drop database if exists db02;
+create database db02;
+use db02;
+drop table if exists t1;
+create table t1 (a tinyint unsigned not null, primary key(a));
+insert into t1 values (255), (0xFC), (254), (253);
+drop table if exists t2;
+create table t2 ( a tinyint not null default 1, tinyint8 tinyint primary key);
+insert into t2 (tinyint8) values (-1),(127),(-128);
+drop table if exists t3;
+create table t3 ( a tinyint not null default 1, tinyint8 tinyint unsigned primary key);
+insert into t3 (tinyint8) values (0),(255), (0xFE), (253);
+drop publication if exists pub01;
+create publication pub01 database db02 account acc02 comment 'publish to acc01';
+drop publication if exists pub02;
+create publication pub02 database db02 account acc02, acc03 comment 'publish to acc01 and acc03';
+drop publication if exists pub03;
+create publication pub03 database db02 table t1, t2 account acc02 comment 'publish to acc02';
+-- @session
+
+-- @session:id=2&user=acc02:test_account&password=111
+drop database if exists sub02;
+create database sub02 from acc01 publication pub01;
+-- @session
+
+-- @ignore:0,2,7
+select * from mo_catalog.mo_subs;
+
+-- @session:id=2&user=acc02:test_account&password=111
+drop database sub02;
+-- @session
+
+-- @session:id=1&user=acc01:test_account&password=111
+drop publication pub01;
+drop publication pub02;
+drop publication pub03;
+drop database db02;
+-- @session
+
 drop account acc01;
+drop account acc02;
+drop account acc03;
