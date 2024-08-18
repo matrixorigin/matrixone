9001db8ab682831135ca67f33d4e205af7823974
nitao <badboynt@126.com>
2024-08-18 16:35:31 +0800

    fix

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -421,6 +421,7 @@ func (c *Compile) SetIsPrepare(isPrepare bool) {
 	c.isPrepare = isPrepare
 }
 
+/*
 func (c *Compile) printPipeline() {
 	if c.IsTpQuery() {
 		fmt.Println("pipeline for tp query!", "sql: ", c.originSQL)
@@ -429,7 +430,7 @@ func (c *Compile) printPipeline() {
 	}
 	fmt.Println(DebugShowScopes(c.scope))
 }
-
+*/
 // run once
 func (c *Compile) runOnce() error {
 	var wg sync.WaitGroup
@@ -456,7 +457,7 @@ func (c *Compile) runOnce() error {
 		_, _ = GetCompileService().removeRunningCompile(c)
 	}()
 
-	c.printPipeline()
+	//c.printPipeline()
 
 	for i := range c.scope {
 		wg.Add(1)
