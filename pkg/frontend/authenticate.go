91db0e103cd3e60d871e1c2649b7d836759c9e8d
Eric Lam <linguoliang@matrixorigin.cn>
2024-08-23 11:06:36 +0100

    Stage fix (#18309)
    Able to import data from SQL "LOAD DATA INFILE"
    Able to read data from External table via local file and S3.
    SELECT INTO OUTFILE accept stage URL as input filepath and write to local file and S3.
    stage URL can specify to a local file, s3 file and another stage with subpath.
    restrict the stage URL format by only supporting file:///, s3:// or stage://
    remove the HashPassword for stage credentials
    support stage_list() function to list the file in the directory or file path with wildcard
    export function work on top of fileservice
    
    Approved by: @heni02, @ouyuanning, @daviszhen, @badboynt1, @qingxinhome, @m-schen

--- pkg/frontend/authenticate.go
+++ pkg/frontend/authenticate.go
@@ -1458,10 +1458,6 @@ const (
 
 	checkStageFormat = `select stage_id, stage_name from mo_catalog.mo_stages where stage_name = "%s" order by stage_id;`
 
-	checkStageStatusFormat = `select stage_id, stage_name from mo_catalog.mo_stages where stage_status = "%s" order by stage_id;`
-
-	checkStageStatusWithStageNameFormat = `select url, stage_status from mo_catalog.mo_stages where stage_name = "%s" order by stage_id;`
-
 	dropStageFormat = `delete from mo_catalog.mo_stages where stage_name = '%s' order by stage_id;`
 
 	updateStageUrlFormat = `update mo_catalog.mo_stages set url = '%s'  where stage_name = '%s' order by stage_id;`
@@ -1538,19 +1534,8 @@ func getSqlForCheckStage(ctx context.Context, stage string) (string, error) {
 	return fmt.Sprintf(checkStageFormat, stage), nil
 }
 
-func getSqlForCheckStageStatus(ctx context.Context, status string) string {
-	return fmt.Sprintf(checkStageStatusFormat, status)
-}
-
 func getSqlForCheckUdfWithDb(dbName string) string {
 	return fmt.Sprintf(checkUdfWithDb, dbName)
-}
-func getSqlForCheckStageStatusWithStageName(ctx context.Context, stage string) (string, error) {
-	err := inputNameIsInvalid(ctx, stage)
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf(checkStageStatusWithStageNameFormat, stage), nil
 }
 
 func getSqlForInsertIntoMoStages(ctx context.Context, stageName, url, credentials, status, createdTime, comment string) (string, error) {
@@ -3266,7 +3251,7 @@ func doCreateStage(ctx context.Context, ses *Session, cs *tree.CreateStage) (err
 		}
 	} else {
 		// format credentials and hash it
-		credentials = HashPassWord(formatCredentials(cs.Credentials))
+		credentials = formatCredentials(cs.Credentials)
 
 		if !cs.Status.Exist {
 			StageStatus = "disabled"
@@ -3276,6 +3261,11 @@ func doCreateStage(ctx context.Context, ses *Session, cs *tree.CreateStage) (err
 
 		if cs.Comment.Exist {
 			comment = cs.Comment.Comment
+		}
+
+		if !(strings.HasPrefix(cs.Url, function.STAGE_PROTOCOL+"://") || strings.HasPrefix(cs.Url, function.S3_PROTOCOL+"://") ||
+			strings.HasPrefix(cs.Url, function.FILE_PROTOCOL+":///")) {
+			return moerr.NewBadConfig(ctx, "URL protocol only supports stage://, s3:// and file:///")
 		}
 
 		sql, err = getSqlForInsertIntoMoStages(ctx, string(cs.Name), cs.Url, credentials, StageStatus, types.CurrentTimestamp().String2(time.UTC, 0), comment)
@@ -3295,87 +3285,23 @@ func doCreateStage(ctx context.Context, ses *Session, cs *tree.CreateStage) (err
 func doCheckFilePath(ctx context.Context, ses *Session, ep *tree.ExportParam) (err error) {
 	//var err error
 	var filePath string
-	var sql string
-	var erArray []ExecResult
-	var stageName string
-	var stageStatus string
-	var url string
 	if ep == nil {
-		return err
-	}
-
-	bh := ses.GetBackgroundExec(ctx)
-	defer bh.Close()
-
-	err = bh.Exec(ctx, "begin;")
-	defer func() {
-		err = finishTxn(ctx, bh, err)
-	}()
-	if err != nil {
 		return err
 	}
 
 	// detect filepath contain stage or not
 	filePath = ep.FilePath
-	if !strings.Contains(filePath, ":") {
-		// the filepath is the target path
-		sql = getSqlForCheckStageStatus(ctx, "enabled")
-		bh.ClearExecResultSet()
-		err = bh.Exec(ctx, sql)
+	if strings.HasPrefix(filePath, function.STAGE_PROTOCOL+"://") {
+		// stage:// URL
+		s, err := function.UrlToStageDef(filePath, ses.proc)
 		if err != nil {
 			return err
 		}
 
-		erArray, err = getResultSet(ctx, bh)
+		// s.ToPath() returns the fileservice filepath, i.e. s3,...:/path for S3 or /path for local file
+		ses.ep.userConfig.StageFilePath, _, err = s.ToPath()
 		if err != nil {
 			return err
-		}
-
-		// if have stage enabled
-		if execResultArrayHasData(erArray) {
-			return moerr.NewInternalError(ctx, "stage exists, please try to check and use a stage instead")
-		} else {
-			// use the filepath
-			return err
-		}
-	} else {
-		stageName = strings.Split(filePath, ":")[0]
-		// check the stage status
-		sql, err = getSqlForCheckStageStatusWithStageName(ctx, stageName)
-		if err != nil {
-			return err
-		}
-		bh.ClearExecResultSet()
-		err = bh.Exec(ctx, sql)
-		if err != nil {
-			return err
-		}
-
-		erArray, err = getResultSet(ctx, bh)
-		if err != nil {
-			return err
-		}
-		if execResultArrayHasData(erArray) {
-			stageStatus, err = erArray[0].GetString(ctx, 0, 1)
-			if err != nil {
-				return err
-			}
-
-			// is the stage staus is disabled
-			if stageStatus == tree.StageStatusDisabled.String() {
-				return moerr.NewInternalError(ctx, "stage '%s' is invalid, please check", stageName)
-			} else if stageStatus == tree.StageStatusEnabled.String() {
-				// replace the filepath using stage url
-				url, err = erArray[0].GetString(ctx, 0, 0)
-				if err != nil {
-					return err
-				}
-
-				filePath = strings.Replace(filePath, stageName+":", url, 1)
-				ses.ep.userConfig.StageFilePath = filePath
-			}
-		} else {
-			return moerr.NewInternalError(ctx, "stage '%s' is not exists, please check", stageName)
 		}
 	}
 	return err
@@ -3440,6 +3366,11 @@ func doAlterStage(ctx context.Context, ses *Session, as *tree.AlterStage) (err e
 		}
 	} else {
 		if as.UrlOption.Exist {
+			if !(strings.HasPrefix(as.UrlOption.Url, function.STAGE_PROTOCOL+"://") ||
+				strings.HasPrefix(as.UrlOption.Url, function.S3_PROTOCOL+"://") ||
+				strings.HasPrefix(as.UrlOption.Url, function.FILE_PROTOCOL+":///")) {
+				return moerr.NewBadConfig(ctx, "URL protocol only supports stage://, s3:// and file:///")
+			}
 			sql = getsqlForUpdateStageUrl(string(as.Name), as.UrlOption.Url)
 			err = bh.Exec(ctx, sql)
 			if err != nil {
@@ -3448,7 +3379,7 @@ func doAlterStage(ctx context.Context, ses *Session, as *tree.AlterStage) (err e
 		}
 
 		if as.CredentialsOption.Exist {
-			credentials = HashPassWord(formatCredentials(as.CredentialsOption))
+			credentials = formatCredentials(as.CredentialsOption)
 			sql = getsqlForUpdateStageCredentials(string(as.Name), credentials)
 			err = bh.Exec(ctx, sql)
 			if err != nil {
