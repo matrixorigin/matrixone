acb9a8e0aee42374909792e5a909a51c4210bc83
Wei Ziran <weiziran125@gmail.com>
2024-08-23 18:26:23 +0800

    add retention daemon task

--- pkg/pb/task/task.go
+++ pkg/pb/task/task.go
@@ -62,6 +62,8 @@ func detailsType(d isDetails_Details) (TaskType, error) {
 	switch d := d.(type) {
 	case *Details_Connector:
 		return TaskType_TypeKafkaSinkConnector, nil
+	case *Details_Retention:
+		return TaskType_TypeRetention, nil
 	default:
 		return TaskType_TypeUnknown, moerr.NewInternalErrorNoCtx("Unknown details type: %T", d)
 	}
