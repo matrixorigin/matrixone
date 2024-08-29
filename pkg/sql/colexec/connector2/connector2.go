e759418707c2132b94b8a4db0a035dc553970adc
chenmingsong <mschenjj@foxmail.com>
2024-08-29 11:14:22 +0800

    fix sca

--- pkg/sql/colexec/connector2/connector2.go
+++ pkg/sql/colexec/connector2/connector2.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//package connector2
+package connector2
 //
 //import (
 //	"bytes"
--- pkg/sql/colexec/dispatch2/sendfunc.go
+++ pkg/sql/colexec/dispatch2/sendfunc.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//package dispatch2
+package dispatch2
 //
 //import (
 //	"context"
