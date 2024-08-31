1da0737033fce66d40f7d58a337d1fdc5f995ef7
badboynt1 <badboynt@126.com>
2024-08-31 11:39:32 +0800

    fix

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -1317,7 +1317,7 @@ func (c *Compile) getStepRegs(step int32) []*process.WaitRegister {
 }
 
 func (c *Compile) constructScopeForExternal(addr string, parallel bool) *Scope {
-	ds := newScope(Normal)
+	ds := newScope(Merge)
 	ds.NodeInfo = getEngineNode(c)
 	if parallel {
 		ds.Magic = Remote
@@ -1366,7 +1366,7 @@ func (c *Compile) compileSourceScan(n *plan.Node) ([]*Scope, error) {
 
 	currentFirstFlag := c.anal.isFirst
 	for i := range ss {
-		ss[i] = newScope(Normal)
+		ss[i] = newScope(Merge)
 		ss[i].NodeInfo = getEngineNode(c)
 		ss[i].Proc = c.proc.NewNoContextChildProc(0)
 		arg := constructStream(n, ps[i])
@@ -1550,7 +1550,7 @@ func (c *Compile) compileExternScan(n *plan.Node) ([]*Scope, error) {
 	}
 
 	if len(fileList) == 0 {
-		ret := newScope(Normal)
+		ret := newScope(Merge)
 		ret.NodeInfo = getEngineNode(c)
 		ret.NodeInfo.Mcpu = 1
 		ret.DataSource = &Source{isConst: true, node: n}
@@ -1733,7 +1733,7 @@ func (c *Compile) compileTableFunction(n *plan.Node, ss []*Scope) []*Scope {
 }
 
 func (c *Compile) compileValueScan(n *plan.Node) ([]*Scope, error) {
-	ds := newScope(Normal)
+	ds := newScope(Merge)
 	ds.NodeInfo = getEngineNode(c)
 	ds.DataSource = &Source{isConst: true, node: n}
 	ds.NodeInfo = engine.Node{Addr: c.addr, Mcpu: 1}
