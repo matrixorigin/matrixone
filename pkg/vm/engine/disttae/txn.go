b4216047b4bb2608013792e823d4bffc4a50bba9
XuPeng-SH <xupeng3112@163.com>
2024-08-31 16:35:59 +0800

    add engine option

--- pkg/vm/engine/disttae/txn.go
+++ pkg/vm/engine/disttae/txn.go
@@ -405,9 +405,6 @@ func (txn *Transaction) dumpBatchLocked(offset int) error {
 
 	//offset < 0 indicates commit.
 	if offset < 0 {
-		//if txn.workspaceSize < WorkspaceThreshold {
-		//	return nil
-		//}
 		if txn.workspaceSize < WorkspaceThreshold && txn.insertCount < InsertEntryThreshold {
 			return nil
 		}
