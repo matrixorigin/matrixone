686799d77ba4f679f5fdeb349bde0c3d4472b5ee
badboynt1 <badboynt@126.com>
2024-09-04 09:19:17 +0800

    fix

--- pkg/sql/compile/scope.go
+++ pkg/sql/compile/scope.go
@@ -613,51 +613,6 @@ func (s *Scope) isTableScan() bool {
 	return isTableScan
 }
 
-<<<<<<< HEAD
-func newParallelScope1(s *Scope, c *Compile) (*Scope, []*Scope) {
-	lenChannels := 0
-	if s.IsJoin {
-		lenChannels = 1
-	}
-
-	dispatchOp := s.RootOp
-	s.RootOp = dispatchOp.GetOperatorBase().GetChildren(0)
-	dispatchOp.GetOperatorBase().ResetChildren()
-	rs := newScope(Normal)
-	rs.NodeInfo = engine.Node{Addr: s.NodeInfo.Addr, Mcpu: 1}
-	rs.Proc = s.Proc.NewNoContextChildProc(1)
-	rs.Proc.Reg.MergeReceivers[0].Ch = make(chan *process.RegisterMessage, s.NodeInfo.Mcpu)
-	rs.Proc.Reg.MergeReceivers[0].NilBatchCnt = s.NodeInfo.Mcpu
-	mergeOp := merge.NewArgument()
-	rs.setRootOperator(mergeOp)
-	rs.setRootOperator(dispatchOp)
-
-	parallelScopes := make([]*Scope, s.NodeInfo.Mcpu)
-	for i := 0; i < s.NodeInfo.Mcpu; i++ {
-		parallelScopes[i] = newScope(Normal)
-		parallelScopes[i].NodeInfo = s.NodeInfo
-		parallelScopes[i].NodeInfo.Mcpu = 1
-		parallelScopes[i].Proc = s.Proc.NewContextChildProc(lenChannels)
-		parallelScopes[i].TxnOffset = s.TxnOffset
-		parallelScopes[i].setRootOperator(dupOperatorRecursively(s.RootOp, i, s.NodeInfo.Mcpu))
-		connArg := connector.NewArgument().WithReg(rs.Proc.Reg.MergeReceivers[0])
-		parallelScopes[i].setRootOperator(connArg)
-	}
-
-	rs.PreScopes = parallelScopes
-	s.PreScopes = append(s.PreScopes, rs)
-
-	// after parallelScope1
-	// s(fake)
-	//   |_ rs(merge->dispatch)
-	//   |     |_ parallelScpes
-	//   |
-	//   |_ prescopes
-	return rs, parallelScopes
-}
-
-=======
->>>>>>> 2832937ab7a895a0e91303ec08c5327152d9e289
 func newParallelScope(s *Scope, c *Compile) (*Scope, []*Scope) {
 	if s.NodeInfo.Mcpu == 1 {
 		return s, nil
