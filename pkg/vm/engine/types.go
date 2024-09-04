23935884ebb74a8f8e04762c1e7e3fd5d723d10b
triump2020 <triump2020@outlook.com>
2024-09-04 20:57:19 +0800

    update

--- pkg/vm/engine/types.go
+++ pkg/vm/engine/types.go
@@ -786,6 +786,19 @@ type Ranges interface {
 
 var _ Ranges = (*objectio.BlockInfoSlice)(nil)
 
+type ChangesHandle_Hint int
+
+const (
+	ChangesHandle_Snapshot ChangesHandle_Hint = iota
+	ChangesHandle_Tail_wip
+	ChangesHandle_Tail_done
+)
+
+type ChangesHandle interface {
+	Next(ctx context.Context, mp *mpool.MPool) (data *batch.Batch, tombstone *batch.Batch, hint ChangesHandle_Hint, err error)
+	Close() error
+}
+
 type Relation interface {
 	Statistics
 
