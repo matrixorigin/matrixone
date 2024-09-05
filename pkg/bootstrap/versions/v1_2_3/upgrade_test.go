8cd0070668bb85de6984c652c32c13339c8b320a
qingxinhome <758355272@qq.com>
2024-09-05 20:58:35 +0800

    add ut test case

--- pkg/bootstrap/versions/v1_2_3/upgrade_test.go
+++ pkg/bootstrap/versions/v1_2_3/upgrade_test.go
@@ -16,17 +16,20 @@ package v1_2_3
 
 import (
 	"context"
+	"strings"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 
+	"github.com/matrixorigin/matrixone/pkg/common/mpool"
 	"github.com/matrixorigin/matrixone/pkg/common/runtime"
+	"github.com/matrixorigin/matrixone/pkg/container/types"
 	mock_frontend "github.com/matrixorigin/matrixone/pkg/frontend/test"
 	"github.com/matrixorigin/matrixone/pkg/pb/txn"
 	"github.com/matrixorigin/matrixone/pkg/util/executor"
 )
 
-func Test_versionHandle_HandleTenantUpgrade(t *testing.T) {
+func Test_Upgrade(t *testing.T) {
 	sid := ""
 	runtime.RunTest(
 		sid,
@@ -56,6 +59,54 @@ func Test_versionHandle_HandleTenantUpgrade(t *testing.T) {
 			if err := Handler.HandleTenantUpgrade(context.Background(), 0, executor); err != nil {
 				t.Errorf("HandleTenantUpgrade() error = %v", err)
 			}
+		},
+	)
+}
+
+func Test_HandleTenantUpgrade(t *testing.T) {
+	sid := ""
+	runtime.RunTest(
+		sid,
+		func(rt runtime.Runtime) {
+			txnOperator := mock_frontend.NewMockTxnOperator(gomock.NewController(t))
+			txnOperator.EXPECT().TxnOptions().Return(txn.TxnOptions{CN: sid}).AnyTimes()
+
+			executor := executor.NewMemTxnExecutor(func(sql string) (executor.Result, error) {
+				if strings.HasPrefix(strings.ToLower(sql), strings.ToLower("SELECT")) {
+					typs := []types.Type{
+						types.New(types.T_varchar, 50, 0),
+						types.New(types.T_varchar, 50, 0),
+						types.New(types.T_int64, 64, 0),
+						types.New(types.T_int64, 64, 0),
+						types.New(types.T_int64, 64, 0),
+						types.New(types.T_int64, 64, 0),
+						types.New(types.T_int32, 32, 0),
+						types.New(types.T_varchar, 10, 0),
+						types.New(types.T_varchar, 10, 0),
+						types.New(types.T_varchar, 10, 0),
+					}
+
+					memRes := executor.NewMemResult(
+						typs,
+						mpool.MustNewZero())
+					memRes.NewBatch()
+					executor.AppendStringRows(memRes, 0, []string{"TIMESTAMP"})
+					executor.AppendStringRows(memRes, 1, []string{"YES"})
+					executor.AppendFixedRows(memRes, 2, []int64{64})
+					executor.AppendFixedRows(memRes, 3, []int64{0})
+					executor.AppendFixedRows(memRes, 4, []int64{0})
+					executor.AppendFixedRows(memRes, 5, []int64{0})
+					executor.AppendFixedRows(memRes, 6, []int32{0})
+					executor.AppendStringRows(memRes, 7, []string{""})
+					executor.AppendStringRows(memRes, 8, []string{""})
+					executor.AppendStringRows(memRes, 9, []string{""})
+					return memRes.GetResult(), nil
+				}
+				return executor.Result{}, nil
+			}, txnOperator)
+
+			upg_mo_snapshots.Upgrade(executor, uint32(0))
+
 		},
 	)
 }
