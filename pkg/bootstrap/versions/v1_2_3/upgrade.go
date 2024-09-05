5f65bc246b05c606deaacc1818f903fdefd35797
qingxinhome <758355272@qq.com>
2024-09-05 22:37:09 +0800

    Annotate unnecessary code

--- pkg/bootstrap/versions/v1_2_3/upgrade.go
+++ pkg/bootstrap/versions/v1_2_3/upgrade.go
@@ -60,23 +60,23 @@ func (v *versionHandle) HandleTenantUpgrade(
 	ctx context.Context,
 	tenantID int32,
 	txn executor.TxnExecutor) error {
-
-	for _, upgEntry := range tenantUpgEntries {
-		start := time.Now()
-
-		err := upgEntry.Upgrade(txn, uint32(tenantID))
-		if err != nil {
-			getLogger(txn.Txn().TxnOptions().CN).Error("tenant upgrade entry execute error", zap.Error(err), zap.Int32("tenantId", tenantID), zap.String("version", v.Metadata().Version), zap.String("upgrade entry", upgEntry.String()))
-			return err
-		}
-
-		duration := time.Since(start)
-		getLogger(txn.Txn().TxnOptions().CN).Info("tenant upgrade entry complete",
-			zap.String("upgrade entry", upgEntry.String()),
-			zap.Int64("time cost(ms)", duration.Milliseconds()),
-			zap.Int32("tenantId", tenantID),
-			zap.String("toVersion", v.Metadata().Version))
-	}
+	// TODO: If the cluster upgrade is not empty, open the following comment
+	//for _, upgEntry := range tenantUpgEntries {
+	//	start := time.Now()
+	//
+	//	err := upgEntry.Upgrade(txn, uint32(tenantID))
+	//	if err != nil {
+	//		getLogger(txn.Txn().TxnOptions().CN).Error("tenant upgrade entry execute error", zap.Error(err), zap.Int32("tenantId", tenantID), zap.String("version", v.Metadata().Version), zap.String("upgrade entry", upgEntry.String()))
+	//		return err
+	//	}
+	//
+	//	duration := time.Since(start)
+	//	getLogger(txn.Txn().TxnOptions().CN).Info("tenant upgrade entry complete",
+	//		zap.String("upgrade entry", upgEntry.String()),
+	//		zap.Int64("time cost(ms)", duration.Milliseconds()),
+	//		zap.Int32("tenantId", tenantID),
+	//		zap.String("toVersion", v.Metadata().Version))
+	//}
 
 	return nil
 }
