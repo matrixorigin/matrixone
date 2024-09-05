3df1d49c7878a27895187c676bcbadaef76456c8
Wei Ziran <weiziran125@gmail.com>
2024-09-05 15:00:41 +0800

    add ut test

--- pkg/vm/engine/tae/db/merge/config_test.go
+++ pkg/vm/engine/tae/db/merge/config_test.go
@@ -15,19 +15,45 @@
 package merge
 
 import (
+	"testing"
+
+	apipb "github.com/matrixorigin/matrixone/pkg/pb/api"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/catalog"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
+
+func newTestSchema(colCnt, pkIdx int, config *BasicPolicyConfig) *catalog.Schema {
+	schema := catalog.MockSchema(colCnt, pkIdx)
+	schema.Extra = &apipb.SchemaExtra{
+		MinOsizeQuailifed: config.ObjectMinOsize,
+		MaxObjOnerun:      uint32(config.MergeMaxOneRun),
+		MaxOsizeMergedObj: config.MaxOsizeMergedObj,
+		Hints:             config.MergeHints,
+		MinCnMergeSize:    config.MinCNMergeSize,
+	}
+	return schema
+}
 
 func TestString(t *testing.T) {
 	require.Equal(t, "minOsizeObj:110.00MB, maxOneRun:16, maxOsizeMergedObj: 128.00MB, offloadToCNSize:78.12GB, hints: []", defaultBasicConfig.String())
 }
 
 func TestConfigForTable(t *testing.T) {
-	tbl := catalog.MockStaloneTableEntry(0, catalog.MockSchema(1, 0))
+	tbl := catalog.MockStaloneTableEntry(0, newTestSchema(1, 0, defaultBasicConfig))
 	configProvider := newCustomConfigProvider()
 	defaultConfig := configProvider.getConfig(tbl)
-	require.Equal(t, defaultBasicConfig, defaultConfig)
+	require.Equal(t, defaultBasicConfig.MinCNMergeSize, defaultConfig.MinCNMergeSize)
+	require.Equal(t, defaultBasicConfig.MaxOsizeMergedObj, defaultConfig.MaxOsizeMergedObj)
+	require.Equal(t, defaultBasicConfig.MergeMaxOneRun, defaultConfig.MergeMaxOneRun)
+	require.Equal(t, defaultBasicConfig.ObjectMinOsize, defaultConfig.ObjectMinOsize)
+	require.Equal(t, defaultBasicConfig.MergeHints, defaultConfig.MergeHints)
 
+	tbl2 := catalog.MockStaloneTableEntry(0, newTestSchema(1, 0, &BasicPolicyConfig{}))
+	configProvider = newCustomConfigProvider()
+	config := configProvider.getConfig(tbl2)
+	require.Equal(t, defaultBasicConfig.MinCNMergeSize, config.MinCNMergeSize)
+	require.Equal(t, defaultBasicConfig.MaxOsizeMergedObj, config.MaxOsizeMergedObj)
+	require.Equal(t, defaultBasicConfig.MergeMaxOneRun, config.MergeMaxOneRun)
+	require.Equal(t, defaultBasicConfig.ObjectMinOsize, config.ObjectMinOsize)
+	require.Equal(t, defaultBasicConfig.MergeHints, config.MergeHints)
 }
