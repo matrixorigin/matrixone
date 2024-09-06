683afc921b0dac1bdf80ff5cbc58ee5c98e4fb3e
xzxiong <xzxiong@yeah.net>
2024-09-06 22:45:17 +0800

    fix result while enable merge

--- test/distributed/cases/zz_statement_query_type/aggr_error_stmt.sql
+++ test/distributed/cases/zz_statement_query_type/aggr_error_stmt.sql
@@ -2,5 +2,5 @@
 -- check result of statement_query_type/aggr_error_stmt.sql
 
 -- Tips: cnt +1000, sum + 1000 both make sure result format is current.
--- @ignore:3,4
-select statement, error, count(1) < sum(aggr_count) check_result, count(1) cnt, sum(aggr_count) sum from system.statement_info where account="bvt_aggr_error_stmt" and sql_source_type="cloud_nonuser_sql" group by `statement`, error;
+-- @ignore:2,3,4
+select error, count(1) < sum(aggr_count) check_result, count(1) cnt, sum(aggr_count) sum, statement from system.statement_info where account="bvt_aggr_error_stmt" and sql_source_type="cloud_nonuser_sql" group by `statement`, error;
