e875b362ac824e103f35464de8b2f6d873584ba5
reusee <reusee@gmail.com>
2024-09-08 01:34:21 +0800

    fileservice: optimize io merger (#18592)
    Optimize the io merger to skip if it does not read caches.
    
    Approved by: @sukki37

--- pkg/fileservice/s3_fs_test.go
+++ pkg/fileservice/s3_fs_test.go
@@ -26,6 +26,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -151,10 +152,11 @@ func testS3FS(
 		fs, err := NewS3FS(
 			ctx,
 			ObjectStorageArguments{
-				Name:     "s3",
-				Endpoint: config.Endpoint,
-				Bucket:   config.Bucket,
-				RoleARN:  config.RoleARN,
+				Name:      "s3",
+				Endpoint:  config.Endpoint,
+				Bucket:    config.Bucket,
+				RoleARN:   config.RoleARN,
+				KeyPrefix: time.Now().Format("2006-01-02.15:04:05.000000"),
 			},
 			DisabledCacheConfig,
 			nil,
@@ -165,8 +167,21 @@ func testS3FS(
 		var counterSet, counterSet2 perfcounter.CounterSet
 		ctx = perfcounter.WithCounterSet(ctx, &counterSet)
 		ctx = perfcounter.WithCounterSet(ctx, &counterSet2)
+
+		err = fs.Write(ctx, IOVector{
+			FilePath: "foo",
+			Entries: []IOEntry{
+				{
+					Data: []byte("foo"),
+					Size: 3,
+				},
+			},
+		})
+		assert.Nil(t, err)
+
 		entries, err := fs.List(ctx, "")
 		assert.Nil(t, err)
+
 		assert.True(t, len(entries) > 0)
 		assert.True(t, counterSet.FileService.S3.List.Load() > 0)
 		assert.True(t, counterSet2.FileService.S3.List.Load() > 0)
@@ -1092,4 +1107,67 @@ func TestNewS3NoDefaultCredential(t *testing.T) {
 	)
 	assert.True(t, moerr.IsMoErrCode(err, moerr.ErrInvalidInput))
 	assert.True(t, strings.Contains(err.Error(), "no valid credentials"))
+}
+
+func TestS3FSIOMerger(t *testing.T) {
+	fs, err := NewS3FS(
+		context.Background(),
+		ObjectStorageArguments{
+			Endpoint: "disk",
+			Bucket:   t.TempDir(),
+		},
+		CacheConfig{
+			MemoryCapacity: ptrTo(toml.ByteSize(1 << 20)),
+			DiskPath:       ptrTo(t.TempDir()),
+			DiskCapacity:   ptrTo(toml.ByteSize(10 * (1 << 20))),
+			CheckOverlaps:  false,
+		},
+		nil,
+		false,
+		false,
+	)
+	assert.Nil(t, err)
+	defer fs.Close()
+
+	var counterSet perfcounter.CounterSet
+	ctx := perfcounter.WithCounterSet(context.Background(), &counterSet)
+
+	err = fs.Write(ctx, IOVector{
+		FilePath: "foo",
+		Policy:   SkipDiskCache, // skip disk cache
+		Entries: []IOEntry{
+			{
+				Data: []byte("foo"),
+				Size: 3,
+			},
+		},
+	})
+	assert.Nil(t, err)
+
+	nThreads := 256
+	wg := new(sync.WaitGroup)
+	wg.Add(nThreads)
+	for range nThreads {
+		go func() {
+			defer wg.Done()
+			vec := &IOVector{
+				FilePath: "foo",
+				Entries: []IOEntry{
+					{
+						Size:        3,
+						ToCacheData: CacheOriginalData,
+					},
+				},
+			}
+			err := fs.Read(ctx, vec)
+			assert.Nil(t, err)
+			assert.Equal(t, []byte("foo"), vec.Entries[0].CachedData.Bytes())
+			vec.Release()
+		}()
+	}
+	wg.Wait()
+
+	assert.Equal(t, int64(1), counterSet.FileService.S3.Put.Load())
+	assert.True(t, counterSet.FileService.S3.Get.Load() < int64(nThreads))
+
 }
