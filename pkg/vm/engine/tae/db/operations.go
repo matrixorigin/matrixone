49dd70cd56cf0dbef6753e1ce2dfcaff018f5b5c
gouhongshen <gouhongshen@hotmail.com>
2024-09-08 18:44:38 +0800

    adding object count metric for accounts. (#18363)
    adding object count metric for accounts.
    
    Approved by: @daviszhen, @aptend, @heni02, @XuPeng-SH, @zhangxu19830126, @qingxinhome, @xzxiong

--- pkg/vm/engine/tae/db/operations.go
+++ pkg/vm/engine/tae/db/operations.go
@@ -285,17 +285,36 @@ type StorageUsageResp_V0 struct {
 	BlockEntries []*BlockMetaInfo
 }
 
-type StorageUsageResp struct {
+type StorageUsageResp_V1 struct {
 	Succeed bool
 	AccIds  []int64
 	Sizes   []uint64
 	Magic   uint64
 }
 
-func (s *StorageUsageResp) MarshalBinary() ([]byte, error) {
+func (s *StorageUsageResp_V1) MarshalBinary() ([]byte, error) {
 	return s.Marshal()
 }
 
-func (s *StorageUsageResp) UnmarshalBinary(data []byte) error {
+func (s *StorageUsageResp_V1) UnmarshalBinary(data []byte) error {
+	return s.Unmarshal(data)
+}
+
+type StorageUsageResp_V2 struct {
+	Succeed bool
+	AccIds  []int64
+	Sizes   []uint64
+	Magic   uint64
+
+	ObjCnts []uint64
+	BlkCnts []uint64
+	RowCnts []uint64
+}
+
+func (s *StorageUsageResp_V2) MarshalBinary() ([]byte, error) {
+	return s.Marshal()
+}
+
+func (s *StorageUsageResp_V2) UnmarshalBinary(data []byte) error {
 	return s.Unmarshal(data)
 }
