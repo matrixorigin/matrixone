48988380f40a7583450223abab14d8b873bc8b99
Ariznawl@163.com <ariznawl@163.com>
2024-09-10 15:13:01 +0800

    add case

--- test/distributed/cases/ddl/test.sql
+++ test/distributed/cases/ddl/test.sql
@@ -99,3 +99,103 @@ select mo_ctl('cn', 'task', ':retention');
 select sleep(1);
 -- @ignore:2
 select * from mo_catalog.mo_retention;
+
+
+
+
+-- create view depends on retention table
+drop database if exists test01;
+create database test01;
+use test01;
+drop table if exists retention06;
+create table retention06 (col1 int, col2 decimal(6), col3 varchar(30)) with retention period 1 second;
+insert into retention06 values (1, null, 'database');
+insert into retention06 values (2, 38291.32132, 'database');
+insert into retention06 values (3, null, 'database management system');
+select count(*) from retention06;
+show create table retention06;
+drop view if exists v01;
+create view v01 as select * from retention06;
+select * from v01;
+-- @ignore:2
+select * from mo_catalog.mo_retention;
+select sleep(2);
+-- @ignore:0
+select mo_ctl('cn', 'task', ':retention');
+select sleep(2);
+-- @ignore:2
+select * from mo_catalog.mo_retention;
+show tables;
+select * from v01;
+drop view v01;
+
+
+
+
+-- insert into select
+drop table if exists retention08;
+create table retention08(t1 time,t2 time,t3 time) with retention period 1 second;
+insert into retention08 values("-838:59:59.0000","838:59:59.00","22:00:00");
+insert into retention08 values("0:00:00.0000","0","0:00");
+insert into retention08 values(null,NULL,null);
+insert into retention08 values("23","1122","-1122");
+
+drop table if exists retention09;
+create table retention09(t1 time,t2 time,t3 time);
+insert into retention09 select * from retention08;
+-- @ignore:2
+select * from mo_catalog.mo_retention;
+select sleep(2);
+-- @ignore:0
+select mo_ctl('cn', 'task', ':retention');
+select sleep(2);
+-- @ignore:2
+select * from mo_catalog.mo_retention;
+show tables;
+select * from retention08;
+select * from retention09;
+drop table retention09;
+
+
+
+
+-- fk table, pri table is retention table
+-- @bvt:issue#18647
+drop table if exists aff01;
+drop table if exists pri01;
+create table pri01(a int primary key, b int unique key) with retention period 2 second;
+create table aff01 (a int, b int, foreign key f_a(a) references pri01(a));
+insert into pri01 values (1,1), (2,2), (3,3);
+insert into aff01 values (1,1), (2,2), (3,3);
+-- @ignore:2
+select * from mo_catalog.mo_retention;
+select sleep(2);
+-- @ignore:0
+select mo_ctl('cn', 'task', ':retention');
+select sleep(1);
+-- @ignore:2
+select * from mo_catalog.mo_retention;
+show tables;
+-- @bvt:issue
+
+
+
+
+-- fk table, aff table is retention table
+drop table if exists aff01;
+drop table if exists pri01;
+create table pri01(a int primary key, b int unique key);
+create table aff01 (a int, b int, foreign key f_a(a) references pri01(a))  with retention period 2 second;
+insert into pri01 values (1,1), (2,2), (3,3);
+insert into aff01 values (1,1), (2,2), (3,3);
+-- @ignore:2
+select * from mo_catalog.mo_retention;
+select sleep(2);
+-- @ignore:0
+select mo_ctl('cn', 'task', ':retention');
+select sleep(1);
+-- @ignore:2
+select * from mo_catalog.mo_retention;
+show tables;
+select * from pri01;
+drop table pri01;
