43b30b9f62dd7c0161362eb87bead5aac54ece21
XuPeng-SH <xupeng3112@163.com>
2024-09-11 09:11:47 +0800

    update 1

---
+++ pkg/objectio/meta_entry.go
@@ -0,0 +1,65 @@
+// Copyright 2021 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package objectio
+
+type ObjectMeta []byte
+
+func (e ObjectMeta) MustGetMeta(metaType DataMetaType) ObjectDataMeta {
+	return objectMetaV3(e[IOEntryHeaderSize:]).MustGetMeta(metaType)
+}
+
+func (e ObjectMeta) HeaderLength() uint32 {
+	return objectMetaV3(e[IOEntryHeaderSize:]).HeaderLength()
+}
+
+func (e ObjectMeta) DataMetaCount() uint16 {
+	return objectMetaV3(e[IOEntryHeaderSize:]).DataMetaCount()
+}
+
+func (e ObjectMeta) TombstoneMetaCount() uint16 {
+	return objectMetaV3(e[IOEntryHeaderSize:]).TombstoneMetaCount()
+}
+
+func (e ObjectMeta) DataMeta() (ObjectDataMeta, bool) {
+	return objectMetaV3(e[IOEntryHeaderSize:]).DataMeta()
+}
+
+func (e ObjectMeta) MustDataMeta() ObjectDataMeta {
+	return objectMetaV3(e[IOEntryHeaderSize:]).MustDataMeta()
+}
+
+func (e ObjectMeta) TombstoneMeta() (ObjectDataMeta, bool) {
+	return objectMetaV3(e[IOEntryHeaderSize:]).TombstoneMeta()
+}
+
+func (e ObjectMeta) MustTombstoneMeta() ObjectDataMeta {
+	return objectMetaV3(e[IOEntryHeaderSize:]).MustTombstoneMeta()
+}
+
+func (e ObjectMeta) SubMeta(pos uint16) (ObjectDataMeta, bool) {
+	return objectMetaV3(e[IOEntryHeaderSize:]).SubMeta(pos)
+}
+
+func (e ObjectMeta) SubMetaCount() uint16 {
+	return objectMetaV3(e[IOEntryHeaderSize:]).SubMetaCount()
+}
+
+func (e ObjectMeta) SubMetaIndex() SubMetaIndex {
+	return objectMetaV3(e[IOEntryHeaderSize:]).SubMetaIndex()
+}
+
+func (e ObjectMeta) SubMetaTypes() []uint16 {
+	return objectMetaV3(e[IOEntryHeaderSize:]).SubMetaTypes()
+}
