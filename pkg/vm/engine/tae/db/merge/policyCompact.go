344a540ce470173d63b1026b75194b0f137d2470
Wei Ziran <weiziran125@gmail.com>
2024-09-11 14:08:24 +0800

    refactor compact

--- pkg/vm/engine/tae/db/merge/policyCompact.go
+++ pkg/vm/engine/tae/db/merge/policyCompact.go
@@ -2,11 +2,13 @@ package merge
 
 import (
 	"context"
-	"fmt"
+
 	"github.com/matrixorigin/matrixone/pkg/fileservice"
+	"github.com/matrixorigin/matrixone/pkg/logutil"
 	"github.com/matrixorigin/matrixone/pkg/objectio"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/blockio"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/catalog"
+	"go.uber.org/zap"
 )
 
 type objCompactPolicy struct {
@@ -23,21 +25,15 @@ func newObjCompactPolicy(fs fileservice.FileService) *objCompactPolicy {
 }
 
 func (o *objCompactPolicy) onObject(entry *catalog.ObjectEntry, config *BasicPolicyConfig) bool {
+	if o.tblEntry == nil {
+		return false
+	}
 	if entry.IsTombstone {
 		return false
 	}
 
 	if entry.OriginSize() < config.ObjectMinOsize {
 		return false
-	}
-
-	o.objects = append(o.objects, entry)
-	return true
-}
-
-func (o *objCompactPolicy) revise(cpu, mem int64, config *BasicPolicyConfig) []reviseResult {
-	if o.tblEntry == nil {
-		return nil
 	}
 
 	tIter := o.tblEntry.MakeTombstoneObjectIt()
@@ -55,25 +51,40 @@ func (o *objCompactPolicy) revise(cpu, mem int64, config *BasicPolicyConfig) []r
 
 		tombstoneStats = append(tombstoneStats, tIter.Item().GetObjectStats())
 	}
-	results := make([]reviseResult, 0)
+	if len(tombstoneStats) == 0 {
+		return false
+	}
 
+	sels, err := blockio.FindTombstonesOfObject(context.TODO(), *entry.ID(), tombstoneStats, o.fs)
+	if err != nil {
+		return false
+	}
+	iter := sels.Iterator()
+	deletedRows := uint32(0)
+	for iter.HasNext() {
+		deletedRows += tombstoneStats[iter.Next()].Rows()
+	}
+	rows := entry.Rows()
+	logutil.Info("[MERGE-POLICY-REVISE]",
+		zap.String("policy", "compact"),
+		zap.String("data object", entry.String()),
+		zap.Uint32("data rows", rows),
+		zap.Uint32("tombstone rows", deletedRows),
+	)
+	if deletedRows > rows {
+		o.objects = append(o.objects, entry)
+		return true
+	}
+	return false
+}
+
+func (o *objCompactPolicy) revise(cpu, mem int64, config *BasicPolicyConfig) []reviseResult {
+	if o.tblEntry == nil {
+		return nil
+	}
+	results := make([]reviseResult, 0, len(o.objects))
 	for _, obj := range o.objects {
-		deletedRows := uint32(0)
-		sels, err := blockio.FindTombstonesOfObject(context.TODO(), *obj.ID(), tombstoneStats, o.fs)
-		if err != nil {
-			return nil
-		}
-		iter := sels.Iterator()
-		for iter.HasNext() {
-			statsOffset := iter.Next()
-			tombstoneStat := tombstoneStats[statsOffset]
-			deletedRows += tombstoneStat.Rows()
-		}
-		rows := obj.Rows()
-		fmt.Println(rows, deletedRows)
-		if deletedRows > rows {
-			results = append(results, reviseResult{[]*catalog.ObjectEntry{obj}, TaskHostDN})
-		}
+		results = append(results, reviseResult{[]*catalog.ObjectEntry{obj}, TaskHostDN})
 	}
 	return results
 }
