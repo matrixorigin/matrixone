21ddbde368166fb3eef19404483be5d37a6de1fd
YANGGMM <18916107305@163.com>
2024-09-11 10:18:27 +0800

    add bvt test

--- test/distributed/cases/system/system.sql
+++ test/distributed/cases/system/system.sql
@@ -1,3 +1,52 @@
 select mo_cpu("total") >= mo_cpu("available");
 select mo_memory("total") >= mo_memory("available");
 select * from information_schema.files limit 1;
+
+use system;
+show tables;
+select * from statement_info limit 0;
+explain select * from statement_info limit 0;
+select * from sql_statement_hotspot limit 0;
+explain select * from sql_statement_hotspot limit 0;
+select * from span_info limit 0;
+explain select * from span_info limit 0;
+select * from rawlog limit 0;
+explain select * from rawlog limit 0;
+select * from log_info limit 0;
+explain select * from log_info limit 0;
+select * from error_info limit 0;
+explain select * from error_info limit 0;
+
+create database db01;
+use db01;
+create table table01 (id int);
+insert into table01 values(1);
+select * from table01;
+select * from table01 limit 0;
+create table table02 (id int);
+insert into table02 select * from table01 limit 0;
+(select * from table01 limit 0) union all (select * from table02 limit 0);
+
+drop account if exists acc01;
+create account acc01 admin_name = 'test_account' identified by '111';
+-- @session:id=1&user=acc01:test_account&password=111
+use system;
+show tables;
+select * from statement_info limit 0;
+explain select * from statement_info limit 0;
+create database db01;
+use db01;
+create table table01 (id int);
+insert into table01 values(1);
+select * from table01;
+select * from table01 limit 0;
+create table table02 (id int);
+insert into table02 select * from table01 limit 0;
+(select * from table01 limit 0) union all (select * from table02 limit 0);
+-- @session
+select * from system.statement_info limit 0;
+explain select * from system.statement_info limit 0;
+
+select * from db01.table01 limit 0;
+drop database db01;
+drop account if exists acc01;
