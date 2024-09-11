1ff0b621e4897b3478b4ef4567f2371ce0d24672
Wei Ziran <weiziran125@gmail.com>
2024-09-11 15:20:55 +0800

    add deleted rows check

--- pkg/vm/engine/tae/db/merge/policyCompact.go
+++ pkg/vm/engine/tae/db/merge/policyCompact.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"go.uber.org/zap"
 
+	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/fileservice"
 	"github.com/matrixorigin/matrixone/pkg/logutil"
 	"github.com/matrixorigin/matrixone/pkg/objectio"
@@ -61,16 +62,20 @@ func (o *objCompactPolicy) onObject(entry *catalog.ObjectEntry, config *BasicPol
 	}
 	iter := sels.Iterator()
 	deletedRows := uint32(0)
+	tombstoneRows := uint32(0)
 	for iter.HasNext() {
-		deletedRows += tombstoneStats[iter.Next()].Rows()
+		i := iter.Next()
+		deletedRows += o.countRowsInOneTombstoneForOneObject(entry, tombstoneStats[i])
+		tombstoneRows += tombstoneStats[i].Rows()
 	}
 	rows := entry.Rows()
-	if deletedRows > rows {
+	if deletedRows > rows/2 || tombstoneRows > rows*10 {
 		logutil.Info("[MERGE-POLICY-REVISE]",
 			zap.String("policy", "compact"),
 			zap.String("data object", entry.String()),
 			zap.Uint32("data rows", rows),
-			zap.Uint32("tombstone rows", deletedRows),
+			zap.Uint32("deleted rows", deletedRows),
+			zap.Uint32("tombstone rows", tombstoneRows),
 		)
 		o.objects = append(o.objects, entry)
 		return true
@@ -92,4 +97,42 @@ func (o *objCompactPolicy) revise(cpu, mem int64, config *BasicPolicyConfig) []r
 func (o *objCompactPolicy) resetForTable(entry *catalog.TableEntry) {
 	o.tblEntry = entry
 	o.objects = o.objects[:0]
+}
+
+func (o *objCompactPolicy) countRowsInOneTombstoneForOneObject(entry *catalog.ObjectEntry, stats objectio.ObjectStats) uint32 {
+	count := uint32(0)
+	for i := 0; i < int(stats.BlkCnt()); i++ {
+		loc := stats.BlockLocation(uint16(i), o.tblEntry.GetLastestSchema(true).BlockMaxRows)
+		c, err := o.countRowsInOneTombstoneBlockForOneObject(context.TODO(), entry, loc)
+		if err != nil {
+			return 0
+		}
+		count += c
+	}
+	return count
+}
+
+func (o *objCompactPolicy) countRowsInOneTombstoneBlockForOneObject(ctx context.Context, entry *catalog.ObjectEntry, loc objectio.Location) (uint32, error) {
+	count := uint32(0)
+	vectors, closeFunc, err := blockio.LoadColumns2(
+		ctx, []uint16{0, 1}, nil, o.fs, loc,
+		fileservice.Policy(0), false, nil,
+	)
+	defer closeFunc()
+	if err != nil {
+		logutil.Warn("[MERGE-POLICY-REVISE] failed to load tombstone columns",
+			zap.String("policy", "compact"),
+			zap.Error(err),
+		)
+		return 0, err
+	}
+	for j := range vectors[0].Length() {
+		rowID := vectors[0].Get(j).(types.Rowid)
+		blkID2, _ := rowID.Decode()
+		if *blkID2.Object() != *entry.ID() {
+			continue
+		}
+		count++
+	}
+	return count, nil
 }
