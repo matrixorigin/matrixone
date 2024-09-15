894c4282cca9c3fd820548b98ab6b4941a9af5ea
XuPeng-SH <xupeng3112@163.com>
2024-09-15 19:57:53 +0800

    Improve incremental dedup (#18813)
    1. remove some apis
    2. reduce heap allocation
    
    Approved by: @LeftHandCold

--- pkg/vm/engine/tae/catalog/object.go
+++ pkg/vm/engine/tae/catalog/object.go
@@ -16,7 +16,6 @@ package catalog
 
 import (
 	"bytes"
-	"context"
 	"fmt"
 	"sync/atomic"
 	"time"
@@ -28,7 +27,6 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/common"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/iface/data"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/iface/txnif"
-	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/index"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/txn/txnbase"
 )
 
@@ -59,29 +57,9 @@ func (entry *ObjectEntry) GetCreatedAt() types.TS {
 	return entry.CreatedAt
 }
 func (entry *ObjectEntry) GetLoaded() bool {
-	stats := entry.GetObjectStats()
-	return stats.Rows() != 0
+	return entry.Rows() != 0
 }
 
-func (entry *ObjectEntry) GetSortKeyZonemap() index.ZM {
-	stats := entry.GetObjectStats()
-	return stats.SortKeyZoneMap()
-}
-
-func (entry *ObjectEntry) GetRows() int {
-	stats := entry.GetObjectStats()
-	return int(stats.Rows())
-}
-
-func (entry *ObjectEntry) GetOriginSize() int {
-	stats := entry.GetObjectStats()
-	return int(stats.OriginSize())
-}
-
-func (entry *ObjectEntry) GetCompSize() int {
-	stats := entry.GetObjectStats()
-	return int(stats.Size())
-}
 func (entry *ObjectEntry) GetLastMVCCNode() *txnbase.TxnMVCCNode {
 	if !entry.DeleteNode.Start.IsEmpty() {
 		return &entry.DeleteNode
@@ -226,7 +204,7 @@ func (entry *ObjectEntry) PrepareRollback() (err error) {
 
 func (entry *ObjectEntry) StatsString(zonemapKind common.ZonemapPrintKind) string {
 	zonemapStr := "nil"
-	if z := entry.GetSortKeyZonemap(); z != nil {
+	if z := entry.SortKeyZoneMap(); z != nil {
 		switch zonemapKind {
 		case common.ZonemapPrintKindNormal:
 			zonemapStr = z.String()
@@ -239,9 +217,9 @@ func (entry *ObjectEntry) StatsString(zonemapKind common.ZonemapPrintKind) strin
 	return fmt.Sprintf(
 		"loaded:%t, oSize:%s, cSzie:%s rows:%d, zm: %s",
 		entry.GetLoaded(),
-		common.HumanReadableBytes(entry.GetOriginSize()),
-		common.HumanReadableBytes(entry.GetCompSize()),
-		entry.GetRows(),
+		common.HumanReadableBytes(int(entry.OriginSize())),
+		common.HumanReadableBytes(int(entry.Size())),
+		entry.Rows(),
 		zonemapStr,
 	)
 }
@@ -314,8 +292,8 @@ func (entry *ObjectEntry) HasPersistedData() bool {
 	return entry.ObjectPersisted()
 }
 func (entry *ObjectEntry) GetObjectData() data.Object { return entry.objData }
-func (entry *ObjectEntry) GetObjectStats() (stats objectio.ObjectStats) {
-	return entry.ObjectStats
+func (entry *ObjectEntry) GetObjectStats() (stats *objectio.ObjectStats) {
+	return &entry.ObjectStats
 }
 
 func (entry *ObjectEntry) Less(b *ObjectEntry) bool {
@@ -515,19 +493,6 @@ func (entry *ObjectEntry) HasCommittedPersistedData() bool {
 	} else {
 		return entry.IsCommitted()
 	}
-}
-func (entry *ObjectEntry) MustGetObjectStats() (objectio.ObjectStats, error) {
-	return entry.GetObjectStats(), nil
-}
-
-func (entry *ObjectEntry) GetPKZoneMap(
-	ctx context.Context,
-) (zm index.ZM, err error) {
-	stats, err := entry.MustGetObjectStats()
-	if err != nil {
-		return
-	}
-	return stats.SortKeyZoneMap(), nil
 }
 
 // TODO: REMOVEME
