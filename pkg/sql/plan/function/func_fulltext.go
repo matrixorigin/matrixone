702277abb9c113426ae021e42cf318fdb407a6ed
cpegeric <cpegeric@gmail.com>
2024-09-19 13:59:08 +0100

    non-index fulltext_match function not implemented error

--- pkg/sql/plan/function/func_fulltext.go
+++ pkg/sql/plan/function/func_fulltext.go
@@ -16,86 +16,90 @@ package function
 
 import (
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
-	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
-	"github.com/matrixorigin/matrixone/pkg/logutil"
 	"github.com/matrixorigin/matrixone/pkg/vm/process"
 )
 
 func fullTextMatch(ivecs []*vector.Vector, result vector.FunctionResultWrapper, proc *process.Process, length int, selectList *FunctionSelectList) error {
-	p1 := vector.GenerateFunctionStrParameter(ivecs[0])
-	p2 := vector.GenerateFunctionFixedTypeParameter[int64](ivecs[1])
-	rs := vector.MustFunctionResult[bool](result)
+	return moerr.NewNotSupported(proc.Ctx, "function fulltext_match not supported")
 
-	if !ivecs[0].IsConst() || ivecs[0].IsConstNull() {
-		return moerr.NewInvalidArg(proc.Ctx, "fullTextMatch search pattern", "not scalar")
-	}
+	/*
+		p1 := vector.GenerateFunctionStrParameter(ivecs[0])
+		p2 := vector.GenerateFunctionFixedTypeParameter[int64](ivecs[1])
+		rs := vector.MustFunctionResult[bool](result)
 
-	if !ivecs[1].IsConst() || ivecs[1].IsConstNull() {
-		return moerr.NewInvalidArg(proc.Ctx, "fullTextMatch search mode", "not scalar")
-	}
-
-	// pattern
-	pattern, _ := p1.GetStrValue(0)
-	mode, _ := p2.GetValue(0)
-
-	logutil.Infof("pattern %s, mode %d", pattern, mode)
-	ncols := len(ivecs)
-
-	recs := make([]vector.FunctionParameterWrapper[types.Varlena], ncols-2)
-
-	for i := 2; i < ncols; i++ {
-		recs[i-2] = vector.GenerateFunctionStrParameter(ivecs[i])
-	}
-
-	for i := uint64(0); i < uint64(length); i++ {
-		for j := 0; j < len(recs); j++ {
-			s, null := recs[j].GetStrValue(i)
-			logutil.Infof("[%d] col %d str=%s, null=%d", i, j, s, null)
+		if !ivecs[0].IsConst() || ivecs[0].IsConstNull() {
+			return moerr.NewInvalidArg(proc.Ctx, "fullTextMatch search pattern", "not scalar")
 		}
 
-		rs.Append(true, false)
-	}
+		if !ivecs[1].IsConst() || ivecs[1].IsConstNull() {
+			return moerr.NewInvalidArg(proc.Ctx, "fullTextMatch search mode", "not scalar")
+		}
 
-	return nil
-	//return moerr.NewNotSupported(proc.Ctx, "function fulltext_match (one key) not supported")
+		// pattern
+		pattern, _ := p1.GetStrValue(0)
+		mode, _ := p2.GetValue(0)
+
+		logutil.Infof("pattern %s, mode %d", pattern, mode)
+		ncols := len(ivecs)
+
+		recs := make([]vector.FunctionParameterWrapper[types.Varlena], ncols-2)
+
+		for i := 2; i < ncols; i++ {
+			recs[i-2] = vector.GenerateFunctionStrParameter(ivecs[i])
+		}
+
+		for i := uint64(0); i < uint64(length); i++ {
+			for j := 0; j < len(recs); j++ {
+				s, null := recs[j].GetStrValue(i)
+				logutil.Infof("[%d] col %d str=%s, null=%d", i, j, s, null)
+			}
+
+			rs.Append(true, false)
+		}
+
+		return nil
+	*/
 }
 
 func fullTextMatchScore(ivecs []*vector.Vector, result vector.FunctionResultWrapper, proc *process.Process, length int, selectList *FunctionSelectList) error {
-	p1 := vector.GenerateFunctionStrParameter(ivecs[0])
-	p2 := vector.GenerateFunctionFixedTypeParameter[int64](ivecs[1])
-	rs := vector.MustFunctionResult[float32](result)
+	return moerr.NewNotSupported(proc.Ctx, "function fulltext_match_score not supported")
 
-	if !ivecs[0].IsConst() || ivecs[0].IsConstNull() {
-		return moerr.NewInvalidArg(proc.Ctx, "fullTextMatch search pattern", "not scalar")
-	}
+	/*
+		p1 := vector.GenerateFunctionStrParameter(ivecs[0])
+		p2 := vector.GenerateFunctionFixedTypeParameter[int64](ivecs[1])
+		rs := vector.MustFunctionResult[float32](result)
 
-	if !ivecs[1].IsConst() || ivecs[1].IsConstNull() {
-		return moerr.NewInvalidArg(proc.Ctx, "fullTextMatch search mode", "not scalar")
-	}
-
-	// pattern
-	pattern, _ := p1.GetStrValue(0)
-	mode, _ := p2.GetValue(0)
-
-	logutil.Infof("pattern %s, mode %d", pattern, mode)
-	ncols := len(ivecs)
-
-	recs := make([]vector.FunctionParameterWrapper[types.Varlena], ncols-2)
-
-	for i := 2; i < ncols; i++ {
-		recs[i-2] = vector.GenerateFunctionStrParameter(ivecs[i])
-	}
-
-	for i := uint64(0); i < uint64(length); i++ {
-		for j := 0; j < len(recs); j++ {
-			s, null := recs[j].GetStrValue(i)
-			logutil.Infof("[%d] col %d str=%s, null=%d", i, j, s, null)
+		if !ivecs[0].IsConst() || ivecs[0].IsConstNull() {
+			return moerr.NewInvalidArg(proc.Ctx, "fullTextMatch search pattern", "not scalar")
 		}
 
-		rs.Append(float32(1.33), false)
-	}
+		if !ivecs[1].IsConst() || ivecs[1].IsConstNull() {
+			return moerr.NewInvalidArg(proc.Ctx, "fullTextMatch search mode", "not scalar")
+		}
 
-	return nil
-	//return moerr.NewNotSupported(proc.Ctx, "function fulltext_match (one key) not supported")
+		// pattern
+		pattern, _ := p1.GetStrValue(0)
+		mode, _ := p2.GetValue(0)
+
+		logutil.Infof("pattern %s, mode %d", pattern, mode)
+		ncols := len(ivecs)
+
+		recs := make([]vector.FunctionParameterWrapper[types.Varlena], ncols-2)
+
+		for i := 2; i < ncols; i++ {
+			recs[i-2] = vector.GenerateFunctionStrParameter(ivecs[i])
+		}
+
+		for i := uint64(0); i < uint64(length); i++ {
+			for j := 0; j < len(recs); j++ {
+				s, null := recs[j].GetStrValue(i)
+				logutil.Infof("[%d] col %d str=%s, null=%d", i, j, s, null)
+			}
+
+			rs.Append(float32(1.33), false)
+		}
+
+		return nil
+	*/
 }
