6fadfdd8b98a764762708e89e319c55229ad980e
bRong Njam <longran1989@gmail.com>
2024-09-20 15:59:58 +0800

    fix ut

--- pkg/sql/plan/explain/explain_node.go
+++ pkg/sql/plan/explain/explain_node.go
@@ -144,6 +144,8 @@ func (ndesc *NodeDescribeImpl) GetNodeBasicInfo(ctx context.Context, options *Ex
 		pname = "Lock"
 	case plan.Node_APPLY:
 		pname = "CROSS APPLY"
+	case plan.Node_MULTI_UPDATE:
+		pname = "Multi Update"
 	default:
 		panic("error node type")
 	}
