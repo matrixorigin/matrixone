6d4b762e57b98fa08811e4ac34b4f45360a22603
Jackson <xzxiong@yeah.net>
2024-09-24 10:34:59 +0800

    ignore system, system_metics schema check in 1.3

--- test/distributed/cases/log/show_table.sql
+++ test/distributed/cases/log/show_table.sql
@@ -1,4 +1,6 @@
 -- check system / system_metrics in sys account
+-- @bvt:issue#14293
 SHOW CREATE TABLE system_metrics.metric;
 SHOW CREATE TABLE system.statement_info;
+-- @bvt:issue
 SHOW CREATE TABLE system.rawlog;
