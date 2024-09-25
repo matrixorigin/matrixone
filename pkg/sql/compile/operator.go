a8db9981f337b06505140800bc08bc6bf8d29325
badboynt1 <badboynt@126.com>
2024-09-25 10:52:05 +0800

    fix

--- pkg/sql/compile/operator.go
+++ pkg/sql/compile/operator.go
@@ -17,6 +17,7 @@ package compile
 import (
 	"context"
 	"fmt"
+
 	"github.com/matrixorigin/matrixone/pkg/common/bitmap"
 
 	"github.com/matrixorigin/matrixone/pkg/vm/message"
@@ -450,11 +451,13 @@ func dupOperator(sourceOp vm.Operator, index int, maxParallel int) vm.Operator {
 		return op
 	case vm.Shuffle:
 		sourceArg := sourceOp.(*shuffle.Shuffle)
-		if sourceArg.GetShufflePool() == nil {
+		op := shuffle.NewArgument()
+
+		if index%8 == 0 { // 8 shuffle operator share one shufflepool
 			sourceArg.SetShufflePool(shuffle.NewShufflePool(sourceArg.BucketNum))
 		}
-		op := shuffle.NewArgument()
 		op.SetShufflePool(sourceArg.GetShufflePool())
+
 		op.ShuffleType = sourceArg.ShuffleType
 		op.ShuffleColIdx = sourceArg.ShuffleColIdx
 		op.ShuffleColMax = sourceArg.ShuffleColMax
