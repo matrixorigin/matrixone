a7b253d5d28292983c558706e78ccd9d2b28cb34
GreatRiver <great.river703@gmail.com>
2024-09-26 15:29:50 +0800

    code done

--- pkg/vm/engine/tae/db/gc/v3/table.go
+++ pkg/vm/engine/tae/db/gc/v3/table.go
@@ -18,6 +18,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"github.com/matrixorigin/matrixone/pkg/common/malloc"
 	"sync"
 
 	"github.com/matrixorigin/matrixone/pkg/common/bitmap"
@@ -145,7 +146,7 @@ func (t *GCTable) SoftGC(
 		sels []int64
 	)
 
-	sinker := t.getSinker()
+	sinker := t.getSinker(64 * malloc.MB)
 	defer sinker.Close()
 
 	processSoftGCBatch := func(
@@ -176,7 +177,7 @@ func (t *GCTable) SoftGC(
 		data.Shrink(sels, true)
 		return sinker.Write(ctx, tmpBat)
 	}
-	softGCSinker := t.getSinker()
+	softGCSinker := t.getSinker(0)
 	defer softGCSinker.Close()
 	err := t.insertFlow(ctx, softGCSinker, t.LoadBatchData, processSoftGCBatch)
 	if err != nil {
@@ -301,6 +302,8 @@ func (t *GCTable) SoftGC(
 
 	gcStats, _ := softGCSinker.GetResult()
 	err = t.doneAllBatches(ctx, &t.tsRange.start, &t.tsRange.end, gcStats)
+	t.files.stats = t.files.stats[0:]
+	t.files.stats = append(t.files.stats, gcStats...)
 	return gc, snapList, err
 }
 
@@ -335,7 +338,7 @@ func isSnapshotRefers(createTS, dropTS *types.TS, snapVec []types.TS, name strin
 	return false
 }
 
-func (t *GCTable) getSinker() *engine_util.Sinker {
+func (t *GCTable) getSinker(tailSize int) *engine_util.Sinker {
 	return engine_util.NewSinker(
 		ObjectTablePrimaryKeyIdx,
 		ObjectTableAttrs,
@@ -343,6 +346,7 @@ func (t *GCTable) getSinker() *engine_util.Sinker {
 		FSinkerFactory,
 		t.mp,
 		t.fs,
+		engine_util.WithTailSizeCap(tailSize),
 	)
 }
 
@@ -381,7 +385,7 @@ func (t *GCTable) Process(
 	loadNextBatch func(context.Context, *batch.Batch, *mpool.MPool) (bool, error),
 	processOneBatch func(context.Context, *batch.Batch) error,
 ) error {
-	sinker := t.getSinker()
+	sinker := t.getSinker(0)
 	defer sinker.Close()
 	err := t.insertFlow(ctx, sinker, loadNextBatch, processOneBatch)
 	if err != nil {
@@ -468,12 +472,6 @@ func (t *GCTable) Close() {
 	}
 }
 
-func (t *GCTable) closeBatch(bs []*containers.Batch) {
-	for i := range bs {
-		bs[i].Close()
-	}
-}
-
 // collectData collects data from memory that can be written to s3
 func (t *GCTable) CollectMapData(cxt context.Context, bat *batch.Batch, mp *mpool.MPool) (bool, error) {
 	if len(t.objects) == 0 {
@@ -532,62 +530,37 @@ func loader(
 
 }
 
-func (t *GCTable) rebuildTable(bats []*containers.Batch, idx BatchType, objects map[string]*ObjectEntry) {
-	for i := 0; i < bats[idx].Length(); i++ {
-		name := string(bats[idx].GetVectorByName(GCAttrObjectName).Get(i).([]byte))
-		creatTS := bats[idx].GetVectorByName(GCCreateTS).Get(i).(types.TS)
-		deleteTS := bats[idx].GetVectorByName(GCDeleteTS).Get(i).(types.TS)
-		tid := bats[idx].GetVectorByName(GCAttrTableId).Get(i).(uint64)
-		if t.objects[name] != nil {
-			continue
-		}
-		object := &ObjectEntry{
-			createTS: creatTS,
-			dropTS:   deleteTS,
-			table:    tid,
-		}
-		addObjectLocked(name, object, objects)
+func (t *GCTable) rebuildTable(bat *batch.Batch) {
+	t.files.Lock()
+	defer t.files.Unlock()
+	for i := 0; i < bat.Vecs[0].Length(); i++ {
+		stats := objectio.NewObjectStats()
+		stats.UnMarshal(bat.Vecs[0].GetRawBytesAt(i))
+		t.files.stats = append(t.files.stats, *stats)
 	}
 }
 
 func (t *GCTable) replayData(
 	ctx context.Context,
-	typ BatchType,
-	attrs []string,
-	types []types.Type,
-	bats []*containers.Batch,
+	bat *batch.Batch,
 	bs []objectio.BlockObject,
 	reader *blockio.BlockReader) (func(), error) {
-	idxes := make([]uint16, len(attrs))
-	for i := range attrs {
-		idxes[i] = uint16(i)
-	}
-	mobat, release, err := reader.LoadColumns(ctx, idxes, nil, bs[typ].GetID(), common.DefaultAllocator)
+	idxes := []uint16{0}
+	var release func()
+	var err error
+	bat, release, err = reader.LoadColumns(ctx, idxes, nil, bs[0].GetID(), common.DefaultAllocator)
 	if err != nil {
 		return nil, err
-	}
-	for i := range attrs {
-		pkgVec := mobat.Vecs[i]
-		var vec containers.Vector
-		if pkgVec.Length() == 0 {
-			vec = containers.MakeVector(types[i], common.DefaultAllocator)
-		} else {
-			vec = containers.ToTNVector(pkgVec, common.DefaultAllocator)
-		}
-		bats[typ].AddVector(attrs[i], vec)
 	}
 	return release, nil
 }
 
 // ReadTable reads an s3 file and replays a GCTable in memory
 func (t *GCTable) ReadTable(ctx context.Context, name string, size int64, fs *objectio.ObjectFS, ts types.TS) error {
-	var release1, release2 func()
+	var release1 func()
 	defer func() {
 		if release1 != nil {
 			release1()
-		}
-		if release2 != nil {
-			release2()
 		}
 	}()
 	reader, err := blockio.NewFileReaderNoCache(fs.Service, name)
@@ -598,18 +571,14 @@ func (t *GCTable) ReadTable(ctx context.Context, name string, size int64, fs *ob
 	if err != nil {
 		return err
 	}
-	bats := t.makeBatchWithGCTable()
-	defer t.closeBatch(bats)
-	release1, err = t.replayData(ctx, ObjectList, BlockSchemaAttr, BlockSchemaTypes, bats, bs, reader)
-	if err != nil {
-		return err
-	}
-	release2, err = t.replayData(ctx, TombstoneList, BlockSchemaAttr, BlockSchemaTypes, bats, bs, reader)
+	buffer := t.fetchBuffer()
+	defer t.putBuffer(buffer)
+	release1, err = t.replayData(ctx, buffer, bs, reader)
 	if err != nil {
 		return err
 	}
 	t.Lock()
-	t.rebuildTable(bats, ObjectList, t.objects)
+	t.rebuildTable(buffer)
 	t.Unlock()
 	return nil
 }
