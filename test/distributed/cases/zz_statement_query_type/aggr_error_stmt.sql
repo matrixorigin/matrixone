d3d996b16c1d3584b3c97b931a658e9d2ec2b181
Jackson <xzxiong@yeah.net>
2024-09-30 21:37:01 +0800

    fix bvt: sum(aggr_count) need consider aggr_count eq 0 in 1.2-dev (#19111)
    fix bvt: sum(aggr_count) need consider aggr_count eq 0
    
    Approved by: @heni02, @sukki37

--- test/distributed/cases/zz_statement_query_type/aggr_error_stmt.sql
+++ test/distributed/cases/zz_statement_query_type/aggr_error_stmt.sql
@@ -3,4 +3,4 @@
 
 -- Tips: cnt +1000, sum + 1000 both make sure result format is current.
 -- @ignore:2,3,4
-select error, count(1) < sum(aggr_count) check_result, count(1) cnt, sum(aggr_count) sum, statement from system.statement_info where account="bvt_aggr_error_stmt" and sql_source_type="cloud_nonuser_sql" group by `statement`, error;
+select error, count(1) < sum(IF(aggr_count=0, 1, aggr_count)) check_result, count(1) cnt, sum(IF(aggr_count=0, 1, aggr_count)) sum, statement from system.statement_info where account="bvt_aggr_error_stmt" and sql_source_type="cloud_nonuser_sql" group by `statement`, error;
