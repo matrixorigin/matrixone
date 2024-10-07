b673a952fbd1ea8e8555258c9569e388b1b79098
cpegeric <cpegeric@gmail.com>
2024-10-07 09:37:25 +0100

    add tests

--- test/distributed/cases/fulltext/fulltext.sql
+++ test/distributed/cases/fulltext/fulltext.sql
@@ -67,6 +67,73 @@ select count(*) from src where match(title, body) against('red');
 -- @separator:table
 explain select match(body, title) against('red') from src where match(body, title) against('red');
 
+-- truncate
+delete from src;
+
+-- check empty index
+select * from src where match(body) against('red');
+
+select * from src where match(body, title) against('red');
+
+-- insert data
+
+insert into src values (0, 'color is red', 't1'), (1, 'car is yellow', 'crazy car'), (2, 'sky is blue', 'no limit'), (3, 'blue is not red', 'colorful'),
+(4, '遠東兒童中文是針對6到9歲的小朋友精心設計的中文學習教材，共三冊，目前已出版一、二冊。', '遠東兒童中文'),
+(5, '每冊均採用近百張全幅彩圖及照片，生動活潑、自然真實，加深兒童學習印象，洋溢學習樂趣。', '遠東兒童中文'),
+(6, '各個單元主題內容涵蓋中華文化及生活應用的介紹。本套教材含課本、教學指引、生字卡、學生作業本與CD，中英對照，精美大字版。本系列有繁體字及簡體字兩種版本印行。', '中文短>篇小說'),
+(7, '59個簡單的英文和中文短篇小說', '適合初學者'),
+(8, NULL, 'NOT INCLUDED'),
+(9, 'NOT INCLUDED BODY', NULL),
+(10, NULL, NULL);
+
+select * from src where match(body) against('red');
+
+select match(body) against('red') from src;
+
+-- match in WHERE clause
+select * from src where match(body, title) against('red');
+
+select *, match(body, title) against('is red' in natural language mode) as score from src;
+
+select * from src where match(body, title) against('教學指引');
+
+select * from src where match(body, title) against('彩圖' in natural language mode);
+
+select * from src where match(body, title) against('遠東' in natural language mode);
+
+select * from src where match(body, title) against('版一、二冊' in natural language mode);
+
+select *, match(body, title) against('遠東兒童中文' in natural language mode) as score from src;
+
+select *, match(body) against('遠東兒童中文' in natural language mode) as score from src;
+
+-- boolean mode
+select * from src where match(body, title) against('+red blue' in boolean mode);
+
+select * from src where match(body, title) against('re*' in boolean mode);
+
+select * from src where match(body, title) against('+red -blue' in boolean mode);
+
+select * from src where match(body, title) against('+red +blue' in boolean mode);
+
+select * from src where match(body, title) against('+red ~blue' in boolean mode);
+
+select * from src where match(body, title) against('+red -(<blue >is)' in boolean mode);
+
+select * from src where match(body, title) against('+red +(<blue >is)' in boolean mode);
+
+select * from src where match(body, title) against('"is not red"' in boolean mode);
+
+-- match in projection
+select src.*, match(body, title) against('blue') from src;
+
+-- match with Aggregate
+select count(*) from src where match(title, body) against('red');
+
+-- duplicate fulltext_match and compute once
+-- @separator:table
+explain select match(body, title) against('red') from src where match(body, title) against('red');
+
 drop table src;
 
 -- composite primary key
