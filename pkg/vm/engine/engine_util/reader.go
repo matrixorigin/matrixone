918dbee8961c70376b5c67f3f359de131e18adec
badboynt1 <badboynt@126.com>
2024-10-11 11:29:06 +0800

    fix

--- pkg/vm/engine/engine_util/reader.go
+++ pkg/vm/engine/engine_util/reader.go
@@ -310,7 +310,7 @@ func NewReader(
 	tableDef *plan.TableDef,
 	ts timestamp.Timestamp,
 	expr *plan.Expr,
-//orderedScan bool, // it should be included in filter or expr.
+	//orderedScan bool, // it should be included in filter or expr.
 	source engine.DataSource,
 ) (*reader, error) {
 
@@ -433,7 +433,7 @@ func (r *reader) Read(
 	}
 
 	var policy fileservice.Policy
-	if r.readBlockCnt > 512 {
+	if r.readBlockCnt > 256 {
 		policy = fileservice.SkipMemoryCacheWrites
 	}
 	r.readBlockCnt++
