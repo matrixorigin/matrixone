e00ee7205bc03669ab888d8d1c31aa08ab989729
badboynt1 <badboynt@126.com>
2024-10-12 15:33:50 +0800

    fix

--- pkg/sql/plan/stats.go
+++ pkg/sql/plan/stats.go
@@ -657,7 +657,7 @@ func estimateFilterBlockSelectivity(ctx context.Context, expr *plan.Expr, tableD
 	col := extractColRefInFilter(expr)
 	if col != nil {
 		sortOrder := GetSortOrder(tableDef, col.ColPos)
-		blocksel := calcBlockSelectivityUsingShuffleRange(s.ShuffleRangeMap[col.Name], expr, sortOrder)
+		blocksel := calcBlockSelectivityUsingShuffleRange(s, col.Name, expr, sortOrder)
 		switch sortOrder {
 		case 0:
 			blocksel = math.Min(blocksel, 0.2)
@@ -1485,7 +1485,7 @@ func DeepCopyStats(stats *plan.Stats) *plan.Stats {
 	}
 }
 
-func calcBlockSelectivityUsingShuffleRange(s *pb.ShuffleRange, expr *plan.Expr, sortOrder int) float64 {
+func calcBlockSelectivityUsingShuffleRange(s *pb.StatsInfo, colname string, expr *plan.Expr, sortOrder int) float64 {
 	sel := expr.Selectivity
 	if s == nil {
 		if expr.GetF().Func.ObjName == "isnull" || expr.GetF().Func.ObjName == "is_null" {
@@ -1501,10 +1501,14 @@ func calcBlockSelectivityUsingShuffleRange(s *pb.ShuffleRange, expr *plan.Expr, 
 	if sortOrder == 0 || sortOrder == 1 {
 		return sel * math.Pow(10, float64(sortOrder+1))
 	}
-	if s.Overlap > 0.5 {
+	overlap := s.ShuffleRangeMap[colname].Overlap
+	if colname == "k" {
+		logutil.Infof("block selectivity, expr %v sel %v overlap %v", expr.GetF().Func.ObjName, expr.Selectivity, overlap)
+	}
+	if overlap > 0.5 {
 		return 1
 	}
-	ret := sel * math.Pow(1000000, s.Overlap)
+	ret := sel * math.Pow(1000000, overlap)
 	if ret > 1 {
 		ret = 1
 	}
