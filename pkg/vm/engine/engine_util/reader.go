b49b3f3bf77696985c10b9b21c761ff0333510ab
badboynt1 <badboynt@126.com>
2024-10-14 16:55:29 +0800

    fix

--- pkg/vm/engine/engine_util/reader.go
+++ pkg/vm/engine/engine_util/reader.go
@@ -433,7 +433,7 @@ func (r *reader) Read(
 	}
 
 	var policy fileservice.Policy
-	if r.readBlockCnt > 256 {
+	if r.readBlockCnt > 2000 {
 		policy = fileservice.SkipMemoryCacheWrites
 	}
 	r.readBlockCnt++
