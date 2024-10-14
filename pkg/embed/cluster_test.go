911e1bf5cb07bb07cd3c8f9ddc2366f4518c7ec3
zhangxu <zhangxu19830126@gmail.com>
2024-10-14 11:04:22 +0800

    fix ut

--- pkg/embed/cluster_test.go
+++ pkg/embed/cluster_test.go
@@ -182,7 +182,7 @@ func TestRunSQLWithFrontend(t *testing.T) {
 	)
 }
 
-func TestGetInitPort(t *testing.T) {
+func TestGetInitValue(t *testing.T) {
 	var wg sync.WaitGroup
 	var ports []uint64
 	var lock sync.Mutex
@@ -208,6 +208,14 @@ func TestGetInitPort(t *testing.T) {
 		return ports[i] < ports[j]
 	})
 	require.Equal(t, []uint64{10000, 11000, 12000, 13000}, ports)
+}
+
+func TestGetInitValueWithEmptyNameMustPanic(t *testing.T) {
+	defer func() {
+		err := recover()
+		require.NotNil(t, err)
+	}()
+	getInitValue("")
 }
 
 func validCNCanWork(
