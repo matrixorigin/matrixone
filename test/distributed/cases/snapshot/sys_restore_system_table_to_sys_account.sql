cd2145740143532a1f13f7c5979ba45ccdc5a405
Ariznawl@163.com <ariznawl@163.com>
2024-10-16 10:00:31 +0800

    add case

--- test/distributed/cases/snapshot/sys_restore_system_table_to_sys_account.sql
+++ test/distributed/cases/snapshot/sys_restore_system_table_to_sys_account.sql
@@ -1,4 +1,3 @@
--- @bvt:issue#17333
 set global enable_privilege_cache = off;
 
 -- create udf, create snapshot, drop udf, restore
@@ -488,4 +487,3 @@ drop snapshot sp01;
 drop role role_r1, role_r2, role_r3;
 drop user role_u1, role_u2, role_u3;
 set global enable_privilege_cache = on;
--- @bvt:issue
