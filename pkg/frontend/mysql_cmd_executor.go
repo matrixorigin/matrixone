06fe46e3795aa7d2cee6a3815f1c46fce05fd1bf
Jackson <xzxiong@yeah.net>
2024-10-16 12:22:15 +0800

    notes

--- pkg/frontend/mysql_cmd_executor.go
+++ pkg/frontend/mysql_cmd_executor.go
@@ -3048,6 +3048,7 @@ func ExecRequest(ses *Session, execCtx *ExecCtx, req *Request) (resp *Response, 
 			} else {
 				resp = NewGeneralErrorResponse(COM_QUERY, ses.txnHandler.GetServerStatus(), moe)
 			}
+			// log the query's statement and error info.
 			logStatementStatus(execCtx.reqCtx, ses, execCtx.stmt, fail, err)
 		}
 	}()
