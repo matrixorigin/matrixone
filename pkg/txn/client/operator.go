0174dfda65ce5c62f600a027f86de126e1f70a27
fagongzi <zhangxu19830126@gmail.com>
2024-10-18 11:28:38 +0800

    make txn do not wait in incrservice while the logtail is paused to 1.2 (#19417)
    Fix hung. Consider the following timing:
    
    logtail paused
    cancel all active pipelines
    reset logtail state to normal
    When a pipeline in 2 is executing and a new transaction is opened in another goroutine to do something (e.g., update the cache of a auto-increment), the new transaction is blocked until the logtail state returns to normal. This creates a circular wait.
    
    Approved by: @iamlinjunhong, @sukki37

--- pkg/txn/client/operator.go
+++ pkg/txn/client/operator.go
@@ -187,6 +187,12 @@ func WithDisableTrace(value bool) TxnOption {
 	}
 }
 
+func WithDisableWaitPaused() TxnOption {
+	return func(tc *txnOperator) {
+		tc.options = tc.options.WithDisableWaitPaused()
+	}
+}
+
 func WithSessionInfo(info string) TxnOption {
 	return func(tc *txnOperator) {
 		tc.options.SessionInfo = info
