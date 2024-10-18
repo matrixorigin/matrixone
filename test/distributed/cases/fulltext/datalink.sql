64e754c3db87635c0deaf283176d8499a042db2a
cpegeric <cpegeric@gmail.com>
2024-10-18 16:25:41 +0100

    bug fix cross apply

--- test/distributed/cases/fulltext/datalink.sql
+++ test/distributed/cases/fulltext/datalink.sql
@@ -13,7 +13,6 @@ select * from datasrc where match(fpath) against('Nulla facilisi' in natural lan
 
 drop table datasrc;
 
--- @bvt:issue#19437
 create table datasrc (id bigint primary key, fpath datalink, fpath2 datalink, fulltext(fpath, fpath2));
 
 insert into datasrc values (0, 'file:///$resources/fulltext/mo.pdf', 'file:///$resources/fulltext/chinese.pdf');
@@ -21,4 +20,3 @@ insert into datasrc values (0, 'file:///$resources/fulltext/mo.pdf', 'file:///$r
 select * from datasrc where match(fpath, fpath2) against('+matrixone +慢慢地' in boolean mode);
 
 drop table datasrc;
--- @bvt:issue
