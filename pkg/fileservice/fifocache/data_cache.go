92bc98241a815f1b3fdbc6226391e66009ff3038
reus <reusee@gmail.com>
2024-10-22 14:59:56 +0800

    fifocache: refine key hash func and optimize evict

--- pkg/fileservice/fifocache/data_cache.go
+++ pkg/fileservice/fifocache/data_cache.go
@@ -16,6 +16,7 @@ package fifocache
 
 import (
 	"context"
+	"hash/maphash"
 	"math"
 
 	"github.com/matrixorigin/matrixone/pkg/fileservice/fscache"
@@ -32,12 +33,19 @@ func NewDataCache(
 	postGet func(key fscache.CacheKey, value fscache.Data),
 	postEvict func(key fscache.CacheKey, value fscache.Data),
 ) *DataCache {
-	keyShardFunc := func(key fscache.CacheKey) uint8 {
-		return uint8(key.Offset ^ key.Sz)
-	}
 	return &DataCache{
-		fifo: New(capacity, keyShardFunc, postSet, postGet, postEvict),
+		fifo: New(capacity, shardCacheKey, postSet, postGet, postEvict),
 	}
+}
+
+var seed = maphash.MakeSeed()
+
+func shardCacheKey(key fscache.CacheKey) uint8 {
+	sum64 := uint64(key.Offset) ^ uint64(key.Sz) ^ maphash.String(seed, key.Path)
+	sum32 := uint32(sum64) ^ uint32(sum64>>32)
+	sum16 := uint16(sum32) ^ uint16(sum32>>16)
+	sum8 := uint8(sum16) ^ uint8(sum16>>8)
+	return sum8
 }
 
 var _ fscache.DataCache = new(DataCache)
@@ -78,8 +86,6 @@ func (d *DataCache) EnsureNBytes(want int, target int) {
 	if int(d.Available()) >= want {
 		return
 	}
-	d.fifo.queueLock.Lock()
-	defer d.fifo.queueLock.Unlock()
 	d.fifo.evict(nil, int64(target))
 }
 
@@ -88,8 +94,6 @@ func (d *DataCache) Evict(ch chan int64) {
 }
 
 func (d *DataCache) Flush() {
-	d.fifo.queueLock.Lock()
-	defer d.fifo.queueLock.Unlock()
 	d.fifo.evict(nil, math.MaxInt64)
 }
 
