d155c8cc245b39578ba91ea484f38e8cc76947b0
GreatRiver <great.river703@gmail.com>
2024-10-24 15:44:01 +0800

    add log

--- pkg/vm/engine/tae/db/gc/v3/window.go
+++ pkg/vm/engine/tae/db/gc/v3/window.go
@@ -568,7 +568,8 @@ func (w *GCWindow) String(objects map[string]*ObjectEntry) string {
 	var buf bytes.Buffer
 	_, _ = buf.WriteString("objects:[\n")
 	for name, entry := range objects {
-		_, _ = buf.WriteString(fmt.Sprintf("name: %s, createTS: %v ", name, entry.createTS.ToString()))
+		_, _ = buf.WriteString(fmt.Sprintf("name: %s, createTS: %v dropTS: %v",
+			name, entry.createTS.ToString(), entry.dropTS.ToString()))
 	}
 	_, _ = buf.WriteString("]\n")
 	return buf.String()
