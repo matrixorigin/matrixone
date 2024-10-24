1db2de835c82ea15ae37b24426622c1245b33541
XuPeng-SH <xupeng3112@163.com>
2024-10-24 12:25:10 +0800

    update

--- pkg/container/vector/tools.go
+++ pkg/container/vector/tools.go
@@ -271,6 +271,7 @@ func VectorToProtoVector(vec *Vector) (ret api.Vector, err error) {
 	if err != nil {
 		return
 	}
+	sz := vec.typ.TypeSize()
 	return api.Vector{
 		Nsp:      nsp,
 		Nullable: true,
@@ -278,8 +279,8 @@ func VectorToProtoVector(vec *Vector) (ret api.Vector, err error) {
 		IsConst:  vec.IsConst(),
 		Len:      uint32(vec.length),
 		Type:     TypeToProtoType(vec.typ),
-		Data:     vec.data,
-		// Data:     vec.data[:vec.length*sz],
+		// Data:     vec.data,
+		Data: vec.data[:vec.length*sz],
 	}, nil
 }
 
