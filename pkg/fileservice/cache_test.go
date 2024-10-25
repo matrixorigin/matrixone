b5cf6d80deb478033b89aeb82cf0b521a0d8a0c3
davis zhen <daviszhen007@gmail.com>
2024-10-26 01:39:25 +0800

    debug context deadline exceeded (#19361)
    用于定位context deadline exceeded问题。
    
    1, 用context.WithTimeoutCause替换context.WithTimeout
    2,定义了CauseXXX error。 XXX是context.WithTimeout所在的函数名称
    3, 在用ctx时，如果出来错误，用Context.Cause取cause,随err 带出来
    4, static check 禁止使用WithTimeout,只能用WithTimeoutCause.
    
    
    
    https://github.com/matrixorigin/ci-test/actions/runs/11362658814
    
    Approved by: @ck89119, @reusee, @fengttt, @xzxiong, @m-schen, @jiangxinmeng1, @badboynt1, @w-zr, @volgariver6, @LeftHandCold, @zhangxu19830126, @qingxinhome, @triump2020, @XuPeng-SH, @sukki37, @ouyuanning

---
+++ pkg/fileservice/cache_test.go
@@ -0,0 +1,140 @@
+// Copyright 2022 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fileservice
+
+import (
+	"context"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/matrixorigin/matrixone/pkg/common/moerr"
+	"github.com/matrixorigin/matrixone/pkg/fileservice/fscache"
+)
+
+func Test_readCache(t *testing.T) {
+	slowCacheReadThreshold = time.Second
+
+	size := int64(128)
+	m := NewMemCache(fscache.ConstCapacity(size), nil, nil, "")
+	defer m.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*3)
+	defer cancel()
+
+	newReadVec := func() *IOVector {
+		vec := &IOVector{
+			FilePath: "foo",
+			Entries: []IOEntry{
+				{
+					Size: 3,
+					ToCacheData: func(reader io.Reader, data []byte, allocator CacheDataAllocator) (fscache.Data, error) {
+						cacheData := allocator.AllocateCacheData(1)
+						cacheData.Bytes()[0] = 42
+						return cacheData, nil
+					},
+				},
+			},
+		}
+		return vec
+	}
+
+	err := readCache(ctx, m, newReadVec())
+	assert.NoError(t, err)
+
+	ctx, cancel = context.WithTimeoutCause(context.Background(), 0, moerr.NewInternalErrorNoCtx("ut tester"))
+	defer cancel()
+
+	newReadVec2 := func() *IOVector {
+		vec := &IOVector{
+			FilePath: "etl:\u00ad",
+			Entries: []IOEntry{
+				{
+					Size: 3,
+					ToCacheData: func(reader io.Reader, data []byte, allocator CacheDataAllocator) (fscache.Data, error) {
+						return nil, context.DeadlineExceeded
+					},
+				},
+			},
+		}
+		return vec
+	}
+
+	err = readCache(ctx, m, newReadVec2())
+	assert.Error(t, err)
+}
+
+var _ IOVectorCache = (*testCache)(nil)
+
+type testCache struct {
+}
+
+func (cache *testCache) Read(ctx context.Context, vector *IOVector) error {
+	return context.DeadlineExceeded
+}
+
+func (cache *testCache) Update(ctx context.Context, vector *IOVector, async bool) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (cache *testCache) Flush() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (cache *testCache) DeletePaths(ctx context.Context, paths []string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (cache *testCache) Evict(done chan int64) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (cache *testCache) Close() {}
+
+func Test_readCache2(t *testing.T) {
+	slowCacheReadThreshold = time.Second
+
+	m := &testCache{}
+	defer m.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*3)
+	defer cancel()
+
+	newReadVec := func() *IOVector {
+		vec := &IOVector{
+			FilePath: "foo",
+			Entries: []IOEntry{
+				{
+					Size: 3,
+					ToCacheData: func(reader io.Reader, data []byte, allocator CacheDataAllocator) (fscache.Data, error) {
+						cacheData := allocator.AllocateCacheData(1)
+						cacheData.Bytes()[0] = 42
+						return cacheData, nil
+					},
+				},
+			},
+		}
+		return vec
+	}
+
+	err := readCache(ctx, m, newReadVec())
+	assert.NoError(t, err)
+}
