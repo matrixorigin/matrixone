25a73b9763f1e885f64849cb5b0b58719fc6bffe
gouhongshen <gouhongshen@hotmail.com>
2024-10-27 12:08:56 +0800

    fix conflict

--- pkg/vm/engine/disttae/logtailreplay/blocks_iter.go
+++ pkg/vm/engine/disttae/logtailreplay/blocks_iter.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
+	"github.com/matrixorigin/matrixone/pkg/logutil"
 	"github.com/matrixorigin/matrixone/pkg/objectio"
 	"github.com/tidwall/btree"
 )
