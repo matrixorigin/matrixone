f5f61df39cae9d7b8564df80c840315cb9beb351
cpegeric <cpegeric@gmail.com>
2024-10-30 09:28:18 +0000

    add more tests

--- test/distributed/cases/plugin/plugin.sql
+++ test/distributed/cases/plugin/plugin.sql
@@ -40,3 +40,23 @@ from plugin_exec('["cat"]', cast('file:///$resources/plugin/multistream.json?off
 select * from t1;
 
 drop table t1;
+
+create stage llmstage URL='file:///$resources/plugin/';
+
+create table src (pkey int primary key, dlink datalink);
+
+create table embed (pkey int, chunk int, e vecf32(3));
+
+insert into src values 
+(0, 'stage://llmstage/multistream.json?offset=0&size=116'),
+(1, 'stage://llmstage/multistream.json?offset=116&size=125');
+
+insert into embed select src.pkey, json_unquote(json_extract(f.result, "$.chunk")), json_unquote(json_extract(f.result, "$.e"))
+from src CROSS APPLY plugin_exec('["cat"]', src.dlink) as f;
+
+select * from embed;
+
+drop stage llmstage;
+drop table src;
+drop table embed;
+
