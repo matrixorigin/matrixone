5c6b2e8904b585e5790688c1379c98003b850f40
GreatRiver <great.river703@gmail.com>
2024-11-04 12:45:16 +0800

    Update code

--- pkg/vm/engine/tae/rpc/handle_debug.go
+++ pkg/vm/engine/tae/rpc/handle_debug.go
@@ -271,6 +271,10 @@ func (h *Handle) HandleBackup(
 		return nil, err
 	}
 	data := h.db.BGCheckpointRunner.GetAllCheckpoints()
+	compact := h.db.BGCheckpointRunner.GetCompacted()
+	if compact != nil {
+		data = append(data, compact)
+	}
 	locations += location + ";"
 	for i := range data {
 		locations += data[i].GetLocation().String()
