628dcd6a4cdd2d1a76c8f148329879b9c6167cef
cpegeric <cpegeric@gmail.com>
2024-11-04 16:23:40 +0000

    change https:// to stage://

--- test/distributed/cases/function/func_wasm.sql
+++ test/distributed/cases/function/func_wasm.sql
@@ -6,17 +6,20 @@ create stage mystage URL='file:///$resources/plugin/';
 
 select wasm('stage://mystage/hello.wasm', 'mowasm_hello', 'world');
 
-select wasm('https://github.com/matrixorigin/mojo/raw/main/plugin/hello/hello.wasm', 'mowasm_hello', 'world');
-select wasm('https://github.com/matrixorigin/mojo/raw/main/plugin/hello/hello.wasm', 'mowasm_add', '[3, 5]');
+select wasm('stage://mystage/hello.wasm', 'mowasm_hello', 'world');
+select wasm('stage://mystage/hello.wasm', 'mowasm_add', '[3, 5]');
 
 select wasm('https://github.com/matrixorigin/mojo/raw/main/plugin/hello/notexist.wasm', 'mowasm_add', '[3, 5]');
 select try_wasm('https://github.com/matrixorigin/mojo/raw/main/plugin/hello/notexist.wasm', 'mowasm_add', '[3, 5]');
 
-select wasm('https://github.com/matrixorigin/mojo/raw/main/plugin/hello/hello.wasm', 'mowasm_add2', '[3, 5]');
-select try_wasm('https://github.com/matrixorigin/mojo/raw/main/plugin/hello/hello.wasm', 'mowasm_add2', '[3, 5]');
+select wasm('stage://mystage/notexist.wasm', 'mowasm_add', '[3, 5]');
+select try_wasm('stage://mystage/notexist.wasm', 'mowasm_add', '[3, 5]');
 
-select wasm('https://github.com/matrixorigin/mojo/raw/main/plugin/hello/hello.wasm', 'mowasm_add', '[1, 3, 5]');
-select try_wasm('https://github.com/matrixorigin/mojo/raw/main/plugin/hello/hello.wasm', 'mowasm_add', '[1, 3, 5]');
+select wasm('stage://mystage/hello.wasm', 'mowasm_add2', '[3, 5]');
+select try_wasm('stage://mystage/hello.wasm', 'mowasm_add2', '[3, 5]');
+
+select wasm('stage://mystage/hello.wasm', 'mowasm_add', '[1, 3, 5]');
+select try_wasm('stage://mystage/hello.wasm', 'mowasm_add', '[1, 3, 5]');
 
 create table wasmt(id int, fn varchar(255), arg varchar(255));
 insert into wasmt values
@@ -33,18 +36,12 @@ insert into wasmt values
 ;
 
 select count(*) from wasmt;
-select id, wasm('https://github.com/matrixorigin/mojo/raw/main/plugin/hello/hello.wasm', fn, arg)
+select id, wasm('stage://mystage/hello.wasm', fn, arg)
 from wasmt;
-select id, try_wasm('https://github.com/matrixorigin/mojo/raw/main/plugin/hello/hello.wasm', fn, arg)
+select id, try_wasm('stage://mystage/hello.wasm', fn, arg)
 from wasmt;
 
 drop table wasmt;
 
 drop stage mystage;
-
-
-
-
-
-
 
