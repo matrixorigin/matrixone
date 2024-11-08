c08cf4b87922cca11ee55e3d6a763b7949f4db0e
badboynt1 <badboynt@126.com>
2024-11-08 15:21:19 +0800

    fix

--- pkg/sql/compile/scope.go
+++ pkg/sql/compile/scope.go
@@ -644,13 +644,8 @@ func (s *Scope) handleRuntimeFilter(c *Compile) error {
 		panic("can not expand ranges on remote pipeline!")
 	}
 
-<<<<<<< HEAD
 	for _, e := range s.DataSource.BlockFilterList {
 		err = plan2.EvalFoldExpr(s.Proc, e, &c.filterExprExes)
-=======
-		counterSet := new(perfcounter.CounterSet)
-		relData, err := c.expandRanges(s.DataSource.node, s.DataSource.Rel, s.DataSource.Db, s.DataSource.Ctx, newExprList, counterSet)
->>>>>>> dafa51b631d696b2869ef990a42578cb7dc37f41
 		if err != nil {
 			return err
 		}
@@ -662,7 +657,7 @@ func (s *Scope) handleRuntimeFilter(c *Compile) error {
 	}
 
 	counterSet := new(perfcounter.CounterSet)
-	relData, err := c.expandRanges(s.DataSource.node, newExprList, counterSet)
+	relData, err := c.expandRanges(s.DataSource.node, s.DataSource.Rel, s.DataSource.Db, s.DataSource.Ctx, newExprList, counterSet)
 	if err != nil {
 		return err
 	}
