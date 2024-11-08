5722a06885cda7230172b8a75793044b83efb539
YANGGMM <www.yangzhao123@gmail.com>
2024-11-08 19:17:13 +0800

    support ip whitelist feat (#19882)
    support ip whitelist
    
    Approved by: @daviszhen, @heni02

--- test/distributed/cases/zz_accesscontrol/password_management/password_management_system_variable.sql
+++ test/distributed/cases/zz_accesscontrol/password_management/password_management_system_variable.sql
@@ -7,13 +7,6 @@ SET GLOBAL validate_password.mixed_case_count = default;
 SET GLOBAL validate_password.number_count = default;
 SET GLOBAL validate_password.special_char_count = default;
 
-select @@validate_password;
-select @@validate_password.changed_characters_percentage;
-select @@validate_password.policy;
-select @@validate_password.length;
-select @@validate_password.mixed_case_count;
-select @@validate_password.number_count;
-select @@validate_password.special_char_count;
 
 select @@global.validate_password;
 select @@global.validate_password.check_user_name;
