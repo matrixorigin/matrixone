6f8b0a42156a0e249bd7eb7de21de17847355261
ou yuanning <45346669+ouyuanning@users.noreply.github.com>
2024-11-08 08:52:36 +0800

    add debug tool for remote run (#19833)
    add debug tool for remote run
    
    主要解决在查hung问题的时候，有时候是hung在等其他cn的某个go routine执行结果的返回。
    而之前从 txn leak里只能跟踪到直接hung的那个go routine，不知道被等待的那个go routine是什么，这里通过一个注入语句，可以在运行时增加对应的log信息，以便通过等待的compile的内存指针，去找被等待的那个go routine id
    
    使用方法：
    假设要执行的sql是： update test_table set b = b + 1 where a < 5000000;
    
    select mo_ctl("dn", "addfaultpoint", "enable_fault_injection");
    select mo_ctl("dn", "addfaultpoint", "inject_send_pipeline.1:1:1:.echo.1.test_table");   //在使用时只要修改这里的 test_table 为你要执行的sql的关键字就好了。
    
    那么我们将可以在日志上看到类似这样的日志：
    ```
    2024/11/06 14:47:57.001363 +0800 INFO compile/remoterunServer.go:97 inject_send_pipeline: client2server,compile = 0x1402b9584e0, goRoutineId=14402
    ```
    
    这样就可以根据txn leak的go routine堆栈去找出问题的地方了
    
    Approved by: @aunjgr, @heni02, @m-schen, @zhangxu19830126

--- pkg/sql/compile/remoterunServer.go
+++ pkg/sql/compile/remoterunServer.go
@@ -42,6 +42,7 @@ import (
 	plan2 "github.com/matrixorigin/matrixone/pkg/sql/plan"
 	"github.com/matrixorigin/matrixone/pkg/txn/client"
 	"github.com/matrixorigin/matrixone/pkg/udf"
+	"github.com/matrixorigin/matrixone/pkg/util/debug/goroutine"
 	v2 "github.com/matrixorigin/matrixone/pkg/util/metric/v2"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine"
 	"github.com/matrixorigin/matrixone/pkg/vm/process"
@@ -90,6 +91,9 @@ func CnServerMessageHandler(
 	if !ok {
 		logutil.Errorf("cn server should receive *pipeline.Message, but get %v", message)
 		panic("cn server receive a message with unexpected type")
+	}
+	if msg.DebugMsg != "" {
+		logutil.Infof("%s, goRoutineId=%d", msg.GetDebugMsg(), goroutine.GetRoutineId())
 	}
 
 	// prepare the receiver structure, just for easy using the `send` method.
