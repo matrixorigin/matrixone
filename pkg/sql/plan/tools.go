246e4d262001d07576a9a9b9ba63a4824e5ae688
iamlinjunhong <1030420200@qq.com>
2024-11-08 15:46:56 +0800

    fix nested recursive cte

--- pkg/sql/plan/tools.go
+++ pkg/sql/plan/tools.go
@@ -14,34 +14,8 @@
 
 package plan
 
-import (
-	"go/constant"
-
-	"github.com/matrixorigin/matrixone/pkg/sql/parsers/tree"
-)
-
 const (
-	moRecursiveLevelCol            = "__mo_recursive_level_col"
-	moDefaultRecursionMax          = 100
-	moCheckRecursionLevelFun       = "mo_check_level"
 	moEnumCastIndexToValueFun      = "cast_index_to_value"
 	moEnumCastValueToIndexFun      = "cast_value_to_index"
 	moEnumCastIndexValueToIndexFun = "cast_index_value_to_index"
 )
-
-func makeZeroRecursiveLevel() tree.SelectExpr {
-	return tree.SelectExpr{
-		Expr: tree.NewNumValWithType(constant.MakeInt64(0), "0", false, tree.P_int64),
-		As:   tree.NewCStr(moRecursiveLevelCol, 1),
-	}
-
-}
-
-func makePlusRecursiveLevel(name string, lower int64) tree.SelectExpr {
-	a := tree.NewUnresolvedName(tree.NewCStr(name, lower), tree.NewCStr(moRecursiveLevelCol, 1))
-	b := tree.NewNumValWithType(constant.MakeInt64(1), "1", false, tree.P_int64)
-	return tree.SelectExpr{
-		Expr: tree.NewBinaryExpr(tree.PLUS, a, b),
-		As:   tree.NewCStr("", 1),
-	}
-}
