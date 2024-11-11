f0b623f8c592e4826bb48e45888a8f29193c1e2a
Wei Ziran <weiziran125@gmail.com>
2024-11-11 17:10:23 +0800

    ignore bvt case 2.0-dev (#19935)
    ignore bvt case for https://github.com/matrixorigin/matrixone/pull/19916
    
    Approved by: @heni02, @sukki37

--- test/distributed/cases/log/show_table.sql
+++ test/distributed/cases/log/show_table.sql
@@ -1,4 +1,6 @@
 -- check system / system_metrics in sys account
+-- @bvt:issue#19912
 SHOW CREATE TABLE system_metrics.metric;
 SHOW CREATE TABLE system.statement_info;
 SHOW CREATE TABLE system.rawlog;
+-- @bvt:issue
