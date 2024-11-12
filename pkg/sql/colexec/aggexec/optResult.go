6b83e1f818e8191a0cf54b845b70d57ab9eac8c0
chenmingsong <983362525@qq.com>
2024-11-12 19:22:48 +0800

    fix

--- pkg/sql/colexec/aggexec/optResult.go
+++ pkg/sql/colexec/aggexec/optResult.go
@@ -79,9 +79,15 @@ type optSplitResult struct {
 	resultType types.Type
 
 	// the list of each group's result and empty situation.
-	resultList       []*vector.Vector
-	emptyList        []*vector.Vector
-	nowIdx1, nowIdx2 int
+	resultList []*vector.Vector
+	emptyList  []*vector.Vector
+	nowIdx1    int
+
+	// for easy get / set from emptyList.
+	bsFromEmptyList [][]bool
+
+	// for easy get / set result and empty from outer.
+	accessIdx1, accessIdx2 int
 }
 
 func (r *optSplitResult) init(
@@ -103,7 +109,7 @@ func (r *optSplitResult) init(
 
 	r.resultList = append(r.resultList, vector.NewOffHeapVecWithType(typ))
 	r.emptyList = append(r.emptyList, vector.NewOffHeapVecWithType(types.T_bool.ToType()))
-	r.nowIdx1, r.nowIdx2 = 0, 0
+	r.nowIdx1 = 0
 }
 
 func (r *optSplitResult) noNeedToCountEmptyGroup() {
@@ -111,6 +117,7 @@ func (r *optSplitResult) noNeedToCountEmptyGroup() {
 	r.optInformation.shouldSetNullToEmptyGroup = false
 }
 
+// getNspFromBoolVector generate a nsp bitmap from a bool type vector and return it.
 func getNspFromBoolVector(v *vector.Vector) *nulls.Nulls {
 	bs := vector.MustFixedColNoTypeCheck[bool](v)
 
@@ -122,6 +129,23 @@ func getNspFromBoolVector(v *vector.Vector) *nulls.Nulls {
 		}
 	}
 	return nsp
+}
+
+// updateAccessIdx set real index from a logic index.
+//
+// TODO: do a range from outer is more effective, this can reduce many div and mod cost.
+func (r *optSplitResult) updateAccessIdx(logicIndex int) (int, int) {
+	r.accessIdx1 = logicIndex / r.optInformation.eachSplitCapacity
+	r.accessIdx2 = logicIndex % r.optInformation.eachSplitCapacity
+	return r.accessIdx1, r.accessIdx2
+}
+
+func (r *optSplitResult) isEmpty(realIndex1, realIndex2 int) bool {
+	return r.bsFromEmptyList[realIndex1][realIndex2]
+}
+
+func (r *optSplitResult) setNotEmpty(realIndex1, realIndex2 int) {
+	r.bsFromEmptyList[realIndex1][realIndex2] = true
 }
 
 // flushOneVector return the agg result one by one.
@@ -142,70 +166,143 @@ func (r *optSplitResult) flushOneVector() *vector.Vector {
 	return nil
 }
 
+// extend try to expend `more` groups to optSplitResult's result and empty situations.
+// If the capacity is not enough, allocate and update the capacity.
 func (r *optSplitResult) extend(more int) error {
 
-	// simple extend if there is no need to do append.
-	maxToExtendWithoutAppend := r.optInformation.eachSplitCapacity - r.nowIdx2
-	if maxToExtendWithoutAppend > more {
-		if err := r.resultList[r.nowIdx1].PreExtend(more, r.mp); err != nil {
-			return err
-		}
-		if err := r.emptyList[r.nowIdx1].PreExtend(more, r.mp); err != nil {
+	// try tp full the using part first.
+	l1 := r.resultList[r.nowIdx1].Length()
+	maxToExtendWithinTheUsingPart := r.optInformation.eachSplitCapacity - l1
+	if maxToExtendWithinTheUsingPart > more {
+		if err := r.preExtendPartK(r.nowIdx1, more); err != nil {
 			return err
 		}
 
-		r.nowIdx2 += more
-		r.resultList[r.nowIdx1].SetLength(r.nowIdx2)
-		r.emptyList[r.nowIdx2].SetLength(r.nowIdx2)
+		r.setLengthPartK(r.nowIdx1, l1+more)
 		return nil
 	}
-
-	// full the last result first.
-	if err := r.resultList[r.nowIdx1].PreExtend(maxToExtendWithoutAppend, r.mp); err != nil {
+	if err := r.preExtendPartK(r.nowIdx1, maxToExtendWithinTheUsingPart); err != nil {
 		return err
 	}
-	if err := r.emptyList[r.nowIdx1].PreExtend(maxToExtendWithoutAppend, r.mp); err != nil {
-		return err
+	r.setLengthPartK(r.nowIdx1, r.optInformation.eachSplitCapacity)
+	more -= maxToExtendWithinTheUsingPart
+
+	// try to full the allocated part first.
+	maxToExtendWithoutPartAppend := (len(r.resultList) - 1 - r.nowIdx1) * r.optInformation.eachSplitCapacity
+	if maxToExtendWithoutPartAppend > more {
+		fullPart, rowMore := more/r.optInformation.eachSplitCapacity, more%r.optInformation.eachSplitCapacity
+
+		for i, j := r.nowIdx1+1, r.nowIdx1+fullPart; i < j; i++ {
+			if err := r.preExtendPartK(i, r.optInformation.eachSplitCapacity); err != nil {
+				return err
+			}
+			r.setLengthPartK(i, r.optInformation.eachSplitCapacity)
+		}
+
+		if rowMore > 0 {
+			r.nowIdx1 += fullPart
+			if err := r.preExtendPartK(r.nowIdx1, rowMore); err != nil {
+				return err
+			}
+			r.setLengthPartK(r.nowIdx1, r.optInformation.eachSplitCapacity)
+		}
+		return nil
 	}
-	r.resultList[r.nowIdx1].SetLength(r.optInformation.eachSplitCapacity)
-	r.emptyList[r.nowIdx2].SetLength(r.optInformation.eachSplitCapacity)
-
-	more -= maxToExtendWithoutAppend
-
-	apFullCount, rowMore := more/r.optInformation.eachSplitCapacity, more%r.optInformation.eachSplitCapacity
-	for i := 0; i < apFullCount; i++ {
-		v1 := vector.NewOffHeapVecWithType(r.resultType)
-		v2 := vector.NewOffHeapVecWithType(types.T_bool.ToType())
-		if err := v1.PreExtend(r.optInformation.eachSplitCapacity, r.mp); err != nil {
+	for i := r.nowIdx1 + 1; i < len(r.resultList); i++ {
+		if err := r.preExtendPartK(i, r.optInformation.eachSplitCapacity); err != nil {
 			return err
 		}
-		if err := v2.PreExtend(r.optInformation.eachSplitCapacity, r.mp); err != nil {
-			return err
-		}
-		v1.SetLength(r.optInformation.eachSplitCapacity)
-		v2.SetLength(r.optInformation.eachSplitCapacity)
-
-		r.resultList = append(r.resultList, v1)
-		r.emptyList = append(r.emptyList, v2)
+		r.setLengthPartK(i, r.optInformation.eachSplitCapacity)
 	}
+	more -= maxToExtendWithoutPartAppend
 
+	// append more part.
+	apFullPart, rowMore := more/r.optInformation.eachSplitCapacity, more%r.optInformation.eachSplitCapacity
+	for i := 0; i < apFullPart; i++ {
+		k := r.appendPartK()
+		if err := r.preExtendPartK(k, r.optInformation.eachSplitCapacity); err != nil {
+			return nil
+		}
+		r.setLengthPartK(k, r.optInformation.eachSplitCapacity)
+	}
 	if rowMore > 0 {
-		v1 := vector.NewOffHeapVecWithType(r.resultType)
-		v2 := vector.NewOffHeapVecWithType(types.T_bool.ToType())
-
-		if err := v1.PreExtend(rowMore, r.mp); err != nil {
+		k := r.appendPartK()
+		if err := r.preExtendPartK(k, rowMore); err != nil {
 			return err
 		}
-		if err := v2.PreExtend(rowMore, r.mp); err != nil {
-			return err
-		}
-		v1.SetLength(rowMore)
-		v2.SetLength(rowMore)
-
-		r.resultList = append(r.resultList, v1)
-		r.emptyList = append(r.emptyList, v2)
+		r.setLengthPartK(k, rowMore)
 	}
 
-	r.nowIdx1, r.nowIdx2 = len(r.resultList)-1, rowMore
 	return nil
+}
+
+func (r *optSplitResult) preExtendPartK(k int, more int) error {
+	if err := r.resultList[k].PreExtend(more, r.mp); err != nil {
+		return err
+	}
+	if r.optInformation.doesThisNeedEmptyList {
+		return r.emptyList[k].PreExtend(more, r.mp)
+	}
+	return nil
+}
+
+func (r *optSplitResult) setLengthPartK(k int, row int) {
+	r.resultList[k].SetLength(row)
+	if r.optInformation.doesThisNeedEmptyList {
+		r.emptyList[k].SetLength(row)
+	}
+}
+
+func (r *optSplitResult) appendPartK() int {
+	r.resultList = append(r.resultList, vector.NewOffHeapVecWithType(r.resultType))
+	if r.optInformation.doesThisNeedEmptyList {
+		r.emptyList = append(r.emptyList, vector.NewOffHeapVecWithType(types.T_bool.ToType()))
+	}
+	return len(r.resultList) - 1
+}
+
+// preAllocate only extend the capacity for the result
+// and keep it with the origin using length.
+func (r *optSplitResult) preAllocate(more int) (err error) {
+	oldNowIdx1 := r.nowIdx1
+	oldNowIdx2 := r.resultList[oldNowIdx1].Length()
+
+	if err = r.extend(more); err != nil {
+		return err
+	}
+
+	r.resultList[oldNowIdx1].SetLength(oldNowIdx2)
+	for i := oldNowIdx1 + 1; i < len(r.resultList); i++ {
+		r.resultList[i].SetLength(0)
+	}
+
+	if r.optInformation.doesThisNeedEmptyList {
+		r.emptyList[oldNowIdx1].SetLength(oldNowIdx2)
+
+		for i := oldNowIdx1 + 1; i < len(r.resultList); i++ {
+			r.emptyList[i].SetLength(0)
+		}
+	}
+	return nil
+}
+
+func (r *optSplitResult) free() {
+	if r.mp == nil {
+		return
+	}
+
+	for _, v := range r.resultList {
+		if v == nil {
+			continue
+		}
+		v.Free(r.mp)
+	}
+	for _, v := range r.emptyList {
+		if v == nil {
+			continue
+		}
+		v.Free(r.mp)
+	}
+	r.resultList = nil
+	r.emptyList = nil
 }
