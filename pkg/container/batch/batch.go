fa1c503fe79e13d28f0f6f38eb7ba936f13ca6fa
Wenbin1002 <waterdance1002@gmail.com>
2024-11-12 10:55:15 +0800

    fix

--- pkg/container/batch/batch.go
+++ pkg/container/batch/batch.go
@@ -611,7 +611,7 @@ func (bat *Batch) Allocated() int {
 func (bat *Batch) Window(start, end int) (*Batch, error) {
 	b := NewWithSize(len(bat.Vecs))
 	var err error
-	b.Attrs = append(b.Attrs, bat.Attrs...)
+	b.Attrs = bat.Attrs
 	for i, vec := range bat.Vecs {
 		b.Vecs[i], err = vec.Window(start, end)
 		if err != nil {
