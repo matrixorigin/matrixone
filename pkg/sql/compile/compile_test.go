9131ff731f50f524d7b645b737a447cae0b6e3ba
badboynt1 <badboynt@126.com>
2024-11-13 15:49:30 +0800

    fix

--- pkg/sql/compile/compile_test.go
+++ pkg/sql/compile/compile_test.go
@@ -266,10 +266,6 @@ func GetFilePath() string {
 }
 
 func TestShuffleBlocksByHash(t *testing.T) {
-	testCompile := &Compile{
-		proc: testutil.NewProcess(),
-	}
-	testCompile.cnList = engine.Nodes{engine.Node{Addr: "cn1:6001", Data: &engine_util.BlockListRelData{}}, engine.Node{Addr: "cn2:6001", Data: &engine_util.BlockListRelData{}}}
 	s := objectio.BlockInfoSlice{}
 	stats := objectio.NewObjectStats()
 	for i := 0; i < 100; i++ {
@@ -279,24 +275,8 @@ func TestShuffleBlocksByHash(t *testing.T) {
 	}
 	reldata := &engine_util.BlockListRelData{}
 	reldata.SetBlockList(s)
-	shuffleBlocksByHash(testCompile, reldata, testCompile.cnList)
-}
-
-func TestShuffleBlocksByMoCtl(t *testing.T) {
-	testCompile := &Compile{
-		proc: testutil.NewProcess(),
-	}
-	testCompile.cnList = engine.Nodes{engine.Node{Addr: "cn1:6001", Data: &engine_util.BlockListRelData{}}, engine.Node{Addr: "cn2:6001", Data: &engine_util.BlockListRelData{}}}
-	s := objectio.BlockInfoSlice{}
-	stats := objectio.NewObjectStats()
-	for i := 0; i < 100; i++ {
-		var blk objectio.BlockInfo
-		stats.ConstructBlockInfoTo(uint16(0), &blk)
-		s.AppendBlockInfo(&blk)
-	}
-	reldata := &engine_util.BlockListRelData{}
-	reldata.SetBlockList(s)
-	require.NoError(t, shuffleBlocksByMoCtl(reldata, 2, testCompile.cnList))
+	newreldata := &engine_util.BlockListRelData{}
+	shuffleBlocksByHash(reldata, newreldata, 3, 1)
 }
 
 func TestPutBlocksInCurrentCN(t *testing.T) {
@@ -314,25 +294,6 @@ func TestPutBlocksInCurrentCN(t *testing.T) {
 	reldata := &engine_util.BlockListRelData{}
 	reldata.SetBlockList(s)
 	putBlocksInCurrentCN(testCompile, reldata, true)
-}
-
-func TestShuffleBlocksToMultiCN(t *testing.T) {
-	testCompile := &Compile{
-		proc: testutil.NewProcess(),
-	}
-	testCompile.cnList = engine.Nodes{engine.Node{Addr: "cn1:6001", Data: &engine_util.BlockListRelData{}}, engine.Node{Addr: "cn2:6001", Data: &engine_util.BlockListRelData{}}}
-	s := objectio.BlockInfoSlice{}
-	stats := objectio.NewObjectStats()
-	for i := 0; i < 100; i++ {
-		var blk objectio.BlockInfo
-		stats.ConstructBlockInfoTo(uint16(0), &blk)
-		s.AppendBlockInfo(&blk)
-	}
-	reldata := &engine_util.BlockListRelData{}
-	reldata.SetBlockList(s)
-	n := &plan.Node{Stats: plan2.DefaultStats()}
-	_, err := shuffleBlocksToMultiCN(testCompile, nil, reldata, n)
-	require.NoError(t, err)
 }
 
 var _ morpc.RPCClient = new(testRpcClient)
