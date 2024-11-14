46b16e89685e9686607591fa72a9374b5d3a043e
YANGGMM <18916107305@163.com>
2024-11-14 18:51:02 +0800

    fix

--- test/distributed/cases/snapshot/cluster/restore_dropped_account_with_cluster.sql
+++ test/distributed/cases/snapshot/cluster/restore_dropped_account_with_cluster.sql
@@ -1,11 +1,11 @@
 drop account if exists acc01;
 create account acc01 admin_name = 'test_account' identified by '111';
 drop account if exists acc02;
-create account acc02 admin_name = 'test_account' identified by '111' comment 'test_comment';
+create account acc02 admin_name = 'test_account' identified by '1111' comment 'test_comment';
 drop account if exists acc03;
-create account acc03 admin_name = 'test_account' identified by '111';
+create account acc03 admin_name = 'test_account' identified by '11111';
 drop account if exists acc04;
-create account acc04 admin_name = 'test_account' identified by '111'  comment 'test_comment';
+create account acc04 admin_name = 'test_account' identified by '111111'  comment 'test_comment';
 
 drop snapshot if exists snapshot_01;
 create snapshot snapshot_01 for cluster;
