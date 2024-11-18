040f6baf5d40eed3f47c754f928c77b31b2ac1eb
YANGGMM <18916107305@163.com>
2024-11-18 17:23:43 +0800

    add log

--- pkg/frontend/snapshot.go
+++ pkg/frontend/snapshot.go
@@ -281,6 +281,7 @@ func doCreateSnapshot(ctx context.Context, ses *Session, stmt *tree.CreateSnapSh
 			return err
 		}
 	}
+	getLogger(ses.GetService()).Info(fmt.Sprintf("create snapshot %s success", snapshotName))
 
 	// insert record to the system table
 
@@ -328,6 +329,8 @@ func doDropSnapshot(ctx context.Context, ses *Session, stmt *tree.DropSnapShot) 
 			return err
 		}
 	}
+
+	getLogger(ses.GetService()).Info(fmt.Sprintf("drop snapshot %s success", string(stmt.Name)))
 	return err
 }
 
