4faf7ac852f30274e349f4999876882fcb41febb
Kai Cao <ck89119@users.noreply.github.com>
2024-11-18 14:12:22 +0800

    Merge mo pubs (#20096)
    Merge all mo_pubs tables into sys account
    
    Approved by: @aressu1985, @daviszhen, @aunjgr, @zhangxu19830126, @sukki37

--- pkg/frontend/snapshot.go
+++ pkg/frontend/snapshot.go
@@ -68,8 +68,6 @@ var (
 
 	restoreTableDataByNameFmt = "insert into `%s`.`%s` SELECT * FROM `%s`.`%s` {SNAPSHOT = '%s'}"
 
-	getPubInfoWithSnapshotFormat = `select pub_name, database_name, database_id, table_list, account_list, created_time, update_time, owner, creator, comment from mo_catalog.mo_pubs {snapshot = '%s'} where database_name = '%s';`
-
 	getRestoreAccountsFmt = "select account_name, account_id from mo_catalog.mo_account where account_name in (select account_name from mo_catalog.mo_account {MO_TS = %d }) ORDER BY account_id ASC;"
 
 	getSubsSqlFmt = "select sub_account_id, sub_name, sub_time, pub_account_name, pub_name, pub_database, pub_tables, pub_time, pub_comment, status from mo_catalog.mo_subs %s where 1=1"
@@ -413,13 +411,13 @@ func doRestoreSnapshot(ctx context.Context, ses *Session, stmt *tree.RestoreSnap
 		var srcAccountId uint32
 		srcAccountId, err = getAccountId(ctx, bh, srcAccountName)
 		if err != nil {
-			return stats, err
+			return
 		}
 
 		var sp string
 		sp, err = insertSnapshotRecord(ctx, ses.GetService(), bh, snapshot.snapshotName, snapshot.ts, uint64(srcAccountId), srcAccountName)
 		if err != nil {
-			return stats, err
+			return
 		}
 		snapshotName = sp
 		defer func() {
@@ -434,14 +432,16 @@ func doRestoreSnapshot(ctx context.Context, ses *Session, stmt *tree.RestoreSnap
 		// restore cluster
 		subDbToRestore := make(map[string]*subDbRestoreRecord)
 		if err = restoreToCluster(ctx, ses, bh, snapshotName, snapshot.ts, subDbToRestore); err != nil {
-			return stats, err
+			return
 		}
 
-		if len(subDbToRestore) > 0 {
-			for _, subDb := range subDbToRestore {
-				if err = restoreToSubDb(ctx, ses.GetService(), bh, snapshotName, subDb); err != nil {
-					return stats, err
-				}
+		if err = restorePubsWithSnapshotName(ctx, ses.GetService(), bh, snapshotName); err != nil {
+			return
+		}
+
+		for _, subDb := range subDbToRestore {
+			if err = restoreToSubDb(ctx, ses.GetService(), bh, snapshotName, subDb); err != nil {
+				return
 			}
 		}
 		return
@@ -850,13 +850,6 @@ func restoreToDatabaseOrTable(
 			return
 		}
 	}
-
-	if !restoreToTbl {
-		getLogger(sid).Info(fmt.Sprintf("[%s] start to create pub: %v", snapshotName, dbName))
-		if err = createPub(ctx, sid, bh, snapshotName, dbName, toAccountId); err != nil {
-			return
-		}
-	}
 	return
 }
 
@@ -1236,8 +1229,11 @@ func doResolveSnapshotWithSnapshotName(ctx context.Context, ses FeSession, snaps
 	}
 
 	var accountId uint32
-	if accountId, err = getAccountId(ctx, bh, record.accountName); err != nil {
-		return
+	// cluster level record has no accountName, so accountId is 0
+	if record.accountName != "" {
+		if accountId, err = getAccountId(ctx, bh, record.accountName); err != nil {
+			return
+		}
 	}
 
 	return &pbplan.Snapshot{
@@ -1757,8 +1753,11 @@ func getSnapshotPlanWithSharedBh(ctx context.Context, bh BackgroundExec, snapsho
 	}
 
 	var accountId uint32
-	if accountId, err = getAccountId(ctx, bh, record.accountName); err != nil {
-		return
+	// cluster level record has no accountName, so accountId is 0
+	if record.accountName != "" {
+		if accountId, err = getAccountId(ctx, bh, record.accountName); err != nil {
+			return
+		}
 	}
 
 	return &pbplan.Snapshot{
@@ -1838,33 +1837,31 @@ func checkDatabaseIsMaster(
 	return false, nil
 }
 
-// createPub create pub after the database is created
-func createPub(
+// only used in restore cluster
+func restorePubsWithSnapshotName(
 	ctx context.Context,
 	sid string,
 	bh BackgroundExec,
-	snapshotName,
-	dbName string,
-	toAccountId uint32) (err error) {
-	// read pub info from mo_pubs
-	sql := fmt.Sprintf(getPubInfoWithSnapshotFormat, snapshotName, dbName)
-	bh.ClearExecResultSet()
-	getLogger(sid).Info(fmt.Sprintf("[%s] create pub: get pub info sql: %s", snapshotName, sql))
+	snapshotName string,
+) (err error) {
+	getLogger(sid).Info(fmt.Sprintf("[%s] start to restorePub with snapshot", snapshotName))
 
-	if err = bh.Exec(ctx, sql); err != nil {
+	var pubInfos []*pubsub.PubInfo
+	if pubInfos, err = getAllPubInfosBySnapshotName(ctx, bh, snapshotName); err != nil {
 		return
 	}
 
-	erArray, err := getResultSet(ctx, bh)
-	if err != nil {
-		return
-	}
+	return createPubs(ctx, sid, bh, snapshotName, pubInfos)
+}
 
-	pubInfos, err := extractPubInfosFromExecResult(ctx, erArray)
-	if err != nil {
-		return err
-	}
-
+// createPub create pub after the database is created
+func createPubs(
+	ctx context.Context,
+	sid string,
+	bh BackgroundExec,
+	snapshotName string,
+	pubInfos []*pubsub.PubInfo,
+) (err error) {
 	// restore pub to toAccount
 	var ast []tree.Statement
 	defer func() {
@@ -1874,7 +1871,7 @@ func createPub(
 	}()
 
 	for _, pubInfo := range pubInfos {
-		toCtx := defines.AttachAccount(ctx, toAccountId, pubInfo.Owner, pubInfo.Creator)
+		toCtx := defines.AttachAccount(ctx, pubInfo.PubAccountId, pubInfo.Owner, pubInfo.Creator)
 		if ast, err = mysql.Parse(toCtx, pubInfo.GetCreateSql(), 1); err != nil {
 			return
 		}
@@ -1893,7 +1890,7 @@ func checkPubExistOrNot(
 	bh BackgroundExec,
 	snapshotName string,
 	subName string,
-	timsStampTs int64,
+	timestampTs int64,
 ) (bool, error) {
 	getLogger(sid).Info(fmt.Sprintf("[%s] start to check pub exist or not", snapshotName))
 	subInfos, err := getSubInfosFromSubWithSnapshot(
@@ -1902,7 +1899,7 @@ func checkPubExistOrNot(
 		bh,
 		snapshotName,
 		subName,
-		timsStampTs)
+		timestampTs)
 	if err != nil {
 		return false, err
 	} else if len(subInfos) == 0 {
@@ -1911,7 +1908,7 @@ func checkPubExistOrNot(
 
 	subInfo := subInfos[0]
 	var isPubValid bool
-	isPubValid, err = checkSubscriptionExist(
+	isPubValid, err = checkPubValid(
 		ctx,
 		sid,
 		bh,
@@ -1972,7 +1969,7 @@ func getSubInfosFromSubWithSnapshot(
 	return extractSubInfosFromExecResult(ctx, erArray)
 }
 
-func checkSubscriptionExist(
+func checkPubValid(
 	ctx context.Context,
 	sid string,
 	bh BackgroundExec,
