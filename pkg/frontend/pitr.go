239a78e5d72721c137950b4e3c40c8ea56d98bc6
YANGGMM <18916107305@163.com>
2024-11-18 10:32:00 +0800

    fix

--- pkg/frontend/pitr.go
+++ pkg/frontend/pitr.go
@@ -1539,7 +1539,7 @@ func getCreateTableSqlWithTs(ctx context.Context, bh BackgroundExec, ts int64, d
 	// cols: table_name, create_sql
 	colsList, err := getStringColsList(ctx, bh, sql, 1)
 	if err != nil {
-		return "", err
+		return "", nil
 	}
 	if len(colsList) == 0 || len(colsList[0]) == 0 {
 		return "", moerr.NewNoSuchTable(ctx, dbName, tblName)
