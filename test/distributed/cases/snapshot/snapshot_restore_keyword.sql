7885dc72f0af4ebf0e5f2c97975a153cfb83dcd4
Ariznawl@163.com <ariznawl@163.com>
2024-11-19 14:34:39 +0800

    add case for restore db/table where db_name or table_name named with keywords(2.0-dev)

--- test/distributed/cases/snapshot/snapshot_restore_keyword.sql
+++ test/distributed/cases/snapshot/snapshot_restore_keyword.sql
@@ -29,3 +29,50 @@ select * from `mocloud_meta`.`lock`;
 drop snapshot if exists metadb202411181350;
 drop account if exists `0192fbbb-bfb0-7d54-a2fe-b7fd26dbdb14`;
 drop account if exists `01929da4-89af-7ed8-8f32-8df9ed5ddd71`;
+
+
+-- db_name,table_name is keywords
+drop database if exists `select`;
+create database `select`;
+use `select`;
+drop table if exists `_binary`;
+create table `_binary` (`add` int, `all` bigint, `alter` smallint, `analyze` decimal, `and` char, `as` varchar, `asc` int, `begin` float);
+show create table `_binary`;
+
+drop snapshot if exists sp01;
+create snapshot sp01 for account sys;
+
+drop table `_binary`;
+restore account sys from snapshot sp01;
+show databases;
+use `select`;
+show tables;
+show create table `_binary`;
+
+drop database `select`;
+drop snapshot sp01;
+
+
+
+
+-- db_name,table_name,snapshot_name is keywords
+drop database if exists `column`;
+create database `column`;
+use `column`;
+drop table if exists `current_date`;
+create table `current_date` (`current_role` int, `current_time` bigint, `current_timestamp` smallint, `current_user` decimal, `database` char, `databases` varchar, `day_hour` int, `day_microsecond` float);
+show create table `current_date`;
+drop table `current_date`;
+
+drop snapshot if exists `div`;
+create snapshot `div` for account sys;
+
+drop database `column`;
+restore account sys from snapshot `div`;
+
+use `column`;
+show tables;
+show create table `current_date`;
+
+drop database `column`;
+drop snapshot `div`;
