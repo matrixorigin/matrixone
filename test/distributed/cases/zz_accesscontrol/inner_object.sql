dccf2795489648ef4d1a814396da5264a40c8b9e
XuPeng-SH <xupeng3112@163.com>
2024-11-20 17:18:18 +0800

    update 2

--- test/distributed/cases/zz_accesscontrol/inner_object.sql
+++ test/distributed/cases/zz_accesscontrol/inner_object.sql
@@ -9,7 +9,7 @@ select user_name,owner from mo_catalog.mo_user where user_name="root";
 select role_id,role_name,owner from mo_catalog.mo_role where role_name in ("moadmin","public");
 
 select enable_fault_injection();
-select add_fault_point('fj/trace/partitionstate', ':::', 'echo', 1, 'mo_tables');
+select add_fault_point('fj/trace/partitionstate', ':::', 'echo', 20, 'mo_tables');
 
 --验证moadminaccount初始化，sys租户root下创建普通租户下管理员用户查看
 create account account1 ADMIN_NAME 'admin' IDENTIFIED BY '123456';
