a8d1fa81c0f27cb112a89742d9aafb514b3d8199
Wenbin1002 <waterdance1002@gmail.com>
2024-11-21 11:56:09 +0800

    enhance fault injection

--- pkg/sql/plan/function/ctl/cmd_addfaultpoint.go
+++ pkg/sql/plan/function/ctl/cmd_addfaultpoint.go
@@ -15,17 +15,23 @@
 package ctl
 
 import (
+	"context"
+	"fmt"
 	"github.com/fagongzi/util/protoc"
+	"github.com/matrixorigin/matrixone/pkg/clusterservice"
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/pb/api"
+	"github.com/matrixorigin/matrixone/pkg/pb/metadata"
+	"github.com/matrixorigin/matrixone/pkg/pb/query"
+	"github.com/matrixorigin/matrixone/pkg/util/fault"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/cmd_util"
 	"github.com/matrixorigin/matrixone/pkg/vm/process"
 	"strconv"
 	"strings"
 )
 
-func handleAddFaultPoint() handleFunc {
+func handleTNAddFaultPoint() handleFunc {
 	return GetTNHandlerFunc(
 		api.OpCode_OpAddFaultPoint,
 		func(string) ([]uint64, error) { return nil, nil },
@@ -72,4 +78,137 @@ func handleAddFaultPoint() handleFunc {
 			protoc.MustUnmarshal(&resp, data)
 			return resp, nil
 		})
+}
+
+func handleAddFaultPoint(
+	proc *process.Process,
+	service serviceType,
+	parameter string,
+	sender requestSender,
+) (Result, error) {
+	if service != cn && service != tn {
+		return Result{}, moerr.NewInvalidInputNoCtxf("invalid service %s", service)
+	}
+
+	if service == tn {
+		return handleTNAddFaultPoint()(proc, service, parameter, sender)
+	}
+
+	// parameter like "cnid.name.freq.action.iarg.sarg"
+	name, freq, action, iarg, sarg, cns, err := getInputs(proc.Ctx, parameter)
+	if err != nil {
+		return Result{}, err
+	}
+
+	if len(cns) == 1 && strings.ToLower(cns[0]) == "all" {
+		clusterservice.GetMOCluster(proc.GetService()).GetCNService(
+			clusterservice.Selector{},
+			func(cn metadata.CNService) bool {
+				cns = make([]string, 0)
+				cns = append(cns, cn.ServiceID)
+				return true
+			},
+		)
+	}
+
+	info := map[string]string{}
+	for idx := range cns {
+		// the current cn also need to process this span cmd
+		if cns[idx] == proc.GetQueryClient().ServiceID() {
+			info[cns[idx]] = HandleCnFaultInjection(proc.Ctx, name, freq, action, iarg, sarg)
+		} else {
+			request := proc.GetQueryClient().NewRequest(query.CmdMethod_FaultInjection)
+			request.FaultInjectionRequest = &query.FaultInjectionRequest{
+				Name:   name,
+				Freq:   freq,
+				Action: action,
+				Iarg:   iarg,
+				Sarg:   sarg,
+			}
+			// transfer query to another cn and receive its response
+			resp, _ := transferRequest2OtherCNs(proc, cns[idx], request)
+			if resp == nil {
+				// no such cn service
+				info[cns[idx]] = "no such cn service"
+			} else {
+				info[cns[idx]] = resp.TraceSpanResponse.Resp
+			}
+		}
+	}
+
+	data := ""
+	for k, v := range info {
+		data += fmt.Sprintf("%s:%s; ", k, v)
+	}
+
+	return Result{
+		Method: AddFaultPointMethod,
+		Data:   data,
+	}, nil
+}
+
+func getInputs(ctx context.Context, input string) (
+	name string,
+	freq string,
+	action string,
+	iarg int64,
+	sarg string,
+	cns []string,
+	err error,
+) {
+	args := strings.Split(input, ".")
+	// cn uuid
+	if len(args) < 2 {
+		err = moerr.NewInternalError(ctx, "invalid argument! should be like \"cnid.name.freq.action.iarg.sarg\"")
+		return
+	}
+	cns = strings.Split(args[0], ",")
+	name = args[1]
+	if name == "" {
+		err = moerr.NewInternalError(ctx, "fault point's name should not be empty!")
+		return
+	}
+	if len(args) == 2 {
+		return
+	}
+	if len(args) != 6 {
+		err = moerr.NewInternalError(ctx, "invalid argument! should be like \"cnid.name.freq.action.iarg.sarg\"")
+		return
+	}
+	freq = args[2]
+	action = args[3]
+	i, err := strconv.Atoi(args[4])
+	iarg = int64(i)
+	if err != nil {
+		return
+	}
+	sarg = args[5]
+
+	return
+}
+
+const (
+	enable  = "enable_fault_injection"
+	disable = "disable_fault_injection"
+)
+
+func HandleCnFaultInjection(
+	ctx context.Context,
+	name string,
+	freq string,
+	action string,
+	iarg int64,
+	sarg string,
+) string {
+	switch name {
+	case enable:
+		fault.Enable()
+	case disable:
+		fault.Disable()
+	default:
+		if err := fault.AddFaultPoint(ctx, name, freq, action, iarg, sarg); err != nil {
+			return err.Error()
+		}
+	}
+	return "OK"
 }
