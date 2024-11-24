4f1033c2ea18728ae556cdbed8069bf3073774a0
fagongzi <zhangxu19830126@gmail.com>
2024-11-24 18:57:22 +0800

    Revert "speed up abort all active txn (#20253)" (#20324)

--- pkg/sql/compile/compile2.go
+++ pkg/sql/compile/compile2.go
@@ -144,6 +144,12 @@ func (c *Compile) Run(_ uint64) (queryResult *util2.RunResult, err error) {
 	c.proc.ResetCloneTxnOperator()
 	c.InitPipelineContextToExecuteQuery()
 
+	// record this query to compile service.
+	GetCompileService().recordRunningCompile(c, txnOperator)
+	defer func() {
+		GetCompileService().removeRunningCompile(c, txnOperator)
+	}()
+
 	// check if there is any action to cancel this query.
 	if err = thisQueryStillRunning(c.proc, txnOperator); err != nil {
 		return nil, err
@@ -164,7 +170,6 @@ func (c *Compile) Run(_ uint64) (queryResult *util2.RunResult, err error) {
 		seq = txnOperator.NextSequence()
 		writeOffset = uint64(txnOperator.GetWorkspace().GetSnapshotWriteOffset())
 		txnOperator.GetWorkspace().IncrSQLCount()
-		txnOperator.EnterRunSql()
 	}
 
 	var isExplainPhyPlan = false
@@ -178,9 +183,6 @@ func (c *Compile) Run(_ uint64) (queryResult *util2.RunResult, err error) {
 		// if a rerun occurs, it differs from the original c, so we need to release it.
 		if runC != c {
 			runC.Release()
-		}
-		if txnOperator != nil {
-			txnOperator.ExitRunSql()
 		}
 	}()
 
