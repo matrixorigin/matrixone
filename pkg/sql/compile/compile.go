cd024d5597c3f8a09b9096ea37408e65c9d8d8e5
chenmingsong <59043531+m-schen@users.noreply.github.com>
2024-11-26 23:49:34 +0800

    remove a global recorder for Compile (#20376)
    移除compileService相关代码
    
    Approved by: @badboynt1, @ouyuanning, @sukki37, @aunjgr, @qingxinhome

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -146,7 +146,7 @@ func (c *Compile) Release() {
 		c.proc.ResetQueryContext()
 		c.proc.ResetCloneTxnOperator()
 	}
-	releaseCompile(c)
+	doCompileRelease(c)
 }
 
 func (c Compile) TypeName() string {
