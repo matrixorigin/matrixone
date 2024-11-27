c6e626478521f4b9b2fba6114149f51851412311
Jackson <xzxiong@yeah.net>
2024-11-27 17:06:07 +0800

    Revert "fix case agrr-no-merge."
    This reverts commit acba10e15fa5942bb73eb36a3a9e76d19854062c.

--- pkg/util/trace/impl/motrace/report_statement.go
+++ pkg/util/trace/impl/motrace/report_statement.go
@@ -79,6 +79,9 @@ func StatementInfoNew(i table.Item, ctx context.Context) table.Item {
 
 		// initialize the AggrCount as 0 here since aggr is not started
 		stmt.AggrCount = 0
+		// fixme: StmtBuilder maybe not best choose
+		stmt.StmtBuilder.Reset()
+		stmt.StmtBuilder.Write(s.Statement)
 
 		return stmt
 	}
@@ -86,7 +89,6 @@ func StatementInfoNew(i table.Item, ctx context.Context) table.Item {
 }
 
 func StatementInfoUpdate(ctx context.Context, existing, new table.Item) {
-	enableStmtMerge := GetTracerProvider().enableStmtMerge
 
 	e := existing.(*StatementInfo)
 	n := new.(*StatementInfo)
@@ -106,15 +108,9 @@ func StatementInfoUpdate(ctx context.Context, existing, new table.Item) {
 		e.RequestAt = e.ResponseAt.Truncate(windowSize)
 		e.ResponseAt = e.RequestAt.Add(windowSize)
 		e.AggrCount = 1
-		// append the first query.
-		if enableStmtMerge {
-			// fixme: StmtBuilder maybe not best choose
-			e.StmtBuilder.Reset()
-			e.StmtBuilder.Write(e.Statement)
-		}
 	}
 	// update the stats
-	if enableStmtMerge {
+	if GetTracerProvider().enableStmtMerge {
 		e.StmtBuilder.WriteString(";\n")
 		e.StmtBuilder.Write(n.Statement)
 	}
