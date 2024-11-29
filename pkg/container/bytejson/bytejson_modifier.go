88387a8dceb831d75271e1f091b988ad33170b8b
YANGGMM <18916107305@163.com>
2024-11-29 14:50:07 +0800

    support

---
+++ pkg/container/bytejson/bytejson_modifier.go
@@ -0,0 +1,101 @@
+// Copyright 2024 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bytejson
+
+import "github.com/matrixorigin/matrixone/pkg/common/moerr"
+
+type bytejsonModifier struct {
+	bj        ByteJson
+	modifyPtr *byte
+	modifyVal ByteJson
+}
+
+func (bm *bytejsonModifier) set(path *Path, newBj ByteJson) (ByteJson, error) {
+	result := bm.bj.querySimple(path)
+	if CompareByteJson(result, Null) != 0 {
+		// set
+		bm.modifyPtr = &result.Data[0]
+		bm.modifyVal = newBj
+		return bm.rebuild(), nil
+	}
+	// insert
+	// return bm.rebuild()
+	return Null, moerr.NewInvalidArgNoCtx("invalid path", path.String())
+}
+
+func (bm *bytejsonModifier) rebuild() ByteJson {
+	buf := make([]byte, 0, len(bm.bj.Data)+len(bm.modifyVal.Data))
+	value, tpCode := bm.rebuildTo(buf)
+	return ByteJson{Type: tpCode, Data: value}
+}
+
+func (bm *bytejsonModifier) rebuildTo(buf []byte) ([]byte, TpCode) {
+	if bm.modifyPtr == &bm.bj.Data[0] {
+		bm.modifyPtr = nil
+		return append(buf, bm.modifyVal.Data...), bm.modifyVal.Type
+	} else if bm.modifyPtr == nil {
+		return append(buf, bm.bj.Data...), bm.bj.Type
+	}
+
+	bj := bm.bj
+	if bj.Type != TpCodeArray && bj.Type != TpCodeObject {
+		return append(buf, bj.Data...), bj.Type
+	}
+
+	docOff := len(buf)
+	elemCount := bj.GetElemCnt()
+
+	var valEntryStart int
+	if bj.Type == TpCodeArray {
+		// json array
+		copySize := headerSize + elemCount*valEntrySize
+		valEntryStart = headerSize
+		buf = append(buf, bj.Data[:copySize]...)
+	} else {
+		// josn object
+		copySize := headerSize + elemCount*(keyEntrySize+valEntrySize)
+		valEntryStart = headerSize + elemCount*keyEntrySize
+		buf = append(buf, bj.Data[:copySize]...)
+		if elemCount > 0 {
+			firstKeyOff := int(endian.Uint32(bj.Data[headerSize:]))
+			lastKeyOff := int(endian.Uint32(bj.Data[headerSize+keyEntrySize*(elemCount-1):]))
+			lastKeyLen := int(endian.Uint16(bj.Data[headerSize+keyEntrySize*(elemCount-1)+docSizeOff:]))
+			buf = append(buf, bj.Data[firstKeyOff:lastKeyOff+lastKeyLen]...)
+		}
+	}
+
+	for i := 0; i < elemCount; i++ {
+		valEntryOff := valEntryStart + i*valEntrySize
+		elem := bj.getValEntry(valEntryOff)
+		bm.bj = elem
+		var tpCode TpCode
+		valOff := len(buf) - docOff
+		buf, tpCode = bm.rebuildTo(buf)
+		buf[docOff+valEntryOff] = tpCode
+		if tpCode == TpCodeLiteral {
+			lastIdx := len(buf) - 1
+			endian.PutUint32(buf[docOff+valEntryOff+valTypeSize:], uint32(buf[lastIdx]))
+			buf = buf[:lastIdx]
+		} else {
+			endian.PutUint32(buf[docOff+valEntryOff+valTypeSize:], uint32(valOff))
+		}
+	}
+	endian.PutUint32(buf[docOff+docSizeOff:], uint32(len(buf)-docOff))
+	return buf, bj.Type
+}
+
+func (bm *bytejsonModifier) doInsert(path *Path, newBj ByteJson) {
+
+}
--- pkg/sql/plan/function/function_id.go
+++ pkg/sql/plan/function/function_id.go
@@ -289,6 +289,11 @@ const (
 	JSON_QUOTE
 	JSON_UNQUOTE
 	JSON_ROW
+
+	JSON_SET
+	JSON_INSERT
+	JSON_REPLACE
+
 	JQ
 	TRY_JQ
 	WASM
@@ -601,6 +606,9 @@ var functionIdRegister = map[string]int32{
 	"json_quote":                     JSON_QUOTE,
 	"json_unquote":                   JSON_UNQUOTE,
 	"json_row":                       JSON_ROW,
+	"josn_set":                       JSON_SET,
+	"json_insert":                    JSON_INSERT,
+	"json_replace":                   JSON_REPLACE,
 	"jq":                             JQ,
 	"try_jq":                         TRY_JQ,
 	"wasm":                           WASM,
