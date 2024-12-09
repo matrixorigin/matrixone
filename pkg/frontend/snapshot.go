806ccfcdfda37c2dea8465d2c92a94f3dd184155
YANGGMM <18916107305@163.com>
2024-12-09 11:43:01 +0800

    fix restore view with lower case table namse

--- pkg/frontend/snapshot.go
+++ pkg/frontend/snapshot.go
@@ -1006,7 +1006,7 @@ func restoreViews(
 
 	g := toposort{next: make(map[string][]string)}
 	for key, view := range viewMap {
-		stmts, err := parsers.Parse(ctx, dialect.MYSQL, view.createSql, 1)
+		stmts, err := parsers.Parse(ctx, dialect.MYSQL, view.createSql, 0)
 		if err != nil {
 			return err
 		}
