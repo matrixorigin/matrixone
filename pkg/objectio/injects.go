f033ff53511db62476cf0a977862a86dd052b289
Wenbin <85331908+Wenbin1002@users.noreply.github.com>
2024-12-13 20:52:24 +0800

    Refactor fault injection (#20523)
    新增fault_inject函数，与mo_ctl平级，只有admin可以使用，语法如下：
    ```
    select fault_inject('pod','method','parameter');
    ```
    在原来的fault inject基础上增添了一些新特性
    * 指定CN|DN
    * 指定fault key 是否为constant
    * 更好的可读性
    
    具体用法参考文档：
    https://github.com/matrixorigin/docs/blob/main/tech-notes/dnservice/fault_injection.md
    
    Approved by: @LeftHandCold, @m-schen, @qingxinhome, @badboynt1, @ouyuanning, @reusee, @daviszhen, @zhangxu19830126, @aunjgr, @XuPeng-SH, @sukki37

--- pkg/objectio/injects.go
+++ pkg/objectio/injects.go
@@ -225,6 +225,7 @@ func InjectLogging(
 		"echo",
 		iarg,
 		sarg,
+		false,
 	); err != nil {
 		return
 	}
@@ -258,6 +259,7 @@ func InjectPrefetchThreshold(threshold int) (rmFault func(), err error) {
 		"echo",
 		int64(threshold),
 		"",
+		false,
 	); err != nil {
 		return
 	}
