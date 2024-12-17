d0b15030a3aa7a9d992c6c69d54202ea601b89a5
Cao Kai <ck89119@gmail.com>
2024-12-17 16:33:55 +0800

    add ut

--- pkg/sql/plan/utils.go
+++ pkg/sql/plan/utils.go
@@ -2721,7 +2721,7 @@ func offsetToString(offset int) string {
 // return !strings.HasPrefix(tableDef.Name, catalog.IndexTableNamePrefix)
 // }
 
-// DbNameOfObjRef return subscription name of ObjectRef if exists,
+// DbNameOfObjRef return subscription name of ObjectRef if exists, to avoid the mismatching of account id and db name
 func DbNameOfObjRef(objRef *ObjectRef) string {
 	if objRef.SubscriptionName == "" {
 		return objRef.SchemaName
