c11a4d3b3a05b4df548f70fb9041f3fa0db0a703
YANGGMM <18916107305@163.com>
2024-12-17 18:11:27 +0800

    support create database/table level snapshot in parser

--- pkg/sql/parsers/dialect/mysql/mysql_sql_test.go
+++ pkg/sql/parsers/dialect/mysql/mysql_sql_test.go
@@ -2560,6 +2560,18 @@ var (
 			output: "alter publication pub1 account acc0 database db1 table t1, t2",
 		},
 		{
+			input: "create snapshot cluster_sp for cluster",
+		},
+		{
+			input: "create snapshot snapshot_01 for account account_01",
+		},
+		{
+			input: "create snapshot snapshot_01 for database db1",
+		},
+		{
+			input: "create snapshot snapshot_01 for table db1 t1",
+		},
+		{
 			input: "restore cluster from snapshot snapshot_01",
 		},
 		{
