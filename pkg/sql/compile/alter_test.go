ed813aa219b19c053f6f532156eefcb0c2b72327
qingxinhome <758355272@qq.com>
2024-12-18 19:20:22 +0800

    add ut test case

---
+++ pkg/sql/compile/alter_test.go
@@ -0,0 +1,292 @@
+// Copyright 2024 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package compile
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/prashantv/gostub"
+	"github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/matrixorigin/matrixone/pkg/common/buffer"
+	"github.com/matrixorigin/matrixone/pkg/common/moerr"
+	mock_frontend "github.com/matrixorigin/matrixone/pkg/frontend/test"
+	"github.com/matrixorigin/matrixone/pkg/pb/lock"
+	plan2 "github.com/matrixorigin/matrixone/pkg/pb/plan"
+	"github.com/matrixorigin/matrixone/pkg/sql/plan"
+	"github.com/matrixorigin/matrixone/pkg/testutil"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine"
+	"github.com/matrixorigin/matrixone/pkg/vm/process"
+)
+
+func TestScope_AlterTableInplace(t *testing.T) {
+	tableDef := &plan.TableDef{
+		TblId: 282826,
+		Name:  "dept",
+		Cols: []*plan.ColDef{
+			{
+				ColId: 0,
+				Name:  "deptno",
+				Alg:   plan2.CompressType_Lz4,
+				Typ: plan.Type{
+					Id:          27,
+					NotNullable: false,
+					AutoIncr:    true,
+					Width:       32,
+					Scale:       -1,
+				},
+				Default: &plan2.Default{},
+				NotNull: true,
+				Primary: true,
+				Pkidx:   0,
+			},
+			{
+				ColId: 1,
+				Name:  "dname",
+				Alg:   plan2.CompressType_Lz4,
+				Typ: plan.Type{
+					Id:          61,
+					NotNullable: false,
+					AutoIncr:    false,
+					Width:       15,
+					Scale:       0,
+				},
+				Default: &plan2.Default{},
+				NotNull: false,
+				Primary: false,
+				Pkidx:   0,
+			},
+			{
+				ColId: 2,
+				Name:  "loc",
+				Alg:   plan2.CompressType_Lz4,
+				Typ: plan.Type{
+					Id:          61,
+					NotNullable: false,
+					AutoIncr:    false,
+					Width:       50,
+					Scale:       0,
+				},
+				Default: &plan2.Default{},
+				NotNull: false,
+				Primary: false,
+				Pkidx:   0,
+			},
+		},
+		Pkey: &plan.PrimaryKeyDef{
+			Cols:        nil,
+			PkeyColId:   0,
+			PkeyColName: "deptno",
+			Names:       []string{"deptno"},
+		},
+		Defs: []*plan2.TableDef_DefType{
+			{
+				Def: &plan.TableDef_DefType_Properties{
+					Properties: &plan.PropertiesDef{
+						Properties: []*plan.Property{
+							{
+								Key:   "relkind",
+								Value: "r",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	alterTable := &plan2.AlterTable{
+		Database: "test",
+		TableDef: tableDef,
+		Actions: []*plan2.AlterTable_Action{
+			{
+				Action: &plan2.AlterTable_Action_AddIndex{
+					AddIndex: &plan2.AlterTableAddIndex{
+						DbName:                "test",
+						TableName:             "dept",
+						OriginTablePrimaryKey: "deptno",
+						IndexTableExist:       true,
+						IndexInfo: &plan2.CreateTable{
+							TableDef: &plan.TableDef{
+								Indexes: []*plan.IndexDef{
+									{
+										IndexName:      "idx",
+										Parts:          []string{"dname", "__mo_alias_deptno"},
+										Unique:         false,
+										IndexTableName: "__mo_index_secondary_0193d918",
+										TableExist:     true,
+									},
+								},
+							},
+							IndexTables: []*plan.TableDef{
+								{
+									Name: "__mo_index_secondary_0193d918-3e7b",
+									Cols: []*plan.ColDef{
+										{
+											Name: "__mo_index_idx_col",
+											Alg:  plan2.CompressType_Lz4,
+											Typ: plan.Type{
+												Id:          61,
+												NotNullable: false,
+												AutoIncr:    false,
+												Width:       65535,
+												Scale:       0,
+											},
+											NotNull: false,
+											Default: &plan2.Default{
+												NullAbility: false,
+											},
+											Pkidx: 0,
+										},
+										{
+											Name: "__mo_index_pri_col",
+											Alg:  plan2.CompressType_Lz4,
+											Typ: plan.Type{
+												Id:          27,
+												NotNullable: false,
+												AutoIncr:    false,
+												Width:       32,
+												Scale:       -1,
+											},
+											NotNull: false,
+											Default: &plan2.Default{
+												NullAbility: false,
+											},
+											Pkidx: 0,
+										},
+									},
+									Pkey: &plan2.PrimaryKeyDef{
+										PkeyColName: "__mo_index_idx_col",
+										Names:       []string{"__mo_index_idx_col"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	cplan := &plan.Plan{
+		Plan: &plan2.Plan_Ddl{
+			Ddl: &plan2.DataDefinition{
+				DdlType: plan2.DataDefinition_ALTER_TABLE,
+				Definition: &plan2.DataDefinition_AlterTable{
+					AlterTable: alterTable,
+				},
+			},
+		},
+	}
+
+	s := &Scope{
+		Magic:     AlterTable,
+		Plan:      cplan,
+		TxnOffset: 0,
+	}
+
+	sql := `alter table dept add index idx(dname)`
+
+	convey.Convey("create table lock mo_database", t, func() {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		proc := testutil.NewProcess()
+		proc.Base.SessionInfo.Buf = buffer.New()
+
+		ctx := context.Background()
+		proc.Ctx = context.Background()
+		txnCli, txnOp := newTestTxnClientAndOpWithPessimistic(ctrl)
+		proc.Base.TxnClient = txnCli
+		proc.Base.TxnOperator = txnOp
+		proc.ReplaceTopCtx(ctx)
+
+		relation := mock_frontend.NewMockRelation(ctrl)
+		relation.EXPECT().GetTableID(gomock.Any()).Return(uint64(1)).AnyTimes()
+
+		mockDb := mock_frontend.NewMockDatabase(ctrl)
+		mockDb.EXPECT().GetDatabaseId(gomock.Any()).Return("12").AnyTimes()
+		mockDb.EXPECT().Relation(gomock.Any(), gomock.Any(), gomock.Any()).Return(relation, nil).AnyTimes()
+
+		eng := mock_frontend.NewMockEngine(ctrl)
+		eng.EXPECT().Database(gomock.Any(), gomock.Any(), gomock.Any()).Return(mockDb, nil).AnyTimes()
+
+		getConstraintDef := gostub.Stub(&GetConstraintDef, func(_ context.Context, _ engine.Relation) (*engine.ConstraintDef, error) {
+			return nil, nil
+		})
+		defer getConstraintDef.Reset()
+
+		lockMoDb := gostub.Stub(&lockMoDatabase, func(_ *Compile, _ string, _ lock.LockMode) error {
+			return moerr.NewTxnNeedRetryWithDefChangedNoCtx()
+		})
+		defer lockMoDb.Reset()
+
+		c := NewCompile("test", "test", sql, "", "", eng, proc, nil, false, nil, time.Now())
+		assert.Error(t, s.AlterTableInplace(c))
+	})
+
+	convey.Convey("create table lock mo_tables", t, func() {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		proc := testutil.NewProcess()
+		proc.Base.SessionInfo.Buf = buffer.New()
+
+		ctx := context.Background()
+		proc.Ctx = context.Background()
+		txnCli, txnOp := newTestTxnClientAndOpWithPessimistic(ctrl)
+		proc.Base.TxnClient = txnCli
+		proc.Base.TxnOperator = txnOp
+		proc.ReplaceTopCtx(ctx)
+
+		relation := mock_frontend.NewMockRelation(ctrl)
+		relation.EXPECT().GetTableID(gomock.Any()).Return(uint64(1)).AnyTimes()
+
+		mockDb := mock_frontend.NewMockDatabase(ctrl)
+		mockDb.EXPECT().GetDatabaseId(gomock.Any()).Return("12").AnyTimes()
+		mockDb.EXPECT().Relation(gomock.Any(), gomock.Any(), gomock.Any()).Return(relation, nil).AnyTimes()
+
+		eng := mock_frontend.NewMockEngine(ctrl)
+		eng.EXPECT().Database(gomock.Any(), gomock.Any(), gomock.Any()).Return(mockDb, nil).AnyTimes()
+
+		getConstraintDef := gostub.Stub(&GetConstraintDef, func(_ context.Context, _ engine.Relation) (*engine.ConstraintDef, error) {
+			return nil, nil
+		})
+		defer getConstraintDef.Reset()
+
+		lockMoDb := gostub.Stub(&lockMoDatabase, func(_ *Compile, _ string, _ lock.LockMode) error {
+			return nil
+		})
+		defer lockMoDb.Reset()
+
+		lockMoTbl := gostub.Stub(&lockMoTable, func(_ *Compile, _ string, _ string, _ lock.LockMode) error {
+			return moerr.NewTxnNeedRetryNoCtx()
+		})
+		defer lockMoTbl.Reset()
+
+		lockTbl := gostub.Stub(&lockTable, func(_ context.Context, _ engine.Engine, _ *process.Process, _ engine.Relation, _ string, _ []string, _ bool) error {
+			return moerr.NewTxnNeedRetryNoCtx()
+		})
+		defer lockTbl.Reset()
+
+		c := NewCompile("test", "test", sql, "", "", eng, proc, nil, false, nil, time.Now())
+		assert.Error(t, s.AlterTableInplace(c))
+	})
+
+}
--- pkg/sql/compile/ddl.go
+++ pkg/sql/compile/ddl.go
@@ -3533,7 +3533,7 @@ func doLockTable(
 	return err
 }
 
-func lockTable(
+var lockTable = func(
 	ctx context.Context,
 	eng engine.Engine,
 	proc *process.Process,
@@ -3753,7 +3753,7 @@ func getLockVector(proc *process.Process, accountId uint32, names []string) (*ve
 	return vec, nil
 }
 
-func lockMoDatabase(c *Compile, dbName string, lockMode lock.LockMode) error {
+var lockMoDatabase = func(c *Compile, dbName string, lockMode lock.LockMode) error {
 	dbRel, err := getRelFromMoCatalog(c, catalog.MO_DATABASE)
 	if err != nil {
 		return err
@@ -3770,7 +3770,7 @@ func lockMoDatabase(c *Compile, dbName string, lockMode lock.LockMode) error {
 	return nil
 }
 
-func lockMoTable(
+var lockMoTable = func(
 	c *Compile,
 	dbName string,
 	tblName string,
