5082c3a0362cf415620841c640884992287f859b
davis zhen <daviszhen007@gmail.com>
2024-12-18 19:01:53 +0800

    refine bytes converting (#20805)
    原因：从profile上看，有make slice，有slice转string，然后又从string转slice这类操作。
    计算引擎中输出的slice格式的数据类型（types.T_json. types.T_char, types.T_varchar, types.T_blob, types.T_text, types.T_binary, types.T_varbinary, types.T_datalink）会先make slice，之后在编码输出时有这类转换。
    
    ![image](https://github.com/user-attachments/assets/b8061c6c-54ed-4f42-bd48-baafd520f698)
    
    
    修改：
    针对计算引擎中输出的slice格式的数据类型（types.T_json. types.T_char, types.T_varchar, types.T_blob, types.T_text, types.T_binary, types.T_varbinary, types.T_datalink），在编码输出到网络之前，直接引用计算引擎的slice，不再分配新slice。并且在编码时，不再做slice与string相互转换。
    
    这种修改可行的原因是：被引用的slice，会在之后的编码阶段被复制到网络缓存区。在batch转换完成后，就不再引用此slice了。所以是安全的。
    
    这种修改的限制：对于query result,select into file，show table status，不能做此修改。因为slice在被写入存储前，会被一直引用。如果直接引用slice，内容可能被修改。
    
    Approved by: @qingxinhome, @sukki37

--- pkg/frontend/mysql_protocol.go
+++ pkg/frontend/mysql_protocol.go
@@ -380,7 +380,7 @@ func (mp *MysqlProtocolImpl) Write(execCtx *ExecCtx, crs *perfcounter.CounterSet
 	ses := execCtx.ses.(*Session)
 	isShowTableStatus := ses.GetShowStmtType() == ShowTableStatus
 	for j := 0; j < n; j++ { //row index
-		err := extractRowFromEveryVector(execCtx.reqCtx, execCtx.ses, bat, j, mrs.Data[0])
+		err := extractRowFromEveryVector(execCtx.reqCtx, execCtx.ses, bat, j, mrs.Data[0], !isShowTableStatus)
 		if err != nil {
 			return err
 		}
@@ -1264,6 +1264,10 @@ func (mp *MysqlProtocolImpl) writeStringLenEnc(data []byte, pos int, value strin
 	return mp.writeStringFix(data, pos, value, len(value))
 }
 
+var AppendStringLenEnc = func(mp *MysqlProtocolImpl, value string) error {
+	return mp.appendStringLenEnc(value)
+}
+
 // append a string with length encoded to the buffer
 // return the buffer
 func (mp *MysqlProtocolImpl) appendStringLenEnc(value string) error {
@@ -1276,6 +1280,10 @@ func (mp *MysqlProtocolImpl) appendStringLenEnc(value string) error {
 		return err
 	}
 	return nil
+}
+
+var AppendCountOfBytesLenEnc = func(mp *MysqlProtocolImpl, value []byte) error {
+	return mp.appendCountOfBytesLenEnc(value)
 }
 
 // append bytes with length encoded to the buffer
@@ -2372,12 +2380,29 @@ func (mp *MysqlProtocolImpl) appendResultSetBinaryRow(mrs *MysqlResultSet, rowId
 		// Binary/varbinary will be sent out as varchar type.
 		case defines.MYSQL_TYPE_VARCHAR, defines.MYSQL_TYPE_VAR_STRING, defines.MYSQL_TYPE_STRING,
 			defines.MYSQL_TYPE_BLOB, defines.MYSQL_TYPE_TEXT, defines.MYSQL_TYPE_JSON:
-			if value, err := mrs.GetString(mp.ctx, rowIdx, i); err != nil {
+			if value, err := mrs.GetValue(mp.ctx, rowIdx, i); err != nil {
 				return err
 			} else {
-				err = mp.appendStringLenEnc(value)
-				if err != nil {
-					return err
+				switch realVal := value.(type) {
+				case []byte:
+					err = AppendCountOfBytesLenEnc(mp, realVal)
+					if err != nil {
+						return err
+					}
+				case string:
+					err = AppendStringLenEnc(mp, realVal)
+					if err != nil {
+						return err
+					}
+				default:
+					if value2, err3 := mrs.GetString(mp.ctx, rowIdx, i); err3 != nil {
+						return err3
+					} else {
+						err = AppendStringLenEnc(mp, value2)
+						if err != nil {
+							return err
+						}
+					}
 				}
 			}
 		// TODO: some type, we use string now. someday need fix it
@@ -2494,15 +2519,6 @@ func (mp *MysqlProtocolImpl) appendResultSetTextRow(mrs *MysqlResultSet, r uint6
 		}
 
 		switch mysqlColumn.ColumnType() {
-		case defines.MYSQL_TYPE_JSON:
-			if value, err2 := mrs.GetString(mp.ctx, r, i); err2 != nil {
-				return err2
-			} else {
-				err = mp.appendStringLenEnc(value)
-				if err != nil {
-					return err
-				}
-			}
 		case defines.MYSQL_TYPE_BOOL:
 			if value, err2 := mrs.GetString(mp.ctx, r, i); err2 != nil {
 				return err2
@@ -2624,13 +2640,30 @@ func (mp *MysqlProtocolImpl) appendResultSetTextRow(mrs *MysqlResultSet, r uint6
 			}
 		// Binary/varbinary will be sent out as varchar type.
 		case defines.MYSQL_TYPE_VARCHAR, defines.MYSQL_TYPE_VAR_STRING, defines.MYSQL_TYPE_STRING,
-			defines.MYSQL_TYPE_BLOB, defines.MYSQL_TYPE_TEXT:
-			if value, err2 := mrs.GetString(mp.ctx, r, i); err2 != nil {
+			defines.MYSQL_TYPE_BLOB, defines.MYSQL_TYPE_TEXT, defines.MYSQL_TYPE_JSON:
+			if value, err2 := mrs.GetValue(mp.ctx, r, i); err2 != nil {
 				return err2
 			} else {
-				err = mp.appendStringLenEnc(value)
-				if err != nil {
-					return err
+				switch realVal := value.(type) {
+				case []byte:
+					err = AppendCountOfBytesLenEnc(mp, realVal)
+					if err != nil {
+						return err
+					}
+				case string:
+					err = AppendStringLenEnc(mp, realVal)
+					if err != nil {
+						return err
+					}
+				default:
+					if value2, err3 := mrs.GetString(mp.ctx, r, i); err3 != nil {
+						return err3
+					} else {
+						err = AppendStringLenEnc(mp, value2)
+						if err != nil {
+							return err
+						}
+					}
 				}
 			}
 		case defines.MYSQL_TYPE_DATE:
