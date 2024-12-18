fa228feb8d5712d6aad75d317fb1761c18216339
Wei Ziran <weiziran125@gmail.com>
2024-12-18 12:23:46 +0800

    align merge with main (#20488)
    align merge with main
    
    Approved by: @aressu1985, @XuPeng-SH, @zhangxu19830126, @sukki37

--- pkg/vm/engine/tae/tables/jobs/mergeobjects.go
+++ pkg/vm/engine/tae/tables/jobs/mergeobjects.go
@@ -188,7 +188,7 @@ func (task *mergeObjectsTask) GetMPool() *mpool.MPool {
 	return task.rt.VectorPool.Transient.GetMPool()
 }
 
-func (task *mergeObjectsTask) HostHintName() string { return "DN" }
+func (task *mergeObjectsTask) HostHintName() string { return "TN" }
 
 func (task *mergeObjectsTask) LoadNextBatch(
 	ctx context.Context, objIdx uint32,
@@ -369,7 +369,7 @@ func (task *mergeObjectsTask) Execute(ctx context.Context) (err error) {
 		return moerr.NewInternalErrorNoCtxf("LockMerge give up in exec %v", task.Name())
 	}
 	phaseDesc = "1-DoMergeAndWrite"
-	if err = mergesort.DoMergeAndWrite(ctx, task.txn.String(), sortkeyPos, task, task.isTombstone); err != nil {
+	if err = mergesort.DoMergeAndWrite(ctx, task.txn.String(), sortkeyPos, task); err != nil {
 		return err
 	}
 
