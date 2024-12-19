15037ecddf6957760f812016891a0e09b3480335
jiangxinmeng1 <jiangxinmeng@matrixorigin.io>
2024-12-19 15:28:50 +0800

    fix

--- pkg/vm/engine/tae/db/open.go
+++ pkg/vm/engine/tae/db/open.go
@@ -253,6 +253,7 @@ func Open(
 		zap.Duration("replay-checkpoints-cost", time.Since(now)),
 		zap.String("max-checkpoint", checkpointed.ToString()),
 	)
+	ckpReplayer.Close()
 
 	now = time.Now()
 	db.Replay(dataFactory, checkpointed, ckpLSN, valid)
