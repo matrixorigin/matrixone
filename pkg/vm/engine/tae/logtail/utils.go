f491bcde51e312954466ad68e8cc1fdf6d71c5ea
jiangxinmeng1 <51114574+jiangxinmeng1@users.noreply.github.com>
2024-12-20 21:07:57 +0800

    Optimize replay (#20837)
    Optimize replay, replay checkpoint with 10 workers
    
    Approved by: @XuPeng-SH

--- pkg/vm/engine/tae/logtail/utils.go
+++ pkg/vm/engine/tae/logtail/utils.go
@@ -492,14 +492,15 @@ func NewGlobalCollector(
 }
 
 func (data *CheckpointData) ApplyReplayTo(
+	objectlistReplayer catalog.ObjectListReplayer,
 	c *catalog.Catalog,
 	dataFactory catalog.DataFactory,
 	forSys bool,
 ) (err error) {
 	objectInfo := data.GetTombstoneObjectBatchs()
-	c.OnReplayObjectBatch(objectInfo, true, dataFactory, forSys)
+	c.OnReplayObjectBatch(objectlistReplayer, objectInfo, true, dataFactory, forSys)
 	objectInfo = data.GetObjectBatchs()
-	c.OnReplayObjectBatch(objectInfo, false, dataFactory, forSys)
+	c.OnReplayObjectBatch(objectlistReplayer, objectInfo, false, dataFactory, forSys)
 	return
 }
 
