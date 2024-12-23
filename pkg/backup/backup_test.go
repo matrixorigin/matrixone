688bd2199d078aadb59a7d4295c38a34ed6bcb91
jiangxinmeng1 <jiangxinmeng@matrixorigin.io>
2024-12-23 15:43:14 +0800

    fix

--- pkg/backup/backup_test.go
+++ pkg/backup/backup_test.go
@@ -345,10 +345,10 @@ func TestBackupData4(t *testing.T) {
 	defer testutils.AfterTest(t)()
 	testutils.EnsureNoLeak(t)
 	ctx := context.Background()
-	
+
 	fault.Enable()
 	defer fault.Disable()
-	fault.AddFaultPoint(ctx, "back up UT", ":::", "echo", 0, "debug", false)
+	fault.AddFaultPoint(ctx, "back up UT", ":::", "echo", 0, "debug")
 	defer fault.RemoveFaultPoint(ctx, "back up UT")
 
 	opts := config.WithLongScanAndCKPOpts(nil)
