f34475673028ba7737012195e4aaff5c898ae785
gouhongshen <gouhongshen@hotmail.com>
2024-12-24 19:05:45 +0800

    adding bvt

--- test/distributed/cases/disttae/mo_table_stats/mo_table_stats3.sql
+++ test/distributed/cases/disttae/mo_table_stats/mo_table_stats3.sql
@@ -2,3 +2,15 @@
 use mo_catalog;
 -- @ignore:3,5,10,11,12
 show table status;
+
+drop database if exists testdb;
+create database testdb;
+
+use testdb;
+
+create table t1 (a int);
+insert into t1 select * from generate_series(1, 100*1000)g;
+-- @ignore:3,5,10,11,12
+show table status;
+
+drop database testdb;
