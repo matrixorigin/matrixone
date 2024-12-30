b341d95c9285a7cbee528c8c824292c9b08613bc
Wenbin1002 <waterdance1002@gmail.com>
2024-12-30 15:18:17 +0800

    ut

--- pkg/vm/engine/tae/rpc/tool_test.go
+++ pkg/vm/engine/tae/rpc/tool_test.go
@@ -16,9 +16,18 @@ package rpc
 
 import (
 	"context"
+	"path"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/matrixorigin/matrixone/pkg/common/moerr"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/catalog"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/db/testutil"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/tables"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/testutils"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/testutils/config"
 )
 
 func Test_objGetArg(t *testing.T) {
@@ -31,5 +40,63 @@ func Test_objGetArg(t *testing.T) {
 
 	_, err = get.GetData(ctx)
 	assert.Error(t, err)
+}
 
+func Test_gcArg(t *testing.T) {
+	defer testutils.AfterTest(t)()
+	testutils.EnsureNoLeak(t)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	opts := config.WithLongScanAndCKPOpts(nil)
+	tae := testutil.InitTestDB(ctx, ModuleName, t, opts)
+	defer tae.Close()
+	dir := path.Join(tae.Dir, "data")
+	dump := gcDumpArg{
+		ctx: &inspectContext{
+			db: tae,
+		},
+		file: path.Join(dir, "test"),
+	}
+
+	txn, _ := tae.StartTxn(nil)
+	database, _ := txn.CreateDatabase("db", "", "")
+	schema := catalog.MockSchema(1, 0)
+	schema.Extra.BlockMaxRows = 1000
+	schema.Extra.ObjectMaxBlocks = 2
+	rel, _ := database.CreateRelation(schema)
+	tableMeta := rel.GetMeta().(*catalog.TableEntry)
+	dataFactory := tables.NewDataFactory(tae.Runtime, tae.Dir)
+	tableFactory := dataFactory.MakeTableFactory()
+	table := tableFactory(tableMeta)
+	handle := table.GetHandle(false)
+	_, err := handle.GetAppender()
+	assert.True(t, moerr.IsMoErrCode(err, moerr.ErrAppendableObjectNotFound))
+	obj, _ := rel.CreateObject(false)
+	id := obj.GetMeta().(*catalog.ObjectEntry).AsCommonID()
+	appender := handle.SetAppender(id)
+	assert.NotNil(t, appender)
+
+	blkCnt := 3
+	rows := schema.Extra.BlockMaxRows * uint32(blkCnt)
+	_, _, toAppend, err := appender.PrepareAppend(false, rows, nil)
+	assert.Equal(t, schema.Extra.BlockMaxRows, toAppend)
+	assert.Nil(t, err)
+	t.Log(toAppend)
+
+	assert.NoError(t, dump.Run())
+
+	assert.NoError(t, tae.ForceCheckpoint(ctx, tae.TxnMgr.Now(), 0))
+
+	assert.NoError(t, dump.Run())
+
+	remove := gcRemoveArg{
+		file:    path.Join(dir, "test"),
+		oriDir:  dir,
+		tarDir:  path.Join(dir, "tar"),
+		modTime: time.Now().Unix(),
+	}
+
+	assert.NoError(t, remove.Run())
+
+	assert.NoError(t, err)
 }
