5fb08bc00a38f0a96c68bda8681ec8ab328e8bf2
cpegeric <cpegeric@gmail.com>
2025-01-03 09:12:02 +0000

    update

--- pkg/sql/plan/build_show_util_test.go
+++ pkg/sql/plan/build_show_util_test.go
@@ -118,7 +118,7 @@ func Test_buildTestShowCreateTable(t *testing.T) {
 				PRIMARY KEY (id),
 				FULLTEXT(json1) WITH PARSER json,
 				FULLTEXT(json1,json2) WITH PARSER json)`,
-			want: "CREATE TABLE `src` (\n  `id` bigint NOT NULL,\n  `json1` json DEFAULT NULL,\n  `json2` json DEFAULT NULL,\n  PRIMARY KEY (`id`),\n FULLTEXT(`json1`) WITH PARSER json,\n FULLTEXT(`json1`,`json2`) WITH PARSER json\n)",
+			want: "CREATE TABLE `src` (\n  `id` bigint NOT NULL,\n  `json1` json DEFAULT NULL,\n  `json2` json DEFAULT NULL,\n  PRIMARY KEY (`id`),\n FULLTEXT (`json1`) WITH PARSER json,\n FULLTEXT (`json1`,`json2`) WITH PARSER json\n)",
 		},
 		{
 			name: "test8",
