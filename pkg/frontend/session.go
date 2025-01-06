01e9467b45b6b351303bb5b859ace09b26bc20a7
daviszhen <daviszhen007@gmail.com>
2025-01-06 12:36:48 +0800

    disable logs

--- pkg/frontend/session.go
+++ pkg/frontend/session.go
@@ -1341,7 +1341,7 @@ func (ses *Session) AuthenticateUser(ctx context.Context, userInput string, dbNa
 	}
 
 	if needCheckHost {
-		ses.Infof(tenantCtx, "check client address %s", ses.clientAddr)
+		ses.Debugf(tenantCtx, "check client address %s", ses.clientAddr)
 		err = whetherValidIpInInvitedNodes(tenantCtx, ses, ses.clientAddr)
 		if err != nil {
 			return nil, err
@@ -1473,7 +1473,7 @@ func (ses *Session) AuthenticateUser(ctx context.Context, userInput string, dbNa
 	//------------------------------------------------------------------------------------------------------------------
 	// record the id :routine pair in RoutineManager
 	ses.getRoutineManager().accountRoutine.recordRoutine(tenantID, ses.getRoutine(), accountVersion)
-	ses.Info(ctx, tenant.String())
+	ses.Debug(ctx, tenant.String())
 	ses.SetCreateVersion(createVersion)
 
 	return GetPassWord(pwd)
