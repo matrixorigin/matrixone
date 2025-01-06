c8275559e81fef8ca19462871e43642908bdc67a
jiangxinmeng1 <jiangxinmeng@matrixorigin.io>
2025-01-06 17:49:27 +0800

    fix

--- pkg/vm/engine/tae/db/replay.go
+++ pkg/vm/engine/tae/db/replay.go
@@ -23,11 +23,11 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/logutil"
 	"github.com/matrixorigin/matrixone/pkg/objectio"
+	"go.uber.org/zap"
 
 	"sync"
 
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/catalog"
-	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/common"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/iface/txnif"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/logstore/driver"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/tables"
