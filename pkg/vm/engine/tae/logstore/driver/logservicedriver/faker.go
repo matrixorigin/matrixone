d5137f754d6d5353eef45557fcb47e15ae8729a4
XuPeng-SH <xupeng3112@163.com>
2025-01-19 00:44:59 +0800

    update 3

--- pkg/vm/engine/tae/logstore/driver/logservicedriver/faker.go
+++ pkg/vm/engine/tae/logstore/driver/logservicedriver/faker.go
@@ -16,12 +16,35 @@ package logservicedriver
 
 import (
 	"context"
+	"sort"
+	"sync"
+	"unsafe"
 
+	"github.com/matrixorigin/matrixone/pkg/common/moerr"
+	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/logservice"
 	pb "github.com/matrixorigin/matrixone/pkg/pb/logservice"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/logstore/driver"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/logstore/driver/entry"
 )
+
+var _ BackendClient = (*mockBackendClient)(nil)
+
+func MockTruncateLogRecord(psn uint64) logservice.LogRecord {
+	ret := logservice.LogRecord{
+		Type: pb.Internal,
+		Lsn:  psn,
+		Data: make([]byte, 8),
+	}
+	buf := types.EncodeUint64(&psn)
+	copy(ret.Data, buf)
+
+	return ret
+}
+
+func GetTruncatedPSNFromMockTruncateRecord(r logservice.LogRecord) uint64 {
+	return types.DecodeUint64(r.Data)
+}
 
 func makeMockLogRecord(pos int) logservice.LogRecord {
 	return logservice.LogRecord{
@@ -131,5 +154,102 @@ func (d *mockDriver) readFromBackend(
 	return
 }
 
-type mockClient struct {
+type mockBackendClient struct {
+	sync.RWMutex
+	store     []logservice.LogRecord
+	nextPSN   uint64
+	truncated uint64
+	shardId   uint64
+}
+
+func (c *mockBackendClient) Close() (err error) {
+	return
+}
+
+func (c *mockBackendClient) GetLogRecord(size int) logservice.LogRecord {
+	return logservice.NewUserLogRecord(c.shardId, size)
+}
+
+func (c *mockBackendClient) Append(
+	ctx context.Context, record logservice.LogRecord,
+) (psn uint64, err error) {
+	cloned := record.Clone()
+	c.Lock()
+	defer c.Unlock()
+	cloned.Lsn = c.nextPSN
+	cloned.Type = pb.UserRecord
+	c.store = append(c.store, cloned)
+	c.nextPSN++
+	return cloned.Lsn, nil
+}
+
+func (c *mockBackendClient) GetTruncatedLsn(
+	ctx context.Context,
+) (psn uint64, err error) {
+	c.RLock()
+	defer c.RUnlock()
+	return c.truncated, nil
+}
+
+func (c *mockBackendClient) Truncate(
+	ctx context.Context, psnIntent uint64,
+) (err error) {
+	c.Lock()
+	defer c.Unlock()
+	if psnIntent <= c.truncated {
+		err = moerr.NewInvalidTruncateLsn(ctx, c.shardId, psnIntent)
+		return
+	}
+	var offset int
+	for _, record := range c.store {
+		if record.Lsn >= psnIntent {
+			break
+		}
+		offset++
+	}
+	truncateRecord := MockTruncateLogRecord(psnIntent)
+	truncateRecord.Lsn = c.nextPSN
+	c.store = append(c.store, truncateRecord)
+	c.store = c.store[offset:]
+	c.truncated = psnIntent
+	c.nextPSN++
+	return
+}
+
+func (c *mockBackendClient) Read(
+	ctx context.Context, firstPSN, maxSize uint64,
+) (ret []logservice.LogRecord, nextPSN uint64, err error) {
+	c.RLock()
+	defer c.RUnlock()
+	var records []logservice.LogRecord
+	idx, found := sort.Find(len(c.store), func(i int) int {
+		if c.store[i].Lsn > firstPSN {
+			return 1
+		} else if c.store[i].Lsn == firstPSN {
+			return 0
+		} else {
+			return -1
+		}
+	})
+	if !found {
+		nextPSN = firstPSN
+		return
+	}
+	var (
+		i    = idx
+		size = 0
+	)
+	for ; i < len(c.store); i++ {
+		records = append(records, c.store[i])
+		size += len(c.store[i].Data) + int(unsafe.Sizeof(logservice.LogRecord{}))
+		if size >= int(maxSize) {
+			break
+		}
+	}
+	if i < len(c.store)-1 {
+		nextPSN = c.store[i+1].Lsn
+	} else {
+		nextPSN = c.nextPSN
+	}
+	return
 }
