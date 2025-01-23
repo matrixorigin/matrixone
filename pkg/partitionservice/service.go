fa2ec450aa67bcea63ace81b39f034a7e0401cab
fagongzi <zhangxu19830126@gmail.com>
2025-01-23 12:12:44 +0800

    add linear hash partition support (#21331)
    add linear hash partition support
    
    Approved by: @iamlinjunhong

--- pkg/partitionservice/service.go
+++ pkg/partitionservice/service.go
@@ -295,10 +295,8 @@ func (s *service) getManualPartitions(
 		TableName:    def.Name,
 		DatabaseName: def.DbName,
 		Method:       method,
-		// TODO: ???
-		Expression:  "",
-		Description: partitionDesc,
-		Columns:     validColumns,
+		Description:  partitionDesc,
+		Columns:      validColumns,
 	}
 
 	for i, p := range option.Partitions {
@@ -308,7 +306,7 @@ func (s *service) getManualPartitions(
 				Name:               p.Name.String(),
 				PartitionTableName: fmt.Sprintf("%s_%s", def.Name, p.Name.String()),
 				Position:           uint32(i),
-				Comment:            applyPartitionComment(p),
+				Expression:         applyPartitionComment(p),
 			},
 		)
 	}
