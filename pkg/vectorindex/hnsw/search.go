ae1de7ebdff23cf8e429b7fd43a28ab8beaffc03
Eric Lam <cpegeric@gmail.com>
2025-02-07 19:51:45 +0000

    update

--- pkg/vectorindex/hnsw/search.go
+++ pkg/vectorindex/hnsw/search.go
@@ -188,15 +188,13 @@ func (s *HnswSearch) Search(query []float32, limit uint) (keys []int64, distance
 	s.Mutex.Lock()
 	for s.Concurrency.Load() >= MaxUSearchThreads {
 		s.Mutex.Unlock()
-		time.Sleep(10 * time.Microsecond)
+		time.Sleep(time.Millisecond)
 		s.Mutex.Lock()
 	}
 	s.Concurrency.Add(1)
 	s.Mutex.Unlock()
 
 	defer func() {
-		s.Mutex.Lock()
-		defer s.Mutex.Unlock()
 		s.Concurrency.Add(int32(-1))
 	}()
 
