a88f47aa71bd988ff0de97d4d7fc0d8843e26e7b
gouhongshen <gouhongshen@hotmail.com>
2025-02-19 18:57:38 +0800

    remove cache, pState recorded in relData

--- pkg/vm/engine/disttae/merge.go
+++ pkg/vm/engine/disttae/merge.go
@@ -84,6 +84,12 @@ func newCNMergeTask(
 	targetObjSize uint32,
 ) (*cnMergeTask, error) {
 	relData := readutil.NewBlockListRelationData(1)
+	part, err := tbl.getPartitionState(ctx)
+	if err != nil {
+		return nil, err
+	}
+	relData.SetPState(part)
+
 	source, err := tbl.buildLocalDataSource(
 		ctx,
 		0,
