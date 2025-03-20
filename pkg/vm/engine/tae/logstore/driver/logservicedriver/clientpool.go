9c158446ec4e7c88aa510c565fe7b766c02a1595
XuPeng-SH <xupeng3112@163.com>
2025-03-20 20:34:33 +0800

    fix timeout - main (#21571)
    fix timeout
    
    Approved by: @jiangxinmeng1

--- pkg/vm/engine/tae/logstore/driver/logservicedriver/clientpool.go
+++ pkg/vm/engine/tae/logstore/driver/logservicedriver/clientpool.go
@@ -172,17 +172,20 @@ func (c *wrappedClient) Append(
 		}
 	}()
 
-	for ; retryTimes < c.pool.cfg.MaxRetryCount+1; retryTimes++ {
-		var cancel context.CancelFunc
-		ctx, cancel = context.WithTimeoutCause(
-			ctx, c.pool.cfg.MaxTimeout, timeoutCause,
+	for {
+		ctx, cancel := context.WithTimeoutCause(
+			ctx, DefaultOneTryTimeout, timeoutCause,
 		)
 		psn, err = c.wrapped.Append(ctx, c.buf)
 		cancel()
 		if err == nil {
 			break
 		}
-		time.Sleep(c.pool.cfg.RetryInterval())
+		retryTimes++
+		if time.Since(now) > c.pool.cfg.MaxTimeout {
+			break
+		}
+		time.Sleep(c.pool.cfg.RetryInterval(retryTimes))
 	}
 	return
 }
