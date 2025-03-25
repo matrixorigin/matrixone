8e7a9ed019956e591babffd84bf0455c120dfa93
bRong Njam <longran1989@gmail.com>
2025-03-25 19:12:26 +0800

    [opt] merge multiple filters on primary key (#21593)
    to mitigate performance issue on CDC
    
    Approved by: @ouyuanning

--- pkg/sql/plan/utils.go
+++ pkg/sql/plan/utils.go
@@ -364,8 +364,19 @@ func applyDistributivity(ctx context.Context, expr *plan.Expr) *plan.Expr {
 
 		condMap := make(map[string]int)
 
+		relPos := int32(-1)
 		for _, cond := range rightConds {
 			condMap[cond.String()] = JoinSideRight
+			if col := cond.GetF().Args[0].GetCol(); col != nil {
+				if relPos == -1 {
+					relPos = col.RelPos
+				} else if relPos != col.RelPos {
+					relPos = -2
+				}
+			}
+		}
+		if relPos >= 0 {
+			return expr
 		}
 
 		var commonConds, leftOnlyConds, rightOnlyConds []*plan.Expr
