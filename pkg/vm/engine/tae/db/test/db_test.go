00e6738dd68bd90f8f6f75e8a9781d5ccd3f8b28
GreatRiver <great.river703@gmail.com>
2025-03-27 15:49:04 +0800

    Fix TestPitrMeta

--- pkg/vm/engine/tae/db/test/db_test.go
+++ pkg/vm/engine/tae/db/test/db_test.go
@@ -7391,7 +7391,6 @@ func TestPitrMeta(t *testing.T) {
 	if db.Runtime.Scheduler.GetPenddingLSNCnt() != 0 {
 		return
 	}
-	db.ForceCheckpoint(ctx, tae.TxnMgr.Now())
 	db.DiskCleaner.GetCleaner().EnableGC()
 	assert.Equal(t, uint64(0), db.Runtime.Scheduler.GetPenddingLSNCnt())
 	initMinMerged := db.DiskCleaner.GetCleaner().GetMinMerged()
