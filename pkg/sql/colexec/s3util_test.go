08b2d2cc679a2da289e0594c51082100e92844c5
gouhongshen <gouhongshen@hotmail.com>
2025-04-01 13:16:52 +0800

    refactor cn s3 writer. (#21522)
    Refactor the CN s3 writer to simplify the CN flush routine; the users do not need to worry about the object split and other details.
    
    to flush a s3 object, you need:
    ```
    // NewCNS3TombstoneWriter(...)
    w := NewCNS3DataWriter(...)
    w.write(batch)
    objList := w.sync()
    
    // if you need to get the block info bat
    bat := w.FillBlockInfoBat()
    
    w.close()
    ```
    
    Approved by: @zhangxu19830126, @aunjgr, @ouyuanning, @XuPeng-SH, @fengttt

--- pkg/sql/colexec/s3util_test.go
+++ pkg/sql/colexec/s3util_test.go
@@ -17,17 +17,17 @@ import (
 	"context"
 	"testing"
 
-	"github.com/matrixorigin/matrixone/pkg/objectio"
-	"github.com/matrixorigin/matrixone/pkg/objectio/mergeutil"
-
-	"github.com/stretchr/testify/require"
-
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 	"github.com/matrixorigin/matrixone/pkg/common/mpool"
 	"github.com/matrixorigin/matrixone/pkg/container/batch"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
+	"github.com/matrixorigin/matrixone/pkg/defines"
+	"github.com/matrixorigin/matrixone/pkg/fileservice"
+	"github.com/matrixorigin/matrixone/pkg/objectio"
+	"github.com/matrixorigin/matrixone/pkg/objectio/mergeutil"
 	"github.com/matrixorigin/matrixone/pkg/testutil"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSortKey(t *testing.T) {
@@ -66,29 +66,36 @@ func TestSortKey(t *testing.T) {
 func TestSetStatsCNCreated(t *testing.T) {
 	proc := testutil.NewProc()
 	ctx := proc.Ctx
-	s3writer := &S3Writer{}
-	s3writer.sortIndex = 0
-	s3writer.isTombstone = true
-	_, err := s3writer.generateWriter(proc)
-	require.NoError(t, err)
 
-	bat := batch.NewWithSize(1)
+	bat := batch.NewWithSize(2)
 	bat.Vecs[0] = vector.NewVec(types.T_Rowid.ToType())
+	bat.Vecs[1] = vector.NewVec(types.T_int32.ToType())
 
 	for i := 0; i < 100; i++ {
 		row := types.RandomRowid()
-		err = vector.AppendFixed[types.Rowid](bat.Vecs[0], row, false, proc.GetMPool())
+		err := vector.AppendFixed[types.Rowid](bat.Vecs[0], row, false, proc.GetMPool())
+		require.NoError(t, err)
+
+		err = vector.AppendFixed[int32](bat.Vecs[1], int32(i), false, proc.GetMPool())
 		require.NoError(t, err)
 	}
 	bat.SetRowCount(100)
 
-	s3writer.StashBatch(proc, bat)
-	_, stats, err := s3writer.SortAndSync(ctx, proc)
+	fs, err := fileservice.Get[fileservice.FileService](proc.Base.FileService, defines.SharedFileServiceName)
 	require.NoError(t, err)
 
-	require.True(t, stats.GetCNCreated())
-	require.Equal(t, uint32(bat.VectorCount()), stats.BlkCnt())
-	require.Equal(t, uint32(bat.Vecs[0].Length()), stats.Rows())
+	s3writer := NewCNS3TombstoneWriter(proc.Mp(), fs, types.T_int32.ToType())
+
+	err = s3writer.Write(ctx, proc.Mp(), bat)
+	require.NoError(t, err)
+
+	stats, err := s3writer.Sync(ctx, proc.Mp())
+	require.NoError(t, err)
+	require.Equal(t, 1, len(stats))
+
+	require.True(t, stats[0].GetCNCreated())
+	require.Equal(t, uint32(1), stats[0].BlkCnt())
+	require.Equal(t, uint32(bat.Vecs[0].Length()), stats[0].Rows())
 
 }
 
@@ -413,12 +420,16 @@ func TestS3Writer_SortAndSync(t *testing.T) {
 	pool, err := mpool.NewMPool("", mpool.GB, 0)
 	require.NoError(t, err)
 
-	bat := batch.NewWithSize(1)
+	bat := batch.NewWithSize(2)
 	bat.Vecs[0] = vector.NewVec(types.T_Rowid.ToType())
+	bat.Vecs[1] = vector.NewVec(types.T_int32.ToType())
 
 	for i := 0; i < 10; i++ {
 		row := types.RandomRowid()
-		err := vector.AppendFixed[types.Rowid](bat.Vecs[0], row, false, pool)
+		err = vector.AppendFixed[types.Rowid](bat.Vecs[0], row, false, pool)
+		require.NoError(t, err)
+
+		err = vector.AppendFixed[int32](bat.Vecs[1], int32(i), false, pool)
 		require.NoError(t, err)
 	}
 	bat.SetRowCount(10)
@@ -428,27 +439,26 @@ func TestS3Writer_SortAndSync(t *testing.T) {
 		proc := testutil.NewProc()
 		ctx := proc.Ctx
 
-		s3writer := &S3Writer{}
-		s3writer.sortIndex = 0
-		s3writer.isTombstone = true
-
-		_, s, err := s3writer.SortAndSync(ctx, proc)
+		fs, err := fileservice.Get[fileservice.FileService](proc.Base.FileService, defines.SharedFileServiceName)
 		require.NoError(t, err)
-		require.True(t, s.IsZero())
+
+		s3writer := NewCNS3TombstoneWriter(proc.Mp(), fs, types.T_int32.ToType())
+
+		s, err := s3writer.Sync(ctx, proc.Mp())
+		require.NoError(t, err)
+		require.Nil(t, s)
 	}
 
 	// test no SHARED service err
 	{
-		proc := testutil.NewProc(
-			testutil.WithFileService(nil))
+		proc := testutil.NewProc()
 		ctx := proc.Ctx
 
-		s3writer := &S3Writer{}
-		s3writer.sortIndex = -1
-		s3writer.isTombstone = true
-		s3writer.StashBatch(proc, bat)
+		s3writer := NewCNS3TombstoneWriter(proc.Mp(), proc.GetFileService(), types.T_int32.ToType())
+		err = s3writer.Write(ctx, proc.Mp(), bat)
+		require.NoError(t, err)
 
-		_, _, err := s3writer.SortAndSync(ctx, proc)
+		_, err = s3writer.Sync(ctx, proc.Mp())
 		require.Equal(t, err.(*moerr.Error).ErrorCode(), moerr.ErrNoService)
 	}
 
@@ -457,12 +467,15 @@ func TestS3Writer_SortAndSync(t *testing.T) {
 		proc := testutil.NewProc()
 		ctx := proc.Ctx
 
-		s3writer := &S3Writer{}
-		s3writer.sortIndex = 0
-		s3writer.isTombstone = true
-		s3writer.StashBatch(proc, bat)
+		fs, err := fileservice.Get[fileservice.FileService](proc.Base.FileService, defines.SharedFileServiceName)
+		require.NoError(t, err)
 
-		_, _, err = s3writer.SortAndSync(ctx, proc)
+		s3writer := NewCNS3TombstoneWriter(proc.Mp(), fs, types.T_int32.ToType())
+
+		err = s3writer.Write(ctx, proc.Mp(), bat)
+		require.NoError(t, err)
+
+		_, err = s3writer.Sync(ctx, proc.Mp())
 		require.NoError(t, err)
 	}
 
@@ -475,25 +488,31 @@ func TestS3Writer_SortAndSync(t *testing.T) {
 			testutil.WithMPool(pool))
 		ctx := proc.Ctx
 
-		bat2 := batch.NewWithSize(1)
+		bat2 := batch.NewWithSize(2)
 		bat2.Vecs[0] = vector.NewVec(types.T_Rowid.ToType())
+		bat2.Vecs[1] = vector.NewVec(types.T_int32.ToType())
 
 		objectio.SetObjectSizeLimit(mpool.KB)
 		cnt := (objectio.ObjectSizeLimit) / types.RowidSize * 3
 
 		for i := 0; i < cnt; i++ {
 			row := types.RandomRowid()
-			err := vector.AppendFixed[types.Rowid](bat2.Vecs[0], row, false, pool)
+			err = vector.AppendFixed[types.Rowid](bat2.Vecs[0], row, false, pool)
+			require.NoError(t, err)
+
+			err = vector.AppendFixed[int32](bat2.Vecs[1], int32(i), false, pool)
 			require.NoError(t, err)
 		}
 		bat2.SetRowCount(cnt)
 
-		s3writer := &S3Writer{}
-		s3writer.sortIndex = 0
-		s3writer.isTombstone = true
-		s3writer.StashBatch(proc, bat2)
+		fs, err := fileservice.Get[fileservice.FileService](proc.Base.FileService, defines.SharedFileServiceName)
+		require.NoError(t, err)
+		s3writer := NewCNS3TombstoneWriter(proc.Mp(), fs, types.T_int32.ToType())
 
-		_, _, err = s3writer.SortAndSync(ctx, proc)
+		err = s3writer.Write(ctx, proc.Mp(), bat2)
+		require.NoError(t, err)
+
+		_, err = s3writer.Sync(ctx, proc.Mp())
 		require.Equal(t, err.(*moerr.Error).ErrorCode(), moerr.ErrTooLargeObjectSize)
 	}
 }
