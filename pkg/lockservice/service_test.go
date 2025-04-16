5aadb9e72c734a0b1845edd61c9252260a243075
iamlinjunhong <1030420200@qq.com>
2025-04-16 15:58:42 +0800

    fix

--- pkg/lockservice/service_test.go
+++ pkg/lockservice/service_test.go
@@ -2327,6 +2327,34 @@ func TestIssue5176(t *testing.T) {
 	)
 }
 
+func TestIssue5176_2(t *testing.T) {
+	runLockServiceTestsWithLevel(
+		t,
+		zapcore.DebugLevel,
+		[]string{"s1"},
+		time.Second*1,
+		func(alloc *lockTableAllocator, s []*service) {
+			l := s[0]
+			l.cfg.TxnIterFunc = func(f func([]byte) bool) {
+				f([]byte("txn1"))
+			}
+
+			ctx, cancel := context.WithTimeout(
+				context.Background(),
+				time.Second*10)
+			defer cancel()
+
+			l.activeTxnHolder.getActiveTxn([]byte("txn1"), true, "")
+
+			l.setStatus(pb.Status_ServiceLockWaiting)
+
+			l.checkTxnTimeout(ctx)
+			require.False(t, l.activeTxnHolder.empty())
+		},
+		nil,
+	)
+}
+
 func TestReLockSuccWithKeepBindTimeout(t *testing.T) {
 	runLockServiceTestsWithLevel(
 		t,
