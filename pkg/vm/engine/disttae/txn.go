e7beba67c851f8c388e1fc311bc899880c6c8279
jiangxinmeng1 <jiangxinmeng@matrixorigin.io>
2025-04-16 11:23:10 +0800

    fix mergeTxnWorkspaceLocked

--- pkg/vm/engine/disttae/txn.go
+++ pkg/vm/engine/disttae/txn.go
@@ -1106,6 +1106,9 @@ func (txn *Transaction) mergeTxnWorkspaceLocked(ctx context.Context) error {
 		for _, e := range txn.writes {
 			if sels, ok := txn.batchSelectList[e.bat]; ok {
 				txn.approximateInMemInsertCnt -= len(sels)
+				sort.Slice(sels, func(i, j int) bool {
+					return sels[i] < (sels[j])
+				})
 				shrinkBatchWithRowids(e.bat, sels)
 				delete(txn.batchSelectList, e.bat)
 			}
