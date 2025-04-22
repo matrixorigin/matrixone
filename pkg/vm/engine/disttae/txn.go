06be51daa46d3b1302bcf479c587f1d2b9b460de
gouhongshen <gouhongshen@hotmail.com>
2025-04-22 14:28:11 +0800

    fix bvt

--- pkg/vm/engine/disttae/txn.go
+++ pkg/vm/engine/disttae/txn.go
@@ -180,7 +180,9 @@ func (txn *Transaction) WriteBatch(
 		}
 	}
 
-	if typ == DELETE && bat != nil && bat.RowCount() > 1 && !bat.Vecs[0].GetSorted() {
+	if typ == DELETE && !catalog.IsSystemTable(tableId) &&
+		bat != nil && bat.RowCount() > 1 && !bat.Vecs[0].GetSorted() {
+
 		// attr: row_id, pk
 		if err = mergeutil.SortColumnsByIndex(bat.Vecs, 0, txn.proc.Mp()); err != nil {
 			return nil, err
