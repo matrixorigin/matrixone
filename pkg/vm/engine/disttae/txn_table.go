b09e9f4d1da70aa9f8cbe52305e9f38a55cbc8e0
gouhongshen <gouhongshen@hotmail.com>
2025-04-24 18:52:16 +0800

    sort delete offsets

--- pkg/vm/engine/disttae/txn_table.go
+++ pkg/vm/engine/disttae/txn_table.go
@@ -19,6 +19,7 @@ import (
 	"context"
 	"fmt"
 	"runtime/debug"
+	"slices"
 	"strconv"
 	"strings"
 	"sync"
@@ -1653,6 +1654,7 @@ func (tbl *txnTable) rewriteObjectByDeletion(
 		func(blk objectio.BlockInfo, blkMeta objectio.BlockObject) bool {
 
 			del := deletes[blk.BlockID]
+			slices.Sort(del)
 			if bat, err = blockio.BlockCompactionRead(
 				tbl.getTxn().proc.Ctx,
 				blk.MetaLoc[:],
