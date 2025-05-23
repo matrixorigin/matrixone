fc450b1a1d758794f4e9da7bd73d9b0c7d74cfda
cpegeric <cpegeric@gmail.com>
2025-05-23 15:58:16 +0100

    start with empty

--- pkg/vectorindex/hnsw/sync_test.go
+++ pkg/vectorindex/hnsw/sync_test.go
@@ -18,6 +18,7 @@ import (
 	"testing"
 
 	"github.com/matrixorigin/matrixone/pkg/common/mpool"
+	"github.com/matrixorigin/matrixone/pkg/container/batch"
 	"github.com/matrixorigin/matrixone/pkg/testutil"
 	"github.com/matrixorigin/matrixone/pkg/util/executor"
 	"github.com/matrixorigin/matrixone/pkg/vectorindex"
@@ -40,8 +41,42 @@ func mock_runSql_streaming(proc *process.Process, sql string, ch chan executor.R
 }
 */
 
+// give metadata [index_id, checksum, timestamp]
+func mock_runSql_empty(proc *process.Process, sql string) (executor.Result, error) {
+
+	return executor.Result{Mp: proc.Mp(), Batches: []*batch.Batch{}}, nil
+}
+
 func mock_runTxn(proc *process.Process, fn func(exec executor.TxnExecutor) error) error {
 	return nil
+}
+
+func TestSyncUpsertWithEmpty(t *testing.T) {
+
+	m := mpool.MustNewZero()
+	proc := testutil.NewProcessWithMPool("", m)
+
+	runSql = mock_runSql_empty
+	runSql_streaming = mock_runSql_streaming
+	runCatalogSql = mock_runCatalogSql
+	runTxn = mock_runTxn
+
+	cdc := vectorindex.VectorIndexCdc[float32]{Data: make([]vectorindex.VectorIndexCdcEntry[float32], 0, 1000)}
+
+	key := int64(1000)
+	v := []float32{0.1, 0.2, 0.3}
+
+	for i := 0; i < 1000; i++ {
+		e := vectorindex.VectorIndexCdcEntry[float32]{Type: vectorindex.CDC_UPSERT, PKey: key, Vec: v}
+		cdc.Data = append(cdc.Data, e)
+		key += 1
+		for i := range len(v) {
+			v[i] += 0.01
+		}
+	}
+
+	err := CdcSync(proc, "db", "src", 3, &cdc)
+	require.Nil(t, err)
 }
 
 func TestSyncUpsert(t *testing.T) {
