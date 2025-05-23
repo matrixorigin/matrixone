48a3f59ebe346b5a0f2abc7757de3a58d8eebc70
cpegeric <cpegeric@gmail.com>
2025-05-23 13:43:41 +0100

    add test

--- pkg/vectorindex/hnsw/model.go
+++ pkg/vectorindex/hnsw/model.go
@@ -267,7 +267,7 @@ func (idx *HnswModel) Empty() (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	fmt.Printf("Check empty Size = %d\n", sz)
+	os.Stderr.WriteString(fmt.Sprintf("Check empty Size = %d\n", sz))
 	return (sz == 0), nil
 }
 
