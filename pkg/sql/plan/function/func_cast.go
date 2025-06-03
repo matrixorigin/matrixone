94549ac960a4fbadecde69495dce82863ce72197
cpegeric <cpegeric@gmail.com>
2025-06-03 10:36:57 +0100

    check vector dimension when cast from string

--- pkg/sql/plan/function/func_cast.go
+++ pkg/sql/plan/function/func_cast.go
@@ -4394,6 +4394,8 @@ func strToArray[T types.RealNumbers](
 	from vector.FunctionParameterWrapper[types.Varlena],
 	to *vector.FunctionResult[types.Varlena], length int, _ types.Type) error {
 
+	toType := to.GetType()
+
 	var i uint64
 	var l = uint64(length)
 	for i = 0; i < l; i++ {
@@ -4403,11 +4405,17 @@ func strToArray[T types.RealNumbers](
 				return err
 			}
 		} else {
-
-			b, err := types.StringToArrayToBytes[T](convertByteSliceToString(v))
+			arr, err := types.StringToArray[T](convertByteSliceToString(v))
 			if err != nil {
 				return err
 			}
+
+			if int(toType.Width) != len(arr) {
+				return moerr.NewArrayDefMismatchNoCtx(int(toType.Width), len(arr))
+			}
+
+			b := types.ArrayToBytes[T](arr)
+
 			if err = to.AppendBytes(b, false); err != nil {
 				return err
 			}
