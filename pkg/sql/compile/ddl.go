cb81039c3d64c6eb7a2734b7629c92cf40f921b7
triump2020 <triump2020@outlook.com>
2025-06-06 10:03:12 +0800

    fix compile error

--- pkg/sql/compile/ddl.go
+++ pkg/sql/compile/ddl.go
@@ -22,7 +22,6 @@ import (
 
 	"github.com/matrixorigin/matrixone/pkg/catalog"
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
-	"github.com/matrixorigin/matrixone/pkg/compress"
 	"github.com/matrixorigin/matrixone/pkg/container/batch"
 	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
