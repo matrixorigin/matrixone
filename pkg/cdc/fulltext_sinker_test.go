c393b7ca5c00a8d12da8de963403ff6a83fea610
cpegeric <cpegeric@gmail.com>
2025-06-20 11:56:53 +0100

    update

--- pkg/cdc/fulltext_sinker_test.go
+++ pkg/cdc/fulltext_sinker_test.go
@@ -42,6 +42,39 @@ func newTestFulltextTableDef(pkName string, pkType types.T, vecColName string, v
 	}
 }
 
+func newTestFulltextTableDef2Parts(pkName string, pkType types.T, vecColName string, vecColName2 string, vecType types.T, vecWidth int32) *plan.TableDef {
+	return &plan.TableDef{
+		Name: "test_orig_tbl",
+		Name2ColIndex: map[string]int32{
+			pkName:      0,
+			vecColName:  1,
+			vecColName2: 2,
+			"dummy":     3, // Add another col to make sure pk/vec col indices are used
+		},
+		Cols: []*plan.ColDef{
+			{Name: pkName, Typ: plan.Type{Id: int32(pkType)}},
+			{Name: vecColName, Typ: plan.Type{Id: int32(vecType), Width: vecWidth}},
+			{Name: vecColName2, Typ: plan.Type{Id: int32(vecType), Width: vecWidth}},
+			{Name: "dummy", Typ: plan.Type{Id: int32(types.T_int32)}},
+		},
+		Pkey: &plan.PrimaryKeyDef{
+			Names:       []string{pkName},
+			PkeyColName: pkName,
+		},
+		Indexes: []*plan.IndexDef{
+			{
+				IndexName:          "fulltext_idx",
+				TableExist:         true,
+				IndexAlgo:          catalog.MOIndexFullTextAlgo.ToString(),
+				IndexAlgoTableType: "",
+				IndexTableName:     "fulltext_tbl",
+				Parts:              []string{vecColName, vecColName2},
+				IndexAlgoParams:    `{"parser":"ngram"}`,
+			},
+		},
+	}
+}
+
 func TestNewFulltextSqlWriter(t *testing.T) {
 	var ctx context.Context
 
@@ -67,16 +100,16 @@ func TestNewFulltextSqlWriter(t *testing.T) {
 func TestNewFulltextSqlWriterCPkey(t *testing.T) {
 	var ctx context.Context
 
-	tabledef := newTestFulltextTableDef("id", types.T_varbinary, "body", types.T_varchar, 256)
+	tabledef := newTestFulltextTableDef2Parts("__mo_cpkey", types.T_varbinary, "body", "title", types.T_varchar, 256)
 
 	writer, err := NewIndexSqlWriter("fulltext", tabledef, tabledef.Indexes)
 	require.Nil(t, err)
 
-	row := []any{[]uint8("abcdef12"), []uint8("hello world"), nil}
+	row := []any{[]uint8("abcdef12"), []uint8("hello world"), []uint8("one title"), nil}
 	err = writer.Upsert(ctx, row)
 	require.Nil(t, err)
 
-	row = []any{[]uint8("abc"), []uint8("hello world"), nil}
+	row = []any{[]uint8("abc"), []uint8("hello world"), []uint8("two title"), nil}
 	err = writer.Upsert(ctx, row)
 	require.Nil(t, err)
 
