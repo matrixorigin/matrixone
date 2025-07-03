e6f0b40561dded1cdbd18a42f7f498bc461cda17
Cabbage74 <321027920@qq.com>
2025-07-03 11:58:38 +0800

    frequency bug

--- pkg/frontend/cdc_options.go
+++ pkg/frontend/cdc_options.go
@@ -209,8 +209,8 @@ func (opts *CDCCreateTaskOptions) ValidateAndFill(
 				if err = opts.handleFrequency(ctx, ses, req, level, value); err != nil {
 					return
 				}
-				extraOpts[cdc.CDCTaskExtraOptions_Frequency] = value
 			}
+			extraOpts[cdc.CDCTaskExtraOptions_Frequency] = value
 		}
 	}
 
