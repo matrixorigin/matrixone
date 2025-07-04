d4063880dbe7602380cc6f9f627973d6c87c45be
bRong Njam <longran1989@gmail.com>
2025-07-04 13:47:46 +0800

    do column-pruning for VALUE_SCAN node (#22105)
    to support selecting part of columns from VALUES clause
    
    Approved by: @qingxinhome, @ouyuanning, @heni02

--- test/distributed/cases/dml/select/values.sql
+++ test/distributed/cases/dml/select/values.sql
@@ -3,3 +3,5 @@ values row(1,1), row(2,2), row(3,3) order by column_0 desc;
 select * from (values row(1,1), row(2,2), row(3,3)) a;
 select * from (values row(1,1), row(2,2), row(3,3)) a(a, b);
 select * from (values row(1,"1",1.0), row(abs(-2),"2",2.0)) a;
+select column_1 from (values row(0, 1, cast('[3, 4, 5]' as vecf32(3))));
+select column_2 from (values row(0, 1, cast('[3, 4, 5]' as vecf32(3))));
