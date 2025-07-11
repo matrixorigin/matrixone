5b9e78b2019e9bfec13cbbbed7dd19109c6fdfd0
iamlinjunhong <1030420200@qq.com>
2025-07-11 17:54:15 +0800

    fix

--- test/distributed/cases/pessimistic_transaction/drop_table.sql
+++ test/distributed/cases/pessimistic_transaction/drop_table.sql
@@ -1,23 +1,24 @@
 create database if not exists test;
 use test;
-drop table if exists t1;
-create table t1(a int);
-insert into t1 values (1);
-drop table if exists t2;
-create table t2(a int);
+drop table if exists tt1;
+create table tt1(a int);
+insert into tt1 values (1);
+drop table if exists tt2;
+create table tt2(a int);
 begin;
-drop table t1;
+drop table tt1;
 -- @session:id=1{
 use test;
-select * from t1;
-insert into t2 select * from t1;
+select * from tt1;
+insert into tt2 select * from tt1;
 -- @session}
 commit;
-select * from t2;
+select * from tt2;
 begin;
-drop table t2;
+drop table tt2;
 -- @session:id=1{
+use test;
 -- @wait:0:commit
-insert into t2 select * from t2;
+insert into tt2 select * from tt2;
 -- @session}
 commit;
