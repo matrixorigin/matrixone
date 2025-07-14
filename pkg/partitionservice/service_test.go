a8fe69bee0c66fd4947c57bc647fd57ad4604616
fagongzi <zhangxu19830126@gmail.com>
2025-07-14 22:33:58 +0800

    partitioned tables support index queries (#22158)
    partitioned tables support index queries
    
    Approved by: @aunjgr, @iamlinjunhong, @gouhongshen

--- pkg/partitionservice/service_test.go
+++ pkg/partitionservice/service_test.go
@@ -51,7 +51,7 @@ func TestDelete(t *testing.T) {
 			assert.NoError(t, s.Create(ctx, tableID, stmt, txnOp))
 
 			require.NoError(t, txnOp.Commit(ctx))
-			require.NotEmpty(t, s.mu.tables)
+			require.Empty(t, s.mu.tables)
 
 			require.NoError(t, s.Delete(ctx, tableID, nil))
 		},
