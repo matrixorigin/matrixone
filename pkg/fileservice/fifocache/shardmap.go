96c0b4552d5528148cc5396f073931a67ffbf52e
cpegeric <cpegeric@gmail.com>
2025-07-15 17:48:52 +0100

    single shard map

--- pkg/fileservice/fifocache/shardmap.go
+++ pkg/fileservice/fifocache/shardmap.go
@@ -14,44 +14,28 @@
 
 package fifocache
 
-import (
-	"golang.org/x/sys/cpu"
-)
-
 const numShards = 256
 
 type ShardMap[K comparable, V any] struct {
-	shards [numShards]struct {
-		//sync.RWMutex
-		values map[K]V
-		_      cpu.CacheLinePad
-	}
+	values map[K]V
 	hashfn func(K) uint64
 }
 
 func NewShardMap[K comparable, V any](hashfn func(K) uint64) *ShardMap[K, V] {
-	m := &ShardMap[K, V]{hashfn: hashfn}
-
-	for i := range m.shards {
-		m.shards[i].values = make(map[K]V, 1024)
+	m := &ShardMap[K, V]{hashfn: hashfn,
+		values: make(map[K]V, 1024),
 	}
 	return m
 }
 
 func (m *ShardMap[K, V]) Set(key K, value V, postfn func(V)) bool {
 
-	s := &m.shards[m.hashfn(key)%numShards]
-	/*
-		s.Lock()
-		defer s.Unlock()
-	*/
-
-	_, ok := s.values[key]
+	_, ok := m.values[key]
 	if ok {
 		return false
 	}
 
-	s.values[key] = value
+	m.values[key] = value
 
 	if postfn != nil {
 		// call postSet protected by mutex.Lock
@@ -62,13 +46,7 @@ func (m *ShardMap[K, V]) Set(key K, value V, postfn func(V)) bool {
 
 func (m *ShardMap[K, V]) Get(key K, postfn func(V)) (V, bool) {
 
-	s := &m.shards[m.hashfn(key)%numShards]
-	/*
-		s.RLock()
-		defer s.RUnlock()
-	*/
-	v, ok := s.values[key]
-
+	v, ok := m.values[key]
 	if !ok {
 		return v, ok
 	}
@@ -81,51 +59,29 @@ func (m *ShardMap[K, V]) Get(key K, postfn func(V)) (V, bool) {
 }
 
 func (m *ShardMap[K, V]) Remove(key K) {
-
-	s := &m.shards[m.hashfn(key)%numShards]
-	/*
-		s.Lock()
-		defer s.Unlock()
-	*/
-	delete(s.values, key)
+	delete(m.values, key)
 }
 
 func (m *ShardMap[K, V]) CompareAndDelete(key K, fn func(k1, k2 K) bool, postfn func(V)) {
 
-	for i := range m.shards {
-		s := &m.shards[i]
-		func() {
-			/*
-				s.Lock()
-				defer s.Unlock()
-			*/
-			for k, v := range s.values {
-				if fn(k, key) {
-					delete(s.values, k)
-					if postfn != nil {
-						// call postfn to let parent know the item get deleted. (protected by mutex.Lock)
-						postfn(v)
-					}
-				}
+	for k, v := range m.values {
+		if fn(k, key) {
+			delete(m.values, k)
+			if postfn != nil {
+				// call postfn to let parent know the item get deleted. (protected by mutex.Lock)
+				postfn(v)
 			}
-		}()
+		}
 	}
 }
 
 func (m *ShardMap[K, V]) GetAndDelete(key K, postfn func(V)) (V, bool) {
-
-	s := &m.shards[m.hashfn(key)%numShards]
-	/*
-		s.Lock()
-		defer s.Unlock()
-	*/
-
-	v, ok := s.values[key]
+	v, ok := m.values[key]
 	if !ok {
 		return v, ok
 	}
 
-	delete(s.values, key)
+	delete(m.values, key)
 
 	if postfn != nil {
 		// call postfn to let parent know the item get deleted. (protected by mutex.Lock)
