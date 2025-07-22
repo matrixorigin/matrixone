af7647691f2d6693dc9b5bb85839b2a50702905b
GreatRiver <great.river703@gmail.com>
2025-07-22 14:03:31 +0800

    Update

--- pkg/vm/engine/tae/options/types.go
+++ pkg/vm/engine/tae/options/types.go
@@ -42,7 +42,7 @@ const (
 	DefaultCheckpointMinCount           = int64(100)
 	DefaultCheckpointIncremetalInterval = time.Minute
 	DefaultCheckpointGlobalMinCount     = 10
-	DefaultGlobalVersionInterval        = time.Millisecond
+	DefaultGlobalVersionInterval        = 0
 	DefaultGCCheckpointInterval         = time.Minute
 	DefaultOverallFlushMemControl       = common.Const1GBytes
 
