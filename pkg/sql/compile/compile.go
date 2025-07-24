71b44c0a6970e315d8236bd32317d5d9fd93922a
bRong Njam <longran1989@gmail.com>
2025-07-24 18:49:00 +0800

    fix shuffle RIGHT DEDUP join

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -2350,11 +2350,7 @@ func (c *Compile) compileUnionAll(node *plan.Node, ss []*Scope, children []*Scop
 func (c *Compile) compileJoin(node, left, right *plan.Node, probeScopes, buildScopes []*Scope) []*Scope {
 	if node.Stats.HashmapStats.Shuffle {
 		if len(c.cnList) == 1 {
-			if node.JoinType == plan.Node_DEDUP {
-				logutil.Infof("not support shuffle v2 for dedup join now")
-			} else {
-				return c.compileShuffleJoinV2(node, left, right, probeScopes, buildScopes)
-			}
+			return c.compileShuffleJoinV2(node, left, right, probeScopes, buildScopes)
 		}
 		return c.compileShuffleJoin(node, left, right, probeScopes, buildScopes)
 	}
