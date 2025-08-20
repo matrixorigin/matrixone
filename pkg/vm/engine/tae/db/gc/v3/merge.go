ca2dd01a248e27000ead974937187a234b98911e
GreatRiver <great.river703@gmail.com>
2025-08-20 15:48:04 +0800

    U

--- pkg/vm/engine/tae/db/gc/v3/merge.go
+++ pkg/vm/engine/tae/db/gc/v3/merge.go
@@ -248,11 +248,11 @@ func getCheckpointLocation(
 		return nil, err
 	}
 	files := make([]string, 0)
+	files = append(files, checkpoint.GetLocation().Name().UnsafeString())
 	tableIDLocations := checkpoint.GetTableIDLocation()
 	for i := 0; i < tableIDLocations.Len(); i++ {
 		location := tableIDLocations.Get(i)
 		files = append(files, location.Name().UnsafeString())
-		continue
 	}
 
 	for _, loc := range reader.GetLocations() {
