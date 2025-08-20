b850d2d4b361fe2ad70f10445dda8b5e2f3fd932
GreatRiver <great.river703@gmail.com>
2025-08-20 15:23:10 +0800

    Fix GCKP files leak

--- pkg/vm/engine/tae/db/gc/v3/checkpoint.go
+++ pkg/vm/engine/tae/db/gc/v3/checkpoint.go
@@ -933,6 +933,18 @@ func (c *checkpointCleaner) mergeCheckpointFilesLocked(
 				zap.String("gckp", nameMeta),
 			)
 			deleteFiles = append(deleteFiles, nameMeta)
+
+			// fix gckp files leak
+			var files []string
+			files, err = getCheckpointLocation(ctx, ckp, c.fs)
+			if err != nil {
+				extraErrMsg = "getCheckpointLocation failed"
+				return err
+			}
+			if len(files) > 0 {
+				deleteFiles = append(deleteFiles, files...)
+			}
+
 		}
 	}
 	if c.GCCheckpointEnabled() {
