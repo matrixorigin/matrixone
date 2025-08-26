9efa664d13160f6e62320dc8c4a668a7078d0a6b
cpegeric <cpegeric@gmail.com>
2025-08-26 15:08:20 +0100

    fix sca

--- pkg/sql/compile/ddl.go
+++ pkg/sql/compile/ddl.go
@@ -41,7 +41,6 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/container/vector"
 	"github.com/matrixorigin/matrixone/pkg/defines"
 	"github.com/matrixorigin/matrixone/pkg/incrservice"
-	"github.com/matrixorigin/matrixone/pkg/logutil"
 	"github.com/matrixorigin/matrixone/pkg/partitionservice"
 	"github.com/matrixorigin/matrixone/pkg/pb/api"
 	"github.com/matrixorigin/matrixone/pkg/pb/lock"
