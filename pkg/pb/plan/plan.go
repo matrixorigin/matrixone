matrixorigin/matrixone

commit 0af7e1e46d339d0f3963045cc8fe5df39a97415a

avoid using the slow String() of protobuf (#22413)
serialization takes a significant part in CPU profile

Approved by: @iamlinjunhong, @ouyuanning

pkg/pb/plan/plan.go
@@ -81,3 +81,42 @@ func (def *ColDef) GetOriginCaseName() string {
 	}
 	return def.OriginName
 }
+
+func (m *Expr) ExprString() string {
+	if m == nil {
+		return ""
+	}
+
+	data, err := m.Marshal()
+	if err != nil {
+		return ""
+	}
+
+	return string(data)
+}
+
+func (m *ColRef) ColRefBytes() []byte {
+	if m == nil {
+		return nil
+	}
+
+	data, err := m.Marshal()
+	if err != nil {
+		return nil
+	}
+
+	return data
+}
+
+func (m *CorrColRef) CorrColRefBytes() []byte {
+	if m == nil {
+		return nil
+	}
+
+	data, err := m.Marshal()
+	if err != nil {
+		return nil
+	}
+
+	return data
+}

pkg/sql/plan/utils.go
@@ -371,7 +371,7 @@ func applyDistributivity(ctx context.Context, expr *plan.Expr) *plan.Expr {
 
 		relPos := int32(-1)
 		for _, cond := range rightConds {
-			condMap[cond.String()] = JoinSideRight
+			condMap[cond.ExprString()] = JoinSideRight
 			args := cond.GetF().GetArgs()
 			if len(args) != 2 {
 				continue
@@ -391,7 +391,7 @@ func applyDistributivity(ctx context.Context, expr *plan.Expr) *plan.Expr {
 		var commonConds, leftOnlyConds, rightOnlyConds []*plan.Expr
 
 		for _, cond := range leftConds {
-			exprStr := cond.String()
+			exprStr := cond.ExprString()
 
 			if condMap[exprStr] == JoinSideRight {
 				commonConds = append(commonConds, cond)
@@ -403,7 +403,7 @@ func applyDistributivity(ctx context.Context, expr *plan.Expr) *plan.Expr {
 		}
 
 		for _, cond := range rightConds {
-			if condMap[cond.String()] == JoinSideRight {
+			if condMap[cond.ExprString()] == JoinSideRight {
 				rightOnlyConds = append(rightOnlyConds, cond)
 			}
 		}
@@ -494,9 +494,9 @@ func checkDNF(expr *plan.Expr) []string {
 		return ret
 
 	case *plan.Expr_Corr:
-		ret = append(ret, exprImpl.Corr.String())
+		ret = append(ret, string(exprImpl.Corr.CorrColRefBytes()))
 	case *plan.Expr_Col:
-		ret = append(ret, exprImpl.Col.String())
+		ret = append(ret, string(exprImpl.Col.ColRefBytes()))
 	}
 	return ret
 }
@@ -700,7 +700,10 @@ func extractColRefInFilter(expr *plan.Expr) *ColRef {
 // for col1=col2 and col3 = col4, trying to deduce new pred
 // for example , if col1 and col3 are the same, then we can deduce that col2=col4
 func deduceTranstivity(expr *plan.Expr, col1, col2, col3, col4 *ColRef) (bool, *plan.Expr) {
-	if col1.String() == col3.String() || col1.String() == col4.String() || col2.String() == col3.String() || col2.String() == col4.String() {
+	if bytes.Equal(col1.ColRefBytes(), col3.ColRefBytes()) ||
+		bytes.Equal(col1.ColRefBytes(), col4.ColRefBytes()) ||
+		bytes.Equal(col2.ColRefBytes(), col3.ColRefBytes()) ||
+		bytes.Equal(col2.ColRefBytes(), col4.ColRefBytes()) {
 		retExpr := DeepCopyExpr(expr)
 		substituteMatchColumn(retExpr, col3, col4)
 		return true, retExpr
@@ -713,13 +716,13 @@ func substituteMatchColumn(expr *plan.Expr, onPredCol1, onPredCol2 *ColRef) bool
 	var ret bool
 	switch exprImpl := expr.Expr.(type) {
 	case *plan.Expr_Col:
-		colName := exprImpl.Col.String()
-		if colName == onPredCol1.String() {
+		colName := exprImpl.Col.ColRefBytes()
+		if bytes.Equal(colName, onPredCol1.ColRefBytes()) {
 			exprImpl.Col.RelPos = onPredCol2.RelPos
 			exprImpl.Col.ColPos = onPredCol2.ColPos
 			exprImpl.Col.Name = onPredCol2.Name
 			return true
-		} else if colName == onPredCol2.String() {
+		} else if bytes.Equal(colName, onPredCol2.ColRefBytes()) {
 			exprImpl.Col.RelPos = onPredCol1.RelPos
 			exprImpl.Col.ColPos = onPredCol1.ColPos
 			exprImpl.Col.Name = onPredCol1.Name

pkg/sql/parsers/tree/constant.go
@@ -186,7 +186,7 @@ func NewNumVal[T bool | int64 | uint64 | float64 | string](val T, originString s
 }
 
 func (node *NumVal) Format(ctx *FmtCtx) {
-	if node.origString != "" {
+	if strings.Contains(node.origString, "\\") {
 		ctx.WriteValue(node.ValType, FormatString(node.origString))
 		return
 	}

pkg/sql/plan/order_binder.go
@@ -132,14 +132,27 @@ func (b *OrderBinder) BindExpr(astExpr tree.Expr) (*plan.Expr, error) {
 	var colPos int32
 	var ok bool
 
-	exprStr := expr.String()
+	var exprStr string
+	protoSz := expr.ProtoSize()
+	if protoSz < 256 {
+		exprBytes := make([]byte, protoSz)
+		_, err := expr.MarshalToSizedBuffer(exprBytes)
+		if err != nil {
+			return nil, err
+		}
+
+		exprStr = string(exprBytes)
+	}
+
 	if colPos, ok = b.ctx.projectByExpr[exprStr]; !ok {
 		if b.ctx.isDistinct {
 			return nil, moerr.NewSyntaxError(b.GetContext(), "for SELECT DISTINCT, ORDER BY expressions must appear in select list")
 		}
 
 		colPos = int32(len(b.ctx.projects))
-		b.ctx.projectByExpr[exprStr] = colPos
+		if len(exprStr) > 0 {
+			b.ctx.projectByExpr[exprStr] = colPos
+		}
 		b.ctx.projects = append(b.ctx.projects, expr)
 	}
 

pkg/sql/plan/query_builder.go
@@ -3254,9 +3254,19 @@ func (builder *QueryBuilder) bindProjection(
 
 	resultLen = len(ctx.projects)
 	for i, proj := range ctx.projects {
-		exprStr := proj.String()
-		if _, ok := ctx.projectByExpr[exprStr]; !ok {
-			ctx.projectByExpr[exprStr] = int32(i)
+		protoSz := proj.ProtoSize()
+		if protoSz < 256 {
+			exprBytes := make([]byte, protoSz)
+			_, err = proj.MarshalToSizedBuffer(exprBytes)
+			if err != nil {
+				return
+			}
+
+			exprStr := string(exprBytes)
+			if _, ok := ctx.projectByExpr[exprStr]; !ok {
+				ctx.projectByExpr[exprStr] = int32(i)
+			}
+
 		}
 
 		if exprCol, ok := proj.Expr.(*plan.Expr_Col); ok {

commit 50f729e467558be1efae39f6ac3d9d8f3332483e

fix OOM caused by #22413 (#22444)
* change some return value from []byte to string
* make sure tpc-h q7 and q19 are properly optimized
* remove more String() calls to plan.Expr

Approved by: @ouyuanning, @daviszhen, @gouhongshen

pkg/pb/plan/plan.go
@@ -95,28 +95,15 @@ func (m *Expr) ExprString() string {
 	return string(data)
 }
 
-func (m *ColRef) ColRefBytes() []byte {
+func (m *ColRef) ColRefString() string {
 	if m == nil {
-		return nil
+		return ""
 	}
 
 	data, err := m.Marshal()
 	if err != nil {
-		return nil
+		return ""
 	}
 
-	return data
-}
-
-func (m *CorrColRef) CorrColRefBytes() []byte {
-	if m == nil {
-		return nil
-	}
-
-	data, err := m.Marshal()
-	if err != nil {
-		return nil
-	}
-
-	return data
+	return string(data)
 }

pkg/sql/plan/tools/match_pattern.go
@@ -159,7 +159,7 @@ func (pattern *MatchPattern) WithExactAssignedOutputs(expectedAliases []RValueMa
 				})
 			} else {
 				ret = append(ret, VarRef{
-					Name: expr.String(),
+					Name: expr.ExprString(),
 					Type: expr.GetTyp(),
 				})
 			}
@@ -187,7 +187,7 @@ func (pattern *MatchPattern) WithExactAssignments(expectedAliases []RValueMatche
 				})
 			} else {
 				ret = append(ret, VarRef{
-					Name: expr.String(),
+					Name: expr.ExprString(),
 					Type: expr.GetTyp(),
 				})
 			}

pkg/sql/plan/tools/matcher.go
@@ -293,7 +293,7 @@ func (matcher *ExprMatcher) GetAssignedVar(node *plan2.Node, aliases UnorderedMa
 				}
 			} else {
 				res = &VarRef{
-					Name: expr.String(),
+					Name: expr.ExprString(),
 					Type: expr.GetTyp(),
 				}
 			}
@@ -435,7 +435,7 @@ func (matcher *AggrFuncMatcher) GetAssignedVar(node *plan2.Node, aliases Unorder
 				}
 			} else {
 				res = &VarRef{
-					Name: expr.String(),
+					Name: expr.ExprString(),
 					Type: expr.GetTyp(),
 				}
 			}

pkg/sql/plan/utils.go
@@ -26,8 +26,6 @@ import (
 	"strings"
 	"time"
 
-	"go.uber.org/zap"
-
 	"github.com/matrixorigin/matrixone/pkg/catalog"
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 	"github.com/matrixorigin/matrixone/pkg/common/mpool"
@@ -48,6 +46,7 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/stage/stageutil"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/common"
 	"github.com/matrixorigin/matrixone/pkg/vm/process"
+	"go.uber.org/zap"
 )
 
 func GetBindings(expr *plan.Expr) []int32 {
@@ -493,10 +492,8 @@ func checkDNF(expr *plan.Expr) []string {
 		}
 		return ret
 
-	case *plan.Expr_Corr:
-		ret = append(ret, string(exprImpl.Corr.CorrColRefBytes()))
 	case *plan.Expr_Col:
-		ret = append(ret, string(exprImpl.Col.ColRefBytes()))
+		ret = append(ret, exprImpl.Col.ColRefString())
 	}
 	return ret
 }
@@ -532,14 +529,8 @@ func walkThroughDNF(ctx context.Context, expr *plan.Expr, keywords string) *plan
 			return expr
 		}
 
-	case *plan.Expr_Corr:
-		if exprImpl.Corr.String() == keywords {
-			return expr
-		} else {
-			return nil
-		}
 	case *plan.Expr_Col:
-		if exprImpl.Col.String() == keywords {
+		if exprImpl.Col.ColRefString() == keywords {
 			return expr
 		} else {
 			return nil
@@ -700,10 +691,10 @@ func extractColRefInFilter(expr *plan.Expr) *ColRef {
 // for col1=col2 and col3 = col4, trying to deduce new pred
 // for example , if col1 and col3 are the same, then we can deduce that col2=col4
 func deduceTranstivity(expr *plan.Expr, col1, col2, col3, col4 *ColRef) (bool, *plan.Expr) {
-	if bytes.Equal(col1.ColRefBytes(), col3.ColRefBytes()) ||
-		bytes.Equal(col1.ColRefBytes(), col4.ColRefBytes()) ||
-		bytes.Equal(col2.ColRefBytes(), col3.ColRefBytes()) ||
-		bytes.Equal(col2.ColRefBytes(), col4.ColRefBytes()) {
+	if col1.ColRefString() == col3.ColRefString() ||
+		col1.ColRefString() == col4.ColRefString() ||
+		col2.ColRefString() == col3.ColRefString() ||
+		col2.ColRefString() == col4.ColRefString() {
 		retExpr := DeepCopyExpr(expr)
 		substituteMatchColumn(retExpr, col3, col4)
 		return true, retExpr
@@ -716,13 +707,13 @@ func substituteMatchColumn(expr *plan.Expr, onPredCol1, onPredCol2 *ColRef) bool
 	var ret bool
 	switch exprImpl := expr.Expr.(type) {
 	case *plan.Expr_Col:
-		colName := exprImpl.Col.ColRefBytes()
-		if bytes.Equal(colName, onPredCol1.ColRefBytes()) {
+		colName := exprImpl.Col.ColRefString()
+		if colName == onPredCol1.ColRefString() {
 			exprImpl.Col.RelPos = onPredCol2.RelPos
 			exprImpl.Col.ColPos = onPredCol2.ColPos
 			exprImpl.Col.Name = onPredCol2.Name
 			return true
-		} else if bytes.Equal(colName, onPredCol2.ColRefBytes()) {
+		} else if colName == onPredCol2.ColRefString() {
 			exprImpl.Col.RelPos = onPredCol1.RelPos
 			exprImpl.Col.ColPos = onPredCol1.ColPos
 			exprImpl.Col.Name = onPredCol1.Name

pkg/sql/colexec/filter/filter_test.go
@@ -1034,9 +1034,9 @@ func TestConstantTranspose(t *testing.T) {
 			require.NoError(t, err)
 
 			if tt.expect == nil {
-				require.Equal(t, tt.input.String(), result.String())
+				require.Equal(t, tt.input.ExprString(), result.ExprString())
 			} else {
-				require.Equal(t, tt.expect.String(), result.String())
+				require.Equal(t, tt.expect.ExprString(), result.ExprString())
 			}
 		})
 	}