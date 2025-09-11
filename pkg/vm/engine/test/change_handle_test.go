916f4d941d1d0e22526ca7a6ed398e9a247c9833
jiangxinmeng1 <51114574+jiangxinmeng1@users.noreply.github.com>
2025-09-11 15:21:14 +0800

    update register job (#22503)
    register job support start from now
    
    Approved by: @ck89119, @XuPeng-SH

--- pkg/vm/engine/test/change_handle_test.go
+++ pkg/vm/engine/test/change_handle_test.go
@@ -1525,6 +1525,7 @@ func TestISCPExecutor1(t *testing.T) {
 			DBName:    "srcdb",
 			TableName: "src_table",
 		},
+		false,
 	)
 	assert.True(t, ok)
 	assert.NoError(t, err)
@@ -1686,6 +1687,7 @@ func TestISCPExecutor2(t *testing.T) {
 			DBName:    "srcdb",
 			TableName: "src_table",
 		},
+		false,
 	)
 	assert.True(t, ok)
 	assert.NoError(t, err)
@@ -1705,6 +1707,7 @@ func TestISCPExecutor2(t *testing.T) {
 			DBName:    "srcdb",
 			TableName: "src_table",
 		},
+		false,
 	)
 	assert.False(t, ok)
 	assert.NoError(t, err)
@@ -1761,6 +1764,7 @@ func TestISCPExecutor2(t *testing.T) {
 			DBName:    "srcdb",
 			TableName: "src_table",
 		},
+		false,
 	)
 	assert.True(t, ok)
 	assert.NoError(t, err)
@@ -1858,6 +1862,7 @@ func TestISCPExecutor3(t *testing.T) {
 				DBName:    "srcdb",
 				TableName: "src_table",
 			},
+			false,
 		)
 		assert.True(t, ok)
 		assert.NoError(t, err)
@@ -2068,6 +2073,7 @@ func TestISCPExecutor4(t *testing.T) {
 				DBName:    "srcdb",
 				TableName: "src_table",
 			},
+			false,
 		)
 		assert.True(t, ok)
 		assert.NoError(t, err)
@@ -2256,6 +2262,7 @@ func TestISCPExecutor5(t *testing.T) {
 				DBName:    dbName,
 				TableName: tableName,
 			},
+			false,
 		)
 		assert.True(t, ok)
 		assert.NoError(t, err)
@@ -2390,6 +2397,7 @@ func TestISCPExecutor6(t *testing.T) {
 			DBName:    "srcdb",
 			TableName: "src_table",
 		},
+		false,
 	)
 	assert.True(t, ok)
 	assert.NoError(t, err)
@@ -2492,6 +2500,7 @@ func TestISCPExecutor7(t *testing.T) {
 			DBName:    "srcdb",
 			TableName: "src_table",
 		},
+		false,
 	)
 	assert.True(t, ok)
 	assert.NoError(t, err)
@@ -2595,6 +2604,7 @@ func TestISCPExecutor8(t *testing.T) {
 			DBName:    "srcdb",
 			TableName: "src_table",
 		},
+		false,
 	)
 	assert.True(t, ok)
 	assert.NoError(t, err)
@@ -2707,6 +2717,7 @@ func TestUpdateJobSpec(t *testing.T) {
 			DBName:    dbName,
 			TableName: tableName,
 		},
+		false,
 	)
 	assert.True(t, ok)
 	assert.NoError(t, err)
@@ -2884,6 +2895,7 @@ func TestFlushWatermark(t *testing.T) {
 			DBName:    dbName,
 			TableName: tableName,
 		},
+		false,
 	)
 	assert.True(t, ok)
 	assert.NoError(t, err)
@@ -2985,6 +2997,7 @@ func TestGCInMemoryJob(t *testing.T) {
 			DBName:    "srcdb",
 			TableName: "src_table",
 		},
+		false,
 	)
 	assert.True(t, ok)
 	assert.NoError(t, err)
@@ -3111,6 +3124,7 @@ func TestIteration(t *testing.T) {
 				DBName:    "srcdb",
 				TableName: tableName,
 			},
+			false,
 		)
 		assert.True(t, ok)
 		assert.NoError(t, err)
@@ -3235,6 +3249,7 @@ func TestDropJobsByDBName(t *testing.T) {
 				DBName:    "srcdb",
 				TableName: tableName,
 			},
+			false,
 		)
 		assert.True(t, ok)
 		assert.NoError(t, err)
@@ -3491,4 +3506,113 @@ func TestCancelIteration2(t *testing.T) {
 	close(cancelCh)
 	wg.Wait()
 
+}
+
+func TestStartFromNow(t *testing.T) {
+	catalog.SetupDefines("")
+
+	// idAllocator := common.NewIdAllocator(1000)
+
+	var (
+		accountId = catalog.System_Account
+	)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ctx = context.WithValue(ctx, defines.TenantIDKey{}, accountId)
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, time.Minute*5)
+	defer cancel()
+
+	disttaeEngine, taeHandler, rpcAgent, _ := testutil.CreateEngines(ctx, testutil.TestOptions{}, t)
+	defer func() {
+		disttaeEngine.Close(ctx)
+		taeHandler.Close(true)
+		rpcAgent.Close()
+	}()
+
+	err := mock_mo_indexes(disttaeEngine, ctxWithTimeout)
+	require.NoError(t, err)
+	err = mock_mo_foreign_keys(disttaeEngine, ctxWithTimeout)
+	require.NoError(t, err)
+	err = mock_mo_intra_system_change_propagation_log(disttaeEngine, ctxWithTimeout)
+	require.NoError(t, err)
+	t.Log(taeHandler.GetDB().Catalog.SimplePPString(3))
+
+	// create database and table
+
+	bat := CreateDBAndTableForCNConsumerAndGetAppendData(t, disttaeEngine, ctxWithTimeout, "srcdb", "src_table", 10)
+	bats := bat.Split(10)
+	defer bat.Close()
+
+	// append 1 row
+	_, rel, txn, err := disttaeEngine.GetTable(ctxWithTimeout, "srcdb", "src_table")
+	require.Nil(t, err)
+
+	tableID := rel.GetTableID(ctxWithTimeout)
+
+	err = rel.Write(ctxWithTimeout, containers.ToCNBatch(bats[0]))
+	require.Nil(t, err)
+
+	txn.Commit(ctxWithTimeout)
+
+	// init cdc executor
+	cdcExecutor, err := iscp.NewISCPTaskExecutor(
+		ctxWithTimeout,
+		disttaeEngine.Engine,
+		disttaeEngine.GetTxnClient(),
+		"",
+		&iscp.ISCPExecutorOption{
+			GCInterval:             time.Hour,
+			GCTTL:                  time.Hour,
+			SyncTaskInterval:       time.Millisecond * 100,
+			FlushWatermarkInterval: time.Millisecond * 100,
+			RetryTimes:             1,
+		},
+		common.DebugAllocator,
+	)
+	require.NoError(t, err)
+	cdcExecutor.SetRpcHandleFn(taeHandler.GetRPCHandle().HandleGetChangedTableList)
+
+	cdcExecutor.Start()
+	defer cdcExecutor.Stop()
+
+	fault.Enable()
+	defer fault.Disable()
+
+	rmFn, err := objectio.InjectCDCExecutor("changesNext")
+	require.NoError(t, err)
+	defer rmFn()
+
+	// register cdc job
+	txn, err = disttaeEngine.NewTxnOperator(ctx, disttaeEngine.Engine.LatestLogtailAppliedTime())
+	require.NoError(t, err)
+	ok, err := iscp.RegisterJob(
+		ctx, "", txn, "pitr",
+		&iscp.JobSpec{
+			ConsumerInfo: iscp.ConsumerInfo{
+				ConsumerType: int8(iscp.ConsumerType_CNConsumer),
+			},
+		},
+		&iscp.JobID{
+			JobName:   "hnsw_idx",
+			DBName:    "srcdb",
+			TableName: "src_table",
+		},
+		true,
+	)
+	assert.True(t, ok)
+	assert.NoError(t, err)
+	assert.NoError(t, txn.Commit(ctxWithTimeout))
+
+	now := taeHandler.GetDB().TxnMgr.Now()
+	testutils.WaitExpect(
+		4000,
+		func() bool {
+			ts, ok := cdcExecutor.GetWatermark(accountId, tableID, "hnsw_idx")
+			return ok && ts.GE(&now)
+		},
+	)
+	ts, ok := cdcExecutor.GetWatermark(accountId, tableID, "hnsw_idx")
+	assert.True(t, ok)
+	assert.True(t, ts.GE(&now))
 }
