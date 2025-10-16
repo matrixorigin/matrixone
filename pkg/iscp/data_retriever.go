aa48108d20455eed1fdf18081022332854c6af9e
jiangxinmeng1 <51114574+jiangxinmeng1@users.noreply.github.com>
2025-10-16 17:46:36 +0800

    fix data retriever (#22632)
    fix data retriever
    
    Approved by: @XuPeng-SH

--- pkg/iscp/data_retriever.go
+++ pkg/iscp/data_retriever.go
@@ -126,12 +126,12 @@ func NewDataRetriever(
 }
 
 func (r *DataRetrieverImpl) Next() *ISCPData {
+	var data *ISCPData
 	select {
 	case <-r.ctx.Done():
 		return nil
-	default:
+	case data = <-r.insertDataCh:
 	}
-	data := <-r.insertDataCh
 	return data
 }
 
