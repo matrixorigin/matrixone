02131a730d9714c4c0466dbd58090e760990eed9
jiangxinmeng <jiangxinmeng@matrixorigin.io>
2025-11-10 14:54:43 +0800

    add ut

--- pkg/vm/engine/test/change_handle_test.go
+++ pkg/vm/engine/test/change_handle_test.go
@@ -37,8 +37,10 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/frontend"
 	"github.com/matrixorigin/matrixone/pkg/objectio"
 	"github.com/matrixorigin/matrixone/pkg/objectio/ioutil"
+	pbtxn "github.com/matrixorigin/matrixone/pkg/pb/txn"
 	"github.com/matrixorigin/matrixone/pkg/txn/client"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine"
+	cmd_util "github.com/matrixorigin/matrixone/pkg/vm/engine/cmd_util"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/disttae"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/disttae/logtailreplay"
 	catalog2 "github.com/matrixorigin/matrixone/pkg/vm/engine/tae/catalog"
@@ -1640,5 +1642,164 @@ func newObjectEntryForCheckpointTest(t *testing.T, id byte, appendable bool, cnC
 		ObjectStats: *stats,
 		CreateTime:  create,
 		DeleteTime:  delete,
+	}
+}
+
+func TestPartitionChangesHandleStaleRead(t *testing.T) {
+	/*
+		This test verifies that when nextFrom is not in the checkpoint entry range (minTS, maxTS),
+		it returns a stale read error. This tests the logic in change_handle.go:223-226:
+		if nextFrom.LT(&minTS) || nextFrom.GT(&maxTS) {
+			logutil.Infof("ChangesHandle-Split nextFrom is not in the checkpoint entry range: %s-%s", minTS.ToString(), maxTS.ToString())
+			return false, moerr.NewErrStaleReadNoCtx(minTS.ToString(), nextFrom.ToString())
+		}
+	*/
+
+	catalog.SetupDefines("")
+
+	var (
+		accountId    = catalog.System_Account
+		tableName    = "test_stale_read"
+		databaseName = "db_stale_read"
+	)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ctx = context.WithValue(ctx, defines.TenantIDKey{}, accountId)
+
+	disttaeEngine, taeHandler, rpcAgent, _ := testutil.CreateEngines(ctx, testutil.TestOptions{}, t)
+	defer func() {
+		disttaeEngine.Close(ctx)
+		taeHandler.Close(true)
+		rpcAgent.Close()
+	}()
+
+	schema := catalog2.MockSchemaAll(20, 0)
+	schema.Name = tableName
+	bat := catalog2.MockBatch(schema, 3)
+	defer bat.Close()
+	bats := bat.Split(3)
+
+	ctx, cancel = context.WithTimeout(ctx, time.Minute*5)
+	defer cancel()
+
+	// Create database and table
+	_, _, err := disttaeEngine.CreateDatabaseAndTable(ctx, databaseName, tableName, schema)
+	require.NoError(t, err)
+
+	// Insert first batch and commit
+	txn, rel := testutil2.GetRelation(t, accountId, taeHandler.GetDB(), databaseName, tableName)
+	require.Nil(t, rel.Append(ctx, bats[0]))
+	require.Nil(t, txn.Commit(ctx))
+	t1 := txn.GetCommitTS()
+
+	txn, rel = testutil2.GetRelation(t, accountId, taeHandler.GetDB(), databaseName, tableName)
+	id := rel.GetMeta().(*catalog2.TableEntry).AsCommonID()
+	require.Nil(t, txn.Commit(ctx))
+
+	// Force checkpoint to create a checkpoint entry
+	now := taeHandler.GetDB().TxnMgr.Now()
+	taeHandler.GetDB().ForceCheckpoint(ctx, now)
+	now = taeHandler.GetDB().TxnMgr.Now()
+	taeHandler.GetDB().ForceCheckpoint(ctx, now)
+
+	// Insert second batch
+	txn, rel = testutil2.GetRelation(t, accountId, taeHandler.GetDB(), databaseName, tableName)
+	require.Nil(t, rel.Append(ctx, bats[1]))
+	require.Nil(t, txn.Commit(ctx))
+	t2 := txn.GetCommitTS()
+
+	// Force another checkpoint
+	now = taeHandler.GetDB().TxnMgr.Now()
+	taeHandler.GetDB().ForceCheckpoint(ctx, now)
+
+	// Insert third batch
+	txn, rel = testutil2.GetRelation(t, accountId, taeHandler.GetDB(), databaseName, tableName)
+	require.Nil(t, rel.Append(ctx, bats[2]))
+	require.Nil(t, txn.Commit(ctx))
+	t3 := txn.GetCommitTS()
+
+	err = disttaeEngine.SubscribeTable(ctx, id.DbID, id.TableID, databaseName, tableName, false)
+	require.Nil(t, err)
+
+	mp := common.DebugAllocator
+
+	// Force GC to clean up old partition state, making t1 fall outside the current partition state range
+	// This will force the code to use RequestSnapshotRead
+	disttaeEngine.Engine.ForceGC(ctx, t2.Next())
+
+	// Setup stub for RequestSnapshotRead to return a checkpoint entry with a range that doesn't include t1
+	// The key is to return checkpoint entries where minTS > t1, which will trigger the stale read error
+	ssStub := gostub.Stub(
+		&disttae.RequestSnapshotRead,
+		disttae.GetSnapshotReadFnWithHandler(
+			func(ctx context.Context, meta pbtxn.TxnMeta, req *cmd_util.SnapshotReadReq, resp *cmd_util.SnapshotReadResp) (func(), error) {
+				// Create a fake checkpoint entry with time range [t2, t3]
+				// When we try to read from t1 (which is < t2), it will be less than minTS
+				// This will trigger the stale read error at line 223-226
+				t2Timestamp := t2.ToTimestamp()
+				t3Timestamp := t3.ToTimestamp()
+
+				resp.Succeed = true
+				resp.Entries = []*cmd_util.CheckpointEntryResp{
+					{
+						Start:     &t2Timestamp,
+						End:       &t3Timestamp,
+						Location1: []byte("fake_location1"),
+						Location2: []byte("fake_location2"),
+						EntryType: 0,
+						Version:   1,
+					},
+				}
+				return func() {}, nil
+			},
+		),
+	)
+	defer ssStub.Reset()
+
+	// Try to collect changes from t1 (which is now before the available checkpoint range)
+	// This should trigger the stale read error at line 223-226
+	{
+		_, rel, _, err := disttaeEngine.GetTable(ctx, databaseName, tableName)
+		require.Nil(t, err)
+
+		// Try to collect changes starting from a time that's been GC'd
+		// Since t1.Prev() < t2 (minTS of checkpoint entry), this should trigger the stale read error
+		handle, err := rel.CollectChanges(ctx, t1.Prev(), t3.Next(), true, mp)
+
+		// We expect either:
+		// 1. An error during CollectChanges (stale read error)
+		// 2. Or an error when iterating through Next()
+		if err != nil {
+			// Check if it's a stale read error
+			t.Logf("Got expected error during CollectChanges: %v", err)
+			assert.True(t, moerr.IsMoErrCode(err, moerr.ErrStaleRead), "Expected stale read error, got: %v", err)
+		} else {
+			// Try to get data, should fail with stale read error
+			gotError := false
+			for {
+				data, tombstone, _, err := handle.Next(ctx, mp)
+				if err != nil {
+					t.Logf("Got expected error during Next: %v", err)
+					assert.True(t, moerr.IsMoErrCode(err, moerr.ErrStaleRead), "Expected stale read error, got: %v", err)
+					gotError = true
+					break
+				}
+				if data != nil {
+					data.Clean(mp)
+				}
+				if tombstone != nil {
+					tombstone.Clean(mp)
+				}
+				if data == nil && tombstone == nil {
+					break
+				}
+			}
+			// We expect to get the stale read error
+			assert.True(t, gotError, "Expected to encounter stale read error")
+			if handle != nil {
+				handle.Close()
+			}
+		}
 	}
 }
