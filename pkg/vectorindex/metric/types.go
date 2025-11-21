6b326bfc7579da1019291cdd9ffc5d771a4806e8
bRong Njam <longran1989@gmail.com>
2025-11-21 17:17:04 +0800

    fix ivfflat distfunc inconsistency (#23052)
    ## What type of PR is this?
    
    - [ ] API-change
    - [x] BUG
    - [ ] Improvement
    - [ ] Documentation
    - [ ] Feature
    - [ ] Test and CI
    - [ ] Code Refactoring
    
    ## Which issue(s) this PR fixes:
    
    issue https://github.com/matrixorigin/MO-Cloud/issues/6660
    
    ## What this PR does / why we need it:
    cherry-pick changes from main
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pkg/vectorindex/metric/types.go
+++ pkg/vectorindex/metric/types.go
@@ -26,6 +26,7 @@ type MetricType uint16
 
 const (
 	OpType_L2Distance     = "vector_l2_ops"
+	OpType_L2sqDistance   = "vector_l2sq_ops"
 	OpType_InnerProduct   = "vector_ip_ops"
 	OpType_CosineDistance = "vector_cosine_ops"
 	OpType_L1Distance     = "vector_l1_ops"
@@ -58,14 +59,10 @@ var (
 		DistFn_InnerProduct:   OpType_InnerProduct,
 		DistFn_CosineDistance: OpType_CosineDistance,
 	}
-	DistFuncInternalDistFunc = map[string]string{
-		DistFn_L2Distance:     DistIntFn_L2Distance,
-		DistFn_InnerProduct:   DistIntFn_InnerProduct,
-		DistFn_CosineDistance: DistIntFn_CosineDistance,
-	}
 
 	OpTypeToIvfMetric = map[string]MetricType{
-		OpType_L2Distance:     Metric_L2Distance,
+		OpType_L2Distance:     Metric_L2sqDistance,
+		OpType_L2sqDistance:   Metric_L2sqDistance,
 		OpType_InnerProduct:   Metric_InnerProduct,
 		OpType_CosineDistance: Metric_CosineDistance,
 		OpType_L1Distance:     Metric_L1Distance,
@@ -73,6 +70,7 @@ var (
 
 	OpTypeToUsearchMetric = map[string]usearch.Metric{
 		OpType_L2Distance:     usearch.L2sq,
+		OpType_L2sqDistance:   usearch.L2sq,
 		OpType_InnerProduct:   usearch.InnerProduct,
 		OpType_CosineDistance: usearch.Cosine,
 		/*
