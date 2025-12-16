9c94d310a2e8d07c8d16953f41ccbd071920399c
Eric <cpegeric@gmail.com>
2025-12-16 12:12:38 +0000

    gpu concurrent test

--- pkg/vectorindex/brute_force/gpu_test.go
+++ pkg/vectorindex/brute_force/gpu_test.go
@@ -18,11 +18,15 @@ package brute_force
 
 import (
 	//"fmt"
+	"math/rand/v2"
 	"sync"
 	"testing"
 
+	"github.com/matrixorigin/matrixone/pkg/common/mpool"
+	"github.com/matrixorigin/matrixone/pkg/testutil"
 	"github.com/matrixorigin/matrixone/pkg/vectorindex"
 	"github.com/matrixorigin/matrixone/pkg/vectorindex/metric"
+	"github.com/matrixorigin/matrixone/pkg/vectorindex/sqlexec"
 	"github.com/stretchr/testify/require"
 )
 
@@ -69,4 +73,67 @@ func TestGpuBruteForce(t *testing.T) {
 
 	wg.Wait()
 
+}
+
+func TestGpuBruteForceConcurrent(t *testing.T) {
+
+	m := mpool.MustNewZero()
+	proc := testutil.NewProcessWithMPool(t, "", m)
+	sqlproc := sqlexec.NewSqlProcess(proc)
+	dimension := uint(128)
+	ncpu := uint(4)
+	limit := uint(3)
+	elemsz := uint(4) // float32
+
+	dsize := 10000
+	dataset := make([][]float32, dsize)
+	for i := range dataset {
+		dataset[i] = make([]float32, dimension)
+		for j := range dataset[i] {
+			dataset[i][j] = rand.Float32()
+		}
+	}
+
+	query := dataset
+
+	idx, err := NewGpuBruteForceIndex[float32](dataset, dimension, metric.Metric_L2sqDistance, elemsz)
+	require.NoError(t, err)
+	defer idx.Destroy()
+
+	err = idx.Load(nil)
+	require.NoError(t, err)
+
+	// limit 3
+	{
+		rt := vectorindex.RuntimeConfig{Limit: limit, NThreads: ncpu}
+
+		anykeys, distances, err := idx.Search(sqlproc, query, rt)
+		require.NoError(t, err)
+
+		keys := anykeys.([]int64)
+		// fmt.Printf("keys %v, dist %v\n", keys, distances)
+		require.Equal(t, int(rt.Limit)*len(query), len(keys))
+		for i := range query {
+			offset := i * int(rt.Limit)
+			require.Equal(t, int64(i), keys[offset])
+			require.Equal(t, float64(0), distances[offset])
+		}
+	}
+
+	// limit 1
+	{
+		rt := vectorindex.RuntimeConfig{Limit: 1, NThreads: ncpu}
+
+		anykeys, distances, err := idx.Search(sqlproc, query, rt)
+		require.NoError(t, err)
+
+		keys := anykeys.([]int64)
+		// fmt.Printf("keys %v, dist %v\n", keys, distances)
+		require.Equal(t, int(rt.Limit)*len(query), len(keys))
+		for i := range query {
+			offset := i * int(rt.Limit)
+			require.Equal(t, int64(i), keys[offset])
+			require.Equal(t, float64(0), distances[offset])
+		}
+	}
 }
