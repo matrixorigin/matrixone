f9864d77108f1a14ccaca79abdbaa5dce59da386
XuPeng-SH <xupeng3112@163.com>
2025-12-29 14:51:10 +0800

    fix: panic during muti-cn ivf search (#23388)
    fix: panic during muti-cn ivf search
    
    Approved by: @aunjgr

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -2168,7 +2168,7 @@ func (c *Compile) compileTableScanDataSource(s *Scope) error {
 	s.DataSource.AccountId = node.ObjRef.GetPubInfo()
 	s.DataSource.RuntimeFilterSpecs = node.RuntimeFilterProbeList
 	s.DataSource.OrderBy = node.OrderBy
-	s.DataSource.IndexReaderParam = node.IndexReaderParam
+	s.DataSource.IndexReaderParam = plan2.DeepCopyIndexReaderParam(node.IndexReaderParam)
 	s.DataSource.RecvMsgList = node.RecvMsgList
 
 	return nil
