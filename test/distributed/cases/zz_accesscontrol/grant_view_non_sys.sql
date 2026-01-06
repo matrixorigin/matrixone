00c45be0ea655bfc147e7a11c8b570ae8aa1882d
gouhongshen <gouhongshen@hotmail.com>
2026-01-06 11:30:25 +0800

    test: add BVT tests and results for GRANT/REVOKE on VIEW

--- test/distributed/cases/zz_accesscontrol/grant_view_non_sys.sql
+++ test/distributed/cases/zz_accesscontrol/grant_view_non_sys.sql
@@ -8,52 +8,36 @@ create account acc_view_test admin_name = 'admin' identified by '111';
 
 -- switch to tenant admin
 -- @session:id=1&user=acc_view_test:admin&password=111
-select account_id, user_name, role_name from mo_catalog.mo_user where user_name = 'admin';
-
 -- setup inside tenant
-create user user_v1 identified by '111';
 create role role_v1;
+create user user_v1 identified by '111' default role role_v1;
 grant role_v1 to user_v1;
+
 create database db_v1;
 use db_v1;
 create table t1 (a int);
 insert into t1 values (1);
 create view v1 as select * from t1;
 
--- test case 1: default no permission for normal user
+-- test case 1: grant permissions
+grant select on view db_v1.v1 to role_v1;
+
+-- test case 2: verify grant appears
 -- @session:id=2&user=acc_view_test:user_v1&password=111
-use db_v1;
--- @ignore:error
-select * from v1;
+show grants;
 -- @session
 
--- back to tenant admin to grant
+-- back to tenant admin
 -- @session:id=1&user=acc_view_test:admin&password=111
-use db_v1;
-grant select on db_v1.v1 to role_v1;
+revoke select on view db_v1.v1 from role_v1;
 -- @session
 
--- test case 2: verify permission
+-- test case 3: verify revoke
 -- @session:id=2&user=acc_view_test:user_v1&password=111
-use db_v1;
-select * from v1;
--- @session
-
--- back to tenant admin to revoke
--- @session:id=1&user=acc_view_test:admin&password=111
-revoke select on db_v1.v1 from role_v1;
--- @session
-
--- test case 3: verify revoked
--- @session:id=2&user=acc_view_test:user_v1&password=111
-use db_v1;
--- @ignore:error
-select * from v1;
--- @session
-
--- cleanup inside tenant (optional, dropping account cleans all)
+show grants;
 -- @session
 
 -- cleanup: drop account
+-- @session
 drop account acc_view_test;
 set global enable_privilege_cache = on;
