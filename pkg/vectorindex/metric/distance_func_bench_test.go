757606dada22fa9b3ee7be99e7e75b0013e52515
cpegeric <cpegeric@gmail.com>
2026-01-09 14:00:41 +0000

    add benchmark

--- pkg/vectorindex/metric/distance_func_bench_test.go
+++ pkg/vectorindex/metric/distance_func_bench_test.go
@@ -27,8 +27,8 @@ Benchmark_L2Distance/L2_Distance(v1,_NormalizeL2)-10         	     589376	      
 func Benchmark_L2Distance(b *testing.B) {
 	dim := 128
 
-	b.Run("L2 Distance", func(b *testing.B) {
-		v1, v2 := randomVectors(b.N, dim), randomVectors(b.N, dim)
+	b.Run("L2 Distance float64", func(b *testing.B) {
+		v1, v2 := randomVectors[float64](b.N, dim), randomVectors[float64](b.N, dim)
 		b.ResetTimer()
 
 		for i := 0; i < b.N; i++ {
@@ -36,8 +36,17 @@ func Benchmark_L2Distance(b *testing.B) {
 		}
 	})
 
-	b.Run("Normalize L2", func(b *testing.B) {
-		v1 := randomVectors(b.N, dim)
+	b.Run("L2 Distance float32", func(b *testing.B) {
+		v1, v2 := randomVectors[float32](b.N, dim), randomVectors[float32](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, _ = L2Distance[float32](v1[i], v2[i])
+		}
+	})
+
+	b.Run("Normalize L2 float64", func(b *testing.B) {
+		v1 := randomVectors[float64](b.N, dim)
 		b.ResetTimer()
 
 		for i := 0; i < b.N; i++ {
@@ -46,8 +55,18 @@ func Benchmark_L2Distance(b *testing.B) {
 		}
 	})
 
-	b.Run("L2 Distance(v1, NormalizeL2)", func(b *testing.B) {
-		v1, v2 := randomVectors(b.N, dim), randomVectors(b.N, dim)
+	b.Run("Normalize L2 float32", func(b *testing.B) {
+		v1 := randomVectors[float32](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			res := make([]float32, dim)
+			_ = NormalizeL2[float32](v1[i], res)
+		}
+	})
+
+	b.Run("L2 Distance(v1, NormalizeL2) float64", func(b *testing.B) {
+		v1, v2 := randomVectors[float64](b.N, dim), randomVectors[float64](b.N, dim)
 		b.ResetTimer()
 
 		for i := 0; i < b.N; i++ {
@@ -56,14 +75,168 @@ func Benchmark_L2Distance(b *testing.B) {
 			_, _ = L2Distance[float64](v1[i], res)
 		}
 	})
-
 }
 
-func randomVectors(size, dim int) [][]float64 {
-	vectors := make([][]float64, size)
+func Benchmark_L2DistanceSq(b *testing.B) {
+	dim := 128
+
+	b.Run("L2 DistanceSq float64", func(b *testing.B) {
+		v1, v2 := randomVectors[float64](b.N, dim), randomVectors[float64](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, _ = L2DistanceSq[float64](v1[i], v2[i])
+		}
+	})
+
+	b.Run("L2 DistanceSq float32", func(b *testing.B) {
+		v1, v2 := randomVectors[float32](b.N, dim), randomVectors[float32](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, _ = L2DistanceSq[float32](v1[i], v2[i])
+		}
+	})
+}
+
+func Benchmark_L1Distance(b *testing.B) {
+	dim := 128
+
+	b.Run("L1 Distance float64", func(b *testing.B) {
+		v1, v2 := randomVectors[float64](b.N, dim), randomVectors[float64](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, _ = L1Distance[float64](v1[i], v2[i])
+		}
+	})
+
+	b.Run("L1 Distance float32", func(b *testing.B) {
+		v1, v2 := randomVectors[float32](b.N, dim), randomVectors[float32](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, _ = L1Distance[float32](v1[i], v2[i])
+		}
+	})
+}
+
+func Benchmark_InnerProduct(b *testing.B) {
+	dim := 128
+
+	b.Run("Inner Product float64", func(b *testing.B) {
+		v1, v2 := randomVectors[float64](b.N, dim), randomVectors[float64](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, _ = InnerProduct[float64](v1[i], v2[i])
+		}
+	})
+
+	b.Run("Inner Product float32", func(b *testing.B) {
+		v1, v2 := randomVectors[float32](b.N, dim), randomVectors[float32](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, _ = InnerProduct[float32](v1[i], v2[i])
+		}
+	})
+}
+
+func Benchmark_CosineDistance(b *testing.B) {
+	dim := 128
+
+	b.Run("Cosine Distance float64", func(b *testing.B) {
+		v1, v2 := randomVectors[float64](b.N, dim), randomVectors[float64](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, _ = CosineDistance[float64](v1[i], v2[i])
+		}
+	})
+
+	b.Run("Cosine Distance float32", func(b *testing.B) {
+		v1, v2 := randomVectors[float32](b.N, dim), randomVectors[float32](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, _ = CosineDistance[float32](v1[i], v2[i])
+		}
+	})
+}
+
+func Benchmark_CosineSimilarity(b *testing.B) {
+	dim := 128
+
+	b.Run("Cosine Similarity float64", func(b *testing.B) {
+		v1, v2 := randomVectors[float64](b.N, dim), randomVectors[float64](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, _ = CosineSimilarity[float64](v1[i], v2[i])
+		}
+	})
+
+	b.Run("Cosine Similarity float32", func(b *testing.B) {
+		v1, v2 := randomVectors[float32](b.N, dim), randomVectors[float32](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, _ = CosineSimilarity[float32](v1[i], v2[i])
+		}
+	})
+}
+
+func Benchmark_SphericalDistance(b *testing.B) {
+	dim := 128
+
+	b.Run("Spherical Distance float64", func(b *testing.B) {
+		v1, v2 := randomVectors[float64](b.N, dim), randomVectors[float64](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, _ = SphericalDistance[float64](v1[i], v2[i])
+		}
+	})
+
+	b.Run("Spherical Distance float32", func(b *testing.B) {
+		v1, v2 := randomVectors[float32](b.N, dim), randomVectors[float32](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_, _ = SphericalDistance[float32](v1[i], v2[i])
+		}
+	})
+}
+
+func Benchmark_ScaleInPlace(b *testing.B) {
+	dim := 128
+
+	b.Run("ScaleInPlace float64", func(b *testing.B) {
+		v1 := randomVectors[float64](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			ScaleInPlace[float64](v1[i], 0.5)
+		}
+	})
+
+	b.Run("ScaleInPlace float32", func(b *testing.B) {
+		v1 := randomVectors[float32](b.N, dim)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			ScaleInPlace[float32](v1[i], 0.5)
+		}
+	})
+}
+
+func randomVectors[T float32 | float64](size, dim int) [][]T {
+	vectors := make([][]T, size)
 	for i := range vectors {
+		vectors[i] = make([]T, dim)
 		for j := 0; j < dim; j++ {
-			vectors[i] = append(vectors[i], rand.Float64())
+			vectors[i][j] = T(rand.Float64())
 		}
 	}
 	return vectors
