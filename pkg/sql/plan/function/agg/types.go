cbfccb021ec32f3dac2bf21e16cba0f66c7cfdff
flypiggyyoyoyo <shran2004@outlook.com>
2026-01-09 11:07:46 +0800

    fix ci

--- pkg/sql/plan/function/agg/types.go
+++ pkg/sql/plan/function/agg/types.go
@@ -23,6 +23,10 @@ type numeric interface {
 	types.Ints | types.UInts | types.Floats
 }
 
+type numericWithMaxScale interface {
+	int64 | uint64 | float64
+}
+
 type canCompare interface {
 	constraints.Integer | constraints.Float | types.Date | types.Datetime | types.Timestamp | types.Time | types.MoYear
 }
