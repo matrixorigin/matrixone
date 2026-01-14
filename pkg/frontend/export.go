a7a92c8e6431a56415fae839cccf8f346b4b5b9c
gouhongshen <gouhongshen@hotmail.com>
2026-01-14 14:33:35 +0800

    fix export

--- pkg/frontend/export.go
+++ pkg/frontend/export.go
@@ -338,17 +338,8 @@ var writeDataToCSVFile = func(ep *ExportConfig, output []byte) error {
 	return nil
 }
 
-func formatJsonString(str string, flag bool, terminatedBy string) string {
-	if len(str) < 2 {
-		return "\"" + str + "\""
-	}
-	var tmp string
-	if !flag {
-		tmp = strings.ReplaceAll(str, terminatedBy, "\\"+terminatedBy)
-	} else {
-		tmp = strings.ReplaceAll(str, "\",", "\"\",")
-	}
-	return tmp
+func formatJsonString(str string) string {
+        return strings.ReplaceAll(str, "\"", "\"\"")
 }
 
 func escapeJSONControlChars(s string) string {
@@ -421,7 +412,6 @@ func constructByte(ctx context.Context, obj FeSession, bat *batch.Batch, index i
 
 	symbol := ep.Symbol
 	closeby := ep.userConfig.Fields.EnclosedBy.Value
-	terminated := ep.userConfig.Fields.Terminated.Value
 	flag := ep.ColumnFlag
 
 	buffer := &bytes.Buffer{}
@@ -434,13 +424,9 @@ func constructByte(ctx context.Context, obj FeSession, bat *batch.Batch, index i
 			}
 			switch vec.GetType().Oid { //get col
 			case types.T_json:
-				val := types.DecodeJson(vec.GetBytesAt(i))
-				formatOutputString(ep, []byte(formatJsonString(
-					escapeJSONControlChars(
-						val.String(),
-					),
-					flag[j], terminated),
-				), symbol[j], closeby, flag[j], buffer)
+                                val := types.DecodeJson(vec.GetBytesAt(i))
+                                value := addEscapeToString([]byte(val.String()), closeby)
+                                formatOutputString(ep, value, symbol[j], closeby, true, buffer)
 			case types.T_bool:
 				val := vector.GetFixedAtNoTypeCheck[bool](vec, i)
 				if val {
@@ -593,24 +579,11 @@ func constructByte(ctx context.Context, obj FeSession, bat *batch.Batch, index i
 }
 
 func addEscapeToString(s []byte, escape byte) []byte {
-	pos := make([]int, 0)
-	for i := 0; i < len(s); i++ {
-		if s[i] == escape {
-			pos = append(pos, i)
-		}
-	}
-	if len(pos) == 0 {
-		return s
-	}
-	ret := make([]byte, 0)
-	cur := 0
-	for i := 0; i < len(pos); i++ {
-		ret = append(ret, s[cur:pos[i]]...)
-		ret = append(ret, escape)
-		cur = pos[i]
-	}
-	ret = append(ret, s[cur:]...)
-	return ret
+        s = bytes.ReplaceAll(s, []byte("\\"[:1]), []byte("\\\\"[:2]))
+        if escape != 0 && escape != "\\"[0] {
+                s = bytes.ReplaceAll(s, []byte{escape}, []byte{escape, escape})
+        }
+        return s
 }
 
 func exportDataFromResultSetToCSVFile(oq *ExportConfig) error {
