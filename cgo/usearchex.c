a38411f844981af11a831ebf80702f45167587de
cpegeric <cpegeric@gmail.com>
2026-01-16 13:05:32 +0000

    cleanup

--- cgo/usearchex.c
+++ cgo/usearchex.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int filtered_search_cb(usearch_key_t key, void *data) {
+static int filtered_search_cb(usearch_key_t key, void *data) {
     bloomfilter_t *bf = (bloomfilter_t*) data;
     if (bf) {
         return bloomfilter_test(bf, (const char *)&key, sizeof(usearch_key_t));
@@ -17,5 +17,13 @@ size_t usearchex_filtered_search_with_bloomfilter(
     void *bf,
     usearch_key_t* keys, usearch_distance_t* distances, usearch_error_t* error) {
 
-    return usearch_filtered_search((usearch_index_t)index, query_vector, (usearch_scalar_kind_t)query_kind, count, filtered_search_cb, (bloomfilter_t*)bf, keys, distances, error);
+    return usearch_filtered_search((usearch_index_t)index, 
+            query_vector, 
+            (usearch_scalar_kind_t)query_kind, 
+            count, 
+            filtered_search_cb, 
+            (bloomfilter_t*)bf, 
+            keys, 
+            distances, 
+            error);
 }
