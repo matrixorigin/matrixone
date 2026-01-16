fb390abb1fcc932d96f2b06549887816e6c4cc3d
cpegeric <cpegeric@gmail.com>
2026-01-16 10:17:40 +0000

    usearchex

---
+++ cgo/usearchex.c
@@ -0,0 +1,21 @@
+#include "usearchex.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+
+int filtered_search_cb(usearch_key_t key, void *data) {
+    bloomfilter_t *bf = (bloomfilter_t*) data;
+    if (bf) {
+        return bloomfilter_test(bf, (const char *)&key, sizeof(usearch_key_t));
+    }
+    return 1;
+}
+
+size_t usearchex_filtered_search_with_bloomfilter(
+    usearch_index_t index,
+    void const* query_vector, usearch_scalar_kind_t query_kind, size_t count,
+    bloomfilter_t *bf,
+    usearch_key_t* keys, usearch_distance_t* distances, usearch_error_t* error) {
+
+    return usearch_filtered_search(index, query_vector, query_kind, count, filtered_search_cb, bf, keys, distances, error);
+}
---
+++ cgo/usearchex.h
@@ -0,0 +1,17 @@
+#ifndef _USEARCH_EXTEND_H
+#define _USEARCH_EXTEND_H
+
+#include "bloom.h"
+#include "usearch.h"
+
+typedef char const *mo_error_t;
+
+/* usearch */
+size_t usearchex_filtered_search_with_bloomfilter(
+    usearch_index_t index,
+    void const* query_vector, usearch_scalar_kind_t query_kind, size_t count,
+    bloomfilter_t *bf,
+    usearch_key_t* keys, usearch_distance_t* distances, usearch_error_t* error);
+
+#endif
+
