b7cb28f83f3fb3461752f1c75f81a9baae7a62f6
robll-v1 <3922295484@qq.com>
2026-01-26 13:41:21 +0800

    add some sql

--- test/distributed/cases/operator/row_constructor.sql
+++ test/distributed/cases/operator/row_constructor.sql
@@ -30,6 +30,11 @@ select (1,2) in ((1,3),(3,4));
 select (1,2) in ((1,2));
 select (1,2) not in ((1,2));
 select (1,2) not in ((1,3),(3,4));
+select (1,2) in ((1,null));
+select (1,2) in ((1,null),(1,2));
+select (1,2) not in ((1,null));
+select (1,null) in ((1,null));
+select (1,2) not in ((1,null),(2,2));
 
 -- + - * / % mod
 select (1,2,3) > (-1,-3+2,2*3);
