c123cb37b689ef05c40fa301a11319f41fbe3aed
robll-v1 <3922295484@qq.com>
2026-02-04 17:29:54 +0800

    fix error

--- test/distributed/cases/udf/udf.sql
+++ test/distributed/cases/udf/udf.sql
@@ -15,12 +15,11 @@ use db1;
 create function db1.helloworld5 () returns int language sql as 'select id from tbl1 limit 1';
 drop function db1.helloworld5();
 drop function db2.helloworld5();
-use mo_catalog;
+create database if not exists udf_bvt;
+use udf_bvt;
 drop database if exists db1;
 
 -- bvt: preserve backslash in $$...$$ string for UDF body
-create database if not exists udf_bvt;
-use udf_bvt;
 create function sql_nl() returns varchar language sql as $$select '\\n'$$;
 select hex(body) as body_hex from mo_catalog.mo_user_defined_function where name = 'sql_nl' and db = 'udf_bvt';
 drop database udf_bvt;
