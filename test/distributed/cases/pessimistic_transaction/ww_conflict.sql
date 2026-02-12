5e2da90b29795f6a228750e7dc9f2f8557edd9c7
Ariznawl@163.com <ariznawl@163.com>
2026-02-12 15:42:52 +0800

    add tag 23741

--- test/distributed/cases/pessimistic_transaction/ww_conflict.sql
+++ test/distributed/cases/pessimistic_transaction/ww_conflict.sql
@@ -22,6 +22,7 @@ start transaction;
 drop table if exists dis_table_02;
 
 -- transcation: w-w conflict
+-- @bvt:issue#23741
 drop table if exists dis_table_02;
 create table dis_table_02(a int not null auto_increment,b varchar(25) not null,c datetime,primary key(a),key bstr (b),key cdate (c) );
 insert into dis_table_02(b,c) values ('aaaa','2020-09-08');
@@ -39,7 +40,7 @@ select * from dis_table_02;
 -- @session}
 select * from dis_table_02;
 drop table dis_table_02;
-
+-- @bvt:issue
 ---------------------------------------------------
 -- alter table add primary key
 drop table if exists dis_table_02;
