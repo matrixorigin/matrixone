0366bd736ab742e650b5b98c5f93f2041a88ef4a
Eric Lam <cpegeric@gmail.com>
2026-02-27 06:45:16 +0000

    ivfflat index build port from main (#23737)
    port from main
    
    Approved by: @ouyuanning, @fengttt, @heni02, @XuPeng-SH, @aunjgr

---
+++ pkg/vectorindex/brute_force/brute_force_test.go
@@ -0,0 +1,149 @@
+//go:build !gpu
+
+// Copyright 2022 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package brute_force
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"testing"
+
+	"github.com/matrixorigin/matrixone/pkg/common/mpool"
+	"github.com/matrixorigin/matrixone/pkg/testutil"
+	"github.com/matrixorigin/matrixone/pkg/vectorindex"
+	"github.com/matrixorigin/matrixone/pkg/vectorindex/cache"
+	"github.com/matrixorigin/matrixone/pkg/vectorindex/metric"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBruteForce(t *testing.T) {
+
+	m := mpool.MustNewZero()
+	proc := testutil.NewProcessWithMPool(t, "", m)
+
+	dataset := [][]float32{{1, 2, 3}, {3, 4, 5}}
+	query := [][]float32{{1, 2, 3}, {3, 4, 5}}
+	dimension := uint(3)
+	ncpu := uint(1)
+	limit := uint(2)
+	elemsz := uint(4) // float32
+
+	idx, err := NewUsearchBruteForceIndex[float32](dataset, dimension, metric.Metric_L2sqDistance, elemsz)
+	require.NoError(t, err)
+
+	rt := vectorindex.RuntimeConfig{Limit: limit, NThreads: ncpu}
+
+	keys, distances, err := idx.Search(proc, query, rt)
+	require.NoError(t, err)
+	fmt.Printf("keys %v, dist %v\n", keys, distances)
+
+}
+
+func TestGoBruteForce(t *testing.T) {
+
+	m := mpool.MustNewZero()
+	proc := testutil.NewProcessWithMPool(t, "", m)
+
+	dataset := [][]float32{{1, 2, 3}, {3, 4, 5}}
+	query := [][]float32{{1, 2, 3}, {3, 4, 5}}
+	dimension := uint(3)
+	ncpu := uint(1)
+	limit := uint(2)
+	elemsz := uint(4) // float32
+
+	idx, err := NewGoBruteForceIndex[float32](dataset, dimension, metric.Metric_L2sqDistance, elemsz)
+	require.NoError(t, err)
+
+	rt := vectorindex.RuntimeConfig{Limit: limit, NThreads: ncpu}
+
+	keys, distances, err := idx.Search(proc, query, rt)
+	require.NoError(t, err)
+	fmt.Printf("keys %v, dist %v\n", keys, distances)
+
+}
+
+func runBruteForceConcurrent(t *testing.T, is_usearch bool) {
+
+	m := mpool.MustNewZero()
+	proc := testutil.NewProcessWithMPool(t, "", m)
+	dimension := uint(128)
+	ncpu := uint(4)
+	limit := uint(3)
+	elemsz := uint(4) // float32
+
+	dsize := 10000
+	dataset := make([][]float32, dsize)
+	for i := range dataset {
+		dataset[i] = make([]float32, dimension)
+		for j := range dataset[i] {
+			dataset[i][j] = rand.Float32()
+		}
+	}
+
+	query := dataset
+
+	var idx cache.VectorIndexSearchIf
+	var err error
+	if is_usearch {
+		idx, err = NewUsearchBruteForceIndex[float32](dataset, dimension, metric.Metric_L2sqDistance, elemsz)
+	} else {
+		idx, err = NewGoBruteForceIndex[float32](dataset, dimension, metric.Metric_L2sqDistance, elemsz)
+	}
+	require.NoError(t, err)
+
+	// limit 3
+	{
+		rt := vectorindex.RuntimeConfig{Limit: limit, NThreads: ncpu}
+
+		anykeys, distances, err := idx.Search(proc, query, rt)
+		require.NoError(t, err)
+
+		keys := anykeys.([]int64)
+		// fmt.Printf("keys %v, dist %v\n", keys, distances)
+		require.Equal(t, int(rt.Limit)*len(query), len(keys))
+		for i := range query {
+			offset := i * int(rt.Limit)
+			require.Equal(t, int64(i), keys[offset])
+			require.Equal(t, float64(0), distances[offset])
+		}
+	}
+
+	// limit 1
+	{
+		rt := vectorindex.RuntimeConfig{Limit: 1, NThreads: ncpu}
+
+		anykeys, distances, err := idx.Search(proc, query, rt)
+		require.NoError(t, err)
+
+		keys := anykeys.([]int64)
+		// fmt.Printf("keys %v, dist %v\n", keys, distances)
+		require.Equal(t, int(rt.Limit)*len(query), len(keys))
+		for i := range query {
+			offset := i * int(rt.Limit)
+			require.Equal(t, int64(i), keys[offset])
+			require.Equal(t, float64(0), distances[offset])
+		}
+	}
+
+}
+
+func TestGoBruteForceConcurrent(t *testing.T) {
+	runBruteForceConcurrent(t, false)
+}
+
+func TestUsearchBruteForceConcurrent(t *testing.T) {
+	runBruteForceConcurrent(t, true)
+}
