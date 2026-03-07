938d4c6f58390ef46a0a88ebfeb6246bd18f2e0d
cpegeric <cpegeric@gmail.com>
2026-03-07 19:30:49 +0000

    bug fix cap < n pool.Put back the memory

--- pkg/sql/colexec/productl2/product_l2.go
+++ pkg/sql/colexec/productl2/product_l2.go
@@ -226,10 +226,11 @@ var (
 func get1D[T any](pool *sync.Pool, n int) *[]T {
 	v := pool.Get().(*[]T)
 	if cap(*v) < n {
-		*v = make([]T, n)
-	} else {
-		*v = (*v)[:n]
+		pool.Put(v)
+		newSlice := make([]T, n)
+		return &newSlice
 	}
+	*v = (*v)[:n]
 	return v
 }
 
