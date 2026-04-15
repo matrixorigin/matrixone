91174fd04f61f97d523f99cf9e5cd9b8a23f7bde
Cao Kai <ck89119@gmail.com>
2026-04-15 20:21:08 +0800

    fix: address review races in GlobalStats.Get

--- pkg/vm/engine/disttae/stats.go
+++ pkg/vm/engine/disttae/stats.go
@@ -169,6 +169,9 @@ type GlobalStats struct {
 
 	// beforeCacheRemoteInfo is for test only.
 	beforeCacheRemoteInfo func(pb.StatsInfoKey)
+
+	// beforeSubscribeTable is for test only.
+	beforeSubscribeTable func(pb.StatsInfoKey)
 }
 
 func NewGlobalStats(
@@ -262,6 +265,9 @@ func (gs *GlobalStats) cacheRemoteInfoIfSubscribed(
 	}
 
 	gs.mu.statsInfoMap[key] = remoteInfo
+	if gs.mu.cond != nil {
+		gs.mu.cond.Broadcast()
+	}
 	return remoteInfo
 }
 
@@ -281,6 +287,9 @@ func (gs *GlobalStats) Get(ctx context.Context, key pb.StatsInfoKey, sync bool) 
 
 	// after checking first potential patched cache
 	// we check the approx to avoid taking a place in statInfo map
+	if gs.beforeSubscribeTable != nil {
+		gs.beforeSubscribeTable(key)
+	}
 	ps, err := gs.engine.pClient.toSubscribeTable(
 		ctx,
 		uint64(key.AccId),
