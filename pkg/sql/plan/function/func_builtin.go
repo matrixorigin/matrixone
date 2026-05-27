fbb3531ab3080b87c8f7de4f529dc541d04321b7
davis zhen <daviszhen007@gmail.com>
2026-05-27 15:53:48 +0800

    update

--- pkg/sql/plan/function/func_builtin.go
+++ pkg/sql/plan/function/func_builtin.go
@@ -2894,6 +2894,24 @@ func builtInASin(parameters []*vector.Vector, result vector.FunctionResultWrappe
 	return nil
 }
 
+func builtInDegrees(parameters []*vector.Vector, result vector.FunctionResultWrapper, proc *process.Process, length int, selectList *FunctionSelectList) error {
+	p1 := vector.GenerateFunctionFixedTypeParameter[float64](parameters[0])
+	rs := vector.MustFunctionResult[float64](result)
+	for i := uint64(0); i < uint64(length); i++ {
+		v, null := p1.GetValue(i)
+		if null {
+			if err := rs.Append(0, true); err != nil {
+				return err
+			}
+		} else {
+			if err := rs.Append(v*180/math.Pi, false); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
 func builtInATan(parameters []*vector.Vector, result vector.FunctionResultWrapper, proc *process.Process, length int, selectList *FunctionSelectList) error {
 	p1 := vector.GenerateFunctionFixedTypeParameter[float64](parameters[0])
 	rs := vector.MustFunctionResult[float64](result)
@@ -2935,13 +2953,6 @@ func builtInATan2(parameters []*vector.Vector, result vector.FunctionResultWrapp
 		}
 	}
 	return nil
-}
-
-func builtInDegrees(parameters []*vector.Vector, result vector.FunctionResultWrapper, proc *process.Process, length int, selectList *FunctionSelectList) error {
-	return opUnaryFixedToFixed[float64, float64](parameters, result, proc, length, func(v float64) float64 {
-		// Convert radians to degrees: degrees = radians * (180 / π)
-		return v * (180.0 / math.Pi)
-	}, selectList)
 }
 
 func builtInRadians(parameters []*vector.Vector, result vector.FunctionResultWrapper, proc *process.Process, length int, selectList *FunctionSelectList) error {
