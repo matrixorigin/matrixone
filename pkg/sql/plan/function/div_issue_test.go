b1a555d50f0c36bfc5b8d4edcae08d89c7aab50d
jiangxinmeng <jiangxinmeng@matrixorigin.io>
2026-05-29 16:19:11 +0800

    fix: preserve decimal div overflow error

--- pkg/sql/plan/function/div_issue_test.go
+++ pkg/sql/plan/function/div_issue_test.go
@@ -1,3 +1,17 @@
+// Copyright 2026 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package function
 
 import (
@@ -40,6 +54,11 @@ func TestDecimal128IntDivCorrectness(t *testing.T) {
 		succeed, info := tcc.Run()
 		require.True(t, succeed, tc.info, info)
 	}
+}
+
+func TestDecimal128IntDivOverflowMessage(t *testing.T) {
+	_, err := decimal128IntDivToInt64(mustParseD128("12345678909876543212345678909876543243"), types.Decimal128{B0_63: 4}, 0, 0)
+	require.ErrorContains(t, err, "data out of range: data type BIGINT")
 }
 
 func mustParseD128(s string) types.Decimal128 {
