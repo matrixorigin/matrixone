540d9c785abd1ecbeb8f79f568e61b607b5ddac6
ULookup <CHBulookup@outlook.com>
2026-06-04 21:02:05 -0700

    fix(plan): gofmt whitespace in mock.go

--- pkg/sql/plan/mock.go
+++ pkg/sql/plan/mock.go
@@ -683,7 +683,7 @@ func NewMockCompilerContext(isDml bool) *MockCompilerContext {
 	*/
 	constraintTestSchema["dept"] = &Schema{
 		tblId: 88888,
-		cols:  []col{
+		cols: []col{
 			{"deptno", types.T_uint32, true, 32, 0},
 			{"dname", types.T_varchar, true, 15, 0},
 			{"loc", types.T_varchar, true, 50, 0},
@@ -746,7 +746,7 @@ func NewMockCompilerContext(isDml bool) *MockCompilerContext {
 	*/
 	constraintTestSchema["dept_composite_uk"] = &Schema{
 		tblId: 88889,
-		cols:  []col{
+		cols: []col{
 			{"deptno", types.T_uint32, true, 32, 0},
 			{"dname", types.T_varchar, true, 15, 0},
 			{"loc", types.T_varchar, true, 50, 0},
