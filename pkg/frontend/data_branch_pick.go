e4cb379e4c66b5be875b5bbb4d32ae6f414aab6c
gouhongshen <gouhongshen@hotmail.com>
2026-06-15 16:10:01 +0800

    fix: support decimal256 data branch pick keys

--- pkg/frontend/data_branch_pick.go
+++ pkg/frontend/data_branch_pick.go
@@ -19,6 +19,7 @@ import (
 	"context"
 	"fmt"
 	"math"
+	"math/big"
 	"runtime"
 	"strconv"
 	"strings"
@@ -445,7 +446,7 @@ func extractPKAsString(
 func extractPKVal(vec *vector.Vector, rowIdx int) any {
 	switch vec.GetType().Oid {
 	case types.T_datetime, types.T_timestamp, types.T_decimal64,
-		types.T_decimal128, types.T_time:
+		types.T_decimal128, types.T_decimal256, types.T_time:
 		return types.DecodeValue(vec.GetRawBytesAt(rowIdx), vec.GetType().Oid)
 	case types.T_bool:
 		return vector.GetFixedAtNoTypeCheck[bool](vec, rowIdx)
@@ -712,6 +713,8 @@ func formatPickKeyVectorValueAsString(ses *Session, vec *vector.Vector, rowIdx i
 		return vector.GetFixedAtNoTypeCheck[types.Decimal64](vec, rowIdx).Format(vec.GetType().Scale), nil
 	case types.T_decimal128:
 		return vector.GetFixedAtNoTypeCheck[types.Decimal128](vec, rowIdx).Format(vec.GetType().Scale), nil
+	case types.T_decimal256:
+		return vector.GetFixedAtNoTypeCheck[types.Decimal256](vec, rowIdx).Format(vec.GetType().Scale), nil
 	case types.T_date:
 		return vector.GetFixedAtNoTypeCheck[types.Date](vec, rowIdx).String(), nil
 	case types.T_datetime:
@@ -1221,9 +1224,49 @@ func numericGap(a, b []byte, pkType types.Type) float64 {
 		va := types.Decimal128ToFloat64(types.DecodeDecimal128(a), pkType.Scale)
 		vb := types.Decimal128ToFloat64(types.DecodeDecimal128(b), pkType.Scale)
 		return math.Abs(vb - va)
+	case types.T_decimal256:
+		return decimal256AbsGap(
+			types.DecodeDecimal256(a),
+			types.DecodeDecimal256(b),
+			pkType.Scale,
+		)
 	default:
 		return 0
 	}
+}
+
+func decimal256AbsGap(a, b types.Decimal256, scale int32) float64 {
+	ai := decimal256ToBigInt(a)
+	bi := decimal256ToBigInt(b)
+	diff := new(big.Int).Sub(bi, ai)
+	diff.Abs(diff)
+
+	f := new(big.Float).SetPrec(256).SetInt(diff)
+	if scale > 0 {
+		divisor := new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(scale)), nil)
+		f.Quo(f, new(big.Float).SetPrec(256).SetInt(divisor))
+	}
+	v, _ := f.Float64()
+	return v
+}
+
+func decimal256ToBigInt(x types.Decimal256) *big.Int {
+	negative := x.Sign()
+	if negative {
+		x = x.Minus()
+	}
+
+	n := new(big.Int).SetUint64(x.B192_255)
+	n.Lsh(n, 64)
+	n.Add(n, new(big.Int).SetUint64(x.B128_191))
+	n.Lsh(n, 64)
+	n.Add(n, new(big.Int).SetUint64(x.B64_127))
+	n.Lsh(n, 64)
+	n.Add(n, new(big.Int).SetUint64(x.B0_63))
+	if negative {
+		n.Neg(n)
+	}
+	return n
 }
 
 func isNumericType(oid types.T) bool {
@@ -1231,7 +1274,7 @@ func isNumericType(oid types.T) bool {
 	case types.T_int8, types.T_int16, types.T_int32, types.T_int64,
 		types.T_uint8, types.T_uint16, types.T_uint32, types.T_uint64,
 		types.T_float32, types.T_float64,
-		types.T_decimal64, types.T_decimal128:
+		types.T_decimal64, types.T_decimal128, types.T_decimal256:
 		return true
 	default:
 		return false
@@ -1351,6 +1394,12 @@ func appendNumericStringToVec(vec *vector.Vector, s string, pkType types.Type, t
 		return vector.AppendFixed(vec, v, false, mp)
 	case types.T_decimal128:
 		v, err := types.ParseDecimal128(s, pkType.Width, pkType.Scale)
+		if err != nil {
+			return err
+		}
+		return vector.AppendFixed(vec, v, false, mp)
+	case types.T_decimal256:
+		v, err := types.ParseDecimal256(s, pkType.Width, pkType.Scale)
 		if err != nil {
 			return err
 		}
