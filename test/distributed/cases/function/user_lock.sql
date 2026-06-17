1eeb08006ba817ddf81fcdd31d2425986e6287b5
daviszhen <daviszhen007@gmail.com>
2026-06-17 18:25:37 +0800

    add bvt

---
+++ test/distributed/cases/function/user_lock.sql
@@ -0,0 +1,49 @@
+-- @suite
+-- @case
+-- @desc:test mysql-compatible user-level lock functions
+-- @label:bvt
+
+drop database if exists user_lock_bvt_db;
+create database user_lock_bvt_db;
+use user_lock_bvt_db;
+
+create table user_lock_bvt_holder (conn_id bigint unsigned);
+
+select is_free_lock('user_lock_bvt_lock');
+select is_used_lock('user_lock_bvt_lock') is null;
+select release_lock('user_lock_bvt_lock') is null;
+
+-- @session:id=1{
+use user_lock_bvt_db;
+insert into user_lock_bvt_holder values (connection_id());
+select get_lock('user_lock_bvt_lock', 0);
+select get_lock('user_lock_bvt_lock', 0);
+-- @session}
+
+select is_free_lock('user_lock_bvt_lock');
+select get_lock('user_lock_bvt_lock', 0);
+select is_used_lock('user_lock_bvt_lock') = (select conn_id from user_lock_bvt_holder);
+select release_lock('user_lock_bvt_lock');
+
+-- @session:id=2{
+use user_lock_bvt_db;
+select is_used_lock('user_lock_bvt_lock') = (select conn_id from user_lock_bvt_holder);
+select release_all_locks();
+-- @session}
+
+-- @session:id=1{
+use user_lock_bvt_db;
+select is_free_lock('user_lock_bvt_lock');
+select release_lock('user_lock_bvt_lock');
+select is_free_lock('user_lock_bvt_lock');
+select release_all_locks();
+-- @session}
+
+select is_free_lock('user_lock_bvt_lock');
+select is_used_lock('user_lock_bvt_lock') is null;
+select get_lock('user_lock_bvt_lock', 0);
+select release_all_locks();
+select release_all_locks();
+
+drop table user_lock_bvt_holder;
+drop database user_lock_bvt_db;
