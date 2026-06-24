3858289f53a06f7bbb469bcb258ed09faa9a6ad1
iamlinjunhong <1030420200@qq.com>
2026-06-24 16:50:37 +0800

    fix(vector): address include review blockers

--- pkg/sql/plan/build_alter_change_column.go
+++ pkg/sql/plan/build_alter_change_column.go
@@ -89,7 +89,7 @@ func ChangeColumn(
 	}
 
 	if oldColName != newColName {
-		sqls, err := renameColumnInVectorIndexIncludedColumns(tableDef, oldColName, newColName)
+		sqls, err := handleAlterRenameColumnWithPluginHooks(tableDef, oldColName, newColName)
 		if err != nil {
 			return false, err
 		}
---
+++ pkg/vectorindex/ivfflat/plugin/plan/alter.go
@@ -0,0 +1,31 @@
+// Copyright 2026 Matrix Origin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package plan
+
+import (
+	"github.com/matrixorigin/matrixone/pkg/catalog"
+	planplugin "github.com/matrixorigin/matrixone/pkg/indexplugin/plan"
+	planpb "github.com/matrixorigin/matrixone/pkg/pb/plan"
+)
+
+var _ planplugin.AlterColumnHooks = Hooks{}
+
+func (Hooks) HandleAlterDropColumn(_ *planpb.TableDef, indexDef *planpb.IndexDef, colName string) (bool, error) {
+	return planplugin.IncludedColumnAffected(indexDef, colName), nil
+}
+
+func (Hooks) HandleAlterRenameColumn(tableDef *planpb.TableDef, oldColName, newColName string) ([]string, error) {
+	return planplugin.RenameIncludedColumnsForAlgo(tableDef, catalog.MoIndexIvfFlatAlgo.ToString(), oldColName, newColName, false)
+}
