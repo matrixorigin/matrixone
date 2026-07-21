ade89ca1df0d2c01f6c3bae53f0bd2be60c55fa9
aptend <aptend@hotmail.com>
2026-07-22 00:34:23 +0800

    fix: align BVT with strict resource stats schemas

--- test/distributed/cases/log/query_cu.sql
+++ test/distributed/cases/log/query_cu.sql
@@ -9,8 +9,8 @@ select json_extract(@stats, '$[6]') val;
 select CAST(mo_cu(@stats, @duration) AS DECIMAL(32,6)) as cu, CAST(mo_cu(@stats, @duration, "total") AS DECIMAL(32,6)) cu_total, CAST(mo_cu(@stats, @duration, "cpu") AS DECIMAL(32,6)) cu_cpu, CAST(mo_cu(@stats, @duration, "mem") AS DECIMAL(32,6)) cu_mem, CAST(mo_cu(@stats, @duration, "ioin") AS DECIMAL(32,6)) cu_ioin, CAST(mo_cu(@stats, @duration, "ioout") AS DECIMAL(32,6)) cu_ioout, CAST(mo_cu(@stats, @duration, "network") AS DECIMAL(32,6)) cu_network;
 select CAST(JSON_UNQUOTE(JSON_EXTRACT(@stats, '$[1]')) * 3.45e-14 / 1.002678e-06  AS DECIMAL(32,6)) - CAST(mo_cu(@stats, @duration, "cpu") AS DECIMAL(32,6)) val;
 select CAST(JSON_UNQUOTE(JSON_EXTRACT(@stats, '$[2]')) * 4.56e-24 * @duration / 1.002678e-06 AS DECIMAL(32,6)) - CAST(mo_cu(@stats, @duration, "mem") AS DECIMAL(32,6)) val;
-select mo_cu('[1,1,2,3,4,5,0,0]', 0) val;
-select mo_cu('[3,1,2,3,4,5,0,0]', 0) val;
+select mo_cu('[1,1,2,3,4]', 0) val;
+select mo_cu('[3,1,2,3,4,5,0]', 0) val;
 -- old version, value: 0
 select mo_cu('[4,1,2,3,4,5,6,7,8]', 0, 'iodelete') val;
 select mo_cu('[4,1,2,3,4,5,6,7,8]', 0, 'iolist') val;
