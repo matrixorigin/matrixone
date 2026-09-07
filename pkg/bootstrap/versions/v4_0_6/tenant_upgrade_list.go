749bac4f53333dca85353a650b22fb60947a2f5e
iamlinjunhong <1030420200@qq.com>
2026-09-07 20:26:11 +0800

    fix: advance view metadata protocol gate

--- pkg/bootstrap/versions/v4_0_6/tenant_upgrade_list.go
+++ pkg/bootstrap/versions/v4_0_6/tenant_upgrade_list.go
@@ -89,7 +89,7 @@ func upgradeInformationSchemaMetadataVisibilityView(viewName, viewDDL string) ve
 	} else if viewName == "VIEWS" {
 		// The definition function is encoded into remotely executed plans. Do not
 		// install this catalog contract until every CN can resolve function IDs 578 and 579.
-		requiredProtocol = defines.MORPCVersion55
+		requiredProtocol = defines.MORPCVersion56
 	}
 	return versions.UpgradeEntry{
 		Schema:                  sysview.InformationDBConst,
