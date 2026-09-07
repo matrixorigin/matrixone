aefd3eb04a2eda3de0fda22531917a5f156d5b69
VioletQwQ-0 <1659648118@qq.com>
2026-09-07 16:51:44 +0800

    feat(json): add JSON storage size and free functions

--- pkg/sql/plan/function/list_builtIn.go
+++ pkg/sql/plan/function/list_builtIn.go
@@ -1600,6 +1600,68 @@ var supportedStringBuiltIns = []FuncNew{
 		},
 	},
 
+	// function `json_storage_size`
+	{
+		functionId: JSON_STORAGE_SIZE,
+		class:      plan.Function_STRICT,
+		layout:     STANDARD_FUNCTION,
+		checkFn:    jsonStorageTypeMatch,
+
+		Overloads: []overload{
+			{
+				overloadId: 0,
+				args:       []types.T{types.T_json},
+				retType: func(parameters []types.Type) types.Type {
+					return types.T_int64.ToType()
+				},
+				newOp: func() executeLogicOfOverload {
+					return JsonStorageSize
+				},
+			},
+			{
+				overloadId: 1,
+				args:       []types.T{types.T_varchar},
+				retType: func(parameters []types.Type) types.Type {
+					return types.T_int64.ToType()
+				},
+				newOp: func() executeLogicOfOverload {
+					return JsonStorageSize
+				},
+			},
+		},
+	},
+
+	// function `json_storage_free`
+	{
+		functionId: JSON_STORAGE_FREE,
+		class:      plan.Function_STRICT,
+		layout:     STANDARD_FUNCTION,
+		checkFn:    jsonStorageTypeMatch,
+
+		Overloads: []overload{
+			{
+				overloadId: 0,
+				args:       []types.T{types.T_json},
+				retType: func(parameters []types.Type) types.Type {
+					return types.T_int64.ToType()
+				},
+				newOp: func() executeLogicOfOverload {
+					return JsonStorageFree
+				},
+			},
+			{
+				overloadId: 1,
+				args:       []types.T{types.T_varchar},
+				retType: func(parameters []types.Type) types.Type {
+					return types.T_int64.ToType()
+				},
+				newOp: func() executeLogicOfOverload {
+					return JsonStorageFree
+				},
+			},
+		},
+	},
+
 	// function `json_keys`
 	{
 		functionId: JSON_KEYS,
