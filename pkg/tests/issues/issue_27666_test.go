940256bebe805075576d3198056d3db37448cd85
XuPeng-SH <xupeng3112@163.com>
2026-09-10 07:08:32 +0800

    Revert "test: remove unused CDC transaction helper"
    This reverts commit 7eee9f6cf28134974a3be36502272d2dc6e48424.

--- pkg/tests/issues/issue_27666_test.go
+++ pkg/tests/issues/issue_27666_test.go
@@ -433,6 +433,11 @@ func execInternalSQL(ctx context.Context, sqlExec executor.SQLExecutor, statemen
 	return err
 }
 
+func execInternalTxnSQL(txn executor.TxnExecutor, statement string) error {
+	_, err := execInternalTxnSQLWithAffectedRows(txn, statement)
+	return err
+}
+
 func execInternalTxnSQLWithAffectedRows(txn executor.TxnExecutor, statement string) (uint64, error) {
 	result, err := txn.Exec(statement, executor.StatementOption{}.WithAccountID(0))
 	affectedRows := result.AffectedRows
