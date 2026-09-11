d9e8427c5ea670ff901471dd30ee9ff88ceccc75
iamlinjunhong <1030420200@qq.com>
2026-09-11 09:38:34 +0800

    fix: align view metadata protocol after rebase

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -8004,7 +8004,7 @@ func supportsRemoteViewDefinitionFunction(service string) bool {
 		return false
 	}
 	protocolVersion, ok := version.(int64)
-	return ok && protocolVersion >= defines.MORPCVersion61
+	return ok && protocolVersion >= defines.MORPCVersion63
 }
 
 func supportsRemoteParquetWholeFileFanout(service string) bool {
--- pkg/util/sysview/sysview.go
+++ pkg/util/sysview/sysview.go
@@ -76,7 +76,7 @@ var (
 )
 
 func InitInformationSchemaSysTablesForProtocol(protocol int64) []string {
-	if protocol >= defines.MORPCVersion61 {
+	if protocol >= defines.MORPCVersion63 {
 		return InitInformationSchemaSysTables
 	}
 
