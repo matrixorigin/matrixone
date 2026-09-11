22bfcce3ffed895c630ca379389771c72159f9be
jiangxinmeng <jiangxinmeng@matrixorigin.io>
2026-09-10 21:46:30 -0400

    test(cdc): accept protocol 63 boundary

--- pkg/cdc/types_test.go
+++ pkg/cdc/types_test.go
@@ -898,8 +898,8 @@ func TestValidateStableInitialSnapshotProtocol(t *testing.T) {
 }
 
 func TestValidateLosslessNoFullStartProtocolBoundary(t *testing.T) {
-	require.ErrorContains(t, ValidateLosslessNoFullStartProtocol(context.Background(), defines.MORPCVersion60), "protocol version 61")
-	require.NoError(t, ValidateLosslessNoFullStartProtocol(context.Background(), defines.MORPCVersion61))
+	require.ErrorContains(t, ValidateLosslessNoFullStartProtocol(context.Background(), defines.MORPCVersion72), "protocol version 73")
+	require.NoError(t, ValidateLosslessNoFullStartProtocol(context.Background(), defines.MORPCVersion73))
 }
 
 func TestActiveRoutine_ClosePause(t *testing.T) {
