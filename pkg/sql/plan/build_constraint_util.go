7fa988a7b50fe4445f9a3b5f3f4511000bae45ee
VioletQwQ-0 <1659648118@qq.com>
2026-09-11 13:22:28 +0800

    fix: return planner error with nil plan

--- pkg/sql/plan/build_constraint_util.go
+++ pkg/sql/plan/build_constraint_util.go
@@ -2263,7 +2263,7 @@ func buildValueScan(
 			}
 			if rowAlias != nil {
 				if _, ok := colToIdx[expr.Names[0].ColName()]; !ok {
-					return moerr.NewBadFieldErrorf(builder.GetContext(),
+					return nil, moerr.NewBadFieldErrorf(builder.GetContext(),
 						"invalid input: column '%s' does not exist", expr.Names[0].ColNameOrigin())
 				}
 				for _, offset := range collectParamExprOffsets(expr.Expr) {
