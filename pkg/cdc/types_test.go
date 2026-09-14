a370168ccaa19b4823c9df10d2baaffa06713306
jiangxinmeng <jiangxinmeng@matrixorigin.io>
2026-09-13 22:39:02 -0400

    test(cdc): align protocol boundary with v67

--- pkg/cdc/types_test.go
+++ pkg/cdc/types_test.go
@@ -908,8 +908,8 @@ func TestValidateStableInitialSnapshotProtocol(t *testing.T) {
 }
 
 func TestValidateLosslessNoFullStartProtocolBoundary(t *testing.T) {
-	require.ErrorContains(t, ValidateLosslessNoFullStartProtocol(context.Background(), defines.MORPCVersion63), "protocol version 64")
-	require.NoError(t, ValidateLosslessNoFullStartProtocol(context.Background(), defines.MORPCVersion64))
+	require.ErrorContains(t, ValidateLosslessNoFullStartProtocol(context.Background(), defines.MORPCVersion66), "protocol version 67")
+	require.NoError(t, ValidateLosslessNoFullStartProtocol(context.Background(), defines.MORPCVersion67))
 }
 
 func TestActiveRoutine_ClosePause(t *testing.T) {
