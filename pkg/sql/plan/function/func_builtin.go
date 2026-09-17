d6d65100e84b1d762b3d987e2ceebff1089c778c
VioletQwQ-0 <1659648118@qq.com>
2026-09-17 17:24:18 +0800

    fix: preserve single argument CONCAT binding

--- pkg/sql/plan/function/func_builtin.go
+++ pkg/sql/plan/function/func_builtin.go
@@ -997,7 +997,7 @@ func builtInInternalCharacterSet(parameters []*vector.Vector, result vector.Func
 }
 
 func builtInConcatCheck(overloads []overload, inputs []types.Type) checkResult {
-	if len(inputs) > 1 {
+	if len(inputs) > 0 {
 		hasJSON := false
 		shouldCast := false
 
