4c65bbef36883e137379f3a7433c785964554d01
Cao Kai <ck89119@gmail.com>
2026-09-17 19:42:56 +0800

    test: cover alter database compatibility privileges

--- test/distributed/cases/database/alter_database_collation_compatibility.sql
+++ test/distributed/cases/database/alter_database_collation_compatibility.sql
@@ -1,6 +1,16 @@
 drop database if exists alter_database_collation_compat;
 create database alter_database_collation_compat;
 alter database `alter_database_collation_compat` character set utf8mb4 collate utf8mb4_bin;
+create role alter_database_collation_role;
+grant connect on account * to alter_database_collation_role;
+create user alter_database_collation_user identified by '111' default role alter_database_collation_role;
+-- @session:id=1&user=sys:alter_database_collation_user:alter_database_collation_role&password=111
+alter database `alter_database_collation_compat` character set utf8mb4 collate utf8mb4_bin;
+-- @session
+grant ownership on database alter_database_collation_compat to alter_database_collation_role;
+-- @session:id=1&user=sys:alter_database_collation_user:alter_database_collation_role&password=111
+alter database `alter_database_collation_compat` character set utf8mb4 collate utf8mb4_bin;
+-- @session
 use alter_database_collation_compat;
 select database();
 alter database missing_alter_database_collation_compat character set utf8mb4 collate utf8mb4_bin;
@@ -14,4 +24,6 @@ select count(*) from t;
 prepare alter_database_collation_stmt from 'alter database `alter_database_collation_compat` character set utf8mb4 collate utf8mb4_bin';
 execute alter_database_collation_stmt;
 deallocate prepare alter_database_collation_stmt;
+drop user alter_database_collation_user;
+drop role alter_database_collation_role;
 drop database alter_database_collation_compat;
