f4e1ea51aad722ef58e42bfb1bfc4a9c48b02665
jiangxinmeng1 <51114574+jiangxinmeng1@users.noreply.github.com>
2026-09-21 15:36:25 +0800

    fix: preserve normal temporal cast folding

--- pkg/sql/plan/function/func_cast.go
+++ pkg/sql/plan/function/func_cast.go
@@ -8712,7 +8712,7 @@ func strToDate(proc *process.Process,
 	isBinary := from.GetSourceVector().GetIsBin()
 	assignmentCast := mode.isAssignment()
 	allowInvalidDates := false
-	if assignmentCast || mode == castModeExplicit || mode == castModeNormal {
+	if assignmentCast || mode == castModeExplicit {
 		var err error
 		allowInvalidDates, err = process.ResolveAllowInvalidDates(proc)
 		if err != nil {
@@ -8860,7 +8860,7 @@ func strToDatetime(proc *process.Process,
 	isBinary := from.GetSourceVector().GetIsBin()
 	assignmentCast := mode.isAssignment()
 	allowInvalidDates := false
-	if assignmentCast || mode == castModeExplicit || mode == castModeNormal {
+	if assignmentCast || mode == castModeExplicit {
 		var err error
 		allowInvalidDates, err = process.ResolveAllowInvalidDates(proc)
 		if err != nil {
